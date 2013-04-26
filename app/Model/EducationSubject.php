961fd74d51b69388909beee5d2ae23898b13de7c
Weijie <wlin@kordit.com>
2013-04-26 09:33:26 +0800

    PHPOE-251 changed base POT , base po files for all languages

--- app/Model/EducationSubject.php
+++ app/Model/EducationSubject.php
@@ -11,7 +11,7 @@ class EducationSubject extends AppModel {
 			),
 			'isUnique' => array(
 				'rule' => 'isUnique',
-				'message' => 'This subject is already exists in the system.'
+				'message' => 'This subject already exists in the system.'
 			)
 		)
 	);
