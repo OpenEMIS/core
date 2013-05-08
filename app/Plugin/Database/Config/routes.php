5819fa999a1a15286f56b6e6105b4543c2511b4f
Adrian Lee <alee@Adrians-iMac.local>
2013-05-08 14:18:31 +0800

    PHPOE-301 Fixed the Database Controller "parent" Breadcrumb

--- app/Plugin/Database/Config/routes.php
+++ app/Plugin/Database/Config/routes.php
@@ -1,4 +1,5 @@
 <?php
 Router::connect('/Database/Security/login', array('controller' => 'Security', 'action' => 'login'));
 Router::connect('/Database', array('plugin' => 'Database', 'controller' => 'Database'));
+Router::connect('/Database/Setup', array('controller' => 'Setup'));
 Router::connect('/Database/:action/*', array('plugin' => 'Database', 'controller' => 'Database'));
