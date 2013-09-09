60738379e730edb028af55235d4ec7642a2705a5
Faizal Hussain <fhussain@kordit.com>
2013-09-09 09:24:37 +0800

    PHPOE-186 - change

--- app/Plugin/Survey/webroot/js/survey.js
+++ app/Plugin/Survey/webroot/js/survey.js
@@ -148,7 +148,6 @@ var Survey = {
 			});
 			if(flag){
 				document.getElementById(n[0]+"Checked").checked = false;
-				document.getElementById(n[0]+"_row").className = 'table_row inactive';
 			}
 			var row = $(obj).closest('.table_row');
 			if(!row.hasClass('inactive')) {
