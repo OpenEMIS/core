7e8f472e802cb2e1ede5036fe195e8bedbdd8425
David Zhu <dzhu@kordit.com>
2013-12-16 16:40:35 +0800

    PHPOE-501 - Add 'Number of Seats' and 'Number of Shifts' to class

--- app/Controller/InstitutionSitesController.php
+++ app/Controller/InstitutionSitesController.php
@@ -946,6 +946,9 @@ class InstitutionSitesController extends AppController {
 			$this->set('grades', $grades);
 			$this->set('students', $students);
 			$this->set('teachers', $teachers);
+                        $this->set('no_of_seats', $classObj['InstitutionSiteClass']['no_of_seats']);
+                        $this->set('no_of_shifts', $classObj['InstitutionSiteClass']['no_of_shifts']);
+                        
             $this->set('subjects', $subjects);
 		} else {
 			$this->redirect(array('action' => 'classesList'));
@@ -971,6 +974,9 @@ class InstitutionSitesController extends AppController {
 			$this->set('grades', $grades);
 			$this->set('students', $students);
 			$this->set('teachers', $teachers);
+                        $this->set('no_of_seats', $classObj['InstitutionSiteClass']['no_of_seats']);
+                        $this->set('no_of_shifts', $classObj['InstitutionSiteClass']['no_of_shifts']);
+                        
             $this->set('subjects', $subjects);
 		} else {
 			$this->redirect(array('action' => 'classesList'));
