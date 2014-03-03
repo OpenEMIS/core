c7900a540542648a34c08bc26b842693542b1753
Jamie Sim <jsim@kordit.com>
2014-03-03 15:31:04 +0800

    PHPOE-696 Update the views

--- app/Plugin/Staff/Model/StaffTrainingSelfStudy.php
+++ app/Plugin/Staff/Model/StaffTrainingSelfStudy.php
@@ -61,13 +61,6 @@ class StaffTrainingSelfStudy extends StaffAppModel {
             	'message' => 'End Date must be greater than Start Date'
             )
         ),
-        'training_provider_id' => array(
-            'ruleRequired' => array(
-				'rule' => 'notEmpty',
-				'required' => true,
-				'message' => 'Please select a Course Provider.'
-			)
-        ),
 		'hours' => array(
 			'ruleRequired' => array(
 				'rule' => 'notEmpty',
