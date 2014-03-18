8cbbfe4db3887026068c88060dc2076820681af8
Jamie Sim <jsim@kordit.com>
2014-03-18 15:10:19 +0800

    PHPOE-760 Update navigation logic

--- app/Controller/Component/NavigationComponent.php
+++ app/Controller/Component/NavigationComponent.php
@@ -431,26 +431,25 @@ class NavigationComponent extends Component {
             if(($i+1)>=$currentLinkIndex){
                 $wizardLink[$i+1]['completed'] = '-1';
             }
-            $this->Session->write('WizardLink', $wizardLink);
 
          	$ConfigItem = ClassRegistry::init('ConfigItem');
        	
-
        		for($c=$i+1;$c<=count($wizardLink);$c++){
        			$mandatory = $ConfigItem->field('ConfigItem.value', array('ConfigItem.name' => strtolower($this->controller->className).'_'.$wizardLink[$c]['action'], 'ConfigItem.type' => 'Wizard - Add New '.$this->controller->className));
 
+       			$wizardLink[$c]['completed'] = '-1';
        			if($mandatory!='2'){
 					if(isset($wizardLink[$c]['new_id'])){
-
 					  	$this->Session->write('WizardLink', $wizardLink);
 		                $this->controller->redirect(array('action'=>$wizardLink[$c]['new_action'], $wizardLink[$c]['new_id']));
 		            }else{
-		            	  $this->Session->write('WizardLink', $wizardLink);
+	            	  	$this->Session->write('WizardLink', $wizardLink);
 		                $this->controller->redirect(array('action'=>$wizardLink[$c]['new_action']));
 		            }
 		            break;
        			}
        		}
+       		$this->Session->write('WizardLink', $wizardLink);
 	        $this->controller->redirect(array('action'=>$wizardLink[count($wizardLink)-1]['new_action']));
         }
     }
