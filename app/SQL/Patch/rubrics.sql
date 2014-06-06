1aaa40a572a2be52df8b24772535bf0a2a28d7aa
Jeff Zheng <jzheng@kordit.com>
2014-06-06 11:06:53 +0800

    Fixed institution site view area

--- app/SQL/Patch/rubrics.sql
+++ app/SQL/Patch/rubrics.sql
@@ -1,16 +1,16 @@
-RENAME TABLE `institution_site_class_teachers` TO `institution_site_class_staffs` ;
+-- RENAME TABLE `institution_site_class_teachers` TO `institution_site_class_staffs` ;
 
-ALTER TABLE `institution_site_class_staffs` CHANGE `teacher_id` `staff_id` INT( 11 ) NOT NULL ;
+-- ALTER TABLE `institution_site_class_staffs` CHANGE `teacher_id` `staff_id` INT( 11 ) NOT NULL ;
 
-ALTER TABLE `institution_site_class_staffs ` DROP INDEX `teacher_id`, ADD INDEX `staff_id` ( `staff_id` ) COMMENT ‘’;
+-- ALTER TABLE `institution_site_class_staffs ` DROP INDEX `teacher_id`, ADD INDEX `staff_id` ( `staff_id` );
 
 ALTER TABLE `quality_institution_rubrics` CHANGE `teacher_id` `staff_id` INT( 11 ) NOT NULL ;
 
-ALTER TABLE `quality_institution_rubrics ` DROP INDEX `teacher_id`, ADD INDEX `staff_id` ( `staff_id` ) COMMENT ‘’;
+ALTER TABLE `quality_institution_rubrics` DROP INDEX `teacher_id`, ADD INDEX `staff_id` ( `staff_id` );
 
 ALTER TABLE `quality_institution_visits` CHANGE `teacher_id` `staff_id` INT( 11 ) NOT NULL ;
 
-ALTER TABLE `quality_institution_visits` DROP INDEX `teacher_id`, ADD INDEX `staff_id` ( `staff_id` ) COMMENT ‘’;
+ALTER TABLE `quality_institution_visits` DROP INDEX `teacher_id`, ADD INDEX `staff_id` ( `staff_id` );
 
 ALTER TABLE `quality_institution_visit_attachments` ADD `visible` INT( 1 ) NOT NULL DEFAULT '1' AFTER `file_content` ;
 
