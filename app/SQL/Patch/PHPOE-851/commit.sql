311d35ad7c20ab7b251643f51fe57238ddc3b5b6
David Zhu <dzhu@kordit.com>
2014-07-02 12:43:54 +0800

    PHPOE-851 - Institution Assessments - sql statements

--- app/SQL/Patch/PHPOE-851/commit.sql
+++ app/SQL/Patch/PHPOE-851/commit.sql
@@ -11,3 +11,29 @@ UPDATE `security_functions` SET `_view` = 'behaviourStaffList|behaviourStaff|beh
 INSERT INTO `navigations` (`id`, `module`, `plugin`, `controller`, `header`, `title`, `action`, `pattern`, `attributes`, `parent`, `is_wizard`, `order`, `visible`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES
 (143, 'Institution', NULL, 'InstitutionSites', 'BEHAVIOURS', 'Students', 'behaviourStudentList', 'behaviourStudent', NULL, 3, 0, 35, 1, NULL, NULL, 1, '0000-00-00 00:00:00'),
 (144, 'Institution', NULL, 'InstitutionSites', 'BEHAVIOURS', 'Staff', 'behaviourStaffList', 'behaviourStaff', NULL, 3, 0, 36, 1, NULL, NULL, 1, '0000-00-00 00:00:00');
+
+--
+-- for Assessments 
+--
+INSERT INTO `navigations` (
+`id` ,
+`module` ,
+`plugin` ,
+`controller` ,
+`header` ,
+`title` ,
+`action` ,
+`pattern` ,
+`attributes` ,
+`parent` ,
+`is_wizard` ,
+`order` ,
+`visible` ,
+`modified_user_id` ,
+`modified` ,
+`created_user_id` ,
+`created`
+)
+VALUES (
+NULL , 'Institution', NULL , 'InstitutionSites', 'ASSESSMENTS', 'Results', 'assessments', 'assessments', NULL , '3', '0', '146', '1', NULL , NULL , '1', '0000-00-00 00:00:00'
+);
