5619524a089e7762188e26c70f717ad9baebb440
Jeff Zheng <jzheng@kordit.com>
2014-07-09 11:11:55 +0800

    PHPOE-869 - Updated record order

--- app/SQL/Patch/PHPOE-869/commit.sql
+++ app/SQL/Patch/PHPOE-869/commit.sql
@@ -9,8 +9,8 @@ INSERT INTO `student_categories` (`id`, `name`, `order`, `visible`, `internation
 (2, 'Promoted (Transferred in)', 2, 1, NULL, NULL, 1, '2013-01-24 13:57:26', 1, '2010-10-12 14:57:00'),
 (3, 'Repeated', 3, 1, NULL, NULL, 1, '2013-01-24 13:57:26', 1, '2010-10-12 14:57:00'),
 (4, 'Repeated (Transferred in)', 4, 1, NULL, NULL, 1, '2013-01-24 13:57:26', 1, '2010-10-12 14:57:00'),
-(5, 'Transfer Student (Local)', 1, 1, NULL, NULL, 1, '2013-05-08 12:47:27', 1, '2013-01-24 13:57:26'),
-(6, 'Transfer Student (Overseas)', 2, 1, NULL, NULL, 1, '2013-05-08 12:47:27', 1, '2013-01-24 13:57:26');
+(5, 'Transfer Student (Local)', 5, 1, NULL, NULL, 1, '2013-05-08 12:47:27', 1, '2013-01-24 13:57:26'),
+(6, 'Transfer Student (Overseas)', 6, 1, NULL, NULL, 1, '2013-05-08 12:47:27', 1, '2013-01-24 13:57:26');
 
 
 Update institution_site_class_students set student_category_id=1
