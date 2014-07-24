f6ced6606a9362186cb619424d9bf380500e36b2
Vincent Wong <vwong@kordit.com>
2014-07-24 17:59:53 +0800

    PHPOE-934 - Converted from FusionCharts to HighCharts

---
+++ app/Plugin/Visualizer/webroot/js/visualizer.visualization.js
@@ -0,0 +1,21 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(document).ready(function() {
+	if($('#highchart-container').length > 0){
+		var container = $('#highchart-container');
+		
+		$.ajax({
+			type: "POST",
+			dataType: 'json',
+			url: getRootURL() + 'Visualizer/VisualizeHighChart/' + container.attr('type') + '/' + container.attr('ref'),
+			//data: {searchStr: $(this).val(), areaLvl: $('#areaLevel').val()},
+			success: function(data) {
+				$('#highchart-container').highcharts(data);
+			}
+		});
+	}
+});
