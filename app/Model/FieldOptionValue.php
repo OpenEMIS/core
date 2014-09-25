8ec214dc11135f456679b2439b8d7837b2cbeaaf
jzheng <jzheng@kordit.com>
2014-09-25 09:34:41 +0800

    Fix FieldOptionValue.getIdByName

--- app/Model/FieldOptionValue.php
+++ app/Model/FieldOptionValue.php
@@ -160,15 +160,5 @@ class FieldOptionValue extends AppModel {
 		
 		return !empty($result) ? $result[$alias]['id'] : NULL;
 	}
-	
-	public function getIdByName($name) {
-		$alias = $this->alias;
-		
-		$result = $this->find('first', array(
-			'conditions' => array($alias.'.name' => $name, 'FieldOption.code' => $alias)
-		));
-		
-		return !empty($result) ? $result[$alias]['id'] : NULL;
-	}
 }
 ?>
