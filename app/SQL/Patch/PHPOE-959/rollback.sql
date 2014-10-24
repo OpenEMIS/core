eaa7afe1b98da23b529823f81e73b6a5534e6f7c
David Zhu <dzhu@kordit.com>
2014-10-24 14:26:21 +0800

    PHPOE-959 - solve report with missing rows

--- app/SQL/Patch/PHPOE-959/rollback.sql
+++ app/SQL/Patch/PHPOE-959/rollback.sql
@@ -24,3 +24,11 @@ $this->InstitutionSiteCustomValue->bindModel(array(
 $data = $this->InstitutionSiteCustomValue->find('all',array('fields'=>array('InstitutionSite.name AS InstitutionName','InstitutionSiteCustomField.name AS Information','InstitutionSiteCustomField.type AS Type','InstitutionSiteCustomValue.value AS Value','InstitutionSiteCustomFieldOption.value AS Option'),{cond}));foreach($data as $key => &$value) { if ($value['InstitutionSiteCustomField']['Type'] != 2 && $value['InstitutionSiteCustomField']['Type'] != 5) { $value['InstitutionSiteCustomValue']['Value'] = $value['InstitutionSiteCustomFieldOption']['Option']; }}" 
 WHERE `report_id` = @customFieldsReportId;
 
+
+SET @studentReportId := 0;
+
+SELECT `id` INTO @studentReportId FROM `reports` WHERE `module` LIKE 'Institution Totals' AND `category` LIKE 'Institution Totals Reports' AND `name` LIKE 'Student';
+
+UPDATE `batch_reports` SET `query` = "$data = $this->CensusStudent->find('all',array('fields'=>array('SchoolYear.name AS AcademicYear','InstitutionSite.name AS InstitutionName','EducationGrade.name AS EducationGradeName','StudentCategory.name AS Category','CensusStudent.male AS Male','CensusStudent.female AS Female'),{cond}));",
+`template` = "AcademicYear,InstitutionName,EducationGradeName,Category,Male,Female" 
+WHERE `report_id` = @studentReportId;
