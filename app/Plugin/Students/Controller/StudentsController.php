7eceaa70820cbd70762ee89b4f18cc4fb9762692
Thed Kuok Shiun <kuokshiun@hotmail.com>
2014-11-21 14:00:52 +0800

    PHPOE-1001 - Bugs fixed. 1) Delete Student.wizard when click on advanced search. 2) Apply the same fix to Staff. 3) Fix not able to view Student after click Add new Student.

--- app/Plugin/Students/Controller/StudentsController.php
+++ app/Plugin/Students/Controller/StudentsController.php
@@ -91,8 +91,9 @@ class StudentsController extends StudentsAppController {
 			$this->bodyTitle = __('New Student');
 			$studentId = $this->Session->read('Student.id');
 			if (empty($studentId)) {
+				$skipActions = array('InstitutionSiteStudent', 'edit', 'view', 'add');
 				$wizardActions = $this->Wizard->getAllActions('Student');
-				if($this->action != 'InstitutionSiteStudent' && $this->action != 'edit' && !in_array($this->action, $wizardActions)){
+				if(!in_array($this->action, $skipActions) && !in_array($this->action, $wizardActions)){
 					return $this->redirect(array('action' => 'edit'));
 				}
 			}
@@ -191,6 +192,8 @@ class StudentsController extends StudentsAppController {
 						$this->redirect(array('action' => 'index'));
 					}
 				}
+
+				$this->Session->delete('Student.wizard');
 			}
 		} else {
 			$search = $this->request->data;
