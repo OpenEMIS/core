a44cb10c0d420979ef00e0cf2f692ae9c1eebb95
David Zhu <dzhu@kordit.com>
2014-11-27 10:28:04 +0800

    PHPOE-1090 - fixed the bug regarding permission of Advanced Search and Preferences

--- app/Controller/Component/AccessControlComponent.php
+++ app/Controller/Component/AccessControlComponent.php
@@ -28,7 +28,7 @@ class AccessControlComponent extends Component {
 	
 	public $ignoreList = array(
 		'HOME' => array('index', 'support', 'systemInfo', 'license', 'partners'),
-		'PREFERENCES' => array('account', 'accountEdit', 'password'),
+		'PREFERENCES' => array('index', 'account', 'accountEdit', 'password'),
 		'SECURITY' => array('login', 'logout'), 
 		'CONFIG' => array('getI18n', 'getJSConfig', 'fetchImage'),
 		'STUDENTS' => array('viewStudent'),
@@ -301,7 +301,7 @@ class AccessControlComponent extends Component {
 						}
 						
 					}else{
-						if($controller == "INSTITUTIONSITES" && ($action == 'index' || $action == 'add')){
+						if($controller == "INSTITUTIONSITES" && ($action == 'index' || $action == 'add' || $action == 'advanced')){
 							$access = isset($check[$controller][$action]) ? $check[$controller][$action] : false;
 							
 						}else{
