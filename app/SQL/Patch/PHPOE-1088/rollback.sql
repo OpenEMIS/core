f343baad385880d713bd115c612106a14a4703aa
Thed Kuok Shiun <kuokshiun@hotmail.com>
2014-12-05 09:07:36 +0800

    PHPOE-1088 - Task completed. Only SQL patch file to be executed. Applied the changes to Permissions page.

--- app/SQL/Patch/PHPOE-1088/rollback.sql
+++ app/SQL/Patch/PHPOE-1088/rollback.sql
@@ -5,3 +5,12 @@ UPDATE `navigations` SET `order` = '7' WHERE `navigations`.`id` = 7;
 UPDATE `navigations` SET `order` = '9' WHERE `navigations`.`id` = 9;
 
 UPDATE `navigations` SET `header` = 'General', `order` = '4' WHERE `navigations`.`id` = 4;
+
+
+UPDATE `security_functions` SET `category` = 'General', `order` = '3' WHERE `security_functions`.`id` = 187;
+
+UPDATE `security_functions` SET `order` = '4' WHERE `security_functions`.`id` = 10;
+
+UPDATE `security_functions` SET `order` = '5' WHERE `security_functions`.`id` = 12;
+
+UPDATE `security_functions` SET `order` = '7' WHERE `security_functions`.`id` = 14;
