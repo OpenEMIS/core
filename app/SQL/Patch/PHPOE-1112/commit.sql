3f4f07f1b4401efa04a831bb28d851eb472dbe96
Thed Kuok Shiun <kuokshiun@hotmail.com>
2014-12-09 09:13:49 +0800

    PHPOE-1112 - Bugs fixed. SQL patch file to be executed.

---
+++ app/SQL/Patch/PHPOE-1112/commit.sql
@@ -0,0 +1,3 @@
+UPDATE `security_functions` SET `_view` = 'InstitutionSiteStudentAbsence|InstitutionSiteStudentAbsence.index|InstitutionSiteStudentAbsence.view|InstitutionSiteStudentAbsence.absence' WHERE `security_functions`.`id` = 16;
+
+UPDATE `security_functions` SET `_view` = 'InstitutionSiteStaffAbsence|InstitutionSiteStaffAbsence.index|InstitutionSiteStaffAbsence.view|InstitutionSiteStaffAbsence.absence' WHERE `security_functions`.`id` = 29;
