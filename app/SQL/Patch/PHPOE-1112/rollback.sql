c7b68c682fbb7d159f4a60040ac737cefed5096d
Thed Kuok Shiun <kuokshiun@hotmail.com>
2014-12-10 14:38:38 +0800

    PHPOE-1112 - Change SQL where clause.

--- app/SQL/Patch/PHPOE-1112/rollback.sql
+++ app/SQL/Patch/PHPOE-1112/rollback.sql
@@ -1,3 +1,13 @@
-UPDATE `security_functions` SET `_view` = 'InstitutionSiteStudentAbsence|InstitutionSiteStudentAbsence.index|InstitutionSiteStudentAbsence.absence' WHERE `security_functions`.`id` = 16;
+UPDATE 	`security_functions`
+SET 	`_view` = 'InstitutionSiteStudentAbsence|InstitutionSiteStudentAbsence.index|InstitutionSiteStudentAbsence.absence'
+WHERE 	`security_functions`.`name` = 'Students'
+		AND `security_functions`.`controller` = 'InstitutionSites'
+		AND `security_functions`.`module` = 'Institutions'
+		AND `security_functions`.`category` = 'Attendance';
 
-UPDATE `security_functions` SET `_view` = 'InstitutionSiteStaffAbsence|InstitutionSiteStaffAbsence.index|InstitutionSiteStaffAbsence.absence' WHERE `security_functions`.`id` = 29;
+UPDATE 	`security_functions`
+SET 	`_view` = 'InstitutionSiteStaffAbsence|InstitutionSiteStaffAbsence.index|InstitutionSiteStaffAbsence.absence'
+WHERE 	`security_functions`.`name` = 'Staff'
+		AND `security_functions`.`controller` = 'InstitutionSites'
+		AND `security_functions`.`module` = 'Institutions'
+		AND `security_functions`.`category` = 'Attendance';
