2d2e17c892689a656571d16864c8f1440b0f428e
Malcolm Lee <mlee@kordit.com>
2014-12-12 19:12:47 +0800

    PHPOE-1113 Absence class to section bug

--- app/Model/InstitutionSiteStudentAbsence.php
+++ app/Model/InstitutionSiteStudentAbsence.php
@@ -628,7 +628,7 @@ class InstitutionSiteStudentAbsence extends AppModel {
 			$yearId = $defaultYearId;
 		}
 		
-		$sectionOptions = $this->InstitutionSiteClass->getClassListByInstitution($institutionSiteId, $yearId);
+		$sectionOptions = $this->InstitutionSiteSection->getSectionListByInstitution($institutionSiteId, $yearId);
 		if(!empty($sectionOptions)){
 			if ($sectionId != 0) {
 				if (!array_key_exists($sectionId, $sectionOptions)) {
