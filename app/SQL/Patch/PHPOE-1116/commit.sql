8453029732b7f3ba8205619fd1e36745d2b40cea
David Zhu <dzhu@kordit.com>
2014-12-16 17:52:30 +0800

    PHPOE-1116 - commit sql statements

--- app/SQL/Patch/PHPOE-1116/commit.sql
+++ app/SQL/Patch/PHPOE-1116/commit.sql
@@ -1,1 +1,3 @@
-DELETE FROM `navigations` WHERE 
+UPDATE `navigations` SET `visible` = 0 WHERE `module` LIKE 'Student' AND `plugin` LIKE 'Students' AND `title` LIKE 'Add new Student';
+
+UPDATE `navigations` SET `visible` = 0 WHERE `module` LIKE 'Staff' AND `plugin` LIKE 'Staff' AND `title` LIKE 'Add new Staff';
--- app/SQL/Patch/PHPOE-1116/rollback.sql
+++ app/SQL/Patch/PHPOE-1116/rollback.sql
@@ -1,1 +1,3 @@
+UPDATE `navigations` SET `visible` = 1 WHERE `module` LIKE 'Student' AND `plugin` LIKE 'Students' AND `title` LIKE 'Add new Student';
 
+UPDATE `navigations` SET `visible` = 1 WHERE `module` LIKE 'Staff' AND `plugin` LIKE 'Staff' AND `title` LIKE 'Add new Staff';
