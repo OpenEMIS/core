77903c9b7cc26f3a56a629baa850a04e43b73094
David Zhu <dzhu@kordit.com>
2014-12-17 09:22:00 +0800

    PHPOE-1116 - update sql statements

--- app/SQL/Patch/PHPOE-1116/rollback.sql
+++ app/SQL/Patch/PHPOE-1116/rollback.sql
@@ -1,3 +1,7 @@
 UPDATE `navigations` SET `visible` = 1 WHERE `module` LIKE 'Student' AND `plugin` LIKE 'Students' AND `title` LIKE 'Add new Student';
 
 UPDATE `navigations` SET `visible` = 1 WHERE `module` LIKE 'Staff' AND `plugin` LIKE 'Staff' AND `title` LIKE 'Add new Staff';
+
+UPDATE `navigations` SET `title` = 'Add existing Student' WHERE `module` LIKE 'Student' AND `plugin` LIKE 'Students' AND `title` LIKE 'Add Student';
+
+UPDATE `navigations` SET `title` = 'Add existing Staff' WHERE `module` LIKE 'Staff' AND `plugin` LIKE 'Staff' AND `title` LIKE 'Add Staff';
