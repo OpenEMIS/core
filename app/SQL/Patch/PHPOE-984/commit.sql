cb47a44b3a2eeb5a0b295bcb4462c85af859302e
Jeff Zheng <jzheng@kordit.com>
2014-12-29 09:40:30 +0800

    PHPOE-984 - Updated commit.sql

--- app/SQL/Patch/PHPOE-984/commit.sql
+++ app/SQL/Patch/PHPOE-984/commit.sql
@@ -9,21 +9,22 @@ UPDATE `navigations` SET `header` = 'Communications' WHERE `module` LIKE 'Admini
 -- Table structure for table `alerts`
 --
 
+DROP TABLE IF EXISTS `alerts`;
 CREATE TABLE `alerts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `code` varchar(50) COLLATE utf8_estonian_ci NOT NULL,
-  `name` varchar(100) COLLATE utf8_estonian_ci NOT NULL,
+  `code` varchar(50) NOT NULL,
+  `name` varchar(100) NOT NULL,
   `threshold` int(5) NOT NULL,
   `status` int(1) NOT NULL DEFAULT '1',
-  `method` varchar(50) COLLATE utf8_estonian_ci NOT NULL,
-  `subject` varchar(255) COLLATE utf8_estonian_ci NOT NULL,
-  `message` text COLLATE utf8_estonian_ci NOT NULL,
+  `method` varchar(50) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `message` text NOT NULL,
   `modified_user_id` int(5) DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   `created_user_id` int(5) NOT NULL,
   `created` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_estonian_ci AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `alerts`
@@ -49,6 +50,7 @@ INSERT INTO `navigations` (`id`, `module`, `plugin`, `controller`, `header`, `ti
 -- Table structure for table `alert_roles`
 --
 
+DROP TABLE IF EXISTS `alert_roles`;
 CREATE TABLE `alert_roles` (
   `id` char(36) NOT NULL,
   `alert_id` int(11) NOT NULL,
@@ -65,6 +67,7 @@ CREATE TABLE `alert_roles` (
 -- Table structure for table `alert_logs`
 --
 
+DROP TABLE IF EXISTS `alert_logs`;
 CREATE TABLE `alert_logs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `method` varchar(20) NOT NULL,
@@ -79,13 +82,14 @@ CREATE TABLE `alert_logs` (
   KEY `type` (`type`),
   KEY `security_user_id` (`created_user_id`),
   KEY `method` (`method`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 
 --
 -- Table structure for table `system_processes`
 --
 
+DROP TABLE IF EXISTS `system_processes`;
 CREATE TABLE `system_processes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
@@ -100,7 +104,7 @@ CREATE TABLE `system_processes` (
   `created` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `name` (`name`,`process_id`,`status`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 
 --
