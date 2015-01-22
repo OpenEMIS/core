a3f414ba5cfbae5502906dc2af0d0ff3c804f0f6
Thed Kuok Shiun <kuokshiun@hotmail.com>
2015-01-22 13:45:20 +0800

    PHPOE-1167 - Generate XForms to screen or download as file.

--- app/Controller/Component/CustomField2Component.php
+++ app/Controller/Component/CustomField2Component.php
@@ -14,6 +14,10 @@ have received a copy of the GNU General Public License along with this program. 
 <http://www.gnu.org/licenses/>.  For more information please wire to contact@openemis.org.
 */
 
+define("NS_XF", "http://www.w3.org/2002/xforms");
+define("NS_OE", "https://www.openemis.org");
+App::uses('Xml', 'Utility');
+
 class CustomField2Component extends Component {
 	private $controller;
 
@@ -540,7 +544,7 @@ class CustomField2Component extends Component {
 			$this->Field->updateAll(array($orderField => $orderField . ' - 1'), $updateConditions);
 		}
 
-		$params = array_merge(array('action' => 'reorder', $groupId), $params);
+		$params = array_merge(array('action' => 'reorder'), $params);
 		return $this->controller->redirect($params);
     }
 
@@ -632,8 +636,8 @@ class CustomField2Component extends Component {
 		$this->controller->autoRender = false;
 
     	$params = $this->controller->params->named;
-
-		$selectedModule = $this->checkModule();
+    	$selectedModule = $this->checkModule();
+		
 		if(!is_null($selectedModule)) {
 			$groupsConditions = array(
 				$this->Group->alias.'.'.Inflector::underscore($this->Module->alias).'_id' => $selectedModule
@@ -659,7 +663,7 @@ class CustomField2Component extends Component {
 			if(!empty($templates)) {
 				$protocol = $_SERVER['SERVER_PORT'] == '443' ? 'https://' : 'http://';
 				$host = $_SERVER['HTTP_HOST'];
-				$url = $protocol . $host . $this->controller->webroot . $this->controller->params->controller . '/download/';
+				$url = $protocol . $host . $this->controller->webroot . $this->controller->params->controller . '/download/xform/';
 
 				$list = array();
 				foreach ($templates as $key => $template) {
@@ -684,13 +688,16 @@ class CustomField2Component extends Component {
     	return json_encode($result);
     }
 
-    public function download($id=0, $format="xform", $output=true) {
+    public function download($format="xform", $id=0, $output=true) {
+		$this->controller->autoRender = false;
+		$groupName = $this->Group->field('name', array($this->Group->alias.'.id' => $id));
+
     	$fieldContains = array();
 		$fieldContains = isset($this->FieldOption) ? array_merge(array($this->FieldOption->alias), $fieldContains) : $fieldContains;
 		//$fieldContains = isset($this->TableColumn) ? array_merge(array($this->TableColumn->alias), $fieldContains) : $fieldContains;
 		//$fieldContains = isset($this->TableRow) ? array_merge(array($this->TableRow->alias), $fieldContains) : $fieldContains;
 		$this->Field->contain($fieldContains);
-		$data = $this->Field->find('all', array(
+		$fields = $this->Field->find('all', array(
 			'conditions' => array(
 				$this->Field->alias.'.'.Inflector::underscore($this->Group->alias).'_id' => $id,
 				$this->Field->alias.'.visible' => 1
@@ -701,22 +708,115 @@ class CustomField2Component extends Component {
 			)
 		));
 
-		$this->controller->set('data', $data);
-		$render = false;
 		if ($output) { // true = output to screen
 			switch ($format) {
 				case 'xform':
-					$this->controller->layout = false;
-					$render = 'xform';
+					$xml = $this->getXML($format, $groupName, $fields);
+					return $xml->asXML();
 					break;
 				default:
 					break;
 			}
 		} else { // download as file
+			$xml = $this->getXML($format, $groupName, $fields);
+			$fileName = $format . '_' . date('Ymdhis') . '.xml';
+		    
+		    header('Expires: 0');
+		    header('Content-Encoding: UTF-8');
+		    // force download  
+		    header("Content-Type: application/force-download; charset=UTF-8'");
+		    header("Content-Type: application/octet-stream; charset=UTF-8'");
+		    header("Content-Type: application/download; charset=UTF-8'");
+		    // disposition / encoding on response body
+		    header("Content-Disposition: attachment;filename={$fileName}");
+		    header("Content-Transfer-Encoding: binary");
 
+		   	if (ob_get_contents()){
+			    ob_end_clean();
+			}
+			ob_start();
+			$df = fopen("php://output", 'w');
+			fputs($df, $xml->asXML());
+			fclose($df);
+			return ob_get_clean();
 		}
-		if ($render !== false) {
-			$this->doRender($render);
-		}
+    }
+
+    public function getXML($instanceId, $title, $fields) {
+		$xmlstr = '<?xml version="1.0" encoding="UTF-8"?>
+    				<html
+    					xmlns="http://www.w3.org/1999/xhtml"
+    					xmlns:xf="' .NS_XF. '"
+	    				xmlns:ev="http://www.w3.org/2001/xml-events"
+	    				xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+	    				xmlns:oe="' .NS_OE. '">
+					</html>';
+
+    	$xml = new SimpleXMLElement($xmlstr);
+
+		$headNode = $xml->addChild("head");
+			$headNode->addChild("title", $title);
+				$modelNode = $headNode->addChild("model", null, NS_XF);
+					$instanceNode = $modelNode->addChild("instance", null, NS_XF);
+					$instanceNode->addAttribute("id", $instanceId);
+						$index = 1;
+						foreach ($fields as $key => $field) {
+							if(isset($this->Group) && $key == 0) {
+								$groupNode = $instanceNode->addChild($this->Group->alias, null, NS_OE);
+									$groupNode->addChild('id', $field[$this->Field->alias][Inflector::underscore($this->Group->alias).'_id'], NS_OE);
+							}
+
+							if($field[$this->Field->alias]['type'] < 7) { //Skip Table
+								$fieldNode = $instanceNode->addChild($this->Field->alias, null, NS_OE);
+									$fieldNode->addChild('id', $field[$this->Field->alias]['id'], NS_OE);
+									$fieldNode->addChild('name', $field[$this->Field->alias]['name'], NS_OE);
+									$fieldNode->addChild('type', $field[$this->Field->alias]['type'], NS_OE);
+									$fieldNode->addChild('isMandatory', $field[$this->Field->alias]['is_mandatory'], NS_OE);
+									$fieldNode->addChild('isUnique', $field[$this->Field->alias]['is_unique'], NS_OE);
+									$fieldNode->addChild('value', null, NS_OE);
+
+								$bindNode = $modelNode->addChild("bind", null, NS_XF);
+								$bindNode->addAttribute("nodeset", "instance('" . $instanceId . "')/question[".$index."]");
+								switch($field[$this->Field->alias]['type']) {
+									case 1:	//Section Break
+										$fieldType = 'string';
+										break;
+									case 2:	//Text
+										$fieldType = 'string';
+										break;
+									case 3:	//Dropdown
+										$fieldType = 'integer';
+										break;
+									case 4:	//Checkbox
+										$fieldType = 'integer';
+										break;
+									case 5:	//Textarea
+										$fieldType = 'string';
+										break;
+									case 6:	//Number
+										$fieldType = 'integer';
+										break;
+								}
+								$bindNode->addAttribute("type", $fieldType);
+								if($field[$this->Field->alias]['is_mandatory']) {
+									$bindNode->addAttribute("required", 'true()');
+								} else {
+									$bindNode->addAttribute("required", 'false()');
+								}
+
+								$index++;
+							}
+						}
+
+		$bodyNode = $xml->addChild("body");
+			$repeatNode = $bodyNode->addChild("repeat", null, NS_XF);
+			$repeatNode->addAttribute("nodeset", "instance('" . $instanceId . "')/question");
+				$inputNode = $repeatNode->addChild("input", null, NS_XF);
+				$inputNode->addAttribute("ref", "value");
+					$labelNode = $inputNode->addChild("label", null, NS_XF);
+						$outputNode = $labelNode->addChild("output", null, NS_XF);
+						$outputNode->addAttribute("ref", "name");
+
+    	return $xml;
     }
 }
