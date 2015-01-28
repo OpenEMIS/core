ff9c5605eced3cd79c4ac686f63633f7852b0d04
Malcolm Lee <mlee@kordit.com>
2015-01-28 15:51:40 +0800

    PHPOE-1190 Committing because completed SQL changes * InstitutionSiteClass added subject_id * config_items removed max_subject_per_class * institution_class_subject migrated
    Others
    * changed to controlleraction2
    * need more PHP coding

--- app/SQL/Patch/PHPOE-1190/commit.sql
+++ app/SQL/Patch/PHPOE-1190/commit.sql
@@ -54,3 +54,32 @@ INSERT INTO `navigations` (
 ) 
 VALUES (NULL , 'Student', 'Students' , 'Students', 'Details', 'Sections', 'StudentSection', 'StudentSection|StudentSection.index', NULL , '62', '0', @orderOfStudentProgrammesNav + 1, '1', NULL , NULL , '1', '0000-00-00 00:00:00');
 
+
+
+
+
+-- Malcolm SQL START 
+UPDATE `navigations` SET `action` = 'InstitutionSiteClass', `pattern` = 'InstitutionSiteClass' WHERE controller = 'InstitutionSites' AND header = 'Details' AND title = 'Classes';
+ALTER TABLE `institution_site_classes` ADD `education_grade_subject_id` INT NULL DEFAULT NULL AFTER `academic_period_id`;
+
+CREATE TABLE IF NOT EXISTS 1190_config_items LIKE config_items;
+INSERT 1190_config_items SELECT * FROM config_items WHERE name = 'max_subjects_per_class';
+DELETE FROM config_items WHERE name = 'max_subjects_per_class';
+
+
+UPDATE
+      institution_site_classes t1
+  JOIN 
+      ( SELECT institution_site_class_subjects.institution_site_class_id, MIN(education_grade_subject_id) AS education_grade_subject_id
+        FROM institution_site_class_subjects 
+        GROUP BY institution_site_class_subjects.institution_site_class_id
+      ) AS t2
+  ON t1.id = t2.institution_site_class_id 
+SET t1.education_grade_subject_id = t2.education_grade_subject_id;
+
+-- SELECT * FROM institution_site_classes INNER JOIN institution_site_class_subjects ON institution_site_classes.id = institution_site_class_subjects.institution_site_class_id GROUP BY institution_site_classes.id ORDER BY institution_site_class_subjects.id; 
+
+RENAME TABLE institution_site_class_subjects to 1190_institution_site_class_subjects;
+
+-- Malcolm SQL END
+
