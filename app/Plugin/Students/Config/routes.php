29e0067fe95a6056d4efa652077ea58b330304d9
Jeff Zheng <jzheng@kordit.com>
2015-01-28 15:45:50 +0800

    PHPOE-954 - Added StaffReports

--- app/Plugin/Students/Config/routes.php
+++ app/Plugin/Students/Config/routes.php
@@ -3,6 +3,6 @@ Router::connect('/Students/Security/login', array('controller' => 'Security', 'a
 Router::connect('/Students', array('plugin' => 'Students', 'controller' => 'Students'));
 Router::connect('/Students/:action/*', array('plugin' => 'Students', 'controller' => 'Students'));
 
-Router::connect('/StudentReports/Security/login', array('controller' => 'StudentReports', 'action' => 'login'));
+Router::connect('/StudentReports/Security/login', array('controller' => 'Security', 'action' => 'login'));
 Router::connect('/StudentReports', array('plugin' => 'Students', 'controller' => 'StudentReports'));
 Router::connect('/StudentReports/:action/*', array('plugin' => 'Students', 'controller' => 'StudentReports'));
