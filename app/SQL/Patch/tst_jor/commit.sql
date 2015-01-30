aed7cec3304f3ead95556d0745cc64698993ddaa
Malcolm Lee <mlee@kordit.com>
2015-01-30 17:54:48 +0800

    PHPOE1190 Fixed migration SQL for updating subject for existing classes

--- app/SQL/Patch/tst_jor/commit.sql
+++ app/SQL/Patch/tst_jor/commit.sql
@@ -128,15 +128,31 @@ DELETE FROM config_items WHERE name = 'max_subjects_per_class';
 
 UPDATE
       institution_site_classes t1
-  JOIN 
+  INNER JOIN 
       ( SELECT institution_site_class_subjects.institution_site_class_id, MIN(education_grade_subject_id) AS education_grade_subject_id
         FROM institution_site_class_subjects 
         GROUP BY institution_site_class_subjects.institution_site_class_id
       ) AS t2
   ON t1.id = t2.institution_site_class_id 
-SET t1.education_grade_subject_id = t2.education_grade_subject_id;
+  INNER JOIN (
+  		SELECT education_grades_subjects.id, education_grades_subjects.education_subject_id
+  		FROM education_grades_subjects
+  	) AS t3
+  ON t2.education_grade_subject_id = t3.id
+SET t1.education_subject_id = t3.education_subject_id;
 
--- SELECT * FROM institution_site_classes INNER JOIN institution_site_class_subjects ON institution_site_classes.id = institution_site_class_subjects.institution_site_class_id GROUP BY institution_site_classes.id ORDER BY institution_site_class_subjects.id; 
+-- select * from institution_site_classes t1
+--   INNER JOIN 
+--       ( SELECT institution_site_class_subjects.institution_site_class_id, MIN(education_grade_subject_id) AS education_grade_subject_id
+--         FROM institution_site_class_subjects 
+--         GROUP BY institution_site_class_subjects.institution_site_class_id
+--       ) AS t2
+--   ON t1.id = t2.institution_site_class_id 
+--   INNER JOIN (
+--   		SELECT education_grades_subjects.id, education_grades_subjects.education_subject_id
+--   		FROM education_grades_subjects
+--   	) AS t3
+--   ON t2.education_grade_subject_id = t3.id
 
 RENAME TABLE institution_site_class_subjects to 1190_institution_site_class_subjects;
 
