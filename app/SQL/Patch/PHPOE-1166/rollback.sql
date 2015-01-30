7448b3e6fc282cc7d0ad6643650ca18d2ddc6b65
David Zhu <dzhu@kordit.com>
2015-01-30 10:32:00 +0800

    PHPOE-1166 - updates to permission for report for attendance & assessment

--- app/SQL/Patch/PHPOE-1166/rollback.sql
+++ app/SQL/Patch/PHPOE-1166/rollback.sql
@@ -1,1 +1,5 @@
+UPDATE `security_functions` SET `_execute` = NULL WHERE `module` LIKE 'Institutions' AND `category` LIKE 'Attendance' AND `name` LIKE 'Students';
 
+UPDATE `security_functions` SET `_execute` = NULL WHERE `module` LIKE 'Institutions' AND `category` LIKE 'Attendance' AND `name` LIKE 'Staff';
+
+UPDATE `security_functions` SET `_execute` = NULL WHERE `module` LIKE 'Institutions' AND `category` LIKE 'Assessment' AND `name` LIKE 'Results';
