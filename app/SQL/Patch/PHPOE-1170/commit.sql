bb028e41e2d8b1c4e55c5c3c885486aa098e7dcf
David Zhu <dzhu@kordit.com>
2015-02-09 15:55:19 +0800

    fixed permissions on institutionSiteStaff and institutionSiteStudent

--- app/SQL/Patch/PHPOE-1170/commit.sql
+++ app/SQL/Patch/PHPOE-1170/commit.sql
@@ -1,1 +1,5 @@
-UPDATE `navigations` SET `visible` = 1, `title` = 'Add Staff' WHERE `module` LIKE 'Staff' AND `plugin` LIKE 'Staff' AND `title` LIKE 'Add new Staff' AND `action` LIKE 'add';
+UPDATE `security_functions` SET `_view` = NULL WHERE `module` LIKE 'Institutions' AND `category` LIKE 'Details' AND `name` LIKE 'Student';
+UPDATE `security_functions` SET `_view` = 'view|InstitutionSiteStudent|InstitutionSiteStudent.index' WHERE `module` LIKE 'Students' AND `category` LIKE 'General' AND `name` LIKE 'Student';
+
+UPDATE `security_functions` SET `_view` = NULL WHERE `module` LIKE 'Institutions' AND `category` LIKE 'Details' AND `name` LIKE 'Staff';
+UPDATE `security_functions` SET `_view` = 'view|InstitutionSiteStaff|InstitutionSiteStaff.index' WHERE `module` LIKE 'Staff' AND `category` LIKE 'General' AND `name` LIKE 'Staff';
