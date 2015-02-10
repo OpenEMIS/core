514dce99f364d34b265a24675ac4b39a82e9024d
Jeff Zheng <jzheng@kordit.com>
2015-02-10 09:04:52 +0800

    PHPOE-1235 - Added execute permission for Student/Staff

---
+++ app/SQL/Patch/PHPOE-1235/rollback.sql
@@ -0,0 +1,2 @@
+UPDATE `security_functions` SET `_execute` = NULL WHERE `name` = 'Student' AND `controller` = 'Students' AND `module` = 'Students' AND `category` = 'General';
+UPDATE `security_functions` SET `_execute` = NULL WHERE `name` = 'Staff' AND `controller` = 'Staff' AND `module` = 'Staff' AND `category` = 'General';
