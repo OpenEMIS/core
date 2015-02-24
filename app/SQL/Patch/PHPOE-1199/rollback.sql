28fd6ead0883f181eda70a58419bd2d754e0fdbe
Malcolm Lee <mlee@kordit.com>
2015-02-24 16:48:38 +0800

    PHPOE-1199 SQL fix

--- app/SQL/Patch/PHPOE-1199/rollback.sql
+++ app/SQL/Patch/PHPOE-1199/rollback.sql
@@ -2,4 +2,4 @@ UPDATE navigations SET action = 'salaries', pattern = 'salaries' WHERE module = 
 -- SELECT * FROM `navigations` WHERE `title` LIKE 'Salary'
 
 ALTER TABLE `staff_salary_additions` CHANGE `amount` `addition_amount`  DECIMAL(11,2) NULL DEFAULT NULL;
-ALTER TABLE `staff_salary_deductions` CHANGE `amount` `addition_amount`  DECIMAL(11,2) NULL DEFAULT NULL;
+ALTER TABLE `staff_salary_deductions` CHANGE `amount` `deduction_amount`  DECIMAL(11,2) NULL DEFAULT NULL;
