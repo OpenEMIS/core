64a3c71f807390c146e09bf614c11ff7f7d57431
Malcolm Lee <mlee@kordit.com>
2015-02-24 16:48:38 +0800

    PHPOE-1199 SQL fix

--- app/SQL/Patch/PHPOE-1199/commit.sql
+++ app/SQL/Patch/PHPOE-1199/commit.sql
@@ -2,4 +2,4 @@ UPDATE navigations SET action = 'StaffSalary', pattern = 'StaffSalary' WHERE mod
 -- SELECT * FROM `navigations` WHERE `title` LIKE 'Salary'
 
 ALTER TABLE `staff_salary_additions` CHANGE `addition_amount` `amount` DECIMAL(11,2) NULL DEFAULT NULL;
-ALTER TABLE `staff_salary_deductions` CHANGE `addition_amount` `amount` DECIMAL(11,2) NULL DEFAULT NULL;
+ALTER TABLE `staff_salary_deductions` CHANGE `deduction_amount` `amount` DECIMAL(11,2) NULL DEFAULT NULL;
