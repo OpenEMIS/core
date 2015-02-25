96094869f89685f3b06a4492482701c2a58a7985
Malcolm Lee <mlee@kordit.com>
2015-02-25 15:07:15 +0800

    PHPOE-1134 Fixed remove InstitutionSiteHistory from InstitutionSiteProvider, InstitutionSiteSector, InstitutionSiteStatus remove alter statement for institution_site_history in commit.sql

--- app/SQL/Patch/PHPOE-1134/rollback.sql
+++ app/SQL/Patch/PHPOE-1134/rollback.sql
@@ -4,10 +4,6 @@ UPDATE navigations SET pattern = REPLACE(pattern, '|delete', '') WHERE controlle
 -- reactivate student gender
 UPDATE field_options SET visible = 1 WHERE code = 'Gender' AND name = 'Gender' AND parent = 'Student';
 
-ALTER TABLE `institution_site_history` DROP `institution_site_sector_id`;
-ALTER TABLE `institution_site_history` DROP `institution_site_provider_id`;
-ALTER TABLE `institution_site_history` DROP `institution_site_gender_id`;
-
 ALTER TABLE `field_options` DROP `plugin`;
 
 -- remove country
