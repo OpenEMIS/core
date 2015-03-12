fd45f1f18d6ad98dad7b9a22e6ce194fd9bdff10
Malcolm Lee <mlee@kordit.com>
2015-03-12 12:54:26 +0800

    PHPOE-1291 Education Cycle - New input type 'readonly' to handle uneditable 'disabled fields' that needs to be POSTed

--- app/Model/EducationCycle.php
+++ app/Model/EducationCycle.php
@@ -198,11 +198,15 @@ class EducationCycle extends AppModel {
 		return $age['EducationCycle']['admission_age'] + $age['EducationGrade']['order'] - 1;
 	}
 
-	public function getCycles() {
+	public function getCycles($options = array()) {
 		$this->unbindModel(array('hasMany' => array('EducationProgramme'), 'belongsTo' => array('EducationLevel')));
 		// $records = $this->find('list', array('conditions' => array('EducationCycle.visible' => 1)));
-		$records = $this->find('all', array('conditions' => array('EducationCycle.visible' => 1)));
-		$records = $this->formatArray($records);
+		$listOnly = (array_key_exists('listOnly', $options))? $options['listOnly']: false;
+		$type = ($listOnly)? 'list': 'all';
+		$records = $this->find($type, array('conditions' => array('EducationCycle.visible' => 1)));
+		if ($type == 'all') {
+			$records = $this->formatArray($records);
+		}
 		return $records;
 	}
 }
