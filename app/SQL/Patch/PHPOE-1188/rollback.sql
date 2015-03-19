a3a383758e73d5ffc3547810497fa6a6f579d67d
Thed Kuok Shiun <kuokshiun@hotmail.com>
2015-03-19 10:38:00 +0800

    PHPOE-1188 - Bugs fixed. SQL patch file to be executed. 1) Update security_functions for permission error

--- app/SQL/Patch/PHPOE-1188/rollback.sql
+++ app/SQL/Patch/PHPOE-1188/rollback.sql
@@ -22,3 +22,10 @@ UPDATE `navigations` SET `action` = 'leaves', `pattern` = 'leaves' WHERE `contro
 --
 
 UPDATE `security_functions` SET `_view` = 'leaves|leavesView', `_edit` = '_view:leavesEdit' , `_add` = '_view:leavesAdd', `_delete` = '_view:leavesDelete' WHERE `controller` = 'Staff' AND `module` = 'Staff' AND `category` = 'Details' AND `name` = 'Leave';
+
+--
+-- 3. Update Security Functions for (v2.3.8)
+--
+
+UPDATE `security_functions` SET `_delete` = '_view:delete' WHERE `controller` = 'Workflows' AND `module` = 'Administration' AND `category` = 'Workflows' AND `name` = 'Workflows';
+UPDATE `security_functions` SET `_delete` = '_view:delete' WHERE `controller` = 'WorkflowSteps' AND `module` = 'Administration' AND `category` = 'Workflows' AND `name` = 'WorkflowSteps';
