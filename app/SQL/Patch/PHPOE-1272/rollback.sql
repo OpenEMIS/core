c4c11fdf5353a08d45b8c7182281ad3ab63a973f
Malcolm Lee <mlee@kordit.com>
2015-04-10 10:16:31 +0800

    PHPOE-1272 Edited SQL to suit 985

--- app/SQL/Patch/PHPOE-1272/rollback.sql
+++ app/SQL/Patch/PHPOE-1272/rollback.sql
@@ -1,9 +1,5 @@
 ALTER TABLE `countries` DROP `identity_type_id`;
 
-ALTER TABLE `student_identities` CHANGE `issue_date` `issue_date` DATE NOT NULL;
-ALTER TABLE `student_identities` CHANGE `expiry_date` `expiry_date` DATE NOT NULL;
-ALTER TABLE `student_identities` CHANGE `issue_location` `issue_location` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
-
-ALTER TABLE `staff_identities` CHANGE `issue_date` `issue_date` DATE NOT NULL;
-ALTER TABLE `staff_identities` CHANGE `expiry_date` `expiry_date` DATE NOT NULL;
-ALTER TABLE `staff_identities` CHANGE `issue_location` `issue_location` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+ALTER TABLE `user_identities` CHANGE `issue_date` `issue_date` DATE NOT NULL;
+ALTER TABLE `user_identities` CHANGE `expiry_date` `expiry_date` DATE NOT NULL;
+ALTER TABLE `user_identities` CHANGE `issue_location` `issue_location` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
