6b7a8e4e56ec86c87404916c488a0c510a440235
David Zhu <dzhu@kordit.com>
2015-04-21 10:10:22 +0800

    PHPOE-1338 - add App::import('Vendor', 'PhpExcel/IOFactory.php'); issue only happens on server

--- app/Controller/Component/PhpExcelComponent.php
+++ app/Controller/Component/PhpExcelComponent.php
@@ -60,6 +60,7 @@ class PhpExcelComponent extends Component {
     public function loadWorksheet($file) {
         // load vendor classes
         App::import('Vendor', 'PhpExcel');
+		App::import('Vendor', 'PhpExcel/IOFactory.php');
 
         $this->_xls = PHPExcel_IOFactory::load($file);
         $this->setActiveSheet(0);
