ac0efad37b1777943cac6847e5861f605783988e
Jeff Zheng <jzheng@kordit.com>
2015-04-21 08:47:30 +0800

    Merged to dev for deploying PHPOE-1293, PHPOE-1328, PHPOE-1338 (SQL), PHPOE-1359, PHPOE-1366

---
+++ app/SQL/Version/v2.4.5/rollback.sql
@@ -0,0 +1,6 @@
+-- PHPOE-1338
+
+UPDATE `navigations` SET `pattern` = 'index$|advanced' WHERE `module` LIKE 'Institution' AND `title` LIKE 'List of Institutions';
+UPDATE `security_functions` SET `_add` = 'add' WHERE `module` LIKE 'Institutions' AND `category` LIKE 'General' AND `name` LIKE 'Institution';
+
+DROP  TABLE IF EXISTS `import_mapping`;
