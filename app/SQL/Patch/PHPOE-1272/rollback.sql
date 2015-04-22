b015d227a0d7f3b0c55b470870f89723e9333e36
Malcolm Lee <mlee@kordit.com>
2015-04-22 09:28:46 +0800

    PHPOE-1272 "not delete (student_identities, student_nationalities, staff_identities, staff_nationalities) from config_items anymore. please revert the sql back to original."

--- app/SQL/Patch/PHPOE-1272/rollback.sql
+++ app/SQL/Patch/PHPOE-1272/rollback.sql
@@ -4,11 +4,6 @@ ALTER TABLE `user_identities` CHANGE `issue_date` `issue_date` DATE NOT NULL;
 ALTER TABLE `user_identities` CHANGE `expiry_date` `expiry_date` DATE NOT NULL;
 ALTER TABLE `user_identities` CHANGE `issue_location` `issue_location` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
 
-
--- We should remove config_items (student_identities, student_nationalities, staff_identities, staff_nationalities) (rollback)
-INSERT INTO config_items SELECT * FROM z1272_config_items;
-
-
 -- Removing is_wizard (rollback)
 UPDATE navigations SET is_wizard = 1 WHERE action = 'StudentNationality';
 UPDATE navigations SET is_wizard = 1 WHERE action = 'StudentIdentity';
