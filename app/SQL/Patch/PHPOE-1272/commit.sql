6c318134381a75fe54e4c60b9382012de3022ac3
Malcolm Lee <mlee@kordit.com>
2015-04-22 09:28:46 +0800

    PHPOE-1272 "not delete (student_identities, student_nationalities, staff_identities, staff_nationalities) from config_items anymore. please revert the sql back to original."

--- app/SQL/Patch/PHPOE-1272/commit.sql
+++ app/SQL/Patch/PHPOE-1272/commit.sql
@@ -13,12 +13,6 @@ INSERT INTO z1272_config_items SELECT * FROM config_items WHERE type = 'Wizard -
 INSERT INTO z1272_config_items SELECT * FROM config_items WHERE type = 'Wizard - Add New Staff' AND name = 'staff_nationalities';
 INSERT INTO z1272_config_items SELECT * FROM config_items WHERE type = 'Wizard - Add New Staff' AND name = 'staff_identities';
 
-
-DELETE FROM config_items WHERE type = 'Wizard - Add New Student' AND name = 'student_nationalities';
-DELETE FROM config_items WHERE type = 'Wizard - Add New Student' AND name = 'student_identities';
-DELETE FROM config_items WHERE type = 'Wizard - Add New Staff' AND name = 'staff_nationalities';
-DELETE FROM config_items WHERE type = 'Wizard - Add New Staff' AND name = 'staff_identities';
-
 -- Removing is_wizard
 UPDATE navigations SET is_wizard = 0 WHERE action = 'StudentNationality';
 UPDATE navigations SET is_wizard = 0 WHERE action = 'StudentIdentity';
