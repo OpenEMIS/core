c3d7b0cb1cc8f266ceb15ab7a93f92c1b5da1938
Sani <ssarip@kordit.com>
2015-04-22 09:35:40 +0800

    PHPOE-1378 - Changed WHERE condition to select from 'controller', 'module', 'category', and '_view', as 'id' may not be the same across all environments.

--- app/SQL/Patch/PHPOE-1378/commit.sql
+++ app/SQL/Patch/PHPOE-1378/commit.sql
@@ -1,3 +1,3 @@
 UPDATE `security_functions` SET
-`_add` = '_view:financesAdd'
-WHERE `id` = '41';
+`_add` = '_view:finances'
+WHERE `controller` = 'Census' AND `module` = 'Institutions' AND `category` = 'Totals' AND `_view` = 'finances' LIMIT 1;
