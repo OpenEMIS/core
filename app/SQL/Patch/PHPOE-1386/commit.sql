fb34c168554f42948c08f9c5f93ebc084290fbf2
Malcolm Lee <mlee@kordit.com>
2015-04-28 15:42:28 +0800

    PHPOE-1386 Bugfixes

--- app/SQL/Patch/PHPOE-1386/commit.sql
+++ app/SQL/Patch/PHPOE-1386/commit.sql
@@ -2,3 +2,13 @@ UPDATE config_items SET name = "StaffIdentity" WHERE name = "staff_identities";
 UPDATE config_items SET name = "StaffNationality" WHERE name = "staff_nationalities";
 UPDATE config_items SET name = "StaffContact" WHERE name = "staff_contacts";
 UPDATE config_items SET name = "StaffSpecialNeed" WHERE name = "staff_specialNeed";
+
+
+-- removing 
+DELETE FROM config_items WHERE name = 'staff_membership';
+DELETE FROM config_items WHERE name = 'staff_license';
+DELETE FROM config_items WHERE name = 'staff_languages';
+DELETE FROM config_items WHERE name = 'staff_comments';
+DELETE FROM config_items WHERE name = 'staff_bankAccounts';
+DELETE FROM config_items WHERE name = 'staff_award';
+DELETE FROM config_items WHERE name = 'staff_attachments';
