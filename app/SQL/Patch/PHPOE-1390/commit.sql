fb93a4aca44ed14ed346be8c7d7880af62f47b40
Malcolm Lee <mlee@kordit.com>
2015-04-28 15:12:33 +0800

    PHPOE-1390 Need to fix dependancy

---
+++ app/SQL/Patch/PHPOE-1390/commit.sql
@@ -0,0 +1,4 @@
+UPDATE config_items SET name = "StudentIdentity" WHERE name = "student_identities";
+UPDATE config_items SET name = "StudentNationality" WHERE name = "student_nationalities";
+UPDATE config_items SET name = "StudentContact" WHERE name = "student_contacts";
+UPDATE config_items SET name = "StudentSpecialNeed" WHERE name = "student_specialNeed";
---
+++ app/SQL/Patch/PHPOE-1390/rollback.sql
@@ -0,0 +1,4 @@
+UPDATE config_items SET name = "student_identities" WHERE name = "StudentIdentity";
+UPDATE config_items SET name = "student_nationalities" WHERE name = "StudentNationality";
+UPDATE config_items SET name = "student_contacts" WHERE name = "StudentContact";
+UPDATE config_items SET name = "student_specialNeed" WHERE name = "StudentSpecialNeed";
