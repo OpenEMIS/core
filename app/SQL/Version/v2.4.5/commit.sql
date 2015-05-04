ffd4229e7e981d5c16063359917f0f5f5d48556f
Jeff Zheng <jzheng@kordit.com>
2015-05-04 12:45:42 +0800

    PHPOE-1354 - Merged to www for testing

--- app/SQL/Version/v2.4.5/commit.sql
+++ app/SQL/Version/v2.4.5/commit.sql
@@ -172,3 +172,18 @@ UPDATE navigations SET title = 'Add Student' WHERE navigations.controller = 'Stu
 -- making sure left nav is correct
 UPDATE navigations SET visible = '1' WHERE navigations.controller = 'Students' AND navigations.title = 'Add Student';
 
+-- PHPOE-1354
+
+DROP TABLE IF EXISTS `security_rest_sessions`;
+CREATE TABLE `security_rest_sessions` (
+  `id` char(36) NOT NULL,
+  `access_token` char(40) NOT NULL,
+  `refresh_token` char(40) NOT NULL,
+  `expiry_date` datetime NOT NULL,
+  `modified_user_id` int(11) DEFAULT NULL,
+  `modified` datetime NOT NULL,
+  `created_user_id` int(11) DEFAULT NULL,
+  `created` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
