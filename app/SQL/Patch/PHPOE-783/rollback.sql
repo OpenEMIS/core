31d7d995d69f973532d2c833809e70cf8d3c5fc3
Hanafi Ahmat <hahmat@kordit.com>
2015-05-06 09:23:15 +0800

    PHPOE-783 - modified rollback.sql

--- app/SQL/Patch/PHPOE-783/rollback.sql
+++ app/SQL/Patch/PHPOE-783/rollback.sql
@@ -3,5 +3,5 @@
 -- 
 
 DELETE FROM `contact_types` WHERE `contact_option_id`=(SELECT `id` FROM `contact_options` where `name`='Emergency');
-DELETE FROM `OpenEmis`.`contact_options` WHERE `contact_options`.`name` = 'Emergency';
+DELETE FROM `contact_options` WHERE `contact_options`.`name` = 'Emergency';
 UPDATE `contact_options` SET `contact_options`.`order`=`contact_options`.`order`-1 WHERE `contact_options`.`name` = 'Other';
