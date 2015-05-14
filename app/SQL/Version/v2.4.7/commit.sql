d6e8a00dea58d7d997db5908c8e144bb0588bd60
Jeff Zheng <jzheng@kordit.com>
2015-05-14 09:26:53 +0800

    PHPOE-1383 - Updated commit.sql to delete the permissions

--- app/SQL/Version/v2.4.7/commit.sql
+++ app/SQL/Version/v2.4.7/commit.sql
@@ -201,10 +201,10 @@ SELECT CONCAT_WS('-', `is`.`id`, `is`.`security_group_id`) as `id`
 UPDATE `navigations` SET `visible` = 0 WHERE `module` = 'Administration' AND `plugin` = 'Datawarehouse' AND `controller` = 'Datawarehouse' AND `header` = 'Data Processing';
 UPDATE `navigations` SET `visible` = 0 WHERE `module` = 'Administration' AND `plugin` = 'DataProcessing' AND `controller` = 'DataProcessing' AND `header` = 'Data Processing';
 
-UPDATE `security_functions` SET `visible` = 0 WHERE `name` = 'Build' AND `controller` = 'DataProcessing' AND `module` = 'Administration' AND `category` = 'Data Processing';
-UPDATE `security_functions` SET `visible` = 0 WHERE `name` = 'Generate' AND `controller` = 'DataProcessing' AND `module` = 'Administration' AND `category` = 'Data Processing';
-UPDATE `security_functions` SET `visible` = 0 WHERE `name` = 'Export' AND `controller` = 'DataProcessing' AND `module` = 'Administration' AND `category` = 'Data Processing';
-UPDATE `security_functions` SET `visible` = 0 WHERE `name` = 'Processes' AND `controller` = 'DataProcessing' AND `module` = 'Administration' AND `category` = 'Data Processing';
+DELETE FROM `security_functions` WHERE `name` = 'Build' AND `controller` = 'DataProcessing' AND `module` = 'Administration' AND `category` = 'Data Processing';
+DELETE FROM `security_functions` WHERE `name` = 'Generate' AND `controller` = 'DataProcessing' AND `module` = 'Administration' AND `category` = 'Data Processing';
+DELETE FROM `security_functions` WHERE `name` = 'Export' AND `controller` = 'DataProcessing' AND `module` = 'Administration' AND `category` = 'Data Processing';
+DELETE FROM `security_functions` WHERE `name` = 'Processes' AND `controller` = 'DataProcessing' AND `module` = 'Administration' AND `category` = 'Data Processing';
 
 
 -- Update version number
