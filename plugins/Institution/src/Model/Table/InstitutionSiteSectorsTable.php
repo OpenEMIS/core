630a4978b452b3bb6ae54ccf3efe182ea0bb36ff
Hanafi Ahmat <hahmat@kordit.com>
2015-05-26 10:41:27 +0800

    PHPOE-1407 - Migrated Areas to Area plugin with basic functionality. To re-visit at a later time. Consists of the following models 	* Areas 	* AreaLevels 	* AreaAdministratives 	* AreaAdministrativeLevels Updated Institutions relations with areas. Access urls:  	* /areas  	* /areas/levels  	* /areas/administratives  	* /areas/administrativeLevels

--- plugins/Institution/src/Model/Table/InstitutionSiteSectorsTable.php
+++ plugins/Institution/src/Model/Table/InstitutionSiteSectorsTable.php
@@ -1,10 +1,10 @@
 <?php
 namespace Institution\Model\Table;
 
-use Cake\ORM\Table;
+use App\Model\Table\AppTable;
 use Cake\Validation\Validator;
 
-class InstitutionSiteSectorsTable extends Table {
+class InstitutionSiteSectorsTable extends AppTable {
 	public function initialize(array $config) {
 		$this->table('field_option_values');
 		
--- plugins/Institution/src/Model/Table/InstitutionSiteStudentsTable.php
+++ plugins/Institution/src/Model/Table/InstitutionSiteStudentsTable.php
@@ -1,10 +1,10 @@
 <?php
 namespace Institution\Model\Table;
 
-use Cake\ORM\Table;
+use App\Model\Table\AppTable;
 use Cake\Validation\Validator;
 
-class InstitutionSiteStudentsTable extends Table {
+class InstitutionSiteStudentsTable extends AppTable {
 	public function initialize(array $config) {
 		// $this->table('security_users');
 
