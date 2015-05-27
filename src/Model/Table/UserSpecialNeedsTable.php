b462e4c7fe01bbbc96fccf4d80da68b6b22d1b0f
Malcolm Lee <mlee@kordit.com>
2015-05-27 14:43:25 +0800

    PHPOE-1407 FieldOptionsBehavior asd related 'Student General' tables

--- src/Model/Table/UserSpecialNeedsTable.php
+++ src/Model/Table/UserSpecialNeedsTable.php
@@ -7,10 +7,18 @@ use Cake\Validation\Validator;
 class UserSpecialNeedsTable extends AppTable {
 	public function initialize(array $config) {
 		parent::initialize($config);
+		$this->belongsTo('SecurityUsers', ['className' => 'SecurityUsers']);
+		$this->belongsTo('SpecialNeedTypes', ['className' => 'SpecialNeedTypes']);
+		$this->belongsTo('ModifiedUser', ['className' => 'SecurityUsers', 'foreignKey' => 'modified_user_id']);
+		$this->belongsTo('CreatedUser', ['className' => 'SecurityUsers', 'foreignKey' => 'created_user_id']);
+	}
+
+	public function beforeAction() {
+		$this->fields['special_need_type_id']['type'] = 'select';
+		$this->fields['special_need_type_id']['options'] = $this->SpecialNeedTypes->getList();
 	}
 
 	public function validationDefault(Validator $validator) {
-		
 		return $validator;
 	}
 
