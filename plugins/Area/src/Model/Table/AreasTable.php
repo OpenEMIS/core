5c738194660b00819fc28e975a438e311021c84a
Thed Kuok Shiun <kuokshiun@hotmail.com>
2015-05-27 16:31:08 +0800

    PHPOE-1407 - Update Area and Infrastructure.

--- plugins/Area/src/Model/Table/AreasTable.php
+++ plugins/Area/src/Model/Table/AreasTable.php
@@ -2,24 +2,9 @@
 namespace Area\Model\Table;
 
 use App\Model\Table\AppTable;
-use Cake\Validation\Validator;
 
 class AreasTable extends AppTable {
 	public function initialize(array $config) {
-
 		$this->belongsTo('AreaLevels', ['className' => 'Area.AreaLevels']);
-
-		$this->hasMany('Institutions', ['className' => 'Institution.Institutions']);
-
 	}
-
-	public function validationDefault(Validator $validator) {
-		
-		return $validator;
-	}
-
-	public function beforeAction() {
-		
-	}
-
 }
--- plugins/Infrastructure/src/Model/Table/InfrastructureTypesTable.php
+++ plugins/Infrastructure/src/Model/Table/InfrastructureTypesTable.php
@@ -2,24 +2,9 @@
 namespace Infrastructure\Model\Table;
 
 use App\Model\Table\AppTable;
-use Cake\Validation\Validator;
 
 class InfrastructureTypesTable extends AppTable {
 	public function initialize(array $config) {
-
 		$this->belongsTo('InfrastructureLevels', ['className' => 'Infrastructure.InfrastructureLevels']);
-		$this->hasMany('InfrastructureCustomFields', ['className' => 'Infrastructure.InfrastructureCustomFields']);
-		// $this->hasMany('Institutions', ['className' => 'Institution.Institutions']);
-
 	}
-
-	public function validationDefault(Validator $validator) {
-		
-		return $validator;
-	}
-
-	public function beforeAction() {
-		
-	}
-
 }
