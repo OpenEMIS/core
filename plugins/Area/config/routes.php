bb7d0f1a70b4a2ca1b03a2864507f20f2dea1452
Thed Kuok Shiun <kuokshiun@hotmail.com>
2015-05-27 17:10:42 +0800

    PHPOE-1407 - 1) Added Security Users, Security Groups and Security Roles. 2) Removed fallbacks() in routes.php

--- plugins/Area/config/routes.php
+++ plugins/Area/config/routes.php
@@ -4,5 +4,4 @@ use Cake\Routing\Router;
 Router::scope('/Areas', ['plugin' => 'Area'], function ($routes) {
 	Router::connect('/Areas', ['plugin' => 'Area', 'controller' => 'Areas']);
 	Router::connect('/Areas/:action/*', ['plugin' => 'Area', 'controller' => 'Areas']);
-	$routes->fallbacks('InflectedRoute');
 });
--- plugins/Infrastructure/config/routes.php
+++ plugins/Infrastructure/config/routes.php
@@ -4,5 +4,4 @@ use Cake\Routing\Router;
 Router::scope('/Infrastructures', ['plugin' => 'Infrastructure'], function ($routes) {
 	Router::connect('/Infrastructures', ['plugin' => 'Infrastructure', 'controller' => 'Infrastructures']);
 	Router::connect('/Infrastructures/:action/*', ['plugin' => 'Infrastructure', 'controller' => 'Infrastructures']);
-	$routes->fallbacks('InflectedRoute');
 });
