3bafc70e6d4509a80810eea9dc91f70162d07b68
Jeff Zheng <jzheng@kordit.com>
2015-06-02 15:22:20 +0800

    PHPOE-1407 - changed event data to function signature

--- plugins/ControllerAction/src/Controller/Component/ControllerActionComponent.php
+++ plugins/ControllerAction/src/Controller/Component/ControllerActionComponent.php
@@ -12,8 +12,6 @@ that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warra
 or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details. You should 
 have received a copy of the GNU General Public License along with this program.  If not, see 
 <http://www.gnu.org/licenses/>.  For more information please wire to contact@openemis.org.
-
-ControllerActionComponent - Version 2.0.1
 */
 
 namespace ControllerAction\Controller\Component;
