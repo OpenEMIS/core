d9ea62355cc7ab66d509f37b992bad9c1c8d7ec4
Malcolm Lee <mlee@kordit.com>
2015-06-03 09:39:33 +0800

    PHPOE-1407 Reverted Institution Student and Staff Behaviour to previously correct and working version

---
+++ plugins/Institution/src/Model/Table/StaffBehavioursTable.php
@@ -0,0 +1,21 @@
+<?php
+namespace Institution\Model\Table;
+
+use App\Model\Table\AppTable;
+use Cake\Validation\Validator;
+
+class StaffBehavioursTable extends AppTable {
+	public function initialize(array $config) {
+		parent::initialize($config);
+		
+		$this->belongsTo('Institutions', ['className' => 'Institution.Institutions', 'foreignKey' => 'institution_site_id']);
+	}
+
+	public function validationDefault(Validator $validator) {
+		return $validator;
+	}
+
+	public function beforeAction() {
+		
+	}
+}
