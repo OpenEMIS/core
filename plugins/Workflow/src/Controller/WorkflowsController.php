90fc8eceee11e30cfecd3b83d0d3c6eadbb65f5c
Thed Kuok Shiun <kuokshiun@hotmail.com>
2015-06-03 16:58:53 +0800

    PHPOE-1407 - Update Workflow.

--- plugins/Workflow/src/Controller/WorkflowsController.php
+++ plugins/Workflow/src/Controller/WorkflowsController.php
@@ -15,22 +15,16 @@ class WorkflowsController extends AppController
 
     public function implementedEvents() {
     	$events = parent::implementedEvents();
-    	$events['ControllerAction.onInitialize'] = 'onInitialize';
     	$events['ControllerAction.beforePaginate'] = 'beforePaginate';
     	return $events;
     }
 
     public function beforeFilter(Event $event) {
     	parent::beforeFilter($event);
+        $header = __('Workflow');
+        $this->Navigation->addCrumb('Workflow', ['plugin' => 'Workflow', 'controller' => 'Workflows', 'action' => 'index']);
+        $this->set('contentHeader', $header);
 	}
-
-    public function onInitialize($event, $model) {
-		$header = __('Workflow');
-
-		$this->Navigation->addCrumb('Workflow', ['plugin' => 'Workflow', 'controller' => 'Workflows', 'action' => 'index']);
-
-		$this->set('contentHeader', $header);
-    }
 
     public function beforePaginate($event, $model, $options) {
 		return $options;
