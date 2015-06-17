828e85439e9cbd9705588a1ec491dd1937ffc560
Hanafi Ahmat <hahmat@kordit.com>
2015-06-17 16:56:49 +0800

    PHPOE-1458	- Implemented simple phpunit tests 			- Re-visit Institutions overview and attachments

---
+++ plugins/Institution/tests/Fixture/InstitutionSiteActivitiesFixture.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Institution\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+class InstitutionSiteActivitiesFixture extends TestFixture {
+
+	public $import = ['table' => 'institution_site_activities'];
+	
+	public $records = [];
+}
