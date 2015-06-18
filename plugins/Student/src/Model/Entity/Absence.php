4ea2e85a417a024edea43a9e1812b5bc15e28872
Malcolm Lee <mlee@kordit.com>
2015-06-18 12:59:59 +0800

    PHPOE-1459 JSform change() for select boxes redirecting using named params - view student/.. /template/element/Absences/controls.ctp for example

---
+++ plugins/Student/src/Model/Entity/Absence.php
@@ -0,0 +1,48 @@
+<?php
+namespace Student\Model\Entity;
+
+use Cake\ORM\Entity;
+use Cake\ORM\TableRegistry;
+use Cake\ORM\Query;
+
+class Absence extends Entity
+{
+	protected $_virtual = ['days', 'time'];
+	
+    protected function _getDays() {
+    	$name = '';
+    	$InstitutionSiteStudentAbsences = TableRegistry::get('Institution.InstitutionSiteStudentAbsences');
+    	$settingWeekdays = $InstitutionSiteStudentAbsences->getWeekdaysBySetting();
+
+    	if ($this->full_day_absent == 'Yes') {
+			$stampFirstDateAbsent = strtotime($this->first_date_absent);
+			$stampLastDateAbsent = strtotime($this->last_date_absent);
+			if(!empty($this->last_date_absent) && $stampLastDateAbsent > $stampFirstDateAbsent){
+				$lastDateFormatted = $InstitutionSiteStudentAbsences->formatDate($this->last_date_absent, null, false);
+				$totalWeekdays = $InstitutionSiteStudentAbsences->getAbsenceDaysBySettings($this->first_date_absent, $this->last_date_absent, $settingWeekdays);
+				$noOfDays = sprintf('%s (to %s)', $totalWeekdays, $lastDateFormatted);
+			}else{
+				$noOfDays = 1;
+			}
+		} else {
+			$noOfDays = 1;
+		}
+    	return $noOfDays;
+	}
+
+	protected function _getTime() {
+    	$name = '';
+
+    	if ($this->has('full_day_absent')) {
+    		$InstitutionSiteStudentAbsences = TableRegistry::get('Institution.InstitutionSiteStudentAbsences');
+    		$settingWeekdays = $InstitutionSiteStudentAbsences->getWeekdaysBySetting();
+
+    		if ($this->full_day_absent == 'Yes') {
+				$timeStr = __('full day');
+    		} else {
+				$timeStr = sprintf('%s - %s', $absenceObj['start_time_absent'], $absenceObj['end_time_absent']);
+    		}
+    	}
+    	return $timeStr;
+	}
+}
--- plugins/Student/src/Model/Table/AbsencesTable.php
+++ plugins/Student/src/Model/Table/AbsencesTable.php
@@ -3,6 +3,8 @@ namespace Student\Model\Table;
 
 use App\Model\Table\AppTable;
 use Cake\Validation\Validator;
+use Cake\Event\Event;
+use Cake\ORM\TableRegistry;
 
 class AbsencesTable extends AppTable {
 	public function initialize(array $config) {
@@ -12,6 +14,68 @@ class AbsencesTable extends AppTable {
 		$this->belongsTo('Users', ['className' => 'User.Users', 'foreignKey' =>'security_user_id']);
 		$this->belongsTo('InstitutionSiteSections', ['className' => 'Institution.InstitutionSiteSections']);
 		$this->belongsTo('StudentAbsenceReasons', ['className' => 'FieldOption.StudentAbsenceReasons']);
+	}
+
+	public function beforeAction($event) {
+		$this->fields['student_absence_reason_id']['type'] = 'select';
+	}
+
+	public function indexBeforeAction(Event $event) {
+		$toolbarElements = [
+            ['name' => 'Student.Absences/controls', 'data' => [], 'options' => []]
+        ];
+        $this->controller->set('toolbarElements',$toolbarElements);
+
+        $academicPeriodList = TableRegistry::get('AcademicPeriod.AcademicPeriods')->getList();
+        $monthOptions = $this->generateMonthOptions();
+		$currentMonthId = $this->getCurrentMonthId();
+        $this->controller->set('academicPeriodList', $academicPeriodList);
+		$this->controller->set('monthOptions', $monthOptions);
+		$this->controller->set('academicPeriodId', $academicPeriodList);
+		$this->controller->set('monthOptionsId', $monthOptions);
+
+
+		$this->fields['last_date_absent']['visible'] = false;
+		$this->fields['full_day_absent']['visible'] = false;
+		$this->fields['start_time_absent']['visible'] = false;
+		$this->fields['end_time_absent']['visible'] = false;
+		$this->fields['comment']['visible'] = false;
+		$this->fields['security_user_id']['visible'] = false;
+		$this->fields['institution_site_section_id']['visible'] = false;
+
+		$this->ControllerAction->addField('days', []);
+		$this->ControllerAction->addField('time', []);
+
+		$order = 0;
+		$this->ControllerAction->setFieldOrder('first_date_absent', $order++);
+		$this->ControllerAction->setFieldOrder('days', $order++);
+		$this->ControllerAction->setFieldOrder('time', $order++);
+		$this->ControllerAction->setFieldOrder('student_absence_reason_id', $order++);
+		$this->ControllerAction->setFieldOrder('absence_type', $order++);
+	}
+
+	public function generateMonthOptions(){
+		$options = array();
+		for ($i = 1; $i <= 12; $i++)
+		{
+				$options[$i] = date("F", mktime(0, 0, 0, $i+1, 0, 0, 0));
+		}
+		
+		return $options;
+	}
+
+	public function getCurrentMonthId(){
+		$options = $this->generateMonthOptions();
+		$currentMonth = date("F");
+		$monthId = 1;
+		foreach($options AS $id => $month){
+			if($currentMonth === $month){
+				$monthId = $id;
+				break;
+			}
+		}
+		
+		return $monthId;
 	}
 }
 
