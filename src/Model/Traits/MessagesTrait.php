b84a982f25d09dd123c793887fbbec749e877687
Malcolm Lee <mlee@kordit.com>
2015-06-19 09:32:22 +0800

    PHPOE-1459 Included pluginName in the search for validation messages (e.g. Institution.Institutions.longitude.ruleLongitude)

--- src/Model/Traits/MessagesTrait.php
+++ src/Model/Traits/MessagesTrait.php
@@ -41,272 +41,250 @@ trait MessagesTrait {
 		],
 		
 		// Validation Messages
-		'Institutions' => [
-			'noProgrammes' => 'There is no available Programme set for this Institution.',
-			'noSections' => 'There is no available Section under the selected Academic Period.',
-			'date_closed' => [
-				'ruleCompareDateReverse' => 'Date Closed should not be earlier than Date Opened'
-			]
-		],
-
-		'Users' => [
-			'first_name' => [
-				'ruleNotBlank' => 'Please enter a valid First Name',
-				'ruleCheckIfStringGotNoNumber' => 'Please enter a valid First Name'
-			],
-			'middle_name' => [
-				'ruleCheckIfStringGotNoNumber' => 'Please enter a valid Middle Name'
-			],
-			'third_name' => [
-				'ruleCheckIfStringGotNoNumber' => 'Please enter a valid Third Name'
-			],
-			'last_name' => [
-				'ruleNotBlank' => 'Please enter a valid Last Name',
-				'ruleCheckIfStringGotNoNumber' => 'Please enter a valid Last Name'
-			],
-			'preferred_name' => [
-				'ruleCheckIfStringGotNoNumber' => 'Please enter a valid Preferred Name'
-			],
-			'openemis_no' => [
-				'ruleNotBlank' => 'Please enter a valid OpenEMIS ID',
-				'ruleUnique' => 'Please enter a unique OpenEMIS ID'
-			],
-			'gender_id' => [
-				'ruleNotBlank' => 'Please select a Gender'
-			],
-			'address' => [
-				'ruleNotBlank' => 'Please enter a valid Address'
-			],
-			'date_of_birth' => [
-				'ruleNotBlank' => 'Please select a Date of Birth',
-				'ruleCompare' => 'Please select a Date of Birth',
-				'ruleCompare' => 'Date of Birth cannot be future date'
-			],
-			'username' => [
-				'ruleNotBlank' => 'Please enter a valid username',
-				'ruleNoSpaces' => 'Only alphabets and numbers are allowed',
-				'ruleUnique' => 'This username is already in use.'
-			],
-			'password' => [
-				'ruleChangePassword' => 'Incorrect password.',
-				'ruleCheckUsernameExists' => 'Please enter a valid password',
-				'ruleMinLength' => 'Password must be at least 6 characters'
-			],
-			'retype_password' => [
-				'ruleChangePassword' => 'Please confirm your new password',
-				'ruleCompare' => 'Both passwords do not match'
-			],
-			'newPassword' => [
-				'ruleChangePassword' => 'Please enter your new password',
-				'ruleMinLength' => 'Password must be at least 6 characters'
-			],
-			'retypeNewPassword' => [
-				'ruleChangePassword' => 'Please confirm your new password',
-				'ruleCompare' => 'Both passwords do not match'
-			]
-		],
-		'Contacts' => [
-			'contact_type_id' => [
-				'ruleNotBlank' => 'Please enter a Contact Type'
-			],
-			'value' => [
-				'ruleNotBlank' => 'Please enter a valid value'
-			],
-			'preferred' => [
-				'comparison' => 'Please select a preferred for the selected contact type'
+		'Institution' => [
+			'Institutions' => [
+				'noProgrammes' => 'There is no available Programme set for this Institution.',
+				'noSections' => 'There is no available Section under the selected Academic Period.',
+				'date_closed' => [
+					'ruleCompareDateReverse' => 'Date Closed should not be earlier than Date Opened'
+				]
 			],
 		],
-		'Identities' => [
-			'identity_type_id' => [
-				'ruleNotBlank' => 'Please select a Type'
+		'User' => [
+			'Users' => [
+				'first_name' => [
+					'ruleNotBlank' => 'Please enter a valid First Name',
+					'ruleCheckIfStringGotNoNumber' => 'Please enter a valid First Name'
+				],
+				'middle_name' => [
+					'ruleCheckIfStringGotNoNumber' => 'Please enter a valid Middle Name'
+				],
+				'third_name' => [
+					'ruleCheckIfStringGotNoNumber' => 'Please enter a valid Third Name'
+				],
+				'last_name' => [
+					'ruleNotBlank' => 'Please enter a valid Last Name',
+					'ruleCheckIfStringGotNoNumber' => 'Please enter a valid Last Name'
+				],
+				'preferred_name' => [
+					'ruleCheckIfStringGotNoNumber' => 'Please enter a valid Preferred Name'
+				],
+				'openemis_no' => [
+					'ruleNotBlank' => 'Please enter a valid OpenEMIS ID',
+					'ruleUnique' => 'Please enter a unique OpenEMIS ID'
+				],
+				'gender_id' => [
+					'ruleNotBlank' => 'Please select a Gender'
+				],
+				'address' => [
+					'ruleNotBlank' => 'Please enter a valid Address'
+				],
+				'date_of_birth' => [
+					'ruleNotBlank' => 'Please select a Date of Birth',
+					'ruleCompare' => 'Please select a Date of Birth',
+					'ruleCompare' => 'Date of Birth cannot be future date'
+				],
+				'username' => [
+					'ruleNotBlank' => 'Please enter a valid username',
+					'ruleNoSpaces' => 'Only alphabets and numbers are allowed',
+					'ruleUnique' => 'This username is already in use.'
+				],
+				'password' => [
+					'ruleChangePassword' => 'Incorrect password.',
+					'ruleCheckUsernameExists' => 'Please enter a valid password',
+					'ruleMinLength' => 'Password must be at least 6 characters'
+				],
+				'retype_password' => [
+					'ruleChangePassword' => 'Please confirm your new password',
+					'ruleCompare' => 'Both passwords do not match'
+				],
+				'newPassword' => [
+					'ruleChangePassword' => 'Please enter your new password',
+					'ruleMinLength' => 'Password must be at least 6 characters'
+				],
+				'retypeNewPassword' => [
+					'ruleChangePassword' => 'Please confirm your new password',
+					'ruleCompare' => 'Both passwords do not match'
+				]
 			],
-			'number' => [
-				'ruleNotBlank' => 'Please enter a valid Number'
+			'Contacts' => [
+				'contact_type_id' => [
+					'ruleNotBlank' => 'Please enter a Contact Type'
+				],
+				'value' => [
+					'ruleNotBlank' => 'Please enter a valid value'
+				],
+				'preferred' => [
+					'comparison' => 'Please select a preferred for the selected contact type'
+				],
 			],
-			'issue_location' => [
-				'ruleNotBlank' => 'Please enter a valid Issue Location'
+			'Identities' => [
+				'identity_type_id' => [
+					'ruleNotBlank' => 'Please select a Type'
+				],
+				'number' => [
+					'ruleNotBlank' => 'Please enter a valid Number'
+				],
+				'issue_location' => [
+					'ruleNotBlank' => 'Please enter a valid Issue Location'
+				],
+				'issue_date' => [
+					'comparison' => 'Issue Date Should be Earlier Than Expiry Date'
+				],
+				'expiry_date' => [
+					'ruleNotBlank' => 'Expiry Date Is Required'
+				]
 			],
-			'issue_date' => [
-				'comparison' => 'Issue Date Should be Earlier Than Expiry Date'
+			'Languages' => [
+				'language_id' => [
+					'ruleNotBlank' => 'Please select a Language'
+				],
+				'listening' => [
+					'ruleNotBlank' => 'Please enter a number between 0 and 5'
+				],
+				'speaking' => [
+					'ruleNotBlank' => 'Please enter a number between 0 and 5'
+				],
+				'reading' => [
+					'ruleNotBlank' => 'Please enter a number between 0 and 5'
+				],
+				'writing' => [
+					'ruleNotBlank' => 'Please enter a number between 0 and 5'
+				],
 			],
-			'expiry_date' => [
-				'ruleNotBlank' => 'Expiry Date Is Required'
-			]
+			'Comments' => [
+				'title' => [
+					'ruleNotBlank' => 'Please enter a valid Title'
+				],
+				'comment' => [
+					'ruleNotBlank' => 'Please enter a valid Comment'
+				],
+			],
+			'SpecialNeeds' => [
+				'special_need_type_id' => [
+					'ruleNotBlank' => 'Please select a valid Special Need Type.'
+				]
+			],
+			'Awards' => [
+				'award' => [
+					'ruleNotBlank' => 'Please enter a valid Award.'
+				],
+				'issuer' => [
+					'ruleNotBlank' => 'Please enter a valid Issuer.'
+				]
+			],
+			'Attachments' => [
+				'name' => [
+					'ruleNotBlank' => 'Please enter a File name'
+				]
+			],
+			'BankAccounts' => [
+				'account_name' => [
+					'ruleNotBlank' => 'Please enter an Account name'
+				],
+				'account_number' => [
+					'ruleNotBlank' => 'Please enter an Account number'
+				],
+				'bank_id' => [
+					'ruleNotBlank' => 'Please select a Bank'
+				],
+				'bank_branch_id' => [
+					'ruleNotBlank' => 'Please select a Bank Branch'
+				]
+			],
 		],
-		'Languages' => [
-			'language_id' => [
-				'ruleNotBlank' => 'Please select a Language'
-			],
-			'listening' => [
-				'ruleNotBlank' => 'Please enter a number between 0 and 5'
-			],
-			'speaking' => [
-				'ruleNotBlank' => 'Please enter a number between 0 and 5'
-			],
-			'reading' => [
-				'ruleNotBlank' => 'Please enter a number between 0 and 5'
-			],
-			'writing' => [
-				'ruleNotBlank' => 'Please enter a number between 0 and 5'
+		'Student' => [
+			'Extracurriculars' => [
+				'name' => [
+					'ruleNotBlank' => 'Please enter a valid Title.'
+				],
+				'hours' => [
+					'ruleNotBlank' => 'Please enter a valid Hours.'
+				],
+				'start_date' => [
+					'ruleCompareDate' => 'Start Date cannot be later than End Date',
+				]
 			],
 		],
-		'Comments' => [
-			'title' => [
-				'ruleNotBlank' => 'Please enter a valid Title'
+		'Staff' => [
+			'Qualifications' => [
+				'qualification_title' => [
+					'required' => 'Please enter a valid Qualification Title'
+				],
+				'graduate_year' => [
+					'required' => 'Please enter a valid Graduate Year'
+				],
+				'qualification_level_id' => [
+					'required' => 'Please enter a valid Qualification Level'
+				],
+				'qualification_specialisation_id' => [
+					'required' => 'Please enter a valid Major/Specialisation'
+				],
+				'qualification_institution_name' => [
+					'validHiddenId' => 'Please enter a valid Institution'
+				]
 			],
-			'comment' => [
-				'ruleNotBlank' => 'Please enter a valid Comment'
+			'Leaves' => [
+				'date_from' => [
+					'ruleCompareDate' => 'Date From cannot be later than Date To',
+					'ruleNoOverlap' => 'Leave have been selected for this date. Please choose a different date'
+				],
+				'number_of_days' => [
+					'ruleNotBlank' => 'Please enter the number of days'
+				]
 			],
-		],
-		'SpecialNeeds' => [
-			'special_need_type_id' => [
-				'ruleNotBlank' => 'Please select a valid Special Need Type.'
-			]
-		],
-		'Awards' => [
-			'award' => [
-				'ruleNotBlank' => 'Please enter a valid Award.'
+			'Extracurriculars' => [
+				'name' => [
+					'ruleNotBlank' => 'Please enter a valid Title.'
+				],
+				'hours' => [
+					'ruleNotBlank' => 'Please enter a valid Hours.'
+				],
+				'start_date' => [
+					'ruleCompareDate' => 'Start Date cannot be later than End Date',
+				]
 			],
-			'issuer' => [
-				'ruleNotBlank' => 'Please enter a valid Issuer.'
-			]
-		],
-		'Attachments' => [
-			'name' => [
-				'ruleNotBlank' => 'Please enter a File name'
-			]
-		],
-		'Programmes' => [
-
-		],
-		'Sections' => [
-
-		],
-		'Classes' => [
-
-		],
-		'Absences' => [
-
-		],
-		'Behaviours' => [
-			'title' => [
-				'ruleNotBlank' => 'Please enter a valid title'
+			'Employments' => [
+				'employment_type_id' => [
+					'ruleNotBlank' => 'Please select a Type'
+				],
+				'employment_date' => [
+					'ruleNotBlank' => 'Please enter a valid Date'
+				]
 			],
-			'student_behaviour_category_id' => [
-				'ruleNotBlank' => 'Please select an category'
+			'Salaries' => [
+				'salary_date' => [
+					'ruleNotBlank' => 'Please select a Salary Date'
+				],
+				'gross_salary' => [
+					'ruleNotBlank' => 'Please enter a valid Gross Salary'
+				],
+				'net_salary' => [
+					'ruleNotBlank' => 'Please enter a valid Net Salary'
+				]
 			],
-			'staff_behaviour_category_id' => [
-				'ruleNotBlank' => 'Please select an category'
-			]
-		],
-		'Results' => [
-
-		],
-		'Extracurriculars' => [
-			'name' => [
-				'ruleNotBlank' => 'Please enter a valid Title.'
+			'Memberships' => [
+				'membership' => [
+					'ruleNotBlank' => 'Please enter a valid Membership.'
+				],
+				'issue_date' => [
+			        'ruleCompareDate' => 'Issue Date cannot be later than Expiry Date',
+			    ]
 			],
-			'hours' => [
-				'ruleNotBlank' => 'Please enter a valid Hours.'
+			'Licenses' => [
+				'license_type_id' => [
+					'ruleNotBlank' => 'Please select a valid License Type.'
+				],
+				'issuer' => [
+					'ruleNotBlank' => 'Please enter a valid Issuer.'
+				],
+				'license_number' => [
+					'ruleNotBlank' => 'Please enter a valid License Number.'
+				],
+				'issue_date' => [
+					'ruleCompareDate' => 'Issue Date cannot be later than Expiry Date',
+				]
 			],
-			'start_date' => [
-				'ruleCompareDate' => 'Start Date cannot be later than End Date',
-			]
-		],
-		'BankAccounts' => [
-			'account_name' => [
-				'ruleNotBlank' => 'Please enter an Account name'
-			],
-			'account_number' => [
-				'ruleNotBlank' => 'Please enter an Account number'
-			],
-			'bank_id' => [
-				'ruleNotBlank' => 'Please select a Bank'
-			],
-			'bank_branch_id' => [
-				'ruleNotBlank' => 'Please select a Bank Branch'
-			]
-		],
-		'StudentFees' => [
-
-		],
-
-		// staff
-		'Qualifications' => [
-			'qualification_title' => [
-				'required' => 'Please enter a valid Qualification Title'
-			],
-			'graduate_year' => [
-				'required' => 'Please enter a valid Graduate Year'
-			],
-			'qualification_level_id' => [
-				'required' => 'Please enter a valid Qualification Level'
-			],
-			'qualification_specialisation_id' => [
-				'required' => 'Please enter a valid Major/Specialisation'
-			],
-			'qualification_institution_name' => [
-				'validHiddenId' => 'Please enter a valid Institution'
-			]
-		],
-		'Positions' => [
-
-		],
-		'Sections' => [
-
-		],
-		'Classes' => [
-
-		],
-		'Leaves' => [
-			'date_from' => [
-				'ruleCompareDate' => 'Date From cannot be later than Date To',
-				'ruleNoOverlap' => 'Leave have been selected for this date. Please choose a different date'
-			],
-			'number_of_days' => [
-				'ruleNotBlank' => 'Please enter the number of days'
-			]
-		],
-		'Employments' => [
-			'employment_type_id' => [
-				'ruleNotBlank' => 'Please select a Type'
-			],
-			'employment_date' => [
-				'ruleNotBlank' => 'Please enter a valid Date'
-			]
-		],
-		'Salaries' => [
-			'salary_date' => [
-				'ruleNotBlank' => 'Please select a Salary Date'
-			],
-			'gross_salary' => [
-				'ruleNotBlank' => 'Please enter a valid Gross Salary'
-			],
-			'net_salary' => [
-				'ruleNotBlank' => 'Please enter a valid Net Salary'
-			]
-		],
-		'Memberships' => [
-			'membership' => [
-				'ruleNotBlank' => 'Please enter a valid Membership.'
-			],
-			'issue_date' => [
-		        'ruleCompareDate' => 'Issue Date cannot be later than Expiry Date',
-		    ]
-		],
-		'Licenses' => [
-			'license_type_id' => [
-				'ruleNotBlank' => 'Please select a valid License Type.'
-			],
-			'issuer' => [
-				'ruleNotBlank' => 'Please enter a valid Issuer.'
-			],
-			'license_number' => [
-				'ruleNotBlank' => 'Please enter a valid License Number.'
-			],
-			'issue_date' => [
-				'ruleCompareDate' => 'Issue Date cannot be later than Expiry Date',
-			]
 		],
 	];
 
