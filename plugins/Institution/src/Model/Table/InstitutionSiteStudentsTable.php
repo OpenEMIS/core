c4a44d15e8a72c40c64112fcd4901fbc4f5c2413
Malcolm Lee <mlee@kordit.com>
2015-06-23 13:02:10 +0800

    PHPOE-1459 Committing to pull changes - Working on Institution/Student/add

--- plugins/Institution/src/Model/Table/InstitutionSiteStudentsTable.php
+++ plugins/Institution/src/Model/Table/InstitutionSiteStudentsTable.php
@@ -2,6 +2,7 @@
 namespace Institution\Model\Table;
 
 use Cake\Event\Event;
+use Cake\ORM\Entity;
 use App\Model\Table\AppTable;
 use Cake\ORM\Query;
 use Cake\ORM\TableRegistry;
@@ -32,6 +33,79 @@ class InstitutionSiteStudentsTable extends AppTable {
 		
 		// $this->belongsTo('Institutions', ['className' => 'Institution.Institutions', 'foreignKey' => 'institution_site_id']);
 	}
+
+
+
+	public function addBeforeAction(Event $event) {
+		// pr();
+		// foreach ($this->fields as $key => $value) {
+		// 	var_dump($key.'<br>');
+		// }
+		
+		$this->ControllerAction->field('institution');
+		$this->ControllerAction->field('academic_period');
+		// education_programme_id
+		$this->ControllerAction->field('education_grade');
+		$this->ControllerAction->field('section');
+		// student_status_id // category
+		// start_date
+		// end_date
+		$this->ControllerAction->field('search');
+
+		$this->fields['start_year']['visible'] = false;
+		$this->fields['end_year']['visible'] = false;
+		$this->fields['security_user_id']['visible'] = false;
+
+		$this->ControllerAction->setFieldOrder([
+			'institution', 'academic_period', 'education_programme_id', 'education_grade', 'section', 'student_status_id', 'start_date', 'end_date', 'search'
+			]);
+	}
+
+	public function onUpdateFieldInstitution(Event $event, array $attr, $action, $request) {
+		$attr['type'] = 'readonly';
+
+		$institutionsId = $this->Session->read('Institutions.id');
+		$result = $this->Institutions
+			->find()
+			->where([$this->Institutions->primaryKey()=>$institutionsId])
+			->first()
+		;
+		$result = $result->toArray();
+		if (!empty($result)) {
+			$attr['attr']['value'] = $result['name'];
+		}
+		return $attr;
+	}
+
+	public function onUpdateFieldAcademicPeriod(Event $event, array $attr, $action, $request) {
+		$institutionsId = $this->Session->read('Institutions.id');
+		$conditions = array(
+			'InstitutionSiteProgrammes.institution_site_id' => $institutionsId
+		);
+
+		$InstitutionSiteProgramme = TableRegistry::get('Institution.InstitutionSiteProgrammes');
+		$list = $InstitutionSiteProgramme->getAcademicPeriodOptions($conditions);
+
+		$attr['type'] = 'select';
+		$attr['options'] = $list;
+		$attr['onChangeReload'] = 'changePeriod';
+
+		return $attr;
+	}
+
+	public function addEditOnChangePeriod(Event $event, Entity $entity, array $data, array $options) {
+		// pr('lala');
+	}
+
+	public function onUpdateFieldEducationGrade(Event $event, array $attr, $action, $request) {
+	}
+
+	public function onUpdateFieldSection(Event $event, array $attr, $action, $request) {
+	}
+
+	public function onUpdateFieldSearch(Event $event, array $attr, $action, $request) {
+	}
+
 
 	public function validationDefault(Validator $validator) {
 		$validator = parent::validationDefault($validator);
