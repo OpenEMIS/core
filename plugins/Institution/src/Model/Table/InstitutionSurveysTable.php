3e0bf6164d5f0603a14f6ddc4328c08800dbfb10
Thed Kuok Shiun <kuokshiun@hotmail.com>
2015-06-30 17:56:21 +0800

    PHPOE-1461 - Updated Institution Survey.

--- plugins/Institution/src/Model/Table/InstitutionSurveysTable.php
+++ plugins/Institution/src/Model/Table/InstitutionSurveysTable.php
@@ -1,14 +1,13 @@
 <?php
 namespace Institution\Model\Table;
 
+use ArrayObject;
 use App\Model\Table\AppTable;
 use Cake\ORM\Entity;
 use Cake\Network\Request;
 use Cake\Event\Event;
 
 class InstitutionSurveysTable extends AppTable {
-	private $_contain = ['AcademicPeriods', 'SurveyForms', 'Institutions'];
-
 	private $status = [
 		0 => 'New',
 		1 => 'Draft',
@@ -34,8 +33,112 @@ class InstitutionSurveysTable extends AppTable {
 			'tableColumnKey' => 'survey_table_column_id',
 			'tableRowKey' => 'survey_table_row_id'
 		]);
+	}
 
+	public function buildSurveyRecords() {
+		$CustomModules = $this->SurveyForms->CustomModules;
+		$customModuleResults = $CustomModules
+			->find('all')
+			->select([
+				$CustomModules->aliasField('id'),
+				$CustomModules->aliasField('field_option')
+			])
+			->where([
+				$CustomModules->aliasField('model') => $this->request->params['controller']
+			])
+			->first();
+		$customModuleId = $customModuleResults->id;
+		$todayDate = date("Y-m-d");
 
+		$institutionId = null;
+		$session = $this->controller->request->session();
+		if ($session->check('Institutions.id')) {
+			$institutionId = $session->read('Institutions.id');
+		}
+		$SurveyStatuses = $this->SurveyForms->SurveyStatuses;
+		$SurveyStatusPeriods = $this->SurveyForms->SurveyStatuses->SurveyStatusPeriods;
+
+		$surveyForms = $this->SurveyForms
+			->find('list')
+			->where([$this->SurveyForms->aliasField('custom_module_id') => $customModuleId])
+			->toArray();
+
+		foreach ($surveyForms as $surveyFormId => $surveyForm) {
+			$surveyStatuesIds = $SurveyStatuses
+				->find('list', ['keyField' => 'id', 'valueField' => 'id'])
+				->where([$SurveyStatuses->aliasField('survey_form_id') => $surveyFormId])
+				->toArray();
+
+			$academicPeriodIds = $SurveyStatusPeriods
+				->find('list', ['keyField' => 'academic_period_id', 'valueField' => 'academic_period_id'])
+				->where([$SurveyStatusPeriods->aliasField('survey_status_id IN') => $surveyStatuesIds])
+				->toArray();
+
+			foreach ($academicPeriodIds as $key => $academicPeriodId) {
+				if (!is_null($institutionId)) {
+					$results = $this
+						->find('all')
+						->where([
+							$this->aliasField('academic_period_id') => $academicPeriodId,
+							$this->aliasField('survey_form_id') => $surveyFormId,
+							$this->aliasField('institution_site_id') => $institutionId,
+							//$this->aliasField('status IN') => [1, 2]
+						])
+						->all();
+
+					if ($results->isEmpty()) {
+						$InstitutionSurvey = $this->newEntity();
+						$InstitutionSurvey->status = 0;
+						$InstitutionSurvey->academic_period_id = $academicPeriodId;
+						$InstitutionSurvey->survey_form_id = $surveyFormId;
+						$InstitutionSurvey->institution_site_id = $institutionId;
+
+						if ($this->save($InstitutionSurvey)) {
+						} else {
+							$this->log($InstitutionSurvey->errors(), 'debug');
+						}
+					}
+				}
+			}
+		}
+	}
+
+	public function onGetSurveyFormId(Event $event, Entity $entity) {
+		return $event->subject()->Html->link($entity->survey_form->name, [
+			'plugin' => $this->controller->plugin,
+			'controller' => $this->controller->name,
+			'action' => $this->alias,
+			'edit',
+			$entity->id
+		]);
+	}
+
+	public function onGetDescription(Event $event, Entity $entity) {
+		return $entity->survey_form->description;
+	}
+
+	public function onGetLastModified(Event $event, Entity $entity) {
+		return $entity->modified;
+	}
+
+	public function onGetToBeCompletedBy(Event $event, Entity $entity) {
+		$value = '';
+		return $value;
+	}
+
+	public function onGetCompletedOn(Event $event, Entity $entity) {
+		return $entity->modified;
+	}
+
+	public function indexOnInitializeButtons(Event $event, $buttons) {
+		list(, $selectedStatus) = array_values($this->getSelectOptions());
+		if ($selectedStatus == 0) {
+			unset($buttons['delete']);
+		} else if ($selectedStatus == 1) {
+		} else if ($selectedStatus == 2) {
+			unset($buttons['edit']);
+		}
+		return $buttons;
 	}
 
 	public function indexBeforeAction(Event $event) {
@@ -58,13 +161,42 @@ class InstitutionSurveysTable extends AppTable {
 
         $this->controller->set('tabElements', $tabElements);
         $this->controller->set('selectedAction', $statusOptions[$selectedStatus]);
+
+        if ($selectedStatus == 0) {	//New
+        	$this->ControllerAction->field('description');
+			$this->ControllerAction->field('to_be_completed_by');
+
+			$this->ControllerAction->setFieldOrder([
+				'survey_form_id', 'description', 'academic_period_id', 'to_be_completed_by'
+			]);
+
+			$this->buildSurveyRecords();
+        } else if ($selectedStatus == 1) {	//Draft
+			$this->ControllerAction->field('description');
+			$this->ControllerAction->field('last_modified');
+			$this->ControllerAction->field('to_be_completed_by');
+
+			$this->ControllerAction->setFieldOrder([
+				'survey_form_id', 'description', 'academic_period_id', 'last_modified', 'to_be_completed_by'
+			]);
+        } else if ($selectedStatus == 2) {	//Completed
+			$this->ControllerAction->field('description');
+			$this->ControllerAction->field('completed_on');
+
+			$this->ControllerAction->setFieldOrder([
+				'survey_form_id', 'description', 'academic_period_id', 'completed_on'
+			]);
+        }
+
+        $this->fields['status']['visible'] = false;
 	}
 
 	public function indexBeforePaginate(Event $event, Request $request, array $options) {
 		list(, $selectedStatus) = array_values($this->getSelectOptions());
-
-		$options['contain'] = array_merge($options['contain'], $this->_contain);
 		$options['conditions'][$this->aliasField('status')] = $selectedStatus;
+		$options['order'] = [
+			$this->AcademicPeriods->aliasField('order')
+		];
 
 		return $options;
 	}
@@ -78,18 +210,15 @@ class InstitutionSurveysTable extends AppTable {
 	}
 
 	public function addEditBeforeAction(Event $event) {
-		//Setup fields
-		list($statusOptions, , $formOptions) = array_values($this->getSelectOptions());
-
-		$this->fields['status']['type'] = 'select';
-		$this->fields['status']['options'] = $statusOptions;
-
-		$this->fields['academic_period_id']['type'] = 'select';
-
-		$this->fields['survey_form_id']['type'] = 'select';
-		$this->fields['survey_form_id']['options'] = $formOptions;
-		$this->fields['survey_form_id']['onChangeReload'] = true;
+		$this->fields['status']['type'] = 'hidden';
+		$this->fields['academic_period_id']['type'] = 'hidden';
+		$this->fields['survey_form_id']['type'] = 'hidden';
 	}
+
+    public function addEditBeforePatch(Event $event, Entity $entity, ArrayObject $data, ArrayObject $options) {
+		$status = $data[$this->alias()]['status'] == 0 ? 1 : 2;
+		$data[$this->alias()]['status'] = $status;
+    }
 
 	public function addEditAfterAction(Event $event, Entity $entity) {
 		if ($this->behaviors()->hasMethod('addEditAfterAction')) {
@@ -98,38 +227,11 @@ class InstitutionSurveysTable extends AppTable {
 		return $entity;
 	}
 
-	public function addOnInitialize(Event $event, Entity $entity) {
-		//Initialize field values
-		list(, , , $selectedForm) = array_values($this->getSelectOptions());
-		$entity->survey_form_id = $selectedForm;
-
-		return $entity;
-	}
-
 	public function getSelectOptions() {
 		//Return all required options and their key
 		$statusOptions = $this->status;
 		$selectedStatus = !is_null($this->request->query('status')) ? $this->request->query('status') : key($statusOptions);
 
-		$CustomModules = $this->SurveyForms->CustomModules;
-		$customModuleResults = $CustomModules
-			->find('all')
-			->select([
-				$CustomModules->aliasField('id'),
-				$CustomModules->aliasField('field_option')
-			])
-			->where([
-				$CustomModules->aliasField('model') => $this->request->params['controller']
-			])
-			->first();
-		$customModuleId = $customModuleResults->id;
-		
-		$formOptions = $this->SurveyForms
-			->find('list')
-			->where([$this->SurveyForms->aliasField('custom_module_id') => $customModuleId])
-			->toArray();
-		$selectedForm = key($formOptions);
-
-		return compact('statusOptions', 'selectedStatus', 'formOptions', 'selectedForm');
+		return compact('statusOptions', 'selectedStatus');
 	}
 }
