3a5e063e21c42770e11f1c16fa1593c2f5583c2f
Malcolm Lee <mlee@kordit.com>
2015-06-30 17:37:30 +0800

    PHPOE-1459 updated labels for student and staff

--- sql/Version/3.0.1/labels.sql
+++ sql/Version/3.0.1/labels.sql
@@ -52,5 +52,87 @@ INSERT INTO `labels` (`module`, `field`, `code`, `en`) VALUES
 ('InstitutionSiteClasses', 'education_subject_id', NULL, 'Subject Name'),
 ('InstitutionSiteClasses', 'education_subject_code', NULL, 'Subject Code'),
 -- Users
-('Users', 'photo_content', NULL, 'Profile Image');
+('Users', 'name', NULL, 'User'),
+('Users', 'username', NULL, 'User'),
+('Users', 'openemis_no', NULL, 'Openemis'),
+('Users', 'first_name', NULL, 'First'),
+('Users', 'middle_name', NULL, 'Middle'),
+('Users', 'third_name', NULL, 'Third'),
+('Users', 'last_name', NULL, 'Last'),
+('Users', 'preferred_name', NULL, 'Preferred'),
+('Users', 'address', NULL, 'Address'),
+('Users', 'postal_code', NULL, 'Postal'),
+('Users', 'address_area_id', NULL, 'Address'),
+('Users', 'birthplace_area_id', NULL, 'Birthplace'),
+('Users', 'gender_id', NULL, 'Gender'),
+('Users', 'date_of_birth', NULL, 'Date'),
+('Users', 'date_of_death', NULL, 'Date'),
+('Users', 'status', NULL, 'Status'),
+('Users', 'photo_content', NULL, 'Profile Image'),
+-- Contacts
+('Contacts', 'contact_type_id', NULL, 'Description'),
+('Contacts', 'contact_option_id', NULL, 'Type'),
+-- Identities
+('Identities', 'number', NULL, 'Number'),
+('Identities', 'issue_date', NULL, 'Issue Date'),
+('Identities', 'expiry_date', NULL, 'Expiry Date'),
+('Identities', 'issue_location', NULL, 'Issuer'),
+('Identities', 'identity_type_id', NULL, 'Identity Type'),
+-- Languages
+('Languages', 'evaluation_date', NULL, 'Evaluation Date'),
+('Languages', 'language_id', NULL, 'Language'),
+('Languages', 'listening', NULL, 'Listening'),
+('Languages', 'speaking', NULL, 'Speaking'),
+('Languages', 'reading', NULL, 'Reading'),
+('Languages', 'writing', NULL, 'Writing'),
+-- Comments
+('Comments', 'comment_date', NULL, 'Date'),
+('Comments', 'comment', NULL, 'Comment'),
+-- SpecialNeeds
+('SpecialNeeds', 'special_need_type_id', NULL, 'Type'),
+('SpecialNeeds', 'special_need_date', NULL, 'Date'),
+('SpecialNeeds', 'comment', NULL, 'Comment'),
+-- Awards
+('Awards', 'issue_date', NULL, 'Issue Date'),
+('Awards', 'award', NULL, 'Name'),
+('Awards', 'issuer', NULL, 'Issuer'),
+('Awards', 'comment', NULL, 'Comment'),
+-- Absences
+('Absences', 'academic_period_id', NULL, 'Academic Period'),
+('Absences', 'institution_site_class_id', NULL, 'Class'),
+('Absences', 'institution_site_section_id', NULL, 'Section'),
+('Absences', 'student_id', NULL, 'Student'),
+('Absences', 'absence_type', NULL, 'Type'),
+('Absences', 'student_absence_reason_id', NULL, 'Reason'),
+('Absences', 'select_section', NULL, 'Select Section'),
+-- Behaviours
+('Behaviours', 'name', NULL, 'Title'),
+('Behaviours', 'student_behaviour_category_id', NULL, 'Category'),
+('Behaviours', 'staff_behaviour_category_id', NULL, 'Category'),
+('Behaviours', 'date_of_behaviour', NULL, 'Date'),
+('Behaviours', 'time_of_behaviour', NULL, 'Time'),
+-- StudentFees
+('StudentFees', 'title', NULL, 'Fees'),
+('StudentFees', 'programme', NULL, 'Programme'),
+('StudentFees', 'grade', NULL, 'Grade'),
+('StudentFees', 'fees', NULL, 'Fees'),
+('StudentFees', 'paid', NULL, 'Paid'),
+('StudentFees', 'outstanding', NULL, 'Outstanding'),
+('StudentFees', 'no_student', NULL, 'No Student associated in the selected Education Grade and Academic Period.'),
+('StudentFees', 'no_payment', NULL, 'No Payment Records.'),
+('StudentFees', 'no_fees', NULL, 'No Fee Records.'),
+('StudentFees', 'created', NULL, 'Create'),
+-- Qualifications
+('Qualifications', 'qualification_institution_country', NULL, 'Institution Country'),
+('Qualifications', 'gpa', NULL, 'Grade/Score'),
+('Qualifications', 'file_name', NULL, 'Attachment'),
+-- Positions
+('Positions', 'title', NULL, 'Positions'),
+('Positions', 'name', NULL, 'Position'),
+('Positions', 'teaching', NULL, 'Teaching'),
+('Positions', 'number', NULL, 'Number'),
+('Positions', 'institution_site_position_id', NULL, 'Position')
+
+
+;
 
