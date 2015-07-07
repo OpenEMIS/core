5eece1d76f7b00d95d18c3d526aeea2cf5c1a139
Jeff Zheng <jzheng@kordit.com>
2015-07-07 21:14:17 +0800

    PHPOE-1407 - Fixed field options

--- plugins/FieldOption/src/Model/Table/FieldOptionValuesTable.php
+++ plugins/FieldOption/src/Model/Table/FieldOptionValuesTable.php
@@ -2,6 +2,9 @@
 namespace FieldOption\Model\Table;
 
 use ArrayObject;
+use Cake\ORM\Query;
+use Cake\ORM\Entity;
+use Cake\ORM\TableRegistry;
 use Cake\Event\Event;
 use Cake\Network\Request;
 use Cake\Validation\Validator;
@@ -16,15 +19,22 @@ class FieldOptionValuesTable extends AppTable {
 		$this->belongsTo('FieldOptions', ['className' => 'FieldOption.FieldOptions']);
 	}
 
+	public function onGetEditable(Event $event, Entity $entity) {
+		return $entity->editable == 1 ? '<i class="fa fa-check"></i>' : '<i class="fa fa-close"></i>';
+	}
+
+	public function onGetDefault(Event $event, Entity $entity) {
+		return $entity->default == 1 ? '<i class="fa fa-check"></i>' : '<i class="fa fa-close"></i>';
+	}
+
 	public function beforeAction(Event $event) {
 		$this->ControllerAction->field('field_option_id', ['type' => 'hidden']);
 		$this->ControllerAction->field('order', ['type' => 'hidden']);
-		$this->ControllerAction->field('visible', ['options' => $this->getSelectOptions('general.yesno')]);
 		$this->ControllerAction->field('default', ['options' => $this->getSelectOptions('general.yesno')]);
 		$this->ControllerAction->field('editable', ['options' => $this->getSelectOptions('general.yesno'), 'visible' => ['index' => true]]);
 	}
 
-	public function indexBeforeAction(Event $event) {
+	public function indexBeforeAction(Event $event, Query $query, ArrayObject $settings) {
 		$toolbarElements = [
 			['name' => 'FieldOption.controls', 'data' => [], 'options' => []]
 		];
@@ -33,12 +43,12 @@ class FieldOptionValuesTable extends AppTable {
 		$this->ControllerAction->setFieldOrder([
 			'visible', 'default', 'editable', 'name', 'national_code'
 		]);
-	}
 
-	public function indexBeforePaginate(Event $event, Request $request, ArrayObject $options) {
+		$settings['pagination'] = false;
+
 		$fieldOptions = [];
 		$data = $this->FieldOptions
-			->find('all')
+			->find()
 			->find('visible')
 			->find('order')
 			->all();
@@ -50,14 +60,24 @@ class FieldOptionValuesTable extends AppTable {
 			if (!array_key_exists($parent, $fieldOptions)) {
 				$fieldOptions[$parent] = array();
 		}
-			$fieldOptions[$parent][$key] = __($obj->name);	
+			$fieldOptions[$parent][$key] = __($obj->name);
 		}
 		$this->controller->set('fieldOptions', $fieldOptions);
 
-		$selectedOption = $this->queryString('field_option_id', $fieldOptions, $request);
+		$selectedOption = $this->queryString('field_option_id', $fieldOptions);
 		$this->controller->set('selectedOption', $selectedOption);
 
-		$options['conditions'][$this->aliasField('field_option_id')] = $selectedOption;
+		$fieldOption = $this->FieldOptions->get($selectedOption);
+		if (!empty($fieldOption->params)) {
+			$params = json_decode($fieldOption->params);
+			$table = TableRegistry::get($params->model);
+
+			$query = $table->find();
+		} else {
+			$query->where([$this->aliasField('field_option_id') => $selectedOption]);
+		}
+
+		return $query->find('order');
 	}
 
 	public function viewBeforeAction(Event $event) {
