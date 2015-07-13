3ce77b86aecacb065b1bcf71f09a1f3c9c7c4394
Chiang Hao Long <hchiang@kordit.com>
2015-07-13 15:57:06 +0800

    PHPOE-1577 - Added header information.

--- plugins/Localization/src/Model/Table/TranslationsTable.php
+++ plugins/Localization/src/Model/Table/TranslationsTable.php
@@ -14,11 +14,16 @@ class TranslationsTable extends AppTable {
 		$config['Modified'] = false;
 		$config['Created'] = false;
 		parent::initialize($config);
+		$this->belongsTo('ModifiedUser', ['className' => 'Security.Users', 'foreignKey' => 'modified_user_id']);
+		$this->belongsTo('CreatedUser', ['className' => 'Security.Users', 'foreignKey' => 'created_user_id']);
 	}
 
-	// public function beforeAction(Event $event) {
-	// 	pr($this->fields);die;
-	// }
+	public function beforeAction(Event $event) {
+		$this->ControllerAction->field('modified', ['visible' => false]);
+		$this->ControllerAction->field('modified_user_id', ['visible' => false]);
+		$this->ControllerAction->field('created', ['visible' => false]);
+		$this->ControllerAction->field('created_user_id', ['visible' => false]);
+	}
 
 	// Search component
 
