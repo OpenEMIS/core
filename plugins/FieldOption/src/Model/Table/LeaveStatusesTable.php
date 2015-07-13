5e7536695a1de8c638aff50cdf0121f810180d01
Shivapriya D/O Paramasivam <sparamasivam@kordit.com>
2015-07-13 15:26:02 +0800

    Adding cascadecallbacks to hasMany relations to tables in fieldoption config

--- plugins/FieldOption/src/Model/Table/LeaveStatusesTable.php
+++ plugins/FieldOption/src/Model/Table/LeaveStatusesTable.php
@@ -7,6 +7,6 @@ use Cake\Validation\Validator;
 class LeaveStatusesTable extends AppTable {
 	public function initialize(array $config) {
 		$this->addBehavior('ControllerAction.FieldOption');
-		$this->hasMany('Leaves', ['className' => 'Staff.Leaves']);
+		$this->hasMany('Leaves', ['className' => 'Staff.Leaves', 'dependent' => true, 'cascadeCallbacks' => true]);
 	}
 }
