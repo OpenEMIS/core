a99d95c4341eb004720f8b76c1c6c0d2017eb082
Jeff Zheng <jzheng@kordit.com>
2015-07-14 13:49:46 +0800

    PHPOE-1555 - removed custom note type from table

--- src/Model/Table/ConfigItemsTable.php
+++ src/Model/Table/ConfigItemsTable.php
@@ -217,22 +217,6 @@ class ConfigItemsTable extends AppTable {
 		return $this->recordValueForView('default_value', $entity);
 	}
 
-	public function onGetCustomNotesElement(Event $event, $action, $entity, $attr, $options=[]) {
-		$fieldLabel = Inflector::humanize($attr['field']);
-		if (array_key_exists('label', $attr)) {
-			$fieldName = $attr['label'];
-		}
-		$fieldName = strtolower($attr['model'] . '-' . $attr['field']);
-		if (array_key_exists('fieldName', $attr)) {
-			$fieldName = $attr['fieldName'];
-		}
-		if (!array_key_exists('value', $attr)) {
-			$attr['value'] = '* Please set the note in your model *';
-		}
-		$value = '<div class="input text"><label for="'.$fieldName.'">'.$fieldLabel.'</label><div class="button-label" style="width: 65%;">'.$attr['value'].'</div></div>';
-		return $value;
-	}
-
 	public function onUpdateIncludes(Event $event, ArrayObject $includes, $action) {
 		$includes['configItems'] = ['include' => true, 'js' => ['config']];
 	}
