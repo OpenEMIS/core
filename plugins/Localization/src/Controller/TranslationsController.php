ea453f1cdb13e5f48ca226d0b8aeeaa547f5b6e7
Chiang Hao Long <hchiang@kordit.com>
2015-07-14 17:23:49 +0800

    PHPOE-1577 - Modified translation logic to get the locale file location from the App.paths function

--- plugins/Localization/src/Controller/TranslationsController.php
+++ plugins/Localization/src/Controller/TranslationsController.php
@@ -5,6 +5,7 @@ use Cake\Event\Event;
 use Cake\ORM\Table;
 use Cake\Utility\Inflector;
 use Cake\I18n\I18n;
+use Cake\Core\App;
 
 class TranslationsController extends AppController {
 	public function initialize() {
@@ -24,9 +25,29 @@ class TranslationsController extends AppController {
 		$this->set('contentHeader', __($header));
 	}
 
-	public function beforeAction(Event $event){
-		$currentLocale = I18n.locale();
-		$this->ControllerAction->field("eng");
+	private $defaultLocale = 'en';
+
+	public function convert(){
+		$this->autoRender = false;
+		$locale = $this->request->query('locale');
+		$this->convertPO($locale);
+	}
+
+	private function convertPO($locale){
+		$this->autoRender = false;
+		// /rootURL/openemis-phpoe/src/locales/
+		$localeDir = App::path('locales');
+		$localeDir = $localeDir[0];
+		$fileLocation = $localeDir . $locale . DS . 'default.po';
+		$data = $this->Translations->find('all', [$this->defaultLocale, $locale]);
+		
+		// Check if the translation file exist
+		// if (!file_exists($fileLocation)) {
+		// 	// Open the file for write
+		// 	fopen($fileLocation, 'w');
+		// 	fclose($opFile);
+		// }
+		//pr($data);
 	}
 
 	// public function onInitialize(Event $event, $model) {
