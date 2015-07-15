a5bd738fcc7d825856a8acda800aeb87ef8d4cf3
Chiang Hao Long <hchiang@kordit.com>
2015-07-15 23:36:48 +0800

    PHPOE-1577 - Updated sql files and fixed translation table display bug

--- sql/Patch/PHPOE-1577/commit.sql
+++ sql/Patch/PHPOE-1577/commit.sql
@@ -6,3 +6,10 @@ CHANGE COLUMN `chi` `zh` TEXT NULL DEFAULT NULL,
 CHANGE COLUMN `spa` `es` TEXT NULL DEFAULT NULL,
 CHANGE COLUMN `fre` `fr` TEXT NULL DEFAULT NULL,
 CHANGE COLUMN `rus` `ru` TEXT NULL DEFAULT NULL;
+INSERT INTO `labels` (`module`, `created_user_id`, `created`, `field`, `en`) VALUES
+('Translations', 1, Now(), 'en', 'English'),
+('Translations', 1, Now(), 'ar', 'العربية'),
+('Translations', 1, Now(), 'zh', ' 中文'),
+('Translations', 1, Now(), 'fr', 'Français'),
+('Translations', 1, Now(), 'ru', 'русский'),
+('Translations', 1, Now(), 'es', 'español');
