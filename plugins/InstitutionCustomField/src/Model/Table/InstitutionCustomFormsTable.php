03c3ba56de024092a5128804f5393ad836c8d1c2
Thed Kuok Shiun <kuokshiun@hotmail.com>
2015-07-16 21:18:45 +0800

    PHPOE-1461 - Modified Custom Field (Institution).

--- plugins/InstitutionCustomField/src/Model/Table/InstitutionCustomFormsTable.php
+++ plugins/InstitutionCustomField/src/Model/Table/InstitutionCustomFormsTable.php
@@ -9,16 +9,38 @@ use Cake\Event\Event;
 
 class InstitutionCustomFormsTable extends CustomFormsTable {
 	public function initialize(array $config) {
+		$config['custom_filter'] = [
+			'className' => 'FieldOption.InstitutionSiteTypes',
+			'joinTable' => 'institution_custom_form_filters',
+			'foreignKey' => 'institution_custom_form_id',
+			'targetForeignKey' => 'institution_custom_filter_id',
+			'through' => 'InstitutionCustomField.InstitutionCustomFormsFilters',
+			'dependent' => true
+		];
 		parent::initialize($config);
 		$this->belongsTo('CustomModules', ['className' => 'CustomField.CustomModules']);
 		$this->hasMany('Institutions', ['className' => 'Institution.Institutions', 'foreignKey' => 'infrastructure_site_id', 'dependent' => true, 'cascadeCallbacks' => true]);
 		$this->belongsToMany('CustomFields', [
 			'className' => 'InstitutionCustomField.InstitutionCustomFields',
-			'joinTable' => 'institution_site_custom_forms_fields',
-			'foreignKey' => 'institution_site_custom_form_id',
-			'targetForeignKey' => 'institution_site_custom_field_id',
+			'joinTable' => 'institution_custom_forms_fields',
+			'foreignKey' => 'institution_custom_form_id',
+			'targetForeignKey' => 'institution_custom_field_id',
 			'through' => 'InstitutionCustomField.InstitutionCustomFormsFields',
 			'dependent' => true
 		]);
+	}
+
+	public function _getSelectOptions() {
+		list($moduleOptions, $selectedModule, $applyToAllOptions, $selectedApplyToAll) = array_values(parent::_getSelectOptions());
+		$moduleOptions = $this->CustomModules
+			->find('list')
+			->find('visible')
+			->where([
+				$this->CustomModules->aliasField('code') => 'Institution'
+			])
+			->toArray();
+		$selectedModule = $this->queryString('module', $moduleOptions);
+
+		return compact('moduleOptions', 'selectedModule', 'applyToAllOptions', 'selectedApplyToAll');
 	}
 }
