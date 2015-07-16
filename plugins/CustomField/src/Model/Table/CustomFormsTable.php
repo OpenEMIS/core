88611ca1a2d31874d264a1f25bd2644010d33616
Thed Kuok Shiun <kuokshiun@hotmail.com>
2015-07-16 20:10:14 +0800

    PHPOE-1461 - Make changes to Custom Form.

--- plugins/CustomField/src/Model/Table/CustomFormsTable.php
+++ plugins/CustomField/src/Model/Table/CustomFormsTable.php
@@ -14,6 +14,7 @@ use App\Model\Traits\OptionsTrait;
 class CustomFormsTable extends AppTable {
 	use OptionsTrait;
 
+	private $_fieldOrder = ['custom_module_id', 'name', 'description', 'custom_fields'];
 	private $_contain = ['CustomFilters', 'CustomFields'];
 
 	public function initialize(array $config) {
@@ -25,7 +26,8 @@ class CustomFormsTable extends AppTable {
 			'foreignKey' => 'custom_form_id',
 			'targetForeignKey' => 'custom_filter_id',
 			'through' => 'CustomField.CustomFormsFilters',
-			'dependent' => true
+			'dependent' => true,
+			'saveStrategy' => 'replace'
 		]);
 		$this->belongsToMany('CustomFields', [
 			'className' => 'CustomField.CustomFields',
@@ -37,40 +39,42 @@ class CustomFormsTable extends AppTable {
 		]);
 	}
 
-	public function beforeAction(Event $event) {
-	}
-
-	/*
 	public function afterSave(Event $event, Entity $entity, ArrayObject $options) {
-		if (isset($entity->apply_to_all) && $entity->apply_to_all == 1) {
-			$where = [
-				$this->aliasField('custom_module_id') => $entity->custom_module_id,
-				$this->aliasField('id !=') => $entity->id
-			];
+		if (isset($entity->apply_to_all) && $entity->apply_to_all == 1) {			
+			$customFormIds = $this
+				->find('list', ['keyField' => 'id', 'valueField' => 'id'])
+				->where([
+					$this->aliasField('custom_module_id') => $entity->custom_module_id
+				])
+				->toArray();
 
-			$customFormIds = $this->find('list', ['keyField' => 'id', 'valueField' => 'id'])->where($where)->toArray();
-			$this->CustomFormsFilters->deleteAll([
-				$this->CustomFormsFilters->aliasField('custom_form_id IN') => $customFormIds,
-				$this->CustomFormsFilters->aliasField('custom_filter_id') => 0
+			$CustomFormsFilters = TableRegistry::get('CustomField.CustomFormsFilters');
+			$CustomFormsFilters->deleteAll([
+				$CustomFormsFilters->aliasField('custom_form_id IN') => $customFormIds,
+				$CustomFormsFilters->aliasField('custom_filter_id') => 0
 			]);
 
-			$CustomFormsFiltersTable = $this->CustomFormsFilters;
-			$CustomFormFilter = $CustomFormsFiltersTable->newEntity();
-			$CustomFormFilter->custom_form_id = $entity->id;
-			$CustomFormFilter->custom_filter_id = 0;
-			if ($CustomFormsFiltersTable->save($CustomFormFilter)) {
+			$filterData = [
+				'custom_form_id' => $entity->id,
+				'custom_filter_id' => 0
+			];
+			$filterEntity = $CustomFormsFilters->newEntity($filterData);
+
+			if ($CustomFormsFilters->save($filterEntity)) {
 			} else {
-				$this->CustomFormsFilters->log($CustomFormFilter->errors(), 'debug');
+				$CustomFormsFilters->log($filterEntity->errors(), 'debug');
 			}
 		}
 	}
 
 	public function beforeAction(Event $event) {
-		$this->ControllerAction->field('apply_to_all', ['type' => 'select']);
-		$this->ControllerAction->field('custom_fields', [
-			'type' => 'chosenSelect',
-			'placeholder' => __('Select Fields')
-		]);
+		if ($this->action == 'index') {
+			$this->initFields();
+		}
+	}
+
+	public function afterAction(Event $event) {
+		$this->ControllerAction->setFieldOrder($this->_fieldOrder);
 	}
 
 	public function indexBeforeAction(Event $event) {
@@ -83,7 +87,7 @@ class CustomFormsTable extends AppTable {
 	}
 
 	public function indexBeforePaginate(Event $event, Request $request, ArrayObject $options) {
-		list($moduleOptions, $selectedModule) = array_values($this->getSelectOptions());
+		list($moduleOptions, $selectedModule) = array_values($this->_getSelectOptions());
         $this->controller->set(compact('moduleOptions', 'selectedModule'));
 
 		$options['conditions'][] = [
@@ -92,175 +96,183 @@ class CustomFormsTable extends AppTable {
         $options['contain'] = array_merge($options['contain'], $this->_contain);
 	}
 
-	public function indexAfterAction(Event $event, $data) {
-		list(, $selectedModule) = array_values($this->getSelectOptions());
-		$filter = $this->CustomModules->findById($selectedModule)->first()->filter;
-
-		if (is_null($filter)) {
-			$this->fields['apply_to_all']['visible'] = false;
-		} else {
-			$this->fields['apply_to_all']['visible'] = true;
-		}
-
-		return $data;
-	}
-
-	public function viewBeforeAction(Event $event) {
-		$this->setFieldOrder();
-	}
-
 	public function viewEditBeforeQuery(Event $event, Query $query) {
 		$query->contain($this->_contain);
 	}
 
 	public function viewAfterAction(Event $event, Entity $entity) {
-		$selectedModule = $entity->custom_module_id;
-		$filter = $this->CustomModules->findById($selectedModule)->first()->filter;
-
-		if (is_null($filter)) {
-			$this->fields['apply_to_all']['visible'] = false;
-		} else {
-			$this->fields['apply_to_all']['visible'] = true;
-		}
-
-		return $entity;
-	}
-
-	public function addEditBeforeAction(Event $event) {
-		//Setup fields
-		list($moduleOptions, , $applyToAllOptions) = array_values($this->getSelectOptions());
-
-		$this->fields['custom_module_id']['options'] = $moduleOptions;
-		$this->fields['custom_module_id']['onChangeReload'] = true;
-
-		$this->fields['apply_to_all']['options'] = $applyToAllOptions;
-		$this->fields['apply_to_all']['attr'] = [
-			'onchange' => 'if(this.value == 1){$("#customforms-field-option-values-ids").val("").trigger("chosen:updated");};'
-		];
-
-		$customFieldOptions = $this->CustomFields->find('list')->toArray();
-		$this->fields['custom_fields']['options'] = $customFieldOptions;
-
-		$this->setFieldOrder();
-	}
-
-	public function addEditBeforePatch(Event $event, Entity $entity, ArrayObject $data, ArrayObject $options) {
-		//Required by patchEntity for associated data
-		$newOptions = [];
-		$newOptions['associated'] = $this->_contain;
-
-		$arrayOptions = $options->getArrayCopy();
-		$arrayOptions = array_merge_recursive($arrayOptions, $newOptions);
-		$options->exchangeArray($arrayOptions);
+		$this->initFields();
 	}
 
 	public function addEditAfterAction(Event $event, Entity $entity) {
-		$selectedModule = $entity->custom_module_id;
-		$filter = $this->CustomModules->findById($selectedModule)->first()->filter;
-
-		if (is_null($filter)) {
-			$this->fields['apply_to_all']['visible'] = false;
-		} else {
-			$this->fields['apply_to_all']['visible'] = true;
-
-			$modelAlias = $this->ControllerAction->getModel($filter)['model'];
-			$labelText = Inflector::underscore(Inflector::singularize($modelAlias));
-
-			$filterOptionTable = TableRegistry::get($filter);
-			$filterOptions = $filterOptionTable->getList()->toArray();
-
-			$this->ControllerAction->addField($labelText, [
-				'type' => 'chosenSelect',
-				'fieldNameKey' => 'field_option_values',
-				'fieldName' => $this->alias() . '.field_option_values._ids',
-				'placeholder' => __('Select ') . __(Inflector::humanize($labelText)),
-				'options' => $filterOptions,
-				'order' => 3,
-				'visible' => true,
-				'attr' => [
-					'label' => __(Inflector::humanize($labelText)),
-					'onchange' => 'if($(this).val()){$("#customforms-apply-to-all").val(0);};'
-				]
-			]);
-
-			$order = 2;
-			$this->ControllerAction->setFieldOrder('apply_to_all', $order++);
-			$this->ControllerAction->setFieldOrder($labelText, $order++);
-		}
-
-		return $entity;
+		$this->initFields();
 	}
 
 	public function addOnInitialize(Event $event, Entity $entity) {
 		//Initialize field values
-		list(, $selectedModule, , $selectedApplyToAll) = array_values($this->getSelectOptions());
+		list($moduleOptions, $selectedModule, $applyToAllOptions, $selectedApplyToAll) = array_values($this->_getSelectOptions());
+
 		$entity->custom_module_id = $selectedModule;
 		$entity->apply_to_all = $selectedApplyToAll;
+		$this->request->query['apply_to_all'] = $selectedApplyToAll;
 
 		return $entity;
 	}
 
 	public function editOnInitialize(Event $event, Entity $entity) {
-		//Initialize field values
-		list(, , , $selectedApplyToAll) = array_values($this->getSelectOptions());
+		list(, , $applyToAllOptions, $selectedApplyToAll) = array_values($this->_getSelectOptions());
 
-		$results = $this->CustomFormsFilters
-			->find('all')
+		$CustomFormsFilters = TableRegistry::get('CustomField.CustomFormsFilters');
+		$results = $CustomFormsFilters
+			->find()
 			->where([
-				$this->CustomFormsFilters->aliasField('custom_form_id') => $entity->id,
-				$this->CustomFormsFilters->aliasField('custom_filter_id') => 0		
-			]);
+				$CustomFormsFilters->aliasField('custom_form_id') => $entity->id,
+				$CustomFormsFilters->aliasField('custom_filter_id') => 0		
+			])
+			->all();
 
-		if (!$results->isEmpty()) {
-			$selectedApplyToAll = 1;
+		if ($results->isEmpty()) {
+			$selectedApplyToAll = 0;
 		}
 		$entity->apply_to_all = $selectedApplyToAll;
+		$this->request->query['apply_to_all'] = $selectedApplyToAll;
 
 		return $entity;
 	}
 
-    public function onGetApplyToAll(Event $event, Entity $entity) {
-    	$list = [];
-		if (!empty($entity->field_option_values)) {
-			foreach ($entity->field_option_values as $obj) {
-				$list[] = $obj->name;
-			}
+	public function onGetApplyToAll(Event $event, Entity $entity) {
+		if (sizeof($entity->custom_filters) > 0) {
+			$value = __('No');
 		} else {
-			$results = $this->CustomFormsFilters
-				->find('all')
+			$CustomFormsFilters = TableRegistry::get('CustomField.CustomFormsFilters');
+			$results = $CustomFormsFilters
+				->find()
 				->where([
-					$this->CustomFormsFilters->aliasField('custom_form_id') => $entity->id,
-					$this->CustomFormsFilters->aliasField('custom_filter_id') => 0
-				]);
+					$CustomFormsFilters->aliasField('custom_form_id') => $entity->id,
+					$CustomFormsFilters->aliasField('custom_filter_id') => 0
+				])
+				->all();
 
-			if (!$results->isEmpty()) {
-				$list[] = __('Apply To All');
+			if ($results->isEmpty()) {
+				$value = __('No');
+			} else {
+				$value = __('Yes');
 			}
 		}
 
-        return implode(', ', $list);
+		return $value;
     }
 
-	public function getSelectOptions() {
-		//Return all required options and their key
-		$query = $this->request->query;
+	public function onUpdateFieldCustomModuleId(Event $event, array $attr, $action, $request) {
+		list($moduleOptions, $selectedModule) = array_values($this->_getSelectOptions());
 
+		$attr['options'] = $moduleOptions;
+		$attr['onChangeReload'] = true;
+
+		return $attr;
+	}
+
+	public function onUpdateFieldApplyToAll(Event $event, array $attr, $action, $request) {
+		$attr['options'] = $this->getSelectOptions('general.yesno');
+		$attr['onChangeReload'] = true;
+
+		return $attr;
+	}
+
+	public function onUpdateFieldCustomFilters(Event $event, array $attr, $action, $request) {
+		$moduleOptions = $this->fields['custom_module_id']['options'];
+		$applyToAllOptions = $this->fields['apply_to_all']['options'];
+
+		if ($action == 'index' || $action == 'view') {
+			$selectedModule = $this->queryString('module', $moduleOptions);
+			$selectedApplyToAll = 0;
+		} else {
+			$selectedModule = key($moduleOptions);
+			$selectedModule = $this->queryString('module', $moduleOptions);
+			$selectedApplyToAll = key($applyToAllOptions);
+
+			if ($request->is(['get'])) {
+				$selectedModule = $this->request->query('module');
+				$selectedApplyToAll = $this->request->query('apply_to_all');
+			} else if ($this->request->is(['post', 'put'])) {
+				if (array_key_exists($this->alias(), $request->data)) {
+					if (array_key_exists('custom_module_id', $request->data[$this->alias()])) {
+						$selectedModule = $request->data[$this->alias()]['custom_module_id'];
+					}
+					if (array_key_exists('apply_to_all', $request->data[$this->alias()])) {
+						$selectedApplyToAll = $request->data[$this->alias()]['apply_to_all'];
+					}
+				}
+			}
+		}
+
+		$customModule = $this->CustomModules->get($selectedModule);
+		$filter = $customModule->filter;
+
+		if (empty($filter)) {
+			$this->fields['apply_to_all']['visible'] = false;
+			$attr['visible'] = false;
+		} else {
+			$this->fields['apply_to_all']['visible'] = true;
+
+			if ($selectedApplyToAll == 1) {
+				$attr['visible'] = false;
+
+				$this->_fieldOrder = ['custom_module_id', 'apply_to_all', 'name', 'description', 'custom_fields'];
+			} else {
+				$modelAlias = $this->ControllerAction->getModel($filter)['model'];
+				$labelText = Inflector::underscore(Inflector::singularize($modelAlias));
+				$filterOptions = TableRegistry::get($filter)->getList()->toArray();
+
+				$attr['placeholder'] = __('Select ') . __(Inflector::humanize($labelText));
+				$attr['options'] = $filterOptions;
+				$attr['attr']['label'] = __(Inflector::humanize($labelText));
+				$attr['visible'] = true;
+
+				$this->_fieldOrder = ['custom_module_id', 'apply_to_all', 'custom_filters', 'name', 'description', 'custom_fields'];
+			}
+		}
+		
+
+		return $attr;
+	}
+
+	public function onUpdateFieldCustomFields(Event $event, array $attr, $action, $request) {
+		$customFieldOptions = $this->CustomFields
+			->find('list')
+			->toArray();
+		$attr['options'] = $customFieldOptions;
+
+		return $attr;
+	}
+
+	public function initFields() {
+		$this->ControllerAction->field('custom_module_id');
+		$this->ControllerAction->field('apply_to_all', [
+			'type' => 'select',
+			'visible' => ['index' => true, 'view' => true, 'edit' => false]
+		]);
+		$this->ControllerAction->field('custom_filters', [
+			'type' => 'chosenSelect',
+			'visible' => ['index' => true, 'view' => true, 'edit' => false]
+		]);
+		$this->ControllerAction->field('custom_fields', [
+			'type' => 'chosenSelect',
+			'placeholder' => __('Select Fields')
+		]);
+	}
+
+	public function _getSelectOptions() {
+		//Return all required options and their key
 		$moduleOptions = $this->CustomModules
 			->find('list')
 			->find('visible')
 			->toArray();
-		$selectedModule = isset($query['module']) ? $query['module'] : key($moduleOptions);
+		$selectedModule = $this->queryString('module', $moduleOptions);
 
-		$applyToAllOptions = [0 => __('No'), 1 => __('Yes')];
-		$selectedApplyToAll = key($applyToAllOptions);
+		$applyToAllOptions = $this->getSelectOptions('general.yesno');
+		$selectedApplyToAll = !is_null($this->request->query('apply_to_all')) ? $this->request->query('apply_to_all') : key($applyToAllOptions);
 
 		return compact('moduleOptions', 'selectedModule', 'applyToAllOptions', 'selectedApplyToAll');
 	}
-
-	public function setFieldOrder() {
-		$this->ControllerAction->setFieldOrder([
-			'custom_module_id', 'apply_to_all', 'name', 'custom_fields'
-		]);
-	}
-	*/
 }
