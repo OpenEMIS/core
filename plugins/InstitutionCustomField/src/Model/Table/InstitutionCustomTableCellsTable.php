0244e3190c87e19ea0032f826c820f4a37b40a5a
Thed Kuok Shiun <kthed@kordit.com>
2015-07-19 21:32:16 +0800

    PHPOE-1461 - Change structure of Custom Field (Institution) and Custom Field (Infrastructure) to be same.

--- plugins/InstitutionCustomField/src/Model/Table/InstitutionCustomTableCellsTable.php
+++ plugins/InstitutionCustomField/src/Model/Table/InstitutionCustomTableCellsTable.php
@@ -1,12 +1,12 @@
 <?php
-namespace Institution\Model\Table;
+namespace InstitutionCustomField\Model\Table;
 
 use CustomField\Model\Table\CustomTableCellsTable;
 
 class InstitutionCustomTableCellsTable extends CustomTableCellsTable {
 	public function initialize(array $config) {
 		parent::initialize($config);
-		$this->belongsTo('CustomFields', ['className' => 'CustomField.CustomFields']);
+		$this->belongsTo('CustomFields', ['className' => 'InstitutionCustomField.InstitutionCustomFields', 'foreignKey' => 'institution_custom_field_id']);
 		$this->belongsTo('CustomRecords', ['className' => 'Institution.Institutions', 'foreignKey' => 'institution_site_id']);
 	}
 }
