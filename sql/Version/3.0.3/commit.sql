c02faa011792512a86caf2a5e1ac62c1534e8990
Benson Liang <bliang@kordit.com>
2015-07-24 18:42:49 +0800

    Shuffle around label SQL

--- sql/Version/3.0.3/commit.sql
+++ sql/Version/3.0.3/commit.sql
@@ -7,18 +7,7 @@ CHANGE COLUMN `chi` `zh` TEXT NULL DEFAULT NULL,
 CHANGE COLUMN `spa` `es` TEXT NULL DEFAULT NULL,
 CHANGE COLUMN `fre` `fr` TEXT NULL DEFAULT NULL,
 CHANGE COLUMN `rus` `ru` TEXT NULL DEFAULT NULL;
-INSERT INTO `labels` (`module`, `created_user_id`, `created`, `field`, `en`) VALUES
-('Translations', 1, Now(), 'en', 'English'),
-('Translations', 1, Now(), 'ar', 'العربية'),
-('Translations', 1, Now(), 'zh', ' 中文'),
-('Translations', 1, Now(), 'fr', 'Français'),
-('Translations', 1, Now(), 'ru', 'русский'),
-('Translations', 1, Now(), 'es', 'español');
 -- end PHPOE-1577
-
--- PHPOE-1669
-INSERT INTO `labels` (`module`, `field`, `en`, `created_user_id`, `created`) VALUES ('StaffPositions', 'security_user_id', 'Staff', '1', NOW());
--- end PHPOE-1669
 
 -- DB version
 UPDATE `config_items` SET `value` = '3.0.3' WHERE `code` = 'db_version';
