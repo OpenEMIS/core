d2db98a95a03ca41aee78d964fd92c56fc9bdb56
Chiang Hao Long <hchiang@kordit.com>
2015-07-25 19:59:43 +0800

    PHPOE-1683 - Implemented fix on to check on the student's start and end date to display on institution site class table

--- plugins/Institution/src/Model/Table/InstitutionSiteClassesTable.php
+++ plugins/Institution/src/Model/Table/InstitutionSiteClassesTable.php
@@ -512,18 +512,43 @@ class InstitutionSiteClassesTable extends AppTable {
 
 	public function editAfterAction(Event $event, Entity $entity) {
 		$this->_selectedSectionId = $entity->institution_site_section_classes[0]->institution_site_section_id;
-
 		$teacherOptions = $this->getTeacherOptions();
+
 		/**
 		 * @todo should have additional filter; by start_date, end_date & education_programme_id
 		 */
-		$query = $this
-			->Institutions
-			->InstitutionSiteStudents
-			->find()
+		$academicPeriodObj = $this->AcademicPeriods->get($this->_selectedAcademicPeriodId);
+		$startDate = $this->AcademicPeriods->getDate($academicPeriodObj->start_date);
+        $endDate = $this->AcademicPeriods->getDate($academicPeriodObj->end_date);
+		$students = $this->Institutions->InstitutionSiteStudents;
+		$query = $students->find()
 			->contain(['Users'])
 			->where([
-				'InstitutionSiteStudents.institution_site_id'=>$entity->institution_site_id
+				'InstitutionSiteStudents.institution_site_id'=>$entity->institution_site_id,
+				// Check date behavior to be the same as institution site section table
+				'OR' => array(
+					'OR' => array(
+						array(
+							$students->aliasField('end_date').' IS NOT NULL',
+							$students->aliasField('start_date').' <= "' . $startDate . '"',
+							$students->aliasField('end_date').' >= "' . $startDate . '"'
+						),
+						array(
+							$students->aliasField('end_date').' IS NOT NULL',
+							$students->aliasField('start_date').' <= "' . $endDate . '"',
+							$students->aliasField('end_date').' >= "' . $endDate . '"'
+						),
+						array(
+							$students->aliasField('end_date').' IS NOT NULL',
+							$students->aliasField('start_date').' >= "' . $startDate . '"',
+							$students->aliasField('end_date').' <= "' . $endDate . '"'
+						)
+					),
+					array(
+						$students->aliasField('end_date').' IS NULL',
+						$students->aliasField('start_date').' <= "' . $endDate . '"'
+					)
+				)
 			])
 			->toArray();
 		$studentOptions = [$this->getMessage('Users.select_student')];
