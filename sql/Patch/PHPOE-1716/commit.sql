b84a0f357f2ebe670809e36433c47aa649790d86
Chiang Hao Long <hchiang@kordit.com>
2015-07-27 18:44:57 +0800

    PHPOE-1716 - Added a patch to set the student categories to not visible. Modified the patch file to patch all records instead of just orphan records.

--- sql/Patch/PHPOE-1716/commit.sql
+++ sql/Patch/PHPOE-1716/commit.sql
@@ -27,28 +27,33 @@ INSERT INTO `z_1716_field_option_values`
 SELECT `id`, `name`, `visible`, `editable`, `default`
 FROM `field_option_values`;
 
+-- Set field options for StudentCategories to be not visible
+UPDATE `field_options`
+SET `field_options`.`visible` = 0
+WHERE `field_options`.`code` = 'StudentCategories';
+
 -- Set visibility of all record to not visible
 UPDATE `field_option_values`
 SET `field_option_values`.`visible`=0, `field_option_values`.`default`=0
 WHERE `field_option_values`.`field_option_id` = 
-  ( SELECT field_options.id
-    FROM field_options 
-    WHERE field_options.code = 'StudentCategories');
+  ( SELECT `field_options`.`id`
+    FROM `field_options`
+    WHERE `field_options`.`code` = 'StudentCategories');
 
 -- Add Promoted Options into field option value
 INSERT INTO `field_option_values` (`name`, `order`, `visible`, `editable`, `default`, `field_option_id`, `created_user_id`, `created`)
-VALUES ('Promoted', '0', '1', '1', '1', 
-  (SELECT field_options.id FROM field_options WHERE field_options.code = 'StudentCategories')
+VALUES ('Promoted', '0', '1', '0', '1', 
+  (SELECT `field_options`.`id` FROM field_options WHERE `field_options`.`code` = 'StudentCategories')
     , '1', NOW());
 
 -- Set visibility for Promoted and Visible
 UPDATE `field_option_values`
-SET `field_option_values`.`visible`=1
+SET `field_option_values`.`visible`=1, `field_option_values`.`editable`=0
 WHERE 
   `field_option_values`.`field_option_id` = 
-    ( SELECT field_options.id
+    ( SELECT `field_options`.`id`
       FROM field_options 
-      WHERE field_options.code = 'StudentCategories')
+      WHERE `field_options`.`code` = 'StudentCategories')
   AND `field_option_values`.`name` = 'Promoted'
   OR `field_option_values`.`name` = 'Repeated';
 
@@ -59,12 +64,5 @@ SET student_category_id = (
     FROM `field_option_values`, `field_options`
 	WHERE `field_option_values`.`field_option_id` = `field_options`.`id`
 		AND `field_options`.`code` = 'StudentCategories'
-        AND `field_option_values`.`default` = 1
-)
--- Does not set all records only orphan records only
-WHERE student_category_id NOT IN (
-	SELECT `field_option_values`.`id`
-	FROM field_option_values, field_options
-	WHERE `field_option_values`.`field_option_id` = `field_options`.`id`
-		AND `field_options`.`code` = 'StudentCategories'
+    AND `field_option_values`.`default` = 1
 );
