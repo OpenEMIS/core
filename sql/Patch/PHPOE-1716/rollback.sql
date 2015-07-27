438b64f0c6ed0b404de6e894e0a4bfdf6ab3b587
Chiang Hao Long <hchiang@kordit.com>
2015-07-27 18:44:57 +0800

    PHPOE-1716 - Added a patch to set the student categories to not visible. Modified the patch file to patch all records instead of just orphan records.

--- sql/Patch/PHPOE-1716/rollback.sql
+++ sql/Patch/PHPOE-1716/rollback.sql
@@ -1,3 +1,9 @@
+-- Set field options for StudentCategories to be visible
+UPDATE `field_options`
+SET `field_options`.`visible` = 1
+WHERE `field_options`.`code` = 'StudentCategories';
+
+-- Remove the Promoted option from the student categories
 DELETE FROM `field_option_values`
 WHERE `field_option_values`.`field_option_id` = 
   ( SELECT `field_options`.`id` 
@@ -5,12 +11,14 @@ WHERE `field_option_values`.`field_option_id` =
     WHERE `field_options`.`code` = 'StudentCategories')
   AND `field_option_values`.`name` = 'Promoted';
 
+-- Revert the institution site section table
 UPDATE `institution_site_section_students` 
 LEFT JOIN `z_1716_institution_site_section_students` ON `institution_site_section_students`.`id` = `z_1716_institution_site_section_students`.`id`
   SET `institution_site_section_students`.`student_category_id` = `z_1716_institution_site_section_students`.`student_category_id`
   WHERE `institution_site_section_students`.`id` = `z_1716_institution_site_section_students`.`id`;
 DROP TABLE `z_1716_institution_site_section_students`;
 
+-- Revert the field options values table
 UPDATE `field_option_values`
 LEFT JOIN `z_1716_field_option_values` ON `field_option_values`.`id` = `z_1716_field_option_values`.`id`
   SET `field_option_values`.`name` = `z_1716_field_option_values`.`name`,
