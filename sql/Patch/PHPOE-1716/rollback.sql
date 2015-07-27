38c051a34fcda03d20ab99e42869df81ae34c9cc
Chiang Hao Long <hchiang@kordit.com>
2015-07-27 16:54:35 +0800

    PHPOE-1716 - Amended patch files and rollback

--- sql/Patch/PHPOE-1716/rollback.sql
+++ sql/Patch/PHPOE-1716/rollback.sql
@@ -1,21 +1,21 @@
-Drop Table IF EXISTS `institution_site_section_students`;
+DELETE FROM `field_option_values`
+WHERE `field_option_values`.`field_option_id` = 
+  ( SELECT `field_options`.`id` 
+    FROM `field_options` 
+    WHERE `field_options`.`code` = 'StudentCategories')
+  AND `field_option_values`.`name` = 'Promoted';
 
-CREATE TABLE `institution_site_section_students` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `security_user_id` int(11) NOT NULL,
-  `institution_site_section_id` int(11) NOT NULL,
-  `education_grade_id` int(11) NOT NULL,
-  `student_category_id` int(11) NOT NULL,
-  `status` int(1) NOT NULL,
-  `modified_user_id` int(11) DEFAULT NULL,
-  `modified` datetime DEFAULT NULL,
-  `created_user_id` int(11) NOT NULL,
-  `created` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `institution_site_id` (`institution_site_section_id`),
-  KEY `security_user_id` (`security_user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3334 DEFAULT CHARSET=utf8;
+UPDATE `institution_site_section_students` 
+LEFT JOIN `z_1716_institution_site_section_students` ON `institution_site_section_students`.`id` = `z_1716_institution_site_section_students`.`id`
+  SET `institution_site_section_students`.`student_category_id` = `z_1716_institution_site_section_students`.`student_category_id`
+  WHERE `institution_site_section_students`.`id` = `z_1716_institution_site_section_students`.`id`;
+DROP TABLE `z_1716_institution_site_section_students`;
 
-INSERT INTO `institution_site_section_students` 
-SELECT *
-FROM `z_1716_institution_site_section_students`;
+UPDATE `field_option_values`
+LEFT JOIN `z_1716_field_option_values` ON `field_option_values`.`id` = `z_1716_field_option_values`.`id`
+  SET `field_option_values`.`name` = `z_1716_field_option_values`.`name`,
+    `field_option_values`.`visible` = `z_1716_field_option_values`.`visible`,
+    `field_option_values`.`editable` = `z_1716_field_option_values`.`editable`,
+    `field_option_values`.`default` = `z_1716_field_option_values`.`default`
+  WHERE `field_option_values`.`id` = `z_1716_field_option_values`.`id`;
+DROP TABLE `z_1716_field_option_values`;
