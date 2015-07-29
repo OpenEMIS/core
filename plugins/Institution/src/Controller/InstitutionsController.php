aeb6f77a7c2e0ec40fda5027cd8995b0f57a60e0
Thed Kuok Shiun <kuokshiun@hotmail.com>
2015-07-29 17:40:33 +0800

    PHPOE-872 - Changes for to be implemented with custom autocomplete.

--- plugins/Institution/src/Controller/InstitutionsController.php
+++ plugins/Institution/src/Controller/InstitutionsController.php
@@ -175,12 +175,14 @@ class InstitutionsController extends AppController  {
 	public function beforePaginate(Event $event, Table $model, Query $query, ArrayObject $options) {
 		$session = $this->request->session();
 
-		if ($model->hasField('institution_site_id')) {
-			if (!$session->check('Institutions.id')) {
-				$this->Alert->error('general.notExists');
-				// should redirect
-			} else {
-				$query->where([$model->aliasField('institution_site_id') => $session->read('Institutions.id')]);
+		if (!$model->request->is('ajax')) {
+			if ($model->hasField('institution_site_id')) {
+				if (!$session->check('Institutions.id')) {
+					$this->Alert->error('general.notExists');
+					// should redirect
+				} else {
+					$query->where([$model->aliasField('institution_site_id') => $session->read('Institutions.id')]);
+				}
 			}
 		}
 	}
