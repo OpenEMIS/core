60eed2e394da6e234852e923cf08a6bda071a310
Malcolm Lee <mlee@kordit.com>
2015-07-31 09:59:45 +0800

    PHPOE-1715 User > Date of Birth 'restrict the end date to today's date - 2 year'

--- plugins/User/src/Model/Table/UsersTable.php
+++ plugins/User/src/Model/Table/UsersTable.php
@@ -91,6 +91,10 @@ class UsersTable extends AppTable {
 		$this->ControllerAction->field('address_area_id', ['type' => 'areapicker', 'source_model' => 'Area.AreaAdministratives']);
 		$this->ControllerAction->field('birthplace_area_id', ['type' => 'areapicker', 'source_model' => 'Area.AreaAdministratives']);
 
+		$this->ControllerAction->field('date_of_birth', ['date_options' => 
+			['endDate' => date('d-m-Y', strtotime("-2 year"))]
+		]);
+
 		if ($this->action == 'add') {
 			$this->ControllerAction->field('username', ['visible' => true]);
 			$this->ControllerAction->field('password', ['visible' => true, 'type' => 'password']);
