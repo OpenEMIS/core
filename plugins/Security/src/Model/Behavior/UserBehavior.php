b8503181ef0770d03279711c7ed25b69d1d4c359
Jeff Zheng <jeff.zhengrj@gmail.com>
2015-08-02 12:44:17 +0800

    fixed missing import class

--- plugins/Security/src/Model/Behavior/UserBehavior.php
+++ plugins/Security/src/Model/Behavior/UserBehavior.php
@@ -1,8 +1,10 @@
 <?php
 namespace Security\Model\Behavior;
 
+use ArrayObject;
 use Cake\ORM\Behavior;
 use Cake\Event\Event;
+use Cake\Network\Request;
 
 class UserBehavior extends Behavior {
 	public function initialize(array $config) {
