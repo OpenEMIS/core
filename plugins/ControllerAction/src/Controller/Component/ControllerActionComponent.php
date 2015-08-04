5014334c5bf711fec34046453a157488a60cd4b5
Jeff Zheng <jzheng@kordit.com>
2015-08-04 11:32:02 +0800

    added version number to CA

--- plugins/ControllerAction/src/Controller/Component/ControllerActionComponent.php
+++ plugins/ControllerAction/src/Controller/Component/ControllerActionComponent.php
@@ -12,6 +12,8 @@ that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warra
 or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details. You should 
 have received a copy of the GNU General Public License along with this program.  If not, see 
 <http://www.gnu.org/licenses/>.  For more information please wire to contact@openemis.org.
+
+ControllerActionComponent - Version 3.0.1
 */
 
 namespace ControllerAction\Controller\Component;
