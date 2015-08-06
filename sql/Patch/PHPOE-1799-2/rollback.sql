7880fd8d5b4500280304175a8a367b302740c98c
Hanafi Ahmat <hahmat@kordit.com>
2015-08-06 17:13:04 +0800

    PHPOE-1780 in PHPOE1799-2:	Changed security_user_id to student_id in institution_site_class_students table.

--- sql/Patch/PHPOE-1799-2/rollback.sql
+++ sql/Patch/PHPOE-1799-2/rollback.sql
@@ -4,3 +4,6 @@
 
 ALTER TABLE `institution_site_section_students` ADD `student_category_id` INT(11) NOT NULL AFTER `education_grade_id`;
 ALTER TABLE `institution_site_section_students` CHANGE `student_id` `security_user_id` INT(11) NOT NULL;
+
+-- Latest
+ALTER TABLE `institution_site_class_students` CHANGE `student_id` `security_user_id` INT(11) NOT NULL;
