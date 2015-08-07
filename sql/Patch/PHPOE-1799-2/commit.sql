71e0e1c8a2370bc43757e264165d5def6ebedc86
Jeff Zheng <jeff.zhengrj@gmail.com>
2015-08-07 19:01:29 +0800

    PHPOE-1799 - added promotion permissions

--- sql/Patch/PHPOE-1799-2/commit.sql
+++ sql/Patch/PHPOE-1799-2/commit.sql
@@ -10,3 +10,7 @@ ALTER TABLE `institution_site_class_students` CHANGE `security_user_id` `student
 
 ALTER TABLE `institution_students` CHANGE `end_date` `end_date` DATE NOT NULL ;
 ALTER TABLE `institution_students` CHANGE `end_year` `end_year` INT( 4 ) NOT NULL ;
+
+INSERT INTO `security_functions`
+(`id`, `name`, `controller`, `module`, `category`, `parent_id`, `_view`, `_edit`, `_add`, `_delete`, `_execute`, `order`, `visible`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES
+(1026, 'Grades', 'Institutions', 'Institutions', 'Details', 1000, 'Grades.index', NULL, NULL, NULL, 'Grades.indexEdit', 1026, 1, NULL, NULL, 1, '0000-00-00 00:00:00');
