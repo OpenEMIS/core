586f6e875f5bcc954b5b8831a66560467c211381
Thed Kuok Shiun <kuokshiun@hotmail.com>
2015-08-12 18:03:29 +0800

    PHPOE-1846 - Update Assessment.

--- plugins/Assessment/src/Model/Table/AssessmentStatusesTable.php
+++ plugins/Assessment/src/Model/Table/AssessmentStatusesTable.php
@@ -30,13 +30,24 @@ class AssessmentStatusesTable extends AppTable {
 	}
 
 	public function beforeAction(Event $event) {
-		$this->ControllerAction->field('assessment_id', ['type' => 'select']);
+		$this->ControllerAction->field('assessment_id');
 		$this->ControllerAction->field('academic_period_level');
 		$this->ControllerAction->field('academic_periods');
 
 		$this->ControllerAction->setFieldOrder([
 			'assessment_id', 'date_enabled', 'date_disabled', 'academic_period_level', 'academic_periods'
 		]);
+	}
+
+	public function onUpdateFieldAssessmentId(Event $event, array $attr, $action, Request $request) {
+		$assessmentOptions = $this->Assessments
+			->find('list', ['keyField' => 'id', 'valueField' => 'code_name'])
+			->find('visible')
+			->find('order')
+			->toArray();
+		$attr['options'] = $assessmentOptions;
+
+		return $attr;
 	}
 
 	public function onUpdateFieldAcademicPeriodLevel(Event $event, array $attr, $action, Request $request) {
