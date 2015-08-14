78218b848159d4572d4269045eee58759b2288de
Thed Kuok Shiun <kuokshiun@hotmail.com>
2015-08-14 14:20:56 +0800

    PHPOE-1857 - Add create db_patches if not exists in sql.

--- sql/Patch/PHPOE-1857/commit.sql
+++ sql/Patch/PHPOE-1857/commit.sql
@@ -1,4 +1,9 @@
 -- db_patches
+CREATE TABLE IF NOT EXISTS `db_patches` (
+  `issue` varchar(15) NOT NULL,
+  PRIMARY KEY (`issue`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 INSERT INTO `db_patches` VALUES ('PHPOE-1857');
 
 -- labels
