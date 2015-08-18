36e083bb6c27d970c4c58857a29d4430b6c7df6d
Chiang Hao Long <hchiang@kordit.com>
2015-08-18 18:54:12 +0800

    PHPOE-1892 - Created patch file for the altering of the table and insert of 2 additional student statuses (Pending admission and rejected)

---
+++ sql/Patch/PHPOE-1892/commit.sql
@@ -0,0 +1,9 @@
+INSERT INTO `student_statuses` (`code`, `name`)
+VALUES ('PENDING_ADMISSION', 'Pending Admission');
+
+INSERT INTO `student_statuses` (`code`, `name`)
+VALUES ('REJECTED', 'Rejected');
+
+ALTER TABLE `institution_student_transfers` 
+ADD COLUMN `type` VARCHAR(100) NULL AFTER `comment`, 
+RENAME TO  `institution_student_admission` ;
