62d2b4efdd9903d546c40b33a19806157314dc8e
Chiang Hao Long <hchiang@kordit.com>
2015-08-21 18:04:06 +0800

    PHPOE-1329 - Added the deleteStrategy to EducationController for System, Level, Cycles, Programmes and Grades

--- plugins/Education/src/Model/Table/EducationSystemsTable.php
+++ plugins/Education/src/Model/Table/EducationSystemsTable.php
@@ -1,11 +1,23 @@
 <?php
 namespace Education\Model\Table;
 
+use ArrayObject;
+
+use Cake\ORM\Query;
+use Cake\ORM\Entity;
+use Cake\Event\Event;
+
 use App\Model\Table\AppTable;
 
 class EducationSystemsTable extends AppTable {
 	public function initialize(array $config) {
 		parent::initialize($config);
 		$this->hasMany('EducationLevels', ['className' => 'Education.EducationLevels', 'dependent' => true, 'cascadeCallbacks' => true]);
+	}
+
+	public function deleteOnInitialize(Event $event, Entity $entity, Query $query, ArrayObject $options) {
+		
+		// $institutionId = $this->Session->read('Institutions.id');
+		// $query->where([$this->aliasField('institution_site_id') => $institutionId]);
 	}
 }
