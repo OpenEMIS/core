08d69ae97d6bd31d667d9e5a71a5c6ddaa699230
Chiang Hao Long <hchiang@kordit.com>
2015-08-24 15:50:40 +0800

    PHPOE-1892 - Updated patch files. Change security_user_id to student_id, change type to int (1 for transfer, 2 for admission)

--- sql/Patch/PHPOE-1892/commit.sql
+++ sql/Patch/PHPOE-1892/commit.sql
@@ -7,7 +7,9 @@ INSERT INTO `student_statuses` (`code`, `name`)
 VALUES ('REJECTED', 'Rejected');
 
 ALTER TABLE `institution_student_transfers` 
-ADD COLUMN `type` VARCHAR(100) NULL AFTER `comment`, 
-RENAME TO  `institution_student_admission` ;
+CHANGE COLUMN `security_user_id` `student_id` INT(11) NOT NULL COMMENT '' ,
+ADD COLUMN `type` INT(1) NOT NULL DEFAULT 1 COMMENT '1 -> Transfers, 2 -> Admission' AFTER `comment`, RENAME TO  `institution_student_admission` ;
 
-INSERT INTO `labels` (`module`, `field`, `en`, `created_user_id`, `created`) VALUES ('StudentAdmission', 'security_user_id', 'Student', '1', NOW());
+
+INSERT INTO `labels` (`module`, `field`, `en`, `created_user_id`, `created`) VALUES ('StudentAdmission', 'student_id', 'Student', '1', NOW());
+INSERT INTO `labels` (`module`, `field`, `en`, `created_user_id`, `created`) VALUES ('StudentAdmission', 'created', 'Date of Application', '1', NOW());
