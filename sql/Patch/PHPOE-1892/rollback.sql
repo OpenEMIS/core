2648fcdcd212492206a23b149eaa339e90f9a544
Chiang Hao Long <hchiang@kordit.com>
2015-08-24 15:50:40 +0800

    PHPOE-1892 - Updated patch files. Change security_user_id to student_id, change type to int (1 for transfer, 2 for admission)

--- sql/Patch/PHPOE-1892/rollback.sql
+++ sql/Patch/PHPOE-1892/rollback.sql
@@ -5,9 +5,10 @@ DELETE FROM `student_statuses`
 WHERE `code`='REJECTED';
 
 ALTER TABLE `institution_student_admission` 
-DROP COLUMN `type`, 
-RENAME TO  `institution_student_transfers` ;
+DROP COLUMN `type`,
+CHANGE COLUMN `student_id` `security_user_id` INT(11) NOT NULL COMMENT '' , RENAME TO  `institution_student_transfers` ;
 
-DELETE FROM `labels` WHERE `module`='StudentAdmission' and`field`='security_user_id';
+DELETE FROM `labels` WHERE `module`='StudentAdmission' and `field`='student_id';
+DELETE FROM `labels` WHERE `module`='StudentAdmission' and `field`='created';
 
 DELETE FROM `db_patches` WHERE `issue` = 'PHPOE-1892';
