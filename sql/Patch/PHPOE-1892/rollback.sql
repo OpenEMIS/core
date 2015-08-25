a6906510dbae66ce28b6157302b242b9ab21b8cd
Chiang Hao Long <hchiang@kordit.com>
2015-08-25 18:08:50 +0800

    PHPOE-1892 - Change patch file logic

--- sql/Patch/PHPOE-1892/rollback.sql
+++ sql/Patch/PHPOE-1892/rollback.sql
@@ -9,7 +9,7 @@ DROP COLUMN `type`,
 CHANGE COLUMN `student_id` `security_user_id` INT(11) NOT NULL COMMENT '' , RENAME TO  `institution_student_transfers` ;
 
 UPDATE `security_functions` SET `controller`='Dashboard', `_view`=null, `_execute`='TransferApprovals.edit' WHERE `name`='Transfer Approval';
-DELETE FROM `security_functions` WHERE `name`='Student Admission';
+DELETE FROM `security_functions` WHERE `id`=1028;
 
 DELETE FROM `labels` WHERE `module`='StudentAdmission' and `field`='created';
 DELETE FROM `labels` WHERE `module`='TransferApprovals' and `field`='created';
