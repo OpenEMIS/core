da1012fbd35a919cccf1d276b81de794c23be48c
Thed Kuok Shiun <kuokshiun@hotmail.com>
2015-08-26 15:38:30 +0800

    PHPOE-1573 - 1) Update Institution - Rubrics - Status to -1 = expired instead of delete and reinsert. 2) Modify the Security - Permissions to have view, edit & delete under Rubrics - New and view & delete under Rubrics - Completed.

--- sql/Patch/PHPOE-1573/commit.sql
+++ sql/Patch/PHPOE-1573/commit.sql
@@ -10,5 +10,13 @@ INSERT INTO `db_patches` VALUES ('PHPOE-1573');
 INSERT INTO `labels` (`module`, `field`, `en`, `created_user_id`, `created`) VALUES ('InstitutionRubrics', 'institution_site_section_id', 'Class', 1, NOW());
 INSERT INTO `labels` (`module`, `field`, `en`, `created_user_id`, `created`) VALUES ('InstitutionRubrics', 'institution_site_class_id', 'Subject', 1, NOW());
 
+-- institution_site_quality_rubrics
+ALTER TABLE `institution_site_quality_rubrics` CHANGE `status` `status` INT(1) NOT NULL DEFAULT '0' COMMENT '-1 -> Expired, 0 -> New, 1 -> Draft, 2 -> Completed';
+
 -- institution_site_quality_rubric_answers
 ALTER TABLE `institution_site_quality_rubric_answers` CHANGE `rubric_criteria_option_id` `rubric_criteria_option_id` INT(11) NULL DEFAULT NULL;
+
+-- security_functions
+UPDATE `security_functions` SET `name` = 'New', `category` = 'Rubrics', `_view` = 'Rubrics.index|Rubrics.view|NewRubrics.index|NewRubrics.view', `_edit` = 'Rubrics.edit|NewRubrics.edit|RubricAnswers.edit', `_add` = NULL, `_delete` = 'Rubrics.remove|NewRubrics.remove', `_execute` = NULL WHERE `id` = 1026;
+INSERT INTO `security_functions` (`id`, `name`, `controller`, `module`, `category`, `parent_id`, `_view`, `_edit`, `_add`, `_delete`, `_execute`, `order`, `visible`, `created_user_id`, `created`) VALUES
+(1029, 'Completed', 'Institutions', 'Institutions', 'Rubrics', 1000, 'Rubrics.index|Rubrics.view|CompletedRubrics.index|CompletedRubrics.view|RubricAnswers.edit', NULL, NULL, 'Rubrics.remove|CompletedRubrics.remove', NULL, 1029, 1, 1, NOW());
