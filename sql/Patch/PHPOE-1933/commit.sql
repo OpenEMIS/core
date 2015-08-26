860f5a5e37edb048c519700343b3b9b662a94c61
Jeff Zheng <jzheng@kordit.com>
2015-08-26 08:51:17 +0800

    PHPOE-1933 - removed create db_patches

--- sql/Patch/PHPOE-1933/commit.sql
+++ sql/Patch/PHPOE-1933/commit.sql
@@ -1,9 +1,4 @@
 -- db_patches
-CREATE TABLE IF NOT EXISTS `db_patches` (
-  `issue` varchar(15) NOT NULL,
-  PRIMARY KEY (`issue`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 INSERT INTO `db_patches` VALUES ('PHPOE-1933');
 
 -- security_functions
