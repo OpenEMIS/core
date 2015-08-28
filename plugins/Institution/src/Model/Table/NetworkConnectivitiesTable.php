2e03bd18b7c2ef38a065a0fa670cc7dc7ea69318
Malcolm Lee <mlee@kordit.com>
2015-08-28 14:47:52 +0800

    PHPOE-1961 'Add Network Connectivity field to Institution module'

---
+++ plugins/Institution/src/Model/Table/NetworkConnectivitiesTable.php
@@ -0,0 +1,14 @@
+<?php
+namespace Institution\Model\Table;
+
+use App\Model\Table\AppTable;
+use Cake\Validation\Validator;
+
+class NetworkConnectivitiesTable extends AppTable {
+	public function initialize(array $config) {
+        $this->addBehavior('ControllerAction.FieldOption');
+        parent::initialize($config);
+		
+		$this->hasMany('Institutions', ['className' => 'Institution.Institutions', 'dependent' => true, 'cascadeCallbacks' => true]);
+	}
+}
