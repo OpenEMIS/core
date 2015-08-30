5e7674c5076e3006532009af48b331c2748ac22e
Thed Kuok Shiun <kthed@kordit.com>
2015-08-30 14:06:57 +0800

    PHPOE-1391 - Moving the logic from WorkflowComponent to WorkflowBehavior.

---
+++ plugins/Workflow/src/Controller/Component/WorkflowComponent.php
@@ -0,0 +1,50 @@
+<?php
+namespace Workflow\Controller\Component;
+
+use Cake\ORM\TableRegistry;
+use Cake\Controller\Component;
+use Cake\Event\Event;
+use Cake\Utility\Inflector;
+use Cake\Log\LogTrait;
+
+class WorkflowComponent extends Component {
+    use LogTrait;
+    
+	private $controller;
+	private $action;
+	private $model = null;
+	private $currentAction;
+
+	public $components = ['Auth', 'ControllerAction'];
+
+	public function initialize(array $config) {
+		$this->controller = $this->_registry->getController();
+		$this->action = $this->request->params['action'];
+	}
+
+	public function startup(Event $event) {
+		$controller = $event->subject();
+		$this->model = $this->ControllerAction->model();
+		$this->currentAction = $this->ControllerAction->action();
+
+		if ($this->currentAction == 'view' && $this->model->alias() != 'Attachments') {
+			$alias = $this->model->alias();
+			$registryAlias = $this->model->registryAlias();
+
+			$WorkflowModels = TableRegistry::get('Workflow.WorkflowModels');
+			$setup = $WorkflowModels
+				->find()
+				->where([
+					$WorkflowModels->aliasField('model') => $registryAlias
+				])
+				->first();
+
+			// Trigger WorkflowBehavior if Workflow is applicable for this model.
+			if (!empty($setup)) {
+				$this->model->addBehavior('Workflow.Workflow', [
+					'setup' => $setup
+				]);
+			}
+		}
+	}
+}
