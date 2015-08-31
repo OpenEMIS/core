427edb18594a99736dfe5a32d5c91c21e2268869
Thed Kuok Shiun <kuokshiun@hotmail.com>
2015-08-31 13:19:37 +0800

    PHPOE-1391 - Working on workflow.

--- plugins/Workflow/src/Model/Behavior/WorkflowBehavior.php
+++ plugins/Workflow/src/Model/Behavior/WorkflowBehavior.php
@@ -24,8 +24,8 @@ class WorkflowBehavior extends Behavior {
     ];
 
     private $modelReference;
-    private $workflowId;
-    private $workflowModelId;
+    private $workflow;
+    private $workflowRecord;
 
 	public function initialize(array $config) {
         parent::initialize($config);
@@ -43,18 +43,25 @@ class WorkflowBehavior extends Behavior {
 	public function implementedEvents() {
     	$events = parent::implementedEvents();
     	// priority has to be set at 100 so that onUpdateToolbarButtons in model will be triggered first
-    	$events['Model.custom.onUpdateToolbarButtons'] = ['callable' => 'onUpdateToolbarButtons', 'priority' => 100];
+        $events['ControllerAction.Model.view.beforeAction'] = ['callable' => 'viewBeforeAction', 'priority' => 100];
     	$events['ControllerAction.Model.view.afterAction'] = ['callable' => 'viewAfterAction', 'priority' => 101];
+        $events['Model.custom.onUpdateToolbarButtons'] = ['callable' => 'onUpdateToolbarButtons', 'priority' => 102];
     	return $events;
     }
 
-    public function onUpdateToolbarButtons(Event $event, ArrayObject $buttons, ArrayObject $toolbarButtons, array $attr, $action, $isFromModel) {
+    public function viewBeforeAction(Event $event) {
+        $request = $this->_table->controller->request;
+        if ($request->is(['post', 'put'])) {
+            $requestData = $request->data;
+            $submit = isset($request->data['submit']) ? $request->data['submit'] : 'save';
+        }
     }
 
     public function viewAfterAction(Event $event, Entity $entity) {
         $setup = $this->config('setup');
 
         if (!is_null($setup)) {
+            // Find all Workflow setup for the model
             $workflowIds = $this->Workflows
                 ->find('list', ['keyField' => 'id', 'valueField' => 'id'])
                 ->where([
@@ -78,6 +85,7 @@ class WorkflowBehavior extends Behavior {
                     ])
                     ->all();
 
+                // Use Workflow with filter if found otherwise use Workflow that Apply To All
                 if ($workflowFilterResults->isEmpty()) {
                     $workflowResults = $this->WorkflowsFilters
                         ->find()
@@ -92,7 +100,8 @@ class WorkflowBehavior extends Behavior {
 
                 if (!$workflowResults->isEmpty()) {
                     $workflowId = $workflowResults->first()->workflow_id;
-                    $workflow = $this->Workflows
+                    $this->modelReference = $entity->id;
+                    $this->workflow = $this->Workflows
                         ->find()
                         ->contain([
                             'WorkflowSteps.WorkflowActions'
@@ -102,12 +111,10 @@ class WorkflowBehavior extends Behavior {
                         ])
                         ->first();
 
-                    $this->modelReference = $entity->id;
-                    $this->workflowId = $workflow->id;
-                    $this->workflowModelId = $workflow->workflow_model_id;
+                    $this->workflowRecord = $this->getRecord();
 
                     // Workflow Status - extra field
-                    $status = __('Open');
+                    $status = isset($this->workflowRecord->workflow_step->name) ? $this->workflowRecord->workflow_step->name : __('Open');
                     $this->_table->ControllerAction->field('workflow_status', [
                         'type' => 'element',
                         'element' => 'Workflow.status',
@@ -153,5 +160,176 @@ class WorkflowBehavior extends Behavior {
                 }
             }
         }
+    }
+
+    public function onUpdateToolbarButtons(Event $event, ArrayObject $buttons, ArrayObject $toolbarButtons, array $attr, $action, $isFromModel) {
+        if ($toolbarButtons->offsetExists('edit')) {
+            unset($toolbarButtons['edit']);
+        }
+
+        $workflowStepId = $this->workflowRecord->workflow_step->id;
+        $workflowStep = $this->WorkflowSteps
+            ->find()
+            ->contain(['WorkflowActions' => function ($q) {
+                    return $q
+                        ->find('visible');
+                }
+            ])
+            ->where([
+                $this->WorkflowSteps->aliasField('id') => $workflowStepId
+            ])
+            ->first();
+
+        $actionButtons = [];
+        foreach ($workflowStep->workflow_actions as $actionKey => $actionObj) {
+            $action = $actionObj->action;
+            $button = [
+                'id' => $actionObj->id,
+                'text' => $actionObj->name,
+                'value' => $actionObj->next_workflow_step_id,
+                'comment_required' => $actionObj->comment_required,
+                // 'action' => $actionObj->action
+            ];
+            $buttonAttr = [
+                // ' . json_encode($button) . '
+                'onclick' => 'Workflow.init();Workflow.copy();return false;',
+                'data-toggle' => 'modal',
+                'data-target' => '#transitionComment'
+            ];
+            $buttonAttr = array_merge($attr, $buttonAttr);
+
+            if (is_null($action)) {
+                $actionButtons[] = $button;
+                // Temporary
+                // $actionButton['type'] = 'button';
+                // $actionButton['label'] = $actionObj->name;
+                // $actionButton['url'] = '#';
+                // $actionButton['attr'] = $buttonAttr;
+                // $actionButton['attr']['title'] = __($actionObj->name);
+
+                // $toolbarButtons[$actionKey] = $actionButton;
+                // End
+            } else {
+                if ($action == 0) { // Approve
+                    $approveButton = [];
+                    $approveButton['type'] = 'button';
+                    $approveButton['label'] = '<i class="fa kd-approve"></i>';
+                    $approveButton['url'] = '#';
+                    $approveButton['attr'] = $buttonAttr;
+                    $approveButton['attr']['title'] = __($actionObj->name);
+
+                    $toolbarButtons['approve'] = $approveButton;
+                } else if ($action == 1) { // Reject
+                    $rejectButton = [];
+                    $rejectButton['type'] = 'button';
+                    $rejectButton['label'] = '<i class="fa kd-reject"></i>';
+                    $rejectButton['url'] = '#';
+                    $rejectButton['attr'] = $buttonAttr;
+                    $rejectButton['attr']['title'] = __($actionObj->name);
+
+                    $toolbarButtons['reject'] = $rejectButton;
+                }
+            }
+        }
+
+        // More Actions
+        if (!empty($actionButtons)) {
+            $moreButton = [];
+            $moreButton['type'] = 'element';
+            $moreButton['element'] = 'Workflow.buttons';
+            $moreButton['data'] = [
+                'buttons' => $actionButtons
+            ];
+            $moreButton['options'] = [];
+
+            // $toolbarButtons['more'] = $moreButton;
+        }
+        // End
+
+        // Modal
+        $alias = $this->WorkflowTransitions->alias();
+        $fields = [
+            $alias.'.prev_workflow_step_id' => [
+                'type' => 'hidden',
+                'value' => ''
+            ],
+            $alias.'.workflow_step_id' => [
+                'type' => 'hidden',
+                'value' => ''
+            ],
+            $alias.'.workflow_action_id' => [
+                'type' => 'hidden',
+                'value' => ''
+            ],
+            $alias.'.workflow_record_id' => [
+                'type' => 'hidden',
+                'value' => ''
+            ],
+            $alias.'.comment_required' => [
+                'type' => 'hidden',
+                'value' => '0'
+            ]
+        ];
+
+        $content = 'Action Name:<BR><BR>Comment:';
+
+        $modal = [
+            'id' => 'transitionComment',
+            'fields' => $fields,
+            'title' => __('Add Comment'),
+            'content' => $content,
+            'formOptions' => [
+                'type' => 'workflow',
+                'url' => $this->_table->ControllerAction->url('view')
+            ],
+            'buttons' => [
+                '<button type="submit" class="btn btn-default">' . __('Save') . '</button>'
+            ]
+        ];
+        $this->_table->controller->set('modal', $modal);
+        // End
+    }
+
+    public function getRecord() {
+        $modelReference = $this->modelReference;
+        $workflowId = $this->workflow->id;
+        $workflowModelId = $this->workflow->workflow_model_id;
+        $where = [
+            $this->WorkflowRecords->aliasField('model_reference') => $modelReference,
+            $this->WorkflowRecords->aliasField('workflow_model_id') => $workflowModelId
+        ];
+
+        $recordResults = $this->WorkflowRecords
+            ->find()
+            ->where($where)
+            ->all();
+
+        // Insert a new record if doesn't exists
+        if ($recordResults->isEmpty()) {
+            $workflowStep = $this->WorkflowSteps
+                ->find()
+                ->where([
+                    $this->WorkflowSteps->aliasField('workflow_id') => $workflowId,
+                    $this->WorkflowSteps->aliasField('stage') => 0  // Open
+                ])
+                ->first();
+
+            $data = [
+                'model_reference' => $modelReference,
+                'workflow_model_id' => $workflowModelId,
+                'workflow_step_id' => $workflowStep->id
+            ];
+            $entity = $this->WorkflowRecords->newEntity($data);
+            if ($this->WorkflowRecords->save($entity)) {
+            } else {
+                $this->log($entity->errors(), 'debug');
+            }
+        }
+
+        return $this->WorkflowRecords
+            ->find()
+            ->contain(['WorkflowModels', 'WorkflowSteps'])
+            ->where($where)
+            ->first();
     }
 }
--- plugins/Workflow/src/Template/Element/transitions.ctp
+++ plugins/Workflow/src/Template/Element/transitions.ctp
@@ -1,3 +1,6 @@
+<?php
+	echo $this->Html->script('Workflow.workflow', ['block' => true]);
+?>
 <?php if ($ControllerAction['action'] == 'view') : ?>
 	<?php
 		$tableHeaders = isset($attr['tableHeaders']) ? $attr['tableHeaders'] : [];
