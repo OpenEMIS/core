29545f084782ceb6697ad8de6865fe40afb53740
Malcolm Lee <mlee@kordit.com>
2015-09-01 10:06:41 +0800

    PHPOE-1961 Code refactor for network connectivity

--- sql/Patch/PHPOE-1961/rollback.sql
+++ sql/Patch/PHPOE-1961/rollback.sql
@@ -1,10 +1,10 @@
 SET @fieldOptionId := 0;
-SELECT id INTO @fieldOptionId FROM field_options WHERE code = 'InstitutionSiteNetworkConnectivities';
+SELECT id INTO @fieldOptionId FROM field_options WHERE code = 'NetworkConnectivities';
 
 DELETE FROM field_option_values WHERE field_option_id = @fieldOptionId;
 
-DELETE FROM field_options WHERE code = 'InstitutionSiteNetworkConnectivities';
+DELETE FROM field_options WHERE code = 'NetworkConnectivities';
 
-ALTER TABLE `institution_sites` DROP `institution_site_network_connectivity_id;
+ALTER TABLE `institution_sites` DROP `network_connectivity_id`;
 
-delete from labels where field = 'institution_site_network_connectivity_id';
+DELETE FROM labels WHERE field = 'network_connectivity_id';
