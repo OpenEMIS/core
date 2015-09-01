398936aca405671ef880f193f27dddc3b03dfa91
Shivapriya D/O Paramasivam <sparamasivam@kordit.com>
2015-09-01 09:38:34 +0800

    PHPOE-1904 - using model specified through params in field_options

--- plugins/FieldOption/src/Model/Table/CountriesTable.php
+++ plugins/FieldOption/src/Model/Table/CountriesTable.php
@@ -9,6 +9,6 @@ class CountriesTable extends AppTable {
 		$this->addBehavior('ControllerAction.FieldOption');
 		$this->table('countries');
 		parent::initialize($config);
-		$this->hasMany('Nationalities', ['className' => 'User.Nationalities', 'dependent' => true, 'cascadeCallbacks' => true]);
+		$this->hasMany('Nationalities', ['className' => 'User.Nationalities', 'foreignKey' => 'country_id']);
 	}
 }
