d4b0ef3f236a668ad8ff5b643debd7336df0ecd3
Chiang Hao Long <hchiang@kordit.com>
2015-09-03 14:28:10 +0800

    PHPOE-2019 - Fixed patch files.

--- sql/Patch/PHPOE-2019/rollback.sql
+++ sql/Patch/PHPOE-2019/rollback.sql
@@ -1,5 +1,9 @@
+-- dummy data for the student dropout reasons
+DELETE FROM `field_option_values`
+WHERE `field_option_values`.`id` = (SELECT `field_options`.`id` FROM `field_options` WHERE `code` = 'StudentDropoutReasons') AND `field_option_values`.`name`='Relocation';
+
 -- field_options
 DELETE FROM `field_options` WHERE `plugin`='Students' AND `code`='StudentDropoutReasons';
 
 -- db_patches
-DELETE FROM `db_patches` WHERE `issue` = 'PHPOE-1573';
+DELETE FROM `db_patches` WHERE `issue` = 'PHPOE-2019';
