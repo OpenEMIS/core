b26560a74b47ffb9736de41c94d54b4041e8029c
Shivapriya D/O Paramasivam <sparamasivam@kordit.com>
2015-09-03 13:48:31 +0800

    PHPOE-1902 - partially done

--- plugins/Education/src/Model/Table/EducationProgrammesTable.php
+++ plugins/Education/src/Model/Table/EducationProgrammesTable.php
@@ -6,6 +6,7 @@ use App\Model\Table\AppTable;
 use Cake\ORM\Query;
 use Cake\Network\Request;
 use Cake\Event\Event;
+use Cake\ORM\TableRegistry;
 
 class EducationProgrammesTable extends AppTable {
 	public function initialize(array $config) {
@@ -15,6 +16,13 @@ class EducationProgrammesTable extends AppTable {
 		$this->belongsTo('EducationFieldOfStudies', ['className' => 'Education.EducationFieldOfStudies']);
 		$this->hasMany('EducationGrades', ['className' => 'Education.EducationGrades', 'dependent' => true, 'cascadeCallbacks' => true]);
 		$this->hasMany('InstitutionSiteProgrammes', ['className' => 'Institution.InstitutionSiteProgrammes', 'dependent' => true, 'cascadeCallbacks' => true]);
+	
+
+	}
+
+	public function beforeAction(Event $event) {
+		$this->ControllerAction->field('next_programmes', ['type' => 'custom_next_programme', 'valueClass' => 'table-full-width']);
+		$this->ControllerAction->setFieldOrder('next_programmes');
 	}
 
 	public function indexBeforeAction(Event $event) {
@@ -70,4 +78,125 @@ class EducationProgrammesTable extends AppTable {
 
 		return compact('levelOptions', 'selectedLevel', 'cycleOptions', 'selectedCycle');
 	}
+
+	public function onGetCustomNextProgrammeElement(Event $event, $action, $entity, $attr, $options=[]) {
+		if ($action == 'index') {
+			// $EducationGradesSubjects = TableRegistry::get('EducationGradesSubjects');
+			// $value = $EducationGradesSubjects
+			// 	->findByEducationGradeId($entity->id)
+			// 	->where([$EducationGradesSubjects->aliasField('visible') => 1])
+			// 	->count();
+			// $attr['value'] = $value;
+		} else if ($action == 'view') {
+			// $tableHeaders = [__('Cycle'), __('Next Programme')];
+			// $tableCells = [];
+
+			// $educationSubjects = $entity->extractOriginal(['education_subjects']);
+			// foreach ($educationSubjects['education_subjects'] as $key => $obj) {
+			// 	if ($obj->_joinData->visible == 1) {
+			// 		$rowData = [];
+			// 		$rowData[] = $obj->name;
+			// 		$rowData[] = $obj->code;
+			// 		$rowData[] = $obj->_joinData->hours_required;
+			// 		$tableCells[] = $rowData;
+			// 	}
+			// }
+
+			// $attr['tableHeaders'] = $tableHeaders;
+	  //   	$attr['tableCells'] = $tableCells;
+		} else if ($action == 'edit') {
+			if (isset($entity->id)) {
+				
+				$nextProgrammeOptions = [];
+
+				$currentProgrammSystem = $this->findById($entity->id)->contain(['EducationCycles.EducationLevels.EducationSystems'])->first();
+				$systemId = $currentProgrammSystem->education_cycle->education_level->education_system->id;
+				$currentCycleOrder = $currentProgrammSystem->education_cycle->order;
+				$currentLevelOrder = $currentProgrammSystem->education_cycle->education_level->order;
+
+				$systems = TableRegistry::get('Education.EducationSystems')
+							->findById($systemId)
+							->contain(['EducationLevels.EducationCycles.EducationProgrammes']);
+
+				foreach($systems as $system) {
+					foreach($system->education_levels as $level){
+						if($level->order > $currentLevelOrder) {
+							foreach($level->education_cycles as $cycle){
+								if($cycle->order >= $currentCycleOrder) {
+									foreach($cycle->education_programmes as $programme) {
+										if($programme->id != $entity->id){
+											$nextProgrammeOptions[$programme->id] = $cycle->name.' - ('.$programme->name.')';
+										}
+									}
+								}
+							}
+						}
+					}
+				}			
+
+				$form = $event->subject()->Form;
+
+				$tableHeaders = [__('Cycle - (Programme)')];
+				$tableCells = [];
+				$cellCount = 0;
+
+				$arrayNextProgrammes = [];
+				if ($this->request->is(['get'])) {
+
+					$nextProgrammeslist = TableRegistry::get('Education.EducationProgrammeNext')->findByEducationProgrammeId($entity->id);
+					foreach($nextProgrammeslist as $nextProgramme){
+						$arrayNextProgrammes[] = [
+							'id' => $nextProgramme->id,
+							'next_programme' => $nextProgramme->NextEducationProgrammes->name
+						];
+					}
+				} else if ($this->request->is(['post', 'put'])) {
+					// $requestData = $this->request->data;
+					// if (array_key_exists('education_subjects', $requestData[$this->alias()])) {
+					// 	foreach ($requestData[$this->alias()]['education_subjects'] as $key => $obj) {
+					// 		$arraySubjects[] = $obj['_joinData'];
+					// 	}
+					// }
+
+					// if (array_key_exists('education_subject_id', $requestData[$this->alias()])) {
+					// 	$subjectId = $requestData[$this->alias()]['education_subject_id'];
+					// 	$subjectObj = $this->EducationSubjects
+					// 		->findById($subjectId)
+					// 		->first();
+
+					// 	$arraySubjects[] = [
+					// 		'name' => $subjectObj->name,
+					// 		'code' => $subjectObj->code,
+					// 		'hours_required' => 0,
+					// 		'education_grade_id' => $entity->id,
+					// 		'education_subject_id' => $subjectObj->id,
+					// 		'visible' => 1
+					// 	];
+					// }
+				}
+
+				foreach ($arrayNextProgrammes as $key => $obj) {
+					$nextProgrammeId = $key;
+					$nextProgrammeName = $obj;
+
+					$rowData = [];
+					$rowData[] = $nextProgrammeName;
+					$rowData[] = '<button onclick="jsTable.doRemove(this)" aria-expanded="true" type="button" class="btn btn-dropdown action-toggle btn-single-action"><i class="fa fa-trash"></i>&nbsp;<span>'.__('Delete').'</span></button>';
+
+					$tableCells[] = $rowData;
+					//unset($nextProgrammeOptions[$obj['education_subject_id']]);
+				}
+
+				$attr['tableHeaders'] = $tableHeaders;
+	    		$attr['tableCells'] = $tableCells;
+
+	    		$nextProgrammeOptions[0] = "-- ".__('Add Next Programme') ." --";
+	    		ksort($nextProgrammeOptions);
+	    		$attr['options'] = $nextProgrammeOptions;
+			}
+		}
+
+		return $event->subject()->renderElement('Education.next_programmes', ['attr' => $attr]);
+	}
+
 }
