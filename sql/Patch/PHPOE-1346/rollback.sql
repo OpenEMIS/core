9de3189c3f35ab4d1ae373e10f5e4c2ea17c289d
Thed Kuok Shiun <kuokshiun@hotmail.com>
2015-09-07 08:52:41 +0800

    PHPOE-1346 - Update sql, added Student List field type.

--- sql/Patch/PHPOE-1346/rollback.sql
+++ sql/Patch/PHPOE-1346/rollback.sql
@@ -1,7 +1,10 @@
--- New tables
+-- Drop tables
 DROP TABLE IF EXISTS `institution_student_surveys`;
 DROP TABLE IF EXISTS `institution_student_survey_answers`;
 DROP TABLE IF EXISTS `institution_student_survey_table_cells`;
+
+-- Drop Student List
+DELETE FROM `custom_field_types` WHERE `code` = 'STUDENT_LIST';
 
 -- db_patches
 DELETE FROM `db_patches` WHERE `issue` = 'PHPOE-1346';
