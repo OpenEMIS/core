7b1b723ee59b6befec182f30c4d417a877bfae02
Shivapriya D/O Paramasivam <sparamasivam@kordit.com>
2015-09-07 15:28:53 +0800

    PHPOE-1902 - completed

--- plugins/Education/src/Model/Table/EducationProgrammesNextProgrammesTable.php
+++ plugins/Education/src/Model/Table/EducationProgrammesNextProgrammesTable.php
@@ -9,7 +9,7 @@ use Cake\Event\Event;
 class EducationProgrammesNextProgrammesTable extends AppTable {
 	public function initialize(array $config) {
 		parent::initialize($config);
-		$this->belongsTo('EducationProgrammes', ['className' => 'Education.EducationProgrammes']);
-		$this->belongsTo('EducationnNextProgrammes', ['className' => 'Education.EducationnNextProgrammes']);
+		$this->belongsTo('EducationProgrammes', ['className' => 'Education.EducationProgrammes', 'foreignKey' => 'education_programme_id']);
+		$this->belongsTo('EducationnNextProgrammes', ['className' => 'Education.EducationnNextProgrammes', 'foreignKey' => 'next_programme_id']);
 	}
 }
