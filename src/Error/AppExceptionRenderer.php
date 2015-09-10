f6b65491d5bf0034524fea4f56b36633ba61b930
Hanafi Ahmat <hahmat@kordit.com>
2015-09-10 16:24:44 +0800

    PHPOE-2028	- Revert changes to AppExceptionHandler.php

--- src/Error/AppExceptionRenderer.php
+++ src/Error/AppExceptionRenderer.php
@@ -7,14 +7,14 @@ use Cake\Error\ExceptionRenderer;
 class AppExceptionRenderer extends ExceptionRenderer
 {
 
-    // public function forbidden($error){
-    // 	return $this->controller->redirect(['controller' => 'Errors', 'action' => 'error403', 'plugin'=>'Error']);
-    // }
+    public function forbidden($error){
+    	return $this->controller->redirect(['controller' => 'Errors', 'action' => 'error403', 'plugin'=>'Error']);
+    }
 
-    // public function _template(\Exception $exception, $method, $code){
-    // 	if ($code!=403) {
-    // 		return $this->controller->redirect(['controller' => 'Errors', 'action' => 'error404', 'plugin'=>'Error']);
-    // 	}
-    // 	return false;
-    // }
+    public function _template(\Exception $exception, $method, $code){
+    	if ($code!=403) {
+    		return $this->controller->redirect(['controller' => 'Errors', 'action' => 'error404', 'plugin'=>'Error']);
+    	}
+    	return false;
+    }
 }
