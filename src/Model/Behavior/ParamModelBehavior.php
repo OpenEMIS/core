2e2a61c7c99b2688dfa1754894ee069dceb20530
Shivapriya D/O Paramasivam <sparamasivam@kordit.com>
2015-09-10 12:33:59 +0800

    PHPOE-1904 - shifting methods to parammodel

--- src/Model/Behavior/ParamModelBehavior.php
+++ src/Model/Behavior/ParamModelBehavior.php
@@ -5,52 +5,102 @@ use ArrayObject;
 use Cake\Event\Event;
 use Cake\ORM\Entity;
 use Cake\ORM\Behavior;
+use Cake\ORM\TableRegistry;
 
 class ParamModelBehavior extends Behavior {
+	private $excludeFieldList = ['modified_user_id', 'modified', 'created_user_id', 'created'];
+	private $parentFieldOptionInfo;
+	private $parentFieldOptionList;
+	private $parentFieldOptions;
+
 	public function implementedEvents() {
 		$events = parent::implementedEvents();
 		$events['ControllerAction.Model.beforeAction'] = 'beforeAction';
+		$events['ControllerAction.Model.index.beforeAction'] = 'indexBeforeAction';
+		//$events['ControllerAction.Model.addEdit.beforeAction'] = 'addEditBeforeAction';
 		return $events;
 	}
 
-	// public function beforeSave(Event $event, Entity $entity, ArrayObject $options) {
-	// 	pr('beforeSave in behvaiour');
-	// 	// $config = $this->config();
-	// 	// foreach ($config as $date => $year) {
-	// 	// 	if ($entity->has($date) && !empty($entity->$date)) {
-	// 	// 		$entity->$year = date('Y', strtotime($entity->$date));
-	// 	// 	}
-	// 	// }
-	// }
+	public function initialize(array $config) {
+		$fieldOptionName = $config['fieldOptionName'];
+		$this->parentFieldOptionList = $config['parentFieldOptionList'];
 
-	public function beforeAction(Event $event) {
-		pr('hi there ladies'); die;
+		$parentFieldOptionInfo = $this->parentFieldOptionList[$fieldOptionName];
+		$this->parentFieldOptionInfo = $parentFieldOptionInfo;	
 
-		foreach($defaultFieldOrder as $each) {
-				$this->ControllerAction->field($each, ['visible' => false]);
-			}
+		if(!empty($parentFieldOptionInfo['parentModel']) && !empty($parentFieldOptionInfo['foreignKey'])) {
+			$parentFieldOptionTable = TableRegistry::get($parentFieldOptionInfo['parentModel']);
+			$parentFieldOptions = $parentFieldOptionTable->find('list')->where([$parentFieldOptionTable->aliasField('visible') => 1])->toArray();
+			$this->parentFieldOptions = $parentFieldOptions;
+		}
 
-			$table = TableRegistry::get($currentfieldOption->plugin.'.'.$currentfieldOption->code);
-			$columns = $table->schema()->columns();
-			$fieldOrder = 1000;
-			$fieldOrderExcluded = 5000;
-			foreach ($columns as $key => $attr) {
-				$this->fields[$attr]['model'] = $table->alias();
-				$defaultFieldOrder[] = $attr;
-				if(!in_array($attr, $this->excludeFieldList)) {
-					$this->ControllerAction->field($attr, ['visible' => true, 'order' => $fieldOrder]);
-					$fieldOrder++;
-				} else {
-					$this->ControllerAction->field($attr, ['visible' => ['index' => false, 'edit' => false, 'add' => false, 'view' => true], 'order' => $fieldOrderExcluded]);
-					$fieldOrderExcluded++;
-				}
-			}	
+		// 		// $foreignKeyLabel = Inflector::humanize($foreignKey);
+		// 		// if($this->endsWith($foreignKey, '_id')){
+		// 		// 	$foreignKeyLabel = str_replace(' Id', '', $foreignKeyLabel);
+		// 		// }
 
-			$defaultFieldOrder = ['parent_field_option_id']; 
-			
-		// $config = $this->config();
-		// foreach ($config as $date => $year) {
-		// 	$this->_table->fields[$year]['visible'] = false;
-		// }
-	}
+		// 		$this->ControllerAction->field($foreignKey, ['type' => 'readonly', 
+		// 														  'visible' => ['index' => false, 'add' => true, 'edit' => true, 'view' => true], 
+		// 														  'model' => $parentFieldOptionTable->alias(), 
+		// 														  'value' => $parentFieldOptionValue,
+		// 														  'className' => $parentFieldOptionInfo['parentModel'], 
+		// 														  'attr' => ['value' => $parentFieldOptionValue]]);
+
+
+
+			// $table = TableRegistry::get($fieldOptionName);
+			// $columns = $table->schema()->columns();
+			// $fieldOrder = 1000;
+			// $fieldOrderExcluded = 5000;
+			// foreach ($columns as $key => $attr) {
+			// 	// pr($key);
+			// 	// pr($attr);
+			// 	$this->fields[$attr]['model'] = $table->alias();
+			// 	$defaultFieldOrder[] = $attr;
+			// 	if(!in_array($attr, $this->excludeFieldList)) {
+			// 		$this->_table->ControllerAction->field($attr, ['visible' => true, 'order' => $fieldOrder]);
+			// 		$fieldOrder++;
+			// 	} else {
+			// 		$this->_table->ControllerAction->field($attr, ['visible' => ['index' => false, 'edit' => false, 'add' => false, 'view' => true], 'order' => $fieldOrderExcluded]);
+			// 		$fieldOrderExcluded++;
+			// 	}
+			// }	
+
+		// pr('done'); die;
+	}	
+
+	public function indexBeforeAction(Event $event, Query $query, ArrayObject $settings) {		
+		$this->_table->controller->set('parentFieldOptions', $this->parentFieldOptions);
+		$selectedParentFieldOption = $this->_table->queryString('parent_field_option_id', $this->parentFieldOptions);
+
+		$this->_table->controller->set('selectedParentFieldOption', $selectedParentFieldOption);
+		$this->_table->controller->set('foreignKey', $this->parentFieldOptionInfo['foreignKey']);
+		
+		$parentFieldOptionValue = $this->parentFieldOptions[$selectedParentFieldOption];
+		$foreignKey =  $this->parentFieldOptionInfo['foreignKey'];
+
+		$this->_table->ControllerAction->field('parent_field_option_id', [
+			'type' => 'readonly', 
+			'options' => $this->parentFieldOptions,
+			'visible' => ['index' => false, 'view' => false, 'edit' => false]
+		]);
+
+		
+	}	
+
+	public function indexBeforePaginate(Event $event, Request $request, Query $query, ArrayObject $options) {
+		pr('came');
+
+		$selectedParentFieldOption = $this->_table->ControllerAction->getVar('selectedParentFieldOption');
+		pr($selectedParentFieldOption);
+		$foreignKey = $this->_table->ControllerAction->getVar('foreignKey');
+		if (!empty($selectedParentFieldOption) && !empty($foreignKey)) {	
+			$query->where([$foreignKey => $selectedParentFieldOption]);
+		}	
+		return $query;
+	}	
+
+	// public function addEditBeforeAction(Event $event) {
+	// 	$this->_table->ControllerAction->field('testing', ['visible' => true, 'order' => 1]);
+	// }	
 }
