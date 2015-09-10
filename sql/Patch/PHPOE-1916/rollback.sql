70934df9b800fc33f07466f7510ee78d4c5794d4
Chiang Hao Long <hchiang@kordit.com>
2015-09-10 12:16:09 +0800

    PHPOE-1916 - Added index on editable, visible, parent_id, and current. Altered table to change column type to int(1) for editable and current.

--- sql/Patch/PHPOE-1916/rollback.sql
+++ sql/Patch/PHPOE-1916/rollback.sql
@@ -1,5 +1,10 @@
 ALTER TABLE `academic_periods` 
-CHANGE COLUMN `editable` `available` CHAR(1) NOT NULL DEFAULT '1';
+CHANGE COLUMN `editable` `available` CHAR(1) NOT NULL DEFAULT '1',
+CHANGE COLUMN `visible` `visible` CHAR(1) NOT NULL DEFAULT '1',
+DROP INDEX `parent_id` ,
+DROP INDEX `editable` ,
+DROP INDEX `visible` ,
+DROP INDEX `current` ;
 
 UPDATE `academic_periods`
 LEFT JOIN `z_1916_academic_periods` ON `academic_periods`.`id` = `z_1916_academic_periods`.`id`
