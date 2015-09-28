1586645cfe3e86f5fd00aa8f8111b2c2d5403b77
Chiang Hao Long <hchiang@kordit.com>
2015-09-28 12:17:54 +0800

    PHPOE-2117 - Updated patch file to remove the institution_site_programme_id from the institution_site_grades

--- sql/Patch/PHPOE-2117/commit.sql
+++ sql/Patch/PHPOE-2117/commit.sql
@@ -14,21 +14,6 @@ CREATE TABLE `z_2117_institution_site_grades` (
 INSERT INTO `z_2117_institution_site_grades` (`id`, `institution_site_programme_id`)
 SELECT `id`, `institution_site_programme_id` FROM `institution_site_grades`;
 
-
--- CREATE TABLE `institution_site_grades` (
---   `id` int(11) NOT NULL AUTO_INCREMENT,
---   `education_grade_id` int(11) NOT NULL,
---   `start_date` date NOT NULL,
---   `start_year` int(4) NOT NULL,
---   `end_date` date DEFAULT NULL,
---   `end_year` int(4) DEFAULT NULL,
---   `institution_site_programme_id` int(11) NOT NULL,
---   `institution_site_id` int(11) NOT NULL,
---   `modified_user_id` int(11) DEFAULT NULL,
---   `modified` datetime DEFAULT NULL,
---   `created_user_id` int(11) NOT NULL,
---   `created` datetime NOT NULL,
---   PRIMARY KEY (`id`),
---   KEY `institution_site_id` (`institution_site_id`),
---   KEY `institution_site_programme_id` (`institution_site_programme_id`)
--- ) ENGINE=InnoDB AUTO_INCREMENT=179 DEFAULT CHARSET=utf8
+ALTER TABLE `institution_site_grades` 
+DROP COLUMN `institution_site_programme_id`,
+DROP INDEX `institution_site_programme_id` ;
--- sql/Patch/PHPOE-2117/rollback.sql
+++ sql/Patch/PHPOE-2117/rollback.sql
@@ -2,5 +2,18 @@
 ALTER TABLE `z_2117_institution_site_programmes` 
 RENAME TO  `institution_site_programmes` ;
 
+-- institution_site_grades
+ALTER TABLE `institution_site_grades` 
+ADD COLUMN `institution_site_programme_id` INT(11) NOT NULL DEFAULT 0 AFTER `end_year`;
+
+ALTER TABLE `institution_site_grades` 
+CHANGE COLUMN `institution_site_programme_id` `institution_site_programme_id` INT(11) NOT NULL;
+
+UPDATE `institution_site_grades`
+LEFT JOIN `z_2117_institution_site_grades` ON `institution_site_grades`.`id` = `z_2117_institution_site_grades`.`id`
+  SET `institution_site_grades`.`institution_site_programme_id` = `z_2117_institution_site_grades`.`institution_site_programme_id`
+  WHERE `institution_site_grades`.`id` = `z_2117_institution_site_grades`.`id`;
+DROP TABLE `z_2117_institution_site_grades`;
+
 -- db_patches
 DELETE FROM `db_patches` WHERE `issue` = 'PHPOE-2117';
