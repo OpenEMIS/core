94a3d79f45aa8d5c538c9364f01c52c59d2fd26b
Chiang Hao Long <hchiang@kordit.com>
2015-09-30 17:24:15 +0800

    PHPOE-1329 - Shifted the logic of the onBeforeDelete in the education structure to the ControllerActionComponent. Shifted the logic to delete the educationNextProgramme for the EducationProgrammeTable to afterDelete.

--- plugins/Education/src/Model/Table/EducationSystemsTable.php
+++ plugins/Education/src/Model/Table/EducationSystemsTable.php
@@ -13,16 +13,5 @@ class EducationSystemsTable extends AppTable {
 	public function initialize(array $config) {
 		parent::initialize($config);
 		$this->hasMany('EducationLevels', ['className' => 'Education.EducationLevels']);
-		$this->addBehavior('ControllerAction.Delete');
-	}
-
-	public function onBeforeDelete(Event $event, ArrayObject $options, $id) {
-		if (empty($this->request->data['transfer_to'])) {
-			if ($this->associationCount($this, $id) > 0) {
-				$this->Alert->error('general.deleteTransfer.restrictDelete');
-				$event->stopPropagation();
-				return $this->controller->redirect($this->ControllerAction->url('remove'));
-			}
-		}
 	}
 }
