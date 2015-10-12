4faf57df606e9af452c92022d2e07b61aadcdd16
Chiang Hao Long <hchiang@kordit.com>
2015-10-12 17:18:42 +0800

    PHPOE-2144 - Added patch file to add index to the institution_custom_field_values table

---
+++ sql/Patch/PHPOE-2144/rollback.sql
@@ -0,0 +1,8 @@
+
+-- institution_custom_field_values
+ALTER TABLE `institution_custom_field_values` 
+DROP INDEX `institution_site_id` ,
+DROP INDEX `institution_custom_field_id` ;
+
+-- db_patches
+DELETE FROM `db_patches` WHERE `issue` = 'PHPOE-2144';
