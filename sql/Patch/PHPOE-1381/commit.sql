e1c4ca16691413c3daf3921cb9196cc457b533c4
Chiang Hao Long <hchiang@kordit.com>
2015-10-12 17:36:07 +0800

    PHPOE-1381 - Added patch file to add the index to institution_site_survey_answer table

---
+++ sql/Patch/PHPOE-1381/commit.sql
@@ -0,0 +1,7 @@
+-- db_patches
+INSERT INTO `db_patches` VALUES ('PHPOE-1381');
+
+-- institution_site_survey_answer
+ALTER TABLE `institution_site_survey_answers` 
+ADD INDEX `survey_question_id` (`survey_question_id` ASC)  COMMENT '',
+ADD INDEX `institution_site_survey_id` (`institution_site_survey_id` ASC)  COMMENT '';
