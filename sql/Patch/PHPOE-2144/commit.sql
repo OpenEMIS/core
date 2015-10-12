c08543192ed0335d6fc2d298b7d7f5c6975b58ce
Chiang Hao Long <hchiang@kordit.com>
2015-10-12 17:18:42 +0800

    PHPOE-2144 - Added patch file to add index to the institution_custom_field_values table

---
+++ sql/Patch/PHPOE-2144/commit.sql
@@ -0,0 +1,7 @@
+-- db_patches
+INSERT INTO `db_patches` VALUES ('PHPOE-2144');
+
+-- institution_custom_field_values
+ALTER TABLE `institution_custom_field_values` 
+ADD INDEX `institution_custom_field_id` (`institution_custom_field_id` ASC)  COMMENT '',
+ADD INDEX `institution_site_id` (`institution_site_id` ASC)  COMMENT '';
