e06ec2941dc89e5b0564599fd0d7fcf24e9857a0
Thed Kuok Shiun <kuokshiun@hotmail.com>
2015-10-14 15:40:04 +0800

    PHPOE-2078 - Update sql of PHPOE-1352.

--- sql/Patch/PHPOE-1352/commit.sql
+++ sql/Patch/PHPOE-1352/commit.sql
@@ -2,26 +2,5 @@
 INSERT INTO `db_patches` VALUES ('PHPOE-1352');
 
 -- workflow_models
-UPDATE `workflow_models` SET `name` = 'Staff > Career > Leave' WHERE `model` = 'Staff.Leaves';
 INSERT INTO `workflow_models` (`name`, `model`, `filter`, `created_user_id`, `created`) VALUES
 ('Institutions > Survey > Forms', 'Institution.InstitutionSurveys', NULL, 1, '0000-00-00 00:00:00');
-
--- workflow_steps
-ALTER TABLE `workflow_steps` ADD `is_editable` INT(1) NOT NULL DEFAULT '0' AFTER `stage`, ADD `is_removable` INT(1) NOT NULL DEFAULT '0' AFTER `is_editable`;
-
--- workflow_actions
-ALTER TABLE `workflow_actions` ADD `event_key` VARCHAR(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `next_workflow_step_id`;
-
--- workflow_transitions
-ALTER TABLE `workflow_transitions` CHANGE `comment` `comment` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
-
--- add INDEX
-ALTER TABLE `workflows` ADD INDEX(`workflow_model_id`);
-ALTER TABLE `workflow_steps` ADD INDEX(`workflow_id`);
-ALTER TABLE `workflow_actions` ADD INDEX(`next_workflow_step_id`), ADD INDEX(`workflow_step_id`);
-ALTER TABLE `workflow_records` ADD INDEX(`model_reference`), ADD INDEX(`workflow_model_id`), ADD INDEX(`workflow_step_id`);
-ALTER TABLE `workflow_transitions` ADD INDEX(`prev_workflow_step_id`), ADD INDEX(`workflow_step_id`), ADD INDEX(`workflow_action_id`), ADD INDEX(`workflow_record_id`);
-
--- labels
-INSERT INTO `labels` (`id`, `module`, `field`, `module_name`, `field_name`, `visible`, `created_user_id`, `created`) VALUES (uuid(), 'WorkflowSteps', 'is_editable', 'Workflow -> Steps', 'Editable', 1, 1, NOW());
-INSERT INTO `labels` (`id`, `module`, `field`, `module_name`, `field_name`, `visible`, `created_user_id`, `created`) VALUES (uuid(), 'WorkflowSteps', 'is_removable', 'Workflow -> Steps', 'Removable', 1, 1, NOW());
--- sql/Patch/PHPOE-1352/rollback.sql
+++ sql/Patch/PHPOE-1352/rollback.sql
@@ -1,26 +1,5 @@
 -- workflow_models
-UPDATE `workflow_models` SET `name` = 'Staff Leave' WHERE `model` = 'Staff.Leaves';
 DELETE FROM `workflow_models` WHERE `model` = 'Institution.InstitutionSurveys';
-
--- workflow_steps
-ALTER TABLE `workflow_steps` DROP `is_removable`, DROP `is_editable`;
-
--- workflow_actions
-ALTER TABLE `workflow_actions` DROP `event_key`;
-
--- workflow_transitions
-ALTER TABLE `workflow_transitions` CHANGE `comment` `comment` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
-
--- drop INDEX
-ALTER TABLE `workflows` DROP INDEX `workflow_model_id`;
-ALTER TABLE `workflow_steps` DROP INDEX `workflow_id`;
-ALTER TABLE `workflow_actions` DROP INDEX `next_workflow_step_id`, DROP INDEX `workflow_step_id`;
-ALTER TABLE `workflow_records` DROP INDEX `model_reference`, DROP INDEX `workflow_model_id`, DROP INDEX `workflow_step_id`;
-ALTER TABLE `workflow_transitions` DROP INDEX `prev_workflow_step_id`, DROP INDEX `workflow_step_id`, DROP INDEX `workflow_action_id`, DROP INDEX `workflow_record_id`;
-
--- labels
-DELETE FROM `labels` WHERE `module` = 'WorkflowSteps' AND `field` = 'is_editable';
-DELETE FROM `labels` WHERE `module` = 'WorkflowSteps' AND `field` = 'is_removable';
 
 -- db_patches
 DELETE FROM `db_patches` WHERE `issue` = 'PHPOE-1352';
