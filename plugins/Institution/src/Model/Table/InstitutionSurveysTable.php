64382c7534dedf8c9fd2b034c1e7071af7dab92e
Chiang Hao Long <hchiang@kordit.com>
2015-10-14 18:56:45 +0800

    PHPOE-1381 - Added export to the view page of the completed survey

--- plugins/Institution/src/Model/Table/InstitutionSurveysTable.php
+++ plugins/Institution/src/Model/Table/InstitutionSurveysTable.php
@@ -37,6 +37,7 @@ class InstitutionSurveysTable extends AppTable {
 			'fieldValueClass' => ['className' => 'Institution.InstitutionSurveyAnswers', 'foreignKey' => 'institution_site_survey_id', 'dependent' => true, 'cascadeCallbacks' => true],
 			'tableCellClass' => ['className' => 'Institution.InstitutionSurveyTableCells', 'foreignKey' => 'institution_site_survey_id', 'dependent' => true, 'cascadeCallbacks' => true]
 		]);
+		$this->addBehavior('Excel', ['pages' => ['view']]);
 		$this->addBehavior('AcademicPeriod.AcademicPeriod');
 	}
 
@@ -291,11 +292,14 @@ class InstitutionSurveysTable extends AppTable {
 
 	public function onUpdateToolbarButtons(Event $event, ArrayObject $buttons, ArrayObject $toolbarButtons, array $attr, $action, $isFromModel) {
 		list(, $selectedStatus) = array_values($this->_getSelectOptions());
-
-		if ($selectedStatus == 2) {	//Completed
-			if ($action == 'view') {
+		if ($action == 'view') {
+			if ($selectedStatus == 2) {	//Completed
 				if (isset($toolbarButtons['edit'])) {
 					unset($toolbarButtons['edit']);
+				}
+			} else {
+				if (isset($toolbarButtons['export'])) {
+					unset($toolbarButtons['export']);
 				}
 			}
 		}
