44c0fe890c32568b45cf030f7401b7d6f3a9f579
Chiang Hao Long <hchiang@kordit.com>
2015-10-14 17:17:29 +0800

    PHPOE-1430 - Remove ASC on index as mysql defaults to asc order index. Split commit and rollback files for index into two separate lines.

--- sql/Patch/PHPOE-1430/commit.sql
+++ sql/Patch/PHPOE-1430/commit.sql
@@ -3,11 +3,15 @@ INSERT INTO `db_patches` VALUES ('PHPOE-1430');
 
 -- student_custom_field_values
 ALTER TABLE `student_custom_field_values` 
-ADD INDEX `student_custom_field_id` (`student_custom_field_id` ASC)  COMMENT '',
-ADD INDEX `security_user_id` (`security_user_id` ASC)  COMMENT '';
+ADD INDEX `student_custom_field_id` (`student_custom_field_id`);
+
+ALTER TABLE `student_custom_field_values` 
+ADD INDEX `security_user_id` (`security_user_id`);
 
 -- staff_custom_field_values
 ALTER TABLE `staff_custom_field_values` 
-ADD INDEX `staff_custom_field_id` (`staff_custom_field_id` ASC)  COMMENT '',
-ADD INDEX `security_user_id` (`security_user_id` ASC)  COMMENT '';
+ADD INDEX `staff_custom_field_id` (`staff_custom_field_id`);
+
+ALTER TABLE `staff_custom_field_values` 
+ADD INDEX `security_user_id` (`security_user_id`);
 
--- sql/Patch/PHPOE-1430/rollback.sql
+++ sql/Patch/PHPOE-1430/rollback.sql
@@ -1,12 +1,16 @@
 
 -- student_custom_field_values
 ALTER TABLE `student_custom_field_values` 
-DROP INDEX `security_user_id` ,
+DROP INDEX `security_user_id`;
+
+ALTER TABLE `student_custom_field_values` 
 DROP INDEX `student_custom_field_id` ;
 
 -- staff_custom_field_values
 ALTER TABLE `staff_custom_field_values` 
-DROP INDEX `security_user_id` ,
+DROP INDEX `security_user_id`;
+
+ALTER TABLE `staff_custom_field_values` 
 DROP INDEX `staff_custom_field_id` ;
 
 -- db_patches
