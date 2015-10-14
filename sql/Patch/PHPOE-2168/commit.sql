e2bcdd7d3b49e530467b4e7d494a9c56bc8f1f86
Malcolm Lee <mlee@kordit.com>
2015-10-14 16:48:19 +0800

    PHPOE-2168 Optimized code for deletion of orphaned (institution_site_section_classes , institution_site_classes , institution_site_class_students , institution_site_class_staff)

--- sql/Patch/PHPOE-2168/commit.sql
+++ sql/Patch/PHPOE-2168/commit.sql
@@ -1,11 +1,11 @@
 -- SELECT * FROM institution_site_section_classes WHERE institution_site_section_id NOT IN (SELECT id FROM institution_site_sections);
-DELETE FROM institution_site_section_classes WHERE institution_site_section_id NOT IN (SELECT id FROM institution_site_sections);
+DELETE FROM institution_site_section_classes WHERE NOT EXISTS (SELECT id FROM institution_site_sections WHERE institution_site_sections.id = institution_site_section_classes.institution_site_section_id);
 
 -- SELECT * FROM institution_site_classes WHERE id NOT IN (SELECT institution_site_class_id FROM institution_site_section_classes);
-DELETE FROM institution_site_classes WHERE id NOT IN (SELECT institution_site_class_id FROM institution_site_section_classes);
+DELETE FROM institution_site_classes WHERE NOT EXISTS (SELECT institution_site_class_id FROM institution_site_section_classes WHERE institution_site_section_classes.institution_site_class_id = institution_site_classes.id);
 
 -- SELECT * FROM institution_site_class_students WHERE institution_site_class_id NOT IN (SELECT id FROM institution_site_classes);
-DELETE FROM institution_site_class_students WHERE institution_site_class_id NOT IN (SELECT id FROM institution_site_classes);
+DELETE FROM institution_site_class_students WHERE NOT EXISTS (SELECT id FROM institution_site_classes WHERE institution_site_classes.id = institution_site_class_students.institution_site_class_id);
 
 -- SELECT * FROM institution_site_class_staff WHERE institution_site_class_id NOT IN (SELECT id FROM institution_site_classes);
-DELETE FROM institution_site_class_staff WHERE institution_site_class_id NOT IN (SELECT id FROM institution_site_classes);
+DELETE FROM institution_site_class_staff WHERE NOT EXISTS (SELECT id FROM institution_site_classes WHERE institution_site_classes.id = institution_site_class_staff.institution_site_class_id);
