6133ed7f394492a0c402d0ba7814cce2d8972b0e
Malcolm Lee <mlee@kordit.com>
2015-10-14 12:13:16 +0800

    PHPOE-2168 SQL for delete orphaned records in (institution_site_section_classes , institution_site_classes , institution_site_class_students , institution_site_class_staff)

--- sql/Patch/PHPOE-2168/commit.sql
+++ sql/Patch/PHPOE-2168/commit.sql
@@ -1,41 +1,11 @@
--- backing up data to be deleted
-create table z2168_institution_site_class_students like institution_site_class_students;
-insert into z2168_institution_site_class_students (
-	z2168_institution_site_class_students.id, 
-	z2168_institution_site_class_students.status, 
-	z2168_institution_site_class_students.student_id, 
-	z2168_institution_site_class_students.institution_site_class_id, 
-	z2168_institution_site_class_students.institution_site_section_id, 
-	z2168_institution_site_class_students.modified_user_id, 
-	z2168_institution_site_class_students.modified, 
-	z2168_institution_site_class_students.created_user_id, 
-	z2168_institution_site_class_students.created
-)
-(
-	select 
-	institution_site_class_students.id, 
-	institution_site_class_students.status, 
-	institution_site_class_students.student_id, 
-	institution_site_class_students.institution_site_class_id, 
-	institution_site_class_students.institution_site_section_id, 
-	institution_site_class_students.modified_user_id, 
-	institution_site_class_students.modified, 
-	institution_site_class_students.created_user_id, 
-	institution_site_class_students.created
-	from 
-	institution_site_class_students
-	left join institution_site_sections on (institution_site_class_students.institution_site_section_id = institution_site_sections.id)
-	where institution_site_sections.id IS NULL
-); 
+-- SELECT * FROM institution_site_section_classes WHERE institution_site_section_id NOT IN (SELECT id FROM institution_site_sections);
+DELETE FROM institution_site_section_classes WHERE institution_site_section_id NOT IN (SELECT id FROM institution_site_sections);
 
--- deletion
-delete from institution_site_class_students 
-where institution_site_class_students.id in 
-	(
-		select 
-		z2168_institution_site_class_students.id
-		from 
-		z2168_institution_site_class_students
-	);
+-- SELECT * FROM institution_site_classes WHERE id NOT IN (SELECT institution_site_class_id FROM institution_site_section_classes);
+DELETE FROM institution_site_classes WHERE id NOT IN (SELECT institution_site_class_id FROM institution_site_section_classes);
 
-drop table z2168_institution_site_class_students;
+-- SELECT * FROM institution_site_class_students WHERE institution_site_class_id NOT IN (SELECT id FROM institution_site_classes);
+DELETE FROM institution_site_class_students WHERE institution_site_class_id NOT IN (SELECT id FROM institution_site_classes);
+
+-- SELECT * FROM institution_site_class_staff WHERE institution_site_class_id NOT IN (SELECT id FROM institution_site_classes);
+DELETE FROM institution_site_class_staff WHERE institution_site_class_id NOT IN (SELECT id FROM institution_site_classes);
