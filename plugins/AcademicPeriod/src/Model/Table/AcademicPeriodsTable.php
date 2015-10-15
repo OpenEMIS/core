9fe7e8823b78ec877231d86f2cc497cdf8dd8a6b
Chiang Hao Long <hchiang@kordit.com>
2015-10-16 00:59:51 +0800

    PHPOE-1981 - Fixed student attendance table. Fixed a bug of which the onExcelRender event on the excel behavior is only called once.

--- plugins/AcademicPeriod/src/Model/Table/AcademicPeriodsTable.php
+++ plugins/AcademicPeriod/src/Model/Table/AcademicPeriodsTable.php
@@ -389,7 +389,7 @@ class AcademicPeriodsTable extends AppTable {
 			$year = date('Y', $stampFirstDayOfMonth);
 			
 			$result[] = [
-				'month' => ['inNumber' => $monthNumber, 'inString' => $monthString.' '.$year],
+				'month' => ['inNumber' => $monthNumber, 'inString' => $monthString],
 				'year' => $year
 			];
 			
