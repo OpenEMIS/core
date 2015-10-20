89ed6d15438a2d00f9bbe3d9940740147023442a
Chiang Hao Long <hchiang@kordit.com>
2015-10-20 10:42:46 +0800

    PHPOE-680 - Attached rubrics report behaviour to rubrics table for report

--- plugins/Report/src/Model/Table/RubricsTable.php
+++ plugins/Report/src/Model/Table/RubricsTable.php
@@ -12,7 +12,7 @@ use Cake\ORM\Table;
 use Cake\Utility\Inflector;
 use Institution\Model\Table\InstitutionRubricsTable;
 
-class RubricsTable extends InstitutionRubricsTable  {
+class RubricsTable extends AppTable {
 	public function initialize(array $config) {
 		$this->table('institution_site_quality_rubrics');
 		parent::initialize($config);
@@ -25,7 +25,7 @@ class RubricsTable extends InstitutionRubricsTable  {
 		$this->belongsTo('Users', ['className' => 'User.Users', 'foreignKey' => 'security_user_id']);
 		$this->belongsTo('Institutions', ['className' => 'Institution.Institutions', 'foreignKey' => 'institution_site_id']);
 		$this->hasMany('InstitutionRubricAnswers', ['className' => 'Institution.InstitutionRubricAnswers', 'dependent' => true, 'cascadeCallbacks' => true]);
-		$this->addBehavior('Excel', ['excludes' => ['status', 'comment'], 'pages' => ['view']]);
+		$this->addBehavior('Report.RubricsReport');
 		$this->addBehavior('Report.ReportList');
 	}
 
@@ -37,90 +37,21 @@ class RubricsTable extends InstitutionRubricsTable  {
 		$this->ControllerAction->field('academic_period_id');
 	}
 
-	public function onExcelBeforeStart(Event $event, ArrayObject $settings, ArrayObject $sheets) {
-
-		// Get the template id and the academic period id from the $settings['params']
-		$templateId = 0;
-		$academicPeriodId = 0;
-
-		$sheets[] = [
-			'name' => $this->alias(),
-			'table' => $this,
-			'query' => $this->find(),
-			'rubric_template_id' => $templateId,
-			'academic_period_id' => $academicPeriodId
-		];
-	}
-
-	public function onExcelUpdateFields(Event $event, ArrayObject $settings, ArrayObject $fields) {
-		$templateId = $settings['sheet']['rubric_template_id'];
-
-		// Getting the section and the critieras
-		$rubricSection = $this->getRubricTemplateSectionCriteria($templateId);
-		
-		// Get Maxiumum point for the template
-		$maximumPoint = $this->getRubricTemplateOptionMaxWeighting($templateId);
-		
-		$totalPoints = 0;
-		foreach ($rubricSection as $section) {
-			$fields[] = [
-				'key' => 'Rubric.RubricSections',
-				'field' => 'rubric_section_id',
-				'type' => 'section',
-				'label' => $section['name']
-			];
-			$sectionPoint = 0;
-			foreach ($section['rubric_criterias'] as $criteria) {
-				$type = 'string';
-
-				if ($criteria['type'] == 2) {
-					$type = 'criteria';
-					$sectionPoint += $maximumPoint;
-				} elseif ($criteria['type'] == 1) {
-					$type = 'section_break';
-				}
-
-				$fields[] = [
-					'key' => 'Rubric.RubricCriterias',
-					'field' => 'rubric_criteria_id',
-					'type' => $type,
-					'label' => $criteria['name']
-				];
-			}
-
-			$fields[] = [
-				'key' => 'Rubric.SectionSubTotal',
-				'field' => 'section_subtotal',
-				'type' => 'section_points',
-				'label' => __('Sub Total').' ('.$sectionPoint.')',
-				'points' => $sectionPoint
-			];
-			$totalPoints += $sectionPoint;
-		}
-
-		$fields[] = [
-			'key' => 'Rubric.TotalPoints',
-			'field' => 'total_points',
-			'type' => 'total_points',
-			'label' => __('Sub Total').' ('.$totalPoints.')',
-			'points' => $totalPoints
-		];
-
-		$fields[] = [
-			'key' => 'Rubric.TotalPercentage',
-			'field' => 'total_percentage',
-			'type' => 'total_percentage',
-			'label' => __('Total').' (%)',
-			'points' => $totalPoints
-		];
-	}
-	public function onUpdateRubricTemplateId(Event $event, array $attr, $action, Request $request) {
-		$RubricTemplatesTable = $this->RubricTemplates;
-		
+	public function onUpdateFieldRubricTemplateId(Event $event, array $attr, $action, Request $request) {
+		$templateOptions = $this
+			->find('list', [
+				'keyField' => 'rubric_template_id',
+				'valueField' => 'template_name'
+			])
+			->matching('RubricTemplates')
+			->select(['rubric_template_id' => $this->aliasField('rubric_template_id'), 'template_name' => 'RubricTemplates.name'])
+			->group([$this->aliasField('rubric_template_id')])
+			->toArray();
+		$attr['options'] = $templateOptions;
 		return $attr;
 	}
 
-	public function onUpdateAcademicPeriodId(Event $event, array $attr, $action, Request $request) {
+	public function onUpdateFieldAcademicPeriodId(Event $event, array $attr, $action, Request $request) {
 		return $attr;
 	}
 
