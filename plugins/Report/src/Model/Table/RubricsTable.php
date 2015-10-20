0e0663c380b1c133e1f3c52f32c09aca64fe5160
Chiang Hao Long <hchiang@kordit.com>
2015-10-20 11:33:26 +0800

    PHPOE-680 - Added the Rubric report filtering and onExcelBeforeStart function in the RubricReportBehavior.

--- plugins/Report/src/Model/Table/RubricsTable.php
+++ plugins/Report/src/Model/Table/RubricsTable.php
@@ -13,6 +13,11 @@ use Cake\Utility\Inflector;
 use Institution\Model\Table\InstitutionRubricsTable;
 
 class RubricsTable extends AppTable {
+	const EXPIRED = -1;
+	const NEW_SURVEY = 0;
+	const DRAFT = 1;
+	const COMPLETED = 2;
+
 	public function initialize(array $config) {
 		$this->table('institution_site_quality_rubrics');
 		parent::initialize($config);
@@ -25,6 +30,10 @@ class RubricsTable extends AppTable {
 		$this->belongsTo('Users', ['className' => 'User.Users', 'foreignKey' => 'security_user_id']);
 		$this->belongsTo('Institutions', ['className' => 'Institution.Institutions', 'foreignKey' => 'institution_site_id']);
 		$this->hasMany('InstitutionRubricAnswers', ['className' => 'Institution.InstitutionRubricAnswers', 'dependent' => true, 'cascadeCallbacks' => true]);
+		$this->addBehavior('Excel', [
+			'excludes' => ['comment'],
+			'pages' => false
+		]);
 		$this->addBehavior('Report.RubricsReport');
 		$this->addBehavior('Report.ReportList');
 	}
@@ -33,34 +42,157 @@ class RubricsTable extends AppTable {
 		$this->fields = [];
 		$this->ControllerAction->field('feature');
 		$this->ControllerAction->field('format');
-		$this->ControllerAction->field('rubric_template_id');
-		$this->ControllerAction->field('academic_period_id');
-	}
-
-	public function onUpdateFieldRubricTemplateId(Event $event, array $attr, $action, Request $request) {
-		$templateOptions = $this
-			->find('list', [
-				'keyField' => 'rubric_template_id',
-				'valueField' => 'template_name'
-			])
-			->matching('RubricTemplates')
-			->select(['rubric_template_id' => $this->aliasField('rubric_template_id'), 'template_name' => 'RubricTemplates.name'])
-			->group([$this->aliasField('rubric_template_id')])
-			->toArray();
-		$attr['options'] = $templateOptions;
-		return $attr;
-	}
-
-	public function onUpdateFieldAcademicPeriodId(Event $event, array $attr, $action, Request $request) {
-		return $attr;
+		$this->ControllerAction->field('rubric_template_id', ['type' => 'hidden']);
+		$this->ControllerAction->field('academic_period_id', ['type' => 'hidden']);
+		$this->ControllerAction->field('status', ['type' => 'hidden']);
 	}
 
 	public function onUpdateFieldFeature(Event $event, array $attr, $action, Request $request) {
-		$attr['options'] = $this->controller->getFeatureOptions($this->alias());
-		return $attr;
+		if ($action == 'add') {
+			$attr['options'] = $this->controller->getFeatureOptions($this->alias());
+			$attr['onChangeReload'] = true;
+			if (!(isset($this->request->data[$this->alias()]['feature']))) {
+				$option = $attr['options'];
+				reset($option);
+				$this->request->data[$this->alias()]['feature'] = key($option);
+			}
+			return $attr;
+		}
 	}
 
 	public function onGetReportName(Event $event, ArrayObject $data) {
 		return __('Overview');
+	}
+
+	public function onUpdateFieldRubricTemplateId(Event $event, array $attr, $action, Request $request) {
+		if ($action == 'add') {
+			if (isset($this->request->data[$this->alias()]['feature'])) {
+				$feature = $this->request->data[$this->alias()]['feature'];
+				if ($feature == $this->registryAlias()) {
+					$templateOptions = $this
+						->find('list', [
+							'keyField' => 'rubric_template_id',
+							'valueField' => 'template_name'
+						])
+						->matching('RubricTemplates')
+						->select(['rubric_template_id' => $this->aliasField('rubric_template_id'), 'template_name' => 'RubricTemplates.name'])
+						->group([$this->aliasField('rubric_template_id')])
+						->toArray();
+					$attr['options'] = $templateOptions;
+					$attr['onChangeReload'] = true;
+					$attr['type'] = 'select';
+					if (empty($this->request->data[$this->alias()]['rubric_template_id'])) {
+						$option = $attr['options'];
+						reset($option);
+						$this->request->data[$this->alias()]['rubric_template_id'] = key($option);
+					}
+					return $attr;
+				}
+			}
+		}
+	}
+
+	public function onUpdateFieldAcademicPeriodId(Event $event, array $attr, $action, Request $request) {
+		if ($action == 'add') {
+			if (isset($this->request->data[$this->alias()]['feature']) && isset($this->request->data[$this->alias()]['rubric_template_id'])) {
+				$feature = $this->request->data[$this->alias()]['feature'];
+				$templateId = $this->request->data[$this->alias()]['rubric_template_id'];
+				if ($feature == $this->registryAlias() && !empty($templateId)) {
+					$academicPeriodOptions = $this
+						->find('list', [
+							'keyField' => 'id',
+							'valueField' => 'name'
+						])
+						->contain(['AcademicPeriods'])
+						->select(['id' => 'AcademicPeriods.id', 'name' => 'AcademicPeriods.name'])
+						->where([
+							$this->aliasField('rubric_template_id') => $templateId
+						])
+						->group([
+							$this->aliasField('rubric_template_id'), 
+							$this->aliasField('academic_period_id')
+						])
+						->order(['AcademicPeriods.order'])
+						->toArray();
+					$attr['options'] = $academicPeriodOptions;
+					$attr['onChangeReload'] = true;
+					$attr['type'] = 'select';
+					if (empty($this->request->data[$this->alias()]['academic_period_id'])) {
+						$option = $attr['options'];
+						reset($option);
+						$this->request->data[$this->alias()]['academic_period_id'] = key($option);
+					}
+					return $attr;
+				}
+			}
+		}
+	}
+
+	public function onUpdateFieldStatus(Event $event, array $attr, $action, Request $request) {
+		if ($action == 'add') {
+			if (isset($this->request->data[$this->alias()]['feature']) 
+				&& isset($this->request->data[$this->alias()]['rubric_template_id'])
+				&& isset($this->request->data[$this->alias()]['academic_period_id'])) {
+
+				$feature = $this->request->data[$this->alias()]['feature'];
+				$templateId = $this->request->data[$this->alias()]['rubric_template_id'];
+				$academicPeriodId = $this->request->data[$this->alias()]['academic_period_id'];
+
+				if ($feature == $this->registryAlias() && !empty($academicPeriodId)) {
+
+					$attr['options'] = [
+						self::COMPLETED => __('Completed'),
+						'-1' => __('Not Completed')
+					];
+
+					$attr['type'] = 'select';
+
+					$rubricsTable = $this;
+					$selected = self::COMPLETED;
+
+					$this->advancedSelectOptions($attr['options'], $selected, [
+						'message' => '{{label}} - ' . $this->getMessage($this->aliasField('noSurveys')),
+						'callable' => function($id) use ($rubricsTable, $templateId, $academicPeriodId) {
+
+							$query = $rubricsTable->find('list', [
+								'keyField' => 'rubricsStatus',
+								'valueField' => 'statusCount'
+							]);
+
+							// Add a case to check if the rubrics is completed or not
+							$completedRubrics = $query->newExpr()->addCase(
+								[$query->newExpr()->eq($this->aliasField('status'), self::COMPLETED)], 
+								[self::COMPLETED, -1], 
+								['integer', 'integer']);
+
+							$query->select([
+									'rubricsStatus' => $completedRubrics,
+									'statusCount' => $query->func()->count($this->aliasField('id'))
+								])
+								->group(['rubricsStatus'])
+								->where([
+									$rubricsTable->aliasField('rubric_template_id') => $templateId,
+									$rubricsTable->aliasField('academic_period_id') => $academicPeriodId
+								]);
+
+							return $query->having(['rubricsStatus' => $id])->count();
+						}
+					]);
+					return $attr;
+				}
+			}
+		}
+	}
+
+	public function onExcelGetStatus(Event $event, Entity $entity) {
+		$status = $entity->status;
+		switch ($status) {
+			case self::COMPLETED:
+				return __('Completed');
+				break;
+			default:
+				return __('Not Completed');
+				break;
+		}
 	}
 }
