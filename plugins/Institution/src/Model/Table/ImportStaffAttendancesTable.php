8ea864b433dcab72b2bfe5a426a9990c6d5a3775
Hanafi Ahmat <hahmat@kordit.com>
2015-10-21 16:55:22 +0800

    PHPOE-2084	- WIP

--- plugins/Institution/src/Model/Table/ImportStaffAttendancesTable.php
+++ plugins/Institution/src/Model/Table/ImportStaffAttendancesTable.php
@@ -1,6 +1,7 @@
 <?php
 namespace Institution\Model\Table;
 
+use DateTime;
 use ArrayObject;
 use PHPExcel_Worksheet;
 use Cake\Event\Event;
@@ -15,7 +16,6 @@ class ImportStaffAttendancesTable extends AppTable {
 		parent::initialize($config);
 
         $this->addBehavior('Import.Import', ['plugin'=>'Institution', 'model'=>'StaffAbsences']);
-	    // $this->addBehavior('Import.Import');
 
 	    $this->StaffAbsences = TableRegistry::get('Institution.StaffAbsences');
 	    $this->Institutions = TableRegistry::get('Institution.Institutions');
@@ -23,198 +23,65 @@ class ImportStaffAttendancesTable extends AppTable {
 	    $this->Users = TableRegistry::get('User.Users');
 	}
 
+	public function beforeAction($event) {
+		$session = $this->request->session();
+		if ($session->check('Institution.Institutions.id')) {
+			$this->institutionId = $session->read('Institution.Institutions.id');
+		} else {
+			$this->institutionId = false;
+		}
+		$this->systemDateFormat = TableRegistry::get('ConfigItems')->value('date_format');
+	}
+
 	public function implementedEvents() {
 		$events = parent::implementedEvents();
 		$newEvent = [
 			'Model.import.onImportCheckUnique' => 'onImportCheckUnique',
 			'Model.import.onImportUpdateUniqueKeys' => 'onImportUpdateUniqueKeys',
-			// 'Model.import.onImportPopulateDirectTableData' => 'onImportPopulateDirectTableData',
-			'Model.import.onImportPopulateAcademicPeriodsData' => 'onImportPopulateAcademicPeriodsData',
-			'Model.import.onImportPopulateInstitutionsData' => 'onImportPopulateInstitutionsData',
 			'Model.import.onImportPopulateUsersData' => 'onImportPopulateUsersData',
+			'Model.import.onImportModelSpecificValidation' => 'onImportModelSpecificValidation',
 		];
 		$events = array_merge($events, $newEvent);
 		return $events;
 	}
 
 	public function onImportCheckUnique(Event $event, PHPExcel_Worksheet $sheet, $row, $columns, ArrayObject $tempRow, ArrayObject $importedUniqueCodes) {
-
 		$tempRow['duplicates'] = false;
 		$tempRow['entity'] = $this->StaffAbsences->newEntity();
-
 	}
 
 	public function onImportUpdateUniqueKeys(Event $event, ArrayObject $importedUniqueCodes, Entity $entity) {
 		// $importedUniqueCodes[] = $entity->code;
 	}
 
-	public function onImportPopulateAcademicPeriodsData(Event $event, $lookupPlugin, $lookupModel, $lookupColumn, $sheetName, $translatedCol, ArrayObject $data) {
-		$session = $this->request->session();
-		if ($session->check('Institution.Institutions.id')) {
-			$institutionId = $session->read('Institution.Institutions.id');
-		} else {
-			$institutionId = false;
-		}
-		$lookedUpTable = TableRegistry::get($lookupPlugin . '.' . $lookupModel);
-		$selectFields = ['name', 'academic_period_level_id', 'start_date', 'end_date', 'Levels.name', $lookupColumn];
-
-		$modelData = $lookedUpTable->find('all')->select($selectFields);
-
-		$systemDateFormat = TableRegistry::get('ConfigItems')->value('date_format');
-		$modelData->contain(['Levels'])->where(['parent_id <> 0']);
-
-		$nameCol = $this->getExcelLabel($lookedUpTable, 'name');
-		$periodCol = $this->getExcelLabel($lookedUpTable, 'academic_period_level_id');
-		$startDateCol = $this->getExcelLabel($lookedUpTable, 'start_date');
-		$endDateCol = $this->getExcelLabel($lookedUpTable, 'end_date');
-		$data[$sheetName][] = [$periodCol, $nameCol, $startDateCol, $endDateCol, $translatedCol];
-		if (!empty($modelData)) {
-			foreach($modelData->toArray() as $row) {
-				$data[$sheetName][] = [
-					$row->level->name,
-					$row->name,
-					$row->start_date->format($systemDateFormat),
-					$row->end_date->format($systemDateFormat),
-					$row->$lookupColumn
-				];
-			}
-		}
-		return true;
-	}
-
-	public function onImportPopulateInstitutionsData(Event $event, $lookupPlugin, $lookupModel, $lookupColumn, $sheetName, $translatedCol, ArrayObject $data) {
-		$session = $this->request->session();
-		if ($session->check('Institution.Institutions.id')) {
-			$institutionId = $session->read('Institution.Institutions.id');
-		} else {
-			$institutionId = false;
-		}
-		$lookedUpTable = TableRegistry::get($lookupPlugin . '.' . $lookupModel);
-		if ($lookedUpTable->hasField('name')) {
-			if ($lookupModel == 'AcademicPeriods') {
-				$selectFields = ['name', 'academic_period_level_id', 'start_date', 'end_date', 'Levels.name', $lookupColumn];
-			} else {
-				$selectFields = ['name', $lookupColumn];
-			}
-		} else {
-			$selectFields = ['first_name', 'middle_name', 'third_name', 'last_name', $lookupColumn];
-		}
-
-		$modelData = $lookedUpTable->find('all')->select($selectFields);
-
-		if ($lookupModel == 'AcademicPeriods') {
-			$systemDateFormat = TableRegistry::get('ConfigItems')->value('date_format');
-			$modelData->contain(['Levels'])->where(['parent_id <> 0']);
-
-			$nameCol = $this->getExcelLabel($lookedUpTable, 'name');
-			$periodCol = $this->getExcelLabel($lookedUpTable, 'academic_period_level_id');
-			$startDateCol = $this->getExcelLabel($lookedUpTable, 'start_date');
-			$endDateCol = $this->getExcelLabel($lookedUpTable, 'end_date');
-			$data[$sheetName][] = [$periodCol, $nameCol, $startDateCol, $endDateCol, $translatedCol];
-			if (!empty($modelData)) {
-				foreach($modelData->toArray() as $row) {
-					$data[$sheetName][] = [
-						$row->level->name,
-						$row->name,
-						$row->start_date->format($systemDateFormat),
-						$row->end_date->format($systemDateFormat),
-						$row->$lookupColumn
-					];
-				}
-			}
-			return true;
-		}
-		
-		if ($institutionId) {
-			if ($lookupModel == 'Institutions') {
-				$modelData->where(['id'=>$institutionId]);
-			} else if ($lookupModel == 'Users') {
-				$activeStaff = $this->Staff
-									->find('all')
-									->where([$this->Staff->aliasField('institution_site_id') => $institutionId])
-									;
-				$activeStaffIds = new Collection($activeStaff->toArray());
-				$modelData->where([
-					'id IN' => $activeStaffIds->extract('security_user_id')->toArray()
-				]);
-			}
-		}
-
-		$translatedReadableCol = $this->getExcelLabel($lookedUpTable, 'name');
-		$data[$sheetName][] = [$translatedReadableCol, $translatedCol];
-		if (!empty($modelData)) {
-			foreach($modelData->toArray() as $row) {
-				$data[$sheetName][] = [
-					$row->name,
-					$row->$lookupColumn
-				];
-			}
-		}	
-	}
-
 	public function onImportPopulateUsersData(Event $event, $lookupPlugin, $lookupModel, $lookupColumn, $sheetName, $translatedCol, ArrayObject $data) {
-		$session = $this->request->session();
-		if ($session->check('Institution.Institutions.id')) {
-			$institutionId = $session->read('Institution.Institutions.id');
-		} else {
-			$institutionId = false;
-		}
 		$lookedUpTable = TableRegistry::get($lookupPlugin . '.' . $lookupModel);
-		if ($lookedUpTable->hasField('name')) {
-			if ($lookupModel == 'AcademicPeriods') {
-				$selectFields = ['name', 'academic_period_level_id', 'start_date', 'end_date', 'Levels.name', $lookupColumn];
-			} else {
-				$selectFields = ['name', $lookupColumn];
-			}
-		} else {
-			$selectFields = ['first_name', 'middle_name', 'third_name', 'last_name', $lookupColumn];
-		}
+		$modelData = $lookedUpTable->find('all')->select(['id', 'first_name', 'middle_name', 'third_name', 'last_name', $lookupColumn]);
 
-		$modelData = $lookedUpTable->find('all')->select($selectFields);
+		$allStaff = $this->Staff
+							->find('all')
+							->where([$this->Staff->aliasField('institution_site_id') => $this->institutionId])
+							;
+		// when extracting the security_user_id from $allStaff collection, there will be no duplicates
+		$allStaff = new Collection($allStaff->toArray());
+		$modelData->where([
+			'id IN' => $allStaff->extract('security_user_id')->toArray()
+		]);
 
-		if ($lookupModel == 'AcademicPeriods') {
-			$systemDateFormat = TableRegistry::get('ConfigItems')->value('date_format');
-			$modelData->contain(['Levels'])->where(['parent_id <> 0']);
-
-			$nameCol = $this->getExcelLabel($lookedUpTable, 'name');
-			$periodCol = $this->getExcelLabel($lookedUpTable, 'academic_period_level_id');
-			$startDateCol = $this->getExcelLabel($lookedUpTable, 'start_date');
-			$endDateCol = $this->getExcelLabel($lookedUpTable, 'end_date');
-			$data[$sheetName][] = [$periodCol, $nameCol, $startDateCol, $endDateCol, $translatedCol];
-			if (!empty($modelData)) {
-				foreach($modelData->toArray() as $row) {
-					$data[$sheetName][] = [
-						$row->level->name,
-						$row->name,
-						$row->start_date->format($systemDateFormat),
-						$row->end_date->format($systemDateFormat),
-						$row->$lookupColumn
-					];
-				}
-			}
-			return true;
-		}
-		
-		if ($institutionId) {
-			if ($lookupModel == 'Institutions') {
-				$modelData->where(['id'=>$institutionId]);
-			} else if ($lookupModel == 'Users') {
-				$activeStaff = $this->Staff
-									->find('all')
-									->where([$this->Staff->aliasField('institution_site_id') => $institutionId])
-									;
-				$activeStaffIds = new Collection($activeStaff->toArray());
-				$modelData->where([
-					'id IN' => $activeStaffIds->extract('security_user_id')->toArray()
-				]);
-			}
-		}
-
-		$translatedReadableCol = $this->getExcelLabel($lookedUpTable, 'name');
-		$data[$sheetName][] = [$translatedReadableCol, $translatedCol];
+		$institution = $this->Institutions->get($this->institutionId);
+		$institutionHeader = $this->getExcelLabel('Imports', 'institution_site_id') . ": " . $institution->name;
+		$nameHeader = $this->getExcelLabel($lookedUpTable, 'name');
+		$columnHeader = $this->getExcelLabel($lookedUpTable, $lookupColumn);
+		$data[$sheetName][] = [
+			$institutionHeader,
+			$nameHeader,
+			$columnHeader
+		];
 		if (!empty($modelData)) {
 			foreach($modelData->toArray() as $row) {
+				$focusedStaff = $allStaff->indexBy('security_user_id')->toArray()[$row->id];
 				$data[$sheetName][] = [
+					$institution->name,
 					$row->name,
 					$row->$lookupColumn
 				];
@@ -223,31 +90,24 @@ class ImportStaffAttendancesTable extends AppTable {
 	}
 
 	public function onImportModelSpecificValidation(Event $event, $references, ArrayObject $tempRow, ArrayObject $originalRow, ArrayObject $rowInvalidCodeCols) {
-		// pr('onImportModelSpecificValidation');
-		// pr($tempRow);
-		// die;
-
-		$session = $this->request->session();
-		if ($session->check('Institution.Institutions.id')) {
-			$institutionId = $session->read('Institution.Institutions.id');
-		} else {
-			$institutionId = false;
-		}
-
 		if (empty($tempRow['security_user_id'])) {
 			return false;
 		}
-	
-		$institution = $this->Institutions->exists($tempRow['institution_site_id']);
-		if (!$institution) {
-			$tempRow['duplicates'] = __('No such institution');
-			$tempRow['institution_site_id'] = false;
-			return false;
-		} else if ($institutionId && $institutionId!=$tempRow['institution_site_id']) {
-			$tempRow['duplicates'] = __('Wrong institution');
+
+		if (!$this->institutionId) {
+			$tempRow['duplicates'] = __('No active institution');
 			$tempRow['institution_site_id'] = false;
 			return false;
 		}
+		$tempRow['institution_site_id'] = $this->institutionId;
+
+		$period = $this->getAcademicPeriodByStartDate($tempRow['start_date']);
+		if (!$period) {
+			$tempRow['duplicates'] = __('No matching academic period');
+			$tempRow['academic_period_id'] = false;
+			return false;
+		}
+		$tempRow['academic_period_id'] = $period->id;
 
 		$staff = $this->Staff->find()->where([
 			'institution_site_id' => $tempRow['institution_site_id'],
--- sql/Patch/PHPOE-2084/rollback.sql
+++ sql/Patch/PHPOE-2084/rollback.sql
@@ -3,4 +3,6 @@
 DROP TABLE `import_mapping`;
 ALTER TABLE `z2084_import_mapping` RENAME `import_mapping`;
 
+DELETE FROM `labels` WHERE `module` = 'InstitutionSiteStudentAbsences' OR `module` = 'StaffAbsences' OR `module` = 'Imports';
+
 DELETE FROM `db_patches` WHERE `issue`='PHPOE-2084';
