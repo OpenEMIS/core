9724b88b67c45e0eaa926239c07eaa0a0c9384db
Thed Kuok Shiun <kuokshiun@hotmail.com>
2015-10-23 20:14:30 +0800

    PHPOE-1992 - Working on CRUD of Admin > Training > Sessions.

--- plugins/Training/src/Model/Table/TrainingSessionsTable.php
+++ plugins/Training/src/Model/Table/TrainingSessionsTable.php
@@ -4,16 +4,24 @@ namespace Training\Model\Table;
 use ArrayObject;
 use App\Model\Table\AppTable;
 use Cake\ORM\Entity;
+use Cake\ORM\Query;
+use Cake\ORM\TableRegistry;
 use Cake\Validation\Validator;
 use Cake\Network\Request;
 use Cake\Event\Event;
+use App\Model\Traits\OptionsTrait;
 
 class TrainingSessionsTable extends AppTable {
+	use OptionsTrait;
+
+	private $_contain = ['Trainers.Users'];
+
 	public function initialize(array $config) {
 		parent::initialize($config);
 		$this->belongsTo('Statuses', ['className' => 'Workflow.WorkflowSteps', 'foreignKey' => 'status_id']);
 		$this->belongsTo('Courses', ['className' => 'Training.TrainingCourses', 'foreignKey' => 'training_course_id']);
 		$this->belongsTo('TrainingProviders', ['className' => 'Training.TrainingProviders', 'foreignKey' => 'training_provider_id']);
+		$this->hasMany('Trainers', ['className' => 'Training.TrainingSessionTrainers', 'foreignKey' => 'training_session_id', 'dependent' => true, 'cascadeCallbacks' => true]);
 	}
 
 	public function validationDefault(Validator $validator) {
@@ -45,24 +53,122 @@ class TrainingSessionsTable extends AppTable {
 		$this->ControllerAction->field('status_id', [
 			'visible' => ['index' => true, 'view' => false, 'edit' => true, 'add' => true]
 		]);
-		$this->ControllerAction->field('training_course_id', [
-			'type' => 'select'
-		]);
-		$this->ControllerAction->field('training_provider_id', [
-			'type' => 'select',
-			'visible' => ['index' => false, 'view' => true, 'edit' => true, 'add' => true]
-		]);
-		$this->ControllerAction->field('end_date', [
-			'visible' => ['index' => false, 'view' => true, 'edit' => true, 'add' => true]
-		]);
-		$this->ControllerAction->field('comment', [
-			'visible' => ['index' => false, 'view' => true, 'edit' => true, 'add' => true]
-		]);
+		$visible = ['index' => false, 'view' => true, 'edit' => true, 'add' => true];
+		$this->ControllerAction->field('end_date', ['visible' => $visible]);
+		$this->ControllerAction->field('comment', ['visible' => $visible]);
 
-		// Order
+		$trainerTypeOptions = $this->getSelectOptions($this->aliasField('trainer_types'));
+		$this->controller->set('trainerTypeOptions', $trainerTypeOptions);
+	}
+
+	public function indexBeforeAction(Event $event) {
 		$this->ControllerAction->setFieldOrder([
-			'status_id', 'training_course_id', 'training_provider_id', 'start_date', 'end_date', 'comment'
+			'status_id', 'training_course_id', 'start_date'
 		]);
+	}
+
+	public function viewEditBeforeQuery(Event $event, Query $query) {
+		$query->contain($this->_contain);
+	}
+
+	public function viewAfterAction(Event $event, Entity $entity) {
+		$this->setupFields();
+	}
+
+	public function addEditAfterAction(Event $event, Entity $entity) {
+		$this->setupFields();
+	}
+
+	public function addEditBeforePatch(Event $event, Entity $entity, ArrayObject $data, ArrayObject $options) {
+		//Required by patchEntity for associated data
+		$newOptions = [];
+		$newOptions['associated'] = $this->_contain;
+
+		$arrayOptions = $options->getArrayCopy();
+		$arrayOptions = array_merge_recursive($arrayOptions, $newOptions);
+		$options->exchangeArray($arrayOptions);
+	}
+
+	public function addEditOnChangeCourse(Event $event, Entity $entity, ArrayObject $data, ArrayObject $options) {
+		$request = $this->request;
+		unset($request->query['course']);
+
+		if ($request->is(['post', 'put'])) {
+			if (array_key_exists($this->alias(), $request->data)) {
+				if (array_key_exists('training_course_id', $request->data[$this->alias()])) {
+					$request->query['course'] = $request->data[$this->alias()]['training_course_id'];
+				}
+			}
+		}
+	}
+
+	public function addEditOnAddTrainer(Event $event, Entity $entity, ArrayObject $data, ArrayObject $options) {
+		$trainerOptions = [
+			'type' => key($this->getSelectOptions($this->aliasField('trainer_types'))),
+			'trainer_id' => 0,
+			'name' => '',
+			'visible' => 1
+		];
+		$data[$this->alias()]['trainers'][] = $trainerOptions;
+
+		//Validation is disabled by default when onReload, however immediate line below will not work and have to disabled validation for associated model like the following lines
+		$options['associated'] = [
+			'Trainers' => ['validate' => false]
+		];
+	}
+
+	public function onUpdateFieldTrainingCourseId(Event $event, array $attr, $action, Request $request) {
+		if ($action == 'add' || $action == 'edit') {
+			$courseOptions = $this->Courses
+				->find('list')
+				->toArray();
+			$courseId = $this->queryString('course', $courseOptions);
+
+			$attr['options'] = $courseOptions;
+			$attr['onChangeReload'] = 'changeCourse';
+		}
+
+		return $attr;
+	}
+
+	public function onUpdateFieldTrainingProviderId(Event $event, array $attr, $action, Request $request) {
+		if ($action == 'add' || $action == 'edit') {
+			$courseId = $request->query('course');
+
+			$TrainingCoursesProviders = TableRegistry::get('Training.TrainingCoursesProviders');
+			$providers = $TrainingCoursesProviders
+				->find()
+				->matching('TrainingProviders')
+				->where([
+					$TrainingCoursesProviders->aliasField('training_course_id') => $courseId
+				])
+				->all();
+
+			$providerOptions = [];
+			foreach ($providers as $provider) {
+				$providerOptions[$provider->_matchingData['TrainingProviders']->id] = $provider->_matchingData['TrainingProviders']->name;
+			}
+			$attr['options'] = $providerOptions;
+		}
+
+		return $attr;
+	}
+
+	public function onUpdateFieldTrainers(Event $event, array $attr, $action, Request $request) {
+		if ($action == 'add' || $action == 'edit') {
+			$Users = TableRegistry::get('User.Users');
+			$trainerOptions = $Users
+				->find('list', ['keyField' => 'id', 'valueField' => 'name_with_id'])
+				->where([
+					$Users->aliasField('is_student') => 0,
+					$Users->aliasField('is_staff') => 0,
+					$Users->aliasField('is_guardian') => 0
+				])
+				->toArray();
+			$attr['options'] = $trainerOptions;
+		}
+
+		return $attr;
 	}
 
 	public function onUpdateFieldStatusId(Event $event, array $attr, $action, Request $request) {
@@ -89,5 +195,29 @@ class TrainingSessionsTable extends AppTable {
 				['id' => $entity->id]
 			);
 		}
+	}
+
+	public function setupFields() {
+		$this->ControllerAction->field('training_course_id', [
+			'type' => 'select'
+		]);
+		$this->ControllerAction->field('training_provider_id', [
+			'type' => 'select',
+			'visible' => ['index' => false, 'view' => true, 'edit' => true, 'add' => true]
+		]);
+		$this->ControllerAction->field('trainers', [
+			'type' => 'element',
+			'element' => 'Training.TrainingSessions/trainers',
+			'valueClass' => 'table-full-width'
+		]);
+		$this->ControllerAction->field('trainees', [
+			'type' => 'element',
+			'element' => 'Training.TrainingSessions/trainees',
+			'valueClass' => 'table-full-width'
+		]);
+
+		$this->ControllerAction->setFieldOrder([
+			'status_id', 'training_course_id', 'training_provider_id', 'start_date', 'end_date', 'comment'
+		]);
 	}
 }
---
+++ plugins/Training/src/Template/Element/TrainingSessions/trainers.ctp
@@ -0,0 +1,95 @@
+<?= $this->Html->script('OpenEmis.../plugins/tableCheckable/jquery.tableCheckable', ['block' => true]) ?>
+<?php
+	$model = $ControllerAction['table'];
+	$trainerOptions = isset($attr['options']) ? $attr['options'] : [];
+?>
+<?php if ($action == 'view') : ?>
+	<div class="table-in-view">
+		<table class="table table-striped table-hover table-bordered">
+			<thead>
+				<tr>
+					<th><?= $this->Label->get('general.visible'); ?></th>
+					<th><?= $this->Label->get($model->aliasField('trainer_type')); ?></th>
+					<th><?= $this->Label->get($model->aliasField('internal_trainer')); ?></th>
+					<th><?= $this->Label->get($model->aliasField('external_trainer')); ?></th>
+				</tr>
+			</thead>
+			<?php if (!empty($data->trainers)) : ?>
+				<tbody>
+					<?php foreach ($data->trainers as $key => $obj) : ?>
+					<tr>
+						<td>
+							<?php if ($obj->visible == 1) : ?>
+								<i class="fa fa-check"></i>
+							<?php else : ?>
+								<i class="fa fa-close"></i>
+							<?php endif ?>
+						</td>
+						<td><?= $trainerTypeOptions[$obj->type]; ?></td>
+						<td><?= $obj->user->name_with_id; ?></td>
+						<td><?= $obj->name; ?></td>
+					</tr>
+					<?php endforeach ?>
+				</tbody>
+			<?php endif ?>
+		</table>
+	</div>
+<?php elseif ($action == 'add' || $action == 'edit') : ?>
+	<div class="input">
+		<label for="<?= $attr['id'] ?>"><?= isset($attr['label']) ? $attr['label'] : $attr['field'] ?></label>
+		<div class="table-toolbar">
+			<button onclick="$('#reload').val('addTrainer').click();return false;" class="btn btn-default btn-xs">
+				<i class="fa fa-plus"></i>
+				<span><?= __('Add');?></span>
+			</button>
+		</div>
+		<div class="table-in-view">
+			<table class="table table-striped table-hover table-bordered table-checkable table-input">
+				<thead>
+					<tr>
+						<?php if ($action == 'edit') : ?>
+							<th class="checkbox-column"><input type="checkbox" class="icheck-input" /></th>
+						<?php endif ?>
+						<th><?= $this->Label->get($model->aliasField('trainer_type')); ?></th>
+						<th><?= $this->Label->get($model->aliasField('internal_trainer')); ?></th>
+						<th><?= $this->Label->get($model->aliasField('external_trainer')); ?></th>
+						<th></th>
+					</tr>
+					<?php if (!empty($data->trainers)) : ?>
+						<tbody>
+							<?php foreach ($data->trainers as $key => $obj) : ?>
+								<?php
+									$prefix = $model->alias().'.trainers.'.$key;
+								?>
+								<tr>
+									<?php if ($action == 'edit') : ?>
+										<td class="checkbox-column">
+											<?= $this->Form->checkbox("$prefix.visible", ['class' => 'icheck-input', 'checked' => $obj['visible']]); ?>
+										</td>
+									<?php endif ?>
+										<td>
+											<?php
+												if(isset($obj['id'])) {	// edit
+													echo $this->Form->hidden("$prefix.id");
+												} else {	// add
+													echo $this->Form->hidden("$prefix.visible", ['value' => 1]);
+												}
+												echo $this->Form->input("$prefix.type", ['label' => false, 'options' => $trainerTypeOptions]);
+											?>
+										</td>
+										<td><?= $this->Form->input("$prefix.trainer_id", ['label' => false, 'options' => $trainerOptions]); ?></td>
+										<td><?= $this->Form->input("$prefix.name", ['label' => false]); ?></td>
+										<td>
+											<button class="btn btn-dropdown action-toggle btn-single-action" style="cursor: pointer;" title="<?= $this->Label->get('general.delete.label'); ?>" onclick="jsTable.doRemove(this);">
+												<i class="fa fa-trash"></i>&nbsp;<span><?= __('Delete')?></span>
+											</button>
+										</td>
+								</tr>
+							<?php endforeach ?>
+						</tbody>
+					<?php endif ?>
+				</thead>
+			</table>
+		</div>
+	</div>
+<?php endif ?>
