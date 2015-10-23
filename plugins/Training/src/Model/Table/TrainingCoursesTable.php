e6ce9ea9bd250e0682af84c22863ae91d8b6ca23
Thed Kuok Shiun <kuokshiun@hotmail.com>
2015-10-23 12:38:56 +0800

    PHPOE-1992 - Working on CRUD of Admin > Training > Courses.

--- plugins/Training/src/Model/Table/TrainingCoursesTable.php
+++ plugins/Training/src/Model/Table/TrainingCoursesTable.php
@@ -1,11 +1,128 @@
 <?php
 namespace Training\Model\Table;
 
+use ArrayObject;
 use App\Model\Table\AppTable;
+use Cake\ORM\Entity;
+use Cake\Validation\Validator;
+use Cake\Network\Request;
 use Cake\Event\Event;
 
 class TrainingCoursesTable extends AppTable {
 	public function initialize(array $config) {
 		parent::initialize($config);
+		$this->belongsTo('Statuses', ['className' => 'Workflow.WorkflowSteps', 'foreignKey' => 'status_id']);
+		$this->belongsTo('TrainingFieldStudies', ['className' => 'Training.TrainingFieldStudies', 'foreignKey' => 'training_field_of_study_id']);
+		$this->belongsTo('TrainingCourseTypes', ['className' => 'Training.TrainingCourseTypes', 'foreignKey' => 'training_course_type_id']);
+		$this->belongsTo('TrainingModeDeliveries', ['className' => 'Training.TrainingModeDeliveries', 'foreignKey' => 'training_mode_of_delivery_id']);
+		$this->belongsTo('TrainingRequirements', ['className' => 'Training.TrainingRequirements', 'foreignKey' => 'training_requirement_id']);
+		$this->belongsTo('TrainingLevels', ['className' => 'Training.TrainingLevels', 'foreignKey' => 'training_level_id']);
+		$this->addBehavior('ControllerAction.FileUpload', [
+			// 'name' => 'file_name',
+			// 'content' => 'file_content',
+			'size' => '10MB',
+			'contentEditable' => true,
+			'allowable_file_types' => 'all'
+		]);
+	}
+
+	public function validationDefault(Validator $validator) {
+		$validator = parent::validationDefault($validator);
+		
+		return $validator
+			->allowEmpty('file_content');
+	}
+
+	public function implementedEvents() {
+    	$events = parent::implementedEvents();
+    	$events['Workflow.afterTransition'] = 'workflowAfterTransition';
+
+    	return $events;
+    }
+
+	public function afterSave(Event $event, Entity $entity, ArrayObject $options) {
+		$this->updateStatusId($entity);
+	}
+
+	public function onGetStatusId(Event $event, Entity $entity) {
+		return '<span class="status highlight">' . $entity->status->name . '</span>';
+	}
+
+	public function beforeAction(Event $event) {
+		// Select
+		$this->ControllerAction->field('training_field_of_study_id', ['type' => 'select']);
+		$this->ControllerAction->field('training_course_type_id', ['type' => 'select']);
+		$this->ControllerAction->field('training_mode_of_delivery_id', ['type' => 'select']);
+		$this->ControllerAction->field('training_requirement_id', ['type' => 'select']);
+		$this->ControllerAction->field('training_level_id', ['type' => 'select']);
+
+		// Visible
+		$this->ControllerAction->field('status_id', [
+			'visible' => ['index' => true, 'view' => false, 'edit' => true, 'add' => true]
+		]);
+		$this->ControllerAction->field('description', [
+			'visible' => ['index' => false, 'view' => true, 'edit' => true, 'add' => true]
+		]);
+		$this->ControllerAction->field('objective', [
+			'visible' => ['index' => false, 'view' => true, 'edit' => true, 'add' => true]
+		]);
+		$this->ControllerAction->field('duration', [
+			'visible' => ['index' => false, 'view' => true, 'edit' => true, 'add' => true]
+		]);
+		$this->ControllerAction->field('training_field_of_study_id', [
+			'visible' => ['index' => false, 'view' => true, 'edit' => true, 'add' => true]
+		]);
+		$this->ControllerAction->field('training_course_type_id', [
+			'visible' => ['index' => false, 'view' => true, 'edit' => true, 'add' => true]
+		]);
+		$this->ControllerAction->field('training_mode_of_delivery_id', [
+			'visible' => ['index' => false, 'view' => true, 'edit' => true, 'add' => true]
+		]);
+		$this->ControllerAction->field('training_requirement_id', [
+			'visible' => ['index' => false, 'view' => true, 'edit' => true, 'add' => true]
+		]);
+		$this->ControllerAction->field('training_level_id', [
+			'visible' => ['index' => false, 'view' => true, 'edit' => true, 'add' => true]
+		]);
+		$this->ControllerAction->field('file_name', [
+			'type' => 'hidden',
+			'visible' => ['index' => false, 'view' => true, 'edit' => true, 'add' => true]
+		]);
+		$this->ControllerAction->field('file_content', [
+			'visible' => ['index' => false, 'view' => true, 'edit' => true, 'add' => true]
+		]);
+
+		// Order
+		$this->ControllerAction->setFieldOrder([
+			'status_id', 'code', 'title', 'description', 'objective', 'credit_hours', 'duration',
+			'training_field_of_study_id', 'training_course_type_id', 'training_mode_of_delivery_id', 'training_requirement_id', 'training_level_id',
+			'file_name', 'file_content'
+		]);
+	}
+
+	public function onUpdateFieldStatusId(Event $event, array $attr, $action, Request $request) {
+		if ($action == 'index') {
+			$attr['type'] = 'select';
+		} else {
+			$attr['type'] = 'hidden';
+			$attr['value'] = 0;
+		}
+
+		return $attr;
+	}
+
+	public function workflowAfterTransition(Event $event, $id=null) {
+		$entity = $this->get($id);
+		$this->updateStatusId($entity);
+	}
+
+	public function updateStatusId(Entity $entity) {
+		$workflowRecord = $this->getRecord($this->registryAlias(), $entity);
+		if (!empty($workflowRecord)) {
+			$this->updateAll(
+				['status_id' => $workflowRecord->workflow_step_id],
+				['id' => $entity->id]
+			);
+		}
 	}
 }
