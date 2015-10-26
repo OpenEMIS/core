b0d4336b3dacd8a3511ae19355ef9c63f7b54bd5
Chiang Hao Long <hchiang@kordit.com>
2015-10-26 15:44:35 +0800

    PHPOE-1420 - Added logic to get the header and the values for each of the subjects base on a particular class.

--- plugins/Assessment/src/Model/Table/AssessmentItemResultsTable.php
+++ plugins/Assessment/src/Model/Table/AssessmentItemResultsTable.php
@@ -11,4 +11,27 @@ class AssessmentItemResultsTable extends AppTable {
 		$this->belongsTo('Institutions', ['className' => 'Institution.Institutions', 'foreignKey' => 'institution_site_id']);
 		$this->belongsTo('AcademicPeriods', ['className' => 'AcademicPeriod.AcademicPeriods']);
 	}
+
+	public function getAssessmentItemResults($institutionId, $academicPeriodId) {
+		$results = $this
+			->find()
+			->matching('GradingOptions')
+			->where([
+				$this->aliasField('institution_id') => $institutionId, 
+				$this->aliasField('academic_period_id') => $academicPeriodId
+			])
+			->select(['grade_name' => 'GradingOptions.name', 'grade_code' => 'GradingOptions.code'])
+			->autoFields(true)
+			->hydrate(false)
+			->toArray();
+		$returnArray = [];
+		foreach ($results as $result) {
+			$returnArray[$result['institution_id']][$result['student_id']][$result['assessment_item_id']] = [
+					'marks' => $result['marks'], 
+					'grade_name' => $result['grade_name'], 
+					'grade_code' => $result['grade_code']
+				];
+		}
+		return $returnArray;
+	}
 }
