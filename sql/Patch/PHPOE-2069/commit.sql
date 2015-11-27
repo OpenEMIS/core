ba53bfb2f1ed6ba99e2162bb9d29258695dcf494
Chiang Hao Long <hchiang@kordit.com>
2015-11-27 15:00:39 +0800

    PHPOE-2069 - Updated the patch logic for setting the is_main_country id

--- sql/Patch/PHPOE-2069/commit.sql
+++ sql/Patch/PHPOE-2069/commit.sql
@@ -2,3 +2,13 @@ INSERT INTO `db_patches` VALUES ('PHPOE-2069', NOW());
 
 ALTER TABLE `area_administratives` 
 ADD COLUMN `is_main_country` INT(1) NOT NULL DEFAULT 0 COMMENT '' AFTER `name`;
+
+UPDATE `area_administratives`
+INNER JOIN (
+	SELECT `id`
+    FROM `area_administratives`
+    WHERE `area_administrative_level_id` = 1
+    ORDER BY `id`
+    LIMIT 1
+) `tmp` ON `area_administratives`.`id` = `tmp`.`id`
+SET `is_main_country` = 1;
