1011e31c6ec21d50f9dce803d08a06fd4f39bb7a
Malcolm Lee <mlee@kordit.com>
2015-11-27 16:17:16 +0800

    PHPOE-1833 Put the actual values for parent_id for commit.sql for permissions

--- sql/Patch/PHPOE-1707/commit.sql
+++ sql/Patch/PHPOE-1707/commit.sql
@@ -8,37 +8,29 @@ UPDATE security_functions SET _view = REPLACE(_view, '|Accounts.index', ''), _ed
 UPDATE security_functions SET _view = REPLACE(_view, '|StudentAccount.view', ''), _edit = REPLACE(_view, '|StudentAccount.edit', '')  WHERE id = '1012';
 
 
-
-
 -- Student Accounts
-SELECT id INTO @currentParentId FROM security_functions WHERE category = 'General' AND module = 'Students' AND name = 'Students' LIMIT 1;
 INSERT INTO `security_functions` (`id`, `name`, `controller`, `module`, `category`, `parent_id`, `_view`, `_edit`, `_add`, `_delete`, `_execute`, `order`, `visible`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES 
-	(2020, 'Accounts', 'Students', 'Students', 'General', @currentParentId, 'Accounts.view', 'Accounts.edit', NULL, '', NULL, 2020, 1, NULL, NULL, 1, NOW());
+	(2020, 'Accounts', 'Students', 'Students', 'General', 2000, 'Accounts.view', 'Accounts.edit', NULL, '', NULL, 2020, 1, NULL, NULL, 1, NOW());
 
 -- Staff Accounts
-SELECT id INTO @currentParentId FROM security_functions WHERE category = 'General' AND module = 'Staff' AND name = 'Staff' LIMIT 1;
 INSERT INTO `security_functions` (`id`, `name`, `controller`, `module`, `category`, `parent_id`, `_view`, `_edit`, `_add`, `_delete`, `_execute`, `order`, `visible`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES 
-	(3027, 'Accounts', 'Staff', 'Staff', 'General', @currentParentId, 'Accounts.view', 'Accounts.edit', NULL, '', NULL, 3027, 1, NULL, NULL, 1, NOW());
+	(3027, 'Accounts', 'Staff', 'Staff', 'General', 3000, 'Accounts.view', 'Accounts.edit', NULL, '', NULL, 3027, 1, NULL, NULL, 1, NOW());
 
 -- Guardian Accounts
-SELECT id INTO @currentParentId FROM security_functions WHERE category = 'General' AND module = 'Guardians' AND name = 'Guardians' LIMIT 1;
 INSERT INTO `security_functions` (`id`, `name`, `controller`, `module`, `category`, `parent_id`, `_view`, `_edit`, `_add`, `_delete`, `_execute`, `order`, `visible`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES 
-	(4008, 'Accounts', 'Guardians', 'Guardians', 'General', @currentParentId, 'Accounts.view', 'Accounts.edit', NULL, '', NULL, 4008, 1, NULL, NULL, 1, NOW());
+	(4008, 'Accounts', 'Guardians', 'Guardians', 'General', 4000, 'Accounts.view', 'Accounts.edit', NULL, '', NULL, 4008, 1, NULL, NULL, 1, NOW());
 
 -- Institution Student Accounts
-SELECT id INTO @currentParentId FROM security_functions WHERE category = 'Students' AND module = 'Institutions' AND name = 'Students' LIMIT 1;
 INSERT INTO `security_functions` (`id`, `name`, `controller`, `module`, `category`, `parent_id`, `_view`, `_edit`, `_add`, `_delete`, `_execute`, `order`, `visible`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES 
-	(1032, 'Accounts', 'Institutions', 'Institutions', 'Students', @currentParentId, 'StudentAccount.view', 'StudentAccount.edit', NULL, '', NULL, 1032, 1, NULL, NULL, 1, NOW());
+	(1032, 'Accounts', 'Institutions', 'Institutions', 'Students', 1012, 'StudentAccount.view', 'StudentAccount.edit', NULL, '', NULL, 1032, 1, NULL, NULL, 1, NOW());
 
 -- Institution Staff Accounts
-SELECT id INTO @currentParentId FROM security_functions WHERE category = 'Staff' AND module = 'Institutions' AND name = 'Staff' LIMIT 1;
 INSERT INTO `security_functions` (`id`, `name`, `controller`, `module`, `category`, `parent_id`, `_view`, `_edit`, `_add`, `_delete`, `_execute`, `order`, `visible`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES 
-	(1033, 'Accounts', 'Institutions', 'Institutions', 'Staff', @currentParentId, 'StaffAccount.view', 'StaffAccount.edit', NULL, '', NULL, 1033, 1, NULL, NULL, 1, NOW());
+	(1033, 'Accounts', 'Institutions', 'Institutions', 'Staff', 1016, 'StaffAccount.view', 'StaffAccount.edit', NULL, '', NULL, 1033, 1, NULL, NULL, 1, NOW());
 
 -- User Accounts
-SELECT id INTO @currentParentId FROM security_functions WHERE category = 'Security' AND module = 'Administration' AND name = 'Users' LIMIT 1;
 INSERT INTO `security_functions` (`id`, `name`, `controller`, `module`, `category`, `parent_id`, `_view`, `_edit`, `_add`, `_delete`, `_execute`, `order`, `visible`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES 
-	(5042, 'Accounts', 'Securities', 'Administration', 'Security', @currentParentId, 'Accounts.view', 'Accounts.edit', NULL, '', NULL, 5042, 1, NULL, NULL, 1, NOW());
+	(5042, 'Accounts', 'Securities', 'Administration', 'Security', 5022, 'Accounts.view', 'Accounts.edit', NULL, '', NULL, 5042, 1, NULL, NULL, 1, NOW());
 
 	-- SELECT category FROM security_functions group by category
 	-- SELECT module FROM security_functions group by module
