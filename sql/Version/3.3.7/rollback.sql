7217d671add0c46ce0739d704cc770a0b8f4ef63
jzheng <jzheng@kordit.com>
2015-12-03 09:01:35 +0800

    3.3.7 - updated PHPOE-2086 sql

--- sql/Version/3.3.7/rollback.sql
+++ sql/Version/3.3.7/rollback.sql
@@ -32,6 +32,22 @@ ALTER TABLE `z2086_survey_forms` RENAME `survey_forms`;
 DROP TABLE `survey_questions`;
 ALTER TABLE `z2086_survey_questions` RENAME `survey_questions`;
 
+DELETE FROM `security_functions` WHERE `id` IN (1024, 1025);
+
+INSERT INTO `security_functions` SELECT * FROM `z_2086_security_functions`;
+
+DROP TABLE `z_2086_security_functions`;
+
+-- security_role_functions
+DELETE FROM `security_role_functions` WHERE `id` IN (
+    SELECT `id` FROM `z_2086_security_role_functions`
+);
+
+INSERT INTO `security_role_functions`
+SELECT * FROM `z_2086_security_role_functions`;
+
+DROP TABLE `z_2086_security_role_functions`;
+
 DELETE FROM `db_patches` WHERE `issue`='PHPOE-2086';
 
 UPDATE config_items SET value = '3.3.6' WHERE code = 'db_version';
