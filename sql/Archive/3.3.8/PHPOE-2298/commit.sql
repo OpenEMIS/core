86a39cc1b6c3c4882a703dc06e99d473d8b83069
Chiang Hao Long <hchiang@kordit.com>
2015-12-14 16:00:57 +0800

    PHPOE-2298 - Fixed a missing character in the execute permission

--- sql/Archive/3.3.8/PHPOE-2298/commit.sql
+++ sql/Archive/3.3.8/PHPOE-2298/commit.sql
@@ -7,4 +7,4 @@ CREATE TABLE `z_2298_security_functions` LIKE `security_functions`;
 INSERT INTO `z_2298_security_functions` 
 SELECT * FROM `security_functions` WHERE `id` = 1025;
 
-UPDATE `security_functions` SET `name`='Surveys', `_view`='Surveys.index|Surveys.view', `_edit`='Surveys.edit', `_delete`=NULL, `_execute` = 'Survey.excel' WHERE `id`=1025;
+UPDATE `security_functions` SET `name`='Surveys', `_view`='Surveys.index|Surveys.view', `_edit`='Surveys.edit', `_delete`=NULL, `_execute` = 'Surveys.excel' WHERE `id`=1025;
