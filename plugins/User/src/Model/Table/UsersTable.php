514b5d4897a2982f7aef8654f37dba6f69864020
Malcolm Lee <mlee@kordit.com>
2015-12-16 10:45:20 +0800

    PHPOE-2321 Implemented User/Password field validation fail if spaces

--- plugins/User/src/Model/Table/UsersTable.php
+++ plugins/User/src/Model/Table/UsersTable.php
@@ -387,6 +387,9 @@ class UsersTable extends AppTable {
 			->allowEmpty('username')
 			->allowEmpty('password')
 			->add('password' , [
+				'ruleNoSpaces' => [
+					'rule' => 'checkNoSpaces'
+				],
 				'ruleMinLength' => [
 					'rule' => ['minLength', 6]
 				]
@@ -431,6 +434,9 @@ class UsersTable extends AppTable {
 			->allowEmpty('username')
 			->allowEmpty('password')
 			->add('password' , [
+				'ruleNoSpaces' => [
+					'rule' => 'checkNoSpaces'
+				],
 				'ruleMinLength' => [
 					'rule' => ['minLength', 6]
 				]
@@ -450,6 +456,7 @@ class UsersTable extends AppTable {
 		$thisModel->setValidationCode('openemis_no.ruleUnique', 'User.Users');
 		$thisModel->setValidationCode('username.ruleUnique', 'User.Users');
 		$thisModel->setValidationCode('username.ruleAlphanumeric', 'User.Users');
+		$thisModel->setValidationCode('password.ruleNoSpaces', 'User.Users');
 		$thisModel->setValidationCode('password.ruleMinLength', 'User.Users');
 		$thisModel->setValidationCode('date_of_birth.ruleValidDate', 'User.Users');
 		return $validator;
