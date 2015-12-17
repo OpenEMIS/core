c14715276514a52da5167d998a6808ddd4f0d76d
Hanafi Ahmat <hahmat@kordit.com>
2015-12-17 17:16:33 +0800

    PHPOE-1800	- Added a behavior file to search by users default national id

---
+++ plugins/User/src/Model/Behavior/AdvancedNationalIdSearchBehavior.php
@@ -0,0 +1,41 @@
+<?php
+namespace User\Model\Behavior;
+
+use Cake\ORM\Behavior;
+use Cake\ORM\Query;
+use Cake\ORM\Entity;
+use Cake\ORM\TableRegistry;
+
+class AdvancedNationalIdSearchBehavior extends Behavior {
+	// AdvancedNationalIdSearch Behavior
+	public function addNationalIdSearchConditions(Query $query, $options = []) {
+		if (array_key_exists('searchTerm', $options)) {
+			$search = $options['searchTerm'];
+		}
+
+		$alias = $this->_table->alias();
+		if (array_key_exists('alias', $options)) {
+			$alias = $options['alias'];
+		}
+
+		if (!empty($search)) {
+			$IdentityTypes = TableRegistry::get('FieldOption.IdentityTypes');
+			$default_identity_type = $IdentityTypes->getDefaultValue();
+			$searchString = '%' . $search . '%';
+			$query->join([
+						[
+							'type' => 'LEFT',
+							'table' => 'user_identities',
+							'alias' => 'Identities',
+							'conditions' => [
+								'Identities.security_user_id = '. $alias . '.id',
+								'Identities.identity_type_id = '. $default_identity_type
+							]
+						]
+					]);
+			$query->orWhere(['Identities.number LIKE' =>  $searchString]);
+		}
+		return $query;
+	}	
+
+}
