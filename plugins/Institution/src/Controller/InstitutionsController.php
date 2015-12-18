3dfc4379ea593826d3a9e2de680f8288653ebfcd
Jeff Zheng <jzheng@kordit.com>
2015-12-18 10:33:02 +0800

    3.4.1 - updated variables

--- plugins/Institution/src/Controller/InstitutionsController.php
+++ plugins/Institution/src/Controller/InstitutionsController.php
@@ -133,6 +133,7 @@ class InstitutionsController extends AppController  {
 	public function onInitialize(Event $event, Table $model) {
 		if (!is_null($this->activeObj)) {
 			$session = $this->request->session();
+			$institutionId = $session->read('Institution.Institutions.id');
 			$action = false;
 			$params = $this->request->params;
 			if (isset($params['pass'][0])) {
@@ -176,19 +177,19 @@ class InstitutionsController extends AppController  {
 
 			if ($model->hasField('institution_id') && !in_array($model->alias(), ['TransferRequests'])) {
 				$model->fields['institution_id']['type'] = 'hidden';
-				$model->fields['institution_id']['value'] = $session->read('Institution.Institutions.id');
+				$model->fields['institution_id']['value'] = $institutionId;
 			}
 
 			if ($model->hasField('institution_id') && !is_null($this->activeObj)) {
 				$model->fields['institution_id']['type'] = 'hidden';
-				$model->fields['institution_id']['value'] = $session->read('Institution.Institutions.id');
+				$model->fields['institution_id']['value'] = $institutionId;
 
 				if (count($this->request->pass) > 1) {
 					$modelId = $this->request->pass[1]; // id of the sub model
 
 					$exists = $model->exists([
 						$model->aliasField($model->primaryKey()) => $modelId,
-						$model->aliasField('institution_id') => $this->activeObj->id
+						$model->aliasField('institution_id') => $institutionId
 					]);
 				
 					/**
