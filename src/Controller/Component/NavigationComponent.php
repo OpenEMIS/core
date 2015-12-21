1c7e2bc0ce383f40e49a8d742f67310be2df221e
Chiang Hao Long <hchiang@kordit.com>
2015-12-21 13:30:21 +0800

    PHPOE-2193 - Fixed a bug of which the session is not set when the user edit the institution information directly from the index page.

--- src/Controller/Component/NavigationComponent.php
+++ src/Controller/Component/NavigationComponent.php
@@ -223,6 +223,7 @@ class NavigationComponent extends Component {
 				'title' => 'Institutions', 
 				'icon' => '<span><i class="fa kd-institutions"></i></span>',
 				'params' => ['plugin' => 'Institution'],
+				'selected' => ['Institutions.add']
 			],
 
 			'Directories.index' => [
@@ -268,7 +269,7 @@ class NavigationComponent extends Component {
 				'Institutions.view' => [
 					'title' => 'Overview', 
 					'parent' => 'Institution.General', 
-					'selected' => ['Institutions.view'], 
+					'selected' => ['Institutions.view', 'Institutions.edit'], 
 					'params' => ['plugin' => 'Institution']
 				],
 
