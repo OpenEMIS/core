06914e4827ffa7215dd01270277b75efab941a5a
jzheng <jzheng@kordit.com>
2015-12-23 09:17:40 +0800

    3.4.2 - updated version sql

--- sql/Version/3.4.2/rollback.sql
+++ sql/Version/3.4.2/rollback.sql
@@ -11,3 +11,34 @@ DELETE FROM labels WHERE field = 'institution_network_connectivity_id';
 
 -- db_patches
 DELETE FROM `db_patches` WHERE `issue` = 'PHPOE-1961';
+-- PHPOE-2319
+UPDATE `security_functions` set `_execute` = 'ImportInstitutionSurveys.add|ImportInstitutionSurveys.template|ImportInstitutionSurveys.results' where `id`=1024;
+
+DELETE FROM `security_functions` 
+WHERE 
+`id` = 1034,
+`id` = 1035,
+`id` = 1036,
+`id` = 1037,
+`id` = 7036
+;
+
+DELETE FROM `db_patches` WHERE `issue`='PHPOE-2319';
+
+-- PHPOE-2359
+
+DROP TABLE `import_mapping`;
+ALTER TABLE `z_2359_import_mapping` RENAME `import_mapping`;
+
+DELETE FROM `labels` WHERE `module`='Imports' AND `field`='institution_id';
+
+DELETE FROM `db_patches` WHERE `issue`='PHPOE-2359';
+
+-- PHPOE-2366
+
+DROP TABLE `import_mapping`;
+ALTER TABLE `z_2366_import_mapping` RENAME `import_mapping`;
+
+DELETE FROM `db_patches` WHERE `issue`='PHPOE-2366';
+
+UPDATE config_items SET value = '3.4.1' WHERE code = 'db_version';
