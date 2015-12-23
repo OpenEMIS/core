f357d9218366031f0eb8a9f7844cef142b767408
Hanafi Ahmat <hahmat@kordit.com>
2015-12-23 15:02:16 +0800

    PHPOE-2319	- Added missing permission for downloadFailed pages PHPOE-2359	- Changed sql values InstitutionSiteStudentAbsences to InstitutionStudentAbsences, and InstitutionSiteSurveys to InstitutionSurveys

--- sql/Version/3.4.2/commit.sql
+++ sql/Version/3.4.2/commit.sql
@@ -124,6 +124,20 @@ INSERT INTO `security_functions` (`id`, `name`, `controller`, `module`, `categor
 (7036, 'Import Users', 'Directories', 'Directory', 'General', '7000', NULL, NULL, NULL, NULL, 'ImportUsers.add|ImportUsers.results|ImportUsers.template', '7036', '1', '1', NOW())
 ;
 
+--
+-- PHPOE-2319 PATCH --
+--
+UPDATE `security_functions` SET `_execute`='ImportInstitutions.add|ImportInstitutions.template|ImportInstitutions.results|ImportInstitutions.downloadFailed' WHERE `id`=1034;
+UPDATE `security_functions` SET `_execute`='ImportStudents.add|ImportStudents.template|ImportStudents.results|ImportStudents.downloadFailed' WHERE `id`=1035;
+UPDATE `security_functions` SET `_execute`='ImportStudentAttendances.add|ImportStudentAttendances.template|ImportStudentAttendances.results|ImportStudentAttendances.downloadFailed' WHERE `id`=1036;
+UPDATE `security_functions` SET `_execute`='ImportStaffAttendances.add|ImportStaffAttendances.template|ImportStaffAttendances.results|ImportStaffAttendances.downloadFailed' WHERE `id`=1037;
+UPDATE `security_functions` SET `_execute`='ImportUsers.add|ImportUsers.template|ImportUsers.results|ImportUsers.downloadFailed' WHERE `id`=7036;
+UPDATE `security_functions` SET `_execute`='ImportInstitutionSurveys.add|ImportInstitutionSurveys.template|ImportInstitutionSurveys.results|ImportInstitutionSurveys.downloadFailed' WHERE `id`=1024;
+--
+-- END PHPOE-2319 PATCH --
+--
+
+
 -- PHPOE-2359
 INSERT INTO `db_patches` VALUES ('PHPOE-2359', NOW());
 
@@ -134,8 +148,8 @@ UPDATE `import_mapping` set `model`=concat('Institution.', `model`) where `model
 UPDATE `import_mapping` set `model`=concat('Student.', `model`) where `model`='Students';
 UPDATE `import_mapping` set `model`=concat('Staff.', `model`) where `model`='Staff';
 UPDATE `import_mapping` set `model`=concat('Institution.', `model`) where `model`='StaffAbsences';
-UPDATE `import_mapping` set `model`=concat('Institution.', `model`) where `model`='InstitutionSiteStudentAbsences';
-UPDATE `import_mapping` set `model`=concat('Institution.', `model`) where `model`='InstitutionSiteSurveys';
+UPDATE `import_mapping` set `model`=concat('Institution.', `model`) where `model`='InstitutionStudentAbsences';
+UPDATE `import_mapping` set `model`=concat('Institution.', `model`) where `model`='InstitutionSurveys';
 
 
 INSERT INTO `import_mapping` (`model`, `column_name`, `description`, `order`, `foreign_key`, `lookup_plugin`, `lookup_model`, `lookup_column`) 
