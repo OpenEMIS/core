6a195f4c786c6900d611273fce3e7a819772f991
Hanafi Ahmat <hahmat@kordit.com>
2015-12-30 10:02:46 +0800

    PHPOE-2403	- Added validation for "class" field 			- Added checks in ImportBehavior to allow optional foreign key

--- sql/Patch/PHPOE-2403/commit.sql
+++ sql/Patch/PHPOE-2403/commit.sql
@@ -11,49 +11,12 @@ DROP TABLE IF EXISTS `z_1463_import_mapping`;
 CREATE TABLE `z_2403_import_mapping` LIKE `import_mapping`;
 INSERT INTO `z_2403_import_mapping` SELECT * FROM `import_mapping`;
 
-UPDATE `import_mapping` 
-	SET `id`=80
-	WHERE `model`='User.Users' AND `column_name`='account_type';
-UPDATE `import_mapping` 
-	SET `id`=79
-	WHERE `model`='User.Users' AND `column_name`='birthplace_area_id';
-UPDATE `import_mapping` 
-	SET `id`=78
-	WHERE `model`='User.Users' AND `column_name`='address_area_id';
-UPDATE `import_mapping` 
-	SET `id`=77
-	WHERE `model`='User.Users' AND `column_name`='postal_code';
-UPDATE `import_mapping` 
-	SET `id`=76
-	WHERE `model`='User.Users' AND `column_name`='address';
-UPDATE `import_mapping` 
-	SET `id`=75
-	WHERE `model`='User.Users' AND `column_name`='date_of_birth';
-UPDATE `import_mapping` 
-	SET `id`=74
-	WHERE `model`='User.Users' AND `column_name`='gender_id';
-UPDATE `import_mapping` 
-	SET `id`=73
-	WHERE `model`='User.Users' AND `column_name`='preferred_name';
-UPDATE `import_mapping` 
-	SET `id`=72
-	WHERE `model`='User.Users' AND `column_name`='last_name';
-UPDATE `import_mapping` 
-	SET `id`=71
-	WHERE `model`='User.Users' AND `column_name`='third_name';
-UPDATE `import_mapping` 
-	SET `id`=70
-	WHERE `model`='User.Users' AND `column_name`='middle_name';
-UPDATE `import_mapping` 
-	SET `id`=69
-	WHERE `model`='User.Users' AND `column_name`='first_name';
-UPDATE `import_mapping` 
-	SET `id`=68
-	WHERE `model`='User.Users' AND `column_name`='openemis_no';
+update `import_mapping` set `id` = `id`+1000 where `id` > 66;
+update `import_mapping` set `id` = `id`-999 where `id` > 1000;
 
 INSERT INTO `import_mapping` 
 (`id`, `model`, `column_name`, `description`, `order`, `foreign_key`, `lookup_plugin`, `lookup_model`, `lookup_column`) VALUES
-(67, 'Institution.Students', 'class', 'Code', 5, 2, 'Institution', 'InstitutionSections', 'id');
+(67, 'Institution.Students', 'class', 'Code (Optional)', 5, 2, 'Institution', 'InstitutionSections', 'id');
 
 ALTER TABLE `import_mapping`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=81;
