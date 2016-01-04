b14c8dde6e53d6960983aeeca9af6f6cf4a2a6a4
Malcolm Lee <mlee@kordit.com>
2016-01-04 17:14:44 +0800

    PHPOE-1808 Replace "Openemis no" with OpenEMIS ID

--- sql/Patch/PHPOE-1808/commit.sql
+++ sql/Patch/PHPOE-1808/commit.sql
@@ -1,8 +1,8 @@
-INSERT INTO `db_patches` VALUES ('PHPOE-1808');
+INSERT INTO `db_patches` VALUES ('PHPOE-1808', NOW());
 
 -- labels
-INSERT INTO `labels` (`id`, `module`, `field`, `module_name`, `field_name`, `created_user_id`, `created`) VALUES 
-(uuid(), 'StudentUser', 'openemis_no', 'Institutions -> Students -> General', 'OpenEMIS ID', 1, NOW()),
-(uuid(), 'StaffUser', 'openemis_no', 'Institutions -> Staff -> General', 'OpenEMIS ID', 1, NOW()),
-(uuid(), 'StudentAttendances', 'openemis_no', 'Institutions -> Students -> General', 'OpenEMIS ID', 1, NOW()),
-(uuid(), 'StaffAttendances', 'openemis_no', 'Institutions -> Staff -> General', 'OpenEMIS ID', 1, NOW());
+INSERT INTO `labels` (`id`, `module`, `field`, `module_name`, `field_name`, `created_user_id`, `created`) VALUES (uuid(), 'StudentUser', 'openemis_no', 'Institutions -> Students -> General', 'OpenEMIS ID', 1, NOW());
+INSERT INTO `labels` (`id`, `module`, `field`, `module_name`, `field_name`, `created_user_id`, `created`) VALUES (uuid(), 'StaffUser', 'openemis_no', 'Institutions -> Staff -> General', 'OpenEMIS ID', 1, NOW());
+INSERT INTO `labels` (`id`, `module`, `field`, `module_name`, `field_name`, `created_user_id`, `created`) VALUES (uuid(), 'StudentAttendances', 'openemis_no', 'Institutions -> Students -> General', 'OpenEMIS ID', 1, NOW());
+INSERT INTO `labels` (`id`, `module`, `field`, `module_name`, `field_name`, `created_user_id`, `created`) VALUES (uuid(), 'StaffAttendances', 'openemis_no', 'Institutions -> Staff -> General', 'OpenEMIS ID', 1, NOW());
+INSERT INTO `labels` (`id`, `module`, `field`, `module_name`, `field_name`, `created_user_id`, `created`) VALUES (uuid(), 'Directories', 'openemis_no', 'Institutions -> Staff -> General', 'OpenEMIS ID', 1, NOW());
