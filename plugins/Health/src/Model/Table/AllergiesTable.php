930720ff5a3632a9d865f3f3538acab5ffd7c907
Thed Kuok Shiun <kuokshiun@hotmail.com>
2016-01-05 10:46:10 +0800

    PHPOE-1227 - Working on Health under Directories, Institution > Students and Institution > Staff.

--- plugins/Health/src/Model/Table/AllergiesTable.php
+++ plugins/Health/src/Model/Table/AllergiesTable.php
@@ -1,10 +1,15 @@
 <?php
 namespace Health\Model\Table;
 
+use Cake\ORM\Entity;
+use Cake\Network\Request;
 use Cake\Event\Event;
 use App\Model\Table\AppTable;
+use App\Model\Traits\OptionsTrait;
 
 class AllergiesTable extends AppTable {
+	use OptionsTrait;
+
 	public function initialize(array $config) {
 		$this->table('user_health_allergies');
 		parent::initialize($config);
@@ -15,7 +20,26 @@ class AllergiesTable extends AppTable {
 		$this->addBehavior('Health.Health');
 	}
 
-	public function addEditBeforeAction(Event $event) {
+	public function onGetSevere(Event $event, Entity $entity) {
+		$severeOptions = $this->getSelectOptions('general.yesno');
+		return $severeOptions[$entity->severe];
+	}
+
+	public function viewAfterAction(Event $event, Entity $entity) {
+		$this->setupFields($entity);
+	}
+
+	public function addEditAfterAction(Event $event, Entity $entity) {
+		$this->setupFields($entity);
+	}
+
+	public function onUpdateFieldSevere(Event $event, array $attr, $action, Request $request) {
+		$attr['options'] = $this->getSelectOptions('general.yesno');
+		return $attr;
+	}
+
+	private function setupFields(Entity $entity) {
+		$this->ControllerAction->field('severe');
 		$this->ControllerAction->field('health_allergy_type_id', ['type' => 'select']);
 	}
 }
