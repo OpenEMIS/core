0f66051928fa80beb56ede1c66e14e0ff8563b09
Hanafi Ahmat <hahmat@kordit.com>
2016-01-07 18:33:57 +0800

    PHPOE-832	- Enabled filtering by institution type

--- plugins/Map/src/Controller/MapsController.php
+++ plugins/Map/src/Controller/MapsController.php
@@ -4,6 +4,7 @@ namespace Map\Controller;
 use Map\Controller\AppController;
 use Cake\Event\Event;
 use Cake\ORM\TableRegistry;
+use Cake\Collection\Collection;
 
 class MapsController extends AppController {
 	public function initialize() {
@@ -19,15 +20,68 @@ class MapsController extends AppController {
 
 	public function index() {
 		$model = TableRegistry::get('Institution.Institutions');
-		$institutions = $model->find('all')->limit('15')->toArray();
+		$institutions = $model->find('all')
+							->contain(['Types'])
+							->limit(1000)
+							// ->order(['code' => 'DESC'])
+							->toArray()
+							;
 		$InstitutionTypes = TableRegistry::get('Institution.Types');
 		$institutionTypes = $InstitutionTypes->getList()->toArray();
-// pr($institutionTypes);
-		$this->set('model', $model);
-		$this->set('institutions', $institutions);
-		$this->set('institutionTypes', $institutionTypes);
+
+		$institutionByType = [];
+		$institutionTypeTotal = [];
+		$institutionCollection = new Collection($institutions);
+		$totalKnownType = 0;
+		foreach ($institutionTypes as $id=>$type) {
+
+			$filtered = $institutionCollection->filter(function ($value, $key, $iterator) use ($id) {
+			    return $value->institution_type_id == $id;
+			});
+			// pr($filtered->toArray());die;
+			$institutionByType[$id] = $filtered->toArray();
+			$institutionTypeTotal[$id] = count($filtered->toArray());
+			$totalKnownType = $totalKnownType + $institutionTypeTotal[$id];
+		}
+		$institutionTypeTotal['default'] = count($institutions) - $totalKnownType;
+
+		// pr($institutionTypeTotal);die;
+		$iconColors = [
+			'#2b5cac',
+			'#ff8338',
+			'#049487',
+			'#9d2979',
+			'#3cc4cb',
+			'#d7206f',
+			'#c2cb95',
+			'#93a8b2',
+			'#338ff8',
+			'#487049',
+			'#9799d2',
+			'#4cb3cc',
+			'#2044ab',
+			'#06fd72',
+			'#48781b'
+		];
+
+		$this->set( 'model', $model );
+		$this->set( 'iconColors', $iconColors );
+		$this->set( 'institutions', $institutions );
+		$this->set( 'totalInstitutions', count($institutions) );
+		$this->set( 'institutionTypes', $institutionTypes );
+		$this->set( 'institutionByType', $institutionByType );
+		$this->set( 'institutionTypeTotal', $institutionTypeTotal );
 	}
 
-
+	public function getMarkersData() {
+		$model = TableRegistry::get('Institution.Institutions');
+		$institutions = $model->find('all')->contain(['Types'])->limit(1000)->order(['code' => 'DESC'])->toArray();
+		// $this->ControllerAction->autoRender = false;
+		$this->autoRender = false;
+		$this->getView()->layout = 'ajax';
+		$this->response->type('json');
+		$json = json_encode($institutions);
+		$this->response->body($json);
+	}
 
 }
