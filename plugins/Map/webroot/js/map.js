197066220e3be956359a72f3e8105f574dc6335b
Hanafi Ahmat <hahmat@kordit.com>
2016-01-08 11:08:49 +0800

    PHPOE-832	- Populates institutions without a valid institution type as a "default" type and visible as "Unknown" 			- Dump institutions categorized by institution type as json formatted string on index.ctp within a javascript scope 			- Modified the css to remove the map outer shadows and changed its widith and height 			- Changed the logic to hide and show markers on the map

--- plugins/Map/webroot/js/map.js
+++ plugins/Map/webroot/js/map.js
@@ -6,7 +6,8 @@ $(document).ready(function() {
 var Map = {
     gmap: '',
     iconStyles: [],
-    markers: [],
+    markersByType: [],
+    visibleMarkerTypes: [],
     init: function() {
 
         var config = {
@@ -15,7 +16,7 @@ var Map = {
             zoom: $('#config .default .zoom').data('value')
         };
 
-        this.gmap = new GMaps({
+        Map.gmap = new GMaps({
             el: '#map',
             lat: config.lat,
             lng: config.lng,
@@ -24,10 +25,9 @@ var Map = {
             mapTypeControl: false
         });
         
-        Map.markers = markers;
-        // console.log(Map.markers);
+        Map.markersByType = institutionsData;
         Map.setupIcons();
-        Map.addMarkers();
+        Map.setupMarkers();
         $(".institution-type :checkbox").on("ifToggled", Map.redrawMarkers);
     },
     setupIcons: function () {
@@ -55,42 +55,14 @@ var Map = {
         });
 
     },
-    addMarkers: function () {
-        for (type in Map.markers) {
-            // console.log(type);
-            var typeSelected = $(':checkbox[value="'+ type +'"]').prop('checked');
-
-            if (typeSelected) {
-                for (record in Map.markers[type]) {
-                    
-                    var institution = Map.markers[type][record];
-                    if (institution.latitude != null && institution.longitude != null && institution.latitude !== false && institution.longitude !== false ) {
-                        Map.drawMarker(institution);
-                    } else {
-                        // console.log(institution);
-                        // needs google console account to use this feature without daily limit
-                        // GMaps.geocode({
-                        //     address: institution.address + ' ' + institution.postal_code,
-                        //     callback: function(results, status) {
-                        //         console.log(status);
-                        //         if (status == 'OK') {
-                        //             var latlng = results[0].geometry.location;
-                        //             institution.latitude = latlng.lat();
-                        //             institution.longitude = latlng.lng();
-                        //             Map.drawMarker(institution);
-                        //         } else {
-                                    console.log(institution.name +' does not have the necessary data: '+institution.latitude);
-                        //         }
-                        //     }
-                        // });
-                    }
-                }
-                // return false;
-            }
+    setupMarkers: function () {
+        for (type in Map.markersByType) {
+            Map.visibleMarkerTypes[type]=[];
+            Map.addMarkersByType(type);            
         }
     },
     drawMarker: function (institution) {
-        if (institution.latitude != '' && institution.longitude != '') {
+        // if (institution.latitude != '' && institution.longitude != '') {
 
             var type = institution.institution_type_id;
             if (typeof Map.iconStyles[type] === 'undefined') {
@@ -103,11 +75,12 @@ var Map = {
             infoContent.find('.name').html(institution.name);
             infoContent.find('.code').html(institution.code);
             infoContent.find('a').prop( 'href', infoContent.find('a').prop('href') + '/' + institution.id );
-            Map.gmap.addMarker({
+            var marker = Map.gmap.addMarker({
                 id: institution.code,
                 lat: institution.latitude,
                 lng: institution.longitude,
-                title: institution.name,
+                name: institution.name,
+                title: institution.name + ' (' +institution.code+ ')',
                 animation: google.maps.Animation.DROP,
                 infoWindow: {
                     content: infoContent.html()
@@ -115,89 +88,73 @@ var Map = {
                 icon: icon
             });
 
-        }
-    },
-    // addMarkers: function () {
-
-    //     if (Map.markers.length == 0) {
-    //         console.log();console.log($('#config .plugin-url').prop('href'));return false;
-    //         $.ajax({
-    //             type: 'GET',
-    //             dataType: 'json',
-    //             url: $('#config .plugin-url').prop('href') + '/getMarkersData',
-    //             success: function(data, textStatus, jqXHR) {
-    //                 console.log(data);
-
-    //             },
-    //             error: function(jqXHR, textStatus, errorThrown) {
-                    
-    //             }
-    //         });
-
-    //     } else {
-    //         console.log('markers not empty');
-    //         console.log(Map.markers);
-    //         console.log(Map.markers.length);
-    //         return false;
-    //         // Map.drawMarkers();
-    //     }
-
-    // },
-    oldAddMarkers: function () {
-        
-       $('#markers .marker').each(function(index, value) {
-
-            var type = $(this).find('.type').html();
-            var typeSelected = $(':checkbox[value="'+ type +'"]').prop('checked');
-
-            if (typeSelected) {
-                var lat = $(this).find('.latitude').html();
-                var lng = $(this).find('.longitude').html();
-
-                if (lat == '' || lng == '') {
-                    GMaps.geocode({
-                        address: $(this).find('.address').html() + ' ' + $(this).find('.postal_code').html(),
-                        callback: function(results, status) {
-                            if (status == 'OK') {
-                                var latlng = results[0].geometry.location;
-                                lat = latlng.lat();
-                                lng = latlng.lng();
-                            }
-                        }
-                    });
-                }
-
-                if (typeof Map.iconStyles[type] === 'undefined') {
-                    var icon = Map.iconStyles['default'];
-                } else {
-                    var icon = Map.iconStyles[type];
-                }
-
-                var infoContent = $('#config .marker-body').clone();
-                infoContent.find('.name').html($(this).find('.name').html());
-                infoContent.find('.code').html($(this).find('.code').html());
-                infoContent.find('a').prop( 'href', infoContent.find('a').prop('href') + '/' + $(this).data('id') );
-                this.gmap.addMarker({
-                    id: $(this).find('.code').html(),
-                    lat: lat,
-                    lng: lng,
-                    title: $(this).find('.name').html(),
-                    animation: google.maps.Animation.DROP,
-                    infoWindow: {
-                        content: infoContent.html()
-                    },
-                    icon: icon
-                });
-            }
-            
-        });
-
+            return marker;
+        // } else {
+        //     return null;
+        // }
     },
     redrawMarkers: function () {
         console.log('redrawing markers');
-        Map.gmap.removeMarkers();
-        Map.addMarkers();
+
+        var type = $(this).val();
+        var checked = $(this).prop('checked');
+        if (checked) {
+            Map.toggleMarkersByType(type, true);
+        } else {
+            Map.toggleMarkersByType(type, false);
+        }
+
+    },
+    toggleMarkersByType: function (type, show) {
+        console.log('toggleMarkersByType: '+show);
+
+        $.each(Map.visibleMarkerTypes[type], function(index, obj) {
+            // obj.setVisible(show);
+
+            // unable to use below method since
+            if (show) {
+                var map = Map.gmap.map;
+            } else {
+                var map = null;
+            }
+            obj.setMap(map);
+        });
+    },
+    addMarkersByType: function (type) {
+        console.log('addMarkersByType');
+
+        for (record in Map.markersByType[type]) {
+            
+            var institution = Map.markersByType[type][record];
+            if (institution.latitude != null && institution.longitude != null && institution.latitude !== false && institution.longitude !== false ) {
+
+                var marker = Map.drawMarker(institution);
+                if (marker != null) {
+                    Map.visibleMarkerTypes[type].push(marker);
+                }
+
+            } else {
+                // console.log(institution);
+                // needs google console account to use this feature without daily limit
+                // GMaps.geocode({
+                //     address: institution.address + ' ' + institution.postal_code,
+                //     callback: function(results, status) {
+                //         console.log(status);
+                //         if (status == 'OK') {
+                //             var latlng = results[0].geometry.location;
+                //             institution.latitude = latlng.lat();
+                //             institution.longitude = latlng.lng();
+                //             Map.drawMarker(institution);
+                //         } else {
+                            console.log(institution.name +' does not have the necessary data: '+institution.latitude);
+                //         }
+                //     }
+                // });
+            }
+
+        }
     }
 
 }
 
+        
