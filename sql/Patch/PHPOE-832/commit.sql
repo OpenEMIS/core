0ecc3a218e7418053785c8803cbccafaaac393cb
Hanafi Ahmat <hahmat@kordit.com>
2016-01-11 16:57:20 +0800

    PHPOE-832	- Changed the default value of lat and long to the center of the globe

--- sql/Patch/PHPOE-832/commit.sql
+++ sql/Patch/PHPOE-832/commit.sql
@@ -9,6 +9,6 @@ INSERT INTO `z_832_config_items` SELECT * FROM `config_items`;
 
 INSERT INTO `config_items`
 (`id`, `name`, `code`, `type`, `label`, `value`, `default_value`, `editable`, `visible`, `field_type`, `option_type`, `created_user_id`, `created`) VALUES
-(NULL, 'Google Map Center Longitude', 'google_map_center_longitude', 'Map', 'Google Map Center Longitude', '-88.53892', '-88.53892', '1', '1', '', '', 1, NOW()),
-(NULL, 'Google Map Center Latitude', 'google_map_center_latitude', 'Map', 'Google Map Center Latitude', '17.048877', '17.048877', '1', '1', '', '', 1, NOW()),
+(NULL, 'Google Map Center Longitude', 'google_map_center_longitude', 'Map', 'Google Map Center Longitude', '0', '0', '1', '1', '', '', 1, NOW()),
+(NULL, 'Google Map Center Latitude', 'google_map_center_latitude', 'Map', 'Google Map Center Latitude', '0', '0', '1', '1', '', '', 1, NOW()),
 (NULL, 'Google Map Zoom', 'google_map_zoom', 'Map', 'Google Map Zoom', '10', '10', '1', '1', '', '', 1, NOW());
