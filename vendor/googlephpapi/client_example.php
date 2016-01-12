0838821dafcbf491721954844c2e0a57d49c2dba
Chiang Hao Long <hchiang@kordit.com>
2016-01-12 10:52:48 +0800

    POIDN-3 - added google vendor files

---
+++ vendor/googlephpapi/client_example.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ *
+ * Copyright MITRE 2012
+ *
+ * OpenIDConnectClient for PHP5
+ * Author: Michael Jett <mjett@mitre.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+require "../../autoload.php";
+
+$oidc = new OpenIDConnectClient('https://accounts.google.com/o/oauth2/v2/auth',
+                                '503787316191-mubbf5go6rfj34pc2ns89v5tc01489sr.apps.googleusercontent.com',
+                                'rQJfcNUXOmMhFrD091BlCJxS');
+$oidc->authenticate();
+$name = $oidc->requestUserInfo('given_name');
+
+?>
+
+<html>
+<head>
+    <title>Example OpenID Connect Client Use</title>
+    <style>
+        body {
+            font-family: 'Lucida Grande', Verdana, Arial, sans-serif;
+        }
+    </style>
+</head>
+<body>
+
+    <div>
+        Hello <?php echo $name; ?>
+    </div>
+
+</body>
+</html>
+
