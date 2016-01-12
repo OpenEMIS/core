08e6579907ffd58540a5eaf4699079f34af66d55
Malcolm Lee <mlee@kordit.com>
2016-01-12 17:02:11 +0800

    PHPOE-2433 - Fixed - Admin > Role > Security - Permissions > Institutions > Students & Staff export permissions missing

---
+++ sql/Patch/PHPOE-2433/commit.sql
@@ -0,0 +1,6 @@
+UPDATE security_functions SET _execute = concat(_execute, '|StudentUser.excel')  WHERE id = 1012;
+UPDATE security_functions SET _execute = concat(_execute, '|StaffUser.excel')  WHERE id = 1016;
+
+-- SELECT * FROM security_functions WHERE id IN (1012, 1016);
+
+INSERT INTO `db_patches` VALUES ('PHPOE-2433', NOW());
