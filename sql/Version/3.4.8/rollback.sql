bb38cb98fedc0b599e07fb59dde976f4dacb1ffa
Jeff Zheng <jzheng@kordit.com>
2016-01-13 13:28:12 +0800

    3.4.8 - updated commit sql

---
+++ sql/Version/3.4.8/rollback.sql
@@ -0,0 +1,11 @@
+-- db_patches
+DELETE FROM `db_patches` WHERE `issue` = 'PHPOE-2338';	
+
+DROP TABLE institution_section_students;
+RENAME TABLE z2338_institution_section_students TO institution_section_students;
+
+-- db_patches
+DELETE FROM `db_patches` WHERE `issue` = 'PHPOE-2435';
+
+-- security_functions
+DELETE FROM `security_functions` WHERE `id` = 1038;
