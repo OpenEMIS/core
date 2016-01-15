5a450a9414b2029f771f3e7c7152212d921f32f3
Jeff Zheng <jzheng@kordit.com>
2016-01-15 13:32:10 +0800

    PHPOE-2436 - Deployed to 3.4.9

--- sql/Version/3.4.9/commit.sql
+++ sql/Version/3.4.9/commit.sql
@@ -79,4 +79,31 @@ INSERT INTO `z_2463_institution_section_students` SELECT * FROM `institution_sec
 
 ALTER TABLE `institution_section_students` CHANGE `id` `id` CHAR(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
 
+-- 
+-- PHPOE-2436
+--
+
+INSERT INTO `db_patches` VALUES ('PHPOE-2436', NOW());
+
+CREATE TABLE `z_2436_import_mapping` LIKE `import_mapping`;
+INSERT INTO `z_2436_import_mapping` SELECT * FROM `import_mapping`;
+
+DELETE FROM `import_mapping` WHERE `id`=11 or `id`=13;
+UPDATE `import_mapping` SET `order` = `order`+1000 WHERE `order` > 14 and `model`='Institution.Institutions';
+UPDATE `import_mapping` SET `order` = `order`-1001 WHERE `order` > 1000 and `model`='Institution.Institutions';
+UPDATE `import_mapping` SET `order` = `order`+1000 WHERE `order` > 12 and `model`='Institution.Institutions';
+UPDATE `import_mapping` SET `order` = `order`-1001 WHERE `order` > 1000 and `model`='Institution.Institutions';
+
+UPDATE `import_mapping` SET `id` = `id`+1000 WHERE `id` > 12;
+UPDATE `import_mapping` SET `id` = `id`-1001 WHERE `id` > 1000;
+
+UPDATE `import_mapping` SET `id` = `id`+1000 WHERE `id` > 10;
+UPDATE `import_mapping` SET `id` = `id`-1001 WHERE `id` > 1000;
+
+UPDATE `import_mapping` SET `description` = '( DD/MM/YYYY )' WHERE `column_name` LIKE '%date%';
+
+DROP TABLE IF EXISTS `z_2403_import_mapping`;
+DROP TABLE IF EXISTS `z_2421_import_mapping`;
+
+
 UPDATE config_items SET value = '3.4.9' WHERE code = 'db_version';
