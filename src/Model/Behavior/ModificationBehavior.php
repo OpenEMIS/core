6e8c5eb4c2cd5d195ef9e433506868b25072e2f0
Chiang Hao Long <hchiang@kordit.com>
2016-01-19 17:27:57 +0800

    PHPOE-2485 - Shifted beforeSave function in AppTable to a behaviour to enable event propagation.

---
+++ src/Model/Behavior/ModificationBehavior.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Model\Behavior;
+
+use Cake\Event\Event;
+use Cake\ORM\Behavior;
+use Cake\ORM\Entity;
+use ArrayObject;
+
+class ModificationBehavior extends Behavior
+{
+    public function implementedEvents() {
+        $events = parent::implementedEvents();
+        return $events;
+    }
+
+    public function beforeSave(Event $event, Entity $entity, ArrayObject $options) {
+        $schema = $this->_table->schema();
+        $columns = $schema->columns();
+        
+        $userId = null;
+        if (isset($_SESSION['Auth']) && isset($_SESSION['Auth']['User'])) {
+            $userId = $_SESSION['Auth']['User']['id'];
+        }
+        if (!is_null($userId)) {
+            if (!$entity->isNew()) {
+                if (in_array('modified_user_id', $columns)) {
+                    $entity->modified_user_id = $userId;
+                }
+            } else {
+                if (in_array('created_user_id', $columns)) {
+                    $entity->created_user_id = $userId;
+                }
+            }
+        }
+    } 
+}
