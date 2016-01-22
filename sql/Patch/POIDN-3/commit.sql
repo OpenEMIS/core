e72a3b1cd568e7017cecc6230362d3336cc0d94b
Chiang Hao Long <hchiang@kordit.com>
2016-01-22 17:19:55 +0800

    POIDN-3 - Fixed a bug in the patch file where one of the column name is wrong

--- sql/Patch/POIDN-3/commit.sql
+++ sql/Patch/POIDN-3/commit.sql
@@ -8,7 +8,7 @@ INSERT INTO `config_item_options` (`option_type`, `option`, `value`, `order`, `v
 CREATE TABLE `authentication_type_attributes` (
   `id` CHAR(36) NOT NULL COMMENT '',
   `authentication_type` VARCHAR(50) NOT NULL COMMENT '',
-  `authentication_field` VARCHAR(50) NOT NULL COMMENT '',
+  `attribute_field` VARCHAR(50) NOT NULL COMMENT '',
   `attribute_name` VARCHAR(50) NOT NULL COMMENT '',
   `value` VARCHAR(100) NULL COMMENT '',
   PRIMARY KEY (`id`)  COMMENT '');
