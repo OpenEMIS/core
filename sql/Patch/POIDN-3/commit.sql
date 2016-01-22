43422453098c3a9e09b96f62bd01c023fdd6087b
Chiang Hao Long <hchiang@kordit.com>
2016-01-22 17:06:43 +0800

    POIDN-3 - Fixed patch file for commit and rollback

--- sql/Patch/POIDN-3/commit.sql
+++ sql/Patch/POIDN-3/commit.sql
@@ -1,6 +1,9 @@
 -- db_patches
 INSERT INTO `db_patches` VALUES ('POIDN-3', NOW());
 
+-- config_item_options
+INSERT INTO `config_item_options` (`option_type`, `option`, `value`, `order`, `visible`) VALUES ('authentication_type', 'Google', 'Google', 3, 1);
+
 -- authentication_type_attributes
 CREATE TABLE `authentication_type_attributes` (
   `id` CHAR(36) NOT NULL COMMENT '',
--- sql/Patch/POIDN-3/rollback.sql
+++ sql/Patch/POIDN-3/rollback.sql
@@ -1,3 +1,9 @@
+-- config_item_options
+DELETE FROM `config_item_options` WHERE `option_type` = 'authentication_type' AND `option` = 'Google';
+
+-- config_items
+UPDATE `config_items` SET `value` = 'Local' WHERE `type` = 'Authentication' AND `code` = 'authentication_type';
+
 -- authentication_type_attributes
 DROP TABLE `authentication_type_attributes`;
 
