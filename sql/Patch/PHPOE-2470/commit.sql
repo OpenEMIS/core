b60cec5fb43dc73dfd98b7fd8ad3ce7f8d684c95
Chiang Hao Long <hchiang@kordit.com>
2016-01-25 17:08:15 +0800

    PHPOE-2470 - Change Standards Report to extend from reports table. Amended patch file to use a single reports table to manage both standard and custom reports.

--- sql/Patch/PHPOE-2470/commit.sql
+++ sql/Patch/PHPOE-2470/commit.sql
@@ -1,30 +1,15 @@
 -- db_patches
 INSERT INTO `db_patches` VALUES ('PHPOE-2470', NOW());
 
--- standard_reports
-CREATE TABLE `standard_reports` (
+-- reports
+CREATE TABLE `reports` (
   `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
   `name` VARCHAR(50) NOT NULL COMMENT '',
   `target` VARCHAR(50) NULL COMMENT '',
-  `main_table` VARCHAR(50) NOT NULL COMMENT '',
-  `table_alias` VARCHAR(50) NOT NULL COMMENT '',
   `query` TEXT NOT NULL COMMENT '',
+  `report_type` INT NOT NULL COMMENT '1 => Standard Report, 2 => Custom Report',
   `modified_user_id` INT NULL COMMENT '',
   `modified` INT NULL COMMENT '',
   `created_user_id` INT NOT NULL COMMENT '',
   `created` INT NOT NULL COMMENT '',
-  PRIMARY KEY (`id`)  COMMENT '');
-
--- custom_reports
-CREATE TABLE `custom_reports` (
-  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
-  `name` VARCHAR(50) NOT NULL COMMENT '',
-  `target` VARCHAR(50) NULL COMMENT '',
-  `main_table` VARCHAR(50) NOT NULL COMMENT '',
-  `table_alias` VARCHAR(50) NOT NULL COMMENT '',
-  `query` TEXT NOT NULL COMMENT '',
-  `modified_user_id` INT NULL COMMENT '',
-  `modified` INT NULL COMMENT '',
-  `created_user_id` INT NOT NULL COMMENT '',
-  `created` INT NOT NULL COMMENT '',
-  PRIMARY KEY (`id`)  COMMENT '');
+  PRIMARY KEY (`id`));
---
+++ sql/Patch/PHPOE-2470/rollback.sql
@@ -0,0 +1,6 @@
+-- reports
+DROP TABLE `reports`;
+
+-- db_patches
+DELETE FROM `db_patches` WHERE `issue` = 'PHPOE-2470';
+
