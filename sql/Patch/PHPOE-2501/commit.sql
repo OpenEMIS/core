5265fc02dcdf4112137b16ab9f9a2f6d65fa76e5
Chiang Hao Long <hchiang@kordit.com>
2016-01-27 15:28:16 +0800

    PHPOE-2501 - Fixed student status on institution class page.

--- sql/Patch/PHPOE-2501/commit.sql
+++ sql/Patch/PHPOE-2501/commit.sql
@@ -5,6 +5,22 @@ INSERT INTO `db_patches` VALUES ('PHPOE-2501', NOW());
 ALTER TABLE `institution_section_students` 
 ADD COLUMN `student_status_id` INT NULL COMMENT '' AFTER `education_grade_id`;
 
+CREATE TABLE `z_2501_institution_section_students` LIKE `institution_section_students`;
+
+INSERT INTO `z_2501_institution_section_students`
+SELECT `institution_section_students`.*  FROM  `institution_section_students` 
+INNER JOIN `institution_sections` ON `institution_sections`.`id` = `institution_section_students`.`institution_section_id`
+INNER JOIN `institution_students` ON `institution_sections`.`academic_period_id` = `institution_students`.`academic_period_id`
+    AND `institution_students`.`student_id` = `institution_section_students`.`student_id`
+    AND `institution_section_students`.`education_grade_id` <> `institution_students`.`education_grade_id`
+GROUP BY `institution_section_students`.`id`;
+
+UPDATE `institution_section_students`
+INNER JOIN `institution_sections` ON `institution_sections`.`id` = `institution_section_students`.`institution_section_id`
+INNER JOIN `institution_students` ON `institution_sections`.`academic_period_id` = `institution_students`.`academic_period_id`
+    AND `institution_students`.`student_id` = `institution_section_students`.`student_id`
+SET `institution_section_students`.`education_grade_id` = `institution_students`.`education_grade_id`;
+
 UPDATE `institution_section_students` 
 INNER JOIN `institution_sections` ON `institution_sections`.`id` = `institution_section_students`.`institution_section_id`
 INNER JOIN `institution_students` ON `institution_sections`.`academic_period_id` = `institution_students`.`academic_period_id`
