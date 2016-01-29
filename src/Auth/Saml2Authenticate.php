42b1864cfb996e2ad4ea1dacd603ba95daa6cfc1
Chiang Hao Long <hchiang@kordit.com>
2016-01-29 19:27:01 +0800

    POCOR-2526 - Added vendor file for onelogin saml2 plugin. Remove simplesamlphp vendor file. Added Saml2Authenticate and Saml2AuthComponent with basic authentication using email username.

---
+++ src/Auth/Saml2Authenticate.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Auth;
+
+use Cake\Network\Request;
+use Cake\Network\Response;
+use Cake\Auth\BaseAuthenticate;
+use Cake\ORM\TableRegistry;
+
+class Saml2Authenticate extends BaseAuthenticate
+{
+
+    public function authenticate(Request $request, Response $response)
+    {   
+        $session = $request->session();
+
+        if ($session->check('Saml2.userAttribute')) {
+            $userAttribute = $session->read('Saml2.userAttribute');
+            $email = $userAttribute['User.email'][0];
+            $emailArray = explode('@', $email);
+            $userName = $emailArray[0];
+            $hostedDomain = $emailArray[1];
+            $isFound = $this->_findUser($userName);
+            if ($isFound) {
+                return $isFound;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+        
+
+        // if ($session->check('Google.tokenData')) {
+        // 	$tokenData = $session->read('Google.tokenData');
+        //     // Remove session for the token data after it has been used.
+        //     $session->delete('Google.tokenData');
+        //     $email = $tokenData['payload']['email'];
+        //     $emailArray = explode('@', $tokenData['payload']['email']);
+        //     $userName = $emailArray[0];
+        //     $hostedDomain = $emailArray[1];
+        //     $configHD = $session->read('Google.hostedDomain');
+        //     // Additional check just in case the hosted domain check fail
+        //     if (!empty($configHD) && strtolower($hostedDomain) != strtolower($configHD)) {
+        //     	return false;
+        //     } else {
+        //     	$isFound = $this->_findUser($userName);
+
+	       //      // If user is found login, if not do create user logic
+	       //      if ($isFound) {
+	       //          return $isFound;
+	       //      } else {
+	       //      	$client = $session->read('Google.client');
+	       //          $ServiceOAuth2Object = new \Google_Service_Oauth2($client);
+	       //  		$me = $ServiceOAuth2Object->userinfo->get();
+        //             $userInfo = [
+        //                 'id' => $me->getId(),
+        //                 'firstName' => $me->getFamilyName(),
+        //                 'lastName' => $me->getGivenName(),
+        //                 'gender' => $me->getGender(),
+        //                 'email' => $me->getEmail(),
+        //                 'verifiedEmail' => $me->getVerifiedEmail(),
+        //                 'locale' => $me->getLocale(),
+        //                 'link' => $me->getLink(),
+        //                 'picture' => $me->getPicture(),
+        //             ];
+
+        //             $User = TableRegistry::get('User.Users');
+        //             $event = $User->dispatchEvent('Model.Auth.createAuthorisedUser', [$userName, $userInfo], $this);
+        //             return $this->_findUser($event->result);
+	       //      }
+        //     }
+        // } else {
+        //     return false;
+        // }
+    }
+}
