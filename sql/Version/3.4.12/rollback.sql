f39a4e3f493260dd1a2c97a2beda82aa6db87e66
Jeff Zheng <jzheng@kordit.com>
2016-01-29 17:05:25 +0800

    3.4.12 - updated version commits

--- sql/Version/3.4.12/rollback.sql
+++ sql/Version/3.4.12/rollback.sql
@@ -57,4 +57,18 @@ DELETE FROM `security_functions` WHERE `id` IN ('7045', '7046');
 -- db_patches
 DELETE FROM `db_patches` WHERE `issue` = 'POCOR-2497';
 
+-- institution_section_students
+UPDATE `institution_section_students`
+INNER JOIN `z_2501_institution_section_students` ON `z_2501_institution_section_students`.`id` = `institution_section_students`.`id`
+SET `institution_section_students`.`education_grade_id` = `z_2501_institution_section_students`.`education_grade_id`;
+
+DROP TABLE `z_2501_institution_section_students`;
+
+ALTER TABLE `institution_section_students` 
+DROP COLUMN `student_status_id`,
+DROP INDEX `student_status_id` ;
+
+-- db_patches
+DELETE FROM `db_patches` WHERE `issue` = 'POCOR-2501';
+
 UPDATE config_items SET value = '3.4.11' WHERE code = 'db_version';
