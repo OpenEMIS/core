952b776a594dec2c6962147adebfdc9964dc497a
Jeff Zheng <jzheng@kordit.com>
2016-01-29 15:20:04 +0800

    3.4.12 - updated changelog and commit sql

--- sql/Version/3.4.12/commit.sql
+++ sql/Version/3.4.12/commit.sql
@@ -191,5 +191,25 @@ WHERE `fov`.`field_option_id`=(SELECT `fo`.`id` FROM `field_options` as `fo` WHE
 
 UPDATE `field_option_values` as `fov` set `fov`.`visible`=0 WHERE `fov`.`field_option_id`=(SELECT `fo`.`id` FROM `field_options` as `fo` WHERE `fo`.`code` = 'Types'); 
 
+INSERT INTO `db_patches` VALUES ('POCOR-2465', NOW());
+
+UPDATE security_functions SET _execute = 'Visits.download' WHERE id = 1027;
+
+--
+-- POCOR-2497
+--
+
+-- db_patches
+INSERT INTO `db_patches` VALUES ('POCOR-2497', NOW());
+
+-- security_functions
+UPDATE `security_functions` SET `_view` = 'StaffClasses.index' WHERE `security_functions`.`id` = 7023;
+
+INSERT INTO `security_functions` 
+(`id`, `name`, `controller`, `module`, `category`, `parent_id`, `_view`, `_edit`, `_add`, `_delete`, `_execute`, `order`, `visible`, `created_user_id`, `created`) 
+VALUES 
+('7045', 'Contacts', 'Directories', 'Directory', 'General', '7000', 'Contacts.index|Contacts.view', 'Contacts.edit', 'Contacts.add', 'Contacts.remove', NULL, '7045', '1', '1', NOW()),
+('7046', 'Training Needs', 'Directories', 'Directory', 'Staff - Training', '7000', 'TrainingNeeds.index|TrainingNeeds.view', 'TrainingNeeds.edit', 'TrainingNeeds.add', 'TrainingNeeds.remove', NULL, '7046', '1', '1', NOW());
+
 
 UPDATE config_items SET value = '3.4.12' WHERE code = 'db_version';
