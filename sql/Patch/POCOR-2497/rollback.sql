691fa6c593056c9b3ad62f75aba0a7406dce039e
Jeff Zheng <jzheng@kordit.com>
2016-01-29 15:10:55 +0800

    POCOR-2497 - updated commit/rollback sql

--- sql/Patch/POCOR-2497/rollback.sql
+++ sql/Patch/POCOR-2497/rollback.sql
@@ -1,5 +1,5 @@
 --
--- PHPOE-2497
+-- POCOR-2497
 --
 
 -- security_functions
@@ -7,4 +7,4 @@ UPDATE `security_functions` SET `_view` = 'StaffSubjects.index' WHERE `security_
 DELETE FROM `security_functions` WHERE `id` IN ('7045', '7046');
 
 -- db_patches
-DELETE FROM `db_patches` WHERE `issue` = 'PHPOE-2497';
+DELETE FROM `db_patches` WHERE `issue` = 'POCOR-2497';
