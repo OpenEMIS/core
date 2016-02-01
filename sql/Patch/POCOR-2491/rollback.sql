1b05dd64c21b473c038ad51cac5499f463bbd874
Hanafi Ahmat <hahmat@kordit.com>
2016-02-01 16:16:22 +0800

    POCOR-2491	- Updates commit and rollback sql files

--- sql/Patch/POCOR-2491/rollback.sql
+++ sql/Patch/POCOR-2491/rollback.sql
@@ -1,6 +1,6 @@
 --
--- PHPOE-2491
+-- POCOR-2491
 --
 UPDATE `security_functions` SET `_edit` = 'Sessions.edit' WHERE `security_functions`.`id` = 5040;
 
-DELETE FROM `db_patches` WHERE `issue` = 'PHPOE-2491';
+DELETE FROM `db_patches` WHERE `issue` = 'POCOR-2491';
