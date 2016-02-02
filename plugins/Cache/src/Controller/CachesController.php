54c3590e1d9230d723875fe8f1d93fb0a4daddd7
Jeff Zheng <jzheng@kordit.com>
2016-02-02 11:48:20 +0800

    added Cache clear function

---
+++ plugins/Cache/src/Controller/CachesController.php
@@ -0,0 +1,28 @@
+<?php
+namespace Cache\Controller;
+
+use Cache\Controller\AppController;
+use Cake\Event\Event;
+use Cake\Cache\Cache;
+
+class CachesController extends AppController {
+	public function initialize() {
+		parent::initialize();
+	}
+
+	public function beforeFilter(Event $event) {
+		parent::beforeFilter($event);
+		$this->Auth->allow(['clear']);
+	}
+
+	public function clear() {
+		$this->autoRender = false;
+		Cache::clearGroup('labels');
+		Cache::clearGroup('_cake_core_');
+		Cache::clearGroup('_cake_model_');
+		Cache::gc();
+		Cache::gc('labels');
+		Cache::gc('_cake_core_');
+		Cache::gc('_cake_model_');
+	}
+}
