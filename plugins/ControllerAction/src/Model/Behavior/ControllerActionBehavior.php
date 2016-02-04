aebd151652ee12468b6ca9d9975ed18428ea8581
Thed Kuok Shiun <kuokshiun@hotmail.com>
2016-02-04 15:39:00 +0800

    POCOR-2535 - Changes to ControllerActionV4.

--- plugins/ControllerAction/src/Model/Behavior/ControllerActionBehavior.php
+++ plugins/ControllerAction/src/Model/Behavior/ControllerActionBehavior.php
@@ -83,6 +83,9 @@ class ControllerActionBehavior extends Behavior {
 			if ($actions[$action] !== false) {
 				$behavior = ucfirst($action);
 				if (file_exists(__DIR__ . DS . $behavior . 'Behavior.php')) {
+					if ($action == 'reorder' && !$this->isColumnExists($value['orderField'])) {
+						continue;
+					}
 					if (is_array($value)) {
 						if ($model->hasBehavior($behavior)) {
 							$model->removeBehavior($behavior);
@@ -92,7 +95,6 @@ class ControllerActionBehavior extends Behavior {
 					} else {
 						$model->addBehavior('ControllerAction.' . $behavior);
 					}
-					
 				}
 			}
 		}
@@ -135,6 +137,14 @@ class ControllerActionBehavior extends Behavior {
 		}
 		
 		$this->_table->fields = $fields;
+	}
+
+	public function isColumnExists($field) {
+		$model = $this->_table;
+		$schema = $model->schema();
+		$columns = $schema->columns();
+
+		return in_array($field, $columns);
 	}
 
 	public function actions($action=null) {
