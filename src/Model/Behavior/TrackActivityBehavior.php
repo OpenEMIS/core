a484a329dd4852b765d31158452920c60db19cda
Chiang Hao Long <hchiang@kordit.com>
2016-02-11 18:03:08 +0800

    POCOR-2304 - Updated column name to be institution_id

--- src/Model/Behavior/TrackActivityBehavior.php
+++ src/Model/Behavior/TrackActivityBehavior.php
@@ -155,7 +155,7 @@ class TrackActivityBehavior extends Behavior {
 			$activity['field_type'] = '';
 			$activity['old_value'] = '';
 			$activity['new_value'] = '';
-			$activity['institution_site_id'] = $id;
+			$activity['institution_id'] = $id;
 			$activity['operation'] = 'delete';
 
 			$ActivityModel = TableRegistry::get($this->config('target'));
