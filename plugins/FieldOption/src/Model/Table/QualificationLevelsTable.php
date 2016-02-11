d96a18731631571cc4e8ffc127962cca35fa8117
Malcolm Lee <mlee@kordit.com>
2016-02-11 11:30:27 +0800

    POCOR-1968 Converted qualification levels and specializations to v4

--- plugins/FieldOption/src/Model/Table/QualificationLevelsTable.php
+++ plugins/FieldOption/src/Model/Table/QualificationLevelsTable.php
@@ -1,14 +1,16 @@
 <?php
 namespace FieldOption\Model\Table;
 
-use App\Model\Table\AppTable;
+use App\Model\Table\ControllerActionTable;
 use Cake\Validation\Validator;
 
-class QualificationLevelsTable extends AppTable {
+class QualificationLevelsTable extends ControllerActionTable {
 	public function initialize(array $config) {
 		$this->addBehavior('ControllerAction.FieldOption');
 		$this->table('qualification_levels');
 		parent::initialize($config);
 		$this->hasMany('Qualifications', ['className' => 'Staff.Qualifications', 'foreignKey' => 'qualification_level_id']);
+
+		$this->behaviors()->get('ControllerAction')->config('actions.remove', 'transfer');
 	}
 }
--- plugins/FieldOption/src/Model/Traits/FieldOptionsTrait.php
+++ plugins/FieldOption/src/Model/Traits/FieldOptionsTrait.php
@@ -7,7 +7,5 @@ trait FieldOptionsTrait {
 		'User.ContactTypes' => 			['parentModel' => 'User.ContactOptions', 'foreignKey' => 'contact_option_id', 'behavior' => 'Filter'], 
 		'FieldOption.Banks' => 		['behavior' => 'Display'],
 		'FieldOption.Countries' => 	['behavior' => 'Countries'],
-		'FieldOption.QualificationSpecialisations' => 	['parentModel' => 'FieldOption.QualificationSpecialisations', 'behavior' => 'QualificationSpecialisations'],
-		'FieldOption.QualificationLevels' => 	['behavior' => 'Display'],
 	];
 }
