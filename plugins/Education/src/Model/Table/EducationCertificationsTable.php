2d5590930ef8cc2be0f67d909a14378af1328e4a
Chiang Hao Long <hchiang@kordit.com>
2016-02-12 18:16:38 +0800

    POCOR-2265 - Added RestrictAssociatedDelete Behaviour to restrict delete if there is associated records. Amend Education Certification, Education Subjects, Education Field of studies, Education Programme orientations, setup behaviour, Education Controller to use controller action version 4. Fixed a bug in the openemis behaviour to fix the $extra['entity'] not found issue.

--- plugins/Education/src/Model/Table/EducationCertificationsTable.php
+++ plugins/Education/src/Model/Table/EducationCertificationsTable.php
@@ -1,13 +1,14 @@
 <?php
 namespace Education\Model\Table;
 
-use App\Model\Table\AppTable;
+use App\Model\Table\ControllerActionTable;
 
-class EducationCertificationsTable extends AppTable {
+class EducationCertificationsTable extends ControllerActionTable {
 	public function initialize(array $config) {
 		parent::initialize($config);
 		$this->addBehavior('Education.Setup');
 		$this->hasMany('EducationProgrammes', ['className' => 'Education.EducationProgrammes', 'cascadeCallbacks' => true]);
-		$this->addBehavior('RestrictAssoicatedDelete');
+
+		$this->behaviors()->get('ControllerAction')->config('actions.remove', 'transfer');
 	}
 }
