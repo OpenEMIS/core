d95196a92534b0294092f7c081202e09b367979b
Thed Kuok Shiun <kuokshiun@hotmail.com>
2016-02-18 18:44:19 +0800

    POCOR-2445 - Working on InfrastructureCustomFields.

--- plugins/Infrastructure/src/Model/Table/InfrastructureCustomFieldValuesTable.php
+++ plugins/Infrastructure/src/Model/Table/InfrastructureCustomFieldValuesTable.php
@@ -5,6 +5,7 @@ use CustomField\Model\Table\CustomFieldValuesTable;
 
 class InfrastructureCustomFieldValuesTable extends CustomFieldValuesTable {
 	public function initialize(array $config) {
+		$config['extra'] = ['scope' => 'infrastructure_custom_field_id'];
 		parent::initialize($config);
 		$this->belongsTo('CustomFields', ['className' => 'Infrastructure.InfrastructureCustomFields', 'foreignKey' => 'infrastructure_custom_field_id']);
 		$this->belongsTo('CustomRecords', ['className' => 'Institution.InstitutionInfrastructures', 'foreignKey' => 'institution_infrastructure_id']);
