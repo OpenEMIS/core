c63ba63ac51867dd82a2f87d4cd34f2ded660473
Thed Kuok Shiun <kuokshiun@hotmail.com>
2016-02-18 20:10:13 +0800

    POCOR-2445 - Working on InstitutionSurveys.

--- plugins/Infrastructure/src/Model/Table/InfrastructureCustomFieldValuesTable.php
+++ plugins/Infrastructure/src/Model/Table/InfrastructureCustomFieldValuesTable.php
@@ -4,8 +4,9 @@ namespace Infrastructure\Model\Table;
 use CustomField\Model\Table\CustomFieldValuesTable;
 
 class InfrastructureCustomFieldValuesTable extends CustomFieldValuesTable {
+	protected $extra = ['scope' => 'infrastructure_custom_field_id'];
+
 	public function initialize(array $config) {
-		$config['extra'] = ['scope' => 'infrastructure_custom_field_id'];
 		parent::initialize($config);
 		$this->belongsTo('CustomFields', ['className' => 'Infrastructure.InfrastructureCustomFields', 'foreignKey' => 'infrastructure_custom_field_id']);
 		$this->belongsTo('CustomRecords', ['className' => 'Institution.InstitutionInfrastructures', 'foreignKey' => 'institution_infrastructure_id']);
--- plugins/StudentCustomField/src/Model/Table/StudentCustomFieldValuesTable.php
+++ plugins/StudentCustomField/src/Model/Table/StudentCustomFieldValuesTable.php
@@ -4,8 +4,9 @@ namespace StudentCustomField\Model\Table;
 use CustomField\Model\Table\CustomFieldValuesTable;
 
 class StudentCustomFieldValuesTable extends CustomFieldValuesTable {
+	protected $extra = ['scope' => 'student_custom_field_id'];
+
 	public function initialize(array $config) {
-		$config['extra'] = ['scope' => 'student_custom_field_id'];
 		parent::initialize($config);
 		$this->belongsTo('CustomFields', ['className' => 'StudentCustomField.StudentCustomFields', 'foreignKey' => 'student_custom_field_id']);
 		$this->belongsTo('CustomRecords', ['className' => 'User.Users', 'foreignKey' => 'student_id']);
