55495fd23fd4c6ae491727a25a2401018a98796c
Chiang Hao Long <hchiang@kordit.com>
2016-02-20 01:22:14 +0800

    POCOR-2601 - Removed backup table from patch file. Updated patch file to delete many to many security group areas record.

--- sql/Patch/POCOR-2601/commit.sql
+++ sql/Patch/POCOR-2601/commit.sql
@@ -3,33 +3,15 @@ INSERT INTO `db_patches` VALUES ('POCOR-2601', NOW());
 
 -- security_users
 
-CREATE TABLE `z_2601_security_users` LIKE `security_users`;
-
-INSERT IGNORE INTO `z_2601_security_users`
-SELECT * FROM `security_users`
-WHERE `security_users`.`address_area_id` NOT IN (SELECT id FROM area_administratives);
-
-INSERT IGNORE INTO `z_2601_security_users`
-SELECT * FROM `security_users` 
-WHERE `security_users`.`birthplace_area_id` NOT IN (SELECT id FROM area_administratives);
-
 UPDATE `security_users`
 SET `address_area_id` = NULL
 WHERE `security_users`.`address_area_id` NOT IN (SELECT id FROM area_administratives);
 
 UPDATE `security_users`
-SET `address_area_id` = NULL
+SET `birthplace_area_id` = NULL
 WHERE `security_users`.`birthplace_area_id` NOT IN (SELECT id FROM area_administratives);
 
 -- institutions
-
-CREATE TABLE `z_2601_institutions` LIKE institutions;
-
-INSERT IGNORE INTO `z_2601_institutions`
-SELECT * FROM institutions WHERE area_id NOT IN (SELECT id FROM areas);
-
-INSERT IGNORE INTO `z_2601_institutions`
-SELECT * FROM institutions WHERE area_administrative_id NOT IN (SELECT id FROM area_administratives);
 
 UPDATE `institutions`
 SET area_id = (SELECT id FROM areas WHERE parent_id = -1 LIMIT 1)
@@ -40,12 +22,6 @@ SET area_administrative_id = NULL
 WHERE area_administrative_id NOT IN (SELECT id FROM area_administratives);
 
 -- security_group_areas
-CREATE TABLE `z_2601_security_group_areas` LIKE `security_group_areas`;
-
-INSERT INTO `z_2601_security_group_areas`
-SELECT * FROM `security_group_areas` WHERE area_id NOT IN (SELECT id FROM areas);
-
-UPDATE `security_group_areas`
-SET area_id = (SELECT id FROM areas WHERE parent_id = -1 LIMIT 1)
+DELETE FROM `security_group_areas`
 WHERE area_id NOT IN (SELECT id FROM areas);
 
