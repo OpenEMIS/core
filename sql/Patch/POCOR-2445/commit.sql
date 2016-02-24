02fb625212d211f1997a7dd90e4c8ff2299152cb
Thed Kuok Shiun <kuokshiun@hotmail.com>
2016-02-24 19:48:18 +0800

    POCOR-2445 - Fix bugs of custom fields not appear when add institution.

--- sql/Patch/POCOR-2445/commit.sql
+++ sql/Patch/POCOR-2445/commit.sql
@@ -17,4 +17,10 @@ ALTER TABLE `staff_custom_forms_fields` ADD `section` VARCHAR(250) CHARACTER SET
 ALTER TABLE `infrastructure_custom_forms_fields` ADD `section` VARCHAR(250) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `infrastructure_custom_field_id`;
 
 -- custom_modules
-UPDATE `custom_modules` SET `supported_field_types` = 'TEXT,NUMBER,TEXTAREA,DROPDOWN,CHECKBOX,TABLE', `visible` = 1 WHERE `model` = 'Institution.InstitutionInfrastructures';
+UPDATE `custom_modules` SET `supported_field_types` = 'TEXT,NUMBER,TEXTAREA,DROPDOWN,CHECKBOX,TABLE,DATE,TIME,STUDENT_LIST' WHERE `model` = 'Institution.Institutions';
+UPDATE `custom_modules` SET `supported_field_types` = 'TEXT,NUMBER,TEXTAREA,DROPDOWN,CHECKBOX,TABLE,DATE,TIME' WHERE `model` = 'Student.Students';
+UPDATE `custom_modules` SET `supported_field_types` = 'TEXT,NUMBER,TEXTAREA,DROPDOWN,CHECKBOX,TABLE,DATE,TIME' WHERE `model` = 'Staff.Staff';
+UPDATE `custom_modules` SET `supported_field_types` = 'TEXT,NUMBER,TEXTAREA,DROPDOWN,CHECKBOX,TABLE,DATE,TIME', `visible` = 1 WHERE `model` = 'Institution.InstitutionInfrastructures';
+
+-- custom_field_types
+UPDATE `custom_field_types` SET `visible` = 1 WHERE `code` IN ('DATE', 'TIME');
