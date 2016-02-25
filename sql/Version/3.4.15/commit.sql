589a7caae613cc8b1a9a81bef23b2b493ebd87f6
Steven Hasanuddin <shasanuddin@kordit.com>
2016-02-25 16:26:02 +0800

    3.4.14 - Update commit.sql to include POCOR-2445

--- sql/Version/3.4.15/commit.sql
+++ sql/Version/3.4.15/commit.sql
@@ -27,6 +27,35 @@ DELETE FROM `security_group_areas`
 WHERE area_id NOT IN (SELECT id FROM areas);
 
 
+-- POCOR-2445
+-- db_patches
+INSERT INTO `db_patches` VALUES ('POCOR-2445', NOW());
+
+-- add params column to xxx_custom_fields
+ALTER TABLE `custom_fields` ADD `params` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `is_unique`;
+ALTER TABLE `institution_custom_fields` ADD `params` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `is_unique`;
+ALTER TABLE `student_custom_fields` ADD `params` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `is_unique`;
+ALTER TABLE `staff_custom_fields` ADD `params` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `is_unique`;
+ALTER TABLE `infrastructure_custom_fields` ADD `params` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `is_unique`;
+ALTER TABLE `survey_questions` ADD `params` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `is_unique`;
+
+-- add section column to xxx_custom_forms_fields
+ALTER TABLE `custom_forms_fields` ADD `section` VARCHAR(250) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `custom_field_id`;
+ALTER TABLE `institution_custom_forms_fields` ADD `section` VARCHAR(250) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `institution_custom_field_id`;
+ALTER TABLE `student_custom_forms_fields` ADD `section` VARCHAR(250) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `student_custom_field_id`;
+ALTER TABLE `staff_custom_forms_fields` ADD `section` VARCHAR(250) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `staff_custom_field_id`;
+ALTER TABLE `infrastructure_custom_forms_fields` ADD `section` VARCHAR(250) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `infrastructure_custom_field_id`;
+
+-- custom_modules
+UPDATE `custom_modules` SET `supported_field_types` = 'TEXT,NUMBER,TEXTAREA,DROPDOWN,CHECKBOX,TABLE,DATE,TIME,STUDENT_LIST' WHERE `model` = 'Institution.Institutions';
+UPDATE `custom_modules` SET `supported_field_types` = 'TEXT,NUMBER,TEXTAREA,DROPDOWN,CHECKBOX,TABLE,DATE,TIME' WHERE `model` = 'Student.Students';
+UPDATE `custom_modules` SET `supported_field_types` = 'TEXT,NUMBER,TEXTAREA,DROPDOWN,CHECKBOX,TABLE,DATE,TIME' WHERE `model` = 'Staff.Staff';
+UPDATE `custom_modules` SET `supported_field_types` = 'TEXT,NUMBER,TEXTAREA,DROPDOWN,CHECKBOX,TABLE,DATE,TIME', `visible` = 1 WHERE `model` = 'Institution.InstitutionInfrastructures';
+
+-- custom_field_types
+UPDATE `custom_field_types` SET `visible` = 1 WHERE `code` IN ('DATE', 'TIME');
+
+
 
 -- db_version
 UPDATE config_items SET value = '3.4.15' WHERE code = 'db_version';
