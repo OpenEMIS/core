a1dc0107bf0a609d1c5707e870e356ba88cea5c8
Chiang Hao Long <hchiang@kordit.com>
2016-02-25 12:53:41 +0800

    POCOR-2601 - Change logic to disable delete of area and area administrative levels if there is associated records.

--- plugins/Area/src/Model/Table/AreaLevelsTable.php
+++ plugins/Area/src/Model/Table/AreaLevelsTable.php
@@ -5,19 +5,20 @@ use ArrayObject;
 use App\Model\Table\AppTable;
 use Cake\Network\Request;
 use Cake\Event\Event;
+use App\Model\Table\ControllerActionTable;
 
-class AreaLevelsTable extends AppTable {
+class AreaLevelsTable extends ControllerActionTable {
 	public function initialize(array $config) {
 		parent::initialize($config);
 		$this->hasMany('Areas', ['className' => 'Area.Areas', 'foreign_key' => 'area_level_id']);
+		$this->addBehavior('RestrictAssociatedDelete');
 	}
 
-	public function beforeAction(Event $event) {
-		$this->ControllerAction->field('level');
-		$this->ControllerAction->setFieldOrder('level', 'name');
+	public function beforeAction(Event $event, ArrayObject $extra) {
+		$this->field('level', ['before' => 'name']);
 	}
 
-	public function addEditBeforeAction(Event $event) {
+	public function addEditBeforeAction(Event $event, ArrayObject $extra) {
 		$this->fields['level']['type'] = 'hidden';
 	}
 
