127b99c6a0e0184e4481eea89860aaf35702664b
Chiang Hao Long <hchiang@kordit.com>
2016-03-07 12:22:10 +0800

    POCOR-2604 - Translated role option list. Translated promoted, graduated and repeat statuses.

--- plugins/Institution/src/Model/Table/StaffPositionTitlesTable.php
+++ plugins/Institution/src/Model/Table/StaffPositionTitlesTable.php
@@ -7,10 +7,12 @@ use Cake\Event\Event;
 use Cake\ORM\Entity;
 use Cake\ORM\TableRegistry;
 use Cake\Network\Session;
-
+use ControllerAction\Model\Traits\UtilityTrait;
 use App\Model\Table\ControllerActionTable;
 
 class StaffPositionTitlesTable extends ControllerActionTable {
+	use UtilityTrait;
+
 	public function initialize(array $config) {
         $this->addBehavior('ControllerAction.FieldOption');
         $this->table('staff_position_titles');
@@ -28,8 +30,10 @@ class StaffPositionTitlesTable extends ControllerActionTable {
 			'options' => $this->getSelectOptions('Staff.position_types'),
 			'after' => 'name'
 		]);
-
-		$extra['roleList'] = ['' => '-- '.__('Select Role').' --'] + $this->SecurityRoles->getSystemRolesList();
+		$systemRolesList = ['' => '-- '.__('Select Role').' --'] + $this->SecurityRoles->getSystemRolesList();
+		$selected = '';
+		$this->advancedSelectOptions($systemRolesList, $selected);
+		$extra['roleList'] = $systemRolesList;
 		$this->field('security_role_id', ['after' => 'type', 'options' => $extra['roleList']]);
 	}
 
