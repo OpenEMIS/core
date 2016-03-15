a657e01df7c061001df62b9fafec0e81ba2ff9ca
Thed Kuok Shiun <kuokshiun@hotmail.com>
2016-03-15 16:30:53 +0800

    POCOR-2733 - Remap status_id in institution_student_surveys and data patch.

--- sql/Patch/POCOR-2733/rollback.sql
+++ sql/Patch/POCOR-2733/rollback.sql
@@ -8,5 +8,8 @@ RENAME TABLE `z_2733_institution_surveys` TO `institution_surveys`;
 DROP TABLE IF EXISTS `workflow_records`;
 RENAME TABLE `z_2733_workflow_records` TO `workflow_records`;
 
+-- institution_student_surveys
+ALTER TABLE `institution_student_surveys` DROP `parent_form_id`;
+
 -- db_patches
 DELETE FROM `db_patches` WHERE `issue` = 'POCOR-2733';
