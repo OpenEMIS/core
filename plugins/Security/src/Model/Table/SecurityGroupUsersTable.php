c96a7f890876e7f9aa5b2bcd85ea5cb72d8ad661
Chiang Hao Long <hchiang@kordit.com>
2016-03-15 17:02:34 +0800

    POCOR-2742 - Fixed getting of staff position base on the highest role for the user.

--- plugins/Security/src/Model/Table/SecurityGroupUsersTable.php
+++ plugins/Security/src/Model/Table/SecurityGroupUsersTable.php
@@ -72,10 +72,12 @@ class SecurityGroupUsersTable extends AppTable {
 				'keyField' => 'security_role_id',
 				'valueField' => 'security_role_id'
 			])
+			->innerJoinWith('SecurityRoles')
 			->where([
 				$this->aliasField('security_user_id') => $userId,
 				$this->aliasField('security_group_id').' IN ' => $groupIds
 			])
+			->order('SecurityRoles.order')
 			->group([$this->aliasField('security_role_id')])
 			->select([$this->aliasField('security_role_id')])
 			->hydrate(false)
