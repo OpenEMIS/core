651999a8f871aa23a47742088f78ec65b4e8c08c
Thed Kuok Shiun <kthed@kordit.com>
2016-03-24 17:40:47 +0800

    POCOR-2759 - Setup base code for Institution Results.

--- webroot/js/app/components/institution/result/controller.js
+++ webroot/js/app/components/institution/result/controller.js
@@ -3,24 +3,34 @@ angular.module('institution.result.controller', ['institution.result.service'])
     $scope.gridOptions = null;
 
     angular.element(document).ready(function () {
-        ResultService.getResults($scope).then(function successCallback(_response) {
-            var columnDefs = [];
+        ResultService.getSubjects($scope).then(function successCallback(_subjects) {
+            $scope.subjects = _subjects;
 
-            var rowData = [];
+            ResultService.getColumnDefs($scope).then(function successCallback(_columnDef) {
+                $scope.gridOptions = {
+                    columnDefs: _columnDef,
+                    rowData: [],
+                    headerHeight: 38,
+                    rowHeight: 38,
+                    onReady: function() {
+                        $scope.gridOptions.api.sizeColumnsToFit();
 
-            $scope.gridOptions = {
-                columnDefs: columnDefs,
-                rowData: rowData,
-                headerHeight: 38,
-                rowHeight: 38,
-                onReady: function() {
-                    $scope.gridOptions.api.sizeColumnsToFit();
-                }
-            };
-        }, function errorCallback(_response) {
+                        if (_subjects.length > 0) {
+                            $scope.reloadData(_subjects[0]);
+                        }
+                    }
+                };
+            });
+        });
+    });
 
+    $scope.reloadData = function(_subject) {
+        ResultService.getRowData($scope, _subject).then(function successCallback(_rowData) {
+            $scope.gridOptions.api.setRowData(_rowData);
+        }, function errorCallback(_error) {
+            deferred.reject(_error);
         }, function progressCallback(_response) {
 
         });
-    });
+    };
 });
