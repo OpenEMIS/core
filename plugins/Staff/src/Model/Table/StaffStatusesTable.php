29bbf30b8af2eea6b001f4dca3bb26cfd9c19dc0
Chiang Hao Long <hchiang@kordit.com>
2016-03-24 16:47:08 +0800

    POCOR-2172 - Added update of staff status logic. Fixed beforeTransition event.

--- plugins/Staff/src/Model/Table/StaffStatusesTable.php
+++ plugins/Staff/src/Model/Table/StaffStatusesTable.php
@@ -3,8 +3,9 @@ namespace Staff\Model\Table;
 
 use App\Model\Table\AppTable;
 use Cake\Validation\Validator;
+use App\Model\Table\ControllerActionTable;
 
-class StaffStatusesTable extends AppTable {
+class StaffStatusesTable extends ControllerActionTable {
 	public function initialize(array $config) {
 		parent::initialize($config);
 		$this->hasMany('Staff', ['className' => 'Institution.Staff', 'foreignKey' => 'staff_status_id']);
