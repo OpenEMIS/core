334cfe5a8358427f7943c0f8f1346998fb6fd92c
Jeff Zheng <jzheng@kordit.com>
2016-03-30 15:26:14 +0800

    POCOR-2675 - updated commit.sql

--- sql/Patch/POCOR-2675/commit.sql
+++ sql/Patch/POCOR-2675/commit.sql
@@ -21,3 +21,21 @@ SELECT (MAX(`order`)+1) into @highestOrder from security_roles;
 INSERT INTO `security_roles` (`name`, `code`, `order`, `visible`, `security_group_id`, `modified_user_id`, `modified`, `created_user_id`, `created`) SELECT 'Homeroom Teacher', 'HOMEROOM_TEACHER', @highestOrder, 1, -1, NULL, NULL, 1, '0000-00-00 00:00:00' FROM dual WHERE NOT EXISTS (SELECT 1 FROM security_roles WHERE name = 'Homeroom Teacher');
 
 UPDATE security_roles SET code = 'HOMEROOM_TEACHER' WHERE name = 'Homeroom Teacher';
+
+INSERT INTO labels (id, module, field, module_name, field_name, visible, created_user_id, created)
+values (uuid(), 'InstitutionPositions', 'is_homeroom', 'Institutions -> Positions', 'Homeroom Teacher', 1, 1, NOW());
+
+DROP TABLE IF EXISTS `z1968_staff_qualifications`;
+DROP TABLE IF EXISTS `z2423_assessment_items`;
+DROP TABLE IF EXISTS `z2423_assessment_item_results`;
+DROP TABLE IF EXISTS `z2515_institution_shifts`;
+DROP TABLE IF EXISTS `z_2392_institution_infrastructures`;
+DROP TABLE IF EXISTS `z_2463_institution_section_students`;
+DROP TABLE IF EXISTS `z_2500_institution_section_students`;
+DROP TABLE IF EXISTS `z_2500_security_groups`;
+DROP TABLE IF EXISTS `z_2501_institution_section_students`;
+DROP TABLE IF EXISTS `z_2506_institution_positions`;
+DROP TABLE IF EXISTS `z_2526_authentication_type_attributes`;
+DROP TABLE IF EXISTS `z_2526_config_items`;
+DROP TABLE IF EXISTS `z_2535_institution_positions`;
+
