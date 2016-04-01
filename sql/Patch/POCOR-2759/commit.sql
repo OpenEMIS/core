863c45145f584e5bb7a5800ec09d4455170f61b7
Thed Kuok Shiun <kthed@kordit.com>
2016-04-01 15:10:45 +0800

    POCOR-2759 - Update sql.

--- sql/Patch/POCOR-2759/commit.sql
+++ sql/Patch/POCOR-2759/commit.sql
@@ -1,60 +1,129 @@
 -- db_patches
 INSERT INTO `db_patches` VALUES ('POCOR-2759', NOW());
 
--- Backup tables
-CREATE TABLE `z_2759_assessments` LIKE  `assessments`;
-INSERT INTO `z_2759_assessments` SELECT * FROM `assessments` WHERE 1;
-
-CREATE TABLE `z_2759_assessment_items` LIKE  `assessment_items`;
-INSERT INTO `z_2759_assessment_items` SELECT * FROM `assessment_items` WHERE 1;
-
-CREATE TABLE `z_2759_assessment_item_results` LIKE  `assessment_item_results`;
-INSERT INTO `z_2759_assessment_item_results` SELECT * FROM `assessment_item_results` WHERE 1;
-
-CREATE TABLE `z_2759_assessment_grading_types` LIKE  `assessment_grading_types`;
-INSERT INTO `z_2759_assessment_grading_types` SELECT * FROM `assessment_grading_types` WHERE 1;
-
-RENAME TABLE `assessment_statuses` TO `z_2759_assessment_statuses`;
-RENAME TABLE `assessment_status_periods` TO `z_2759_assessment_status_periods`;
-RENAME TABLE `institution_assessments` TO `z_2759_institution_assessments`;
+-- institution_subjects
+ALTER TABLE `institution_subjects` ADD INDEX(`education_subject_id`);
 
 -- assessments
-ALTER TABLE `assessments` ADD `academic_period_id` INT(11) NOT NULL AFTER `type`;
-ALTER TABLE `assessments` DROP `order`;
-ALTER TABLE `assessments` DROP `visible`;
-ALTER TABLE `assessments` CHANGE `type` `type` INT(1) NOT NULL DEFAULT '1' COMMENT '1 -> Non-official, 2 -> Official';
+RENAME TABLE `assessments` TO `z_2759_assessments`;
+
+DROP TABLE IF EXISTS `assessments`;
+CREATE TABLE IF NOT EXISTS `assessments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(50) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `description` text,
+  `type` int(1) NOT NULL DEFAULT '1' COMMENT '1 -> Non-official, 2 -> Official',
+  `academic_period_id` int(11) NOT NULL,
+  `education_grade_id` int(11) NOT NULL,
+  `modified_user_id` int(11) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `created_user_id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `academic_period_id` (`academic_period_id`),
+  INDEX `education_grade_id` (`education_grade_id`),
+  INDEX `modified_user_id` (`modified_user_id`),
+  INDEX `created_user_id` (`created_user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `assessments` (`id`, `code`, `name`, `description`, `type`, `academic_period_id`, `education_grade_id`, `modified_user_id`, `modified`, `created_user_id`, `created`)
+SELECT `id`, `code`, `name`, `description`, `type`, 0, `education_grade_id`, `modified_user_id`, `modified`, `created_user_id`, `created`
+FROM `z_2759_assessments`;
+
+UPDATE `assessments` SET `type` = 2 WHERE `type` = 1;
+UPDATE `assessments` SET `type` = 1 WHERE `type` = 0;
 
 -- assessment_items
-ALTER TABLE `assessment_items` ADD `weight` DECIMAL(6,2) NULL DEFAULT NULL AFTER `id`;
-ALTER TABLE `assessment_items` DROP `pass_mark`;
-ALTER TABLE `assessment_items` DROP `max`;
-ALTER TABLE `assessment_items` DROP `result_type`;
-ALTER TABLE `assessment_items` CHANGE `id` `id` CHAR(36) NOT NULL;
+RENAME TABLE `assessment_items` TO `z_2759_assessment_items`;
+
+DROP TABLE IF EXISTS `assessment_items`;
+CREATE TABLE IF NOT EXISTS `assessment_items` (
+  `id` char(36) NOT NULL,
+  `weight` decimal(6,2) DEFAULT NULL,
+  `assessment_grading_type_id` int(11) NOT NULL,
+  `assessment_id` int(11) NOT NULL,
+  `education_subject_id` int(11) NOT NULL,
+  `modified_user_id` int(11) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `created_user_id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `assessment_grading_type_id` (`assessment_grading_type_id`),
+  INDEX `assessment_id` (`assessment_id`),
+  INDEX `education_subject_id` (`education_subject_id`),
+  INDEX `modified_user_id` (`modified_user_id`),
+  INDEX `created_user_id` (`created_user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `assessment_items` (`id`, `weight`, `assessment_grading_type_id`, `assessment_id`, `education_subject_id`, `modified_user_id`, `modified`, `created_user_id`, `created`)
+SELECT uuid(), NULL, `assessment_grading_type_id`, `assessment_id`, `education_subject_id`, `modified_user_id`, `modified`, `created_user_id`, `created` FROM `z_2759_assessment_items`;
 
 -- assessment_item_results
-ALTER TABLE `assessment_item_results` ADD `assessment_id` INT(11) NOT NULL AFTER `assessment_item_id`;
-ALTER TABLE `assessment_item_results` ADD `education_subject_id` INT(11) NOT NULL AFTER `assessment_id`;
-ALTER TABLE `assessment_item_results` ADD `assessment_period_id` INT(11) NOT NULL AFTER `academic_period_id`;
-ALTER TABLE `assessment_item_results` DROP `assessment_item_id`;
-ALTER TABLE `assessment_item_results` CHANGE `marks` `marks` DECIMAL(6,2) NULL DEFAULT NULL;
-ALTER TABLE `assessment_item_results` DROP PRIMARY KEY;
-ALTER TABLE `assessment_item_results`
-  ADD PRIMARY KEY (`assessment_id`, `education_subject_id`, `student_id`, `institution_id`, `academic_period_id`, `assessment_period_id`),
-  ADD KEY `assessment_id` (`assessment_id`),
-  ADD KEY `education_subject_id` (`education_subject_id`),
-  ADD KEY `assessment_period_id` (`assessment_period_id`);
-ALTER TABLE `assessment_item_results` PARTITION BY HASH(`academic_period_id`) PARTITIONS 8;
+RENAME TABLE `assessment_item_results` TO `z_2759_assessment_item_results`;
+
+DROP TABLE IF EXISTS `assessment_item_results`;
+CREATE TABLE IF NOT EXISTS `assessment_item_results` (
+  `id` char(36) NOT NULL,
+  `marks` decimal(6,2) DEFAULT NULL,
+  `assessment_grading_option_id` int(11) DEFAULT NULL,
+  `student_id` int(11) NOT NULL COMMENT 'links to security_users.id',
+  `assessment_id` int(11) NOT NULL,
+  `education_subject_id` int(11) NOT NULL,
+  `institution_id` int(11) NOT NULL,
+  `academic_period_id` int(11) NOT NULL,
+  `assessment_period_id` int(11) NOT NULL,
+  `modified_user_id` int(11) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `created_user_id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`student_id`,`assessment_id`,`education_subject_id`,`institution_id`,`academic_period_id`,`assessment_period_id`),
+  INDEX `assessment_grading_option_id` (`assessment_grading_option_id`),
+  INDEX `student_id` (`student_id`),
+  INDEX `assessment_id` (`assessment_id`),
+  INDEX `education_subject_id` (`education_subject_id`),
+  INDEX `institution_id` (`institution_id`),
+  INDEX `academic_period_id` (`academic_period_id`),
+  INDEX `assessment_period_id` (`assessment_period_id`),
+  INDEX `modified_user_id` (`modified_user_id`),
+  INDEX `created_user_id` (`created_user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 PARTITION BY HASH(`academic_period_id`) PARTITIONS 8;
+
+INSERT INTO `assessment_item_results` (`id`, `marks`, `assessment_grading_option_id`, `student_id`, `assessment_id`, `education_subject_id`, `institution_id`, `academic_period_id`, `assessment_period_id`, `modified_user_id`, `modified`, `created_user_id`, `created`)
+SELECT uuid(), `AssessmentItemResults`.`marks`, `AssessmentItemResults`.`assessment_grading_option_id`, `AssessmentItemResults`.`student_id`, `AssessmentItems`.`assessment_id`, `AssessmentItems`.`education_subject_id`, `AssessmentItemResults`.`institution_id`, `AssessmentItemResults`.`academic_period_id`, 0, `AssessmentItemResults`.`modified_user_id`, `AssessmentItemResults`.`modified`, `AssessmentItemResults`.`created_user_id`, `AssessmentItemResults`.`created`
+FROM `z_2759_assessment_item_results` AS `AssessmentItemResults`
+INNER JOIN `z_2759_assessment_items` AS `AssessmentItems`
+ON `AssessmentItems`.`id` = `AssessmentItemResults`.`assessment_item_id`;
 
 -- assessment_grading_types
-ALTER TABLE `assessment_grading_types` ADD `pass_mark` INT(5) NOT NULL AFTER `name`;
-ALTER TABLE `assessment_grading_types` ADD `max` INT(5) NOT NULL AFTER `pass_mark`;
-ALTER TABLE `assessment_grading_types` ADD `result_type` VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER `max`;
-ALTER TABLE `assessment_grading_types` DROP `order`;
+RENAME TABLE `assessment_grading_types` TO `z_2759_assessment_grading_types`;
+
+DROP TABLE IF EXISTS `assessment_grading_types`;
+CREATE TABLE IF NOT EXISTS `assessment_grading_types` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(50) DEFAULT NULL,
+  `name` varchar(80) NOT NULL,
+  `pass_mark` decimal(6,2) NOT NULL,
+  `max` decimal(6,2) NOT NULL,
+  `result_type` varchar(20) NOT NULL,
+  `visible` int(1) NOT NULL DEFAULT '1',
+  `modified_user_id` int(11) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `created_user_id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `code` (`code`),
+  INDEX `modified_user_id` (`modified_user_id`),
+  INDEX `created_user_id` (`created_user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `assessment_grading_types` (`id`, `code`, `name`, `pass_mark`, `max`, `result_type`, `visible`, `modified_user_id`, `modified`, `created_user_id`, `created`)
+SELECT `id`, `code`, `name`, 0, 0, '', `visible`, `modified_user_id`, `modified`, `created_user_id`, `created` FROM `z_2759_assessment_grading_types`;
 
 -- assessment_periods
 DROP TABLE IF EXISTS `assessment_periods`;
 CREATE TABLE IF NOT EXISTS `assessment_periods` (
-  `id` char(36) NOT NULL,
+  `id` char(36) NOT NULL AUTO_INCREMENT,
   `code` varchar(50) NOT NULL,
   `name` varchar(100) NOT NULL,
   `start_date` date NOT NULL,
@@ -66,14 +135,14 @@ CREATE TABLE IF NOT EXISTS `assessment_periods` (
   `modified_user_id` int(11) DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   `created_user_id` int(11) NOT NULL,
-  `created` datetime NOT NULL
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `assessment_id` (`assessment_id`),
+  INDEX `modified_user_id` (`modified_user_id`),
+  INDEX `created_user_id` (`created_user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-ALTER TABLE `assessment_periods`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `assessment_id` (`assessment_id`);
-
-
-ALTER TABLE `assessment_periods`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+-- Backup tables
+RENAME TABLE `assessment_statuses` TO `z_2759_assessment_statuses`;
+RENAME TABLE `assessment_status_periods` TO `z_2759_assessment_status_periods`;
+RENAME TABLE `institution_assessments` TO `z_2759_institution_assessments`;
--- sql/Patch/POCOR-2759/rollback.sql
+++ sql/Patch/POCOR-2759/rollback.sql
@@ -11,12 +11,11 @@ RENAME TABLE `z_2759_assessment_item_results` TO `assessment_item_results`;
 DROP TABLE IF EXISTS `assessment_grading_types`;
 RENAME TABLE `z_2759_assessment_grading_types` TO `assessment_grading_types`;
 
+DROP TABLE IF EXISTS `assessment_periods`;
+
 RENAME TABLE `z_2759_assessment_statuses` TO `assessment_statuses`;
 RENAME TABLE `z_2759_assessment_status_periods` TO `assessment_status_periods`;
 RENAME TABLE `z_2759_institution_assessments` TO `institution_assessments`;
-
--- assessment_periods
-DROP TABLE IF EXISTS `assessment_periods`;
 
 -- db_patches
 DELETE FROM `db_patches` WHERE `issue` = 'POCOR-2759';
