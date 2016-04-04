55767879c2e0377db472e93a71111f64d9623291
HanafiAhmat <hanafi.ahmat@appitnetwork.com>
2016-04-05 02:34:23 +0800

    POCOR-2758	- Enable data exchange across directives

--- plugins/ControllerAction/webroot/js/ng.controller-action.js
+++ plugins/ControllerAction/webroot/js/ng.controller-action.js
@@ -1,8 +1,27 @@
 //Controller Action Angular Functions v.1.0.1
-angular.module('caModule', ['ngRoute', 'kordit.service', 'ui.bootstrap'])
-// angular.module('caModule', ['kordit.service', 'ui.bootstrap'])
-    .directive('caOnChange', function() {
-        // console.log('activated');
+angular.module('caModule', ['kordit.service', 'ui.bootstrap'])
+    .controller('caOnChangeCtrl', function(korditService) {
+
+        var apiUrl = "/restful/";
+        this.changeOptions = function(scope, element, attrs) {
+            var targetUrl = apiUrl + attrs.caOnChangeSourceUrl + element.val();
+            var response = korditService.ajax({url:targetUrl});
+            response  
+                .then(function(data) {
+   
+                    targetOptions = [];
+                    for (var id in data.data) {
+                        targetOptions.push({"id":id, "name":data.data[id]});
+                    }
+                    scope.$root.$broadcast('onChangeOptions', targetOptions);
+                
+                }, function(error) {
+                    console.log('Failure...', error);
+                });
+        };
+
+    })
+    .directive('caOnChangeElement', function() {
         return {
             /**
              * When you create a directive, it is restricted to attribute and elements only by default. In order to create directives that are triggered by class name, you need to use the restrict option.
@@ -18,104 +37,40 @@ angular.module('caModule', ['ngRoute', 'kordit.service', 'ui.bootstrap'])
              * 
              */
             restrict: 'A',
-            scope: {
-                // orientation: '@'
+            scope: {},
+            bindToController: {
+              targetOptions: '='
             },
-            // template: '<div class="split-panes {{orientation}}" ng-transclude></div>',
-            // controller: ['$scope', '$http', '$routeParams', '$location', 'korditService',  function($scope, $http, $routeParams, $location, korditService) {
-            // controller: ['$scope', '$http', '$routeParams', '$location', 'korditService',  function($scope, $http, $routeParams, $location, korditService) {
-            controller: ['$scope', 'korditService',  function($scope, korditService) {
-
-                // var httpParams = ($location.absUrl()).split('/Loads/Publishes');
-                // var indexUrl = httpParams[0] + '/Loads/Publishes';
-                // var id = httpParams[1].replace('/edit/', '');
-                // var gatewayUrl = indexUrl + '/gateway/' + id;
-
-                $scope.options = {};
-                // $http.post(gatewayUrl, korditService.serialize($scope.calculator), {
-                //         headers: {
-                //             'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
-                //         }
-                //     }).
-                //     success(function(data, status, headers, config) {
-                //         if (data!=null && typeof data.error !== 'undefined') {
-                //             $('#alert-modal').find('.modal-body').html(data.error);
-                //             $('#trigger-modal').trigger('click');
-                //         } else if (data!=null) {
-                //             if (!data.status) {
-                //                 $scope.results = data;
-                //                 $scope.spinnerWrapper = false;
-                //             } else {
-                //                 $scope.results = null;
-                //                 $('#alert-modal').find('.modal-body').html(data.message);
-                //                 $('#trigger-modal').trigger('click');
-                //             }
-                //         } else {
-                //             $scope.results = null;
-                //             $('#alert-modal').find('.close').trigger('click');
-                //         }
-                //     }).
-                //     error(function(data, status, headers, config) {
-                //         $('#alert-modal').find('.modal-body').html('<p>An error has occurred</p>');
-                //     });
-
-
-                // $http.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-                var apiUrl = "/rest/";
-            
-                this.onChange = function(element, attrs) {
-                    console.log(attrs);
-                    var targetUrl = apiUrl + attrs.caOnChangeSourceUrl + element.val();
-                    // console.log(targetUrl);
-                    var response = korditService.ajax({url:targetUrl});
-                    response  
-                        .then(function(data) {
-                            console.log('Success!', data);
-                        }, function(error) {
-                            console.log('Failure...', error);
-                        });
-                };
-
-            }],
-            link: function(scope, element, attrs, controller) {
-                // console.log(angular.element());
-                // <select ng-model="selectedCountry.countryId" ng-options="country.countryId as country.name for country in chooseCountries"></select>
+            controller: 'caOnChangeCtrl',
+            controllerAs: 'viewVars',
+            link: function(scope, element, attrs, caOnChangeCtrl) {
 
                 element.on('change', function(event) {
-                    controller.onChange(element, attrs);
+                    caOnChangeCtrl.changeOptions(scope, element, attrs);
                 });
 
             }
         };
     })
-    .directive('caOnChangeTarget', function() {
+    .directive('caOnChangeTargetElement', function() {
         return {
-            // require: ['^^caOnChange'],
-            // template: '<div class="split-panes {{orientation}}" ng-transclude></div>',
-            // controller: ['$scope', '$http', '$routeParams', '$location', 'korditService',  function($scope, $http, $routeParams, $location, korditService) {
-            //     $scope.options = {};
-            // }],
-            // templateUrl: '/assessment/templates/caOnChangeTarget.html',
-            link: function(scope, element, attrs, controller) {
-                // console.log(scope);
-                // <select ng-model="selectedCountry.countryId" ng-options="country.countryId as country.name for country in chooseCountries"></select>
+            restrict: 'A',
+            scope: {},
+            bindToController: {
+              targetOptions: '='
+            },
+            controller: 'caOnChangeCtrl',
+            controllerAs: 'viewVars',
+            templateUrl: '/assessment/templates/caOnChangeTarget.html',
+            link: function(scope, element, attrs, caOnChangeCtrl) {
 
-                // element.on('change', function(event) {
-                //     controller.onChange(element, attrs);
-                // });
+                scope.targetOptions = [];
+
+                scope.$on('onChangeOptions', function (event, targetOptions) {
+                    scope.targetOptions = targetOptions;
+                })
 
             }
         };
     })
     ;
-
-    angular.element(document).ready(function() {
-        // console.log('angular ready!');
-        // angular.bootstrap(document, ['myApp']);
-        // var $injector = angular.injector();
-        // console.log($injector.get());
-        // expect($injector.get('$injector')).toBe($injector);
-        // expect($injector.invoke(function($injector) {
-        //   return $injector;
-        // })).toBe($injector);
-    });
