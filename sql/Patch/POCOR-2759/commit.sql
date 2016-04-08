e9dcf7db08f7e92dea4ab6ebef0491ff0c449811
Thed Kuok Shiun <kthed@kordit.com>
2016-04-08 19:15:45 +0800

    POCOR-2759 - Update sql.

--- sql/Patch/POCOR-2759/commit.sql
+++ sql/Patch/POCOR-2759/commit.sql
@@ -1,9 +1,6 @@
 -- db_patches
 INSERT INTO `db_patches` VALUES ('POCOR-2759', NOW());
 
--- institution_subjects
-ALTER TABLE `institution_subjects` ADD INDEX(`education_subject_id`);
-
 -- assessments
 RENAME TABLE `assessments` TO `z_2759_assessments`;
 
@@ -146,3 +143,74 @@ CREATE TABLE IF NOT EXISTS `assessment_periods` (
 RENAME TABLE `assessment_statuses` TO `z_2759_assessment_statuses`;
 RENAME TABLE `assessment_status_periods` TO `z_2759_assessment_status_periods`;
 RENAME TABLE `institution_assessments` TO `z_2759_institution_assessments`;
+
+-- institution_subjects
+ALTER TABLE `institution_subjects` ADD INDEX(`education_subject_id`);
+
+-- institution_subject_students
+RENAME TABLE `institution_subject_students` TO `z_2759_institution_subject_students`;
+
+DROP TABLE IF EXISTS `institution_subject_students`;
+CREATE TABLE `institution_subject_students` (
+  `id` char(36) NOT NULL,
+  `status` int(1) NOT NULL,
+  `total_mark` decimal(6,2) DEFAULT NULL,
+  `student_id` int(11) NOT NULL COMMENT 'links to security_users.id',
+  `institution_subject_id` int(11) NOT NULL,
+  `institution_class_id` int(11) NOT NULL,
+  `institution_id` int(11) NOT NULL,
+  `academic_period_id` int(11) NOT NULL,
+  `education_subject_id` int(11) NOT NULL,
+  `modified_user_id` int(11) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `created_user_id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `student_id` (`student_id`),
+  INDEX `institution_subject_id` (`institution_subject_id`),
+  INDEX `institution_class_id` (`institution_class_id`),
+  INDEX `institution_id` (`institution_id`),
+  INDEX `academic_period_id` (`academic_period_id`),
+  INDEX `education_subject_id` (`education_subject_id`),
+  INDEX `modified_user_id` (`modified_user_id`),
+  INDEX `created_user_id` (`created_user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `institution_subject_students` (`id`, `status`, `total_mark`, `student_id`, `institution_subject_id`, `institution_class_id`, `institution_id`, `academic_period_id`, `education_subject_id`, `modified_user_id`, `modified`, `created_user_id`, `created`)
+SELECT uuid(), `InstitutionSubjectStudents`.`status`, NULL, `InstitutionSubjectStudents`.`student_id`, `InstitutionSubjectStudents`.`institution_subject_id`, `InstitutionSubjectStudents`.`institution_class_id`, `InstitutionSubjects`.`institution_id`, `InstitutionSubjects`.`academic_period_id`, `InstitutionSubjects`.`education_subject_id`, `InstitutionSubjectStudents`.`modified_user_id`, `InstitutionSubjectStudents`.`modified`, `InstitutionSubjectStudents`.`created_user_id`, `InstitutionSubjectStudents`.`created`
+FROM `z_2759_institution_subject_students` AS `InstitutionSubjectStudents`
+INNER JOIN `institution_subjects` AS `InstitutionSubjects`
+ON `InstitutionSubjects`.`id` = `InstitutionSubjectStudents`.`institution_subject_id`;
+
+-- institution_students
+RENAME TABLE `institution_students` TO `z_2759_institution_students`;
+
+DROP TABLE IF EXISTS `institution_students`;
+CREATE TABLE `institution_students` (
+  `id` char(36) NOT NULL,
+  `student_status_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL COMMENT 'links to security_users.id',
+  `education_grade_id` int(11) NOT NULL,
+  `academic_period_id` int(11) NOT NULL,
+  `start_date` date NOT NULL,
+  `start_year` int(4) NOT NULL,
+  `end_date` date NOT NULL,
+  `end_year` int(4) NOT NULL,
+  `final_result` decimal(6,2) DEFAULT NULL,
+  `institution_id` int(11) NOT NULL,
+  `modified_user_id` int(11) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `created_user_id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `student_status_id` (`student_status_id`),
+  INDEX `student_id` (`student_id`),
+  INDEX `education_grade_id` (`education_grade_id`),
+  INDEX `academic_period_id` (`academic_period_id`),
+  INDEX `institution_id` (`institution_id`),
+  INDEX `modified_user_id` (`modified_user_id`),
+  INDEX `created_user_id` (`created_user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `institution_students` (`id`, `student_status_id`, `student_id`, `education_grade_id`, `academic_period_id`, `start_date`, `start_year`, `end_date`, `end_year`, `final_result`, `institution_id`, `modified_user_id`, `modified`, `created_user_id`, `created`)
+SELECT `id`, `student_status_id`, `student_id`, `education_grade_id`, `academic_period_id`, `start_date`, `start_year`, `end_date`, `end_year`, NULL, `institution_id`, `modified_user_id`, `modified`, `created_user_id`, `created` FROM `z_2759_institution_students`;
