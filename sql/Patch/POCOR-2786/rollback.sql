e170cbeab671acf34c72e56cab3452e75e58c458
Chiang Hao Long <hchiang@kordit.com>
2016-04-21 10:39:54 +0800

    POCOR-2786 - Amended patch file to allow running multiple time. Fixed the wrong start date and the end date of the institution student data.

--- sql/Patch/POCOR-2786/rollback.sql
+++ sql/Patch/POCOR-2786/rollback.sql
@@ -2,5 +2,18 @@
 UPDATE `security_users` INNER JOIN `z_2786_security_users` ON `z_2786_security_users`.`id` = `security_users`.`id`
 SET `security_users`.`date_of_birth` = `z_2786_security_users`.`date_of_birth`, `security_users`.`gender_id` = `z_2786_security_users`.`gender_id`;
 
+DROP TABLE IF EXISTS `z_2786_security_users`;
+
+-- institution_students
+UPDATE `institution_students` 
+INNER JOIN `z_2786_institution_students` 
+    ON `institution_students`.`id` = `z_2786_institution_students`.`id`
+SET `institution_students`.`start_date` = `academic_periods`.`start_date`,
+	`institution_students`.`end_date` = `academic_periods`.`end_date`,
+	`institution_students`.`start_year` = `academic_periods`.`start_year`,
+	`institution_students`.`end_year` = `academic_periods`.`end_year`;
+
+DROP TABLE IF EXISTS `z_2786_institution_students`;
+
 -- db_patches
 DELETE FROM `db_patches` WHERE `issue` = 'POCOR-2786';
