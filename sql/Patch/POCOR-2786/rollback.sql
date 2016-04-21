6b83bbdd08702b1c333204f0188184ab90e6df84
Chiang Hao Long <hchiang@kordit.com>
2016-04-21 14:35:14 +0800

    POCOR-2786 - Fixed sql patch file to fixed wrong created date for institution student. Fixed a bug in the rollback where the rollback is wrong.

--- sql/Patch/POCOR-2786/rollback.sql
+++ sql/Patch/POCOR-2786/rollback.sql
@@ -8,10 +8,11 @@ DROP TABLE IF EXISTS `z_2786_security_users`;
 UPDATE `institution_students` 
 INNER JOIN `z_2786_institution_students` 
     ON `institution_students`.`id` = `z_2786_institution_students`.`id`
-SET `institution_students`.`start_date` = `academic_periods`.`start_date`,
-	`institution_students`.`end_date` = `academic_periods`.`end_date`,
-	`institution_students`.`start_year` = `academic_periods`.`start_year`,
-	`institution_students`.`end_year` = `academic_periods`.`end_year`;
+SET `institution_students`.`start_date` = `z_2786_institution_students`.`start_date`,
+	`institution_students`.`end_date` = `z_2786_institution_students`.`end_date`,
+	`institution_students`.`start_year` = `z_2786_institution_students`.`start_year`,
+	`institution_students`.`end_year` = `z_2786_institution_students`.`end_year`,
+	`institution_students`.`created` = `z_2786_institution_students`.`created`;
 
 DROP TABLE IF EXISTS `z_2786_institution_students`;
 
