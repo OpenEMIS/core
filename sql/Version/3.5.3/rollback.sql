0a6c8fc040c9baf13441976efc30d5a19bfa51f2
Steven Hasanuddin <shasanuddin@kordit.com>
2016-04-25 18:14:49 +0800

    3.5.3 - Updated commit.sql and rollback.sql

--- sql/Version/3.5.3/rollback.sql
+++ sql/Version/3.5.3/rollback.sql
@@ -1,2 +1,13 @@
+-- POCOR-2863
+-- institution_class_students
+DROP TABLE `institution_class_students`;
+
+ALTER TABLE `z_2863_institution_class_students`
+RENAME TO  `institution_class_students`;
+
+-- db_patches
+DELETE FROM `db_patches` WHERE `issue` = 'POCOR-2863';
+
+
 -- 3.5.2
 UPDATE config_items SET value = '3.5.2' WHERE code = 'db_version';
