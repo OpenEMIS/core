11a402c1aeebaa9fb8d0046dd78173242ad65f29
Jeff Zheng <jzheng@kordit.com>
2016-04-27 18:43:05 +0800

    POCOR-2524 - Refactor index()

--- vendor/korditpteltd/kd-cakephp-restful/src/Controller/RestfulController.php
+++ vendor/korditpteltd/kd-cakephp-restful/src/Controller/RestfulController.php
@@ -2,6 +2,7 @@
 namespace Restful\Controller;
 
 use Exception;
+use ArrayObject;
 use Cake\Event\Event;
 use Cake\ORM\Table;
 use Cake\ORM\Query;
@@ -73,52 +74,122 @@ class RestfulController extends AppController
         $this->_outputData([]);
     }
 
-    private function processFinders(Table $table, array $finders)
+    private function processQueryString($requestQueries)
     {
-        $query = $table->find();
-        if (!empty($finders)) {
+        $conditions = [];
+        foreach ($requestQueries as $key => $value) {
+            if (!$this->startsWith($key, '_')) {
+                $conditions[$key] = $value;
+                unset($requestQueries[$key]);
+            }
+        }
+        if (!empty($conditions)) {
+            $requestQueries['_conditions'] = $conditions;
+        }
+        return $requestQueries;
+    }
+
+    // to convert string into json string, and decode into php array
+    private function decode($value)
+    {
+        $list = [];
+        $queryArray = explode(',', $value);
+
+        foreach ($queryArray as $json) {
+            // to convert to a proper json string for decoding into php array
+            $json = str_replace(':', '":"', $json);
+            $json = str_replace('[', '":{"', $json);
+            $json = str_replace(']', '"}}', $json);
+            $json = '{"' . str_replace(';', '","', $json);
+            
+            $noAttributesFound = strripos($json, '"}') === false;
+            if ($noAttributesFound) {
+                $json .= '": {}}';
+            }
+            $array = json_decode($json, true);
+            $list = array_merge($list, $array);
+        }
+        return $list;
+    }
+
+    private function _fields(Query $query, $value, ArrayObject $extra)
+    {
+        if (!empty($value)) {
+            $table = $extra['table'];
+            $columns = $table->schema()->columns();
+            
+            $fields = explode(',', $value);
+            foreach ($fields as $index => $field) {
+                if (in_array($field, $columns)) {
+                    $fields[$index] = $table->aliasField($field);
+                }
+            }
+            $query->select($fields);
+        }
+    }
+
+    private function _find(Query $query, $value, ArrayObject $extra)
+    {
+        if (!empty($value)) {
+            $finders = $this->decode($value);
             foreach ($finders as $name => $options) {
                 $query->find($name, $options);
             }
+            $extra['list'] = array_key_exists('list', $finders);
         }
-        return $query;
     }
 
-    // to convert string into json string
-    private function decode($requestQueries, $key)
+    private function _contain(Query $query, $value, ArrayObject $extra)
     {
-        $finders = [];
-        if (array_key_exists($key, $requestQueries)) {
-            $queryArray = explode(',', $requestQueries[$key]);
-
-            foreach ($queryArray as $finder) {
-                // to convert to a proper json string for decoding into php array
-                $finder = str_replace(':', '":"', $finder);
-                $finder = str_replace('[', '":{"', $finder);
-                $finder = str_replace(']', '"}}', $finder);
-                $finder = '{"' . str_replace(';', '","', $finder);
-                
-                $noAttributesFound = strripos($finder, '"}') === false;
-                if ($noAttributesFound) {
-                    $finder .= '": {}}';
-                }
-                $array = json_decode($finder, true);
-
-                $finders = array_merge($finders, $array);
+        if (!empty($value)) {
+            $contain = [];
+            if ($value === 'true') {
+                // add all associations
+            } else {
+                $contain = explode(',', $value);
             }
+            $query->contain($contain);
         }
-        return $finders;
     }
 
-    private function processGroupBy(Query $query, $requestQueries)
+    private function _conditions(Query $query, $value, ArrayObject $extra)
     {
-        if (array_key_exists('_group', $requestQueries)) {
-            $group = $requestQueries['_group'];
-            $json = '"' . str_replace(',', '","', $group) . '"';
-            $groupBy = json_decode($json, true);
-            $query->group($groupBy);
+        if (!empty($value)) {
+            $conditions = [];
+            $table = $extra['table'];
+            $columns = $table->schema()->columns();
+           
+            foreach ($value as $field => $val) {
+                if (in_array($field, $columns)) {
+                    $conditions[$table->aliasField($field)] = $val;
+                } else {
+                    $conditions[$field] = $val;
+                }
+            }
+            $query->where($conditions);
         }
-        return $query;
+    }
+
+    private function _group(Query $query, $value, ArrayObject $extra)
+    {
+        if (!empty($value)) {
+            $fields = explode(',', $value);
+            $query->group($fields);
+        }
+    }
+
+    private function _limit(Query $query, $value, ArrayObject $extra)
+    {
+        if (!empty($value)) {
+            $query->limit($value);
+        }
+    }
+
+    private function _page(Query $query, $value, ArrayObject $extra)
+    {
+        if (!empty($value)) {
+            $query->page($value);
+        }
     }
 
     public function index()
@@ -128,64 +199,48 @@ class RestfulController extends AppController
         }
 
         $table = $this->model;
+        $query = $table->find();
         $requestQueries = $this->request->query;
+        $extra = new ArrayObject(['table' => $table]);
 
-        $finders = $this->decode($requestQueries, '_finder');
-        $query = $this->processFinders($table, $finders);
-        $listOnly = array_key_exists('list', $finders);
+        $default = ['_limit' => 30, '_page' => 1];
+        $queryString = array_merge($default, $this->processQueryString($requestQueries));
 
-        if (array_key_exists('_finder', $requestQueries)) {
-            $this->_attachFieldSpecificFinders($table, $requestQueries, $query);
-            $this->_attachFinders($table, $requestQueries, $query);
+        foreach ($queryString as $key => $attr) {
+            $this->$key($query, $attr, $extra);
         }
 
-        $containments = $this->_setupContainments($table, $requestQueries, $query);
+        try {
+            $data = [];
+            if ($extra->offsetExists('list') && $extra['list'] == true) {
+                $data = $query->toArray();
+            } else {
+                $data = $this->_formatBinaryValue($query->all());
+            }
+            $this->_outputData($data);
+        } catch (Exception $e) {
+            $this->_outputError($e->getMessage());
+        }
+    }
 
-        $conditions = [];
-        if (!empty($requestQueries)) {
-            $conditions = $this->_setupConditions($table, $requestQueries);
+    private function convertBinaryToBase64(Entity $entity)
+    {
+        foreach ($entity->visibleProperties() as $property) {
+            if (is_resource($entity->$property)) {
+                $entity->$property = base64_encode("data:image/jpeg;base64,".stream_get_contents($entity->$property));
+            }
         }
-        $fields = [];
-        if (!empty($requestQueries)) {
-            $fields = $this->_filterSelectFields($table, $requestQueries, $containments);
-        }
-        if (is_bool($conditions) && !$conditions) {
-            $this->_outputError('Extra query parameters declared do not exists in '.$table->registryAlias());
-        } else if (is_bool($fields) && !$fields) {
-            $this->_outputError('One or more selected fields do not exists in '.$table->registryAlias());
+    }
+
+    private function _formatBinaryValue($data) {
+        if ($data instanceof Entity) {
+            $this->convertBinaryToBase64($data);
         } else {
-            if (!empty($conditions)) {
-                $query->where($conditions);
-            }
-            if (!empty($fields)) {
-                $query->select($fields);
-            }
-
-            $limit = 30;
-            $page = 1;
-            if (array_key_exists('_limit', $requestQueries)) {
-                $limit = $requestQueries['_limit'];
-                if (array_key_exists('_page', $requestQueries)) {
-                    $page = $requestQueries['_page'];
-                }
-            }
-            if ($limit > 0) {
-                $query->limit($limit)->page($page);
-            }
-            $this->processGroupBy($query, $requestQueries);
-
-            try {
-                $data = [];
-                if ($listOnly) {
-                    $data = $query->toArray();
-                } else {
-                    $data = $this->_formatBinaryValue($query->all());
-                }
-                $this->_outputData($data);
-            } catch (Exception $e) {
-                $this->_outputError($e->getMessage());
+            foreach ($data as $key => $value) {
+                $this->convertBinaryToBase64($value);
             }
         }
+        return $data;
     }
 
     public function add($model) {
@@ -310,132 +365,78 @@ class RestfulController extends AppController
             '_serialize' => ['data']
         ]);
     }
-
-    private function _formatBinaryValue($data) {
-        if ($data instanceof Entity) {
-            foreach ($data->visibleProperties() as $property) {
-                if (is_resource($data->$property)) {
-                    $data->$property = base64_encode("data:image/jpeg;base64,".stream_get_contents($data->$property));                      
-                }
-            }
-        } else {
-            foreach ($data as $key => $value) {
-                foreach ($value->visibleProperties() as $property) {
-                    if (is_resource($value->$property)) {
-                        $value->$property = base64_encode("data:image/jpeg;base64,".stream_get_contents($value->$property));                        
-                    }
-                }
-            }
-        }
-        return $data;
-    }
-
-    private function _parseFindByList(Table $target, array $requestQueries) {
-        $finders = explode(',', $requestQueries['_finder']);
-        foreach ($finders as $key => $finder) {
-            if (substr_count($finder, 'list')>0) {
-
-                $bracketPost = strpos($finder, '[');
-                if ($bracketPost>0) {
-                    $parameters = substr($finder, $bracketPost+1, -1);
-                    $parameters = explode(';', $parameters);
-                } else {
-                    $parameters = [];
-                }
-
-                $keyField = $target->primaryKey();
-                $valueField = $target->displayField();
-                $groupField = null;
-                if (isset($parameters[0]) && !empty($parameters[0])) {
-                    $keyField = $parameters[0];
-                }
-                if (isset($parameters[1]) && !empty($parameters[1])) {
-                    $valueField = $parameters[1];
-                }
-                if (isset($parameters[2]) && !empty($parameters[2])) {
-                    $groupField = $parameters[2];
-                }
-                return $target->find('list', [
-                        'keyField' => $keyField,
-                        'valueField' => $valueField,
-                        'groupField' => $groupField
-                    ]);
-
-            }
-        }
-    }
     
-    private $_specificFields = ['visible', 'active', 'order', 'editable'];
-    private function _attachFieldSpecificFinders(Table $target, array $requestQueries, Query $query) {
-        $finders = explode(',', $requestQueries['_finder']);
-        foreach ($finders as $key => $finder) {
-            $strlen = (strpos($finder, '[')>0) ? strpos($finder, '[') : strlen($finder);
-            $functionName = strtolower(substr($finder, 0, $strlen));
-            if (in_array($functionName, $this->_specificFields)) {
-                $targetColumns = $target->schema()->columns();
-                if (in_array($functionName, $targetColumns)) {
-                    $parameters = $this->_setupFinderParams($finder);
-                    if (method_exists($target, 'find'.ucwords($functionName))) {
-                        $query->find($functionName, $parameters);
-                    }
-                }
-            }
-        }
-        return $query;
-    }
+    // private $_specificFields = ['visible', 'active', 'order', 'editable'];
+    // private function _attachFieldSpecificFinders(Table $target, array $requestQueries, Query $query) {
+    //     $finders = explode(',', $requestQueries['_finder']);
+    //     foreach ($finders as $key => $finder) {
+    //         $strlen = (strpos($finder, '[')>0) ? strpos($finder, '[') : strlen($finder);
+    //         $functionName = strtolower(substr($finder, 0, $strlen));
+    //         if (in_array($functionName, $this->_specificFields)) {
+    //             $targetColumns = $target->schema()->columns();
+    //             if (in_array($functionName, $targetColumns)) {
+    //                 $parameters = $this->_setupFinderParams($finder);
+    //                 if (method_exists($target, 'find'.ucwords($functionName))) {
+    //                     $query->find($functionName, $parameters);
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return $query;
+    // }
     
-    private function _attachFinders(Table $target, array $requestQueries, Query $query) {
-        $finders = explode(',', $requestQueries['_finder']);
-        foreach ($finders as $key => $finder) {
-            $strlen = (strpos($finder, '[')>0) ? strpos($finder, '[') : strlen($finder);
-            $functionName = strtolower(substr($finder, 0, $strlen));
-            if (!in_array($functionName, array_merge($this->_specificFields, ['list']))) {
-                $parameters = $this->_setupFinderParams($finder);
-                if (method_exists($target, 'find'.ucwords($functionName))) {
-                    $query->find($functionName, $parameters);
-                } else {
-                    foreach ($target->behaviors()->loaded() as $behaviorName) {
-                        $behavior = $target->behaviors()->get($behaviorName);
-                        if (method_exists($behavior, 'find'.ucwords($functionName))) {
-                            $query->find($functionName, $parameters);
-                        }
-                    }
-                }
-            }
-        }
-        return $query;
-    }
+    // private function _attachFinders(Table $target, array $requestQueries, Query $query) {
+    //     $finders = explode(',', $requestQueries['_finder']);
+    //     foreach ($finders as $key => $finder) {
+    //         $strlen = (strpos($finder, '[')>0) ? strpos($finder, '[') : strlen($finder);
+    //         $functionName = strtolower(substr($finder, 0, $strlen));
+    //         if (!in_array($functionName, array_merge($this->_specificFields, ['list']))) {
+    //             $parameters = $this->_setupFinderParams($finder);
+    //             if (method_exists($target, 'find'.ucwords($functionName))) {
+    //                 $query->find($functionName, $parameters);
+    //             } else {
+    //                 foreach ($target->behaviors()->loaded() as $behaviorName) {
+    //                     $behavior = $target->behaviors()->get($behaviorName);
+    //                     if (method_exists($behavior, 'find'.ucwords($functionName))) {
+    //                         $query->find($functionName, $parameters);
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return $query;
+    // }
 
-    private function _setupFinderParams($finder) {
-        $strlen = (strpos($finder, '[')>0) ? strpos($finder, '[') : strlen($finder);
-        $parameters = substr($finder, $strlen+1, -1);
-        if (!empty($parameters)) {
-            $parameters = explode(';', $parameters);
-            foreach ($parameters as $key => $value) {
-                $buffer = explode(':', $value);
-                $parameters[$buffer[0]] = $buffer[1];
-            }
-        } else {
-            $parameters = [];
-        }
-        return $parameters;
-    }
+    // private function _setupFinderParams($finder) {
+    //     $strlen = (strpos($finder, '[')>0) ? strpos($finder, '[') : strlen($finder);
+    //     $parameters = substr($finder, $strlen+1, -1);
+    //     if (!empty($parameters)) {
+    //         $parameters = explode(';', $parameters);
+    //         foreach ($parameters as $key => $value) {
+    //             $buffer = explode(':', $value);
+    //             $parameters[$buffer[0]] = $buffer[1];
+    //         }
+    //     } else {
+    //         $parameters = [];
+    //     }
+    //     return $parameters;
+    // }
     
-    private $_specialParams = ['_finder', '_limit', '_page', '_fields', '_contain'];
-    private function _setupConditions(Table $target, array $requestQueries) {
-        $targetColumns = $target->schema()->columns();
-        $conditions = [];
-        foreach ($requestQueries as $requestQueryKey => $requestQuery) {
-            if (in_array($requestQueryKey, $this->_specialParams)) {
-                continue;
-            }
-            if (!in_array($requestQueryKey, $targetColumns)) {
-                return false;
-            }
-            $conditions[$target->aliasField($requestQueryKey)] = $requestQuery;
-        }
-        return $conditions;
-    }
+    // private $_specialParams = ['_finder', '_limit', '_page', '_fields', '_contain', '_group'];
+    // private function _setupConditions(Table $target, array $requestQueries) {
+    //     $targetColumns = $target->schema()->columns();
+    //     $conditions = [];
+    //     foreach ($requestQueries as $requestQueryKey => $requestQuery) {
+    //         if (in_array($requestQueryKey, $this->_specialParams)) {
+    //             continue;
+    //         }
+    //         if (!in_array($requestQueryKey, $targetColumns)) {
+    //             return false;
+    //         }
+    //         $conditions[$target->aliasField($requestQueryKey)] = $requestQuery;
+    //     }
+    //     return $conditions;
+    // }
 
     private function _setupContainments(Table $target, array $requestQueries, Query $query) {
         $contains = [];
@@ -486,5 +487,10 @@ class RestfulController extends AppController
             }
         }
         return $fields;
+    }
+
+    private function startsWith($haystack, $needle) {
+        // search backwards starting from haystack length characters from the end
+        return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== false;
     }
 }
