392bb2dd6848baba33545a1c1c1d5da39b9484bb
Chiang Hao Long <hchiang@kordit.com>
2016-04-28 15:32:46 +0800

    POCOR-2786 - Added patch file

--- sql/Patch/POCOR-2786/rollback.sql
+++ sql/Patch/POCOR-2786/rollback.sql
@@ -1,3 +1,4 @@
+
 -- security_users
 UPDATE `security_users` INNER JOIN `z_2786_security_users` ON `z_2786_security_users`.`id` = `security_users`.`id`
 SET `security_users`.`date_of_birth` = `z_2786_security_users`.`date_of_birth`, `security_users`.`gender_id` = `z_2786_security_users`.`gender_id`;
