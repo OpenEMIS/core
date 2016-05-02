3c27b47b4a83896132067a61cc5c73e0decbe051
Chiang Hao Long <hchiang@kordit.com>
2016-05-02 22:39:29 +0800

    POCOR-2747 - Added check for all subject and my subject permission

--- webroot/js/app/services/app/kd.access.svc.js
+++ webroot/js/app/services/app/kd.access.svc.js
@@ -1,10 +1,10 @@
 angular
-	.module('kd.access.svc', [])
+	.module('kd.access.svc', ['kd.session.svc'])
 	.service('KdAccessSvc', KdAccessSvc);
 
-KdSessionSvc.$inject = ['KdSessionSvc'];
+KdAccessSvc.$inject = ['$q', 'KdSessionSvc'];
 
-function KdAccessSvc(KdSessionSvc) {
+function KdAccessSvc($q, KdSessionSvc) {
 	var $this = this;
 	var _base = '';
 	var _model = '_access';
@@ -15,10 +15,25 @@ function KdAccessSvc(KdSessionSvc) {
 
     return service;
 
-    function check(key, roles) {
-    	KdSessionSvc.read(key).then(function(value){
-            console.log(roles);
-            return value;
+    function checkPermission(key, roles) {
+    	var deferred = $q.defer();
+    	KdSessionSvc.read(key)
+    	.then(function(permissionRoles){
+    		var permission = false;
+            var compareRolesArray = [roles, permissionRoles];
+            var result = compareRolesArray.shift().filter(function(v) {
+            	return compareRolesArray.every(function(a) {
+                	return a.indexOf(v) !== -1;
+                });
+            });
+            if (result.length > 0) {
+            	permission = true;
+            }
+            deferred.resolve(permission);
+        }, function(error) {
+        	deferred.reject(error);
         });
+
+        return deferred.promise;
     };
 };
