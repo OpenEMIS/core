a8eebbef984029d44e484eb8aa593e30191b7115
Katini Sani <ksani@kordit.com>
2016-05-05 11:31:07 +0800

    POCOR-2908 - Updated app.js and app.ctrl.js

--- webroot/js/app/app.ctrl.js
+++ webroot/js/app/app.ctrl.js
@@ -1,3 +1,10 @@
 angular.module('app.ctrl', ['app.svc', 'utils.svc'])
-.controller('AppCtrl', function() {
-});
+	.controller('AppCtrl', function($rootScope, $scope) {
+    	$scope.getSplitterElements = function (_response){
+    		$scope.splitElems = _response;
+    	};
+  		$scope.splitterDragCallback = function (_response){
+  			console.log("From AppCtrl >> splitterDragCallback = "+ _response);
+  		}
+    });
+
