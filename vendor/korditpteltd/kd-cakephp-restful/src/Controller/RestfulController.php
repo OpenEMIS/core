d01e964111b94501838a41ab17ad6fc35df766d2
Chiang Hao Long <hchiang@kordit.com>
2016-05-09 16:23:19 +0800

    POCOR-2447 - Resolve merge conflict in RestfulController

--- vendor/korditpteltd/kd-cakephp-restful/src/Controller/RestfulController.php
+++ vendor/korditpteltd/kd-cakephp-restful/src/Controller/RestfulController.php
@@ -20,14 +20,6 @@ class RestfulController extends AppController
     public $components = [
         'RequestHandler'
     ];
-<<<<<<< HEAD
-
-    public function initialize()
-    {
-        parent::initialize();
-    }
-=======
->>>>>>> fe4ad9bb870f240ba147ce6aec1faf6ad1ac0730
 
     public function initialize()
     {
@@ -47,15 +39,9 @@ class RestfulController extends AppController
             $this->request->params['_ext'] = 'json';
         }
 
-<<<<<<< HEAD
-        $pass = $this->request->params['pass'];
-        if (count($pass) > 0) {
-            $model = $this->_instantiateModel($pass[0]);
-=======
         if (isset($this->request->model)) {
             $tableAlias = $this->request->model;
             $model = $this->_instantiateModel($tableAlias);
->>>>>>> fe4ad9bb870f240ba147ce6aec1faf6ad1ac0730
             if ($model != false) {
                 $this->model = $model;
             }
@@ -87,54 +73,6 @@ class RestfulController extends AppController
         $this->_outputData([]);
     }
 
-<<<<<<< HEAD
-    private function processFinders(Table $table, array $finders)
-    {
-        $query = $table->find();
-        if (!empty($finders)) {
-            foreach ($finders as $name => $options) {
-                $query->find($name, $options);
-            }
-        }
-        return $query;
-    }
-
-    // to convert string into json string
-    private function decode($requestQueries, $key)
-    {
-        $finders = [];
-        if (array_key_exists($key, $requestQueries)) {
-            $queryArray = explode(',', $requestQueries[$key]);
-
-            foreach ($queryArray as $finder) {
-                // to convert to a proper json string for decoding into php array
-                $finder = str_replace(':', '":"', $finder);
-                $finder = str_replace('[', '":{"', $finder);
-                $finder = str_replace(']', '"}}', $finder);
-                $finder = '{"' . str_replace(';', '","', $finder);
-                
-                $noAttributesFound = strripos($finder, '"}') === false;
-                if ($noAttributesFound) {
-                    $finder .= '": {}}';
-                }
-                $array = json_decode($finder, true);
-
-                $finders = array_merge($finders, $array);
-            }
-        }
-        return $finders;
-    }
-
-    private function processGroupBy(Query $query, $requestQueries)
-    {
-        if (array_key_exists('_group', $requestQueries)) {
-            $group = $requestQueries['_group'];
-            $json = '"' . str_replace(',', '","', $group) . '"';
-            $groupBy = json_decode($json, true);
-            $query->group($groupBy);
-        }
-        return $query;
-=======
     private function processQueryString($requestQueries)
     {
         $conditions = [];
@@ -292,7 +230,6 @@ class RestfulController extends AppController
             }
         }
         return $data;
->>>>>>> fe4ad9bb870f240ba147ce6aec1faf6ad1ac0730
     }
 
     public function index()
@@ -302,69 +239,6 @@ class RestfulController extends AppController
         }
 
         $table = $this->model;
-<<<<<<< HEAD
-        $requestQueries = $this->request->query;
-
-        $finders = $this->decode($requestQueries, '_finder');
-        $query = $this->processFinders($table, $finders);
-        $listOnly = array_key_exists('list', $finders);
-
-        if (array_key_exists('_finder', $requestQueries)) {
-            $this->_attachFieldSpecificFinders($table, $requestQueries, $query);
-            $this->_attachFinders($table, $requestQueries, $query);
-        }
-
-        $containments = $this->_setupContainments($table, $requestQueries, $query);
-
-        $conditions = [];
-        if (!empty($requestQueries)) {
-            $conditions = $this->_setupConditions($table, $requestQueries);
-        }
-        $fields = [];
-        if (!empty($requestQueries)) {
-            $fields = $this->_filterSelectFields($table, $requestQueries, $containments);
-        }
-        if (is_bool($conditions) && !$conditions) {
-            $this->_outputError('Extra query parameters declared do not exists in '.$table->registryAlias());
-        } else if (is_bool($fields) && !$fields) {
-            $this->_outputError('One or more selected fields do not exists in '.$table->registryAlias());
-        } else {
-            if (!empty($conditions)) {
-                $query->where($conditions);
-            }
-            if (!empty($fields)) {
-                $query->select($fields);
-            }
-
-            $limit = 30;
-            $page = 1;
-            if (array_key_exists('_limit', $requestQueries)) {
-                $limit = $requestQueries['_limit'];
-                if (array_key_exists('_page', $requestQueries)) {
-                    $page = $requestQueries['_page'];
-                }
-            }
-            if ($limit > 0) {
-                $query->limit($limit)->page($page);
-            }
-            $this->processGroupBy($query, $requestQueries);
-
-            try {
-                $data = [];
-                if ($listOnly) {
-                    $data = $query->toArray();
-                } else {
-                    $data = $this->_formatBinaryValue($query->all());
-                }
-                $this->_outputData($data);
-            } catch (Exception $e) {
-                $this->_outputError($e->getMessage());
-            }
-        }
-    }
-
-    public function add($model) {
-=======
         $query = $table->find();
         $requestQueries = $this->request->query;
         $extra = new ArrayObject(['table' => $table, 'fields' => []]);
@@ -403,7 +277,6 @@ class RestfulController extends AppController
 
     public function add($model)
     {
->>>>>>> fe4ad9bb870f240ba147ce6aec1faf6ad1ac0730
         $target = $this->_instantiateModel($model);
         if ($target) {
             $entity = $target->newEntity($this->request->data);
@@ -416,12 +289,8 @@ class RestfulController extends AppController
         }
     }
 
-<<<<<<< HEAD
-    public function view($model, $id) {
-=======
     public function view($model, $id)
     {
->>>>>>> fe4ad9bb870f240ba147ce6aec1faf6ad1ac0730
         $target = $this->_instantiateModel($model);
         if ($target) {
             if ($target->exists([$target->aliasField($target->primaryKey()) => $id])) {
@@ -454,12 +323,8 @@ class RestfulController extends AppController
         }
     }
 
-<<<<<<< HEAD
-    public function edit($model, $id) {
-=======
     public function edit($model, $id)
     {
->>>>>>> fe4ad9bb870f240ba147ce6aec1faf6ad1ac0730
         $target = $this->_instantiateModel($model);
         if ($target) {
             if ($target->exists([$target->aliasField($target->primaryKey()) => $id])) {
@@ -481,12 +346,8 @@ class RestfulController extends AppController
         }
     }
 
-<<<<<<< HEAD
-    public function delete($model, $id) {
-=======
     public function delete($model, $id)
     {
->>>>>>> fe4ad9bb870f240ba147ce6aec1faf6ad1ac0730
         $target = $this->_instantiateModel($model);
         if ($target) {
             if ($target->exists([$target->aliasField($target->primaryKey()) => $id])) {
@@ -525,12 +386,8 @@ class RestfulController extends AppController
         }
     }
 
-<<<<<<< HEAD
-    private function _outputError($message = 'Requested Plugin-Model does not exists') {
-=======
     private function _outputError($message = 'Requested Plugin-Model does not exists')
     {
->>>>>>> fe4ad9bb870f240ba147ce6aec1faf6ad1ac0730
         $model = str_replace('-', '.', $this->request->params['model']);
         $this->set([
             'model' => $model,
@@ -539,150 +396,16 @@ class RestfulController extends AppController
         ]);
     }
 
-<<<<<<< HEAD
-    private function _outputData($data) {
-=======
     private function _outputData($data)
     {
->>>>>>> fe4ad9bb870f240ba147ce6aec1faf6ad1ac0730
         $this->set([
             'data' => $data,
             '_serialize' => ['data']
         ]);
     }
 
-<<<<<<< HEAD
-    private function _formatBinaryValue($data) {
-        if ($data instanceof Entity) {
-            foreach ($data->visibleProperties() as $property) {
-                if (is_resource($data->$property)) {
-                    $data->$property = base64_encode("data:image/jpeg;base64,".stream_get_contents($data->$property));                      
-                }
-            }
-        } else {
-            foreach ($data as $key => $value) {
-                foreach ($value->visibleProperties() as $property) {
-                    if (is_resource($value->$property)) {
-                        $value->$property = base64_encode("data:image/jpeg;base64,".stream_get_contents($value->$property));                        
-                    }
-                }
-            }
-        }
-        return $data;
-    }
-
-    private function _parseFindByList(Table $target, array $requestQueries) {
-        $finders = explode(',', $requestQueries['_finder']);
-        foreach ($finders as $key => $finder) {
-            if (substr_count($finder, 'list')>0) {
-
-                $bracketPost = strpos($finder, '[');
-                if ($bracketPost>0) {
-                    $parameters = substr($finder, $bracketPost+1, -1);
-                    $parameters = explode(';', $parameters);
-                } else {
-                    $parameters = [];
-                }
-
-                $keyField = $target->primaryKey();
-                $valueField = $target->displayField();
-                $groupField = null;
-                if (isset($parameters[0]) && !empty($parameters[0])) {
-                    $keyField = $parameters[0];
-                }
-                if (isset($parameters[1]) && !empty($parameters[1])) {
-                    $valueField = $parameters[1];
-                }
-                if (isset($parameters[2]) && !empty($parameters[2])) {
-                    $groupField = $parameters[2];
-                }
-                return $target->find('list', [
-                        'keyField' => $keyField,
-                        'valueField' => $valueField,
-                        'groupField' => $groupField
-                    ]);
-
-            }
-        }
-    }
-    
-    private $_specificFields = ['visible', 'active', 'order', 'editable'];
-    private function _attachFieldSpecificFinders(Table $target, array $requestQueries, Query $query) {
-        $finders = explode(',', $requestQueries['_finder']);
-        foreach ($finders as $key => $finder) {
-            $strlen = (strpos($finder, '[')>0) ? strpos($finder, '[') : strlen($finder);
-            $functionName = strtolower(substr($finder, 0, $strlen));
-            if (in_array($functionName, $this->_specificFields)) {
-                $targetColumns = $target->schema()->columns();
-                if (in_array($functionName, $targetColumns)) {
-                    $parameters = $this->_setupFinderParams($finder);
-                    if (method_exists($target, 'find'.ucwords($functionName))) {
-                        $query->find($functionName, $parameters);
-                    }
-                }
-            }
-        }
-        return $query;
-    }
-    
-    private function _attachFinders(Table $target, array $requestQueries, Query $query) {
-        $finders = explode(',', $requestQueries['_finder']);
-        foreach ($finders as $key => $finder) {
-            $strlen = (strpos($finder, '[')>0) ? strpos($finder, '[') : strlen($finder);
-            $functionName = strtolower(substr($finder, 0, $strlen));
-            if (!in_array($functionName, array_merge($this->_specificFields, ['list']))) {
-                $parameters = $this->_setupFinderParams($finder);
-                if (method_exists($target, 'find'.ucwords($functionName))) {
-                    $query->find($functionName, $parameters);
-                } else {
-                    foreach ($target->behaviors()->loaded() as $behaviorName) {
-                        $behavior = $target->behaviors()->get($behaviorName);
-                        if (method_exists($behavior, 'find'.ucwords($functionName))) {
-                            $query->find($functionName, $parameters);
-                        }
-                    }
-                }
-            }
-        }
-        return $query;
-    }
-
-    private function _setupFinderParams($finder) {
-        $strlen = (strpos($finder, '[')>0) ? strpos($finder, '[') : strlen($finder);
-        $parameters = substr($finder, $strlen+1, -1);
-        if (!empty($parameters)) {
-            $parameters = explode(';', $parameters);
-            foreach ($parameters as $key => $value) {
-                $buffer = explode(':', $value);
-                $parameters[$buffer[0]] = $buffer[1];
-            }
-        } else {
-            $parameters = [];
-        }
-        return $parameters;
-    }
-    
-    private $_specialParams = ['_finder', '_limit', '_page', '_fields', '_contain'];
-    private function _setupConditions(Table $target, array $requestQueries) {
-        $targetColumns = $target->schema()->columns();
-        $conditions = [];
-        foreach ($requestQueries as $requestQueryKey => $requestQuery) {
-            if (in_array($requestQueryKey, $this->_specialParams)) {
-                continue;
-            }
-            if (!in_array($requestQueryKey, $targetColumns)) {
-                return false;
-            }
-            $conditions[$target->aliasField($requestQueryKey)] = $requestQuery;
-        }
-        return $conditions;
-    }
-
-    private function _setupContainments(Table $target, array $requestQueries, Query $query) {
-=======
     private function _setupContainments(Table $target, array $requestQueries, Query $query)
     {
->>>>>>> fe4ad9bb870f240ba147ce6aec1faf6ad1ac0730
         $contains = [];
         if (array_key_exists('_contain', $requestQueries)) {
             $contains = array_map('trim', explode(',', $requestQueries['_contain']));
@@ -705,12 +428,8 @@ class RestfulController extends AppController
         return $contains;
     }
 
-<<<<<<< HEAD
-    private function _filterSelectFields(Table $target, array $requestQueries, array $containments=[]) {
-=======
     private function _filterSelectFields(Table $target, array $requestQueries, array $containments=[])
     {
->>>>>>> fe4ad9bb870f240ba147ce6aec1faf6ad1ac0730
         $targetColumns = $target->schema()->columns();
         if (!array_key_exists('_fields', $requestQueries)) {
             return [];
@@ -737,13 +456,10 @@ class RestfulController extends AppController
         }
         return $fields;
     }
-<<<<<<< HEAD
-=======
 
     private function startsWith($haystack, $needle)
     {
         // search backwards starting from haystack length characters from the end
         return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== false;
     }
->>>>>>> fe4ad9bb870f240ba147ce6aec1faf6ad1ac0730
 }
