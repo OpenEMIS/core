5800255af7a07c25315eb8bacca7be5763b5bd22
Martinnus Budiarto <mbudiarto@kordit.com>
2016-05-09 13:06:04 +0800

    use NOT EXISTS instead of NOT IN

--- sql/Patch/POCOR-2899/commit.sql
+++ sql/Patch/POCOR-2899/commit.sql
@@ -2,7 +2,8 @@
 INSERT INTO `db_patches` (`issue`, `created`) VALUES ('POCOR-2899', NOW());
 
 -- remove orphan / test record
-DELETE FROM `institution_staff_position_profiles`
-WHERE `institution_id` NOT IN (
-    SELECT `id` FROM `institutions`
-    );
+DELETE FROM `institution_staff_position_profiles` 
+WHERE NOT EXISTS ( 
+	SELECT 1 FROM `institutions`
+		WHERE `institutions`.`id` = `institution_staff_position_profiles`.`institution_id`
+	);
