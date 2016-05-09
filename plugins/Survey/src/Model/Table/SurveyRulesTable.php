eebb6745ef61afad2e93a25d6bfa5aa8211eeda0
Chiang Hao Long <hchiang@kordit.com>
2016-05-09 15:35:24 +0800

    POCOR-2447 - Added survey section filter. Fixed a bug with the commit patch file where there is a missing bracket.

--- plugins/Survey/src/Model/Table/SurveyRulesTable.php
+++ plugins/Survey/src/Model/Table/SurveyRulesTable.php
@@ -8,30 +8,68 @@ use Cake\Validation\Validator;
 use ArrayObject;
 use Cake\ORM\TableRegistry;
 use Cake\Utility\Text;
+use Cake\ORM\Query;
 
 use App\Model\Table\ControllerActionTable;
 
 class SurveyRulesTable extends ControllerActionTable 
 {
-	public function initialize(array $config) 
-	{
-		$this->table('survey_rules');
-		parent::initialize($config);
-		$this->belongsTo('SurveyForms', 					['className' => 'Survey.SurveyForms', 'foreignKey' => 'survey_form_id']);
-		$this->belongsTo('SurveyQuestions', 				['className' => 'Survey.SurveyQuestions', 'foreignKey' => 'survey_question_id']);
-		$this->belongsTo('DependentQuestions',				['className' => 'Survey.SurveyQuestions', 'foreignKey' => 'dependent_question_id']);
-	}
+    public function initialize(array $config) 
+    {
+        $this->table('survey_rules');
+        parent::initialize($config);
+        $this->belongsTo('SurveyForms',                     ['className' => 'Survey.SurveyForms', 'foreignKey' => 'survey_form_id']);
+        $this->belongsTo('SurveyQuestions',                 ['className' => 'Survey.SurveyQuestions', 'foreignKey' => 'survey_question_id']);
+        $this->belongsTo('DependentQuestions',              ['className' => 'Survey.SurveyQuestions', 'foreignKey' => 'dependent_question_id']);
+    }
 
-	private function addSetupControl(ArrayObject $extra, $data = []) 
-	{
-		// $extra['elements']['controls'] = ['name' => 'Education.controls', 'data' => $data, 'order' => 2];
-	}
+    public function indexBeforeAction(Event $event, ArrayObject $extra) 
+    {
+        $extra['elements']['controls'] = ['name' => 'Survey.survey_rules_controls', 'data' => [], 'options' => [], 'order' => 2];
+    }
 
-	public function indexBeforeAction(Event $event, ArrayObject $extra) 
-	{
-		$surveyFormOptions = $this->SurveyForms
-			->find('list')
-			->toArray();
-		$extra['elements']['controls'] = ['name' => 'Survey.survey_rules_controls', 'data' => $surveyFormOptions, 'order' => 2];
-	}
+    public function indexBeforeQuery(Event $event, Query $query, ArrayObject $extra) 
+    {
+        // Survey form options
+        $surveyFormOptions = $this->SurveyForms
+            ->find('list')
+            ->toArray();
+        $surveyFormOptions = ['' => '--'.__('Select Survey').'--'] + $surveyFormOptions;
+        $surveyFormId = $this->request->query('survey_form_id');
+        $this->advancedSelectOptions($surveyFormOptions, $surveyFormId);
+        $this->controller->set(compact('surveyFormOptions'));
+
+        // Survey sections
+        
+        if (!empty($surveyFormId)) 
+        {
+            $SurveyFormsQuestionsTable = TableRegistry::get('Survey.SurveyFormsQuestions');
+
+            $surveySections = $SurveyFormsQuestionsTable
+                ->find('list', [
+                    'keyField' => 'survey_section',
+                    'valueField' => 'survey_section'
+                ])
+                ->select(['survey_section' => $SurveyFormsQuestionsTable->aliasField('section')])
+                ->where([
+                    $SurveyFormsQuestionsTable->aliasField('survey_form_id') => $surveyFormId,
+                    $SurveyFormsQuestionsTable->aliasField('survey_form_id').' IS NOT NULL'
+                ])
+                ->distinct([$SurveyFormsQuestionsTable->aliasField('section')])
+                ->order([$SurveyFormsQuestionsTable->aliasField('order')])
+                ->toArray();
+
+            $sectionOptions = ['0' => '--'.__('Select Section').'--'] + $surveySections;
+            $sectionOptions = array_values($sectionOptions);
+            $sectionId = $this->request->query('section_id');
+            $this->advancedSelectOptions($sectionOptions, $sectionId);
+            $this->controller->set(compact('sectionOptions'));
+        }
+
+        if (!empty($surveyFormId) && !empty($sectionId)) 
+        {
+            $section = $sectionOptions[$sectionId];
+            
+        }
+    }
 }
