4989aff99ba0b345b1949257fde7e7a243bb1494
Steven Hasanuddin <shasanuddin@kordit.com>
2016-05-10 10:18:35 +0800

    3.5.5 - Updated changelog, commit.sql, rollback.sql

--- sql/Version/3.5.5/commit.sql
+++ sql/Version/3.5.5/commit.sql
@@ -1,3 +1,26 @@
+-- POCOR-2614
+-- db_patches
+INSERT INTO `db_patches` VALUES ('POCOR-2614', NOW());
+
+INSERT INTO `translations` (`id`, `code`, `en`, `ar`, `zh`, `es`, `fr`, `ru`, `modified_user_id`, `modified`, `created_user_id`, `created`) SELECT NULL, NULL, 'Next grade in the Education Structure is not available in this Institution.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, now() FROM dual WHERE NOT EXISTS (SELECT * FROM translations WHERE en = 'Next grade in the Education Structure is not available in this Institution.');
+
+INSERT INTO `translations` (`id`, `code`, `en`, `ar`, `zh`, `es`, `fr`, `ru`, `modified_user_id`, `modified`, `created_user_id`, `created`) SELECT NULL, NULL, 'No Available Grades in this Institution', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, now() FROM dual WHERE NOT EXISTS (SELECT * FROM translations WHERE en = 'No Available Grades in this Institution');
+
+INSERT INTO `translations` (`id`, `code`, `en`, `ar`, `zh`, `es`, `fr`, `ru`, `modified_user_id`, `modified`, `created_user_id`, `created`) SELECT NULL, NULL, 'No Available Academic Periods', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, now() FROM dual WHERE NOT EXISTS (SELECT * FROM translations WHERE en = 'No Available Academic Periods');
+
+
+-- POCOR-2899
+-- db_patches
+INSERT INTO `db_patches` (`issue`, `created`) VALUES ('POCOR-2899', NOW());
+
+-- remove orphan / test record
+DELETE FROM `institution_staff_position_profiles`
+WHERE NOT EXISTS (
+        SELECT 1 FROM `institutions`
+                WHERE `institutions`.`id` = `institution_staff_position_profiles`.`institution_id`
+);
+
+
 -- POCOR-2885
 -- db_patches
 INSERT INTO `db_patches` (`issue`, `created`) VALUES ('POCOR-2885', NOW());
