c36b5a9d4635471ebf6986c4b2dc816d3a54d906
Malcolm Lee <mlee@kordit.com>
2016-05-10 10:23:03 +0800

    POCOR-2614 - Fixed db_patch sql

--- sql/Patch/POCOR-2614/commit.sql
+++ sql/Patch/POCOR-2614/commit.sql
@@ -1,5 +1,5 @@
 -- db_patches
-INSERT INTO `db_patches` VALUES ('POCOR-2614', NOW());
+INSERT INTO `db_patches` (issue, created) VALUES ('POCOR-2614', NOW());
 
 INSERT INTO `translations` (`id`, `code`, `en`, `ar`, `zh`, `es`, `fr`, `ru`, `modified_user_id`, `modified`, `created_user_id`, `created`) SELECT NULL, NULL, 'Next grade in the Education Structure is not available in this Institution.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, now() FROM dual WHERE NOT EXISTS (SELECT * FROM translations WHERE en = 'Next grade in the Education Structure is not available in this Institution.');
 
