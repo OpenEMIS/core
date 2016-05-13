eb9cafa65c0de4df0a8153c74f38b0fb4ab2dba6
Harry Suhendra <hsuhendra@kordit.com>
2016-05-13 15:26:44 +0800

    POCOR-2755-changing the validation to ValidationNewStudent, add validation on the staffTable

--- plugins/Institution/src/Model/Table/StaffTable.php
+++ plugins/Institution/src/Model/Table/StaffTable.php
@@ -126,7 +126,7 @@ class StaffTable extends AppTable {
 				'rule' => ['institutionStaffId'],
 				'on' => 'create'
 			])
-			->requirePresence('position_type',false)
+			->requirePresence('position_type')
 		;
 	}
 
