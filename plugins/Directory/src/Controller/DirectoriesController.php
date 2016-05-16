60b50a5009374dc9b34e63d473509ae19c31b3dc
Malcolm Lee <mlee@kordit.com>
2016-05-16 13:49:03 +0800

    POCOR-2373 - Merge conflict resolution

--- plugins/Directory/src/Controller/DirectoriesController.php
+++ plugins/Directory/src/Controller/DirectoriesController.php
@@ -83,14 +83,10 @@ class DirectoriesController extends AppController {
 	public function StaffSubjects() { $this->ControllerAction->process(['alias' => __FUNCTION__, 'className' => 'Staff.StaffSubjects']); }
 	public function StudentClasses() { $this->ControllerAction->process(['alias' => __FUNCTION__, 'className' => 'Student.StudentClasses']); }
 	public function StudentSubjects() { $this->ControllerAction->process(['alias' => __FUNCTION__, 'className' => 'Student.StudentSubjects']); }
-<<<<<<< HEAD
     public function Languages() { $this->ControllerAction->process(['alias' => __FUNCTION__, 'className' => 'User.UserLanguages']); }
     public function SpecialNeeds() { $this->ControllerAction->process(['alias' => __FUNCTION__, 'className' => 'User.SpecialNeeds']); }
-
-=======
 	public function StaffMemberships() { $this->ControllerAction->process(['alias' => __FUNCTION__, 'className' => 'Staff.Memberships']); }
 	public function StaffLicenses() { $this->ControllerAction->process(['alias' => __FUNCTION__, 'className' => 'Staff.Licenses']); }
->>>>>>> 217b87397382c484a4934fcf05f9e8f35e33303f
 	// End
 
 	public function beforeFilter(Event $event) {
