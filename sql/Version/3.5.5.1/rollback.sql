5dbe9d75238827f5c5e4d57253b2dc8f5d8d7147
Steven Hasanuddin <shasanuddin@kordit.com>
2016-05-18 11:36:06 +0800

    3.5.5.1 - Updated commit.sql and rollback.sql

--- sql/Version/3.5.5.1/rollback.sql
+++ sql/Version/3.5.5.1/rollback.sql
@@ -4,6 +4,10 @@ DELETE FROM `labels` WHERE `module` = 'StaffTransferApprovals' AND `field` = 'in
 DELETE FROM `labels` WHERE `module` = 'StaffTransferApprovals' AND `field` = 'start_date';
 DELETE FROM `labels` WHERE `module` = 'StaffTransferApprovals' AND `field` = 'FTE';
 DELETE FROM `labels` WHERE `module` = 'StaffTransferApprovals' AND `field` = 'staff_type_id';
+DELETE FROM `labels` WHERE `module` = 'StaffTransferApprovals' AND `field` = 'institution_id';
+DELETE FROM `labels` WHERE `module` = 'StaffTransferApprovals' AND `field` = 'previous_institution_id';
+INSERT INTO `labels` (`id`, `module`, `field`, `module_name`, `field_name`, `visible`, `created_user_id`, `created`) VALUES (uuid(), 'StaffTransferApprovals', 'institution_id', 'Institution -> Staff Transfer Approvals', 'To Be Approved By', 1, 1, NOW());
+INSERT INTO `labels` (`id`, `module`, `field`, `module_name`, `field_name`, `visible`, `created_user_id`, `created`) VALUES (uuid(), 'StaffTransferApprovals', 'previous_institution_id', 'Institution -> Staff Transfer Approvals', 'Requested By', 1, 1, NOW());
 
 -- db_patches
 DELETE FROM db_patches where `issue` = 'POCOR-2992';
