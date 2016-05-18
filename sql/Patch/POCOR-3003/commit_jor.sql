42918e3ce709738f6c710a81ff6d5fb8b6b655cf
Malcolm Lee <mlee@kordit.com>
2016-05-18 09:01:07 +0800

    POCOR-3003 - Refactored SQL for jordan

--- sql/Patch/POCOR-3003/commit_jor.sql
+++ sql/Patch/POCOR-3003/commit_jor.sql
@@ -8,13 +8,13 @@ CREATE TABLE z_3003_institution_staff LIKE institution_staff;
 INSERT INTO z_3003_institution_staff SELECT * FROM institution_staff;
 
 
--- ADD BACK OLD RECORDS
-INSERT IGNORE INTO security_group_users SELECT * FROM z_2907_backup_security_group_users;
-
 -- REMOVE NEW RECORDS
 DELETE FROM security_group_users WHERE EXISTS (SELECT * FROM z_2907_institution_staff WHERE z_2907_institution_staff.security_group_user_id = security_group_users.id);
 
 DELETE FROM security_group_users WHERE EXISTS (SELECT * FROM z_2907_institution_staff WHERE z_2907_institution_staff.security_group_user_homeroom_id = security_group_users.id);
+
+-- ADD BACK OLD RECORDS
+INSERT IGNORE INTO security_group_users SELECT * FROM z_2907_backup_security_group_users;
 
 -- UPDATE INSTITUTION_STAFF TABLE
 UPDATE institution_staff
