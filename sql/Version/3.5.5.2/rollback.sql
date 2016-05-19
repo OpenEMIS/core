799c0c5dc966a8659407c89970ffdf2bd31cbed4
Steven Hasanuddin <shasanuddin@kordit.com>
2016-05-19 11:15:40 +0800

    3.5.5.2 - Updated commit.sql and rollback.sql

--- sql/Version/3.5.5.2/rollback.sql
+++ sql/Version/3.5.5.2/rollback.sql
@@ -1,3 +1,16 @@
+-- POCOR-3006
+-- update back the position_no value
+UPDATE `institution_positions`
+INNER JOIN `z_3006_institution_positions` ON `institution_positions`.`id` = `z_3006_institution_positions`.`id`
+SET `institution_positions`.`position_no` = `z_3006_institution_positions`.`position_no`;
+
+-- remove backup table
+DROP TABLE IF EXISTS `z_3006_institution_positions`;
+
+-- db_patches
+DELETE FROM `db_patches` WHERE `issue` = 'POCOR-3006';
+
+
 -- POCOR-2992
 -- labels
 DELETE FROM `labels` WHERE `module` = 'StaffTransferApprovals' AND `field` = 'institution_position_id';
