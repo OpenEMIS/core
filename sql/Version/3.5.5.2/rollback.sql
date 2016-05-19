3c98c35780f212fe8d520169715b5f503ea7ee72
Steven Hasanuddin <shasanuddin@kordit.com>
2016-05-19 13:31:38 +0800

    3.5.5.2 - Updated commit.sql and rollback.sql

--- sql/Version/3.5.5.2/rollback.sql
+++ sql/Version/3.5.5.2/rollback.sql
@@ -1,3 +1,21 @@
+-- POCOR-2451
+-- custom_field_types
+DELETE FROM `custom_field_types` WHERE `code` = 'REPEATER';
+
+-- custom_modules
+UPDATE `custom_modules` SET `supported_field_types` = REPLACE(`supported_field_types`, ',REPEATER', '') WHERE `model` = 'Institution.Institutions';
+
+DELETE FROM `custom_modules` WHERE `code` = 'Institution > Repeater';
+
+-- Restore tables
+DROP TABLE IF EXISTS `institution_repeater_surveys`;
+DROP TABLE IF EXISTS `institution_repeater_survey_answers`;
+DROP TABLE IF EXISTS `institution_repeater_survey_table_cells`;
+
+-- db_patches
+DELETE FROM `db_patches` WHERE `issue` = 'POCOR-2451';
+
+
 -- POCOR-3006
 -- update back the position_no value
 UPDATE `institution_positions`
