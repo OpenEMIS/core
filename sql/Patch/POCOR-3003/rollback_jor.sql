9e7b1ca8eb75d7e4f481821f9873a9998fba4cc6
Malcolm Lee <mlee@kordit.com>
2016-05-19 11:39:50 +0800

    POCOR-3003 - Institution 'Position Title not editable on the position edit page'

--- sql/Patch/POCOR-3003/rollback_jor.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE security_group_users;
-RENAME TABLE z_3003_security_group_users TO security_group_users;
-DROP TABLE institution_staff;
-RENAME TABLE z_3003_institution_staff TO institution_staff;
-
--- db_patches
-DELETE FROM `db_patches` WHERE `issue` = 'POCOR-3003';
