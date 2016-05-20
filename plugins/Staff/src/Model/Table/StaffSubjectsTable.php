f93adb5c34b31d2697c914d10b0c6a4998070bcf
Malcolm Lee <mlee@kordit.com>
2016-05-20 10:55:06 +0800

    POCOR-2781 - Implemented 'Add classes and subjects to staff in institutions'

--- plugins/Staff/src/Model/Table/StaffSubjectsTable.php
+++ plugins/Staff/src/Model/Table/StaffSubjectsTable.php
@@ -6,6 +6,7 @@ use Cake\Event\Event;
 use Cake\ORM\Query;
 use Cake\ORM\Entity;
 use Cake\ORM\ResultSet;
+use Cake\ORM\TableRegistry;
 
 use App\Model\Traits\MessagesTrait;
 use App\Model\Table\ControllerActionTable;
@@ -21,14 +22,17 @@ class StaffSubjectsTable extends ControllerActionTable {
 		$this->belongsTo('InstitutionSubjects', ['className' => 'Institution.InstitutionSubjects']);
 		$this->hasMany('InstitutionSubjectStudents', ['className' => 'Institution.InstitutionSubjectStudents', 'dependent' => true, 'cascadeCallbacks' => true]);
 
-		$this->toggle('add', false);
+
+        /*
+            note that in DirectoriesController
+            if ($model instanceof \Staff\Model\Table\StaffSubjectsTable) { 
+            $this->toggle('add', false);
+         */
 		$this->toggle('edit', false);
 		$this->toggle('remove', false);
 	}
 
 	public function indexBeforeAction(Event $event, ArrayObject $extra) {
-		$this->fields['status']['visible'] = false;
-
 		$this->field('academic_period', []);
 		$this->field('institution', []);
 		$this->field('institution_class', []);
@@ -76,4 +80,186 @@ class StaffSubjectsTable extends ControllerActionTable {
 		$this->controller->set('selectedAction', 'Subjects');
 	
 	}
+
+    public function addBeforeAction(Event $event, ArrayObject $extra)
+    {
+        $session = $this->request->session();
+        $staffId = $session->read('Staff.Staff.id');
+        $institutionId = $session->read('Institution.Institutions.id');
+        $institutionName = TableRegistry::get('Institution.Institutions')->get($institutionId)->name;
+
+        $academicPeriodOptions = TableRegistry::get('AcademicPeriod.AcademicPeriods')->getYearList();
+
+        $this->fields = [];
+        $this->field('institution', ['type' => 'readonly', 'attr' => ['value' => $institutionName]]);
+        $this->field('institution_id', ['type' => 'hidden', 'attr' => ['value' => $institutionId]]);
+        $this->field('staff_id', ['type' => 'hidden', 'attr' => ['value' => $staffId]]);
+        $this->field('academic_period_id', ['options' => $academicPeriodOptions, 'onChangeReload' => 'changeAcademicPeriodId', 'attr' => ['required' => true]]);
+
+        $classOptions = [];
+
+        $InstitutionClasses = TableRegistry::get('Institution.InstitutionClasses');
+        if (
+            array_key_exists($this->alias(), $this->request->data)
+             && array_key_exists('academic_period_id', $this->request->data[$this->alias()])
+             && !empty($this->request->data[$this->alias()]['academic_period_id'])) 
+        {
+            $classOptions = $InstitutionClasses->find('list')
+                ->where([
+                    $InstitutionClasses->aliasField('institution_id') => $this->request->data[$this->alias()]['institution_id'],
+                    $InstitutionClasses->aliasField('academic_period_id') => $this->request->data[$this->alias()]['academic_period_id']
+                ])
+                ->toArray()
+                ;
+        }
+
+        $this->field('institution_class_id', ['options' => $classOptions, 'onChangeReload' => 'changeInstitutionClassId', 'attr' => ['required' => true]]);
+
+
+        $subjectOptions = $this->getSubjectOptions();
+
+        $this->field('subjects', [
+            'type' => 'element',
+            'element' => 'Institution.Classes/subjects',
+            'data' => [ 
+                'subjects' => $subjectOptions,
+                'staffId' => $staffId
+            ],
+        ]);
+
+        $extra['subjectOptions'] = $subjectOptions;
+        $extra['staffId'] = $staffId;
+    }
+
+    public function addAfterSave(Event $event, Entity $entity, ArrayObject $data, ArrayObject $extra) {
+        $session = $this->request->session();
+        $staffId = $session->read('Staff.Staff.id');
+        $subjectOptions = $this->getSubjectOptions();
+        $this->field('subjects', [
+            'type' => 'element',
+            'element' => 'Institution.Classes/subjects',
+            'data' => [ 
+                'subjects' => $subjectOptions,
+                'staffId' => $staffId
+            ],
+        ]);
+
+        $extra['subjectOptions'] = $subjectOptions;
+    }
+
+    private function getSubjectOptions() {
+        $subjectOptions = [];
+        if (
+            array_key_exists($this->alias(), $this->request->data)
+             && array_key_exists('institution_class_id', $this->request->data[$this->alias()])
+             && !empty($this->request->data[$this->alias()]['institution_class_id'])) 
+        {
+            //institution_subject_staff
+
+            $subjectOptions = $this->InstitutionSubjects->find()
+                ->matching('Classes', function ($q) {
+                    return $q->where(['Classes.id' => $this->request->data[$this->alias()]['institution_class_id']]);
+                })
+                ->contain([
+                    'Teachers' => function ($q) {
+                        return $q->select(['id', 'first_name', 'middle_name', 'third_name', 'last_name']);
+                    }
+                ])
+                ->where([
+                    $this->InstitutionSubjects->aliasField('institution_id') => $this->request->data[$this->alias()]['institution_id'],
+                    $this->InstitutionSubjects->aliasField('academic_period_id') => $this->request->data[$this->alias()]['academic_period_id']                    
+                ])
+                ->toArray()
+                ;
+            // data massage for teacher names
+            foreach ($subjectOptions as $key => $value) {
+                $tempTeacherArray = [];
+                if ($value->has('teachers')) {
+                    foreach ($value->teachers as $tkey => $tvalue) {
+                        $tempTeacherArray[$tvalue->id] = $tvalue->name;
+                    }
+                }
+                $subjectOptions[$key]->teachers = $tempTeacherArray;
+            }
+        }
+
+        return $subjectOptions;
+    }
+
+
+    public function addBeforeSave(Event $event, Entity $entity, ArrayObject $requestData, ArrayObject $extra) 
+    {
+        $extra['redirect'] = false;
+        $subjectOptions = (array_key_exists('subjectOptions', $extra))? $extra['subjectOptions']: [];
+        $staffId = (array_key_exists('staffId', $extra))? $extra['staffId']: null;
+        $process = function ($model, $entity) use ($requestData, $subjectOptions, $staffId) {
+            if (empty($staffId)) return false;
+            $InstitutionSubjectStaff = TableRegistry::get('Institution.InstitutionSubjectStaff');
+            $result = false;
+            if (array_key_exists('Subjects', $requestData)) {
+                foreach ($requestData['Subjects'] as $key => $value) {
+                    $selectedSubjects[] = $value['subject_id'];
+                }
+            } else {
+                $selectedSubjects = [];
+            }
+
+            foreach ($subjectOptions as $key => $value) {
+                $staffWasIn = false;
+                if (in_array($staffId, array_keys($value->teachers))) {
+                    $staffWasIn = true;
+                }
+
+                if (in_array($value->id, $selectedSubjects)) {
+                    if (!$staffWasIn) {
+                        $InstitutionSubjectStaff->addStaffToSubject($staffId, $value->id);
+                    }
+                } else {
+                    if ($staffWasIn) {
+                        $InstitutionSubjectStaff->removeStaffFromSubject($staffId, $value->id);
+                    }
+                }
+            }
+
+            // not using the regular validation methods, cleaning entity to obtain a success message
+            $entity->clean();
+            return true;
+        };
+        return $process;
+    }
+
+    public function onGetFormButtons(Event $event, ArrayObject $buttons) 
+    {
+        if ($this->action == 'add') {
+            if (array_key_exists('subjects', $this->fields) && empty($this->fields['subjects']['data']['subjects'])) {   
+                // if no options data, do not allow them to save
+                $buttonsArray = $buttons->getArrayCopy();
+                $indexesToRemove = [];
+                foreach ($buttonsArray as $key => $value) {
+                    if (array_key_exists('attr', $value)) {
+                        if (array_key_exists('value', $value['attr'])) {
+                            if ($value['attr']['value'] == 'save') {
+                                // save button identification
+                                $indexesToRemove[] = $key;
+                            }
+                        }
+                    }
+                }
+                foreach ($indexesToRemove as $key => $value) {
+                    // save button removal
+                    unset($buttonsArray[$value]);
+                }
+                $buttons->exchangeArray($buttonsArray);
+            }
+        }
+    }
+
+    public function addOnChangeAcademicPeriodId(Event $event, Entity $entity, ArrayObject $data, ArrayObject $options) {
+        $dataArray = $data->getArrayCopy();
+        if (array_key_exists($this->alias(), $dataArray) && array_key_exists('institution_class_id', $dataArray[$this->alias()]) ) {
+            unset($dataArray[$this->alias()]['institution_class_id']);
+        }
+
+        $data->exchangeArray($dataArray);
+    }
 }
