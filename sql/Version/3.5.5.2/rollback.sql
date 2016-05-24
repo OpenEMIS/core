4d48f08ceceb2caa8c0e37c780d7d159517696d6
Steven Hasanuddin <shasanuddin@kordit.com>
2016-05-24 08:49:49 +0800

    3.5.5.2 - Updated commit.sql and rollback.sql

--- sql/Version/3.5.5.2/rollback.sql
+++ sql/Version/3.5.5.2/rollback.sql
@@ -6,6 +6,21 @@ DROP TABLE `survey_rules`;
 DELETE FROM db_patches WHERE `issue` = 'POCOR-2447';
 
 
+-- POCOR-2784
+-- delete new table and temp
+
+DROP TABLE IF EXISTS `institution_subject_students`;
+
+-- db_rollback
+
+ALTER TABLE `z_2784_institution_subject_students`
+RENAME TO  `institution_subject_students`;
+
+-- db_patches
+
+DELETE FROM `db_patches` WHERE `issue` = 'POCOR-2784';
+
+
 -- POCOR-2723
 UPDATE security_functions
     SET
