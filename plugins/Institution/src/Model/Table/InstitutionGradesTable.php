3523e4ebf1d16e110afba3872fa9b39736230af8
Harry Suhendra <hsuhendra@kordit.com>
2016-05-27 18:55:01 +0800

    POCOR-2580 if the end_date is lesser than today's date then the grade will not appear in the option to add the class

--- plugins/Institution/src/Model/Table/InstitutionGradesTable.php
+++ plugins/Institution/src/Model/Table/InstitutionGradesTable.php
@@ -337,7 +337,10 @@ class InstitutionGradesTable extends AppTable {
 		 * PHPOE-2132, Common statements with getGradeOptionsForIndex() were moved to _gradeOptions().
 		 */
 		$query = $this->find('all')
-					->find('AcademicPeriod', ['academic_period_id' => $academicPeriodId]);
+					->find('AcademicPeriod', ['academic_period_id' => $academicPeriodId])
+					->where([
+						'InstitutionGrades.end_date > CURRENT_DATE OR InstitutionGrades.end_date IS NULL'
+					]);
 		return $this->_gradeOptions($query, $institutionsId, $listOnly);
 	}
 
