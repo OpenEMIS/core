3e8a34c39aea28e3470473901251cded1efe172c
Steven Hasanuddin <shasanuddin@kordit.com>
2016-05-30 17:36:42 +0800

    3.5.6 - Updated commit.sql, rollback.sql

--- sql/Version/3.5.6/commit.sql
+++ sql/Version/3.5.6/commit.sql
@@ -1,3 +1,15 @@
+-- POCOR-3031
+-- db_patches
+INSERT INTO `db_patches` (`issue`, `created`) VALUES ('POCOR-3031', NOW());
+
+-- remove orphan record
+DELETE FROM `institution_staff_position_profiles`
+WHERE NOT EXISTS (
+        SELECT 1 FROM `institution_staff`
+                WHERE `institution_staff`.`id` = `institution_staff_position_profiles`.`institution_staff_id`
+        );
+
+
 -- POCOR-2802
 -- db_patches
 INSERT INTO `db_patches` (issue, created) VALUES ('POCOR-2802', NOW());
