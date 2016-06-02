191fd64ff60116914392073b50093e839547c31e
Steven Hasanuddin <shasanuddin@kordit.com>
2016-06-02 09:56:41 +0800

    3.5.7 - Updated commit.sql and rollback.sql

--- sql/Version/3.5.7/commit.sql
+++ sql/Version/3.5.7/commit.sql
@@ -1,3 +1,18 @@
+-- POCOR-2734
+-- db_patches
+INSERT INTO `db_patches` (`issue`, `created`) VALUES('POCOR-2734', NOW());
+
+-- security_group_users
+-- Re-run patch from POCOR-3003
+UPDATE security_group_users
+JOIN institution_staff s ON s.security_group_user_id = security_group_users.id
+JOIN institution_positions p ON p.id = s.institution_position_id
+JOIN staff_position_titles t
+    ON t.id = p.staff_position_title_id
+    AND t.security_role_id <> security_group_users.security_role_id
+SET security_group_users.security_role_id = t.security_role_id;
+
+
 -- POCOR-2376
 -- db_patches
 INSERT INTO `db_patches` (`issue`, `created`) VALUES ('POCOR-2376', NOW());
