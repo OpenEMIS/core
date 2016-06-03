3cea0dcda1ae2593dbc85d56401d21158f5e0a78
Steven Hasanuddin <shasanuddin@kordit.com>
2016-06-03 10:46:46 +0800

    POCOR-3028 - Updated bootstrap.php, cleaned up unused old test files and directories, added in new test files and directories

---
+++ tests/TestCase/Controller/AreasControllerTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Area\tests\TestCase\Controller;
+
+use Cake\TestSuite\IntegrationTestCase;
+use Cake\ORM\TableRegistry;
+
+class AreasControllerTest extends IntegrationTestCase {
+
+	public function setAuthSession() {
+		
+		$this->session([
+			'Auth' => [
+				'User' => [
+					'id' => 2,
+					'username' => 'admin',
+					'super_admin' => '1'
+				]
+			]
+		]);
+	}
+
+	public function testAreaIndex() {
+
+		$this->setAuthSession();
+		$this->get('/Areas/Areas/index?parent=1');
+		$this->assertResponseCode(200);
+	}
+
+	public function testAddArea() {
+
+ 		$this->setAuthSession();
+
+		$data = [
+			'id' => '2',
+			'code' => 'SGP',
+			'name' => 'Singapore',
+			'parent_id' => 1,
+			'area_level_id' => 2,
+			'order' => 1,
+			'visible' => 1
+		];
+
+		$this->post('/Areas/Areas/add?parent=1', $data);
+
+		$table = TableRegistry::get('Area.Areas');
+		$this->assertNotEmpty($table->get(2));
+	}
+
+	public function testViewArea() {
+
+ 		$this->setAuthSession();
+		$this->get('/Areas/Areas/index?parent=2');
+		$this->assertResponseCode(200);
+	}
+
+	public function testEditArea() {
+
+ 		$this->setAuthSession();
+
+		$data = [
+			'code' => 'JPN',
+			'name' => 'Japan',
+			'area_level_id' => 2,
+		];
+
+		$this->post('/Areas/Areas/edit/2?parent=1', $data);
+
+		$table = TableRegistry::get('Area.Areas');
+		$entity = $table->get(2);
+		$this->assertEquals($data['code'], $entity->code);
+	}
+
+	// public function testDeleteArea() {
+
+	// 	$this->setAuthSession();
+	// 	$this->post('/Areas/Areas/remove/2?parent=1');
+	// 	$table = TableRegistry::get('Area.Areas');
+	// 	$entity = $table->get(2);
+	// 	$this->assertEmpty($entity);
+	// }
+}
