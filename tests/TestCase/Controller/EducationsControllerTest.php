ec4ad152d21df9fd80b8eb8fba175d5efd1f082a
Steven Hasanuddin <shasanuddin@kordit.com>
2016-06-03 14:33:30 +0800

    POCOR-3028 - Fixed REMOTE_ADDR notice error in LocalAuthComponent.php, added default testdatasource.php, added more test cases

--- tests/TestCase/Controller/EducationsControllerTest.php
+++ tests/TestCase/Controller/EducationsControllerTest.php
@@ -3,6 +3,7 @@
 namespace Education\tests\TestCase\Controller;
 
 use Cake\TestSuite\IntegrationTestCase;
+use Cake\ORM\TableRegistry;
 
 class EducationsControllerTest extends IntegrationTestCase {
 
@@ -19,10 +20,68 @@ class EducationsControllerTest extends IntegrationTestCase {
 		]);
 	}
 
-	public function testEducationIndex() {
+	public function testEducationSystemIndex() {
 
 		$this->setAuthSession();
 		$this->get('/Educations/Systems');
 		$this->assertResponseCode(200);
 	}
+
+	public function testAddEducationSystem() {
+
+ 		$this->setAuthSession();
+
+		$data = [
+			'id' => 1,
+			'name' => 'National Education System'
+		];
+
+		$this->post('/Educations/Systems/add', $data);
+
+		$table = TableRegistry::get('Education.EducationSystems');
+		$this->assertNotEmpty($table->get(1));
+	}
+
+	public function testViewEducationSystem() {
+
+		$this->setAuthSession();
+
+		$this->setAuthSession();
+		$this->get('/Educations/Systems/view/1');
+		$this->assertResponseCode(200);
+	}
+
+	public function testEditEducationSystem() {
+
+ 		$this->setAuthSession();
+
+		$data = [
+			'name' => 'PHPUnit Education System'
+		];
+
+		$this->post('/Educations/Systems/edit/1', $data);
+
+		$table = TableRegistry::get('Education.EducationSystems');
+		$entity = $table->get(1);
+		$this->assertEquals($data['name'], $entity->name);
+	}
+
+	public function testDeleteEducationSystem() {
+
+ 		$this->setAuthSession();
+
+ 		$this->get('Educations/Systems/remove/1');
+ 		$this->assertResponseCode(200);
+
+		$data = [
+			'id' => 1,
+			'_method' => 'DELETE',
+		];
+
+		$this->post('/Educations/Systems/remove/1', $data);
+		$table = TableRegistry::get('Education.EducationSystems');
+		$exists = $table->exists([$table->primaryKey() => 1]);
+		$this->assertFalse($exists);
+	}
+
 }
