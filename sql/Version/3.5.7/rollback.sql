db081c6e87964b9fab01c38ded69fc650553f094
Steven Hasanuddin <shasanuddin@kordit.com>
2016-06-08 17:11:40 +0800

    3.5.7 - Updated commit.sql and rollback.sql

--- sql/Version/3.5.7/rollback.sql
+++ sql/Version/3.5.7/rollback.sql
@@ -1,3 +1,12 @@
+-- POCOR-3058
+-- code here
+UPDATE `area_administratives` SET `parent_id` = -1 WHERE `parent_id` IS NULL;
+UPDATE `areas` SET `parent_id` = -1 WHERE `parent_id` IS NULL;
+
+-- db_patches
+DELETE FROM `db_patches` WHERE `issue` = 'POCOR-3058';
+
+
 -- POCOR-2335
 -- staff_training_needs
 DROP TABLE `staff_training_needs`;
