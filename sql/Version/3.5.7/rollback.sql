bc806f7678dead1968426533333a4db59935a398
Steven Hasanuddin <shasanuddin@kordit.com>
2016-06-08 10:54:32 +0800

    3.5.7 - Updated changelog, commit.sql, and rollback.sql

--- sql/Version/3.5.7/rollback.sql
+++ sql/Version/3.5.7/rollback.sql
@@ -1,3 +1,12 @@
+-- POCOR-2335
+-- staff_training_needs
+DROP TABLE `staff_training_needs`;
+ALTER TABLE `z_2335_staff_training_needs` RENAME `staff_training_needs`;
+
+-- db_patches
+DELETE FROM `db_patches` WHERE `issue`='POCOR-2335';
+
+
 -- POCOR-2780
 --
 
