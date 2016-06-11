1c42f2813107a80ab32eda3f0b13e50858c025e9
Steven Hasanuddin <shasanuddin@kordit.com>
2016-06-11 22:31:18 +0800

    3.5.7 - Updated commit.sql and rollback.sql for POCOR-3058 and POCOR-3068

--- sql/Version/3.5.7/rollback.sql
+++ sql/Version/3.5.7/rollback.sql
@@ -1,3 +1,23 @@
+-- POCOR-3068
+-- code here
+INSERT INTO `security_functions` (`id`, `name`, `controller`, `module`, `category`, `parent_id`, `_view`, `_edit`, `_add`, `_delete`, `_execute`, `order`, `visible`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES ('5032', 'Backup', 'Database', 'Administration', 'Database', '5000', NULL, NULL, NULL, NULL, 'backup', '5032', '1', '1', NULL, '1', NOW());
+INSERT INTO `security_functions` (`id`, `name`, `controller`, `module`, `category`, `parent_id`, `_view`, `_edit`, `_add`, `_delete`, `_execute`, `order`, `visible`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES ('5033', 'Restore', 'Database', 'Administration', 'Database', '5000', NULL, NULL, NULL, NULL, 'restore', '5033', '1', '1', NULL, '1', NOW());
+
+
+-- db_patches
+DELETE FROM `db_patches` WHERE `issue` = 'POCOR-3068';
+
+
+-- POCOR-3058
+-- code here
+UPDATE `area_administratives` SET `parent_id` = -1 WHERE `parent_id` IS NULL;
+UPDATE `areas` SET `parent_id` = -1 WHERE `parent_id` IS NULL;
+
+
+-- db_patches
+DELETE FROM `db_patches` WHERE `issue` = 'POCOR-3058';
+
+
 -- POCOR-2335
 -- staff_training_needs
 DROP TABLE `staff_training_needs`;
