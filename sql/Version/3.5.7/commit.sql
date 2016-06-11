bf460cc9036c11716fd7769f9cb2751625385dac
Steven Hasanuddin <shasanuddin@kordit.com>
2016-06-11 22:31:18 +0800

    3.5.7 - Updated commit.sql and rollback.sql for POCOR-3058 and POCOR-3068

--- sql/Version/3.5.7/commit.sql
+++ sql/Version/3.5.7/commit.sql
@@ -1,3 +1,23 @@
+-- POCOR-3068
+-- db_patches
+INSERT INTO `db_patches` (issue, created) VALUES ('POCOR-3068', NOW());
+
+
+-- code here
+DELETE FROM `security_functions` WHERE `security_functions`.`id` = 5032;
+DELETE FROM `security_functions` WHERE `security_functions`.`id` = 5033;
+
+
+-- POCOR-3058
+-- db_patches
+INSERT INTO `db_patches` (issue, created) VALUES ('POCOR-3058', NOW());
+
+
+-- code here
+UPDATE `area_administratives` SET `parent_id` = NULL WHERE `parent_id` = -1;
+UPDATE `areas` SET `parent_id` = NULL WHERE `parent_id` = -1;
+
+
 -- POCOR-2335
 -- db_patches
 INSERT INTO `db_patches` (issue, created) VALUES ('POCOR-2335', NOW());
