6c2db4405aee435a1a5a6fac655513cd13dd5653
Steven Hasanuddin <shasanuddin@kordit.com>
2016-06-15 14:27:53 +0800

    3.5.8 - Updated commit.sql and rollback.sql for POCOR-2780

--- sql/Version/3.5.8/rollback.sql
+++ sql/Version/3.5.8/rollback.sql
@@ -1,2 +1,14 @@
+-- POCOR-2780
+--
+
+DROP TABLE IF EXISTS `import_mapping`;
+RENAME TABLE `z_2780_import_mapping` TO `import_mapping`;
+
+DROP TABLE IF EXISTS `security_functions`;
+RENAME TABLE `z_2780_security_functions` TO `security_functions`;
+
+DELETE FROM `db_patches` WHERE `issue` = 'POCOR-2780';
+
+
 -- 3.5.7
 UPDATE config_items SET value = '3.5.7' WHERE code = 'db_version';
