2b92b553afcae96cf49aa228f1b710690f4ffeb8
Steven Hasanuddin <shasanuddin@kordit.com>
2016-06-16 16:36:22 +0800

    POCOR-2780 - Removed 'Optional' from description

--- sql/Patch/POCOR-2780/commit.sql
+++ sql/Patch/POCOR-2780/commit.sql
@@ -7,7 +7,7 @@ INSERT INTO `db_patches` (issue, created) VALUES ('POCOR-2780', NOW());
 INSERT INTO `import_mapping` (`id`, `model`, `column_name`, `description`, `order`, `foreign_key`, `lookup_plugin`, `lookup_model`, `lookup_column`) VALUES
 (81, 'Institution.Staff', 'institution_position_id', 'Code', 1, 2, 'Institution', 'InstitutionPositions', 'position_no'),
 (82, 'Institution.Staff', 'start_date', '( DD/MM/YYYY )', 2, 0, NULL, NULL, NULL),
-(83, 'Institution.Staff', 'position_type', 'Code (Optional)', 3, 3, NULL, 'PositionTypes', 'id'),
+(83, 'Institution.Staff', 'position_type', 'Code', 3, 3, NULL, 'PositionTypes', 'id'),
 (84, 'Institution.Staff', 'FTE', '(Not Required if Position Type is Full Time)', 4, 3, NULL, 'FTE', 'value'),
 (85, 'Institution.Staff', 'staff_type_id', 'Code', 5, 1, 'FieldOption', 'StaffTypes', 'code'),
 (86, 'Institution.Staff', 'staff_id', 'OpenEMIS ID', 6, 2, 'Staff', 'Staff', 'openemis_no')
