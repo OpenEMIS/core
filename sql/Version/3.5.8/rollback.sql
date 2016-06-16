95f181cbf7056e242282055df72610c77affd88c
Steven Hasanuddin <shasanuddin@kordit.com>
2016-06-16 09:01:07 +0800

    3.5.8 - Updated commit.sql and rollback.sql for POCOR-3037

--- sql/Version/3.5.8/rollback.sql
+++ sql/Version/3.5.8/rollback.sql
@@ -1,3 +1,12 @@
+-- POCOR-3037
+-- code here
+DELETE FROM `security_functions` WHERE `security_functions`.`id` = 7047;
+
+
+-- db_patches
+DELETE FROM `db_patches` WHERE `issue` = 'POCOR-3037';
+
+
 -- POCOR-2780
 --
 
