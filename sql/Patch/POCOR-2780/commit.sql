867caf15b273ac3353d9e6e51e838b5efe036c48
Jeff Zheng <jzheng@kordit.com>
2016-06-16 09:26:07 +0800

    POCOR-2780 - Removed backup sql from commit.sql and added fixed id for import_mapping

--- sql/Patch/POCOR-2780/commit.sql
+++ sql/Patch/POCOR-2780/commit.sql
@@ -4,24 +4,14 @@
 
 INSERT INTO `db_patches` (issue, created) VALUES ('POCOR-2780', NOW());
 
-CREATE TABLE `z_2780_import_mapping` LIKE `import_mapping`;
-INSERT INTO `z_2780_import_mapping` 
-SELECT *
-FROM `import_mapping`;
-
-INSERT INTO `import_mapping` (`model`, `column_name`, `description`, `order`, `foreign_key`, `lookup_plugin`, `lookup_model`, `lookup_column`) VALUES
-('Institution.Staff', 'institution_position_id', 'Code', 1, 2, 'Institution', 'InstitutionPositions', 'position_no'),
-('Institution.Staff', 'start_date', '( DD/MM/YYYY )', 2, 0, NULL, NULL, NULL),
-('Institution.Staff', 'position_type', 'Code (Optional)', 3, 3, NULL, 'PositionTypes', 'id'),
-('Institution.Staff', 'FTE', '(Not Required if Position Type is Full Time)', 4, 3, NULL, 'FTE', 'value'),
-('Institution.Staff', 'staff_type_id', 'Code', 5, 1, 'FieldOption', 'StaffTypes', 'code'),
-('Institution.Staff', 'staff_id', 'OpenEMIS ID', 6, 2, 'Staff', 'Staff', 'openemis_no')
+INSERT INTO `import_mapping` (`id`, `model`, `column_name`, `description`, `order`, `foreign_key`, `lookup_plugin`, `lookup_model`, `lookup_column`) VALUES
+(81, 'Institution.Staff', 'institution_position_id', 'Code', 1, 2, 'Institution', 'InstitutionPositions', 'position_no'),
+(82, 'Institution.Staff', 'start_date', '( DD/MM/YYYY )', 2, 0, NULL, NULL, NULL),
+(83, 'Institution.Staff', 'position_type', 'Code (Optional)', 3, 3, NULL, 'PositionTypes', 'id'),
+(84, 'Institution.Staff', 'FTE', '(Not Required if Position Type is Full Time)', 4, 3, NULL, 'FTE', 'value'),
+(85, 'Institution.Staff', 'staff_type_id', 'Code', 5, 1, 'FieldOption', 'StaffTypes', 'code'),
+(86, 'Institution.Staff', 'staff_id', 'OpenEMIS ID', 6, 2, 'Staff', 'Staff', 'openemis_no')
 ;
-
-CREATE TABLE `z_2780_security_functions` LIKE `security_functions`;
-INSERT INTO `z_2780_security_functions` 
-SELECT *
-FROM `security_functions`;
 
 DELETE FROM `security_functions` WHERE `id` = 7047;
 INSERT INTO `security_functions` (`id`, `name`, `controller`, `module`, `category`, `parent_id`, `_view`, `_edit`, `_add`, `_delete`, `_execute`, `order`, `visible`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES
