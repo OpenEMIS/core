0147536104d7a99ed526a11a10187d0200a4a47d
Chiang Hao Long <hchiang@kordit.com>
2016-06-27 16:01:53 +0800

    POCOR-2978 - Resolve merge conflict

--- tests/TestCase/Controller/EducationsControllerTest.php
+++ tests/TestCase/Controller/EducationsControllerTest.php
@@ -2,30 +2,10 @@
 
 namespace Education\tests\TestCase\Controller;
 
-<<<<<<< HEAD
-use Cake\TestSuite\IntegrationTestCase;
-use Cake\ORM\TableRegistry;
-
-class EducationsControllerTest extends IntegrationTestCase {
-
-	public function setAuthSession() {
-		
-		$this->session([
-			'Auth' => [
-				'User' => [
-					'id' => 2,
-					'username' => 'admin',
-					'super_admin' => '1'
-				]
-			]
-		]);
-	}
-=======
 use App\Test\AppTestCase;
 use Cake\ORM\TableRegistry;
 
 class EducationsControllerTest extends AppTestCase {
->>>>>>> origin_ssh/POCOR-2978-dev
 
 	public function testEducationSystemIndex() {
 
@@ -42,12 +22,7 @@ class EducationsControllerTest extends AppTestCase {
 			'id' => 1,
 			'name' => 'National Education System'
 		];
-<<<<<<< HEAD
-		$this->enableSecurityToken();
-		$this->post('/Educations/Systems/add', $data);
-=======
 		$this->postData('/Educations/Systems/add', $data);
->>>>>>> origin_ssh/POCOR-2978-dev
 
 		$table = TableRegistry::get('Education.EducationSystems');
 		$this->assertNotEmpty($table->get(1));
@@ -69,13 +44,7 @@ class EducationsControllerTest extends AppTestCase {
 		$data = [
 			'name' => 'PHPUnit Education System'
 		];
-<<<<<<< HEAD
-		$this->enableSecurityToken();
-		$this->post('/Educations/Systems/edit/1', $data);
-=======
 		$this->postData('/Educations/Systems/edit/1', $data);
-
->>>>>>> origin_ssh/POCOR-2978-dev
 		$table = TableRegistry::get('Education.EducationSystems');
 		$entity = $table->get(1);
 		$this->assertEquals($data['name'], $entity->name);
@@ -84,11 +53,6 @@ class EducationsControllerTest extends AppTestCase {
 	public function testDeleteEducationSystem() {
 
  		$this->setAuthSession();
-<<<<<<< HEAD
- 		$this->enableSecurityToken();
-=======
-
->>>>>>> origin_ssh/POCOR-2978-dev
  		$this->get('Educations/Systems/remove/1');
  		$this->assertResponseCode(200);
 
@@ -96,12 +60,7 @@ class EducationsControllerTest extends AppTestCase {
 			'id' => 1,
 			'_method' => 'DELETE',
 		];
-<<<<<<< HEAD
-		$this->enableSecurityToken();
-		$this->post('/Educations/Systems/remove/1', $data);
-=======
 		$this->postData('/Educations/Systems/remove/1', $data);
->>>>>>> origin_ssh/POCOR-2978-dev
 		$table = TableRegistry::get('Education.EducationSystems');
 		$exists = $table->exists([$table->primaryKey() => 1]);
 		$this->assertFalse($exists);
