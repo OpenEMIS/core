40854327397e1d22e7960612b4678cf67687fe91
Harry Suhendra <hsuhendra@kordit.com>
2016-07-05 15:42:33 +0800

    POCOR-3103 - Update the field_options table. - Add CommentTypesTable.php to User Plugin. - Add association between CommentTypesTable and CommentsTable.

--- sql/Patch/POCOR-3103/rollback.sql
+++ sql/Patch/POCOR-3103/rollback.sql
@@ -1,5 +1,13 @@
 -- -- code here
+-- field_options table
+DELETE FROM `field_options` WHERE `field_options`.`id` = 101;
+
+-- field_option_value
+DELETE FROM `field_option_values` WHERE `field_option_id` = 101;
+
+-- user_comments table
 ALTER TABLE `user_comments` DROP COLUMN `comment_type_id`;
+
 
 
 -- db_patches
