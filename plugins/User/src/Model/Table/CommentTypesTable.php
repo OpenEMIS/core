aea9f28463a60221341ec4b053c962f5f0ace0a9
Harry Suhendra <hsuhendra@kordit.com>
2016-07-07 14:04:04 +0800

    POCOR-3103 - Updating comment_types to fieldOptions v4

--- plugins/User/src/Model/Table/CommentTypesTable.php
+++ plugins/User/src/Model/Table/CommentTypesTable.php
@@ -1,11 +1,12 @@
 <?php
 namespace User\Model\Table;
 
-use App\Model\Table\AppTable;
+use App\Model\Table\ControllerActionTable;
 
-class CommentTypesTable extends AppTable {
+class CommentTypesTable extends ControllerActionTable {
     public function initialize(array $config) {
         $this->addBehavior('ControllerAction.FieldOption');
+        $this->table('comment_types');
         parent::initialize($config);
         $this->hasMany('Comments', ['className' => 'User.Comment', 'foreignKey' => 'comment_type_id']);
     }
