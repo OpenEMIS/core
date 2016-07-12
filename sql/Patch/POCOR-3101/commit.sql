3e4698332813caf6d8889aab4490eb4294b43b28
Chiang Hao Long <hchiang@kordit.com>
2016-07-12 14:18:13 +0800

    POCOR-3101 - Added additional condition to checking the affected record. To update the student record base on the affected records from the backup table.

--- sql/Patch/POCOR-3101/commit.sql
+++ sql/Patch/POCOR-3101/commit.sql
@@ -7,7 +7,7 @@ INSERT INTO `z_3101_institution_students`
 SELECT intStud2.*
 FROM institution_students intStud2
 INNER JOIN (
-	SELECT intStud1.id, intStud1.created, intStud1.student_id
+	SELECT intStud1.id, intStud1.created, intStud1.student_id, intStud1.institution_id, intStud1.academic_period_id
 	FROM institution_students intStud1
 		INNER JOIN (
 			SELECT student_id
@@ -23,7 +23,10 @@ INNER JOIN (
 		) dup
 			ON intStud1.student_id = dup.student_id
 ) stud
-	ON stud.student_id = intStud2.student_id AND intStud2.created < stud.created
+	ON stud.student_id = intStud2.student_id 
+    AND intStud2.created < stud.created
+    AND stud.institution_id = intStud2.institution_id
+    AND stud.academic_period_id = intStud2.academic_period_id
 WHERE intStud2.student_status_id = 
 (
 	SELECT id 
@@ -34,38 +37,10 @@ GROUP BY intStud2.id;
 
 -- Patch enrolled records for each student that has a count of more than 1
 UPDATE institution_students
+INNER JOIN z_3101_institution_students 
+	ON institution_students.id = z_3101_institution_students.id
 SET institution_students.student_status_id = (
 	SELECT id 
 	FROM student_statuses 
 	WHERE code = 'TRANSFERRED'
-)  
-WHERE institution_students.id IN 
-(SELECT tmpTable.id FROM (
-	SELECT intStud2.id
-	FROM institution_students intStud2
-	INNER JOIN (
-		SELECT intStud1.id, intStud1.created, intStud1.student_id
-		FROM institution_students intStud1
-			INNER JOIN (
-				SELECT student_id
-				FROM institution_students
-				WHERE institution_students.student_status_id = 
-				(
-					SELECT id 
-					FROM student_statuses 
-					WHERE code = 'CURRENT'
-				)
-				GROUP BY student_id
-				HAVING COUNT(student_id) > 1
-			) dup
-				ON intStud1.student_id = dup.student_id
-	) stud
-		ON stud.student_id = intStud2.student_id AND intStud2.created < stud.created
-	WHERE intStud2.student_status_id = 
-	(
-		SELECT id 
-		FROM student_statuses 
-		WHERE code = 'CURRENT'
-	)
-	GROUP BY intStud2.id
-) tmpTable);
+);
