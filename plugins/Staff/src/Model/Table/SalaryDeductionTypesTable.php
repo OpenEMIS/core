64c9f221f3a3e5ff490b5e696eaae32191dbef80
Harry Suhendra <hsuhendra@kordit.com>
2016-07-13 09:50:39 +0800

    POCOR-3179 - Field Options to CAv4.

--- plugins/Staff/src/Model/Table/SalaryDeductionTypesTable.php
+++ plugins/Staff/src/Model/Table/SalaryDeductionTypesTable.php
@@ -1,12 +1,12 @@
 <?php
-namespace FieldOption\Model\Table;
+namespace Staff\Model\Table;
 
-use App\Model\Table\AppTable;
-use Cake\Validation\Validator;
+use App\Model\Table\ControllerActionTable;
 
-class SalaryDeductionTypesTable extends AppTable {
+class SalaryDeductionTypesTable extends ControllerActionTable {
 	public function initialize(array $config) {
 		$this->addBehavior('ControllerAction.FieldOption');
+        $this->table('salary_deduction_types');
 		parent::initialize($config);
 
 		$this->hasMany('StaffSalaryDeductions', ['className' => 'Staff.SalaryDeductions', 'foreignKey' => 'salary_deduction_type_id']);
