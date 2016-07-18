72c62fd560eec45e09ad6f408af7f5c5d56e8593
Martinnus Budiarto <mbudiarto@kordit.com>
2016-07-18 11:48:45 +0800

    POCOR-3198 - adjust the commit.sql based on the review.

--- sql/Patch/POCOR-3198/commit.sql
+++ sql/Patch/POCOR-3198/commit.sql
@@ -13,7 +13,7 @@ WHERE `model` = 'Institution.InstitutionStudentAbsences'
 AND `column_name` = 'student_absence_reason_id';
 
 INSERT INTO `import_mapping` (`id`, `model`, `column_name`, `description`, `order`, `foreign_key`, `lookup_plugin`, `lookup_model`, `lookup_column`) 
-VALUES (NULL, 'Institution.StaffAbsences', 'absence_type_id', 'Code', '5', '2', 'Institution', 'AbsenceTypes', 'code');
+VALUES (87, 'Institution.StaffAbsences', 'absence_type_id', 'Code', '5', '2', 'Institution', 'AbsenceTypes', 'code');
 
 INSERT INTO `import_mapping` (`id`, `model`, `column_name`, `description`, `order`, `foreign_key`, `lookup_plugin`, `lookup_model`, `lookup_column`) 
-VALUES (NULL, 'Institution.InstitutionStudentAbsences', 'absence_type_id', 'Code', '5', '2', 'Institution', 'AbsenceTypes', 'code');
+VALUES (88, 'Institution.InstitutionStudentAbsences', 'absence_type_id', 'Code', '5', '2', 'Institution', 'AbsenceTypes', 'code');
