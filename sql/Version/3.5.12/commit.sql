2c4e3d1e2c21bba6ef729dea2c173ec75f0f267b
Steven Hasanuddin <shasanuddin@kordit.com>
2016-07-21 15:59:09 +0800

    3.5.12 - Updated version and sql cleanups

---
+++ sql/Version/3.5.12/commit.sql
@@ -0,0 +1,3 @@
+-- 3.5.12
+UPDATE config_items SET value = '3.5.12' WHERE code = 'db_version';
+UPDATE db_patches SET version = (SELECT value FROM config_items WHERE code = 'db_version') WHERE version IS NULL;
