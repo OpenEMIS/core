480cc25404e0eb61a150ec8fb1a7029ddb0171c8
Harry Suhendra <hsuhendra@kordit.com>
2016-07-21 11:39:32 +0800

    POCOR-3179 - Adding Unique validation to the fieldOption. - Removed LeaveStatusesTable.php and StudentCategoriesTable.php. - Removed field_options and field_option_values table.

--- plugins/FieldOption/src/Model/Table/BankBranchesTable.php
+++ plugins/FieldOption/src/Model/Table/BankBranchesTable.php
@@ -24,16 +24,26 @@ class BankBranchesTable extends ControllerActionTable {
 		}
 	}
 
-	public function validationDefault(Validator $validator)
+	public function validationUpdate($validator)
 	{
-		$validator = parent::validationDefault($validator);
+        $validator
+            ->add('name', [
+                    'ruleUnique' => [
+                        'rule' => 'validateUnique',
+                        'provider' => 'table',
+                        'message' => __('This field has to be unique')
+                    ]
+                ])
+            ->add('code', [
+                    'ruleUnique' => [
+                        'rule' => 'validateUnique',
+                        'provider' => 'table',
+                        'message' => __('This field has to be unique')
+                    ]
+                ]);
 
-		$validator
-			->notEmpty('name', 'Please enter a name.')
-			->notEmpty('code', 'Please enter a code.');;
-
-		return $validator;
-	}
+        return $validator;
+    }
 
 	public function indexBeforeAction(Event $event, ArrayObject $extra)
 	{
