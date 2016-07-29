a67e22c7745222d45fe333a236943c0a35ccf408
Martinnus Budiarto <mbudiarto@kordit.com>
2016-07-29 10:28:29 +0800

    Revert "POCOR-3080 - fix the proper validation for the array_search function."
    This reverts commit c38d2c3cbd89766244cbb8c3cffeabcdb5ff8eac.

--- plugins/Institution/src/Model/Table/ImportStaffTable.php
+++ plugins/Institution/src/Model/Table/ImportStaffTable.php
@@ -247,8 +247,8 @@ class ImportStaffTable extends AppTable
         foreach ($staffRecord as $key => $value) {
             $institutionList[] = $staffRecord[$key]['institution_id'];
         }
-        
-        if ((isset($institutionList)) && (!is_numeric(array_search($this->_institution->id, $institutionList)))) { //if the current session not on the institution list on where the staff assigned to.
+
+        if (isset($institutionList) && !array_search($this->_institution->id, $institutionList)) { //if the current session not on the institution list on where the staff assigned to.
             $rowInvalidCodeCols['staff_id'] = __('The staff is already assigned to another school');
             return false;
         }
