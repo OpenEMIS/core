e8a3cd784112a771fd52e728e21b814a5a7bf2b5
Jeff Zheng <jzheng@kordit.com>
2016-07-29 15:34:58 +0800

    POCOR-2602 - first review

--- plugins/Institution/src/Model/Table/ShiftOptionsTable.php
+++ plugins/Institution/src/Model/Table/ShiftOptionsTable.php
@@ -1,43 +1,66 @@
 <?php
 namespace Institution\Model\Table;
 
-use App\Model\Table\AppTable;
-use Cake\Validation\Validator;
+use Cake\Event\Event;
+use Cake\ORM\Query;
+
 use App\Model\Table\ControllerActionTable;
 
-use Cake\Event\Event;
-use Cake\I18n\Time;
+class ShiftOptionsTable extends ControllerActionTable
+{
+    public function initialize(array $config) {
+        $this->addBehavior('ControllerAction.FieldOption');
+        $this->table('shift_options');
+        parent::initialize($config);
 
-class ShiftOptionsTable extends ControllerActionTable {
-	public function initialize(array $config) {
-		$this->addBehavior('ControllerAction.FieldOption');
-		$this->table('shift_options');
-		parent::initialize($config);
-		//$this->hasMany('SpecialNeeds', ['className' => 'User.SpecialNeeds', 'foreignKey' => 'special_need_difficulty_id']);
+        $this->behaviors()->get('ControllerAction')->config('actions.remove', false);
+        $this->behaviors()->get('ControllerAction')->config('actions.add', false);
 
-		$this->behaviors()->get('ControllerAction')->config('actions.remove', false);
-		$this->behaviors()->get('ControllerAction')->config('actions.add', false);
-	}
+        $this->hasMany('Shifts', ['className' => 'Institution.InstitutionShifts', 'foreignKey' => 'shift_option_id']);
+    }
 
-	public function beforeAction(Event $event) {
-		$this->field('default', ['visible' => false]);
-		$this->field('editable', ['visible' => false]);
-		$this->field('international_code', ['visible' => false]);
-		$this->field('national_code', ['visible' => false]);
-		$this->field('start_time', ['after' => 'name']);
-		$this->field('end_time', ['after' => 'start_time']);
-	}
+    public function beforeAction(Event $event)
+    {
+        $this->field('default', ['visible' => false]);
+        $this->field('editable', ['visible' => false]);
+        $this->field('international_code', ['visible' => false]);
+        $this->field('national_code', ['visible' => false]);
+        $this->field('start_time', ['after' => 'name']);
+        $this->field('end_time', ['after' => 'start_time']);
+    }
 
-	public function indexBeforeAction(Event $event) {
-		$this->field('default', ['visible' => false]);
-		$this->field('editable', ['visible' => false]);
-		$this->field('international_code', ['visible' => false]);
-		$this->field('national_code', ['visible' => false]);
-	}
+    public function indexBeforeAction(Event $event)
+    {
+        $this->field('default', ['visible' => false]);
+        $this->field('editable', ['visible' => false]);
+        $this->field('international_code', ['visible' => false]);
+        $this->field('national_code', ['visible' => false]);
+    }
 
-	public function getStartEndTime($shiftOptionId, $select) {
-		$data = $this->get($shiftOptionId)->toArray();
-		return $data[$select.'_time'];
-	}
+    public function getStartEndTime($shiftOptionId, $select)
+    {
+        $data = $this->get($shiftOptionId)->toArray();
+        return $data[$select.'_time'];
+    }
+
+    public function findAvailableShifts(Query $query, array $options)
+    {
+        if (array_key_exists('institution_id', $options) && array_key_exists('academic_period_id', $options)) {
+            $conditions = [
+                $this->Shifts->aliasField('shift_option_id = ') . $this->aliasField('id'),
+                $this->Shifts->aliasField('institution_id') => $options['institution_id'],
+                $this->Shifts->aliasField('academic_period_id') => $options['academic_period_id']
+            ];
+            $query->leftJoin(
+                [$this->Shifts->alias() => $this->Shifts->table()],
+                $conditions
+            )
+            ->where(
+                [$this->Shifts->aliasField('id IS NULL')]
+            );
+        } else {
+            pr('fields not exists');die;
+        }
+        return $query;
+    }
 }
-?>
