4e630c205cff3afa80f767dc0086a68219b97b7f
Livvi Sim <lsim@kordit.com>
2016-08-05 15:50:23 +0800

    POCOR-3083 - Specified institution for staff field hyperlink in staff transfer approval

--- plugins/Institution/src/Model/Table/StaffTransferApprovalsTable.php
+++ plugins/Institution/src/Model/Table/StaffTransferApprovalsTable.php
@@ -449,7 +449,9 @@ class StaffTransferApprovalsTable extends StaffTransfer {
 					'controller' => 'Institutions',
 					'action' => 'StaffUser',
 					'0' => 'view',
-					'1' => $staffId
+					'1' => $staffId,
+					'institution_id' => $entity->previous_institution_id
+
 				]);
 	}
 }
