1e9972c9aadfed5044be3001b945b4159106ed73
Chiang Hao Long <hchiang@kordit.com>
2016-08-17 17:57:37 +0800

    POCOR-3258 - Added Configuration component

--- plugins/Configuration/src/Model/Table/ProductListsTable.php
+++ plugins/Configuration/src/Model/Table/ProductListsTable.php
@@ -5,6 +5,7 @@ use App\Model\Table\ControllerActionTable;
 
 class ProductListsTable extends ControllerActionTable {
 	public function initialize(array $config) {
+		$this->table('config_product_lists');
 		parent::initialize($config);
 	}
 
