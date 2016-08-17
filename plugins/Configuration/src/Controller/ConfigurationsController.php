943b624af074bf59db78cae50a92b20fd809b5b6
Livvi Sim <lsim@kordit.com>
2016-08-17 16:41:30 +0800

    POCOR-3258 - added Configuration Plugin

---
+++ plugins/Configuration/src/Controller/ConfigurationsController.php
@@ -0,0 +1,28 @@
+<?php
+namespace Configuration\Controller;
+
+use ArrayObject;
+use FieldOption\Controller\AppController;
+use Cake\Event\Event;
+use Cake\ORM\Table;
+use Cake\Utility\Inflector;
+use Cake\ORM\TableRegistry;
+
+class ConfigurationsController extends AppController {
+    public function initialize()
+    {
+        parent::initialize();
+    }
+
+    public function onInitialize(Event $event, Table $model, ArrayObject $extra)
+    {
+        $alias = $model->alias;
+        $header = __('Configuration') . ' - ' . $model->getHeader($alias);
+
+        $this->Navigation->addCrumb($model->getHeader($alias));
+
+        $this->set('contentHeader', $header);
+    }
+
+    public function ProductLists()                       { $this->ControllerAction->process(['alias' => __FUNCTION__, 'className' => 'Configuration.ProductLists']); }
+}
