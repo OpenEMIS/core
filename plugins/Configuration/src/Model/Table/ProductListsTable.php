f27924376e82ee9e549931cdd4537c82a14214fc
Livvi Sim <lsim@kordit.com>
2016-08-17 16:41:30 +0800

    POCOR-3258 - added Configuration Plugin

---
+++ plugins/Configuration/src/Model/Table/ProductListsTable.php
@@ -0,0 +1,11 @@
+<?php
+namespace Configuration\Model\Table;
+
+use App\Model\Table\ControllerActionTable;
+
+class ProductListsTable extends ControllerActionTable {
+	public function initialize(array $config) {
+		parent::initialize($config);
+	}
+
+}
