30e71f49b5c366397fb318fa80f0a8fd44072279
Malcolm Lee <mlee@kordit.com>
2016-08-25 09:10:39 +0800

    POCOR-2760 - SQL patch edit

--- sql/Patch/POCOR-2760/commit.sql
+++ sql/Patch/POCOR-2760/commit.sql
@@ -5,8 +5,8 @@ UPDATE `security_functions` SET _delete = NULL WHERE id = 5003;
 -- SELECT * FROM `security_functions` WHERE id = 5003;
 
 -- BACKING UP
-CREATE TABLE z2760_security_role_functions LIKE security_role_functions;
-INSERT INTO z2760_security_role_functions SELECT * FROM security_role_functions WHERE security_function_id = 5003;
+CREATE TABLE z_2760_security_role_functions LIKE security_role_functions;
+INSERT INTO z_2760_security_role_functions SELECT * FROM security_role_functions WHERE security_function_id = 5003;
 
 -- DELETING ASSOCIATED RECORDS
 UPDATE security_role_functions SET _delete = 0 WHERE security_function_id = 5003;
