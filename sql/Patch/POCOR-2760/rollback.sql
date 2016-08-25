c1379baaddaa5ecfcc53d65d9556c3c15ac9cd01
Malcolm Lee <mlee@kordit.com>
2016-08-25 09:10:39 +0800

    POCOR-2760 - SQL patch edit

--- sql/Patch/POCOR-2760/rollback.sql
+++ sql/Patch/POCOR-2760/rollback.sql
@@ -3,10 +3,10 @@ UPDATE `security_functions` SET _delete = 'Periods.remove' WHERE id = 5003;
 
 -- BACKING UP
 UPDATE `security_role_functions`
-    JOIN `z2760_security_role_functions` ON z2760_security_role_functions.id = security_role_functions.id
-        SET security_role_functions._delete = z2760_security_role_functions._delete;
+    JOIN `z_2760_security_role_functions` ON z_2760_security_role_functions.id = security_role_functions.id
+        SET security_role_functions._delete = z_2760_security_role_functions._delete;
 
-DROP TABLE z2760_security_role_functions;
+DROP TABLE z_2760_security_role_functions;
 
 -- db_patches
 DELETE FROM `db_patches` WHERE `issue`='POCOR-2760';
