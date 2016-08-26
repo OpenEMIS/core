075ec43de2e18855075632df146b58dd8f32dac5
Chiang Hao Long <hchiang@kordit.com>
2016-08-26 10:02:23 +0800

    POCOR-3303 - Added logic for generation of results report for class and institution level

--- plugins/Assessment/src/Model/Table/AssessmentItemsGradingTypesTable.php
+++ plugins/Assessment/src/Model/Table/AssessmentItemsGradingTypesTable.php
@@ -23,5 +23,22 @@ class AssessmentItemsGradingTypesTable extends AppTable {
         if ($entity->isNew()) {
             $entity->id = Text::uuid();
         }
-	}
+    }
+
+    public function getAssessmentGradeTypes($assessmentId) {
+        $gradeTypes = $this->find('list', [
+                'keyField' => 'period_id',
+                'groupField' => 'item_id',
+                'valueField' => 'type'
+            ])
+            ->matching('AssessmentGradingTypes')
+            ->select([
+                'period_id' => $this->aliasField('assessment_period_id'),
+                'item_id' => $this->aliasField('assessment_item_id'),
+                'type' => 'AssessmentGradingTypes.result_type'
+            ])
+            ->where([$this->aliasField('assessment_id') => $assessmentId])
+            ->toArray();
+        return $gradeTypes;
+    }
 }
