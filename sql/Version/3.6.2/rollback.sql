c16d211099cc322acf351210e086bd67ceaa7cbb
Steven Hasanuddin <shasanuddin@kordit.com>
2016-08-30 16:58:12 +0800

    3.6.2 - Updated commit.sql and rollback.sql for POCOR-3303

--- sql/Version/3.6.2/rollback.sql
+++ sql/Version/3.6.2/rollback.sql
@@ -1,3 +1,11 @@
+-- POCOR-3303
+-- security_functions
+UPDATE `security_functions` SET `_execute` = 'Assessments.excel' WHERE `id` = 1015;
+
+-- db_patches
+DELETE FROM `db_patches` WHERE `issue` = 'POCOR-3303';
+
+
 -- POCOR-3080
 DROP TABLE IF EXISTS `assessment_items_grading_types`;
 
