56a9856bf8426707ab7e9fb092e8fb007ab2a138
Steven Hasanuddin <shasanuddin@kordit.com>
2016-08-31 15:39:47 +0800

    3.6.3 - Updated version, commit.sql, rollback.sql, and performed sql cleanups

---
+++ sql/Version/3.6.3/rollback.sql
@@ -0,0 +1,14 @@
+-- POCOR-3338
+-- workflow_actions
+UPDATE `workflow_actions`
+INNER JOIN `z_3338_workflow_actions` ON `z_3338_workflow_actions`.`id` = `workflow_actions`.`id`
+SET `workflow_actions`.`event_key` = `z_3338_workflow_actions`.`event_key`;
+
+DROP TABLE `z_3338_workflow_actions`;
+
+-- db_patches
+DELETE FROM `db_patches` WHERE `issue` = 'POCOR-3338';
+
+
+-- 3.6.2
+UPDATE config_items SET value = '3.6.2' WHERE code = 'db_version';
