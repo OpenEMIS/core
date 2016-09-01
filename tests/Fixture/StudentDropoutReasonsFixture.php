227dc586a52aba39011187c59bf92e870fe29fcb
Livvi Sim <lsim@kordit.com>
2016-09-01 18:15:01 +0800

    POCOR-3318 - Added fixtures and controller tests for unit testing

--- tests/Fixture/StudentDropoutReasonsFixture.php
+++ tests/Fixture/StudentDropoutReasonsFixture.php
@@ -6,6 +6,138 @@ use Cake\TestSuite\Fixture\TestFixture;
 class StudentDropoutReasonsFixture extends TestFixture
 {
     public $import = ['table' => 'student_dropout_reasons'];
-    public $records = [];
+    public $records = [
+         [
+            'id' => 649,
+            'name' => 'Family Issues',
+            'order' => 5,
+            'visible' => 1,
+            'editable' => 1,
+            'default' => 0,
+            'international_code' => NULL,
+            'national_code' => NULL,
+            'modified_user_id' => NULL,
+            'modified' => NULL,
+            'created_user_id' => 1,
+            'created' => '2015-01-01 00:00:00'
+        ], [
+            'id' => 660,
+            'name' => 'Expelled',
+            'order' => 3,
+            'visible' => 1,
+            'editable' => 1,
+            'default' => 0,
+            'international_code' => NULL,
+            'national_code' => NULL,
+            'modified_user_id' => NULL,
+            'modified' => NULL,
+            'created_user_id' => 1,
+            'created' => '2015-01-01 00:00:00'
+        ], [
+            'id' => 661,
+            'name' => 'Financial Issues',
+            'order' => 6,
+            'visible' => 1,
+            'editable' => 1,
+            'default' => 0,
+            'international_code' => NULL,
+            'national_code' => NULL,
+            'modified_user_id' => NULL,
+            'modified' => NULL,
+            'created_user_id' => 1,
+            'created' => '2015-01-01 00:00:00'
+        ], [
+            'id' => 662,
+            'name' => 'Residential/School Instability',
+            'order' => 8,
+            'visible' => 1,
+            'editable' => 1,
+            'default' => 0,
+            'international_code' => NULL,
+            'national_code' => NULL,
+            'modified_user_id' => NULL,
+            'modified' => NULL,
+            'created_user_id' => 1,
+            'created' => '2015-01-01 00:00:00'
+        ], [
+            'id' => 663,
+            'name' => 'Bullied',
+            'order' => 2,
+            'visible' => 1,
+            'editable' => 1,
+            'default' => 0,
+            'international_code' => NULL,
+            'national_code' => NULL,
+            'modified_user_id' => NULL,
+            'modified' => NULL,
+            'created_user_id' => 1,
+            'created' => '2015-01-01 00:00:00'
+        ], [
+            'id' => 664,
+            'name' => 'Pregnancy',
+            'order' => 9,
+            'visible' => 1,
+            'editable' => 1,
+            'default' => 0,
+            'international_code' => NULL,
+            'national_code' => NULL,
+            'modified_user_id' => NULL,
+            'modified' => NULL,
+            'created_user_id' => 1,
+            'created' => '2015-01-01 00:00:00'
+        ], [
+            'id' => 665,
+            'name' => 'Need to make money to support family',
+            'order' => 7,
+            'visible' => 1,
+            'editable' => 1,
+            'default' => 0,
+            'international_code' => NULL,
+            'national_code' => NULL,
+            'modified_user_id' => NULL,
+            'modified' => NULL,
+            'created_user_id' => 1,
+            'created' => '2015-01-01 00:00:00'
+        ], [
+            'id' => 666,
+            'name' => 'Failing too many classes',
+            'order' => 4,
+            'visible' => 1,
+            'editable' => 1,
+            'default' => 0,
+            'international_code' => NULL,
+            'national_code' => NULL,
+            'modified_user_id' => NULL,
+            'modified' => NULL,
+            'created_user_id' => 1,
+            'created' => '2015-01-01 00:00:00'
+        ], [
+            'id' => 667,
+            'name' => 'Medical Problems',
+            'order' => 10,
+            'visible' => 1,
+            'editable' => 1,
+            'default' => 0,
+            'international_code' => NULL,
+            'national_code' => NULL,
+            'modified_user_id' => NULL,
+            'modified' => NULL,
+            'created_user_id' => 1,
+            'created' => '2015-01-01 00:00:00'
+        ], [
+            'id' => 668,
+            'name' => 'Became a caregiver',
+            'order' => 1,
+            'visible' => 1,
+            'editable' => 1,
+            'default' => 0,
+            'international_code' => NULL,
+            'national_code' => NULL,
+            'modified_user_id' => NULL,
+            'modified' => NULL,
+            'created_user_id' => 1,
+            'created' => '2015-01-01 00:00:00'
+        ]
+    ];
 }
 
