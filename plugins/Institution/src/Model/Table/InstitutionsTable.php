13b67eb6d9fef066f5d3075b34fa8414904a97de
Livvi Sim <lsim@kordit.com>
2016-09-06 18:20:17 +0800

    POCOR-3357 - Added logic to retrieve Providers when a Sector is selected

--- plugins/Institution/src/Model/Table/InstitutionsTable.php
+++ plugins/Institution/src/Model/Table/InstitutionsTable.php
@@ -556,7 +556,7 @@ class InstitutionsTable extends AppTable  {
 	public function viewBeforeAction(Event $event) {
 		$this->ControllerAction->setFieldOrder([
 			'information_section',
-			'name', 'alternative_name', 'code', 'institution_provider_id', 'institution_sector_id', 'institution_type_id',
+			'name', 'alternative_name', 'code', 'institution_sector_id', 'institution_provider_id', 'institution_type_id',
 			'institution_ownership_id', 'institution_gender_id', 'institution_network_connectivity_id', 'institution_status_id', 'date_opened', 'date_closed',
 
 			'shift_section',
@@ -588,7 +588,7 @@ class InstitutionsTable extends AppTable  {
 	public function addEditBeforeAction(Event $event) {
 		$this->ControllerAction->setFieldOrder([
 			'information_section',
-			'name', 'alternative_name', 'code', 'institution_provider_id', 'institution_sector_id', 'institution_type_id',
+			'name', 'alternative_name', 'code', 'institution_sector_id', 'institution_provider_id',  'institution_type_id',
 			'institution_ownership_id', 'institution_gender_id', 'institution_network_connectivity_id', 'institution_status_id', 'date_opened', 'date_closed',
 
 			'location_section',
@@ -608,6 +608,45 @@ class InstitutionsTable extends AppTable  {
 	public function addEditAfterAction(Event $event, Entity $entity) {
 		$this->ControllerAction->field('institution_type_id', ['type' => 'select']);
 	}
+
+	public function onUpdateFieldInstitutionSectorId(Event $event, array $attr, $action, Request $request) {
+        $attr['onChangeReload'] = 'changeInstitutionSectorId';
+        return $attr;
+    }
+
+	public function onUpdateFieldInstitutionProviderId(Event $event, array $attr, $action, Request $request) {
+		$providerOptions = [];
+		$selectedSectorId = '';
+
+		if (isset($request->data[$this->alias()]['institution_sector_id'])) {
+            $selectedSectorId = $request->data[$this->alias()]['institution_sector_id'];
+
+        } else { //first load or select --select--
+        	$SectorTable = TableRegistry::get('Institution.Sectors');
+        	$defaultSector = $SectorTable
+        		->find()
+        		->where([$SectorTable->aliasField('default') => 1])
+        		->first();
+
+        	if(!empty($defaultSector)) {
+        		$selectedSectorId = $defaultSector->id;
+        	}
+        }
+
+        if (!empty($selectedSectorId)) {
+	        $ProviderTable = TableRegistry::get('Institution.Providers');
+	   		$providerOptions = $ProviderTable->find('list', [
+					'keyField' => 'id',
+					'valueField' => 'name'
+				])
+				->where([$ProviderTable->aliasField('institution_sector_id') => $selectedSectorId])
+				->toArray();
+		}
+
+        $attr['options'] = $providerOptions;
+        $attr['empty'] = true;
+        return $attr;
+    }
 
 /******************************************************************************************************************
 **
