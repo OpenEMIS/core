992cd761d8f7572fc726fc5297e57fa41c730d74
Livvi Sim <lsim@kordit.com>
2016-09-06 16:06:20 +0800

    POCOR-3357 - Added associations for Sectors and Providers

--- plugins/Institution/src/Model/Table/ProvidersTable.php
+++ plugins/Institution/src/Model/Table/ProvidersTable.php
@@ -2,6 +2,7 @@
 namespace Institution\Model\Table;
 
 use App\Model\Table\ControllerActionTable;
+use Cake\Event\Event;
 
 class ProvidersTable extends ControllerActionTable {
 	public function initialize(array $config)
@@ -11,7 +12,12 @@ class ProvidersTable extends ControllerActionTable {
         parent::initialize($config);
 
 		$this->hasMany('Institutions', ['className' => 'Institution.Institutions', 'foreignKey' => 'institution_provider_id']);
+        $this->belongsTo('Sectors', ['className' => 'Institution.Sectors', 'foreignKey' => 'institution_sector_id']);
 
 		$this->behaviors()->get('ControllerAction')->config('actions.remove', 'transfer');
 	}
+
+    public function addEditBeforeAction(Event $event) {
+        $this->field('institution_sector_id', ['type' => 'select', 'after' => 'name']);
+    }
 }
