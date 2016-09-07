7060c661f60d9392681ef59490c2b121999fdd8c
Steven Hasanuddin <shasanuddin@kordit.com>
2016-09-07 10:08:17 +0800

    3.6.3 - Updated commit.sql and rollback.sql for POCOR-3257

--- sql/Version/3.6.3/rollback.sql
+++ sql/Version/3.6.3/rollback.sql
@@ -1,3 +1,15 @@
+-- POCOR-3257
+-- code here
+DELETE FROM `config_items` WHERE `code` = 'area_api';
+
+-- Auto_increment
+ALTER TABLE `security_functions` CHANGE `id` `id` INT(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `config_items` CHANGE `id` `id` INT(11) NOT NULL AUTO_INCREMENT;
+
+-- db_patches
+DELETE FROM `db_patches` WHERE `issue` = 'POCOR-3257';
+
+
 -- POCOR-3258
 -- drop config_product_lists table
 DROP TABLE `config_product_lists`;
