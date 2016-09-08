18b3802f69105dc18a4c2fd3f4ac2b6d5bb7d182
Chiang Hao Long <hchiang@kordit.com>
2016-09-08 11:32:36 +0800

    POCOR-2378 - Remove tab element from the beforeFilter and added to getExamsTab function

--- plugins/Examination/src/Model/Table/ExaminationGradingTypesTable.php
+++ plugins/Examination/src/Model/Table/ExaminationGradingTypesTable.php
@@ -3,11 +3,17 @@ namespace Examination\Model\Table;
 
 use App\Model\Table\ControllerActionTable;
 use Cake\Event\Event;
+use ArrayObject;
 
 class ExaminationGradingTypesTable extends ControllerActionTable {
     public function initialize(array $config)
     {
         parent::initialize($config);
 
+    }
+
+    public function afterAction(Event $event, ArrayObject $extra)
+    {
+        $this->controller->getExamsTab();
     }
 }
