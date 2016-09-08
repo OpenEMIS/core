cd7f6d7b6a41a6d2dde56345385036606b118fc5
Chiang Hao Long <hchiang@kordit.com>
2016-09-08 11:23:35 +0800

    POCOR-2378 - Added patch file (WIP)

--- sql/Patch/POCOR-2378/commit.sql
+++ sql/Patch/POCOR-2378/commit.sql
@@ -18,3 +18,78 @@ CREATE TABLE `examinations` (
   KEY `created_user_id` (`created_user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='This table contains the examination for a specific grade';
 
+CREATE TABLE `examination_items` (
+  `id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `weight` decimal(6,2) DEFAULT '0.00',
+  `examination_id` int(11) NOT NULL,
+  `education_subject_id` int(11) NOT NULL,
+  `modified_user_id` int(11) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `created_user_id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `examination_id` (`examination_id`),
+  KEY `education_subject_id` (`education_subject_id`),
+  KEY `modified_user_id` (`modified_user_id`),
+  KEY `created_user_id` (`created_user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `examination_grading_types` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `name` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pass_mark` decimal(6,2) NOT NULL,
+  `max` decimal(6,2) NOT NULL,
+  `result_type` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `visible` int(1) NOT NULL DEFAULT '1',
+  `modified_user_id` int(11) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `created_user_id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `code` (`code`),
+  KEY `modified_user_id` (`modified_user_id`),
+  KEY `created_user_id` (`created_user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='This table contains the list of grading types that can be used for an examination subject';
+
+CREATE TABLE `examination_grading_options` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `name` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `min` decimal(6,2) DEFAULT NULL,
+  `max` decimal(6,2) DEFAULT NULL,
+  `order` int(3) NOT NULL,
+  `visible` int(1) NOT NULL DEFAULT '1',
+  `examination_grading_type_id` int(11) NOT NULL COMMENT 'links to examination_grading_types.id',
+  `modified_user_id` int(11) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `created_user_id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `examination_grading_type_id` (`examination_grading_type_id`),
+  KEY `modified_user_id` (`modified_user_id`),
+  KEY `created_user_id` (`created_user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='This table contains all options linked to a specific grading type';
+
+CREATE TABLE `examination_item_results` (
+  `id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `marks` decimal(6,2) DEFAULT NULL,
+  `examination_grading_option_id` int(11) DEFAULT NULL,
+  `student_id` int(11) NOT NULL COMMENT 'links to security_users.id',
+  `examination_id` int(11) NOT NULL,
+  `education_subject_id` int(11) NOT NULL,
+  `institution_id` int(11) NOT NULL,
+  `examination_centre_id` int(11) NULL,
+  `academic_period_id` int(11) NOT NULL,
+  `examination_period_id` int(11) NOT NULL,
+  `modified_user_id` int(11) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `created_user_id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`student_id`,`examination_id`,`education_subject_id`,`institution_id`,`academic_period_id`,`examination_period_id`),
+  KEY `examination_grading_option_id` (`examination_grading_option_id`),
+  KEY `modified_user_id` (`modified_user_id`),
+  KEY `created_user_id` (`created_user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='This table contains all the examination results for an individual student in an institution and examination centre'
+/*!50100 PARTITION BY HASH (`academic_period_id`)
+PARTITIONS 8 */
