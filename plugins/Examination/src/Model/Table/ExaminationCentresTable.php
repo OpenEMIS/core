e064ca1e18be448beea69ff4e9d09286e0ed6a22
Chiang Hao Long <hchiang@kordit.com>
2016-09-08 15:57:39 +0800

    POCOR-2378 - Added examination centre table file

--- plugins/Examination/src/Model/Table/ExaminationCentresTable.php
+++ plugins/Examination/src/Model/Table/ExaminationCentresTable.php
@@ -3,18 +3,41 @@ namespace Examination\Model\Table;
 
 use App\Model\Table\ControllerActionTable;
 use Cake\Event\Event;
+use Cake\Network\Request;
 use ArrayObject;
 
 class ExaminationCentresTable extends ControllerActionTable {
     public function initialize(array $config)
     {
         $this->table('examination_centres');
-        $this->belongsTo('Examination.Examinations');
+        $this->belongsTo('Examinations', ['className' => 'Examination.Examinations']);
+        $this->belongsTo('AcademicPeriods', ['className' => 'AcademicPeriod.AcademicPeriods']);
         parent::initialize($config);
     }
 
     public function addEditBeforeAction(Event $event) {
+        $this->field('academic_period_id', ['type' => 'select']);
+        $this->field('examination_id', ['type' => 'select']);
+        $this->field('examination_centre', ['type' => 'select']);
+    }
 
+    public function onUpdateFieldAcademicPeriodId(Event $event, array $attr, $action, Request $request)
+    {
+        if ($action == 'add') {
+            $attr['options'] = $this->AcademicPeriods->getYearList(['isEditable' => true]);
+            $attr['onChangeReload'] = true;
+        }
+        return $attr;
+    }
+
+    public function onUpdateFieldExaminations(Event $event, array $attr, $action, Request $request)
+    {
+        if ($action == 'add') {
+            if (isset($request->data[$this->alias()]['academic_period_id'])) {
+
+            }
+        }
+        return $attr;
     }
 
     public function afterAction(Event $event, ArrayObject $extra)
