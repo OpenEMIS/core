2b066c323dfaf6ecd48b51c73afc84ae94b1584c
Harry Suhendra <hsuhendra@kordit.com>
2016-09-09 16:04:49 +0800

    POCOR-3257 - Updating the db patch for the security functions on config item Administrative Boundaries, Authentication, and Product Lists.

--- sql/Patch/POCOR-3257/commit.sql
+++ sql/Patch/POCOR-3257/commit.sql
@@ -10,3 +10,8 @@ VALUES ('100', 'Area Education API', 'area_api', 'Administrative Boundaries', 'A
 ALTER TABLE `security_functions` CHANGE `id` `id` INT(11) NOT NULL;
 ALTER TABLE `config_items` CHANGE `id` `id` INT(11) NOT NULL;
 
+-- Security functions
+UPDATE `security_functions`
+    SET `_view` = 'index|view|AdministrativeBoundaries.view|ProductLists.view|Authentication.view',
+        `_edit` = 'edit|AdministrativeBoundaries.edit|ProductLists.edit|Authentication.edit'
+    WHERE `name` = 'Configurations';
--- sql/Patch/POCOR-3257/rollback.sql
+++ sql/Patch/POCOR-3257/rollback.sql
@@ -1,4 +1,7 @@
--- code here
+-- security functions
+UPDATE `security_functions` SET `_view` = 'index|view', `_edit` = 'edit' WHERE `name` = 'Configurations';
+
+-- config items
 DELETE FROM `config_items` WHERE `code` = 'area_api';
 
 -- Auto_increment
@@ -8,5 +11,3 @@ ALTER TABLE `config_items` CHANGE `id` `id` INT(11) NOT NULL AUTO_INCREMENT;
 
 -- db_patches
 DELETE FROM `db_patches` WHERE `issue` = 'POCOR-3257';
-
-
