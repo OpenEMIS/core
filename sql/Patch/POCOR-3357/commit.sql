1fedfeca7c45a14e68c2c9641c68e3e022ced8f3
Livvi Sim <lsim@kordit.com>
2016-09-14 13:50:16 +0800

    POCOR-3357 - Update commit and rollback

--- sql/Patch/POCOR-3357/commit.sql
+++ sql/Patch/POCOR-3357/commit.sql
@@ -1,10 +1,31 @@
 -- db_patches
 INSERT INTO `db_patches` (`issue`, `created`) VALUES ('POCOR-3357', NOW());
 
--- add `institution_sector_id` column to `institution_providers` table
-ALTER TABLE `institution_providers`
-ADD `institution_sector_id` int(11) NOT NULL COMMENT 'links to institution_sector.id'
-AFTER `default`;
+-- rename `institution_providers` to a backup table
+RENAME TABLE `institution_providers` TO `z_3357_institution_providers`;
+
+-- recreate `institution_providers` with `institution_sector_id` column
+DROP TABLE IF EXISTS `institution_providers`;
+CREATE TABLE IF NOT EXISTS `institution_providers` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `name` varchar(50) NOT NULL,
+ `order` int(3) NOT NULL,
+ `visible` int(1) NOT NULL DEFAULT '1',
+ `editable` int(1) NOT NULL DEFAULT '1',
+ `default` int(1) NOT NULL DEFAULT '0',
+ `institution_sector_id` int(11) NOT NULL COMMENT 'links to institution_sectors.id',
+ `international_code` varchar(50) DEFAULT NULL,
+ `national_code` varchar(50) DEFAULT NULL,
+ `modified_user_id` int(11) DEFAULT NULL,
+ `modified` datetime DEFAULT NULL,
+ `created_user_id` int(11) NOT NULL,
+ `created` datetime NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='This is a field option table containing the list of user-defined providers used by institutions';
+
+INSERT INTO `institution_providers` (`id`, `name`, `order`, `visible`, `editable`, `default`, `international_code`, `national_code`, `modified_user_id`, `modified`, `created_user_id`, `created`)
+SELECT `id`, `name`, `order`, `visible`, `editable`, `default`, `international_code`, `national_code`, `modified_user_id`, `modified`, `created_user_id`, `created`
+FROM `z_3357_institution_providers`;
 
 -- link existing providers to default or first sector
 UPDATE `institution_providers`
