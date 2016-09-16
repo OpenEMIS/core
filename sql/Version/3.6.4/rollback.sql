6840aca2da9d985c1bc3f3f5d475200e7e951d75
Steven Hasanuddin <shasanuddin@kordit.com>
2016-09-16 17:02:43 +0800

    3.6.4 - Updated commit.sql and rollback.sql for POCOR-3357

--- sql/Version/3.6.4/rollback.sql
+++ sql/Version/3.6.4/rollback.sql
@@ -1,3 +1,16 @@
+-- POCOR-3357
+-- Restore tables
+DROP TABLE IF EXISTS `institution_providers`;
+RENAME TABLE `z_3357_institution_providers` TO `institution_providers`;
+
+-- Delete label
+DELETE FROM `labels`
+WHERE `id` = '56e0a017-7bdc-11e6-92c7-525400b263eb';
+
+-- db_patches
+DELETE FROM `db_patches` WHERE `issue` = 'POCOR-3357';
+
+
 -- POCOR-3347
 -- db_patches
 DELETE FROM `db_patches` WHERE `issue`='POCOR-3347';
