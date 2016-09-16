8cbd45035b860dd75aaacce2dae447d3e038642d
Livvi Sim <lsim@kordit.com>
2016-09-16 11:33:46 +0800

    POCOR-3357 - Update commit and rollback patch

--- sql/Patch/POCOR-3357/commit.sql
+++ sql/Patch/POCOR-3357/commit.sql
@@ -23,23 +23,19 @@ CREATE TABLE IF NOT EXISTS `institution_providers` (
  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='This is a field option table containing the list of user-defined providers used by institutions';
 
-INSERT INTO `institution_providers` (`id`, `name`, `order`, `visible`, `editable`, `default`, `international_code`, `national_code`, `modified_user_id`, `modified`, `created_user_id`, `created`)
-SELECT `id`, `name`, `order`, `visible`, `editable`, `default`, `international_code`, `national_code`, `modified_user_id`, `modified`, `created_user_id`, `created`
+INSERT INTO `institution_providers` (`id`, `name`, `order`, `visible`, `editable`, `default`, `institution_sector_id`, `international_code`, `national_code`, `modified_user_id`, `modified`, `created_user_id`, `created`)
+SELECT `id`, `name`, `order`, `visible`, `editable`, `default`, 0, `international_code`, `national_code`, `modified_user_id`, `modified`, `created_user_id`, `created`
 FROM `z_3357_institution_providers`;
 
 -- replace `institution_sector_id` with the sectors from `institutions` that are linked to the providers
+-- if no sector links to a particular provider in `institutions`, replace it with the default or first sector
 UPDATE `institution_providers`
-SET `institution_sector_id` = (
+SET `institution_sector_id` = IFNULL((
     SELECT `institutions`.`institution_sector_id`
     FROM `institutions`
     WHERE `institutions`.`institution_provider_id` = `institution_providers`.`id`
     GROUP BY `institutions`.`institution_provider_id`
-);
-
--- if no sector links to a particular provider in `institutions`, replace it with the default or first sector
-UPDATE `institution_providers`
-SET `institution_sector_id` = IFNULL((SELECT `id` FROM `institution_sectors` WHERE `default` = 1), 1)
-WHERE `institution_sector_id` = 0;
+), IFNULL((SELECT `id` FROM `institution_sectors` WHERE `default` = 1), 1));
 
 -- replace `institution_sector_id` in `institutions` with the sectors that are linked to the providers in `institution_providers`
 UPDATE `institutions`
@@ -51,6 +47,5 @@ SET `institution_sector_id` = (
 );
 
 -- create label for sector
-INSERT INTO `labels`
-VALUES (uuid(), 'Providers', 'institution_sector_id', 'FieldOptions -> Providers', 'Sector', NULL, NULL, 1, NULL, NULL, 1, '2016-01-01 00:00:00')
-
+INSERT INTO `labels` (`id`, `module`, `field`, `module_name`, `field_name`, `visible`, `created_user_id`, `created`)
+VALUES (uuid(), 'Providers', 'institution_sector_id', 'FieldOptions -> Providers', 'Sector', 1, 1, NOW());
--- sql/Patch/POCOR-3357/rollback.sql
+++ sql/Patch/POCOR-3357/rollback.sql
@@ -2,5 +2,10 @@
 DROP TABLE IF EXISTS `institution_providers`;
 RENAME TABLE `z_3357_institution_providers` TO `institution_providers`;
 
+-- Delete label
+DELETE FROM `labels`
+WHERE `module` = 'Providers'
+AND `field` = 'institution_sector_id';
+
 -- db_patches
 DELETE FROM `db_patches` WHERE `issue` = 'POCOR-3357';
