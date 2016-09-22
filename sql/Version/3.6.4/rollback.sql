e3cb09cfbf95a23f62b1082652333dec3e51f23e
Steven Hasanuddin <shasanuddin@kordit.com>
2016-09-22 17:21:05 +0800

    3.6.4 - Updated commit.sql and rollback.sql for POCOR-2378

--- sql/Version/3.6.4/rollback.sql
+++ sql/Version/3.6.4/rollback.sql
@@ -1,3 +1,18 @@
+-- POCOR-2378
+DROP TABLE `examinations`;
+DROP TABLE `examination_items`;
+DROP TABLE `examination_grading_types`;
+DROP TABLE `examination_grading_options`;
+DROP TABLE `examination_centres`;
+DROP TABLE `examination_centre_subjects`;
+DROP TABLE `examination_centre_special_needs`;
+DROP TABLE `examination_centre_students`;
+DROP TABLE `examination_item_results`;
+
+-- db_patches
+DELETE FROM `db_patches` WHERE `issue` = 'POCOR-2378';
+
+
 -- POCOR-3215
 -- db_patches
 DELETE FROM `db_patches` WHERE `issue`='POCOR-3215';
