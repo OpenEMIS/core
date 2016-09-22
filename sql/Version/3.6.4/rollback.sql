0b566a8335bb985d11e2b45ee35468c506763e5a
Steven Hasanuddin <shasanuddin@kordit.com>
2016-09-22 09:02:44 +0800

    3.6.4 - Updated commit.sql and rollback.sql for POCOR-3332

--- sql/Version/3.6.4/rollback.sql
+++ sql/Version/3.6.4/rollback.sql
@@ -1,3 +1,22 @@
+-- POCOR-3332
+-- institution_infrastructures
+UPDATE `institution_infrastructures`
+INNER JOIN `z_3332_institution_infrastructures` ON `z_3332_institution_infrastructures`.`id` = `institution_infrastructures`.`id`
+SET `institution_infrastructures`.`code` = `z_3332_institution_infrastructures`.`code`;
+
+DROP TABLE `z_3332_institution_infrastructures`;
+
+-- institution_rooms
+UPDATE `institution_rooms`
+INNER JOIN `z_3332_institution_rooms` ON `z_3332_institution_rooms`.`id` = `institution_rooms`.`id`
+SET `institution_rooms`.`code` = `z_3332_institution_rooms`.`code`;
+
+DROP TABLE `z_3332_institution_infrastructures`;
+
+-- db_patches
+DELETE FROM `db_patches` WHERE `issue` = 'POCOR-3332';
+
+
 -- POCOR-3357
 -- Restore tables
 DROP TABLE IF EXISTS `institution_providers`;
