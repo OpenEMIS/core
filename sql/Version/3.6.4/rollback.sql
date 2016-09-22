b3e754dc3e8cea7b393766b84c68b279003fb328
Steven Hasanuddin <shasanuddin@kordit.com>
2016-09-22 09:14:59 +0800

    3.6.4 - Updated commit.sql and rollback.sql to remove sql patches of POCOR-3332

--- sql/Version/3.6.4/rollback.sql
+++ sql/Version/3.6.4/rollback.sql
@@ -1,20 +1,6 @@
--- POCOR-3332
--- institution_infrastructures
-UPDATE `institution_infrastructures`
-INNER JOIN `z_3332_institution_infrastructures` ON `z_3332_institution_infrastructures`.`id` = `institution_infrastructures`.`id`
-SET `institution_infrastructures`.`code` = `z_3332_institution_infrastructures`.`code`;
-
-DROP TABLE `z_3332_institution_infrastructures`;
-
--- institution_rooms
-UPDATE `institution_rooms`
-INNER JOIN `z_3332_institution_rooms` ON `z_3332_institution_rooms`.`id` = `institution_rooms`.`id`
-SET `institution_rooms`.`code` = `z_3332_institution_rooms`.`code`;
-
-DROP TABLE `z_3332_institution_infrastructures`;
-
+-- POCOR-3215
 -- db_patches
-DELETE FROM `db_patches` WHERE `issue` = 'POCOR-3332';
+DELETE FROM `db_patches` WHERE `issue`='POCOR-3215';
 
 
 -- POCOR-3357
@@ -40,8 +26,6 @@ WHERE `en` = 'There are no shifts configured for the selected academic period';
 -- import_mapping
 UPDATE `import_mapping` SET `description` = 'Code' WHERE `import_mapping`.`id` = 15;
 
--- db_patches
-DELETE FROM `db_patches` WHERE `issue`='POCOR-3215';
 
 -- 3.6.3
 UPDATE config_items SET value = '3.6.3' WHERE code = 'db_version';
