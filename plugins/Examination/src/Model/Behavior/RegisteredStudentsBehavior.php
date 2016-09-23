cf1f5f849bbebfc76675e6a246ed7da51c28e986
Jeff Zheng <Jeff@Jeffs-iMac.local>
2016-09-23 21:03:03 +0800

    hotfix on Registered/NotRegistered Behavior that caused student_id field out of memory when setting field type to select

--- plugins/Examination/src/Model/Behavior/RegisteredStudentsBehavior.php
+++ plugins/Examination/src/Model/Behavior/RegisteredStudentsBehavior.php
@@ -143,7 +143,7 @@ class RegisteredStudentsBehavior extends Behavior {
             'sort' => ['field' => 'Users.openemis_no']
         ]);
         $model->field('student_id', [
-            'type' => 'select',
+            'type' => 'integer',
             'sort' => ['field' => 'Users.first_name']
         ]);
         $model->field('date_of_birth', ['type' => 'date']);
@@ -349,11 +349,12 @@ class RegisteredStudentsBehavior extends Behavior {
         return $model->controller->redirect($url);
     }
 
-    public function editAfterAction(Event $event, Entity $entity, arrayObject $extra) {
+    public function editAfterAction(Event $event, Entity $entity, ArrayObject $extra) {
         $this->setupFields($entity, $extra);
     }
 
-    public function onGetOpenemisNo(Event $event, Entity $entity) {
+    public function onGetOpenemisNo(Event $event, Entity $entity)
+    {
         $value = '';
         if ($entity->has('user')) {
             $value = $entity->user->openemis_no;
@@ -361,6 +362,17 @@ class RegisteredStudentsBehavior extends Behavior {
             $value = $entity->_matchingData['Users']->openemis_no;
         }
 
+        return $value;
+    }
+
+    public function onGetStudentId(Event $event, Entity $entity)
+    {
+        $value = '';
+        if ($entity->has('user')) {
+            $value = $entity->user->name;
+        } else {
+            $value = $entity->_matchingData['Users']->name;
+        }
         return $value;
     }
 
