a76768932f8930febc30dfe27cd4d49c863d28db
Chiang Hao Long <hchiang@kordit.com>
2016-09-26 14:28:27 +0800

    POCOR-3302 - Fixed security group area fixture and security group fixture

--- tests/Fixture/SecurityGroupAreasFixture.php
+++ tests/Fixture/SecurityGroupAreasFixture.php
@@ -18,43 +18,43 @@ class SecurityGroupAreasFixture extends TestFixture
             'security_group_id' => '516',
             'area_id' => '2',
             'created_user_id' => '2',
-            'created' => '0000-00-00 00:00:00'
+            'created' => '1970-01-01 00:00:00'
         ],
         [
             'security_group_id' => '516',
             'area_id' => '13',
             'created_user_id' => '2',
-            'created' => '0000-00-00 00:00:00'
+            'created' => '1970-01-01 00:00:00'
         ],
         [
             'security_group_id' => '518',
             'area_id' => '2',
             'created_user_id' => '2',
-            'created' => '0000-00-00 00:00:00'
+            'created' => '1970-01-01 00:00:00'
         ],
         [
             'security_group_id' => '518',
             'area_id' => '13',
             'created_user_id' => '2',
-            'created' => '0000-00-00 00:00:00'
+            'created' => '1970-01-01 00:00:00'
         ],
         [
             'security_group_id' => '519',
             'area_id' => '13',
             'created_user_id' => '2',
-            'created' => '0000-00-00 00:00:00'
+            'created' => '1970-01-01 00:00:00'
         ],
         [
             'security_group_id' => '519',
             'area_id' => '1',
             'created_user_id' => '2',
-            'created' => '0000-00-00 00:00:00'
+            'created' => '1970-01-01 00:00:00'
         ],
         [
             'security_group_id' => '520',
             'area_id' => '13',
             'created_user_id' => '2',
-            'created' => '0000-00-00 00:00:00'
+            'created' => '1970-01-01 00:00:00'
         ]
     ];
 }
