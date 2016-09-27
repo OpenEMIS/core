9674f74b2a8e3ab4de4be390a4ae437004856836
Chiang Hao Long <hchiang@kordit.com>
2016-09-27 13:25:19 +0800

    POCOR-2827 - Fixed user behaviour indexBeforeAction

--- plugins/User/src/Model/Behavior/UserBehavior.php
+++ plugins/User/src/Model/Behavior/UserBehavior.php
@@ -153,7 +153,7 @@ class UserBehavior extends Behavior {
         $this->_table->fields['is_guardian']['value'] = 0;
     }
 
-    public function indexBeforeAction(Event $event, Query $query, ArrayObject $settings) {
+    public function indexBeforeAction(Event $event) {
         $plugin = $this->_table->controller->plugin;
         $name = $this->_table->controller->name;
 
