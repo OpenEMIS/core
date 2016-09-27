bbfa2202da656b32a9bab611810d51402754eeb0
Chiang Hao Long <hchiang@kordit.com>
2016-09-27 14:19:13 +0800

    POCOR-3398 - Add a limit for autocomplete searches related to security_users table.

--- plugins/Institution/src/Model/Table/StaffTable.php
+++ plugins/Institution/src/Model/Table/StaffTable.php
@@ -304,7 +304,7 @@ class StaffTable extends AppTable {
 		$StaffPositionProfilesTable = TableRegistry::get('Institution.StaffPositionProfiles');
 		$staffPositionProfilesRecordCount = $StaffPositionProfilesTable->find()
 			->where([
-				$StaffPositionProfilesTable->aliasField('institution_id') => $institutionId, 
+				$StaffPositionProfilesTable->aliasField('institution_id') => $institutionId,
 				$StaffPositionProfilesTable->aliasField('status_id'). ' NOT IN ' => $staffPositionProfileStatuses
 			])
 			->count();
@@ -792,7 +792,7 @@ class StaffTable extends AppTable {
 		}
 	}
 
-	public function addOnChangeStartDate(Event $event, Entity $entity, ArrayObject $data, ArrayObject $options) 
+	public function addOnChangeStartDate(Event $event, Entity $entity, ArrayObject $data, ArrayObject $options)
 	{
 		if (isset($data[$this->alias()]['institution_position_id'])) {
 			unset($data[$this->alias()]['institution_position_id']);
@@ -800,12 +800,12 @@ class StaffTable extends AppTable {
 		$this->Session->delete($this->registryAlias().'.end_date_changed');
 	}
 
-	public function addOnChangeEndDate(Event $event, Entity $entity, ArrayObject $data, ArrayObject $options) 
+	public function addOnChangeEndDate(Event $event, Entity $entity, ArrayObject $data, ArrayObject $options)
 	{
 		$this->Session->write($this->registryAlias().'.end_date_changed', true);
 	}
 
-	public function addOnChangePositionId(Event $event, Entity $entity, ArrayObject $data, ArrayObject $options) 
+	public function addOnChangePositionId(Event $event, Entity $entity, ArrayObject $data, ArrayObject $options)
 	{
 		$data[$this->alias()]['position_id_changed'] = true;
 	}
@@ -870,7 +870,7 @@ class StaffTable extends AppTable {
 		} else {
 			$data[$this->alias()]['FTE'] = 0;
 		}
-		
+
 	}
 
 	public function onUpdateFieldPositionType(Event $event, array $attr, $action, Request $request) {
@@ -900,7 +900,7 @@ class StaffTable extends AppTable {
 		return $attr;
 	}
 
-	public function onUpdateFieldFTE(Event $event, array $attr, $action, Request $request) 
+	public function onUpdateFieldFTE(Event $event, array $attr, $action, Request $request)
 	{
 		if ($action == 'add') {
 			if (!isset($request->data[$this->alias()]['FTE'])) {
@@ -1225,7 +1225,7 @@ class StaffTable extends AppTable {
 		if ($this->request->is(['ajax'])) {
 			$term = $this->request->query['term'];
 			// only search for staff
-			$query = $this->Users->find()->where([$this->Users->aliasField('is_staff') => 1]);
+			$query = $this->Users->find()->where([$this->Users->aliasField('is_staff') => 1])->limit(100);
 
 			$term = trim($term);
 			if (!empty($term)) {
