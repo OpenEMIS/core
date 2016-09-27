c2b4484ce2c8585ab322d4d7c04e64662fe6a46b
Steven Hasanuddin <shasanuddin@kordit.com>
2016-09-27 10:29:39 +0800

    3.6.5 - Updated commit.sql and rollback.sql

--- sql/Version/3.6.5/commit.sql
+++ sql/Version/3.6.5/commit.sql
@@ -1,3 +1,57 @@
+-- POCOR-3106
+-- db_patches
+INSERT INTO `db_patches` (issue, created) VALUES ('POCOR-3106', NOW());
+
+
+-- Security functions
+ALTER TABLE `security_functions` ADD `description` VARCHAR( 255 ) AFTER `visible`;
+
+-- Programmes permission
+UPDATE `security_functions`
+    SET `controller` = 'Institutions',
+        `_view` = 'StudentProgrammes.index|StudentProgrammes.view',
+        `_edit` = 'Students.edit|StudentProgrammes.edit'
+    WHERE `id` = 2011;
+
+-- Overview permission
+UPDATE `security_functions`
+    SET `name` = 'Overview',
+        `controller` = 'Institutions',
+        `_view` = 'StudentUser.view',
+        `_edit` = 'StudentUser.edit',
+        `_add` = NULL,
+        `_delete` = NULL,
+        `_execute` = 'StudentUser.excel',
+        `description` = 'Overview edit will only take effect when classes permission is granted.'
+    WHERE `id` = 2000;
+
+-- Studentlist permission
+UPDATE `security_functions`
+    SET `_view` = 'Students.index|Students.view|StudentSurveys.index|StudentSurveys.view',
+        `_edit` = 'StudentSurveys.edit',
+        `_execute` = 'Students.excel'
+    WHERE `id` = 1012;
+
+-- Translation table
+INSERT INTO `translations` (`id`, `code`, `en`, `ar`, `zh`, `es`, `fr`, `ru`, `editable`, `modified_user_id`, `modified`, `created_user_id`, `created`)
+VALUES (NULL, NULL, 'Overview edit will only take effect when classes permission is granted', NULL, NULL, NULL, NULL, NULL, '1', NULL, NULL, '2', NOW());
+
+-- Security Role functions
+-- copy the value on the student edit permission into the programme edit permission
+UPDATE `security_role_functions` t1, (SELECT `_edit`, `security_role_id` FROM `security_role_functions` WHERE `security_function_id` = 1012) t2
+    SET t1.`_edit` = t2.`_edit`
+    WHERE t1.`security_role_id` = t2.`security_role_id`
+    AND `security_function_id` = 2011;
+
+
+-- POCOR-3371
+-- db_patches
+INSERT INTO `db_patches` (`issue`, `created`) VALUES('POCOR-3371', NOW());
+
+-- staff_qualifications
+ALTER TABLE `staff_qualifications` CHANGE `graduate_year` `graduate_year` INT(4) NULL;
+
+
 -- 3.6.5
 UPDATE config_items SET value = '3.6.5' WHERE code = 'db_version';
 UPDATE db_patches SET version = (SELECT value FROM config_items WHERE code = 'db_version') WHERE version IS NULL;
--- sql/Version/3.6.5/rollback.sql
+++ sql/Version/3.6.5/rollback.sql
@@ -1,2 +1,49 @@
+-- POCOR-3106
+-- security functions
+ALTER TABLE `security_functions` DROP `description`;
+
+-- Programmes permission
+UPDATE `security_functions`
+    SET `controller` = 'Students',
+        `_view` = 'Programmes.index|Programmes.view',
+        `_edit` = NULL
+    WHERE `id` = 2011;
+
+-- Overview permission
+UPDATE `security_functions`
+    SET `name` = 'Students',
+        `controller` = 'Students',
+        `_view` = 'index|view',
+        `_edit` = 'edit',
+        `_add` = 'add',
+        `_delete` = 'remove',
+        `_execute` = 'excel'
+    WHERE `id` = 2000;
+
+-- Studentlist permission
+UPDATE `security_functions`
+    SET `_view` = 'Students.index|Students.view|StudentUser.view|StudentSurveys.index|StudentSurveys.view',
+        `_edit` = 'Students.edit|StudentUser.edit|StudentSurveys.edit',
+        `_execute` = 'Students.excel|StudentUser.excel'
+    WHERE `id` = 1012;
+
+-- Translation table
+DELETE FROM `translations` WHERE `en` = 'Overview edit will only take effect when classes permission is granted';
+
+-- security role functions
+UPDATE `security_role_functions` SET `_edit` = 0 WHERE `security_function_id` = 2011;
+
+-- db_patches
+DELETE FROM `db_patches` WHERE `issue` = 'POCOR-3106';
+
+
+-- POCOR-3371
+-- staff_qualifications
+ALTER TABLE `staff_qualifications` CHANGE `graduate_year` `graduate_year` INT(4) NOT NULL;
+
+-- db_patches
+DELETE FROM `db_patches` WHERE `issue`='POCOR-3371';
+
+
 -- 3.6.4
 UPDATE config_items SET value = '3.6.4' WHERE code = 'db_version';
