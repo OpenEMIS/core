892a538dcea274b9a3bfecf19d5e920b4cf056d4
Steven Hasanuddin <shasanuddin@kordit.com>
2016-09-29 09:18:35 +0800

    3.6.5 - Updated commit.sql and rollback.sql for POCOR-3379

--- sql/Version/3.6.5/rollback.sql
+++ sql/Version/3.6.5/rollback.sql
@@ -1,3 +1,15 @@
+-- POCOR-3379
+-- security_functions
+DELETE FROM `security_functions` WHERE `id` = 1047;
+
+UPDATE `security_functions` SET `order` = '1001' WHERE `id` = '1002';
+
+UPDATE `security_functions` SET `order` = '1002' WHERE `id` = '1001';
+
+-- db_patches
+DELETE FROM `db_patches` WHERE `issue` = 'POCOR-3379';
+
+
 -- POCOR-3339
 -- restore workflow code and name
 UPDATE `workflows`
