d4756fb3b7cdbf9f4555c71a24db2066e7330d13
Steven Hasanuddin <shasanuddin@kordit.com>
2016-09-30 15:21:10 +0800

    3.6.5 - Updated rollback.sql for POCOR-3371

--- sql/Version/3.6.5/rollback.sql
+++ sql/Version/3.6.5/rollback.sql
@@ -78,7 +78,7 @@ DELETE FROM `db_patches` WHERE `issue` = 'POCOR-3106';
 
 -- POCOR-3371
 -- staff_qualifications
-ALTER TABLE `staff_qualifications` CHANGE `graduate_year` `graduate_year` INT(4) NOT NULL;
+-- ALTER TABLE `staff_qualifications` CHANGE `graduate_year` `graduate_year` INT(4) NOT NULL;
 
 -- db_patches
 DELETE FROM `db_patches` WHERE `issue`='POCOR-3371';
