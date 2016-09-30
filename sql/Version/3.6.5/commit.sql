430804aa09a8bc400ab7fd85fd89cb798f5b9ea1
Steven Hasanuddin <shasanuddin@kordit.com>
2016-09-30 09:13:52 +0800

    3.6.5 - Updated commit.sql and rollback.sql for POCOR-3339

--- sql/Version/3.6.5/commit.sql
+++ sql/Version/3.6.5/commit.sql
@@ -1,3 +1,13 @@
+-- POCOR-3339
+-- db_patches
+INSERT INTO `db_patches` (issue, created) VALUES ('POCOR-3339', NOW());
+
+-- change workflow code and name to 'change in assignment'
+UPDATE `workflows`
+SET `code` = 'CHANGE-IN-ASSIGNMENT-01', `name` = 'Change in Assignment'
+WHERE `code` = 'STAFF-POSITION-PROFILE-01';
+
+
 -- POCOR-3379
 -- db_patches
 INSERT INTO `db_patches` (`issue`, `created`) VALUES ('POCOR-3379', NOW());
