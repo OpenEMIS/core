231a453bae1226ad9b65271635e7a18cb0b99612
Harry Suhendra <hsuhendra@kordit.com>
2016-09-30 15:03:47 +0800

    POCOR-3304 - Update the DB patch, so the order will be in sequence.

--- sql/Patch/POCOR-3304/rollback.sql
+++ sql/Patch/POCOR-3304/rollback.sql
@@ -2,20 +2,23 @@
 DELETE FROM `security_role_functions` WHERE `security_function_id` = 3036; -- institution salary_list
 DELETE FROM `security_role_functions` WHERE `security_function_id` = 7048; -- directory salary_list
 
+
 -- security_functions
+-- salaries institution
 DELETE FROM `security_functions` WHERE `id` = 3036; -- institution salary_list
-UPDATE `security_functions` SET `order` = 3023 WHERE `id` = 3023; -- institution bank_account
-UPDATE `security_functions`
-    SET `name` = 'Salaries',
-        `order` = 3020
-    WHERE `id` = 3020; -- institution salary_details
+
+UPDATE `security_functions` SET `name` = 'Salaries' WHERE `id` = 3020;
+UPDATE `security_functions` SET `order` = `order` - 1 WHERE `order` BETWEEN 3022 AND 3024;
+
+UPDATE `security_functions` SET `order` = 3024 WHERE `id` = 3023; -- Bank Account
+UPDATE `security_functions` SET `order` = `order` - 1 WHERE `order` BETWEEN 3021 AND 3036;
 
 
+-- salaries directory
 DELETE FROM `security_functions` WHERE `id` = 7048; -- directory salary_list
-UPDATE `security_functions`
-    SET `name` = 'Salaries',
-        `order` = 7034
-    WHERE `id` = 7034; -- directory salary_details
+
+UPDATE `security_functions` SET `name` = 'Salaries' WHERE `id` = 7034;
+UPDATE `security_functions` SET `order` = `order` - 1 WHERE `order` BETWEEN 7035 AND 7048;
 
 
 -- db_patches
