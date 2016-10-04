373afa538930d01cfa5e139a35caa9a97c3749ef
Harry Suhendra <hsuhendra@kordit.com>
2016-10-04 17:42:45 +0800

    POCOR-3260 - FieldOption > Countries revert back the deleteStrategy to cascade delete. - update CustomField > Institution > pages (institutionCustomFormsTable) filterClass>className to Institution.Types. - update institutionCustomFormsFiltersTable to Institution.Types.

--- plugins/InstitutionCustomField/src/Model/Table/InstitutionCustomFormsFiltersTable.php
+++ plugins/InstitutionCustomField/src/Model/Table/InstitutionCustomFormsFiltersTable.php
@@ -7,6 +7,6 @@ class InstitutionCustomFormsFiltersTable extends AppTable {
 	public function initialize(array $config) {
 		parent::initialize($config);
 		$this->belongsTo('CustomForms', ['className' => 'InstitutionCustomField.InstitutionCustomForms', 'foreignKey' => 'institution_custom_form_id']);
-		$this->belongsTo('CustomFilters', ['className' => 'FieldOption.InstitutionTypes', 'foreignKey' => 'institution_custom_filter_id']);
+		$this->belongsTo('CustomFilters', ['className' => 'Institution.Types', 'foreignKey' => 'institution_custom_filter_id']);
 	}
 }
