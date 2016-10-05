ad38340083eb35a6f5d3a7221ad736154972aa73
Harry Suhendra <hsuhendra@kordit.com>
2016-10-05 16:41:53 +0800

    POCOR-3407 - Add more space for the validation_pattern.

--- sql/Patch/POCOR-3407/commit.sql
+++ sql/Patch/POCOR-3407/commit.sql
@@ -4,4 +4,4 @@ INSERT INTO `db_patches` (issue, created) VALUES ('POCOR-3407', NOW());
 
 -- code here
 ALTER TABLE `identity_types`
-    ADD COLUMN `validation_pattern` varchar(50) DEFAULT NULL AFTER `name`;
+    ADD COLUMN `validation_pattern` varchar(100) DEFAULT NULL AFTER `name`;
