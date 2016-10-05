21abbec0970e3bf400690952e0e5ee6868cc89b0
Harry Suhendra <hsuhendra@kordit.com>
2016-10-05 16:53:55 +0800

    POCOR-3407 - Update the db patches, Birth certificate and Passport to have value.

--- sql/Patch/POCOR-3407/commit.sql
+++ sql/Patch/POCOR-3407/commit.sql
@@ -5,3 +5,6 @@ INSERT INTO `db_patches` (issue, created) VALUES ('POCOR-3407', NOW());
 -- code here
 ALTER TABLE `identity_types`
     ADD COLUMN `validation_pattern` varchar(100) DEFAULT NULL AFTER `name`;
+
+UPDATE `identity_types` SET `validation_pattern` = '^[a-z]{6,}+$' WHERE `name` = 'Birth Certificate';
+UPDATE `identity_types` SET `validation_pattern` = '^[A-Z]{1}[0-9]{6}$' WHERE `name` = 'Passport';
