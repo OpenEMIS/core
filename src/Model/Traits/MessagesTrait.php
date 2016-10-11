e5f5e60ac2a214edee38124535a11ecd624c589a
Livvi Sim <lsim@kordit.com>
2016-10-11 15:03:54 +0800

    POCOR-3417 - Fix translation for messages using vsprintf

--- src/Model/Traits/MessagesTrait.php
+++ src/Model/Traits/MessagesTrait.php
@@ -1398,6 +1398,6 @@ trait MessagesTrait {
             }
         }
 
-        return !is_array($message) ? vsprintf(__($message), $sprintf) : $message;
+        return !is_array($message) ? __(vsprintf($message, $sprintf)) : $message;
     }
 }
