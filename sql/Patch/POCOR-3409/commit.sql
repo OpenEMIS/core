a3e67c2e56327fa338024db59d362a6439addbf1
Chiang Hao Long <hchiang@kordit.com>
2016-10-12 17:09:09 +0800

    POCOR-3409 - Added a fixed ID to the options. Alter the config item options table to remove auto increment for id field.

--- sql/Patch/POCOR-3409/commit.sql
+++ sql/Patch/POCOR-3409/commit.sql
@@ -2,4 +2,7 @@
 INSERT INTO `db_patches` (`issue`, `created`) VALUES ('POCOR-3409', NOW());
 
 -- config_item_options
-INSERT INTO `config_item_options` (`option_type`, `option`, `value`, `order`, `visible`) VALUES ('authentication_type', 'OAuth 2.0 with OpenID Connect', 'OAuth2OpenIDConnect', 5, 1);
+ALTER TABLE `config_item_options`
+CHANGE COLUMN `id` `id` INT(11) NOT NULL COMMENT '' ;
+
+INSERT INTO `config_item_options` (`id`, `option_type`, `option`, `value`, `order`, `visible`) VALUES (37, 'authentication_type', 'OAuth 2.0 with OpenID Connect', 'OAuth2OpenIDConnect', 5, 1);
