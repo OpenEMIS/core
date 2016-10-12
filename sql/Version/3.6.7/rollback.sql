4e2e523781eab34150006419b498a606d9a3df2e
Steven Hasanuddin <shasanuddin@kordit.com>
2016-10-12 17:21:19 +0800

    3.6.7 - Updated commit.sql and rollback.sql for POCOR-3409

--- sql/Version/3.6.7/rollback.sql
+++ sql/Version/3.6.7/rollback.sql
@@ -1,3 +1,22 @@
+-- POCOR-3409
+-- config_items
+UPDATE `config_items`
+SET `value`='Local'
+WHERE `id`='1001' AND `value`='OAuth2OpenIDConnect';
+
+-- config_item_options
+DELETE FROM `config_item_options` WHERE `id` = 37;
+
+ALTER TABLE `config_item_options`
+CHANGE COLUMN `id` `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '' ;
+
+ALTER TABLE `config_item_options`
+CHANGE COLUMN `id` `id` INT(11) NOT NULL COMMENT '' ;
+
+-- db_patches
+DELETE FROM `db_patches` WHERE `issue` = 'POCOR-3409';
+
+
 -- POCOR-3420
 -- code here
 DELETE FROM `translations` WHERE `en` = 'Please enter a valid format';
