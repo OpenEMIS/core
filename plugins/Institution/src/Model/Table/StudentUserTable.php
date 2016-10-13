ac7742e7754a8c1cf90d5bc24e95505aa0dbb372
Harry Suhendra <hsuhendra@kordit.com>
2016-10-13 15:13:48 +0800

    POCOR-3435 - Add condition, not in the current academic period, transferButton not shown. - Add condition, today's date not within the current academic period date, transferButton not shown. - If student doesn't have transferRequest will go transferRequest add page, if already have transferRequest will go to transferRequest view page. (delete button available)

--- plugins/Institution/src/Model/Table/StudentUserTable.php
+++ plugins/Institution/src/Model/Table/StudentUserTable.php
@@ -190,9 +190,21 @@ class StudentUserTable extends UserTable
     private function addTransferButton(ArrayObject $buttons, ArrayObject $toolbarButtons, array $attr, Session $session)
     {
     	$InstitutionStudentsTable = TableRegistry::get('Institution.Students');
+    	$AcademicPeriods = TableRegistry::get('AcademicPeriod.AcademicPeriods');
+
 		$statuses = $InstitutionStudentsTable->StudentStatuses->findCodeList();
 		$id = $session->read('Institution.Students.id');
-		if ($this->AccessControl->check([$this->controller->name, 'TransferRequests', 'add'])) {
+		$currentAcademicPeriodId = $session->read('AcademicPeriod.AcademicPeriods.id');
+		$academicPeriodId = $InstitutionStudentsTable->get($id)->academic_period_id;
+		$currentAcademicPeriodStartDate = $AcademicPeriods->get($currentAcademicPeriodId)->start_date->toUnixString();
+		$currentAcademicPeriodEndDate = $AcademicPeriods->get($currentAcademicPeriodId)->end_date->toUnixString();
+		$todayDate = strtotime(date('Y-m-d'));
+
+		// check the permission and the student academic period id is current academic period id and today date fall within the enrollment period
+		if ($this->AccessControl->check([$this->controller->name, 'TransferRequests', 'add'])
+			&& ($academicPeriodId == $currentAcademicPeriodId)
+			&& ($currentAcademicPeriodStartDate <= $todayDate)
+			&& ($todayDate <= $currentAcademicPeriodEndDate)) {
 			$TransferRequests = TableRegistry::get('Institution.TransferRequests');
 			$studentData = $InstitutionStudentsTable->get($id);
 			$selectedStudent = $studentData->student_id;
@@ -227,7 +239,7 @@ class StudentUserTable extends UserTable
 					'plugin' => $buttons['back']['url']['plugin'],
 					'controller' => $buttons['back']['url']['controller'],
 					'action' => 'TransferRequests',
-					'edit',
+					'view',
 					$transferRequest->id
 				];
 				$toolbarButtons['transfer'] = $transferButton;
