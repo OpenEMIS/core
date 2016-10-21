489491886a39f1dbd3cda24714a865c5920aea35
Chiang Hao Long <hchiang@kordit.com>
2016-10-21 18:50:54 +0800

    POCOR-2827 - Fixed validation on external data source set up

--- plugins/Configuration/src/Model/Behavior/ExternalDataSourceBehavior.php
+++ plugins/Configuration/src/Model/Behavior/ExternalDataSourceBehavior.php
@@ -30,6 +30,9 @@ class ExternalDataSourceBehavior extends Behavior {
 			'ControllerAction.Model.edit.beforeAction'  => 'editBeforeAction',
 			'ControllerAction.Model.view.beforeAction'	=> 'viewBeforeAction',
 			'ControllerAction.Model.index.beforeAction'	=> ['callable' => 'indexBeforeAction', 'priority' => 100],
+			'ControllerAction.Model.edit.beforePatch'	=> 'editBeforePatch'
+
+
 		];
 		$events = array_merge($events, $newEvent);
 		return $events;
@@ -142,6 +145,20 @@ class ExternalDataSourceBehavior extends Behavior {
 		}
 	}
 
+	public function editBeforePatch(Event $event, Entity $entity, ArrayObject $data, ArrayObject $options) {
+    	$configItem = $data[$this->alias];
+    	if ($configItem['type'] == 'External Data Source') {
+    		$configItem['value'] = lcfirst(Inflector::camelize($configItem['value'], ' '));
+
+    		$methodName = $configItem['value'].'Validation';
+
+    		if (method_exists($this, $methodName) && !$this->$methodName($data[$this->alias])) {
+    			$this->_table->Alert->error('ExternalDataSource.emptyFields', ['reset' => true]);;
+    			$entity->errors('error', ['There are invalid attributes']);
+    		}
+    	}
+    }
+
 	public function editAfterSave(Event $event, Entity $entity, ArrayObject $data, ArrayObject $options) {
 		$ExternalDataSourceAttributesTable = TableRegistry::get('Configuration.ExternalDataSourceAttributes');
 		if ($data[$this->alias]['value'] != 'None' && $data[$this->alias]['type'] == 'External Data Source') {
@@ -181,6 +198,19 @@ class ExternalDataSourceBehavior extends Behavior {
 		// $attribute['hd'] = ['label' => 'Hosted Domain', 'type' => 'text', 'required' => false];
 		$attribute['record_uri'] = ['label' => 'Record URI', 'type' => 'text'];
 	}
+
+	public function openemisIdentitiesValidation($attributes) {
+    	$attribute = [];
+    	$this->openemisIdentitiesExternalSource($attribute);
+    	foreach ($attribute as $key => $values) {
+    		if (!isset($values['required'])) {
+    			if (empty($attributes[$key]['value'])) {
+    				return false;
+    			}
+    		}
+    	}
+    	return true;
+    }
 
 	public function onGetExternalDataSourceTypeElement(Event $event, $action, $entity, $attr, $options=[]) {
 		switch ($action){
