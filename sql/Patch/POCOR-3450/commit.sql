55b374f9877a4121c2aa3007fe16d27e61cb7e71
Harry Suhendra <hsuhendra@kordit.com>
2016-10-25 18:16:09 +0800

    POCOR-3450 - Adding permission for institution and directory tab. - Adding appraisal to the directory>staff>appraisal. - Adding condition on the staff_appraisal_type_id to remove edit and delete button if not his own appraisal. - Only self if it for his own appraisal.

--- sql/Patch/POCOR-3450/commit.sql
+++ sql/Patch/POCOR-3450/commit.sql
@@ -82,3 +82,13 @@ UPDATE `field_options` SET `order` = `order` + 2 WHERE `order` >= 19;
 INSERT INTO `field_options` (`id`, `plugin`, `code`, `name`, `parent`, `params`, `order`, `visible`, `modified_user_id`, `modified`, `created_user_id`, `created`)
 VALUES  ('25', 'Staff', 'Competencies', 'Competencies', 'Staff', NULL, 19, '1', NULL, NULL, 0, NOW()),
         ('26', 'Staff', 'CompetencySets', 'Competency Sets', 'Staff', NULL, 20, '1', NULL, NULL, 0, NOW());
+
+-- security_function (permission)
+UPDATE `security_functions` SET `order` = `order` + 1 WHERE `id` BETWEEN 3000 AND 4000 AND `order` >= 3025;
+UPDATE `security_functions` SET `order` = `order` + 1 WHERE `id` BETWEEN 7000 AND 8000 AND `order` >= 7033;
+
+INSERT INTO `security_functions` (`id`, `name`, `controller`, `module`, `category`, `parent_id`, `_view`, `_edit`, `_add`, `_delete`, `_execute`, `order`, `visible`, `description`, `modified_user_id`, `modified`, `created_user_id`, `created`)
+VALUES  ('3037', 'Appraisals', 'Staff', 'Institutions', 'Staff - Professional Development', '3000', 'Appraisals.index|Appraisals.view', 'Appraisals.edit', 'Appraisals.add', 'Appraisals.remove', NULL, '3025', '1', NULL, NULL, NULL, '1', NOW()),
+        ('7049', 'Appraisals', 'Directories', 'Directory', 'Staff - Professional Development', '7000', 'StaffAppraisals.index|StaffAppraisals.view', 'StaffAppraisals.edit', 'StaffAppraisals.add', 'StaffAppraisals.remove', NULL, '7033', '1', NULL, NULL, NULL, '1', NOW());
+
+
--- sql/Patch/POCOR-3450/rollback.sql
+++ sql/Patch/POCOR-3450/rollback.sql
@@ -14,5 +14,17 @@ DELETE FROM `field_options` WHERE `code` = 'CompetencySets';
 UPDATE `field_options` SET `order` = `order` - 2 WHERE `order` >= 19;
 
 
+-- security_function
+DELETE FROM `security_functions` WHERE `id` = 3037;
+DELETE FROM `security_functions` WHERE `id` = 7049;
+UPDATE `security_functions` SET `order` = `order` - 1 WHERE `id` BETWEEN 3000 AND 4000 AND `order` >= 3025;
+UPDATE `security_functions` SET `order` = `order` - 1 WHERE `id` BETWEEN 7000 AND 8000 AND `order` >= 7033;
+
+-- security_role_function
+DELETE FROM `security_role_functions` WHERE `security_function_id` = 3037;
+DELETE FROM `security_role_functions` WHERE `security_function_id` = 7049;
+
+
 -- db_patches
 DELETE FROM `db_patches` WHERE `issue` = 'POCOR-3450';
+
