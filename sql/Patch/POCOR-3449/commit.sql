35278fb2c21ace47f5e686fa2ca45706ffcaff2e
Livvi Sim <lsim@kordit.com>
2016-10-26 11:03:05 +0800

    POCOR-3449 - Amend patch and set up Staff Application page (WIP)

--- sql/Patch/POCOR-3449/commit.sql
+++ sql/Patch/POCOR-3449/commit.sql
@@ -4,7 +4,7 @@ INSERT INTO `db_patches` (`issue`, `created`) VALUES ('POCOR-3449', NOW());
 CREATE TABLE `staff_training_applications` (
   `id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
   `staff_id` int(11) NOT NULL COMMENT 'links to security_users.id',
-  `course_id` int(11) NOT NULL COMMENT 'links to training_courses.id',
+  `training_course_id` int(11) NOT NULL COMMENT 'links to training_courses.id',
   `status_id` int(11) NOT NULL COMMENT 'links to workflow_steps.id',
   `assignee_id` int(11) NOT NULL DEFAULT '0' COMMENT 'links to security_users.id',
   `institution_id` int(11) NOT NULL COMMENT 'links to institutions.id',
@@ -14,7 +14,7 @@ CREATE TABLE `staff_training_applications` (
   `created` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `staff_id` (`staff_id`),
-  KEY `course_id` (`course_id`),
+  KEY `training_course_id` (`training_course_id`),
   KEY `status_id` (`status_id`),
   KEY `assignee_id` (`assignee_id`),
   KEY `institution_id` (`institution_id`),
