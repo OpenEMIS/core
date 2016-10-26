3ac3206fd0de8eb75bbbde819aeef03d88db30dd
Chiang Hao Long <hchiang@kordit.com>
2016-10-26 15:40:07 +0800

    POCOR-3492 - Added logic to display the create new user if the user has permission to create new user

--- plugins/Institution/src/Controller/InstitutionsController.php
+++ plugins/Institution/src/Controller/InstitutionsController.php
@@ -131,7 +131,16 @@ class InstitutionsController extends AppController
 
     public function Students($pass = 'index') {
         if ($pass == 'add') {
+            $session = $this->request->session();
+            $roles = [];
+
+            if (!$this->AccessControl->isAdmin() && $session->check('Institution.Institutions.id')) {
+                $userId = $this->Auth->user('id');
+                $institutionId = $session->read('Institution.Institutions.id');
+                $roles = $this->Institutions->getInstitutionRoles($userId, $institutionId);
+            }
             $this->set('ngController', 'InstitutionsStudentsCtrl as InstitutionStudentController');
+            $this->set('_createNewStudent', $this->AccessControl->check(['Institutions', 'getUniqueOpenemisId'], $roles));
             $externalDataSource = false;
         	$ConfigItemTable = TableRegistry::get('Configuration.ConfigItems');
         	$externalSourceType = $ConfigItemTable->find()->where([$ConfigItemTable->aliasField('code') => 'external_data_source_type'])->first();
