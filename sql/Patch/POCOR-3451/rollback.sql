3f98232fbd7b598bf7b89c9625ba5e27388e580d
Thed Kuok Shiun <kthed@kordit.com>
2016-10-26 11:15:49 +0800

    POCOR-3451 - Update sql patch with pre-insert workflow data.

--- sql/Patch/POCOR-3451/rollback.sql
+++ sql/Patch/POCOR-3451/rollback.sql
@@ -1,6 +1,9 @@
 -- Drop tables
 DROP TABLE IF EXISTS `institution_visit_requests`;
 
+-- workflow_models
+DELETE FROM `workflow_models` WHERE `model` = 'Institution.VisitRequests';
+
 -- security_functions
 DELETE FROM `security_functions` WHERE `id` = 1047;
 
