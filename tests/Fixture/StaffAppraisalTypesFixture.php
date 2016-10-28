989e9893061602e5462b975a54ea2ab771d11b57
Harry Suhendra <hsuhendra@kordit.com>
2016-10-28 10:15:11 +0800

    POCOR-3450 - Unit test case removed app.workflow_records, - Fixed the strict database for value date 0000-00-00. - Adding fixtures and controller test for field options > competencies.

---
+++ tests/Fixture/StaffAppraisalTypesFixture.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+class StaffAppraisalTypesFixture extends TestFixture
+{
+    public $import = ['table' => 'staff_appraisal_types'];
+    public $records = [];
+}
+
---
+++ tests/Fixture/StaffAppraisalsCompetenciesFixture.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+class StaffAppraisalsCompetenciesFixture extends TestFixture
+{
+    public $import = ['table' => 'staff_appraisals_competencies'];
+    public $records = [];
+}
+
