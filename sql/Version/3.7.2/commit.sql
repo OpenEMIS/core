e571a97fb5f11d4ba5d5e681ca7ff42f7bbac32f
Steven Hasanuddin <shasanuddin@kordit.com>
2016-10-28 09:27:25 +0800

    3.7.2 - Updated version file, commit.sql, rollback.sql, did sql cleanups

---
+++ sql/Version/3.7.2/commit.sql
@@ -0,0 +1,3 @@
+-- 3.7.2
+UPDATE config_items SET value = '3.7.2' WHERE code = 'db_version';
+UPDATE db_patches SET version = (SELECT value FROM config_items WHERE code = 'db_version') WHERE version IS NULL;
