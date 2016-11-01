e3a2ee0c0587173605b01dd4a5309dc8397cb15c
Steven Hasanuddin <shasanuddin@kordit.com>
2016-11-01 14:52:37 +0800

    3.7.2 - Updated commit.sql and rollback.sql for POCOR-3451

--- sql/Version/3.7.2/rollback.sql
+++ sql/Version/3.7.2/rollback.sql
@@ -1,4 +1,8 @@
 -- POCOR-3451
+-- restore field_options tables
+RENAME TABLE `z_3451_field_options` TO `field_options`;
+RENAME TABLE `z_3451_field_option_values` TO `field_option_values`;
+
 -- Drop tables
 DROP TABLE IF EXISTS `institution_visit_requests`;
 
