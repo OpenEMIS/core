a3e468bfc0fedcb287607f07a22069113068fc04
Livvi Sim <lsim@kordit.com>
2016-11-09 10:03:30 +0800

    POCOR-3508 - Added RegistrationDirectory page for searching individual students (WIP)

--- plugins/Examination/src/Model/Table/ExaminationCentreStudentsTable.php
+++ plugins/Examination/src/Model/Table/ExaminationCentreStudentsTable.php
@@ -27,7 +27,7 @@ class ExaminationCentreStudentsTable extends ControllerActionTable {
 
         $this->addBehavior('User.AdvancedNameSearch');
         $this->addBehavior('Examination.RegisteredStudents');
-        $this->toggle('add', false);
+        // $this->toggle('add', false);
     }
 
     public function beforeSave(Event $event, Entity $entity, ArrayObject $options) {
@@ -37,6 +37,103 @@ class ExaminationCentreStudentsTable extends ControllerActionTable {
     public function beforeAction(Event $event, ArrayObject $extra) {
         $this->controller->getStudentsTab();
     }
+
+    public function indexbeforeAction(Event $event, ArrayObject $extra)
+    {
+        // add button to course catalogue
+        if (isset($extra['toolbarButtons']['add']['url'])) {
+            $extra['toolbarButtons']['add']['url']['action'] = 'IndividualRegistration';
+            $extra['toolbarButtons']['add']['url'][0] = 'index';
+            $extra['toolbarButtons']['add']['attr']['title'] = 'Register';
+        }
+    }
+
+        // public function addBeforeAction(Event $event, ArrayObject $extra)
+    // {
+    //     $query = $this->ControllerAction->getQueryString();
+    //     if (isset($extra['redirect']['query'])) {
+    //         unset($extra['redirect']['query']);
+    //     }
+
+    //     if ($query) {
+    //         $userId = $query['id'];
+
+    //         $this->fields = [];
+    //         $this->field('student', ['userId' => $userId]);
+
+    //         $this->field('exam_details_header', ['type' => 'section', 'title' => __('Register to Exam')]);
+
+    //         $this->field('academic_period_id');
+    //         $this->field('examination_id');
+    //         $this->field('examination_centre_id');
+
+    //         // name emis num, acad, exam,
+    //     } else {
+    //         // return $this->controller->redirect([
+    //         //     'plugin' => 'Institution',
+    //         //     'controller' => 'Institutions',
+    //         //     'action' => 'StaffTrainingApplications',
+    //         //     '0' => 'index'
+    //         // ]);
+    //     }
+
+    //     // $event->stopPropagation();
+    //     // return $this->controller->redirect($extra['redirect']);
+    // }
+
+    // public function onUpdateFieldStudent(Event $event, array $attr, $action, $request)
+    // {
+    //     $selectedStudent = $attr['userId'];
+
+    //     $attr['type'] = 'readonly';
+    //     $attr['attr']['value'] = $this->get($selectedStudent)->name_with_id;
+
+    //     return $attr;
+    // }
+
+    // public function onUpdateFieldAcademicPeriodId(Event $event, array $attr, $action, $request)
+    // {
+    //     $periodOptions = $this->AcademicPeriods->getYearList(['isEditable' => true]);
+
+    //     $attr['type'] = 'select';
+    //     $attr['options'] = $periodOptions;
+    //     $attr['onChangeReload'] = true;
+    //     return $attr;
+    // }
+
+    // public function onUpdateFieldExaminationId(Event $event, array $attr, $action, $request)
+    // {
+    //     if (!empty($request->data[$this->alias()]['academic_period_id'])) {
+    //         $selectedAcademicPeriod = $request->data[$this->alias()]['academic_period_id'];
+    //         $Examinations = TableRegistry::get('Examination.Examinations');
+    //         $examinationOptions = $Examinations
+    //             ->find('list')
+    //             ->where([$Examinations->aliasField('academic_period_id') => $selectedAcademicPeriod])
+    //             ->toArray();
+    //     }
+
+    //     $attr['type'] = 'select';
+    //     $attr['options'] = !empty($examinationOptions)? $examinationOptions: [];
+    //     $attr['onChangeReload'] = true;
+    //     return $attr;
+    // }
+
+    // public function onUpdateFieldExaminationCentreId(Event $event, array $attr, $action, $request)
+    // {
+    //     if (!empty($request->data[$this->alias()]['examination_id'])) {
+    //         $selectedExam = $request->data[$this->alias()]['examination_id'];
+    //         $ExamCentres = TableRegistry::get('Examination.ExaminationCentres');
+    //         $examCentreOptions = $ExamCentres
+    //             ->find('list')
+    //             ->where([$ExamCentres->aliasField('examination_id') => $selectedExam])
+    //             ->toArray();
+    //     }
+
+    //     $attr['type'] = 'select';
+    //     $attr['options'] = !empty($examCentreOptions)? $examCentreOptions: [];
+
+    //     return $attr;
+    // }
 
     public function findResults(Query $query, array $options) {
         $academicPeriodId = $options['academic_period_id'];
