03a0224ee01d3ca339c2e5831d63bab94091d1e3
Chiang Hao Long <hchiang@kordit.com>
2016-11-09 15:11:50 +0800

    POCOR-3507 - Added rooms details to view page of examination centre

---
+++ plugins/Examination/src/Model/Table/ExaminationCentreRoomsTable.php
@@ -0,0 +1,29 @@
+<?php
+namespace Examination\Model\Table;
+
+use App\Model\Table\ControllerActionTable;
+use Cake\Event\Event;
+use Cake\Network\Request;
+use ArrayObject;
+use Cake\Validation\Validator;
+use Cake\ORM\Query;
+
+class ExaminationCentreRoomsTable extends ControllerActionTable {
+
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+        $this->addBehavior('Area.Areapicker');
+        $this->belongsTo('Examinations', ['className' => 'Examination.Examinations']);
+        $this->belongsTo('AcademicPeriods', ['className' => 'AcademicPeriod.AcademicPeriods']);
+        $this->belongsTo('ExaminationCentres', ['className' => 'Examination.ExaminationCentres']);
+        $this->hasMany('ExaminationCentreRoomStudents', ['className' => 'Examination.ExaminationCentreRoomStudents']);
+        $this->setDeleteStrategy('restrict');
+    }
+
+    public function beforeAction(Event $event, ArrayObject $extra)
+    {
+        $this->field('examination_id', ['visible' => false]);
+        $this->field('examination_centre_id', ['visible' => false]);
+    }
+}
