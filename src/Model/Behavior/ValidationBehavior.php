1369be791fb4466dc13a67c9f16815c193e8a379
Harry Suhendra <hsuhendra@kordit.com>
2016-11-11 15:54:03 +0800

    POCOR-3312 - Reused value function in the configItemsTable to get the value from the specific code.

--- src/Model/Behavior/ValidationBehavior.php
+++ src/Model/Behavior/ValidationBehavior.php
@@ -996,14 +996,10 @@ class ValidationBehavior extends Behavior {
 				$startTime = min($shiftStartTimeArray);
 				$endTime = max($shiftEndTimeArray);
 			} else {
-				$configItems = TableRegistry::get('Configuration.configItems');
+				$ConfigItems = TableRegistry::get('Configuration.configItems');
 
-				$shiftTime = $configItems->find()
-					->where([$configItems->aliasField('type') => 'Attendance'])
-					->toArray();
-
-				$configStartTime = $shiftTime[2]['value'] ? $shiftTime[2]['value'] : $shiftTime[2]['default_value'];
-				$hourPerDay = $shiftTime[1]['value'] ? $shiftTime[1]['value'] : $shiftTime[1]['default_value'];
+				$configStartTime = $ConfigItems->value('start_time');
+				$hourPerDay = $ConfigItems->value('hours_per_day');
 
 				$startTime = new time($configStartTime);
 
