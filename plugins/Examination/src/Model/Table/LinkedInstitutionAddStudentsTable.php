67e59f71194996ce5244181d5e5e70b4a133f0a5
Livvi Sim <lsim@kordit.com>
2016-11-17 18:32:24 +0800

    POCOR-3508 - Fixed bug in breadcrumbs

--- plugins/Examination/src/Model/Table/LinkedInstitutionAddStudentsTable.php
+++ plugins/Examination/src/Model/Table/LinkedInstitutionAddStudentsTable.php
@@ -58,8 +58,10 @@ class LinkedInstitutionAddStudentsTable extends ControllerActionTable {
     public function onGetBreadcrumb(Event $event, Request $request, Component $Navigation, $persona)
     {
         $queryString = $request->query['queryString'];
+        $indexUrl = ['plugin' => 'Examination', 'controller' => 'Examinations', 'action' => 'ExamCentres'];
         $overviewUrl = ['plugin' => 'Examination', 'controller' => 'Examinations', 'action' => 'ExamCentres', 'view', 'queryString' => $queryString];
 
+        $Navigation->substituteCrumb('Examination', 'Examination', $indexUrl);
         $Navigation->substituteCrumb('Linked Institution Add Students', 'Exam Centres', $overviewUrl);
         $Navigation->addCrumb('Students');
     }
