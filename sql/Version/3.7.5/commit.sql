b24bc3202f9ee39d8542bf03c45a35da01308d27
Steven Hasanuddin <shasanuddin@kordit.com>
2016-11-22 16:59:32 +0800

    3.7.5 - Updated commit.sql and rollback.sql for POCOR-3427

--- sql/Version/3.7.5/commit.sql
+++ sql/Version/3.7.5/commit.sql
@@ -1,3 +1,12 @@
+-- POCOR-3427
+-- db_patches
+INSERT INTO `db_patches` (`issue`, `created`) VALUES ('POCOR-3427', NOW());
+
+-- institutions
+ALTER TABLE `institutions`
+CHANGE `is_academic` `classification` INT(1) NOT NULL DEFAULT '1' COMMENT '0 -> Non-academic institution, 1 -> Academic Institution';
+
+
 -- POCOR-3525
 -- db_patches
 INSERT INTO `db_patches` (`issue`, `created`) VALUES('POCOR-3525', NOW());
