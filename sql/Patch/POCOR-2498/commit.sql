add9f35bbca1d16ae229231ebef5fd175ae6b764
Harry Suhendra <hsuhendra@kordit.com>
2016-11-25 18:01:48 +0800

    POCOR-2498 - DB patches for FieldOptions studentBehaviourClassifications and studentBehaviourCategoriesClassifications.

--- sql/Patch/POCOR-2498/commit.sql
+++ sql/Patch/POCOR-2498/commit.sql
@@ -33,9 +33,35 @@ ALTER TABLE `indexes_criteria`
   ADD KEY `index_id` (`index_id`);
 
 
+-- Table structure for table `student_behaviour_categories_classifications`
+CREATE TABLE IF NOT EXISTS `student_behaviour_categories_classifications` (
+  `id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `student_behaviour_classification_id` int(11) NOT NULL COMMENT 'links to student behaviour classifications',
+  `student_behaviour_category_id` int(11) NOT NULL COMMENT 'links to student behaviour categories'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+ALTER TABLE `student_behaviour_categories_classifications`
+  ADD PRIMARY KEY (`student_behaviour_classification_id`,`student_behaviour_category_id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `student_behaviour_classification_id` (`student_behaviour_classification_id`),
+  ADD KEY `student_behaviour_category_id` (`student_behaviour_category_id`);
 
 
-
+-- Table structure for table `student_behaviour_classifications`
+CREATE TABLE IF NOT EXISTS `student_behaviour_classifications` (
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `order` int(3) NOT NULL,
+  `visible` int(1) NOT NULL DEFAULT '1',
+  `editable` int(1) NOT NULL DEFAULT '1',
+  `default` int(1) NOT NULL DEFAULT '0',
+  `international_code` varchar(50) DEFAULT NULL,
+  `national_code` varchar(50) DEFAULT NULL,
+  `modified_user_id` int(11) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `created_user_id` int(11) NOT NULL,
+  `created` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 
