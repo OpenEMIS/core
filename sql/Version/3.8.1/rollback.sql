7e2b0cc8471b29a6b9249511ce25264314a6dc25
Steven Hasanuddin <shasanuddin@kordit.com>
2016-11-29 16:38:16 +0800

    3.8.1 - Updated commit.sql and rollback.sql for POCOR-3568

--- sql/Version/3.8.1/rollback.sql
+++ sql/Version/3.8.1/rollback.sql
@@ -1,2 +1,15 @@
+-- POCOR-3568
+DROP TABLE IF EXISTS `textbooks`;
+
+DROP TABLE IF EXISTS `textbook_conditions`;
+
+DROP TABLE IF EXISTS `textbook_statuses`;
+
+DROP TABLE IF EXISTS `institution_textbooks`;
+
+-- db_patches
+DELETE FROM `db_patches` WHERE `issue`='POCOR-3568';
+
+
 -- 3.7.5
 UPDATE config_items SET value = '3.7.5' WHERE code = 'db_version';
