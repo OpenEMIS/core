ba56ff510be2c7ef0b4b46bea645f09dea938ac9
Thed Kuok Shiun <kthed@kordit.com>
2016-11-30 18:15:18 +0800

    POCOR-3576 - Working on Custom Excel Report.

---
+++ plugins/CustomExcel/src/Controller/Component/ExcelReportComponent.php
@@ -0,0 +1,28 @@
+<?php
+namespace CustomExcel\Controller\Component;
+
+use ArrayObject;
+use Cake\ORM\TableRegistry;
+use Cake\Controller\Component;
+use Cake\Event\Event;
+
+class ExcelReportComponent extends Component
+{
+	private $controller;
+
+	public function initialize(array $config)
+	{
+		parent::initialize($config);
+		$this->controller = $this->_registry->getController();
+	}
+
+	public function renderExcel($params=[])
+	{
+		$className = $params['className'];
+		$model = TableRegistry::get($className);
+
+		$extra = new ArrayObject([]);
+		$event = $model->dispatchEvent('ExcelTemplates.Model.onRenderExcelTemplate', [$extra], $this->controller);
+		if ($event->isStopped()) { return $event->result; }
+	}
+}
