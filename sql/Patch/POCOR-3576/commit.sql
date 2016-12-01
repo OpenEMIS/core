e81f06362734d88f11f47de0415d99d4c48c640c
Thed Kuok Shiun <kthed@kordit.com>
2016-12-01 10:52:24 +0800

    POCOR-3576 - Revert add file_type to FileUpload behavior and remove file_type from excel_templates table.

--- sql/Patch/POCOR-3576/commit.sql
+++ sql/Patch/POCOR-3576/commit.sql
@@ -7,7 +7,6 @@ CREATE TABLE IF NOT EXISTS `excel_templates` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `module` varchar(100) NOT NULL,
   `file_name` varchar(250) DEFAULT NULL,
-  `file_type` varchar (255) DEFAULT NULL,
   `file_content` longblob,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='This table contains template for a specific report';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='This table contains excel template for a specific report';
