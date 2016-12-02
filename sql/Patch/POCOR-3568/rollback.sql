717dbd9efac57e4970f1ea53b220700dd6bda468
Martinnus Budiarto <mbudiarto@kordit.com>
2016-12-02 11:42:30 +0800

    POCOR-3568 - fixes on adjustments, permission and report

--- sql/Patch/POCOR-3568/rollback.sql
+++ sql/Patch/POCOR-3568/rollback.sql
@@ -9,5 +9,14 @@ DROP TABLE IF EXISTS `institution_textbooks`;
 DELETE FROM `labels`
 WHERE `module` = 'InstitutionTextbooks';
 
+DELETE FROM `security_functions`
+WHERE `id` = 5055, 1051, 6010;
+
+-- re-arrange order
+UPDATE `security_functions`
+SET `order` = `order` - 1
+WHERE `id` BETWEEN 6000 AND 7000
+AND `order` > 6003;
+
 -- db_patches
 DELETE FROM `db_patches` WHERE `issue`='POCOR-3568';
