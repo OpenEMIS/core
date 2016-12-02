6c201c92690e4dffa4a8ce0ad2ef56522e426ae6
Chiang Hao Long <hchiang@kordit.com>
2016-12-02 12:12:16 +0800

    3.8.1 - Fixed student/staff attendance and behavior links

--- plugins/Institution/src/Model/Table/StudentAttendancesTable.php
+++ plugins/Institution/src/Model/Table/StudentAttendancesTable.php
@@ -348,7 +348,7 @@ class StudentAttendancesTable extends AppTable {
 			'controller' => 'Institutions',
 			'action' => 'StudentUser',
 			'view',
-			$entity->user->id
+			$this->ControllerAction->paramsEncode(['id' => $entity->user->id])
 		]);
 
 		if ($timeError) {
