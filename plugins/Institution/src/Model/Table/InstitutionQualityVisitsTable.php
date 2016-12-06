a190a940cd390c712908f43c4b430f09c7c9b462
Martinnus Budiarto <mbudiarto@kordit.com>
2016-12-06 11:11:02 +0800

    POCOR-2944 - remove mandatory for subject and staff

--- plugins/Institution/src/Model/Table/InstitutionQualityVisitsTable.php
+++ plugins/Institution/src/Model/Table/InstitutionQualityVisitsTable.php
@@ -105,17 +105,19 @@ class InstitutionQualityVisitsTable extends ControllerActionTable
 
 	public function onGetStaffId(Event $event, Entity $entity)
 	{
-		if ($this->action == 'view') {
-			return $event->subject()->Html->link($entity->staff->name_with_id , [
-				'plugin' => 'Institution',
-				'controller' => 'Institutions',
-				'action' => 'StaffUser',
-				'view',
-				$entity->staff->id
-			]);
-		} else {
-			return $entity->staff->name_with_id;
-		}
+        if ($entity->staff) {
+            if ($this->action == 'view') {
+                return $event->subject()->Html->link($entity->staff->name_with_id , [
+                    'plugin' => 'Institution',
+                    'controller' => 'Institutions',
+                    'action' => 'StaffUser',
+                    'view',
+                    $entity->staff->id
+                ]);
+            } else {
+                return $entity->staff->name_with_id;
+            }
+        }
 	}
 
 	public function onUpdateFieldAcademicPeriodId(Event $event, array $attr, $action, Request $request)
