9cc6f7d3d15f0b862af1487601a71552b2e07d4d
Livvi Sim <lsim@kordit.com>
2016-12-07 15:43:31 +0800

    POCOR-3562 - Add patch to alter institution_subject_students table

---
+++ sql/Patch/POCOR-3562/commit.sql
@@ -0,0 +1,18 @@
+-- db_patches
+INSERT INTO `db_patches` (`issue`, `created`) VALUES('POCOR-3562', NOW());
+
+-- create backup institution_subject_students table
+CREATE TABLE IF NOT EXISTS `z_3562_institution_subject_students` LIKE `institution_subject_students`;
+INSERT INTO `z_3562_institution_subject_students` SELECT * FROM `institution_subject_students`;
+
+-- institution_subject_students
+ALTER TABLE `institution_subject_students`
+DROP COLUMN `status`,
+ADD COLUMN `student_status_id` INT(11) NOT NULL COMMENT 'links to student_statuses.id' AFTER `education_subject_id`;
+
+-- patch student_status_id from institution_class_students
+UPDATE `institution_subject_students`
+INNER JOIN `institution_class_students`
+ON (`institution_subject_students`.`student_id` = `institution_class_students`.`student_id`
+AND `institution_subject_students`.`institution_class_id` = `institution_class_students`.`institution_class_id`)
+SET `institution_subject_students`.`student_status_id` = `institution_class_students`.`student_status_id`;
