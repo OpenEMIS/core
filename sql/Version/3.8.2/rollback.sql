0d4be51bc32fc995fcdb505db9d7f89e5350cbd9
Steven Hasanuddin <shasanuddin@kordit.com>
2016-12-12 13:30:01 +0800

    3.8.2 - Updated commit.sql and rollback.sql for POCOR-2944

--- sql/Version/3.8.2/rollback.sql
+++ sql/Version/3.8.2/rollback.sql
@@ -1,3 +1,12 @@
+-- POCOR-2944
+-- update institution_quality_visits
+ALTER TABLE `institution_quality_visits` CHANGE `institution_subject_id` `institution_subject_id` INT(11) NOT NULL COMMENT 'links to institution_subjects.id';
+ALTER TABLE `institution_quality_visits` CHANGE `staff_id` `staff_id` INT(11) NOT NULL COMMENT 'links to security_users.id';
+
+-- db_patches
+DELETE FROM `db_patches` WHERE `issue`='POCOR-2944';
+
+
 -- POCOR-3457
 -- assessment_grading_options
 ALTER TABLE `assessment_grading_options` DROP `description`;
