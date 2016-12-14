f3feb2a6d6684b61e7399215df7c49c82f23f106
root <root@ip-192-168-0-72.ap-southeast-1.compute.internal>
2016-12-14 14:12:12 +0800

    3.8.2 - Updated commit.sql and rollback.sql for POCOR-3605

--- sql/Version/3.8.2/rollback.sql
+++ sql/Version/3.8.2/rollback.sql
@@ -1,3 +1,12 @@
+-- POCOR-3605
+-- security_functions
+DELETE FROM `security_functions` WHERE `id` = 5056;
+UPDATE `security_functions` SET `order` = 5009 WHERE `id` = 5009;
+
+-- db_patches
+DELETE FROM `db_patches` WHERE `issue`='POCOR-3605';
+
+
 -- POCOR-3539
 -- translations
 DELETE FROM `translations` WHERE `en` = 'Student has been transferred to';
