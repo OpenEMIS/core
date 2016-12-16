af8197f03d7dd6dc3a7697060ca880d0ccd05b03
Steven Hasanuddin <shasanuddin@kordit.com>
2016-12-16 17:28:13 +0800

    3.8.2 - Updated commit.sql and rollback.sql for POCOR-3661

--- sql/Version/3.8.2/rollback.sql
+++ sql/Version/3.8.2/rollback.sql
@@ -14,6 +14,9 @@ DELETE FROM `db_patches` WHERE `issue`='POCOR-3457';
 DELETE FROM `import_mapping`
 WHERE `model` = 'Examination.ExaminationCentreRooms';
 
+-- security_functions
+DELETE FROM `security_functions` WHERE `security_functions`.`id` = 5057;
+
 -- db_patches
 DELETE FROM `db_patches` WHERE `issue` = 'POCOR-3661';
 
