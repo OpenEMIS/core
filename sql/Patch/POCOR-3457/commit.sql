295431ea95d549bf9a459a49631eaada6c6a6412
Martinnus Budiarto <mbudiarto@kordit.com>
2016-12-16 13:39:54 +0800

    POCOR-3457 - description for exam grading option

--- sql/Patch/POCOR-3457/commit.sql
+++ sql/Patch/POCOR-3457/commit.sql
@@ -3,3 +3,6 @@ INSERT INTO `db_patches` (`issue`, `created`) VALUES ('POCOR-3457', NOW());
 
 -- assessment_grading_options
 ALTER TABLE `assessment_grading_options` ADD `description` TEXT NULL DEFAULT NULL AFTER `name`;
+
+-- examination_grading_options
+ALTER TABLE `examination_grading_options` ADD `description` TEXT NULL DEFAULT NULL AFTER `name`;
