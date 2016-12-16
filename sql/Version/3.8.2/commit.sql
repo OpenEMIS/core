933869436ec51c5ae1eff8b6f7a9390efe4fbab5
Steven Hasanuddin <shasanuddin@kordit.com>
2016-12-16 14:11:46 +0800

    3.8.2 - Updated master commit.sql and rollback.sql for POCOR-3457

--- sql/Version/3.8.2/commit.sql
+++ sql/Version/3.8.2/commit.sql
@@ -54,13 +54,6 @@ ALTER TABLE `institution_quality_visits` CHANGE `institution_subject_id` `instit
 ALTER TABLE `institution_quality_visits` CHANGE `staff_id` `staff_id` INT(11) NULL COMMENT 'links to security_users.id';
 
 
--- POCOR-3457
--- db_patches
-INSERT INTO `db_patches` (`issue`, `created`) VALUES ('POCOR-3457', NOW());
-
--- assessment_grading_options
-ALTER TABLE `assessment_grading_options` ADD `description` TEXT NULL DEFAULT NULL AFTER `name`;
-
 
 -- 3.8.2
 UPDATE config_items SET value = '3.8.2' WHERE code = 'db_version';
--- sql/Version/3.8.2/rollback.sql
+++ sql/Version/3.8.2/rollback.sql
@@ -45,13 +45,5 @@ ALTER TABLE `institution_quality_visits` CHANGE `staff_id` `staff_id` INT(11) NO
 DELETE FROM `db_patches` WHERE `issue`='POCOR-2944';
 
 
--- POCOR-3457
--- assessment_grading_options
-ALTER TABLE `assessment_grading_options` DROP `description`;
-
--- db_patches
-DELETE FROM `db_patches` WHERE `issue`='POCOR-3457';
-
-
 -- 3.8.1.1
 UPDATE config_items SET value = '3.8.1.1' WHERE code = 'db_version';
