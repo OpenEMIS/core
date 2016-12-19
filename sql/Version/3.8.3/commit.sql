bc7ef63a99f6bf81157743c0f71945e5046cc5f3
Steven Hasanuddin <shasanuddin@kordit.com>
2016-12-19 13:48:42 +0800

    3.8.3 - Updated sql files

---
+++ sql/Version/3.8.3/commit.sql
@@ -0,0 +1,28 @@
+-- POCOR-3623
+-- db_patches
+INSERT INTO `db_patches` (`issue`, `created`) VALUES ('POCOR-3623', NOW());
+
+-- backup user_identities
+DROP TABLE IF EXISTS `z_3623_user_identities`;
+CREATE TABLE `z_3623_user_identities` LIKE `user_identities`;
+
+INSERT INTO `z_3623_user_identities`
+SELECT * FROM `user_identities` UI
+WHERE NOT EXISTS (
+        SELECT 1
+    FROM `security_users` SU
+    WHERE UI.`security_user_id` = SU.`id`
+);
+
+-- delete user_identities
+DELETE FROM `user_identities`
+WHERE NOT EXISTS (
+        SELECT 1
+    FROM `security_users`
+    WHERE `user_identities`.`security_user_id` = `security_users`.`id`
+);
+
+
+-- 3.8.3
+UPDATE config_items SET value = '3.8.3' WHERE code = 'db_version';
+UPDATE db_patches SET version = (SELECT value FROM config_items WHERE code = 'db_version') WHERE version IS NULL;
