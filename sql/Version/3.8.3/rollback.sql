fff8cd2d133b10e9bd2ca6486c4a7e042e08aa88
Steven Hasanuddin <shasanuddin@kordit.com>
2016-12-20 10:41:27 +0800

    3.8.3 - Updated master commit.sql and rollback.sql for POCOR-3593

--- sql/Version/3.8.3/rollback.sql
+++ sql/Version/3.8.3/rollback.sql
@@ -1,3 +1,25 @@
+-- POCOR-3593
+-- security_users
+ALTER TABLE `security_users`
+DROP COLUMN `identity_type_id`,
+DROP COLUMN `nationality_id`,
+DROP COLUMN `external_reference`;
+
+DELETE FROM `config_item_options` WHERE `id` = 102;
+
+DROP TABLE `external_data_source_attributes`;
+
+ALTER TABLE `z_3593_external_data_source_attributes`
+RENAME TO  `external_data_source_attributes` ;
+
+UPDATE `config_items` INNER JOIN `z_3593_config_items` ON `z_3593_config_items`.`id` = `config_items`.`id` SET `config_items`.`value` = `z_3593_config_items`.`value`;
+
+DROP TABLE `z_3593_config_items`;
+
+-- db_patches
+DELETE FROM `db_patches` WHERE `issue` = 'POCOR-3593';
+
+
 -- POCOR-3632
 -- security_functions
 DELETE FROM `security_functions` WHERE `security_functions`.`id` = 5058;
