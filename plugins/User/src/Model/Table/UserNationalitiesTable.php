426bc5089844e293c6a9a2e84f794e5c7c8fa5ba
Martinnus Budiarto <mbudiarto@kordit.com>
2016-12-20 16:54:25 +0800

    POCOR-3606 - add logic to prevent user to have more than one record for the same nationality.

--- plugins/User/src/Model/Table/UserNationalitiesTable.php
+++ plugins/User/src/Model/Table/UserNationalitiesTable.php
@@ -3,8 +3,13 @@ namespace User\Model\Table;
 
 use ArrayObject;
 
+use Cake\ORM\Query;
+use Cake\ORM\Entity;
+use Cake\ORM\TableRegistry;
 use Cake\Validation\Validator;
 use Cake\Event\Event;
+use Cake\Network\Session;
+use Cake\Network\Request;
 use App\Model\Table\ControllerActionTable;
 
 class UserNationalitiesTable extends ControllerActionTable {
@@ -18,6 +23,11 @@ class UserNationalitiesTable extends ControllerActionTable {
 	public function beforeAction(Event $event) {
 		$this->fields['nationality_id']['type'] = 'select';
 	}
+
+    public function addEditAfterAction(Event $event, Entity $entity, ArrayObject $extra)
+    {
+        $this->setupFields($entity);    
+    }
 
 	public function validationDefault(Validator $validator) {
 		$validator = parent::validationDefault($validator);
@@ -56,6 +66,48 @@ class UserNationalitiesTable extends ControllerActionTable {
 
 	public function afterAction(Event $event) {
 		$this->setupTabElements();
-	}	
+	}
 
+    public function onUpdateFieldNationalityId(Event $event, array $attr, $action, Request $request)
+    {
+        //get the current user nationality
+        $currentNationalities = $this
+                                ->find('list', ['keyField' => 'id', 'valueField' => 'id'])
+                                ->matching('NationalitiesLookUp')
+                                ->where([
+                                    $this->aliasfield('security_user_id') => $this->Session->read('Student.Students.id')
+                                ])
+                                ->select([
+                                    'id' => $this->NationalitiesLookUp->aliasfield('id')
+                                ])
+                                ->toArray();
+
+        if ($action == 'add' || $action == 'edit') {
+            if ($action == 'edit') { //when edit then include the nationality that is being edited.
+                $nationalityId[] = $attr['entity']->nationality_id;
+                $currentNationalities = array_diff($currentNationalities, $nationalityId);
+            }
+
+            $nationalities = $this->NationalitiesLookUp->find('visible')->find('list');
+
+            if (!empty($currentNationalities)) {
+                $nationalities = $nationalities
+                                    ->where([
+                                        $this->NationalitiesLookUp->aliasfield('id NOT IN ') => $currentNationalities
+                                    ]);
+            }
+
+            $nationalities = $nationalities->toArray();
+            $attr['options'] = $nationalities;
+        }
+        return $attr;
+    }
+
+    private function setupFields(Entity $entity)
+    {
+        $this->field('nationality_id', [
+            'type' => 'select', 
+            'entity' => $entity
+        ]);
+    }
 }
