e3e06e31aa5740b7c3b794b689faaca072e778b9
Thed Kuok Shiun <kthed@kordit.com>
2016-12-20 14:12:41 +0800

    POCOR-3576 - Add permission to view, edit and download custom excel templates.

--- sql/Patch/POCOR-3576/commit.sql
+++ sql/Patch/POCOR-3576/commit.sql
@@ -14,3 +14,7 @@ CREATE TABLE IF NOT EXISTS `excel_templates` (
 -- Labels
 INSERT INTO `labels` (`id`, `module`, `field`, `module_name`, `field_name`, `code`, `name`, `visible`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES
 ('ad8fa33a-c0d8-11e6-90e8-525400b263eb', 'ExcelTemplates', 'file_content', 'CustomExcels -> ExcelTemplates', 'Attachment', NULL, NULL, 1, NULL, NULL, 1, NOW());
+
+-- security_functions
+INSERT INTO `security_functions` (`id`, `name`, `controller`, `module`, `category`, `parent_id`, `_view`, `_edit`, `_add`, `_delete`, `_execute`, `order`, `visible`, `description`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES
+(5059, 'Excel Templates', 'CustomExcels', 'Administration', 'CustomExcels', '5000', 'ExcelTemplates.index|ExcelTemplates.view', 'ExcelTemplates.edit', NULL, NULL, 'ExcelTemplates.download', 5059, 1, NULL, NULL, NULL, 1, NOW());
