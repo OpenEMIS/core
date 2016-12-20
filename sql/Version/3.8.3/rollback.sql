69471695371937087a70ec96ea908b9fa9ed7407
Steven Hasanuddin <shasanuddin@kordit.com>
2016-12-20 09:34:49 +0800

    3.8.3 - Updated commit.sql and rollback.sql for POCOR-3632

--- sql/Version/3.8.3/rollback.sql
+++ sql/Version/3.8.3/rollback.sql
@@ -1,3 +1,15 @@
+-- POCOR-3632
+-- security_functions
+DELETE FROM `security_functions` WHERE `security_functions`.`id` = 5058;
+
+UPDATE `security_functions`
+SET `order` = `order` - 1
+WHERE `order` < 6000 AND `order` > 5010;
+
+-- db_patches
+DELETE FROM `db_patches` WHERE `issue` = 'POCOR-3632';
+
+
 -- POCOR-3633
 -- security_function
 UPDATE `security_functions` SET `_edit`='StudentUser.edit' WHERE `id`='2000';
