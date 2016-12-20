a5fefe86105b90265ce575122cc7dbf44e736ceb
Martinnus Budiarto <mbudiarto@kordit.com>
2016-12-20 11:26:09 +0800

    POCOR-3629 - move out Assessments and Textbooks from System Setup

--- src/Controller/Component/NavigationComponent.php
+++ src/Controller/Component/NavigationComponent.php
@@ -788,18 +788,6 @@ class NavigationComponent extends Component
 					'selected' => ['Educations.Systems', 'Educations.Levels', 'Educations.Cycles', 'Educations.Programmes', 'Educations.Grades', 'Educations.Subjects', 'Educations.GradeSubjects', 'Educations.Certifications',
 							'Educations.FieldOfStudies', 'Educations.ProgrammeOrientations']
 				],
-				'Textbooks.Textbooks' => [
-                    'title' => 'Textbooks',
-                    'parent' => 'SystemSetup',
-                    'params' => ['plugin' => 'Textbook'],
-                    'selected' => ['Textbooks.Textbooks']
-                ],
-				'Assessments.Assessments' => [
-					'title' => 'Assessments',
-					'parent' => 'SystemSetup',
-					'params' => ['plugin' => 'Assessment'],
-					'selected' => ['Assessments.Assessments', 'Assessments.AssessmentPeriods', 'Assessments.GradingTypes']
-				],
 				'FieldOptions.index' => [
 					'title' => 'Field Options',
 					'parent' => 'SystemSetup',
@@ -958,7 +946,12 @@ class NavigationComponent extends Component
 					'params' => ['plugin' => 'Training'],
 					'selected' => ['Trainings.Results']
 				],
-
+            'Assessments.Assessments' => [
+                'title' => 'Assessments',
+                'parent' => 'Administration',
+                'params' => ['plugin' => 'Assessment'],
+                'selected' => ['Assessments.Assessments', 'Assessments.AssessmentPeriods', 'Assessments.GradingTypes']
+            ],
 			'Administration.Examinations' => [
 					'title' => 'Examinations',
 					'parent' => 'Administration',
@@ -988,7 +981,12 @@ class NavigationComponent extends Component
 						'params' => ['plugin' => 'Examination'],
 						'selected' => ['Examinations.ExamResults', 'Examinations.Results', 'Examinations.ImportResults']
 					],
-
+            'Textbooks.Textbooks' => [
+                'title' => 'Textbooks',
+                'parent' => 'Administration',
+                'params' => ['plugin' => 'Textbook'],
+                'selected' => ['Textbooks.Textbooks']
+            ],
 			'Workflows.Workflows' => [
 				'title' => 'Workflow',
 				'parent' => 'Administration',
