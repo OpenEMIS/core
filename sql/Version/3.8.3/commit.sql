44c68159fb948bb42f31b073a961e79bc07b3014
Steven Hasanuddin <shasanuddin@kordit.com>
2016-12-20 09:29:25 +0800

    3.8.3 - Updated master commit.sql and rollback.sql

--- sql/Version/3.8.3/commit.sql
+++ sql/Version/3.8.3/commit.sql
@@ -1,3 +1,11 @@
+-- POCOR-3633
+-- db_patches
+INSERT INTO `db_patches` (`issue`, `created`) VALUES ('POCOR-3633', NOW());
+
+-- security_function
+UPDATE `security_functions` SET `_edit`='StudentUser.edit|StudentUser.pull' WHERE `id`='2000';
+
+
 -- POCOR-3623
 -- db_patches
 INSERT INTO `db_patches` (`issue`, `created`) VALUES ('POCOR-3623', NOW());
--- sql/Version/3.8.3/rollback.sql
+++ sql/Version/3.8.3/rollback.sql
@@ -1,2 +1,19 @@
+-- POCOR-3633
+-- security_function
+UPDATE `security_functions` SET `_edit`='StudentUser.edit' WHERE `id`='2000';
+
+-- db_patches
+DELETE FROM `db_patches` WHERE `issue` = 'POCOR-3633';
+
+
+-- POCOR-3623
+-- restore user_identities
+INSERT INTO `user_identities`
+SELECT * FROM `z_3623_user_identities`;
+
+-- db_patches
+DELETE FROM `db_patches` WHERE `issue`='POCOR-3623';
+
+
 -- 3.8.2
 UPDATE config_items SET value = '3.8.2' WHERE code = 'db_version';
