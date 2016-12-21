a1a9322e2156c0d4e5a9b629917d54d8a1c213b6
Chiang Hao Long <hchiang@kordit.com>
2016-12-21 09:58:14 +0800

    POCOR-3633 - Reverted update of identity type logic

--- plugins/User/src/Model/Table/IdentitiesTable.php
+++ plugins/User/src/Model/Table/IdentitiesTable.php
@@ -111,6 +111,18 @@ class IdentitiesTable extends ControllerActionTable
 		return $validator->allowEmpty('number');
 	}
 
+	public function afterSave(Event $event, Entity $entity, ArrayObject $extra)
+	{
+		$this->Users->updateIdentityNumber($entity->security_user_id, $this->getLatestDefaultIdentityNo($entity->security_user_id)); //update identity_number field on security_user table on add/edit action
+	}
+
+	public function afterDelete(Event $event, Entity $entity, ArrayObject $extra)
+	{
+		if ($entity->identity_type_id == $this->IdentityTypes->getDefaultValue()) { //if the delete is done to the default identity type
+			$this->Users->updateIdentityNumber($entity->security_user_id, $this->getLatestDefaultIdentityNo($entity->security_user_id)); //update identity_number field on security_user table on delete action
+		}
+	}
+
 	public function getLatestDefaultIdentityNo($userId)
 	{
 		$UserNationalityTable = TableRegistry::get('User.UserNationalities');
