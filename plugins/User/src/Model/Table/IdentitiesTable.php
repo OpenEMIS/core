409e00cc222de46c2a0535479949f19469631023
Chiang Hao Long <hchiang@kordit.com>
2016-12-21 14:36:29 +0800

    POCOR-3633 - Fixed identity type updating issue

--- plugins/User/src/Model/Table/IdentitiesTable.php
+++ plugins/User/src/Model/Table/IdentitiesTable.php
@@ -118,9 +118,7 @@ class IdentitiesTable extends ControllerActionTable
 
 	public function afterDelete(Event $event, Entity $entity, ArrayObject $extra)
 	{
-		if ($entity->identity_type_id == $this->IdentityTypes->getDefaultValue()) { //if the delete is done to the default identity type
-			$this->Users->updateIdentityNumber($entity->security_user_id, $this->getLatestDefaultIdentityNo($entity->security_user_id)); //update identity_number field on security_user table on delete action
-		}
+		$this->Users->updateIdentityNumber($entity->security_user_id, $this->getLatestDefaultIdentityNo($entity->security_user_id)); //update identity_number field on security_user table on delete action
 	}
 
 	public function getLatestDefaultIdentityNo($userId)
