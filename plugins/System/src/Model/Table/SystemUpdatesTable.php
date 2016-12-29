7d021f5829e114de0b6b25d254edd79398daf523
Jeff Zheng <jzheng@kordit.com>
2016-12-29 18:02:54 +0800

    POCOR-3527 - completed version retrieval via restful API

--- plugins/System/src/Model/Table/SystemUpdatesTable.php
+++ plugins/System/src/Model/Table/SystemUpdatesTable.php
@@ -10,6 +10,7 @@ use Cake\ORM\Query;
 use Cake\ORM\TableRegistry;
 use Cake\Mailer\Email;
 use Cake\I18n\Time;
+use Cake\Network\Http\Client;
 
 use App\Model\Table\ControllerActionTable;
 
@@ -44,28 +45,93 @@ class SystemUpdatesTable extends ControllerActionTable {
         $this->field('status', ['options' => ['1' => __('Pending'), '2' => __('Approved')], 'after' => 'approved_by']);
     }
 
+    public function indexBeforeAction(Event $event, ArrayObject $extra)
+    {
+        $SystemPatches = TableRegistry::get('System.SystemPatches');
+        $versions = $SystemPatches
+            ->find()
+            ->select(['version', 'created'])
+            ->where([
+                'NOT EXISTS (SELECT 1 FROM system_updates WHERE system_updates.version = SystemPatches.version AND system_updates.status = 2)'
+            ])
+            ->group(['version'])
+            ->order(['created' => 'ASC', 'version' => 'ASC'])
+            ->all();
+
+        $query = $this->find();
+        $maxId = $query->select(['max' => $query->func()->max('id')])->hydrate(false)->first();
+
+        foreach ($versions as $version) {
+            if ($this->exists(['version' => $version->version])) {
+                $this->updateAll(['status' => 2], ['version' => $version->version]);
+            } else {
+                $entity = $this->newEntity([
+                    'id' => ++$maxId['max'],
+                    'version' => $version->version,
+                    'date_released' => $version->created,
+                    'date_approved' => $version->created,
+                    'approved_by' => 1,
+                    'status' => 2,
+                    'created_user_id' => 1,
+                    'created' => $version->created
+                ]);
+                $this->save($entity);
+            }
+        }
+
+        $ConfigItems = TableRegistry::get('Configuration.ConfigItems');
+        $domain = $ConfigItems->value('version_api_domain');
+        $api = $domain . '/restful/v1/System-SystemUpdates.json?_fields=id,version,date_released&_limit=0';
+
+        $http = new Client();
+        $response = $http->get($api);
+
+        if ($response->statusCode() == 200) {
+            $data = array_reverse(json_decode($response->body(), true)['data']);
+
+            foreach ($data as $item) {
+
+                if ($item['id'] > $maxId['max']) {
+                    $entity = $this->newEntity([
+                        'id' => $item['id'],
+                        'version' => $item['version'],
+                        'date_released' => $item['date_released'],
+                        'date_approved' => NULL,
+                        'approved_by' => NULL,
+                        'status' => 1,
+                        'created_user_id' => 1,
+                        'created' => Time::now(),
+                    ]);
+                    $this->save($entity);
+                }
+            }
+        }
+    }
+
     public function indexBeforeQuery(Event $event, Query $query, ArrayObject $extra)
     {
-        $query->order([$this->aliasField('created') => 'DESC', $this->aliasField('version') => 'DESC']);
+        $query->order([$this->aliasField('date_released') => 'DESC', $this->aliasField('version') => 'DESC']);
 
-        $extra['toolbarButtons']['update'] = [
-            'type' => 'button',
-            'attr' => [
-                'class' => 'btn btn-xs btn-default',
-                'data-toggle' => 'tooltip',
-                'data-placement' => 'bottom',
-                'escape' => false,
-                'title' => __('Update')
-            ],
-            'label' => '<i class="fa fa-refresh"></i>',
-            'url' => ['updates']
-        ];
+        if ($this->exists(['status' => 1])) {
+            $extra['toolbarButtons']['update'] = [
+                'type' => 'button',
+                'attr' => [
+                    'class' => 'btn btn-xs btn-default',
+                    'data-toggle' => 'tooltip',
+                    'data-placement' => 'bottom',
+                    'escape' => false,
+                    'title' => __('Update')
+                ],
+                'label' => '<i class="fa fa-refresh"></i>',
+                'url' => ['updates']
+            ];
+        }
     }
 
     public function onGetFormButtons(Event $event, ArrayObject $buttons)
     {
         if ($this->action == 'updates') {
-            $name = str_replace('Save', 'Update', $buttons[0]['name']);
+            $name = str_replace('Save', 'Upgrade', $buttons[0]['name']);
             $buttons[0]['name'] = $name;
         }
     }
@@ -73,39 +139,31 @@ class SystemUpdatesTable extends ControllerActionTable {
     public function updates(Event $mainEvent, ArrayObject $extra)
     {
         $ConfigItems = TableRegistry::get('Configuration.ConfigItems');
-        $supportEmails = $ConfigItems->value('version_support_emailss');
+        $supportEmails = $ConfigItems->value('version_support_emails');
 
         if ($this->request->is(['post', 'put'])) {
-            $ConfigItems = TableRegistry::get('Configuration.ConfigItems');
-            $emails = explode(',', $ConfigItems->value('version_support_emails'));
-
-            // pr($emails);die;
+            $emails = explode(',', $supportEmails);
 
             $host = $this->request->env('HTTP_HOST');
             $subject = 'Core Upgrade Request - ' . $host;
-            // die;
 
             $email = new Email('openemis');
             $email
             ->to($emails)
             ->subject($subject)
             ->send($subject);
-            // die;
 
             $this->updateAll(['date_approved' => Time::now(), 'approved_by' => $this->Auth->user('id'), 'status' => 2], ['status' => 1]);
-            // $this->Alert->success()
+            $this->Alert->show('Your upgrade request has been submitted.', 'success');
+
             $mainEvent->stopPropagation();
             return $this->controller->redirect(['action' => 'Updates', 'index']);
         } else {
             if (!$supportEmails) {
-
-                // $mainEvent->stopPropagation();
-                // $this->Alert->show('Support email has not been set up.', 'error');
-                // return $this->controller->redirect(['action' => 'Updates', 'index']);
+                $mainEvent->stopPropagation();
+                $this->Alert->show('Support email has not been set up.', 'error');
+                return $this->controller->redirect(['action' => 'Updates', 'index']);
             }
-            // pr(Time::now());die;
-            $query = $this->find();
-            // pr($query->select(['max' => $query->func()->max('id')])->first());
 
             $toolbarButtonsArray = $extra['toolbarButtons']->getArrayCopy();
             $toolbarAttr = [
@@ -125,9 +183,10 @@ class SystemUpdatesTable extends ControllerActionTable {
             $extra['elements']['edit'] = ['name' => 'OpenEmis.ControllerAction/edit'];
             $this->fields = []; // reset all the fields
 
-            // $this->fields['description'] = [
-            //     'type' =>
-            // ];
+            $this->field('description', [
+                'type' => 'element',
+                'element' => 'System.description'
+            ]);
 
             $entity = $this->newEntity();
 
---
+++ plugins/System/src/Template/Element/description.ctp
@@ -0,0 +1,17 @@
+Here is a list of changes in the latest version of OpenEMIS Core:
+<br>
+https://...
+
+<br><br>
+
+You can review these changes in the latest version of OpenEMIS Core:
+<br>
+https://...
+
+<br><br>
+Please click the upgrade button to approve for the OpenEMIS lab to upgrade your instance of OpenEMIS Core to the latest version.
+<br><br>
+Note that it will take 2-3 working days to complete the upgrade and data migration.
+<br><br>
+Please click here to review the status of the upgrade request.
+<br><br>
