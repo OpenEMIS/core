225258f6a4d991870b75c69e02ee5a726e2ec5ef
Jeff Zheng <jzheng@kordit.com>
2016-12-30 14:36:59 +0800

    POCOR-3527 - Added permissions

--- sql/Patch/POCOR-3527/commit.sql
+++ sql/Patch/POCOR-3527/commit.sql
@@ -19,6 +19,9 @@ CREATE TABLE IF NOT EXISTS `system_updates` (
 
 ALTER TABLE `system_updates` ADD UNIQUE(`version`);
 
+INSERT INTO `security_functions` (id, name, controller, module, category, parent_id, _view, _execute, `order`, `visible`, created_user_id, created) VALUES
+(5060, 'Updates', 'Systems', 'Administration', 'Updates', 5000, 'Updates.index|Updates.view', 'Updates.updates', 5061, 1, 1, NOW());
+
 
 INSERT INTO config_items (id, name, code, type, label, value, default_value, editable, visible, field_type, option_type, created_user_id, created) VALUES
 (200, 'Version Support Emails', 'version_support_emails', 'System', 'Version Support Emails', 'support@openemis.org,shasanuddin@kordit.com', 'support@openemis.org', 0, 0, '', '', 1, NOW()),
@@ -45,9 +48,11 @@ ORDER BY created ASC, version ASC
 
 -- Add below into version commit.sql at the end of the script
 
--- INSERT INTO system_updates (id, version, date_released, date_approved, approved_by, created) VALUES
+-- SET @maxId := 0;
+-- SELECT max(id) + 1 INTO @maxId FROM system_updates;
+-- INSERT IGNORE INTO system_updates (id, version, date_released, date_approved, approved_by, created) VALUES
 -- (
---   NULL,
+--   @maxId,
 --   (SELECT value FROM config_items WHERE code = 'db_version'),
 --   NOW(), NOW(), 1, NOW()
 -- );
--- sql/Patch/POCOR-3527/rollback.sql
+++ sql/Patch/POCOR-3527/rollback.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS `system_updates`;
+DELETE FROM security_functions WHERE id = 5060;
 DELETE FROM config_items WHERE id = 200;
 DELETE FROM config_items WHERE id = 201;
 RENAME TABLE `system_patches` TO `db_patches`;
