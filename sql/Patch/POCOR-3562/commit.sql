06687dca5a576b7c0b22c73333842f533b23a34a
Livvi Sim <lsim@kordit.com>
2017-01-03 13:31:06 +0800

    POCOR-3562 - Fix sql patch

--- sql/Patch/POCOR-3562/commit.sql
+++ sql/Patch/POCOR-3562/commit.sql
@@ -2,8 +2,11 @@
 INSERT INTO `db_patches` (`issue`, `created`) VALUES('POCOR-3562', NOW());
 
 -- create backup institution_subject_students table
-CREATE TABLE IF NOT EXISTS `z_3562_institution_subject_students` LIKE `institution_subject_students`;
-INSERT INTO `z_3562_institution_subject_students` SELECT * FROM `institution_subject_students`;
+CREATE TABLE IF NOT EXISTS `z_3562_institution_subject_students`
+LIKE `institution_subject_students`;
+
+INSERT INTO `z_3562_institution_subject_students`
+SELECT * FROM `institution_subject_students`;
 
 -- institution_subject_students
 ALTER TABLE `institution_subject_students`
@@ -14,5 +17,7 @@ ADD COLUMN `student_status_id` INT(11) NOT NULL COMMENT 'links to student_status
 UPDATE `institution_subject_students`
 INNER JOIN `institution_class_students`
 ON (`institution_subject_students`.`student_id` = `institution_class_students`.`student_id`
-AND `institution_subject_students`.`institution_class_id` = `institution_class_students`.`institution_class_id`)
+AND `institution_subject_students`.`institution_class_id` = `institution_class_students`.`institution_class_id`
+AND `institution_subject_students`.`academic_period_id` = `institution_class_students`.`academic_period_id`
+AND `institution_subject_students`.`institution_id` = `institution_class_students`.`institution_id`)
 SET `institution_subject_students`.`student_status_id` = `institution_class_students`.`student_status_id`;
