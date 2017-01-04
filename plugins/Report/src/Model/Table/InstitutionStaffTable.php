a58811a4bc8a11dc3596a63fb0e911289c302283
Chiang Hao Long <hchiang@kordit.com>
2017-01-04 16:59:20 +0800

    POCOR-2828 - Revert association

--- plugins/Report/src/Model/Table/InstitutionStaffTable.php
+++ plugins/Report/src/Model/Table/InstitutionStaffTable.php
@@ -18,7 +18,7 @@ class InstitutionStaffTable extends AppTable  {
 		parent::initialize($config);
 
 		$this->belongsTo('Users',			['className' => 'Security.Users', 'foreignKey' => 'staff_id']);
-		$this->belongsTo('Positions',		['className' => 'Institution.InstitutionPositions', 'foreignKey' => ['institution_position_id', 'academic_period_id']]);
+		$this->belongsTo('Positions',		['className' => 'Institution.InstitutionPositions', 'foreignKey' => 'institution_position_id']);
 		$this->belongsTo('Institutions',	['className' => 'Institution.Institutions', 'foreignKey' => 'institution_id']);
 		$this->belongsTo('StaffTypes',		['className' => 'Staff.StaffTypes']);
 		$this->belongsTo('StaffStatuses',	['className' => 'Staff.StaffStatuses']);
