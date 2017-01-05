19a5d577a1305d26dfdbdf9b36cb576c65cc7420
Chiang Hao Long <hchiang@kordit.com>
2017-01-05 18:28:38 +0800

    POCOR-2828 - Fixed address mapping

--- sql/Patch/POCOR-2828/rollback.sql
+++ sql/Patch/POCOR-2828/rollback.sql
@@ -3,5 +3,7 @@ UPDATE `security_functions` SET `_add`='StaffUser.add' WHERE `id`='1044';
 UPDATE `security_functions` SET `_add`='Staff.add' WHERE `id`='1016';
 UPDATE `security_functions` SET `_view`='index|view', `_edit`='edit', `_add`='add', `_delete`='remove' WHERE `id`='7000';
 
+DELETE FROM `external_data_source_type` WHERE `attribute_field` = 'address_mapping';
+
 -- system_patches
 DELETE FROM `system_patches` WHERE `issue` = 'POCOR-2828';
