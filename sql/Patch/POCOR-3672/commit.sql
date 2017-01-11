ba52d3e8b10e8d521a4547e282265bc7dd3521c1
Jeff Zheng <jzheng@kordit.com>
2017-01-11 16:51:17 +0800

    POCOR-3672 - updated to varchar(255)

--- sql/Patch/POCOR-3672/commit.sql
+++ sql/Patch/POCOR-3672/commit.sql
@@ -10,7 +10,7 @@ ADD COLUMN `ip_address` VARCHAR(45) NULL AFTER `session_id`;
 CREATE TABLE `single_logout` (
   `id` CHAR(36) NOT NULL,
   `username` VARCHAR(100) NOT NULL,
-  `url` VARCHAR(256) NOT NULL,
+  `url` VARCHAR(255) NOT NULL,
   `session_id` VARCHAR(50) NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `username` (`username`),
