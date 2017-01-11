1b8f441c858a6e934909f813443ac27283717a25
Steven Hasanuddin <shasanuddin@kordit.com>
2017-01-11 16:31:00 +0800

    3.8.6 - Updated commit.sql and rollback.sql for POCOR-3672

--- sql/Version/3.8.6/commit.sql
+++ sql/Version/3.8.6/commit.sql
@@ -1,3 +1,37 @@
+-- POCOR-3672
+-- system_patches
+INSERT INTO `system_patches` (`issue`, `created`) VALUES ('POCOR-3672', NOW());
+
+-- security_user_logins
+ALTER TABLE `security_user_logins`
+ADD COLUMN `session_id` VARCHAR(45) NULL AFTER `login_date_time`,
+ADD COLUMN `ip_address` VARCHAR(45) NULL AFTER `session_id`;
+
+-- single_logout
+CREATE TABLE `single_logout` (
+  `username` VARCHAR(256) NOT NULL,
+  `url` VARCHAR(256) NOT NULL,
+  `session_id` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`username`, `url`, `session_id`),
+  INDEX `username` (`username`),
+  INDEX `url` (`url`),
+  INDEX `session_id` (`session_id`));
+
+-- config_product_list
+CREATE TABLE `z_3672_config_product_lists` LIKE `config_product_lists`;
+
+INSERT INTO `z_3672_config_product_lists`
+SELECT * FROM `config_product_lists` WHERE `deletable` = 0;
+
+ALTER TABLE `config_product_lists`
+ADD COLUMN `auto_login_url` TEXT NULL AFTER `url`,
+ADD COLUMN `auto_logout_url` TEXT NULL AFTER `auto_login_url`;
+
+UPDATE `config_product_lists`
+SET `url` = TRIM(TRAILING '/' FROM `url`),  `auto_login_url` = CONCAT(TRIM(TRAILING '/' FROM `url`), '/Login'), `auto_logout_url` = TRIM(TRAILING '/' FROM `url`)
+WHERE `deletable` = 0;
+
+
 -- POCOR-2828
 -- system_patches
 INSERT INTO `system_patches` (`issue`, `created`) VALUES ('POCOR-2828', NOW());
