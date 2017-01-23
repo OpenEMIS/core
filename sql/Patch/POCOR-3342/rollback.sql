1ce057d4693a7a182c016e0d5c87a9ae388de92f
Martinnus Budiarto <mbudiarto@kordit.com>
2017-01-23 13:05:48 +0800

    POCOR-3342-DEV - list out students and add results

--- sql/Patch/POCOR-3342/rollback.sql
+++ sql/Patch/POCOR-3342/rollback.sql
@@ -1,3 +1,11 @@
+-- competency
+DROP TABLE IF EXISTS `competency_grading_types`;
+DROP TABLE IF EXISTS `competency_grading_options`;
+DROP TABLE IF EXISTS `competency_templates`;
+DROP TABLE IF EXISTS `competency_items`;
+DROP TABLE IF EXISTS `competency_criterias`;
+DROP TABLE IF EXISTS `competency_periods`;
+DROP TABLE IF EXISTS `competency_criteria_results`;
 
 -- system_patches
 DELETE FROM `system_patches` WHERE 'issue' = 'POCOR-3342';
