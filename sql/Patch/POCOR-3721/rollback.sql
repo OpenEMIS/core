377aaa363be7b52b5b73c60fb3ae7bc9f4c47976
Thed Kuok Shiun <kthed@kordit.com>
2017-01-26 10:22:41 +0800

    POCOR-3721 - Add simple workflow to Staff Licenses.

--- sql/Patch/POCOR-3721/rollback.sql
+++ sql/Patch/POCOR-3721/rollback.sql
@@ -4,5 +4,20 @@ DROP TABLE IF EXISTS `license_classifications`;
 -- staff_licenses_classifications
 DROP TABLE IF EXISTS `staff_licenses_classifications`;
 
+-- delete pre-insert workflow
+DELETE FROM `workflow_models` WHERE `id` = 11;
+
+DELETE FROM `workflows` WHERE NOT EXISTS (SELECT 1 FROM `workflow_models` WHERE `workflow_models`.`id` = `workflows`.`workflow_model_id`);
+DELETE FROM `workflows_filters` WHERE NOT EXISTS (SELECT 1 FROM `workflows` WHERE `workflows`.`id` = `workflows_filters`.`workflow_id`);
+DELETE FROM `workflow_steps` WHERE NOT EXISTS (SELECT 1 FROM `workflows` WHERE `workflows`.`id` = `workflow_steps`.`workflow_id`);
+DELETE FROM `workflow_actions` WHERE NOT EXISTS (SELECT 1 FROM `workflow_steps` WHERE `workflow_steps`.`id` = `workflow_actions`.`workflow_step_id`);
+
+DELETE FROM `workflow_statuses` WHERE NOT EXISTS (SELECT 1 FROM `workflow_models` WHERE `workflow_models`.`id` = `workflow_statuses`.`workflow_model_id`);
+DELETE FROM `workflow_statuses_steps` WHERE NOT EXISTS (SELECT 1 FROM `workflow_statuses` WHERE `workflow_statuses`.`id` = `workflow_statuses_steps`.`workflow_status_id`);
+DELETE FROM `workflow_transitions` WHERE NOT EXISTS (SELECT 1 FROM `workflow_models` WHERE `workflow_models`.`id` = `workflow_transitions`.`workflow_model_id`);
+
+-- staff_licenses
+RENAME TABLE `z_3721_staff_licenses` TO `staff_licenses`;
+
 -- system_patches
 DELETE FROM `system_patches` WHERE `issue`='POCOR-3721';
