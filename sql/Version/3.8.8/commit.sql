8effe5e51bc6c4ccbae93598a7bce68072ea568b
Steven Hasanuddin <shasanuddin@kordit.com>
2017-01-26 11:17:10 +0800

    3.8.8 - Fixed master.sql for POCOR-3737

--- sql/Version/3.8.8/commit.sql
+++ sql/Version/3.8.8/commit.sql
@@ -42,7 +42,13 @@ INSERT INTO `system_patches` (`issue`, `created`) VALUES ('POCOR-3737', NOW());
 -- clean up of unused _edit
 CREATE TABLE `z_3737_security_role_functions` LIKE `security_role_functions`;
 
-INSERT INTO `z_3737_security_role_functions`;
+INSERT INTO `z_3737_security_role_functions`
+SELECT * FROM `security_role_functions` WHERE `security_function_id` = 1012;
+
+UPDATE `security_role_functions` SET `_edit`= 0 WHERE `security_function_id` = 1012;
+
+-- security_functions
+UPDATE `security_functions` SET `_edit`= NULL WHERE `id`=1012;
 
 
 -- POCOR-3748
