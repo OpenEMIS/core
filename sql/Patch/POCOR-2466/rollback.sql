97814a530cfca05a146b7bf48b33d81c29ce9d97
Harry Suhendra <hsuhendra@kordit.com>
2017-02-06 16:33:28 +0800

    POCOR-2466 - Update the dbPatches.

--- sql/Patch/POCOR-2466/rollback.sql
+++ sql/Patch/POCOR-2466/rollback.sql
@@ -37,13 +37,16 @@ CREATE TABLE `sms_responses` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- Drop alerts table
+DROP TABLE alerts;
+
 -- security_user table
 ALTER TABLE `security_users` DROP `email` ;
 
 -- Security functions (permission)
 INSERT INTO `security_functions` (`id`, `name`, `controller`, `module`, `category`, `parent_id`, `_view`, `_edit`, `_add`, `_delete`, `_execute`, `order`, `visible`, `description`, `modified_user_id`, `modified`, `created_user_id`, `created`)
 VALUES ('5029', 'Questions', 'Alerts', 'Administration', 'Communications', '5000', 'Questions.index|Questions.view', 'Questions.edit', 'Questions.add', 'Questions.remove', NULL, '5034', '1', NULL, NULL, NULL, '1', '2015-08-04 02:41:01'),
-        ('5030', 'Responses', 'Alerts', 'Administration', 'Communications', '5000', 'Responses.index', NULL, NULL, NULL, NULL, '5035', '1', NULL, NULL, NULL, '1', '2015-08-04 02:41:02')
+        ('5030', 'Responses', 'Alerts', 'Administration', 'Communications', '5000', 'Responses.index', NULL, NULL, NULL, NULL, '5035', '1', NULL, NULL, NULL, '1', '2015-08-04 02:41:02');
 
 UPDATE `security_functions` SET `_view` = 'Logs.index' WHERE `id` = 5031;
 UPDATE `security_functions` SET `_delete` = NULL WHERE `id` = 5031;
@@ -53,13 +56,12 @@ UPDATE `security_functions` SET `order` = `order` - 2 WHERE `order` BETWEEN 5033
 
 -- alert_roles table
 RENAME TABLE `alerts_roles` TO `alert_roles`;
-ALTER TABLE `alerts_roles` CHANGE `alert_rule_id` `alert_id` INT(11) NOT NULL COMMENT 'links to alerts.id';
+ALTER TABLE `alert_roles` CHANGE `alert_rule_id` `alert_id` INT(11) NOT NULL COMMENT 'links to alerts.id';
 
 ALTER TABLE `alert_roles`
     DROP PRIMARY KEY,
     ADD PRIMARY KEY(`id`),
-    DROP INDEX `alert_rule_id`,
-    DROP INDEX `security_role_id`;
+    DROP INDEX `alert_rule_id`;
 
 -- alert_logs
 ALTER TABLE `alert_logs` ADD `type` VARCHAR(20) NOT NULL AFTER `destination`;
