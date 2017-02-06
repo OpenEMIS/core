1e4e59f03c3a7c26946dcdc5b76759a7d8150dae
Steven Hasanuddin <shasanuddin@kordit.com>
2017-02-06 16:10:55 +0800

    3.9.1 - Updated master commit.sql and rollback.sql for POCOR-3459

--- sql/Version/3.9.1/commit.sql
+++ sql/Version/3.9.1/commit.sql
@@ -1,3 +1,16 @@
+-- POCOR-3459
+-- system_patches
+INSERT INTO `system_patches` (`issue`, `created`) VALUES ('POCOR-3459', NOW());
+
+-- institutions
+ALTER TABLE `institutions`
+MODIFY COLUMN `classification` INT(1) NOT NULL DEFAULT '1' COMMENT '1 -> Academic Institution, 2 -> Non-academic institution';
+
+UPDATE `institutions`
+SET `classification` = 2
+WHERE `classification` = 0;
+
+
 -- POCOR-3342
 -- system_patches
 INSERT INTO `system_patches` (`issue`, `created`) VALUES ('POCOR-3342', NOW());
