e348d5e9f4501cc596fb3dd4f77974ea1ae27e65
Livvi Sim <lsim@kordit.com>
2017-02-08 14:10:34 +0800

    POCOR-3764 - Change code that references dropout to withdraw

--- src/Model/Traits/MessagesTrait.php
+++ src/Model/Traits/MessagesTrait.php
@@ -430,7 +430,7 @@ trait MessagesTrait {
         'TransferRequests' => [
             'request' => 'Transfer request has been submitted successfully.',
             'enrolled' => 'This student has already been enrolled in an institution.',
-            'hasDropoutApplication' => 'There is a pending dropout application for this student at the moment, please reject the dropout application before making another request.',
+            'hasWithdrawApplication' => 'There is a pending withdraw application for this student at the moment, please reject the withdraw application before making another request.',
             'invalidEndDate' => 'Transfer is not allowed as the student has already completed the grade.'
         ],
         'TransferApprovals' => [
@@ -472,7 +472,7 @@ trait MessagesTrait {
             'savingPromotionError' => 'The student record was not updated succesfully',
             'noNextAcademicPeriod' => 'There is no next academic period for the promotion.',
             'pendingTransfer' => 'There is a pending transfer request for this student.',
-            'pendingDropout' => 'There is a pending dropout request for this student.',
+            'pendingWithdraw' => 'There is a pending withdraw request for this student.',
         ],
         'StudentTransfer' => [
             'noGrades' => 'No Available Grades',
@@ -517,14 +517,14 @@ trait MessagesTrait {
             'approve' => 'Student admission has been approved successfully.',
             'reject' => 'Student admission has been rejected successfully.'
         ],
-        'DropoutRequests' => [
-            'request' => 'Dropout request hsa been submitted successfully.',
+        'WithdrawRequests' => [
+            'request' => 'Withdraw request has been submitted successfully.',
             'notEligible' =>  'This student is not eligible for this action. Please reject this request.'
         ],
-        'StudentDropout' => [
+        'StudentWithdraw' => [
             'exists' => 'Student has already dropped out from the school.',
-            'approve' => 'Dropout request has been approved successfully.',
-            'reject' => 'Dropout request has been rejected successfully.',
+            'approve' => 'Withdraw request has been approved successfully.',
+            'reject' => 'Withdraw request has been rejected successfully.',
             'hasTransferApplication' => 'There is a pending transfer application for this student at the moment, please remove the transfer application before making another request.'
         ],
         'Import' => [
@@ -787,7 +787,7 @@ trait MessagesTrait {
                     'ruleCompareDateReverse' => 'End Date should not be earlier than Start Date'
                 ],
                 'student_id' => [
-                    'ruleNoNewDropoutRequestInGradeAndInstitution' => 'There is a pending dropout application for this student at the moment, please reject the dropout application before making another request.',
+                    'ruleNoNewWithdrawRequestInGradeAndInstitution' => 'There is a pending withdraw application for this student at the moment, please reject the withdraw application before making another request.',
                     'ruleStudentNotEnrolledInAnyInstitutionAndSameEducationSystem' => [
                         'inTargetSchool' => 'Student is already enrolled in this school.',
                         'inAnotherSchool' => 'Student is already enrolled in another school.',
@@ -880,12 +880,12 @@ trait MessagesTrait {
                     'ruleCompareDateReverse' => 'New Start Date should not be earlier than or same as Start Date'
                 ]
             ],
-            'DropoutRequests' => [
+            'WithdrawRequests' => [
                 'effective_date' => [
                     'ruleDateAfterEnrollment' => 'Effective Date cannot be earlier than the Enrollment Date'
                 ]
             ],
-            'StudentDropout' => [
+            'StudentWithdraw' => [
                 'effective_date' => [
                     'ruleDateAfterEnrollment' => 'Effective Date cannot be earlier than the Enrollment Date'
                 ]
