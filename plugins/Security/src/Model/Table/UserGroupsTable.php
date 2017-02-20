c4b4153867949de344b918c8cc960808a9bd41b2
Martinnus Budiarto <mbudiarto@kordit.com>
2017-02-20 14:41:08 +0800

    POCOR-3746 - fix not updated pointer of HTML link.

--- plugins/Security/src/Model/Table/UserGroupsTable.php
+++ plugins/Security/src/Model/Table/UserGroupsTable.php
@@ -380,7 +380,8 @@ class UserGroupsTable extends AppTable {
 					$rowData[] = $event->subject()->Html->link($obj->openemis_no , [
 						'plugin' => 'Directory',
 						'controller' => 'Directories',
-						'action' => 'view',
+						'action' => 'Directories',
+						'view',
 						$this->paramsEncode(['id' => $obj->id])
 					]);
 					$rowData[] = $obj->name;
