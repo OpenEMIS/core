2e4388cb52bad3b8858336d63cdab36bff804663
Steven Hasanuddin <shasanuddin@kordit.com>
2017-02-20 13:33:21 +0800

    3.9.4 - Updated master commit.sql and rollback.sql

--- sql/Version/3.9.4/rollback.sql
+++ sql/Version/3.9.4/rollback.sql
@@ -1,3 +1,15 @@
+-- POCOR-3644
+-- security_functions
+DELETE FROM `security_functions` WHERE `id` IN (2030);
+
+UPDATE `security_functions`
+SET `order` = `order` - 1
+WHERE `order` >= 2018 AND `order` < 3000;
+
+-- system_patches
+DELETE FROM `system_patches` WHERE `issue` = 'POCOR-3644';
+
+
 -- POCOR-3797
 -- restore competency_criterias
 DROP TABLE IF EXISTS `competency_criterias`;
