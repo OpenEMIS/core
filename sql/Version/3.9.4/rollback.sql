0fd9dcfac9b3c5c02d378d21061dc5fff06b187f
Steven Hasanuddin <shasanuddin@kordit.com>
2017-02-21 16:39:04 +0800

    3.9.4 - Updated master commit.sql and rollback.sql for POCOR-2498

--- sql/Version/3.9.4/rollback.sql
+++ sql/Version/3.9.4/rollback.sql
@@ -1,3 +1,20 @@
+-- POCOR-2498
+-- code here
+DELETE FROM `security_functions` WHERE `id` = 1055;
+DELETE FROM `security_functions` WHERE `id` = 2032;
+DELETE FROM `security_functions` WHERE `id` = 5066;
+ALTER TABLE `student_behaviours` DROP `academic_period_id`;
+ALTER TABLE `student_behaviour_categories` DROP `behaviour_classification_id`;
+DROP TABLE student_indexes_criterias;
+DROP TABLE institution_student_indexes;
+DROP TABLE behaviour_classifications;
+DROP TABLE indexes_criterias;
+DROP TABLE indexes;
+
+-- db_patches
+DELETE FROM `system_patches` WHERE `issue`='POCOR-2498';
+
+
 -- POCOR-3644
 -- security_functions
 DELETE FROM `security_functions` WHERE `id` IN (2031;
