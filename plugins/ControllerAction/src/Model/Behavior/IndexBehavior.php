9f126a9b097168eedfe1c59c6141643244a0e59e
Livvi Sim <lsim@kordit.com>
2017-02-22 10:28:26 +0800

    POCOR-3572 - Move pagination logic from SearchBehavior to IndexBehavior

--- plugins/ControllerAction/src/Model/Behavior/IndexBehavior.php
+++ plugins/ControllerAction/src/Model/Behavior/IndexBehavior.php
@@ -66,6 +66,12 @@ class IndexBehavior extends Behavior {
 		$data = [];
 		if ($hasQuery) {
 			if ($extra['pagination']) {
+				$alias = $model->registryAlias();
+				$session = $model->request->session();
+				$pageOptions = $extra['config']['pageOptions'];
+				$limit = $session->check($alias.'.search.limit') ? $session->read($alias.'.search.limit') : key($pageOptions);
+				$extra['options']['limit'] = $pageOptions[$limit];
+
 				try {
 					$data = $model->Paginator->paginate($query, $extra['options']);
 				} catch (NotFoundException $e) {
