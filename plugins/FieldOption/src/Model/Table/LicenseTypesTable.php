facfdd4956d7c63f42c9ea980e97abff1d15c6d3
Thed Kuok Shiun <kthed@kordit.com>
2017-03-09 15:00:13 +0800

    POCOR-3721 - 1) Change License become not mandatory 2) Not allow to delete non-editable License Types 3) Fix validation unique of License Type name 4) Filter out License Classification which is not visible 5) Add License Type to workbench.

--- plugins/FieldOption/src/Model/Table/LicenseTypesTable.php
+++ plugins/FieldOption/src/Model/Table/LicenseTypesTable.php
@@ -1,8 +1,11 @@
 <?php
 namespace FieldOption\Model\Table;
 
-use App\Model\Table\AppTable;
+use ArrayObject;
+use Cake\ORM\Query;
+use Cake\ORM\Entity;
 use Cake\Validation\Validator;
+use Cake\Event\Event;
 use App\Model\Table\ControllerActionTable;
 
 class LicenseTypesTable extends ControllerActionTable
@@ -16,5 +19,14 @@ class LicenseTypesTable extends ControllerActionTable
         $this->hasMany('Licenses', ['className' => 'Staff.Licenses', 'foreignKey' => 'license_type_id']);
 
         $this->addBehavior('FieldOption.FieldOption');
+    }
+
+    public function deleteOnInitialize(Event $event, Entity $entity, Query $query, ArrayObject $extra)
+	{
+		if ($entity->editable == 0) {
+			$event->stopPropagation();
+			$this->Alert->warning('general.delete.restrictDelete');
+			return $this->controller->redirect($this->url('index'));
+		}
     }
 }
