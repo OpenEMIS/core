588e3c6a22ed43b0944f13a3c43455d83e89a817
Steven Hasanuddin <shasanuddin@kordit.com>
2017-03-14 15:09:08 +0800

    3.9.6 - Updated master commit.sql and rollback.sql for POCOR-3849

--- sql/Version/3.9.6/rollback.sql
+++ sql/Version/3.9.6/rollback.sql
@@ -1,3 +1,12 @@
+-- POCOR-3849
+-- room_types
+DROP TABLE IF EXISTS `room_types`;
+RENAME TABLE `z_3849_room_types` TO `room_types`;
+
+-- db_patches
+DELETE FROM `system_patches` WHERE `issue`='POCOR-3849';
+
+
 -- POCOR-3857
 -- security_functions
 DELETE FROM `config_items` WHERE `id` IN (126, 127);
