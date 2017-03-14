3bad902777514687dcb9293277735035d4df5076
Chiang Hao Long <hchiang@kordit.com>
2017-03-14 15:02:11 +0800

    POCOR-3884 - Added logic for webhooks

---
+++ plugins/Webhook/webroot/js/webhook.js
@@ -0,0 +1,21 @@
+var Webhook = {
+    // Use only for stateful applications
+    triggerEvent: function (url, eventKey) {
+        var urlWithEvent = url.replace(/\/$/, "") + '/' + eventKey;
+        $.ajax({
+            url: urlWithEvent,
+            type: "GET",
+            success: function(response){
+                for (var i = 0; i < response.data.length ; i++) {
+                    var data = response.data[i];
+                    var responseUrl = data.url;
+                    var method = data.method;
+                    $.ajax({
+                        url: responseUrl,
+                        type: method
+                    });
+                }
+            }
+        });
+    }
+}
