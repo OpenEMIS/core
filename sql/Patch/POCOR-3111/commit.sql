8d8e9ac9a233790ca5789158f35ec565dba33e47
Martinnus Budiarto <mbudiarto@kordit.com>
2017-03-14 17:01:04 +0800

    POCOR-3111-dev - adjust table field name

--- sql/Patch/POCOR-3111/commit.sql
+++ sql/Patch/POCOR-3111/commit.sql
@@ -5,15 +5,15 @@ INSERT INTO `system_patches` (`issue`, `created`) VALUES ('POCOR-3111', NOW());
 DROP TABLE IF EXISTS `user_attachments_roles`;
 CREATE TABLE IF NOT EXISTS `user_attachments_roles` (
   `id` char(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `attachment_id` int(11) NOT NULL COMMENT 'links to user_attachments.id',
+  `user_attachment_id` int(11) NOT NULL COMMENT 'links to user_attachments.id',
   `security_role_id` int(11) NOT NULL COMMENT 'links to security_roles.id'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='This table contains the list of Attachments for specific Security Roles';
 
 ALTER TABLE `user_attachments_roles`
-  ADD PRIMARY KEY (`attachment_id`,`security_role_id`),
+  ADD PRIMARY KEY (`user_attachment_id`,`security_role_id`),
   ADD UNIQUE KEY `id` (`id`),
   ADD KEY `id_index` (`id`),
-  ADD KEY `attachment_id` (`attachment_id`),
+  ADD KEY `user_attachment_id` (`user_attachment_id`),
   ADD KEY `security_role_id` (`security_role_id`);
 
 -- labels
