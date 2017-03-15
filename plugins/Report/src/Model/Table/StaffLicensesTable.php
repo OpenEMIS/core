e22747d0cfa8a5e42e2b98b8efd1d2e5b1249b4f
Livvi Sim <lsim@kordit.com>
2017-03-15 11:27:08 +0800

    POCOR-3723 - Add staff licenses report

---
+++ plugins/Report/src/Model/Table/StaffLicensesTable.php
@@ -0,0 +1,90 @@
+<?php
+namespace Report\Model\Table;
+
+use ArrayObject;
+use Cake\ORM\Entity;
+use Cake\ORM\Query;
+use Cake\Event\Event;
+use Cake\Network\Request;
+use App\Model\Table\AppTable;
+
+class StaffLicensesTable extends AppTable  {
+    public function initialize(array $config)
+    {
+        $this->table('staff_licenses');
+        parent::initialize($config);
+        $this->belongsTo('Users', ['className' => 'User.Users', 'foreignKey' => 'staff_id']);
+        $this->belongsTo('LicenseTypes', ['className' => 'FieldOption.LicenseTypes']);
+        $this->belongsToMany('LicenseClassifications', [
+            'className' => 'FieldOption.LicenseClassifications',
+            'joinTable' => 'staff_licenses_classifications',
+            'foreignKey' => 'staff_license_id',
+            'targetForeignKey' => 'license_classification_id',
+            'through' => 'Staff.StaffLicensesClassifications'
+        ]);
+        $this->belongsTo('WorkflowSteps', ['className' => 'Workflow.WorkflowSteps', 'foreignKey' => 'status_id']);
+
+        $this->addBehavior('Excel', [
+            'excludes' => ['status_id', 'assignee_id', 'staff_id', 'license_type_id'],
+            'pages' => false,
+            'orientation' => 'landscape'
+        ]);
+        $this->addBehavior('Report.ReportList');
+    }
+
+    public function onExcelBeforeQuery(Event $event, ArrayObject $settings, Query $query)
+    {
+        $requestData = json_decode($settings['process']['params']);
+        $selectedStatus = !empty($requestData->status) ? $requestData->status : null;
+
+        $query
+            ->select([
+                'openemis_no' => 'Users.openemis_no',
+                'classification' => 'LicenseClassifications.name'
+            ])
+            ->contain(['Users'])
+            ->matching('WorkflowSteps.WorkflowStatuses')
+            ->matching('LicenseClassifications')
+            ->order([$this->aliasField('staff_id')]);
+
+        if (!is_null($selectedStatus)) {
+            $query->where(['WorkflowStatuses.id' => $selectedStatus]);
+        }
+    }
+
+    public function onExcelUpdateFields(Event $event, ArrayObject $settings, ArrayObject $fields)
+    {
+        $newArray = [];
+
+        $newArray[] = [
+            'key' => 'Users.openemis_no',
+            'field' => 'openemis_no',
+            'type' => 'string',
+            'label' => '',
+        ];
+
+        $newArray[] = [
+            'key' => 'StaffLicenses.staff_id',
+            'field' => 'staff_id',
+            'type' => 'integer',
+            'label' => '',
+        ];
+
+        $newArray[] = [
+            'key' => 'LicenseClassifications.name',
+            'field' => 'classification',
+            'type' => 'string',
+            'label' => _('Classification'),
+        ];
+
+        $newArray[] = [
+            'key' => 'StaffLicenses.license_type_id',
+            'field' => 'license_type_id',
+            'type' => 'integer',
+            'label' => '',
+        ];
+
+        $newFields = array_merge($newArray, $fields->getArrayCopy());
+        $fields->exchangeArray($newFields);
+    }
+}
