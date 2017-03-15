0398e340dbcbd09280f9ed6c950f2d395f28662c
Steven Hasanuddin <shasanuddin@kordit.com>
2017-03-15 11:32:56 +0800

    3.9.6 - Updated master commit.sql and rollback.sql

--- sql/Version/3.9.6/rollback.sql
+++ sql/Version/3.9.6/rollback.sql
@@ -1,3 +1,15 @@
+-- POCOR-3680
+-- security_group_users
+DROP TABLE IF EXISTS `security_group_users`;
+RENAME TABLE `z_3680_security_group_users` TO `security_group_users`;
+
+DROP TABLE IF EXISTS `institution_staff`;
+RENAME TABLE `z_3680_institution_staff` TO `institution_staff`;
+
+-- db_patches
+DELETE FROM `system_patches` WHERE `issue` = 'POCOR-3680';
+
+
 -- POCOR-3849
 -- room_types
 DROP TABLE IF EXISTS `room_types`;
