e3ad5d77124ce443363e4bd13e6a91ea6bf4e5bc
Martinnus Budiarto <mbudiarto@kordit.com>
2017-03-16 15:36:06 +0800

    POCOR-3886 - add attachemt into staff ocefoedoddd

--- plugins/Staff/src/Model/Table/EmploymentsTable.php
+++ plugins/Staff/src/Model/Table/EmploymentsTable.php
@@ -2,6 +2,7 @@
 namespace Staff\Model\Table;
 
 use ArrayObject;
+use Cake\ORM\Entity;
 use Cake\Event\Event;
 use App\Model\Table\ControllerActionTable;
 
@@ -11,14 +12,24 @@ class EmploymentsTable extends ControllerActionTable {
 		parent::initialize($config);
 
 		$this->belongsTo('Users', ['className' => 'User.Users', 'foreignKey' => 'staff_id']);
-		$this->belongsTo('EmploymentTypes', ['className' => 'FieldOption.EmploymentTypes']);
+		$this->belongsTo('EmploymentTypes',` ['className' => 'FieldOption.EmploymentTypes']);
 
 		$this->behaviors()->get('ControllerAction')->config('actions.search', false);
+		$this->addBehavior('ControllerAction.FileUpload', [
+			// 'name' => 'file_name',
+			// 'content' => 'file_content',
+			'size' => '2MB',
+			'contentEditable' => true,
+			'allowable_file_types' => 'all',
+			'useDefaultName' => true
+		]);
 	}
 
 	public function beforeAction(Event $event, ArrayObject $extra) {
 		$this->field('employment_type_id', ['type' => 'select', 'before' => 'employment_date']);
-		$this->setupTabElements();
+
+		$this->field('file_name', ['visible' => false]);
+        $this->setupTabElements();
 	}
 
 	private function setupTabElements() {
@@ -27,4 +38,18 @@ class EmploymentsTable extends ControllerActionTable {
 		$this->controller->set('tabElements', $tabElements);
 		$this->controller->set('selectedAction', $this->alias());
 	}
+
+    public function onGetEmploymentTypeId(Event $event, Entity $entity) 
+    {
+        if ($this->action == 'index') {
+            $url = $event->subject()->HtmlField->link($entity->employment_type->name, [
+                        'plugin' => $this->controller->plugin,
+                        'controller' => $this->controller->name,
+                        'action' => 'StaffEmployments',
+                        'view',
+                        $this->paramsEncode(['id' => $entity->id])
+                    ]);
+            return $url;
+        }
+    }
 }
