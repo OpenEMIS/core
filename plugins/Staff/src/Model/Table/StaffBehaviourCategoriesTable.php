6687a0646d88b8c42826276152d76e986258a912
Livvi Sim <lsim@kordit.com>
2017-03-17 18:21:35 +0800

    POCOR-3731 - Move field option to Others category and add associations for BehaviourClassification

--- plugins/Staff/src/Model/Table/StaffBehaviourCategoriesTable.php
+++ plugins/Staff/src/Model/Table/StaffBehaviourCategoriesTable.php
@@ -1,6 +1,11 @@
 <?php
 namespace Staff\Model\Table;
 
+use ArrayObject;
+use Cake\Event\Event;
+use Cake\Network\Request;
+use Cake\ORM\Entity;
+use Cake\ORM\Query;
 use App\Model\Table\ControllerActionTable;
 
 class StaffBehaviourCategoriesTable extends ControllerActionTable
@@ -10,8 +15,19 @@ class StaffBehaviourCategoriesTable extends ControllerActionTable
         $this->table('staff_behaviour_categories');
         parent::initialize($config);
 
+        $this->belongsTo('BehaviourClassifications', ['className' => 'Student.BehaviourClassifications', 'foreignKey' => 'behaviour_classification_id']);
         $this->hasMany('StaffBehaviours', ['className' => 'Staff.StaffBehaviours', 'foreignKey' => 'staff_behaviour_category_id']);
 
         $this->addBehavior('FieldOption.FieldOption');
+    }
+
+    public function beforeAction(Event $event, ArrayObject $extra)
+    {
+        $this->field('behaviour_classification_id', ['after' => 'name', 'type' => 'select']);
+    }
+
+    public function indexBeforeAction(Event $event, ArrayObject $extra)
+    {
+        $this->field('behaviour_classification_id', ['after' => 'name']);
     }
 }
