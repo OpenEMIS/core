3cab29048a48078c97d0c213fedfc2145076f39e
Livvi Sim <lsim@kordit.com>
2017-03-21 10:35:14 +0800

    POCOR-3733 - Add ProfessionalDevelopmentTable and TrainingCoursesTable

---
+++ plugins/Report/src/Model/Table/ProfessionalDevelopmentTable.php
@@ -0,0 +1,69 @@
+<?php
+namespace Report\Model\Table;
+
+use ArrayObject;
+use Cake\ORM\Entity;
+use Cake\ORM\Query;
+use Cake\Event\Event;
+use Cake\Network\Request;
+use App\Model\Table\AppTable;
+
+class ProfessionalDevelopmentTable extends AppTable {
+    public function initialize(array $config)
+    {
+        $this->table('training_courses');
+        parent::initialize($config);
+
+        $this->belongsTo('Statuses', ['className' => 'Workflow.WorkflowSteps', 'foreignKey' => 'status_id']);
+        $this->belongsTo('TrainingFieldStudies', ['className' => 'Training.TrainingFieldStudies', 'foreignKey' => 'training_field_of_study_id']);
+        $this->belongsTo('TrainingCourseTypes', ['className' => 'Training.TrainingCourseTypes', 'foreignKey' => 'training_course_type_id']);
+        $this->belongsTo('TrainingModeDeliveries', ['className' => 'Training.TrainingModeDeliveries', 'foreignKey' => 'training_mode_of_delivery_id']);
+        $this->belongsTo('TrainingRequirements', ['className' => 'Training.TrainingRequirements', 'foreignKey' => 'training_requirement_id']);
+        $this->belongsTo('TrainingLevels', ['className' => 'Training.TrainingLevels', 'foreignKey' => 'training_level_id']);
+        $this->belongsTo('Assignees', ['className' => 'User.Users']);
+
+        $this->addBehavior('Excel', ['pages' => false]);
+        $this->addBehavior('Report.ReportList');
+    }
+
+    public function beforeAction(Event $event)
+    {
+        $this->fields = [];
+        $this->ControllerAction->field('feature', ['select' => false]);
+        $this->ControllerAction->field('status');
+        $this->ControllerAction->field('format');
+    }
+
+    public function onUpdateFieldFeature(Event $event, array $attr, $action, Request $request)
+    {
+        if ($action == 'add') {
+            $attr['options'] = $this->controller->getFeatureOptions($this->alias());
+            $attr['onChangeReload'] = true;
+            if (!isset($this->request->data[$this->alias()]['feature'])) {
+                $option = $attr['options'];
+                reset($option);
+                $this->request->data[$this->alias()]['feature'] = key($option);
+            }
+            return $attr;
+        }
+    }
+
+    public function onUpdateFieldStatus(Event $event, array $attr, $action, Request $request)
+    {
+        if ($action == 'add') {
+            if (isset($this->request->data[$this->alias()]['feature'])) {
+                $feature = $this->request->data[$this->alias()]['feature'];
+
+                list($modelplugin, $modelAlias) = explode('.', $feature);
+                $workflowStatuses = $this->Workflow->getWorkflowStatuses('Training.' . $modelAlias);
+                if (empty($workflowStatuses)) {
+                    $this->Alert->warning('Reports.noWorkflowStatus');
+                }
+
+                $attr['type'] = 'select';
+                $attr['options'] = $workflowStatuses;
+                return $attr;
+            }
+        }
+    }
+}
