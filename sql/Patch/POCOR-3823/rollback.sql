95a516b9491b060fe7172345f5bf51c3b9237235
Martinnus Budiarto <mbudiarto@kordit.com>
2017-03-24 09:22:57 +0800

    POCOR-3823-dev - adjustment after the demo, including: * implement optional for nationality, identity type * validation if nationality / identity type / identity number is not empty * rebuild nationality table on reference tab to show identity type that is tie to the nationality * implement isOptional to source type FieldOption so it able to support (optional)

--- sql/Patch/POCOR-3823/rollback.sql
+++ sql/Patch/POCOR-3823/rollback.sql
@@ -1,6 +1,6 @@
 -- import_mapping
 INSERT INTO `import_mapping` (`id`, `model`, `column_name`, `description`, `order`, `foreign_key`, `lookup_plugin`, `lookup_model`, `lookup_column`) 
-VALUES (111, 'User.Users', 'identity_number', '', 16, 0, NULL, NULL, NULL);
+VALUES(80, 'User.Users', 'Identity', 'Number', 14, 4, 'User', 'Identities', 'FieldOption.IdentityTypes');
 
 DELETE FROM `import_mapping` 
 WHERE `model` = 'User.Users'
--- src/Model/Traits/MessagesTrait.php
+++ src/Model/Traits/MessagesTrait.php
@@ -564,8 +564,11 @@ trait MessagesTrait {
             'no_answers' => 'No record were found in the file imported',
             'institution_network_connectivity_id' => 'code',
             'exam_centre_dont_match' => 'Examination and centre combination cannot be found.',
-            'identity_type_doesnt_match' => 'Identity type selected does not match the one which set on Nationality field options.',
-            'identity_number_exist' => 'Identity Number for selected Identity Type already exists in the system.'
+            'identity_type_doesnt_match' => 'Identity type selected must be %s.',
+            'identity_number_exist' => 'Identity Number for %s already exists.',
+            'identity_type_required' => 'Identity Type cant be empty if Identity Number is specified.',
+            'identity_number_required' => 'Identity Number cant be empty if Identity Type is specified.',
+            'identity_number_invalid_pattern' => 'Invalid Identity Number pattern.'
         ],
         'TrainingSessions' => [
             'trainer_type' => 'Type',
