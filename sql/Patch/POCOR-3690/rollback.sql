e7b4a9f181fee4d80620221b87ab1647216e7a17
Livvi Sim <lsim@kordit.com>
2017-03-29 17:31:59 +0800

    POCOR-3690 - Add patch for examinations database

--- sql/Patch/POCOR-3690/rollback.sql
+++ sql/Patch/POCOR-3690/rollback.sql
@@ -9,6 +9,25 @@ RENAME TABLE `z_3690_examination_centres` TO `examination_centres`;
 DROP TABLE IF EXISTS `examination_centre_special_needs`;
 RENAME TABLE `z_3690_examination_centre_special_needs` TO `examination_centre_special_needs`;
 
+-- examination_centres_examinations_institutions
+DROP TABLE IF EXISTS `examination_centres_examinations_institutions`;
+RENAME TABLE `z_3690_examination_centres_institutions` TO `examination_centres_institutions`;
+
+-- examination_centres_examinations_invigilators
+DROP TABLE IF EXISTS `examination_centres_examinations_invigilators`;
+RENAME TABLE `z_3690_examination_centres_invigilators` TO `examination_centres_invigilators`;
+
+-- examination_centres_examinations_subjects
+DROP TABLE IF EXISTS `examination_centres_examinations_subjects`;
+RENAME TABLE `z_3690_examination_centre_subjects` TO `examination_centre_subjects`;
+
+-- examination_centres_examinations_subjects_students
+DROP TABLE IF EXISTS `examination_centres_examinations_subjects_students`;
+
+-- examination_centres_examinations_students
+DROP TABLE IF EXISTS `examination_centres_examinations_students`;
+RENAME TABLE `z_3690_examination_centre_students` TO `examination_centre_students`;
+
 -- examination_centre_rooms_examinations
 DROP TABLE IF EXISTS `examination_centre_rooms_examinations`;
 
@@ -23,3 +42,7 @@ RENAME TABLE `z_3690_examination_centre_rooms_invigilators` TO `examination_cent
 -- examination_centre_rooms_examinations_students
 DROP TABLE IF EXISTS `examination_centre_rooms_examinations_students`;
 RENAME TABLE `z_3690_examination_centre_room_students` TO `examination_centre_room_students`;
+
+-- examination_item_results
+DROP TABLE IF EXISTS `examination_item_results`;
+RENAME TABLE `z_3690_examination_item_results` TO `examination_item_results`;
