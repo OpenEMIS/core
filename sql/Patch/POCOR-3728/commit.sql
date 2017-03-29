c4c6202111c976519094064b2720b5218d6d1618
Harry Suhendra <hsuhendra@kordit.com>
2017-03-29 15:17:30 +0800

    POCOR-3728 - Adding EmploymentPeriodAlert feature. - AlertTable adding name alphabetical sort.

--- sql/Patch/POCOR-3728/commit.sql
+++ sql/Patch/POCOR-3728/commit.sql
@@ -4,7 +4,8 @@ INSERT INTO `system_patches` (`issue`, `created`) VALUES('POCOR-3728', NOW());
 -- alerts Table
 INSERT INTO `alerts` (`name`, `process_name`, `process_id`, `modified_user_id`, `modified`, `created_user_id`, `created`)
 VALUES  ('LicenseValidity', 'LicenseValidityAlert', NULL, NULL, NULL, '1', NOW()),
-        ('StaffLeave', 'StaffLeaveAlert', NULL, NULL, NULL, '1', NOW());
+        ('StaffLeave', 'StaffLeaveAlert', NULL, NULL, NULL, '1', NOW()),
+        ('Employment', 'EmploymentAlert', NULL, NULL, NULL, '1', NOW());
 
 -- alert_rules Table
 ALTER TABLE `alert_rules` CHANGE `threshold` `threshold` VARCHAR(100) NOT NULL;
