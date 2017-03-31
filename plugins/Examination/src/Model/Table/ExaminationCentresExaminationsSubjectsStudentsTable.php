1c9a0103f4be50c9dcc462e93b419b3f9012879a
Livvi Sim <lsim@kordit.com>
2017-03-31 15:46:50 +0800

    POCOR-3690 - Add ExaminationCentresExaminationsSubjectsStudents

---
+++ plugins/Examination/src/Model/Table/ExaminationCentresExaminationsSubjectsStudentsTable.php
@@ -0,0 +1,30 @@
+<?php
+namespace Examination\Model\Table;
+
+use ArrayObject;
+use Cake\Event\Event;
+use Cake\Validation\Validator;
+use Cake\ORM\Entity;
+use App\Model\Table\ControllerActionTable;
+
+class ExaminationCentresExaminationsSubjectsStudentsTable extends ControllerActionTable {
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+        $this->belongsTo('Students', ['className' => 'Institution.StudentUser']);
+        $this->belongsTo('ExaminationItems', ['className' => 'Examination.ExaminationItems']);
+        $this->belongsTo('ExaminationCentres', ['className' => 'Examination.ExaminationCentres']);
+        $this->belongsTo('EducationSubjects', ['className' => 'Education.EducationSubjects']);
+        $this->belongsTo('Examinations', ['className' => 'Examination.Examinations']);
+        $this->belongsTo('ExaminationCentresExaminationsStudents', [
+            'className' => 'Examination.ExaminationCentresExaminationsStudents',
+            'foreignKey' => ['examination_centre_id', 'student_id', 'examination_id']
+        ]);
+        $this->belongsTo('ExaminationCentresExaminationsSubjects', [
+            'className' => 'Examination.ExaminationCentresExaminationsSubjects',
+            'foreignKey' => ['examination_centre_id', 'examination_item_id']
+        ]);
+
+        $this->addBehavior('CompositeKey');
+    }
+}
