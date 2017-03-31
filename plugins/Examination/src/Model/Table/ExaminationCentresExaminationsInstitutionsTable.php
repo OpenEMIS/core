0fcb00484e70e41f233c45b354a90c9784af04ef
Livvi Sim <lsim@kordit.com>
2017-03-31 15:38:18 +0800

    POCOR-3690 - Rename and fix associations for ExaminationCentresExaminations Institutions/Students/Subjects

--- plugins/Examination/src/Model/Table/ExaminationCentresExaminationsInstitutionsTable.php
+++ plugins/Examination/src/Model/Table/ExaminationCentresExaminationsInstitutionsTable.php
@@ -10,15 +10,17 @@ use Cake\ORM\Query;
 use Cake\ORM\Entity;
 use Cake\Utility\Security;
 
-class ExaminationCentresInstitutionsTable extends ControllerActionTable {
-
-    private $examCentreId = null;
-
+class ExaminationCentresExaminationsInstitutionsTable extends ControllerActionTable {
     public function initialize(array $config)
     {
         parent::initialize($config);
         $this->belongsTo('ExaminationCentres', ['className' => 'Examination.ExaminationCentres']);
+        $this->belongsTo('Examinations', ['className' => 'Examination.Examinations']);
         $this->belongsTo('Institutions', ['className' => 'Institution.Institutions']);
+        $this->belongsTo('ExaminationCentresExaminations', [
+            'className' => 'Examination.ExaminationCentresExaminations',
+            'foreignKey' => ['examination_centre_id', 'examination_id']
+        ]);
 
         $this->addBehavior('CompositeKey');
     }
