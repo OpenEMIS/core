cb9b090868c7e2341b080bab75076b67c7d1c76d
Chiang Hao Long <hchiang@kordit.com>
2017-03-31 15:10:24 +0800

    POCOR-2785 - Fixed area and area administrative psr2 formating

--- plugins/Area/src/Model/Table/AreaAdministrativeLevelsTable.php
+++ plugins/Area/src/Model/Table/AreaAdministrativeLevelsTable.php
@@ -12,100 +12,99 @@ use App\Model\Table\ControllerActionTable;
 
 class AreaAdministrativeLevelsTable extends ControllerActionTable
 {
-	public function initialize(array $config)
-	{
-		parent::initialize($config);
-		$this->belongsTo('Countries', ['className' => 'Area.AreaAdministratives', 'foreignKey' => 'area_administrative_id']);
-		$this->hasMany('Areas', ['className' => 'Area.AreaAdministratives', 'foreignKey' => 'area_administrative_level_id']);
-		$this->addBehavior('RestrictAssociatedDelete');
-		$this->setDeleteStrategy('restrict');
-	}
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+        $this->belongsTo('Countries', ['className' => 'Area.AreaAdministratives', 'foreignKey' => 'area_administrative_id']);
+        $this->hasMany('Areas', ['className' => 'Area.AreaAdministratives', 'foreignKey' => 'area_administrative_level_id']);
+        $this->addBehavior('RestrictAssociatedDelete');
+        $this->setDeleteStrategy('restrict');
+    }
 
-	public function beforeAction(Event $event, ArrayObject $extra)
-	{
-		$this->field('level', ['before' => 'name']);
-		$this->field('area_administrative_id', ['type' => 'hidden', 'visible' => ['index' => false, 'view' => false, 'edit' => true, 'add' => true]]);
-	}
+    public function beforeAction(Event $event, ArrayObject $extra)
+    {
+        $this->field('level', ['before' => 'name']);
+        $this->field('area_administrative_id', ['type' => 'hidden', 'visible' => ['index' => false, 'view' => false, 'edit' => true, 'add' => true]]);
+    }
 
-	public function indexBeforeAction(Event $event, ArrayObject $extra)
-	{
+    public function indexBeforeAction(Event $event, ArrayObject $extra)
+    {
+    }
 
-	}
+    public function indexBeforeQuery(Event $event, Query $query, ArrayObject $extra)
+    {
+        //Add controls filter to index page
+        list($countryOptions, $selectedCountry) = array_values($this->getSelectOptions());
+        $extra['elements']['controls'] = ['name' => 'Area.controls', 'data' => [], 'options' => [], 'order' => 1];
+        $this->controller->set(compact('countryOptions', 'selectedCountry'));
+        $query->where([$this->aliasField('area_administrative_id') => $selectedCountry]);
+    }
 
-	public function indexBeforeQuery(Event $event, Query $query, ArrayObject $extra)
-	{
-		//Add controls filter to index page
-		list($countryOptions, $selectedCountry) = array_values($this->getSelectOptions());
-		$extra['elements']['controls'] = ['name' => 'Area.controls', 'data' => [], 'options' => [], 'order' => 1];
-		$this->controller->set(compact('countryOptions', 'selectedCountry'));
-		$query->where([$this->aliasField('area_administrative_id') => $selectedCountry]);
-	}
+    public function addEditBeforeAction(Event $event, ArrayObject $extra)
+    {
+        $this->fields['level']['type'] = 'hidden';
+    }
 
-	public function addEditBeforeAction(Event $event, ArrayObject $extra)
-	{
-		$this->fields['level']['type'] = 'hidden';
-	}
+    public function onUpdateFieldLevel(Event $event, array $attr, $action, Request $request)
+    {
+        if ($action == 'add') {
+            list(, $selectedCountry) = array_values($this->getSelectOptions());
 
-	public function onUpdateFieldLevel(Event $event, array $attr, $action, Request $request)
-	{
-		if ($action == 'add') {
-			list(, $selectedCountry) = array_values($this->getSelectOptions());
+            $query = $this->find();
+            $results = $query
+                ->select(['level' => $query->func()->max('level')])
+                ->where([$this->aliasField('area_administrative_id') => $selectedCountry])
+                ->all();
 
-			$query = $this->find();
-			$results = $query
-				->select(['level' => $query->func()->max('level')])
-				->where([$this->aliasField('area_administrative_id') => $selectedCountry])
-				->all();
+            $maxLevel = 0;
+            if (!$results->isEmpty()) {
+                $data = $results->first();
+                $maxLevel = $data->level;
+            }
 
-			$maxLevel = 0;
-			if (!$results->isEmpty()) {
-				$data = $results->first();
-				$maxLevel = $data->level;
-			}
+            $attr['attr']['value'] = ++$maxLevel;
+        }
 
-			$attr['attr']['value'] = ++$maxLevel;
-		}
+        return $attr;
+    }
 
-		return $attr;
-	}
+    public function onUpdateFieldAreaAdministrativeId(Event $event, array $attr, $action, Request $request)
+    {
+        if ($action == 'add') {
+            list(, $selectedCountry) = array_values($this->getSelectOptions());
+            $attr['attr']['value'] = $selectedCountry;
+        }
 
-	public function onUpdateFieldAreaAdministrativeId(Event $event, array $attr, $action, Request $request)
-	{
-		if ($action == 'add') {
-			list(, $selectedCountry) = array_values($this->getSelectOptions());
-			$attr['attr']['value'] = $selectedCountry;
-		}
+        return $attr;
+    }
 
-		return $attr;
-	}
-
-	public function deleteOnInitialize(Event $event, Entity $entity, Query $query, ArrayObject $extra)
-	{
-		//check config
+    public function deleteOnInitialize(Event $event, Entity $entity, Query $query, ArrayObject $extra)
+    {
+        //check config
         $ConfigItems = TableRegistry::get('Configuration.ConfigItems');
         $validateAreaAdministrativeLevel = $ConfigItems->value('institution_validate_area_administrative_level_id');
         if ($validateAreaAdministrativeLevel == $entity->id) {
-        	$extra['associatedRecords'][] = ['model' => 'System Configurations - Institution', 'count' => 1];
+            $extra['associatedRecords'][] = ['model' => 'System Configurations - Institution', 'count' => 1];
         }
     }
 
-	public function getSelectOptions()
-	{
-		//Return all required options and their key
-		$levelId = $this
-			->find('all')
-			->select([$this->aliasField('id')])
-			->where([$this->aliasField('level') => 0])
-			->first()
-			->id;
+    public function getSelectOptions()
+    {
+        //Return all required options and their key
+        $levelId = $this
+            ->find('all')
+            ->select([$this->aliasField('id')])
+            ->where([$this->aliasField('level') => 0])
+            ->first()
+            ->id;
 
-		$countryOptions = $this->Countries
-			->find('list')
-			->where([$this->Countries->aliasField('area_administrative_level_id') => $levelId])
-			->order([$this->Countries->aliasField('name')])
-			->toArray();
-		$selectedCountry = !is_null($this->request->query('country')) ? $this->request->query('country') : key($countryOptions);
+        $countryOptions = $this->Countries
+            ->find('list')
+            ->where([$this->Countries->aliasField('area_administrative_level_id') => $levelId])
+            ->order([$this->Countries->aliasField('name')])
+            ->toArray();
+        $selectedCountry = !is_null($this->request->query('country')) ? $this->request->query('country') : key($countryOptions);
 
-		return compact('countryOptions', 'selectedCountry');
-	}
+        return compact('countryOptions', 'selectedCountry');
+    }
 }
