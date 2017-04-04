ed4addf174850bd5e135e83a581d115f5478f13a
Steven Hasanuddin <shasanuddin@kordit.com>
2017-04-04 13:14:02 +0800

    3.9.9 - Updated master sql files

--- sql/Version/3.9.9/rollback.sql
+++ sql/Version/3.9.9/rollback.sql
@@ -1,3 +1,17 @@
+-- POCOR-3728
+-- alerts Table
+DELETE FROM `alerts` WHERE `name` = 'LicenseValidity';
+DELETE FROM `alerts` WHERE `name` = 'RetirementWarning';
+DELETE FROM `alerts` WHERE `name` = 'StaffEmployment';
+DELETE FROM `alerts` WHERE `name` = 'StaffLeave';
+DELETE FROM `alerts` WHERE `name` = 'StaffType';
+
+UPDATE `alerts` SET `process_name` = 'AttendanceAlert' WHERE `name` = 'Attendance';
+
+-- db_patches
+DELETE FROM `system_patches` WHERE `issue`='POCOR-3728';
+
+
 -- POCOR-3851
 ALTER TABLE `contact_types` DROP `validation_pattern`;
 
