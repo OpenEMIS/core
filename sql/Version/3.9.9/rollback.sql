d717558d49d47503650d2f3476ac4d370ee0625b
Steven Hasanuddin <shasanuddin@kordit.com>
2017-04-04 13:11:27 +0800

    3.9.9 - Updated master sql files

--- sql/Version/3.9.9/rollback.sql
+++ sql/Version/3.9.9/rollback.sql
@@ -1,3 +1,10 @@
+-- POCOR-3851
+ALTER TABLE `contact_types` DROP `validation_pattern`;
+
+-- db_patches
+DELETE FROM `system_patches` WHERE `issue`='POCOR-3851';
+
+
 -- 3.9.8.1
 DELETE FROM system_updates WHERE version = (SELECT value FROM config_items WHERE code = 'db_version');
 UPDATE config_items SET value = '3.9.8.1' WHERE code = 'db_version';
