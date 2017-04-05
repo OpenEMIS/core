2d2ec298d0076d3a7a4fdd07d8f6621ad58206ca
Livvi Sim <lsim@kordit.com>
2017-04-05 09:48:19 +0800

    POCOR-3826 - Revert changes to original

--- plugins/Institution/src/Controller/InstitutionsController.php
+++ plugins/Institution/src/Controller/InstitutionsController.php
@@ -816,8 +816,6 @@ class InstitutionsController extends AppController
         $this->autoRender= false;
         $StaffTable = TableRegistry::get('Institution.Staff');
         $positionTable = TableRegistry::get('Institution.InstitutionPositions');
-        $StaffStatuses = TableRegistry::get('Staff.StaffStatuses');
-        $assignedStatus = $StaffStatuses->getIdByCode('ASSIGNED');
         $userId = $this->Auth->user('id');
 
         $selectedFTE = empty($fte) ? 0 : $fte;
@@ -831,7 +829,6 @@ class InstitutionsController extends AppController
             ])
             ->where([
                 $StaffTable->aliasField('institution_id') => $institutionId,
-                $StaffTable->aliasField('staff_status_id') => $assignedStatus
             ])
             ->group($StaffTable->aliasField('institution_position_id'))
             ->having([
