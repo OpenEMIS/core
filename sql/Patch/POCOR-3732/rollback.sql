2cbc331e050aeebe4953685ddd9a76de1f5726ff
Thed Kuok Shiun <kthed@kordit.com>
2017-04-07 15:50:51 +0800

    POCOR-3732 - 1) Change school code to 20 characters 2) Drop modified_user_id and modified in workflow_transitions table 3) Drop action from staff_behaviours and merge it into description 4) Add code to institution_cases 5) Change association of institution_case_records to hasMany.

--- sql/Patch/POCOR-3732/rollback.sql
+++ sql/Patch/POCOR-3732/rollback.sql
@@ -1,8 +1,20 @@
+-- institutions
+DROP TABLE IF EXISTS `institutions`;
+RENAME TABLE `z_3732_institutions` TO `institutions`;
+
+-- workflow_transitions
+DROP TABLE IF EXISTS `workflow_transitions`;
+RENAME TABLE `z_3732_workflow_transitions` TO `workflow_transitions`;
+
+-- staff_behaviours
+DROP TABLE IF EXISTS `staff_behaviours`;
+RENAME TABLE `z_3732_staff_behaviours` TO `staff_behaviours`;
+
 -- institution_cases
 DROP TABLE IF EXISTS `institution_cases`;
 
--- institution_cases_records
-DROP TABLE IF EXISTS `institution_cases_records`;
+-- institution_case_records
+DROP TABLE IF EXISTS `institution_case_records`;
 
 -- workflow_rules
 DROP TABLE IF EXISTS `workflow_rules`;
