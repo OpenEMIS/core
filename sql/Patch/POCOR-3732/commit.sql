5735ab0bdd618020a1b3fdd56fbb0e3474a6256a
Thed Kuok Shiun <kthed@kordit.com>
2017-04-07 15:50:51 +0800

    POCOR-3732 - 1) Change school code to 20 characters 2) Drop modified_user_id and modified in workflow_transitions table 3) Drop action from staff_behaviours and merge it into description 4) Add code to institution_cases 5) Change association of institution_case_records to hasMany.

--- sql/Patch/POCOR-3732/commit.sql
+++ sql/Patch/POCOR-3732/commit.sql
@@ -1,10 +1,55 @@
 -- system_patches
 INSERT INTO `system_patches` (`issue`, `created`) VALUES('POCOR-3732', NOW());
 
+-- institutions
+CREATE TABLE `z_3732_institutions`  LIKE `institutions`;
+INSERT INTO `z_3732_institutions` SELECT * FROM `institutions`;
+
+ALTER TABLE `institutions` CHANGE `code` `code` VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+
+-- workflow_transitions
+CREATE TABLE `z_3732_workflow_transitions`  LIKE `workflow_transitions`;
+INSERT INTO `z_3732_workflow_transitions` SELECT * FROM `workflow_transitions`;
+
+ALTER TABLE `workflow_transitions` DROP `modified_user_id`;
+ALTER TABLE `workflow_transitions` DROP `modified`;
+
+-- staff_behaviours
+RENAME TABLE `staff_behaviours` TO `z_3732_staff_behaviours`;
+
+DROP TABLE IF EXISTS `staff_behaviours`;
+CREATE TABLE `staff_behaviours` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` text NOT NULL,
+  `date_of_behaviour` date NOT NULL,
+  `time_of_behaviour` time DEFAULT NULL,
+  `staff_id` int(11) NOT NULL COMMENT 'links to security_users.id',
+  `institution_id` int(11) NOT NULL COMMENT 'links to institutions.id',
+  `staff_behaviour_category_id` int(11) NOT NULL COMMENT 'links to staff_behaviour_categories.id',
+  `behaviour_classification_id` int(11) NOT NULL COMMENT 'links to behaviour_classifications.id',
+  `modified_user_id` int(11) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `created_user_id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `staff_id` (`staff_id`),
+  KEY `institution_id` (`institution_id`),
+  KEY `staff_behaviour_category_id` (`staff_behaviour_category_id`),
+  KEY `behaviour_classification_id` (`behaviour_classification_id`),
+  KEY `modified_user_id` (`modified_user_id`),
+  KEY `created_user_id` (`created_user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='This table contains all behavioural records of staff';
+
+
+INSERT INTO `staff_behaviours` (`id`, `description`, `date_of_behaviour`, `time_of_behaviour`, `staff_id`, `institution_id`, `staff_behaviour_category_id`, `behaviour_classification_id`, `modified_user_id`, `modified`, `created_user_id`, `created`)
+SELECT `id`, CONCAT(`description`, ' - ', `action`), `date_of_behaviour`, `time_of_behaviour`, `staff_id`, `institution_id`, `staff_behaviour_category_id`, `behaviour_classification_id`, `modified_user_id`, `modified`, `created_user_id`, `created`
+FROM `z_3732_staff_behaviours`;
+
 -- institution_cases
 DROP TABLE IF EXISTS `institution_cases`;
 CREATE TABLE IF NOT EXISTS `institution_cases` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `description` text DEFAULT NULL,
   `status_id` int(11) NOT NULL COMMENT 'links to workflow_steps.id',
@@ -22,18 +67,24 @@ CREATE TABLE IF NOT EXISTS `institution_cases` (
   KEY `created_user_id` (`created_user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='This table contains all the cases in a particular institution';
 
--- institution_cases_records
-DROP TABLE IF EXISTS `institution_cases_records`;
-CREATE TABLE IF NOT EXISTS `institution_cases_records` (
+-- institution_case_records
+DROP TABLE IF EXISTS `institution_case_records`;
+CREATE TABLE IF NOT EXISTS `institution_case_records` (
   `id` char(64) COLLATE utf8mb4_unicode_ci NOT NULL,
   `institution_case_id` int(11) NOT NULL COMMENT 'links to institution_cases.id',
   `record_id` int(11) NOT NULL,
   `feature` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `modified_user_id` int(11) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `created_user_id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
   PRIMARY KEY (`institution_case_id`, `record_id`, `feature`),
   KEY `id` (`id`),
   KEY `institution_case_id` (`institution_case_id`),
   KEY `record_id` (`record_id`),
-  KEY `feature` (`feature`)
+  KEY `feature` (`feature`),
+  KEY `modified_user_id` (`modified_user_id`),
+  KEY `created_user_id` (`created_user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='This table contains the list of records associates with cases';
 
 -- workflow_rules
