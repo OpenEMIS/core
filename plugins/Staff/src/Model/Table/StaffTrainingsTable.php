4818badf04761bffe891dee522da560153b59006
Harry Suhendra <hsuhendra@kordit.com>
2017-04-11 17:19:45 +0800

    POCOR-3927 - Update StaffTrainings to CAv4. - Move StaffTrainings to Staff > Training. - Arrange the staff > training tab > Need, Applications, Result, Course.

--- plugins/Staff/src/Model/Table/StaffTrainingsTable.php
+++ plugins/Staff/src/Model/Table/StaffTrainingsTable.php
@@ -2,35 +2,42 @@
 namespace Staff\Model\Table;
 
 use ArrayObject;
+
 use Cake\Validation\Validator;
 use Cake\Event\Event;
 use Cake\ORM\TableRegistry;
 use Cake\ORM\Query;
 use Cake\ORM\Entity;
-use App\Model\Table\AppTable;
 
-class StaffTrainingsTable extends AppTable {
-	public function initialize(array $config) {
+use App\Model\Table\ControllerActionTable;
+
+class StaffTrainingsTable extends ControllerActionTable
+{
+	public function initialize(array $config)
+	{
 		parent::initialize($config);
 
 		$this->belongsTo('Users', ['className' => 'User.Users', 'foreignKey' => 'staff_id']);
 		$this->belongsTo('StaffTrainingCategories', ['className' => 'Staff.StaffTrainingCategories']);
 	}
 
-	public function beforeAction() {
+	public function beforeAction(Event $event, ArrayObject $extra)
+	{
 		$userId = $this->Session->read('Staff.Staff.id');
-		$this->ControllerAction->field('staff_id', ['type' => 'hidden', 'value' => $userId]);
-		$this->ControllerAction->field('staff_training_category_id', ['type' => 'select']);
-		$this->ControllerAction->field('completed_date', ['default_date' => true]);
+		$this->field('staff_id', ['type' => 'hidden', 'value' => $userId]);
+		$this->field('staff_training_category_id', ['type' => 'select']);
+		$this->field('completed_date', ['default_date' => true]);
 	}
 
-	private function setupTabElements() {
-		$tabElements = $this->controller->getProfessionalDevelopmentTabElements();
+	private function setupTabElements()
+	{
+		$tabElements = $this->controller->getInstitutionTrainingTabElements();
 		$this->controller->set('tabElements', $tabElements);
 		$this->controller->set('selectedAction', 'Trainings');
 	}
 
-	public function afterAction(Event $event) {
+	public function afterAction(Event $event)
+	{
 		$this->setupTabElements();
 	}
 }
