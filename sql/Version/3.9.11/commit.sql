b9c35516004726ba2bb4e2b0ee880c65cdc10075
Steven Hasanuddin <shasanuddin@kordit.com>
2017-04-18 16:09:51 +0800

    3.9.11 - Updated master sql files

--- sql/Version/3.9.11/commit.sql
+++ sql/Version/3.9.11/commit.sql
@@ -1,3 +1,12 @@
+-- POCOR-3876
+-- system_patches
+INSERT INTO `system_patches` (`issue`, `created`) VALUES ('POCOR-3876', NOW());
+
+-- labels
+INSERT INTO `labels` (`id`, `module`, `field`, `module_name`, `field_name`, `code`, `name`, `visible`, `mo                                                                                                           dified_user_id`, `modified`, `created_user_id`, `created`)
+VALUES ('b7b9aad6-1ff1-11e7-a840-525400b263eb', 'InstitutionClasses', 'multigrade', 'Institutions -> Class                                                                                                           es', 'Multi-grade', NULL, NULL, '1', NULL, NULL, '1', '2017-04-13');
+
+
 -- 3.9.11
 UPDATE config_items SET value = '3.9.11' WHERE code = 'db_version';
 UPDATE system_patches SET version = (SELECT value FROM config_items WHERE code = 'db_version') WHERE version IS NULL;
