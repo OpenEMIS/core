1d29efd06d1f74d3a52c27b587f82574c1b8f5cf
Harry Suhendra <hsuhendra@kordit.com>
2017-04-19 14:16:39 +0800

    POCOR-3516 - add decimal type to Custom Fields. WIP

---
+++ sql/Patch/POCOR-3516/commit.sql
@@ -0,0 +1,22 @@
+-- system_patches
+INSERT INTO `system_patches` (`issue`, `created`) VALUES ('POCOR-3516', NOW());
+
+-- custom_field_types
+CREATE TABLE `z_3516_custom_field_types`  LIKE `custom_field_types`;
+INSERT INTO `z_3516_custom_field_types` SELECT * FROM `custom_field_types`;
+
+INSERT INTO `custom_field_types` (`code`, `name`, `value`, `description`, `format`, `is_mandatory`, `is_unique`, `visible`)
+VALUES ('DECIMAL', 'Decimal', 'decimal_value', '', 'OpenEMIS', '1', '1', '1');
+
+-- custom_field_values
+CREATE TABLE `z_3516_custom_field_values`  LIKE `custom_field_values`;
+INSERT INTO `z_3516_custom_field_values` SELECT * FROM `custom_field_values`;
+
+ALTER TABLE `custom_field_values` ADD `decimal_value` DECIMAL(10,2) NULL DEFAULT NULL AFTER `file`;
+
+
+-- institution_custom_field_values
+CREATE TABLE `z_3516_institution_custom_field_values`  LIKE `institution_custom_field_values`;
+INSERT INTO `z_3516_institution_custom_field_values` SELECT * FROM `institution_custom_field_values`;
+
+ALTER TABLE `institution_custom_field_values` ADD `decimal_value` DECIMAL(10,2) NULL DEFAULT NULL AFTER `file`;
