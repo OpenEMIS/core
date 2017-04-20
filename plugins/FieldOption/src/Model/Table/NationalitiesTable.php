f9798497dfff9c401d1b07b3e7d4049ae2673357
Martinnus Budiarto <mbudiarto@kordit.com>
2017-04-20 15:09:16 +0800

    POCOR-3804 : * validate that one identity type can only be used for nationality * validate for existing record if has identity used my multiple grades, then during edit, that option wont be available anymore

--- plugins/FieldOption/src/Model/Table/NationalitiesTable.php
+++ plugins/FieldOption/src/Model/Table/NationalitiesTable.php
@@ -7,6 +7,7 @@ use Cake\Validation\Validator;
 use App\Model\Table\ControllerActionTable;
 use Cake\ORM\TableRegistry;
 use Cake\ORM\Entity;
+use Cake\Network\Request;
 use ArrayObject;
 
 class NationalitiesTable extends ControllerActionTable
@@ -26,8 +27,42 @@ class NationalitiesTable extends ControllerActionTable
         ]);
     }
 
-    public function afterAction(Event $event) {
-        $this->field('identity_type_id', ['type' => 'select', 'after' => 'name']);
+    public function afterAction(Event $event, ArrayObject $extra) 
+    {
+        $this->field('identity_type_id', [
+            'type' => 'select', 
+            'after' => 'name',
+            'entity' => $extra['entity']
+        ]);
+    }
+
+    public function onUpdateFieldIdentityTypeId(Event $event, array $attr, $action, Request $request)
+    {
+        $usedIdentityType = $this
+                            ->find('list', ['keyField' => 'id', 'valueField' => 'id'])
+                            ->select([
+                                'id' => $this->aliasfield('identity_type_id')
+                            ])
+                            ->where([
+                                $this->aliasfield('identity_type_id IS NOT') => null
+                            ])
+                            ->toArray();
+
+        if ($action == 'edit') {
+            $entity = $attr['entity'];
+            if ($entity->has('identity_type_id') && !empty($entity->identity_type_id) && !empty($usedIdentityType)) {
+                unset($usedIdentityType[$entity->identity_type_id]);
+            }
+        }
+        
+        $options = $this->IdentityTypes->find('list')
+                    ->where([
+                        $this->IdentityTypes->aliasField('id NOT IN') => $usedIdentityType
+                    ])
+                    ->toArray();
+        $attr['options'] = $options;
+
+        return $attr;
     }
 
     public function afterSave(Event $event, Entity $entity, ArrayObject $options)
