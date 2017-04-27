3b1fc409a0093b23a3db2daf062558a7e4aa472d
Harry Suhendra <hsuhendra@kordit.com>
2017-04-27 09:52:10 +0800

    POCOR-3896 - Adding validation rule range to restrict the input value 0-99.

--- src/Model/Traits/MessagesTrait.php
+++ src/Model/Traits/MessagesTrait.php
@@ -1351,7 +1351,7 @@ trait MessagesTrait {
         'Education' => [
             'EducationCycles' => [
                 'admission_age' => [
-                    'ruleValidateNumeric' => 'Please enter a valid Numeric value'
+                    'ruleRange' => 'Admision age must be within 0 to 99'
                 ]
             ],
             'EducationGradesSubjects' => [
