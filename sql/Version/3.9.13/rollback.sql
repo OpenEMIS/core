0d9c6ba07edc994653839243d18ab28bebf7a490
Steven Hasanuddin <shasanuddin@kordit.com>
2017-05-12 09:54:17 +0800

    3.9.13 - Updated master sql files

--- sql/Version/3.9.13/rollback.sql
+++ sql/Version/3.9.13/rollback.sql
@@ -1,3 +1,12 @@
+-- POCOR-3936
+-- staff_behaviours
+DROP TABLE IF EXISTS `staff_behaviours`;
+RENAME TABLE `z_3936_staff_behaviours` TO `staff_behaviours`;
+
+-- db_patches
+DELETE FROM `system_patches` WHERE `issue`='POCOR-3936';
+
+
 -- POCOR-3880
 DELETE FROM `authentication_type_attributes` WHERE `attribute_field` = 'role_mapping' OR `attribute_field` = 'saml_role_mapping';
 
