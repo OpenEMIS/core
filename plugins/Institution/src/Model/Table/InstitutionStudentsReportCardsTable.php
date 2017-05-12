e4b400e5fbe273809a7ae9df6ffd177f205e1736
Livvi Sim <lsim@kordit.com>
2017-05-12 17:46:03 +0800

    POCOR-3533 - Move comment finder from InstitutionStudentsReportCards to InstitutionClassStudents

--- plugins/Institution/src/Model/Table/InstitutionStudentsReportCardsTable.php
+++ plugins/Institution/src/Model/Table/InstitutionStudentsReportCardsTable.php
@@ -27,89 +27,10 @@ class InstitutionStudentsReportCardsTable extends ControllerActionTable
             'dependent' => true,
             'cascadeCallbacks' => true
         ]);
-        // $this->belongsToMany('ReportCardSubjects', [
-        //     'className' => 'ReportCard.ReportCardSubjects',
-        //     'joinTable' => 'institution_students_report_cards_comments',
-        //     'foreignKey' => ['report_card_id', 'student_id', 'institution_id', 'academic_period_id', 'education_grade_id'],
-        //     'targetForeignKey' => ['report_card_id', 'education_subject_id'],
-        //     'through' => 'Institution.InstitutionStudentsReportCardsComments',
-        //     'dependent' => true,
-        //     'cascadeCallbacks' => true
-        // ]);
 
         $this->addBehavior('CompositeKey');
         $this->addBehavior('Restful.RestfulAccessControl', [
-            'ReportCardComments' => ['index', 'add']
+            'ReportCardComments' => ['add']
         ]);
-    }
-
-    public function findComments(Query $query, array $options)
-    {
-        $academicPeriodId = $options['academic_period_id'];
-        $institutionId = $options['institution_id'];
-        $classId = $options['institution_class_id'];
-        $educationGradeId = $options['education_grade_id'];
-        $reportCardId = $options['report_card_id'];
-        $educationSubjectId = $options['education_subject_id'];
-        $type = $options['type'];
-
-        $Students = $this->Students;
-        $StudentStatuses = $this->InstitutionClasses->ClassStudents->StudentStatuses;
-
-        $query
-            ->select([
-                $this->aliasField('student_id'),
-                $Students->aliasField('openemis_no'),
-                $Students->aliasField('first_name'),
-                $Students->aliasField('middle_name'),
-                $Students->aliasField('third_name'),
-                $Students->aliasField('last_name'),
-                $Students->aliasField('preferred_name'),
-                $StudentStatuses->aliasField('name')
-            ])
-            ->matching('Students')
-            ->matching('InstitutionClasses.ClassStudents.StudentStatuses')
-            ->where([
-                $this->aliasField('academic_period_id') => $academicPeriodId,
-                $this->aliasField('institution_id') => $institutionId,
-                $this->aliasField('institution_class_id') => $classId,
-                $this->aliasField('education_grade_id') => $educationGradeId,
-                $this->aliasField('report_card_id') => $reportCardId
-            ])
-            ->group([
-                $this->aliasField('student_id'),
-                $this->aliasField('academic_period_id'),
-                $this->aliasField('report_card_id')
-            ])
-            ->order([
-                $Students->aliasField('first_name'), $Students->aliasField('last_name')
-            ]);
-
-
-        if ($type == 'PRINCIPAL') {
-            $query->select(['comments' => $this->aliasfield('principal_comments')]);
-
-        } else if ($type == 'HOMEROOM_TEACHER') {
-            $query->select(['comments' => $this->aliasfield('homeroom_teacher_comments')]);
-
-        } else if ($type == 'TEACHER') {
-            $ReportCardsComments = TableRegistry::get('Institution.InstitutionStudentsReportCardsComments');
-
-            $query
-                ->select([
-                    'comments' => $ReportCardsComments->aliasField('comments'),
-                    'comment_code' => $ReportCardsComments->aliasField('report_card_comment_code_id'),
-                ])
-                ->leftJoin([$ReportCardsComments->alias() => $ReportCardsComments->table()], [
-                    $ReportCardsComments->aliasField('report_card_id = ') . $this->aliasField('report_card_id'),
-                    $ReportCardsComments->aliasField('student_id = ') . $this->aliasField('student_id'),
-                    $ReportCardsComments->aliasField('institution_id = ') . $this->aliasField('institution_id'),
-                    $ReportCardsComments->aliasField('academic_period_id = ') . $this->aliasField('academic_period_id'),
-                    $ReportCardsComments->aliasField('education_grade_id = ') . $this->aliasField('education_grade_id'),
-                    $ReportCardsComments->aliasField('education_subject_id') => $educationSubjectId
-                ]);
-        }
-
-        return $query;
     }
 }
