9ef240457295f54a92374dfac50c9667959c9fc1
Chiang Hao Long <hchiang@kordit.com>
2017-05-17 16:22:01 +0800

    POCOR-3877 - Added logic to retrieve security permissions

--- plugins/Security/webroot/js/angular/permission/security.permission.edit.ctrl.js
+++ plugins/Security/webroot/js/angular/permission/security.permission.edit.ctrl.js
@@ -5,5 +5,44 @@ SecurityPermissionEditController.$inject = ['$scope', '$q', '$window', '$http', 
 
 function SecurityPermissionEditController($scope, $q, $window, $http, UtilsSvc, AlertSvc, SecurityPermissionEditSvc) {
 
+    var Controller = this;
 
+    // variable
+    Controller.modules = [
+        {
+            'key': 'Institutions',
+            'name': 'Institutions'
+        },
+        {
+            'key': 'Directory',
+            'name': 'Directory'
+        },
+        {
+            'key': 'Reports',
+            'name': 'Reports'
+        },
+        {
+            'key': 'Administrations',
+            'name': 'Administrations'
+        }
+    ];
+    Controller.selectedModule = 'Institutions';
+    Controller.roleId = 0;
+
+    // function
+    Controller.changeModule = changeModule;
+
+    angular.element(document).ready(function () {
+        SecurityPermissionEditSvc.init(angular.baseUrl);
+        var module = Controller.modules[0].key;
+        SecurityPermissionEditSvc.getPermissions(Controller.roleId, module)
+        .then(function(permissions) {
+            console.log(permissions);
+        }, function(error) {
+
+        });
+    });
+    function changeModule (module) {
+        console.log(module);
+    }
 }
