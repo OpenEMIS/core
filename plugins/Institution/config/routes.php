c7443691e1b7f4d43447ef9236092d36c88c3944
Chiang Hao Long <hchiang@kordit.com>
2017-05-18 15:49:40 +0800

    POCOR-3853 - Retain url pattern

--- plugins/Institution/config/routes.php
+++ plugins/Institution/config/routes.php
@@ -3,18 +3,20 @@ use Cake\Routing\Router;
 
 Router::scope('/Institutions', ['plugin' => 'Institution', 'controller' => 'Institutions'], function ($routes) {
 
-    // Will have to modify this to point to institution action when institutions table is migrated to CAv4
+    $routes->connect('/',
+        ['action' => 'Institutions']
+    );
 
     $routes->connect('/:institutionId/:action/*',
         ['plugin' => 'Institution', 'controller' => 'Institutions'],
         ['institutionId' => '([\w]+[\.][\w]+)', 'action' => '[a-zA-Z]+']
     );
 
-    // // For the main model's action
-    // $routes->connect('/:subaction',
-    //     ['action' => 'Institutions'],
-    //     ['subaction' => '[a-zA-Z]+', 'pass' => [0 => 'subaction']]
-    // );
+    // For the main model's action
+    $routes->connect('/:subaction',
+        ['action' => 'Institutions'],
+        ['subaction' => '[a-zA-Z]+', 'pass' => [0 => 'subaction']]
+    );
 
     // For controller action version 3
     $routes->connect('/:action/*',
