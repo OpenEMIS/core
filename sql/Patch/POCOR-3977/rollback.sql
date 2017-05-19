40021735f91f50bf17fe4aa414b18064449dec95
Chiang Hao Long <hchiang@kordit.com>
2017-05-19 17:00:46 +0800

    POCOR-3977 - Fixed rollback

--- sql/Patch/POCOR-3977/rollback.sql
+++ sql/Patch/POCOR-3977/rollback.sql
@@ -2,5 +2,5 @@
 INSERT INTO `system_patches` (`issue`, `created`) VALUES('POCOR-3977', NOW());
 
 ALTER TABLE `deleted_records`
-CHANGE COLUMN `reference_key` `reference_key` TEXT CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci' NOT NULL ;
+CHANGE COLUMN `reference_key` `reference_key` CHAR(36) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci' NOT NULL ;
 
