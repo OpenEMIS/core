07c138622e73f33639e93d5b9f94a6d0831dac5e
Livvi Sim <lsim@kordit.com>
2017-05-22 19:37:06 +0800

    POCOR-3533 - Add security_functions

--- sql/Patch/POCOR-3533/commit.sql
+++ sql/Patch/POCOR-3533/commit.sql
@@ -121,3 +121,29 @@ CREATE TABLE IF NOT EXISTS `report_card_comment_codes` (
  `created` datetime NOT NULL,
  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='This is a field option table containing the list of user-defined comment codes that can be used for student report cards';
+
+-- security_functions
+UPDATE `security_functions`
+SET `order` = `order` + 4
+WHERE `order` >= 1050 AND `order` <= 1056;
+
+UPDATE `security_functions`
+SET `order` = `order` + 1
+WHERE `order` >= 2018 AND `order` <= 2031;
+
+UPDATE `security_functions`
+SET `order` = `order` + 1
+WHERE `order` >= 5067 AND `order` <= 5071;
+
+UPDATE `security_functions`
+SET `order` = `order` + 1
+WHERE `order` >= 7017 AND `order` <= 7051;
+
+INSERT INTO `security_functions` (`id`, `name`, `controller`, `module`, `category`, `parent_id`, `_view`, `_edit`, `_add`, `_delete`, `_execute`, `order`, `visible`, `description`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES
+(1057, 'Comments', 'Institutions', 'Institutions', 'Report Cards', 1000, 'ReportCardComments.index|ReportCardComments.view|Comments.index', 'Comments.edit', NULL, NULL, NULL, 1050, 1, NULL, NULL, NULL, 1, NOW()),
+(1058, 'Statuses', 'Institutions', 'Institutions', 'Report Cards', 1000, 'ReportCardStatuses.index|ReportCardStatuses.view', NULL, NULL, NULL, NULL, 1051, 1, NULL, NULL, NULL, 1, NOW()),
+(1059, 'Generate/Download', 'Institutions', 'Institutions', 'Report Cards', 1000, NULL, NULL, NULL, NULL, 'ReportCardStatuses.generate|ReportCardStatuses.generateAll|InstitutionStudentsReportCards.download|ReportCardStatuses.downloadAll', 1052, 1, NULL, NULL, NULL, 1, NOW()),
+(1060, 'Publish/Unpublish', 'Institutions', 'Institutions', 'Report Cards', 1000, NULL, NULL, NULL, NULL, 'ReportCardStatuses.publish|ReportCardStatuses.publishAll|ReportCardStatuses.unpublish|ReportCardStatuses.unpublishAll', 1053, 1, NULL, NULL, NULL, 1, NOW()),
+(2033, 'Report Cards', 'Students', 'Institutions', 'Students - Academic', 1000, 'ReportCards.index|ReportCards.view', NULL, NULL, NULL, 'ReportCards.download', 2018, 1, NULL, NULL, NULL, 1, NOW()),
+(5072, 'Templates', 'ReportCards', 'Administration', 'Report Cards', 5000, 'Templates.index|Templates.view', 'Templates.edit', 'Templates.add', 'Templates.remove', 'Templates.download', 5067, 1, NULL, NULL, NULL, 1, NOW()),
+(7052, 'Report Cards', 'Directories', 'Directory', 'Students - Academic', 7000, 'StudentReportCards.index|StudentReportCards.view', NULL, NULL, NULL, 'StudentReportCards.download', 7017, 1, NULL, NULL, NULL, 1, NOW());
