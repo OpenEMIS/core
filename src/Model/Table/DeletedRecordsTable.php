4d4bba610c0200d0c9a14dd6b9988883fe8cee95
Chiang Hao Long <hchiang@kordit.com>
2017-05-30 13:34:33 +0800

    POCOR-3977 - Change saving of deleted record strategy

--- src/Model/Table/DeletedRecordsTable.php
+++ src/Model/Table/DeletedRecordsTable.php
@@ -1,19 +1,8 @@
 <?php
 namespace App\Model\Table;
 
-// use ArrayObject;
-// use Cake\I18n\Time;
-// use Cake\ORM\Table;
-// use Cake\ORM\Query;
-// use Cake\ORM\Entity;
-// use Cake\ORM\TableRegistry;
-// use Cake\Event\Event;
-// use Cake\Network\Request;
-// use Cake\Utility\Inflector;
-// use Cake\Validation\Validator;
-// use App\Model\Traits\OptionsTrait;
-
-class DeletedRecordsTable extends AppTable {
+class DeletedRecordsTable extends AppTable
+{
     public function initialize(array $config)
     {
         parent::initialize($config);
