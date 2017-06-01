7a9e6b655104ec066649d271cc2b7478d402e566
Steven Hasanuddin <shasanuddin@kordit.com>
2017-06-01 17:06:08 +0800

    3.9.14.1 - Updated master sql files

--- sql/Version/3.9.14.1/commit.sql
+++ sql/Version/3.9.14.1/commit.sql
@@ -1,3 +1,11 @@
+-- POCOR-4013
+-- system_patches
+INSERT INTO `system_patches` (`issue`, `created`) VALUES ('POCOR-4013', NOW());
+
+-- security_functions
+UPDATE `security_functions` SET `_execute` = 'Assessments.excel|resultsExport' WHERE `id` = 1015;
+
+
 -- 3.9.14.1
 UPDATE config_items SET value = '3.9.14.1' WHERE code = 'db_version';
 UPDATE system_patches SET version = (SELECT value FROM config_items WHERE code = 'db_version') WHERE version IS NULL;
--- sql/Version/3.9.14.1/rollback.sql
+++ sql/Version/3.9.14.1/rollback.sql
@@ -1,3 +1,11 @@
+-- POCOR-4013
+-- security_functions
+UPDATE `security_functions` SET `_execute` = 'Assessments.excel|ClassStudents.excel' WHERE `id` = 1015;
+
+-- system_patches
+DELETE FROM `system_patches` WHERE `issue` = 'POCOR-4013';
+
+
 -- 3.9.13.2
 DELETE FROM system_updates WHERE version = (SELECT value FROM config_items WHERE code = 'db_version');
 UPDATE config_items SET value = '3.9.13.2' WHERE code = 'db_version';
