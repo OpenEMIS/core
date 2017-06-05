cf3bb4c81ff1c74e0646c0b6ccb81c3434b8b720
Livvi Sim <lsim@kordit.com>
2017-06-05 16:18:39 +0800

    POCOR-2470 - Add Custom reports table to navigation and reports controller

--- plugins/Report/src/Model/Table/CustomReportsTable.php
+++ plugins/Report/src/Model/Table/CustomReportsTable.php
@@ -9,30 +9,50 @@ use Cake\Network\Request;
 use App\Model\Table\AppTable;
 use Cake\Utility\Inflector;
 
-class CustomReportsTable extends ReportsTable  {
-	public function initialize(array $config) {
+class CustomReportsTable extends AppTable
+{
+	public function initialize(array $config)
+	{
+		$this->table('reports');
 		parent::initialize($config);
+
+        $this->addBehavior('Report.ReportList');
 	}
 
-	public function beforeAction(Event $event) {
-		
+	public function beforeAction(Event $event)
+	{
+		$controllerName = $this->controller->name;
+		$reportName = __('Custom Reports');
+
+		$this->controller->Navigation->substituteCrumb($this->alias(), $reportName);
+		$this->controller->set('contentHeader', __($controllerName).' - '.$reportName);
 	}
 
-	public function addEditBeforeAction(Event $event) {
-		$this->ControllerAction->field('name');
-		$this->ControllerAction->field('target', ['type' => 'hidden']);
-		$this->ControllerAction->field('query', ['type' => 'hidden']);
+	public function addBeforeAction(Event $event)
+    {
+        $this->ControllerAction->field('name', ['type' => 'hidden']);
+        $this->ControllerAction->field('query', ['type' => 'hidden']);
+        $this->ControllerAction->field('filter', ['type' => 'hidden']);
+        $this->ControllerAction->field('excel_template_name', ['type' => 'hidden']);
+        $this->ControllerAction->field('excel_template', ['type' => 'hidden']);
 
-		$query = '{"from":["`institution_students` AS `InstitutionStudents`"], 
-		"join":[{"table":"security_users","type":"INNER","alias":"SecurityUsers","conditions":["`SecurityUsers`.`id` = `InstitutionStudents`.`student_id`"]}], "select":["`InstitutionStudents`.`id` AS `student name`"],
-		"where":["`InstitutionStudents`.`academic_period_id` = 10"],
-		"having":["COUNT(`InstitutionStudents`.`student_status_id`) > 0"],
-		"group":["`InstitutionStudents`.`student_status_id`"]
-		}';
-		$entity = $this->newEntity();
-		$entity->query = $query;
-		$this->setupValues($entity);
-		pr($entity);die;
-		
-	}
+        $this->ControllerAction->field('feature', ['type' => 'select', 'select' => false]);
+        $this->ControllerAction->field('format');
+    }
+
+	public function onUpdateFieldFeature(Event $event, array $attr, $action, Request $request)
+    {
+        if ($action == 'add') {
+        	$reportOptions = $this->find('list')->toArray();
+
+            $attr['options'] = $reportOptions;
+            $attr['onChangeReload'] = true;
+            if (!(isset($this->request->data[$this->alias()]['feature']))) {
+                $option = $attr['options'];
+                reset($option);
+                $this->request->data[$this->alias()]['feature'] = key($option);
+            }
+            return $attr;
+        }
+    }
 }
