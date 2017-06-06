6a7b6d83b50ed822f61576d0b3e24eca4d5395a6
Livvi Sim <lsim@kordit.com>
2017-06-06 11:25:30 +0800

    POCOR-2470 - Add custom report behaviour

--- plugins/Report/src/Model/Table/CustomReportsTable.php
+++ plugins/Report/src/Model/Table/CustomReportsTable.php
@@ -4,6 +4,7 @@ namespace Report\Model\Table;
 use ArrayObject;
 use Cake\ORM\Entity;
 use Cake\ORM\Query;
+use Cake\ORM\TableRegistry;
 use Cake\Event\Event;
 use Cake\Network\Request;
 use App\Model\Table\AppTable;
@@ -17,7 +18,22 @@ class CustomReportsTable extends AppTable
 		parent::initialize($config);
 
         $this->addBehavior('Report.ReportList');
+        $this->addBehavior('CustomExcel.CustomReport');
+        $this->addBehavior('CustomExcel.ExcelReport', [
+            'templateTable' => 'CustomExcel.CustomReports',
+            'templateTableKey' => 'feature',
+            'variableSource' => 'database'
+        ]);
 	}
+
+    public function implementedEvents()
+    {
+        $events = parent::implementedEvents();
+        $events['ExcelTemplates.Model.onExcelTemplateBeforeGenerate'] = 'onExcelTemplateBeforeGenerate';
+        $events['ExcelTemplates.Model.onExcelTemplateGetQueryVariables'] = 'onExcelTemplateGetQueryVariables';
+
+        return $events;
+    }
 
 	public function beforeAction(Event $event)
 	{
@@ -30,14 +46,26 @@ class CustomReportsTable extends AppTable
 
 	public function addBeforeAction(Event $event)
     {
-        $this->ControllerAction->field('name', ['type' => 'hidden']);
-        $this->ControllerAction->field('query', ['type' => 'hidden']);
-        $this->ControllerAction->field('filter', ['type' => 'hidden']);
-        $this->ControllerAction->field('excel_template_name', ['type' => 'hidden']);
-        $this->ControllerAction->field('excel_template', ['type' => 'hidden']);
-
+        $this->fields = [];
         $this->ControllerAction->field('feature', ['type' => 'select', 'select' => false]);
         $this->ControllerAction->field('format');
+
+        if (isset($this->request->data[$this->alias()]['feature'])) {
+            $id = $this->request->data[$this->alias()]['feature'];
+            $customReportData = $this->find()
+                ->where([$this->aliasField('id') => $id])
+                ->first();
+
+            if (!empty($customReportData) && !empty($customReportData->filter)) {
+                $jsonFilters = $customReportData->filter;
+                $filters = json_decode($jsonFilters, true);
+
+                if (array_key_exists('academic_period_id', $filters)) {
+                     $this->ControllerAction->field('academic_period_id');
+                     unset($filters['academic_period_id']);
+                }
+            }
+        }
     }
 
 	public function onUpdateFieldFeature(Event $event, array $attr, $action, Request $request)
@@ -54,5 +82,45 @@ class CustomReportsTable extends AppTable
             }
             return $attr;
         }
+    }
+
+    public function onUpdateFieldAcademicPeriodId(Event $event, array $attr, $action, Request $request)
+    {
+        if ($action == 'add') {
+            $AcademicPeriods = TableRegistry::get('AcademicPeriod.AcademicPeriods');
+            $periodOptions = $AcademicPeriods->getYearList(['isEditable' => true]);
+            $selectedPeriod = $AcademicPeriods->getCurrent();
+
+            $attr['onChangeReload'] = true;
+            $attr['options'] = $periodOptions;
+            $attr['default'] = $selectedPeriod;
+            $attr['type'] = 'select';
+            $attr['select'] = false;
+            return $attr;
+        }
+    }
+
+    public function onExcelTemplateBeforeGenerate(Event $event, array $params, ArrayObject $extra)
+    {
+        $str = $this->get($params['feature'])->name;
+        $reportName = str_replace(' ', '_', $str);
+        $this->behaviors()->get('ExcelReport')->config([
+            'filename' => $reportName
+        ]);
+    }
+
+    public function onExcelTemplateGetQueryVariables(Event $event, array $params, ArrayObject $extra)
+    {
+        $customReportData = $this->get($params['feature']);
+        $jsonQuery = json_decode($customReportData->query, true);
+
+        $variables = new ArrayObject([]);
+        foreach($jsonQuery as $key => $obj) {
+            $query = $this->parseQuery($obj, $params);
+            $entity = $query->toArray();
+            $variables[$key] = $entity;
+        }
+
+        return $variables;
     }
 }
