f6f59ebca6d97bce78b7392987b6aa661a3f0871
Martinnus Budiarto <mbudiarto@kordit.com>
2017-06-07 14:11:39 +0800

    POCOR-3996 - adjust the rollback command sequence.

--- sql/Patch/POCOR-3996/rollback.sql
+++ sql/Patch/POCOR-3996/rollback.sql
@@ -2,9 +2,9 @@
 DELETE FROM `import_mapping`
 WHERE `model` = 'Training.TrainingSessionsTrainees';
 
--- `system_patches`
-DELETE FROM `system_patches` WHERE `issue`='POCOR-3996';
-
 -- `labels`
 DELETE FROM `labels`
 WHERE `id` = '6c3d2497-4b27-11e7-9846-525400b263eb';
+
+-- `system_patches`
+DELETE FROM `system_patches` WHERE `issue`='POCOR-3996';
