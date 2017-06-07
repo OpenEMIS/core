5dd632d67bb93512fdf9feed39c94bd59b2479d4
Steven Hasanuddin <shasanuddin@kordit.com>
2017-06-07 13:50:21 +0800

    3.10.2 - Updated master sql files

--- sql/Version/3.10.2/rollback.sql
+++ sql/Version/3.10.2/rollback.sql
@@ -1,3 +1,20 @@
+-- POCOR-3983
+-- institution_statuses
+DROP TABLE IF EXISTS `institution_statuses`;
+RENAME TABLE `z_3983_institution_statuses` TO `institution_statuses`;
+
+-- institutions
+DROP TABLE IF EXISTS `institutions`;
+RENAME TABLE `z_3983_institutions` TO `institutions`;
+
+-- import_mapping
+DROP TABLE IF EXISTS `import_mapping`;
+RENAME TABLE `z_3983_import_mapping` TO `import_mapping`;
+
+-- db_patches
+DELETE FROM `system_patches` WHERE `issue`='POCOR-3983';
+
+
 -- POCOR-1330
 -- education_grades
 DROP TABLE IF EXISTS `education_grades`;
