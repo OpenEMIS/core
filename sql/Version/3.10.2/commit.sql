f040322c834c80c9bc846c9cd9ef52d2045710da
Steven Hasanuddin <shasanuddin@kordit.com>
2017-06-08 13:36:22 +0800

    3.10.2 - Updated master sql files

--- sql/Version/3.10.2/commit.sql
+++ sql/Version/3.10.2/commit.sql
@@ -1,3 +1,14 @@
+-- POCOR-3556
+-- db_patches
+INSERT INTO `system_patches` (`issue`, `created`) VALUES('POCOR-3556', NOW());
+
+-- training_session_trainers
+CREATE TABLE `z_3556_training_session_trainers` LIKE `training_session_trainers`;
+INSERT `z_3556_training_session_trainers` SELECT * FROM `training_session_trainers`;
+
+ALTER TABLE `training_session_trainers` DROP `type`;
+
+
 -- POCOR-3923
 -- system_patches
 INSERT INTO `system_patches` (`issue`, `created`) VALUES('POCOR-3923', NOW());
