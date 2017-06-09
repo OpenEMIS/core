4243621567e09dde673551601440b4b6fe71a107
Chiang Hao Long <hchiang@kordit.com>
2017-06-09 16:59:56 +0800

    POCOR-3646 - Added username and password field as a text field to appear on all user type

---
+++ sql/Patch/POCOR-3646/commit.sql
@@ -0,0 +1,5 @@
+-- system_patches
+INSERT INTO `system_patches` (`issue`, `created`) VALUES ('POCOR-3646', NOW());
+
+-- security_users
+
---
+++ sql/Patch/POCOR-3646/rollback.sql
@@ -0,0 +1,4 @@
+-- security_users
+
+-- system_patches
+DELETE FROM `system_patches` WHERE `issue` = 'POCOR-3646';
