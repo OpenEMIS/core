25ef444dadc4451e078802cf97188f8255b2b989
Chiang Hao Long <hchiang@kordit.com>
2017-06-09 17:24:52 +0800

    POCOR-3646 - Patch security users table user name field

--- sql/Patch/POCOR-3646/rollback.sql
+++ sql/Patch/POCOR-3646/rollback.sql
@@ -1,4 +1,8 @@
 -- security_users
+-- No rollback due to size of the records
+
+ALTER TABLE `security_users`
+CHANGE COLUMN `username` `username` VARCHAR(50) NULL ;
 
 -- system_patches
 DELETE FROM `system_patches` WHERE `issue` = 'POCOR-3646';
