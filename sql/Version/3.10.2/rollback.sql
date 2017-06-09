52e9b285c8467c8e57873e78243d856ce74ac6ca
Steven Hasanuddin <shasanuddin@kordit.com>
2017-06-09 09:30:06 +0800

    3.10.2 - Updated master sql files

--- sql/Version/3.10.2/rollback.sql
+++ sql/Version/3.10.2/rollback.sql
@@ -1,3 +1,17 @@
+-- POCOR-3824
+-- field options
+DROP TABLE IF EXISTS `training_need_competencies`;
+DROP TABLE IF EXISTS `training_need_standards`;
+DROP TABLE IF EXISTS `training_need_sub_standards`;
+
+-- staff_training_needs
+DROP TABLE IF EXISTS `staff_training_needs`;
+RENAME TABLE `z_3824_staff_training_needs` TO `staff_training_needs`;
+
+-- system_patches
+DELETE FROM `system_patches` WHERE `issue` = 'POCOR-3824';
+
+
 -- POCOR-3556
 -- training_session_trainers
 DROP TABLE IF EXISTS `training_session_trainers`;
