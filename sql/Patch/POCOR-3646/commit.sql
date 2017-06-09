ae0346ccd2835fb9f0181f7c074112e506899191
Chiang Hao Long <hchiang@kordit.com>
2017-06-09 17:24:52 +0800

    POCOR-3646 - Patch security users table user name field

--- sql/Patch/POCOR-3646/commit.sql
+++ sql/Patch/POCOR-3646/commit.sql
@@ -2,4 +2,9 @@
 INSERT INTO `system_patches` (`issue`, `created`) VALUES ('POCOR-3646', NOW());
 
 -- security_users
+UPDATE `security_users`
+SET `username` = `openemis_no`
+WHERE `username` IS NULL;
 
+ALTER TABLE `security_users`
+CHANGE COLUMN `username` `username` VARCHAR(50) NOT NULL ;
