eb3ea16ad300229bad8a9f4c0d13668a16af69fa
Steven Hasanuddin <shasanuddin@kordit.com>
2017-06-09 17:30:23 +0800

    3.10.1.1 - Updated master sql files

--- sql/Version/3.10.1.1/commit.sql
+++ sql/Version/3.10.1.1/commit.sql
@@ -1,3 +1,12 @@
+-- POCOR-4040
+-- system_patches
+INSERT INTO `system_patches` (`issue`, `created`) VALUES ('POCOR-4040', NOW());
+
+ALTER TABLE `institution_rooms`
+CHANGE COLUMN `room_type_id` `room_type_id` INT(11) NOT NULL COMMENT 'links to room_types.id',
+CHANGE COLUMN `room_status_id` `room_status_id` INT(11) NOT NULL COMMENT 'links to infrastructure_statuses.id' ;
+
+
 -- 3.10.1.1
 UPDATE config_items SET value = '3.10.1.1' WHERE code = 'db_version';
 UPDATE system_patches SET version = (SELECT value FROM config_items WHERE code = 'db_version') WHERE version IS NULL;
