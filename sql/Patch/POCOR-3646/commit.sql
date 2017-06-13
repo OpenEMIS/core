b7ecbc56e085aee7fb411b9f35d29487a9864a0b
Chiang Hao Long <hchiang@kordit.com>
2017-06-13 09:18:15 +0800

    POCOR-3646 - Fixed commit character length for username

--- sql/Patch/POCOR-3646/commit.sql
+++ sql/Patch/POCOR-3646/commit.sql
@@ -7,7 +7,7 @@ SET `username` = `openemis_no`
 WHERE `username` IS NULL;
 
 ALTER TABLE `security_users`
-CHANGE COLUMN `username` `username` VARCHAR(50) NOT NULL ;
+CHANGE COLUMN `username` `username` VARCHAR(100) NOT NULL ;
 
 UPDATE `security_functions` SET `_add`='StudentUser.add|getUniqueOpenemisId|generatePassword' WHERE `id`=1043;
 UPDATE `security_functions` SET `_add`='StaffUser.add|getUniqueOpenemisId|generatePassword' WHERE `id`=1044;
