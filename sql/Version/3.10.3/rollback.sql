64e6d8b09342867164f270b4f5fdb6390b3b8d8f
Steven Hasanuddin <shasanuddin@kordit.com>
2017-06-13 13:46:32 +0800

    3.10.3 - Updated master sql files

--- sql/Version/3.10.3/rollback.sql
+++ sql/Version/3.10.3/rollback.sql
@@ -1,3 +1,17 @@
+-- POCOR-3646
+-- security_users
+-- No rollback due to size of the records
+
+ALTER TABLE `security_users`
+CHANGE COLUMN `username` `username` VARCHAR(50) NULL ;
+
+UPDATE `security_functions` SET `_add`='StudentUser.add|getUniqueOpenemisId' WHERE `id`=1043;
+UPDATE `security_functions` SET `_add`='StaffUser.add|getUniqueOpenemisId' WHERE `id`=1044;
+
+-- system_patches
+DELETE FROM `system_patches` WHERE `issue` = 'POCOR-3646';
+
+
 -- POCOR-4025
 -- education_grades_subjects
 ALTER TABLE `education_grades_subjects` CHANGE `hours_required` `hours_required` INT(5) NOT NULL;
