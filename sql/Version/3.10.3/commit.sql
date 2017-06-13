6c5d5397109bb2928479b405d150febf14293575
Steven Hasanuddin <shasanuddin@kordit.com>
2017-06-13 10:54:51 +0800

    3.10.3 - Updated master sql files

--- sql/Version/3.10.3/commit.sql
+++ sql/Version/3.10.3/commit.sql
@@ -1,3 +1,11 @@
+-- POCOR-4025
+-- db_patches
+INSERT INTO `system_patches` (`issue`, `created`) VALUES('POCOR-4025', NOW());
+
+-- education_grades_subjects
+ALTER TABLE `education_grades_subjects` CHANGE `hours_required` `hours_required` DECIMAL(5,2) NULL;
+
+
 -- 3.10.3
 UPDATE config_items SET value = '3.10.3' WHERE code = 'db_version';
 UPDATE system_patches SET version = (SELECT value FROM config_items WHERE code = 'db_version') WHERE version IS NULL;
