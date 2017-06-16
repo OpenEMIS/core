d2f9d17087b177a879b33a2d177095e774ab8203
Steven Hasanuddin <shasanuddin@kordit.com>
2017-06-16 11:27:51 +0800

    3.10.3 - Updated master sql files

--- sql/Version/3.10.3/commit.sql
+++ sql/Version/3.10.3/commit.sql
@@ -1,3 +1,11 @@
+-- POCOR-2785
+-- system_patches
+INSERT INTO `system_patches` (`issue`, `created`) VALUES('POCOR-2785', NOW());
+
+-- translations
+INSERT INTO `translations` (`en`, `editable`, `created_user_id`, `created`) VALUES ('%tree_no_of_item items selected', 1, 1, NOW());
+
+
 -- POCOR-3646
 -- system_patches
 INSERT INTO `system_patches` (`issue`, `created`) VALUES ('POCOR-3646', NOW());
