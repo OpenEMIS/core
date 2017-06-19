3e4fb2b9df3132d2a6cb835d0cccb3a4a59466d0
Chiang Hao Long <hchiang@kordit.com>
2017-06-19 18:10:34 +0800

    POCOR-3931 - Fixed patch and rollback

--- sql/Patch/POCOR-3931/rollback.sql
+++ sql/Patch/POCOR-3931/rollback.sql
@@ -2,9 +2,11 @@ DROP TABLE `system_authentications`;
 
 DROP TABLE `authentication_types`;
 
-DROP TABLE `authentication_type_attributes`;
+DROP TABLE `idp_google`;
 
-DROP TABLE `system_authentication_type_attributes`;
+DROP TABLE `idp_oauth`;
+
+DROP TABLE `idp_saml`;
 
 ALTER TABLE `z_3931_authentication_type_attributes`
 RENAME TO  `authentication_type_attributes` ;
