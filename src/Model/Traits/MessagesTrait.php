bd0ca17a517f74d0b3894a82a6f29f4c39b11c97
Harry Suhendra <hsuhendra@kordit.com>
2017-06-20 17:04:46 +0800

    POCOR-4042 - Remove the mandatory field when add the multigrade classes. - Staff_id is allow empty.

--- src/Model/Traits/MessagesTrait.php
+++ src/Model/Traits/MessagesTrait.php
@@ -827,9 +827,6 @@ trait MessagesTrait
                 'name' => [
                     'ruleUniqueNamePerAcademicPeriod' => 'Class name has to be unique',
                 ],
-                'staff_id' => [
-                    'ruleCheckHomeroomTeacher' => 'This field cannot be left empty',
-                ],
             ],
 
             'InstitutionProgrammes' => [
