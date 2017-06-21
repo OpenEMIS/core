0873d889e6fa70db023517e7585743f4235f7132
Steven Hasanuddin <shasanuddin@kordit.com>
2017-06-21 11:40:05 +0800

    3.10.4 - Updated master sql files

--- sql/Version/3.10.4/commit.sql
+++ sql/Version/3.10.4/commit.sql
@@ -1,3 +1,14 @@
+-- POCOR-4042
+-- db_patches
+INSERT INTO `system_patches` (`issue`, `created`) VALUES('POCOR-4042', NOW());
+
+-- institution_classes
+CREATE TABLE `z_4042_institution_classes` LIKE `institution_classes`;
+INSERT `z_4042_institution_classes` SELECT * FROM `institution_classes`;
+
+ALTER TABLE `institution_classes` CHANGE `staff_id` `staff_id` INT(11) NULL DEFAULT '0' COMMENT 'links to security_users.id';
+
+
 -- 3.10.4
 UPDATE config_items SET value = '3.10.4' WHERE code = 'db_version';
 UPDATE system_patches SET version = (SELECT value FROM config_items WHERE code = 'db_version') WHERE version IS NULL;
