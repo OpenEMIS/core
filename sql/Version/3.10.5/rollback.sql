63280f9ea764350a1ddf4a30b91f0f0aacbbb6d7
Steven Hasanuddin <shasanuddin@kordit.com>
2017-06-28 16:14:32 +0800

    3.10.5 - Updated master sql files

--- sql/Version/3.10.5/rollback.sql
+++ sql/Version/3.10.5/rollback.sql
@@ -1,3 +1,12 @@
+-- POCOR-3809
+-- security_users
+DROP TABLE IF EXISTS `security_users`;
+RENAME TABLE `z_3809_security_users` TO `security_users`;
+
+-- system_patches
+DELETE FROM `system_patches` WHERE `issue` = 'POCOR-3809';
+
+
 -- POCOR-3955
 -- security_functions
 DROP TABLE IF EXISTS `security_functions`;
