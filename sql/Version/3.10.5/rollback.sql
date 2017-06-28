403bb2a47b4e0a85b2f4ec65495a4eb6cf1f3729
Steven Hasanuddin <shasanuddin@kordit.com>
2017-06-28 15:09:06 +0800

    3.10.5 - Updated master sql files

--- sql/Version/3.10.5/rollback.sql
+++ sql/Version/3.10.5/rollback.sql
@@ -1,3 +1,12 @@
+-- POCOR-3955
+-- security_functions
+DROP TABLE IF EXISTS `security_functions`;
+RENAME TABLE `z_3955_security_functions` TO `security_functions`;
+
+-- db_patches
+DELETE FROM `system_patches` WHERE `issue`='POCOR-3955';
+
+
 -- 3.10.4
 DELETE FROM system_updates WHERE version = (SELECT value FROM config_items WHERE code = 'db_version');
 UPDATE config_items SET value = '3.10.4' WHERE code = 'db_version';
