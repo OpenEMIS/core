93cb9bb4cbb184a6595f63258f47eaf5cf1a4669
Chiang Hao Long <hchiang@kordit.com>
2017-06-29 14:20:25 +0800

    POCOR-3931 - Fixed commit and rollback

--- sql/Patch/POCOR-3931/rollback.sql
+++ sql/Patch/POCOR-3931/rollback.sql
@@ -17,4 +17,12 @@ DELETE FROM `config_items` WHERE `id` = 1001;
 INSERT INTO `config_items`
 SELECT * FROM z_3931_config_items;
 
+DROP TABLE z_3931_config_items;
+
+-- security_user_logins
+DROP TABLE security_user_logins;
+
+ALTER TABLE `z_3931_security_user_logins`
+RENAME TO  `security_user_logins` ;
+
 DELETE FROM `system_patches` WHERE `issue` = 'POCOR-3931';
