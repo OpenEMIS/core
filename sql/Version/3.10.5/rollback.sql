59b766360e1594b21a2dee2d1520c4d231cd95a7
Steven Hasanuddin <shasanuddin@kordit.com>
2017-06-30 14:44:40 +0800

    3.10.5 - Updated master sql files

--- sql/Version/3.10.5/rollback.sql
+++ sql/Version/3.10.5/rollback.sql
@@ -1,3 +1,34 @@
+-- POCOR-3931
+DROP TABLE `system_authentications`;
+
+DROP TABLE `authentication_types`;
+
+DROP TABLE `idp_google`;
+
+DROP TABLE `idp_oauth`;
+
+DROP TABLE `idp_saml`;
+
+ALTER TABLE `z_3931_authentication_type_attributes`
+RENAME TO  `authentication_type_attributes` ;
+
+-- config_items
+DELETE FROM `config_items` WHERE `id` = 1001;
+
+INSERT INTO `config_items`
+SELECT * FROM z_3931_config_items;
+
+DROP TABLE z_3931_config_items;
+
+-- security_user_logins
+DROP TABLE security_user_logins;
+
+ALTER TABLE `z_3931_security_user_logins`
+RENAME TO  `security_user_logins` ;
+
+DELETE FROM `system_patches` WHERE `issue` = 'POCOR-3931';
+
+
 -- POCOR-2470
 -- reports
 DROP TABLE IF EXISTS `reports`;
