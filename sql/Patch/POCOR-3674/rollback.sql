037f0bbc9ca22e7031cda13b766d9288b1fc7b30
Tan Wei Hsing <wtan@kordit.com>
2017-07-04 15:48:06 +0800

    POCOR-3674 - updated rollback.sql

--- sql/Patch/POCOR-3674/rollback.sql
+++ sql/Patch/POCOR-3674/rollback.sql
@@ -7,5 +7,9 @@ DROP TABLE IF EXISTS `locale_content_translations`;
 -- locale_contents
 DROP TABLE IF EXISTS `locale_contents`;
 
+-- translations
+RENAME TABLE `z_3674_translations` TO `translations`;
+
+
 -- system_patches
 DELETE FROM `system_patches` WHERE `issue` = 'POCOR-3674';
