2bcfa503acc72712c84381a6ecde0f9a5f7b32f9
Steven Hasanuddin <shasanuddin@kordit.com>
2017-07-06 13:15:26 +0800

    3.10.6 - Updated master sql fileS

--- sql/Version/3.10.6/rollback.sql
+++ sql/Version/3.10.6/rollback.sql
@@ -1,3 +1,15 @@
+-- POCOR-4079
+-- education_subjects_field_of_studies
+DROP TABLE IF EXISTS `education_subjects_field_of_studies`;
+
+-- staff_qualifications
+DROP TABLE IF EXISTS `staff_qualifications`;
+RENAME TABLE `z_4079_staff_qualifications` TO `staff_qualifications`;
+
+-- db_patches
+DELETE FROM `system_patches` WHERE `issue`='POCOR-4079';
+
+
 -- POCOR-3941
 INSERT INTO institution_class_students
 SELECT * FROM z_3941_institution_class_students;
