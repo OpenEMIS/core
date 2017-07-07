d9301cd88feb12b7607fe4f3cf483cd71c19ab5c
Chiang Hao Long <hchiang@kordit.com>
2017-07-07 10:52:54 +0800

    POCOR-4081 - Remove index on username. Alter deleted records table to change key to big int unsign. Added partition to deleted records table.

--- sql/Patch/POCOR-4081/rollback.sql
+++ sql/Patch/POCOR-4081/rollback.sql
@@ -1,6 +1,10 @@
 ALTER TABLE `user_identities`
-DROP INDEX `number`,
-DROP INDEX `username`;
+DROP INDEX `number`;
+
+DROP TABLE `deleted_records`;
+
+ALTER TABLE `z_4081_deleted_records`
+RENAME TO `deleted_records`;
 
 -- system_patches
 DELETE FROM `system_patches` WHERE `issue` = 'POCOR-4081';
