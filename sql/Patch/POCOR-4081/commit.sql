3d31706aaa94382425e585691cd42690a7486f44
Chiang Hao Long <hchiang@kordit.com>
2017-07-07 09:42:47 +0800

    POCOR-4081 - Fixed username check on generation of OpenEMIS No

--- sql/Patch/POCOR-4081/commit.sql
+++ sql/Patch/POCOR-4081/commit.sql
@@ -3,3 +3,6 @@ INSERT INTO `system_patches` (`issue`, `created`) VALUES ('POCOR-4081', NOW());
 
 ALTER TABLE `user_identities`
 ADD INDEX `number` (`number`);
+
+ALTER TABLE `security_users`
+ADD INDEX `username` (`username`);
