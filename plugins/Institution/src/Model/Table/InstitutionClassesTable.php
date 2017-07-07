b670a3d012c98e84f9c27228e9b5af618f475dce
Harry Suhendra <hsuhendra@kordit.com>
2017-07-07 09:35:47 +0800

    POCOR-3995 - Adding checking for the subject, so only the subject with auto allocation will be added automatically to the student.

--- plugins/Institution/src/Model/Table/InstitutionClassesTable.php
+++ plugins/Institution/src/Model/Table/InstitutionClassesTable.php
@@ -240,19 +240,26 @@ class InstitutionClassesTable extends ControllerActionTable
             $subjects = json_decode($this->urlsafeB64Decode($data['subjects']), true);
             $subjectStudents = [];
             foreach ($subjects as $subject) {
-                foreach ($data['class_students'] as $classStudent) {
-                    $subjectStudents[] = [
-                        'student_status_id' => $classStudent['student_status_id'],
-                        'student_id' => $classStudent['student_id'],
-                        'institution_subject_id' => $subject['id'],
-                        'institution_class_id' => $classStudent['institution_class_id'],
-                        'institution_id' => $subject['institution_id'],
-                        'academic_period_id' => $subject['academic_period_id'],
-                        'education_subject_id' => $subject['education_subject_id'],
-                        'education_grade_id' => $classStudent['education_grade_id']
-                    ];
+                // will check in the education grade subject if the subject is an auto allocation subject
+                $isAutoAddSubject = $this->isAutoAddSubject($subject);
+
+                // if the subject is not an add_auto_subject will not be added automatically to the student.
+                if ($isAutoAddSubject) {
+                    foreach ($data['class_students'] as $classStudent) {
+                        $subjectStudents[] = [
+                            'student_status_id' => $classStudent['student_status_id'],
+                            'student_id' => $classStudent['student_id'],
+                            'institution_subject_id' => $subject['id'],
+                            'institution_class_id' => $classStudent['institution_class_id'],
+                            'institution_id' => $subject['institution_id'],
+                            'academic_period_id' => $subject['academic_period_id'],
+                            'education_subject_id' => $subject['education_subject_id'],
+                            'education_grade_id' => $classStudent['education_grade_id']
+                        ];
+                    }
                 }
             }
+
             $data['subject_students'] = $subjectStudents;
             $data->offsetUnset('subjects');
         }
@@ -1220,5 +1227,23 @@ class InstitutionClassesTable extends ControllerActionTable
                 return $q->where(['InstitutionSubjects.education_subject_id' => $subjectId]);
             })
             ->toArray();
+    }
+
+    private function isAutoAddSubject($subject)
+    {
+        // will check in the education grade subject if the subject is an auto allocation subject
+        $EducationGradesSubjects = TableRegistry::get('Education.EducationGradesSubjects');
+
+        $educationGradeId = $subject['education_grade_id'];
+        $educationSubjectId = $subject['education_subject_id'];
+        $educationGradesSubjectsData = $EducationGradesSubjects->find()
+            ->where([
+                $EducationGradesSubjects->aliasField('education_grade_id') => $educationGradeId,
+                $EducationGradesSubjects->aliasField('education_subject_id') => $educationSubjectId
+            ])
+            ->first();
+        $addAutoSubject = $educationGradesSubjectsData['auto_allocation'];
+
+        return $addAutoSubject;
     }
 }
