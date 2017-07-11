9cdba196e63c729e05092078e5803fbe9b225cbc
Steven Hasanuddin <shasanuddin@kordit.com>
2017-07-11 14:03:04 +0800

    3.10.7 - Updated master sql files

--- sql/Version/3.10.7/commit.sql
+++ sql/Version/3.10.7/commit.sql
@@ -1,3 +1,32 @@
+-- POCOR-3995
+-- system_patches
+INSERT INTO `system_patches` (`issue`, `created`) VALUES ('POCOR-3995', NOW());
+
+-- education_grades_subjects
+RENAME TABLE `education_grades_subjects` TO `z_3995_education_grades_subjects`;
+
+DROP TABLE IF EXISTS `education_grades_subjects`;
+CREATE TABLE IF NOT EXISTS `education_grades_subjects` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `hours_required` int(5) NOT NULL,
+    `visible` int(1) NOT NULL DEFAULT '1',
+    `auto_allocation` int(1) NOT NULL DEFAULT '1',
+    `education_grade_id` int(11) NOT NULL COMMENT 'links to education_grades.id',
+    `education_subject_id` int(11) NOT NULL COMMENT 'links to education_subjects.id',
+    `modified_user_id` int(11) DEFAULT NULL,
+    `modified` datetime DEFAULT NULL,
+    `created_user_id` int(11) NOT NULL,
+    `created` datetime NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `education_grade_id` (`education_grade_id`),
+    KEY `education_subject_id` (`education_subject_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='This table contains the list of subjects linked to specific education grade';
+
+INSERT `education_grades_subjects` (`id`, `hours_required`, `visible`, `auto_allocation`, `education_grade_id`, `education_subject_id`, `modified_user_id`, `modified`, `created_user_id`, `created`)
+SELECT `id`, `hours_required`, `visible`, 1, `education_grade_id`, `education_subject_id`, `modified_user_id`, `modified`, `created_user_id`, `created`
+FROM `z_3995_education_grades_subjects`;
+
+
 -- 3.10.7
 UPDATE config_items SET value = '3.10.7' WHERE code = 'db_version';
 UPDATE system_patches SET version = (SELECT value FROM config_items WHERE code = 'db_version') WHERE version IS NULL;
