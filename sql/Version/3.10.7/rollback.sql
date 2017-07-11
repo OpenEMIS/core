9f4ecb8e18fa1408230759c7c15bd8f15ba5d0e7
Steven Hasanuddin <shasanuddin@kordit.com>
2017-07-11 14:03:04 +0800

    3.10.7 - Updated master sql files

--- sql/Version/3.10.7/rollback.sql
+++ sql/Version/3.10.7/rollback.sql
@@ -1,3 +1,12 @@
+-- POCOR-3995
+-- education_grades_subjects
+DROP TABLE IF EXISTS `education_grades_subjects`;
+RENAME TABLE `z_3995_education_grades_subjects` TO `education_grades_subjects`;
+
+-- system_patches
+DELETE FROM `system_patches` WHERE `issue` = 'POCOR-3995';
+
+
 -- 3.10.6
 DELETE FROM system_updates WHERE version = (SELECT value FROM config_items WHERE code = 'db_version');
 UPDATE config_items SET value = '3.10.6' WHERE code = 'db_version';
