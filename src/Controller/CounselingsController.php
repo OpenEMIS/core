164eb1672c44154050d37e549d3a806cab838a0e
Harry Suhendra <hsuhendra@kordit.com>
2017-07-12 13:57:41 +0800

    POCOR-3953 - Using queryString instead queryOptions to passed the some variables. - Used the onRender to displayed counsellor name and guidance type in the view page. - Add the student name in the header. - Combine the add and edit logic to addEditCounseling function.

--- src/Controller/CounselingsController.php
+++ src/Controller/CounselingsController.php
@@ -14,7 +14,15 @@ class CounselingsController extends PageController
         $this->Page->loadElementsFromTable($this->Counselings);
         $this->Page->disable(['delete']);
 
-        $this->loadComponent('RenderDate');
+        $this->loadComponent('RenderDate'); // will get the date format from config
+    }
+
+    public function implementedEvents()
+    {
+        $events = parent::implementedEvents();
+        $events['Controller.Page.onRenderCounselorId'] = 'onRenderCounselorId';
+        $events['Controller.Page.onRenderGuidanceTypeId'] = 'onRenderGuidanceTypeId';
+        return $events;
     }
 
     public function beforeFilter(Event $event)
@@ -25,29 +33,32 @@ class CounselingsController extends PageController
         $studentId = $session->read('Student.Students.id');
         $studentName = $session->read('Student.Students.name');
 
-        // Breadcrumb
+        parent::beforeFilter($event);
+
+        // set Breadcrumb
         $this->Navigation->addCrumb('Institutions', ['plugin' => 'Institution', 'controller' => 'Institutions', 'action' => 'Institutions', 'index']);
         $this->Navigation->addCrumb($institutionName, ['plugin' => 'Institution', 'controller' => 'Institutions', 'action' => 'dashboard', 'institutionId' => $this->ControllerAction->paramsEncode(['id' => $institutionId]), $this->ControllerAction->paramsEncode(['id' => $institutionId])]);
         $this->Navigation->addCrumb('Students', ['plugin' => $this->plugin, 'controller' => 'Institutions', 'action' => 'Students', 'institutionId' => $this->ControllerAction->paramsEncode(['id' => $institutionId])]);
         $this->Navigation->addCrumb($studentName, ['plugin' => $this->plugin, 'controller' => 'Institutions', 'action' => 'StudentUser', 'view', $this->ControllerAction->paramsEncode(['id' => $studentId])]);
-
-        parent::beforeFilter($event);
-        $this->Navigation->addCrumb('Counselings', ['plugin' => false, 'controller' => 'Counselings', 'action' => 'index']);
+        // $this->Navigation->addCrumb('Counselings', ['plugin' => false, 'controller' => 'Counselings', 'action' => 'index']);
+        $this->Navigation->addCrumb('Counselings');
 
         $page = $this->Page;
         $page->exclude(['file_name', 'file_content']);
+
+        // set header
+        $header = $page->getHeader();
+        $page->setHeader($studentName . ' - ' . $header);
+
+        // set queryString
+        $page->setQueryString('institution_id', $institutionId);
+        $page->setQueryString('student_id', $studentId);
     }
 
     public function index()
     {
-        $session = $this->request->session();
-        $institutionId = $session->read('Institution.Institutions.id');
-        $studentId = $session->read('Student.Students.id');
-
         $page = $this->Page;
         $page->exclude(['counselor_id', 'student_id']);
-
-        $page->setQueryOption('student_id', $studentId); // passed the student Id
 
         parent::index();
     }
@@ -62,39 +73,34 @@ class CounselingsController extends PageController
 
     public function add()
     {
-        $page = $this->Page;
-
-        $session = $this->request->session();
-        $institutionId = $session->read('Institution.Institutions.id');
-        $studentId = $session->read('Student.Students.id');
-
-        // set the options for guidance_type_id
-        $guidanceTypeOptions = $this->Counselings->getGuidanceTypeOptions();
-        if (empty($guidanceTypeOptions)) {
-            $guidanceTypeOptions = ['' => __('No Options')];
-        }
-        $page->get('guidance_type_id')->setControlType('dropdown')->setOptions($guidanceTypeOptions);
-
-        // set the options for counselor_id
-        $counselorOptions = $this->Counselings->getCounselorOptions($institutionId);
-        if (empty($counselorOptions)) {
-            $counselorOptions = ['' => __('No Options')];
-        }
-        $page->get('counselor_id')->setControlType('dropdown')->setOptions($counselorOptions);
-
-        // // set student_id
-        $page->get('student_id')->setControlType('hidden')->setValue($studentId);
+        $this->addEditCounseling();
 
         parent::add();
     }
 
     public function edit($id)
     {
+        $this->addEditCounseling();
+
+        parent::edit($id);
+    }
+
+    public function onRenderCounselorId(Event $event, $entity, $key)
+    {
+        return $entity->counselor->name;
+    }
+
+    public function onRenderGuidanceTypeId(Event $event, $entity, $key)
+    {
+        return $entity->guidance_type->name;
+    }
+
+    private function addEditCounseling()
+    {
         $page = $this->Page;
 
-        $session = $this->request->session();
-        $institutionId = $session->read('Institution.Institutions.id');
-        $studentId = $session->read('Student.Students.id');
+        $institutionId = $page->getQueryString('institution_id');
+        $studentId = $page->getQueryString('student_id');
 
         // set the options for guidance_type_id
         $guidanceTypeOptions = $this->Counselings->getGuidanceTypeOptions();
@@ -112,7 +118,5 @@ class CounselingsController extends PageController
 
         // set student_id
         $page->get('student_id')->setControlType('hidden')->setValue($studentId);
-
-        parent::edit($id);
     }
 }
