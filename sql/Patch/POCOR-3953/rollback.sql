ffc4e4f3be8cbee6bf1a4497a3a4fd6812665270
Harry Suhendra <hsuhendra@kordit.com>
2017-07-14 10:22:58 +0800

    POCOR-3953 - Adding the permission to security_functions and security_role_functions, will be the same as institution students permission (1012). - Remove the getGuidanceTypeOptions as the options will be done automatically. - Remove the findIndex as the conditions will be set automatically.

--- sql/Patch/POCOR-3953/rollback.sql
+++ sql/Patch/POCOR-3953/rollback.sql
@@ -4,5 +4,13 @@ DROP TABLE `guidance_types`;
 -- institution_counselors
 DROP TABLE `institution_counselors`;
 
+-- security_functions
+DROP TABLE IF EXISTS `security_functions`;
+RENAME TABLE `z_3953_security_functions` TO `security_functions`;
+
+-- security_role_functions
+DROP TABLE IF EXISTS `security_role_functions`;
+RENAME TABLE `z_3953_security_role_functions` TO `security_role_functions`;
+
 -- system_patches
 DELETE FROM `system_patches` WHERE `issue` = 'POCOR-3953';
