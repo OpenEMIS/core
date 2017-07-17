c4855ef2a6c833f371bcee0803fb0eeebe1284e2
Steven Hasanuddin <shasanuddin@kordit.com>
2017-07-17 15:43:40 +0800

    3.10.8 - Updated master sql files

--- sql/Version/3.10.8/commit.sql
+++ sql/Version/3.10.8/commit.sql
@@ -1,3 +1,36 @@
+-- POCOR-4089
+-- system_patches
+INSERT INTO `system_patches` (`issue`, `created`) VALUES ('POCOR-4089', NOW());
+
+-- education_grades
+CREATE TABLE `z_4089_education_grades` LIKE `education_grades`;
+INSERT INTO `z_4089_education_grades` SELECT * FROM `education_grades`;
+
+SET @index := 1;
+SET @prev := '';
+
+UPDATE `education_grades` edu_grades
+INNER JOIN (
+    SELECT
+        g.`id`,
+        @current := g.`code`,
+        (@index := IF(@current = @prev, @index + 1, 1)) AS current_index,
+        @prev := g.`code`
+    FROM `education_grades` g
+    INNER JOIN (
+        SELECT `code`,
+        COUNT(*) AS `count`
+        FROM `education_grades`
+        GROUP BY `code`
+        HAVING `count` > 1
+    ) c
+    ON g.`code` = c.`code`
+    ORDER BY g.`code`, g.`id`
+) ind
+ON ind.`id` = edu_grades.`id`
+SET `code` = CONCAT(edu_grades.`code`, CONCAT('-', ind.`current_index`));
+
+
 -- 3.10.8
 UPDATE config_items SET value = '3.10.8' WHERE code = 'db_version';
 UPDATE system_patches SET version = (SELECT value FROM config_items WHERE code = 'db_version') WHERE version IS NULL;
--- sql/Version/3.10.8/rollback.sql
+++ sql/Version/3.10.8/rollback.sql
@@ -1,3 +1,12 @@
+-- POCOR-4089
+-- education_grades
+DROP TABLE IF EXISTS `education_grades`;
+RENAME TABLE `z_4089_education_grades` TO `education_grades`;
+
+-- system_patches
+DELETE FROM `system_patches` WHERE `issue` = 'POCOR-4089';
+
+
 -- 3.10.7
 DELETE FROM system_updates WHERE version = (SELECT value FROM config_items WHERE code = 'db_version');
 UPDATE config_items SET value = '3.10.7' WHERE code = 'db_version';
