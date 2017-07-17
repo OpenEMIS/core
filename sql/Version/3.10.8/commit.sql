95c73f38331a4281fdc3234dcfe6160fe8d475f6
Steven Hasanuddin <shasanuddin@kordit.com>
2017-07-17 15:50:11 +0800

    3.10.8 - Updated master sql files

--- sql/Version/3.10.8/commit.sql
+++ sql/Version/3.10.8/commit.sql
@@ -1,3 +1,12 @@
+-- POCOR-3536
+-- system_patches
+INSERT INTO `system_patches` (`issue`, `created`) VALUES ('POCOR-3536', NOW());
+
+-- report_progress
+ALTER TABLE `report_progress`
+ MODIFY COLUMN `name` varchar(200) COLLATE utf8_general_ci NOT NULL;
+
+
 -- POCOR-4089
 -- system_patches
 INSERT INTO `system_patches` (`issue`, `created`) VALUES ('POCOR-4089', NOW());
