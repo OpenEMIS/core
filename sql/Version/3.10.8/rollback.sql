c07020a1ed143fcf80e0a49d904d2851dc733c7d
Steven Hasanuddin <shasanuddin@kordit.com>
2017-07-26 13:23:05 +0800

    3.10.8 - Updated master sql files

--- sql/Version/3.10.8/rollback.sql
+++ sql/Version/3.10.8/rollback.sql
@@ -1,21 +1,3 @@
--- POCOR-3536
--- report_progress
-ALTER TABLE `report_progress`
- MODIFY COLUMN `name` varchar(100) COLLATE utf8_general_ci NOT NULL;
-
--- system_patches
-DELETE FROM `system_patches` WHERE `issue` = 'POCOR-3536';
-
-
--- POCOR-4089
--- education_grades
-DROP TABLE IF EXISTS `education_grades`;
-RENAME TABLE `z_4089_education_grades` TO `education_grades`;
-
--- system_patches
-DELETE FROM `system_patches` WHERE `issue` = 'POCOR-4089';
-
-
 -- 3.10.7
 DELETE FROM system_updates WHERE version = (SELECT value FROM config_items WHERE code = 'db_version');
 UPDATE config_items SET value = '3.10.7' WHERE code = 'db_version';
