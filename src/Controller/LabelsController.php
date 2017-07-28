4f9a43d5d43c40c44f750f5843303a23de471f2c
Harry Suhendra <hsuhendra@kordit.com>
2017-07-28 11:18:34 +0800

    POCOR-4095 - Code is not mandatory field, add allowedEmpty on validation. - Remove bracket on the code field. - Adding institution_rooms, institution_floor, institution_building and institution_lands to the administrator > labels. - Fixed the labels breadcrumb. - Fixed When error occured the code field resented to the entity value.

--- src/Controller/LabelsController.php
+++ src/Controller/LabelsController.php
@@ -17,10 +17,11 @@ class LabelsController extends PageController
 
     public function beforeFilter(Event $event)
     {
-        parent::beforeFilter($event);
-        $this->Navigation->addCrumb('Labels', ['plugin' => false, 'controller' => 'Labels', 'action' => 'index']);
-
         $page = $this->Page;
+
+        parent::beforeFilter($event);
+        $page->addCrumb('Labels', ['plugin' => false, 'controller' => 'Labels', 'action' => 'index']);
+
         $page->exclude(['module', 'field', 'visible']);
     }
 
@@ -29,6 +30,13 @@ class LabelsController extends PageController
         $page = $this->Page;
         $page->get('module_name')->setDisabled(true);
         $page->get('field_name')->setDisabled(true);
+
+        // to keep the value when there is an error occured
+        if (array_key_exists('Labels', $this->request['data']) && !empty($this->request['data']['Labels']['code'])) {
+            $code = $this->request['data']['Labels']['code'];
+            $page->get('code')->setValue($code);
+        }
+
         parent::edit($id);
     }
 }
