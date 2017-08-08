66721f2c56bb204b349ec31086f761818a979037
Jeff Zheng <jzheng@kordit.com>
2017-08-08 11:36:45 +0800

    Fixed Labels test case

--- tests/TestCase/Controller/LabelsControllerTest.php
+++ tests/TestCase/Controller/LabelsControllerTest.php
@@ -8,15 +8,18 @@ class LabelsControllerTest extends AppTestCase
 {
     public $fixtures = [
         'app.config_items',
+        'app.config_product_lists',
         'app.labels',
         'app.security_users',
+        'app.user_identities',
+        'app.identity_types',
         'app.workflow_models',
         'app.workflow_steps',
         'app.workflow_statuses',
         'app.workflow_statuses_steps'
     ];
 
-    public function testLabelsIndex()
+    public function testIndex()
     {
         $this->get('/Labels');
         $this->assertResponseCode(200);
