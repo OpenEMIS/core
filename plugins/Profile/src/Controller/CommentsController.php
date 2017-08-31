6eb78eab23be679813cd20f2fdc39de687b6cd25
Livvi Sim <lsim@kordit.com>
2017-08-31 14:39:35 +0800

    POCOR-4151 - Set up base CommentsController and ProfileCommentsController for the Profile plugin

--- plugins/Profile/src/Controller/CommentsController.php
+++ plugins/Profile/src/Controller/CommentsController.php
@@ -2,7 +2,6 @@
 namespace Profile\Controller;
 
 use Cake\Event\Event;
-
 use App\Controller\PageController;
 
 class CommentsController extends PageController
@@ -14,33 +13,19 @@ class CommentsController extends PageController
         $this->loadModel('Security.Users');
         $this->loadModel('User.UserComments');
         $this->Page->loadElementsFromTable($this->UserComments);
-        $this->Page->disable(['add', 'delete', 'edit']);
     }
 
     public function beforeFilter(Event $event)
     {
         $page = $this->Page;
-        $plugin = $this->plugin;
-        $securityUserId = $this->Auth->user('id');
-        $securityUserName = $this->Auth->user('name');
 
         parent::beforeFilter($event);
 
         $page->exclude(['security_user_id']);
         $page->get('comment_date')->setLabel('Date');
 
-        // set Breadcrumb
-        $this->setBreadCrumb($securityUserId, $securityUserName, $plugin);
-
-        // set header
-        $header = $page->getHeader();
-        $page->setHeader($securityUserName . ' - ' . $header);
-
-        // set TabElement
+        // setup Tabs with incomplete url
         $this->setupTabElements();
-
-        // set queryString
-        $page->setQueryString('security_user_id', $securityUserId);
     }
 
     public function index()
@@ -52,7 +37,7 @@ class CommentsController extends PageController
         $page->move('comment_type_id')->after('comment_date');
 
         // set default ordering
-        // $page->setQueryOption('order', [$this->Comments->aliasField('date') => 'DESC']);
+        $page->setQueryOption('order', [$this->UserComments->aliasField('comment_date') => 'DESC']);
 
         parent::index();
     }
@@ -120,160 +105,32 @@ class CommentsController extends PageController
         }
     }
 
-    private function setupTabElements()
+    public function setupTabElements()
     {
-        // set tabElement
         $page = $this->Page;
-        // $tabPlugin = $this->plugin;
-        $tabPlugin = 'Profile';
-        $tabController = 'Profiles';
 
-        $loginUserId = $this->Auth->user('id');
-        $loginUserData = $this->Users->get($loginUserId);
-        $loginUserNationalityId = $loginUserData->nationality_id;
+        $tabElements = [
+            'Overview' => ['text' => __('Overview')],
+            'Accounts' => ['text' => __('Account')],
+            'Identities' => ['text' => __('Identities')],
+            'Nationalities' =>['text' =>  __('Nationalities')],
+            'Contacts' => ['text' => __('Contacts')],
+            'Languages' => ['text' => __('Languages')],
+            'SpecialNeeds' =>['text' =>  __('Special Needs')],
+            'Attachments' => ['text' => __('Attachments')],
+            'Comments' => ['text' => __('Comments')],
+            'History' => ['text' => __('History')]
+        ];
 
-        $encodedLoginUserId = $this->paramsEncode(['id' => $loginUserId]);
-
-        // overviewTab
-        if ($this->AccessControl->check([$tabController, 'Profiles', 'index'])) {
-            $overviewTab = $page->addTab('Overview');
-            $overviewTab->setTitle('Overview');
-            $url = [
-                'plugin' => $tabPlugin,
-                'controller' => $tabController,
-                'action' => 'Profiles',
-                'view',
-                $encodedLoginUserId
-            ];
-            $overviewTab->setUrl($url);
+        foreach ($tabElements as $action => $options) {
+            // only set subaction in url
+            $subaction = ($action == 'Overview' || $action == 'Accounts') ? 'view' : 'index';
+            $page->addTab($action)
+                ->setTitle($options['text'])
+                ->setUrl(['0' => $subaction]);
         }
 
-        // accountTab
-        if ($this->AccessControl->check([$tabController, 'Accounts', 'index'])) {
-            $accountTab = $page->addTab('Account');
-            $accountTab->setTitle('Account');
-            $url = [
-                'plugin' => $tabPlugin,
-                'controller' => $tabController,
-                'action' => 'Accounts',
-                'view',
-                $encodedLoginUserId
-            ];
-            $accountTab->setUrl($url);
-        }
-
-        // identitiesTab
-        if ($this->AccessControl->check([$tabController, 'Identities', 'index'])) {
-            $identitiesTab = $page->addTab('Identities');
-            $identitiesTab->setTitle('Identities');
-            $url = [
-                'plugin' => $tabPlugin,
-                'controller' => $tabController,
-                'action' => 'Identities',
-                'index',
-                $encodedLoginUserId
-            ];
-            $identitiesTab->setUrl($url);
-        }
-
-        // nationalitiesTab
-        if ($this->AccessControl->check([$tabController, 'Nationalities', 'index'])) {
-            $nationalitiesTab = $page->addTab('Nationalities');
-            $nationalitiesTab->setTitle('Nationalities');
-            $url = [
-                'plugin' => $tabPlugin,
-                'controller' => $tabController,
-                'action' => 'Nationalities',
-                'index',
-                $this->paramsEncode([
-                    'security_user_id' => $loginUserId,
-                    'nationality_id' => $loginUserNationalityId
-                ])
-            ];
-            $nationalitiesTab->setUrl($url);
-        }
-
-        // contactsTab
-        if ($this->AccessControl->check([$tabController, 'Contacts', 'index'])) {
-            $contactsTab = $page->addTab('Contacts');
-            $contactsTab->setTitle('Contacts');
-            $url = [
-                'plugin' => $tabPlugin,
-                'controller' => $tabController,
-                'action' => 'Contacts',
-                'index',
-                $encodedLoginUserId
-            ];
-            $contactsTab->setUrl($url);
-        }
-
-        // languagesTab
-        if ($this->AccessControl->check([$tabController, 'Languages', 'index'])) {
-            $languagesTab = $page->addTab('Languages');
-            $languagesTab->setTitle('Languages');
-            $url = [
-                'plugin' => $tabPlugin,
-                'controller' => $tabController,
-                'action' => 'Languages',
-                'index',
-                $encodedLoginUserId
-            ];
-            $languagesTab->setUrl($url);
-        }
-
-        // commentsTab
-        $commentsTab = $page->addTab('Comments');
-        $commentsTab->setTitle('Comments');
-        $url = [
-                'plugin' => $tabPlugin,
-                'controller' => 'Comments',
-                'action' => 'index',
-                $encodedLoginUserId
-            ];
-        $commentsTab->setUrl($url);
-        $commentsTab->setActive('true');
-
-        // attachmentsTab
-        if ($this->AccessControl->check([$tabController, 'Attachments', 'index'])) {
-            $attachmentsTab = $page->addTab('Attachments');
-            $attachmentsTab->setTitle('Attachments');
-            $url = [
-                'plugin' => $tabPlugin,
-                'controller' => $tabController,
-                'action' => 'Attachments',
-                'index',
-                $encodedLoginUserId
-            ];
-            $attachmentsTab->setUrl($url);
-        }
-
-        // specialneedsTab
-        if ($this->AccessControl->check([$tabController, 'SpecialNeeds', 'index'])) {
-            $specialneedsTab = $page->addTab('SpecialNeeds');
-            $specialneedsTab->setTitle('Special Needs');
-            $url = [
-                'plugin' => $tabPlugin,
-                'controller' => $tabController,
-                'action' => 'SpecialNeeds',
-                'index',
-                $encodedLoginUserId
-            ];
-            $specialneedsTab->setUrl($url);
-        }
-
-        // historyTab
-        if ($this->AccessControl->check([$tabController, 'Histories', 'index'])) {
-            $historyTab = $page->addTab('History');
-            $historyTab->setTitle('History');
-            $url = [
-                'plugin' => $tabPlugin,
-                'controller' => $tabController,
-                'action' => 'History',
-                'index',
-                $encodedLoginUserId
-            ];
-            $historyTab->setUrl($url);
-        }
-        // end of set Tab
+        // set active tab
+        $page->getTab('Comments')->setActive('true');
     }
 }
