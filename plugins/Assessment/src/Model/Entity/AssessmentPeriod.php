7b03cc9857596903761fbb1d8b4d3f6bd59bc770
Jeff Zheng <jzheng@kordit.com>
2017-09-21 17:58:03 +0800

    POCOR-4200 - fixed coding syntax

--- plugins/Assessment/src/Model/Entity/AssessmentPeriod.php
+++ plugins/Assessment/src/Model/Entity/AssessmentPeriod.php
@@ -7,21 +7,22 @@ use DateTimeInterface;
 
 class AssessmentPeriod extends Entity
 {
-	protected $_virtual = ['editable'];
+    protected $_virtual = ['editable'];
 
-    protected function _getEditable() {
-    	$dateToday = date('Y-m-d');
-		$dateEnabled = $this->date_enabled;
-		$dateDisabled = $this->date_disabled;
+    protected function _getEditable()
+    {
+        $dateToday = date('Y-m-d');
+        $dateEnabled = $this->date_enabled;
+        $dateDisabled = $this->date_disabled;
 
-		if ($dateEnabled instanceof DateTimeInterface && $dateDisabled instanceof DateTimeInterface) {
-			return ($dateToday >= $dateEnabled->format('Y-m-d') && $dateToday <= $dateDisabled->format('Y-m-d')) ? 1 : 0;
-		} else {
-			$today = new Date();
-			$dateEnabled = Date::createFromFormat("d/m/Y", $this->date_enabled);
-			$dateDisabled = Date::createFromFormat("d/m/Y", $this->date_disabled);
+        if ($dateEnabled instanceof DateTimeInterface && $dateDisabled instanceof DateTimeInterface) {
+            return ($dateToday >= $dateEnabled->format('Y-m-d') && $dateToday <= $dateDisabled->format('Y-m-d')) ? 1 : 0;
+        } else {
+            $today = new Date();
+            $dateEnabled = Date::createFromFormat("d/m/Y", $this->date_enabled);
+            $dateDisabled = Date::createFromFormat("d/m/Y", $this->date_disabled);
 
             return $today->between($dateEnabled, $dateDisabled);
-		}
-	}
+        }
+    }
 }
