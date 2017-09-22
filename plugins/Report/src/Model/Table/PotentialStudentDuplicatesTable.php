6c9f2819010038e96652889d22e4b500ec061f86
Livvi Sim <lsim@kordit.com>
2017-09-22 10:15:25 +0800

    POCOR-3710 - Refactor code and add institution and education grade columns

--- plugins/Report/src/Model/Table/PotentialStudentDuplicatesTable.php
+++ plugins/Report/src/Model/Table/PotentialStudentDuplicatesTable.php
@@ -7,6 +7,7 @@ use Cake\ORM\Query;
 use Cake\Event\Event;
 use Cake\Network\Request;
 use App\Model\Table\AppTable;
+use Cake\ORM\TableRegistry;
 
 class PotentialStudentDuplicatesTable extends AppTable  {
 	public function initialize(array $config) {
@@ -37,15 +38,105 @@ class PotentialStudentDuplicatesTable extends AppTable  {
 		return $attr;
 	}
 
-	public function onExcelBeforeQuery(Event $event, ArrayObject $settings, Query $query) {
-		$query
-			->where([$this->aliasField('is_student') => 1])
-			->group([
-				$this->aliasField('first_name'),
-				$this->aliasField('last_name'),
-				$this->aliasField('gender_id'),
-				$this->aliasField('date_of_birth')
+	public function onExcelBeforeQuery(Event $event, ArrayObject $settings, Query $query)
+	{
+		$Students = TableRegistry::get('Institution.Students');
+		$Institutions = TableRegistry::get('Institution.Institutions');
+		$EducationGrades = TableRegistry::get('Education.EducationGrades');
+
+		$duplicateStudentSubquery = $this->find()
+			->select([
+				'first_name' => 'first_name',
+				'last_name' => 'last_name',
+				'gender_id' => 'gender_id',
+				'date_of_birth' => 'date_of_birth'
 			])
+			->where(['is_student' => 1])
+			->group(['first_name', 'last_name', 'gender_id', 'date_of_birth'])
 			->having(['COUNT(*) > ' => 1]);
+
+		$latestStudentSubquery = $this->find()
+			->select([$this->find()->func()->max('latest_student.created')])
+			->from(['latest_student' => 'institution_students'])
+			->where(['latest_student.student_id = ' . $Students->aliasField('student_id')])
+			->group('latest_student.student_id');
+
+		$query
+			->select([
+				$this->aliasField('username'),
+				$this->aliasField('openemis_no'),
+				$this->aliasField('first_name'),
+				$this->aliasField('middle_name'),
+				$this->aliasField('third_name'),
+				$this->aliasField('last_name'),
+				$this->aliasField('preferred_name'),
+				$this->aliasField('email'),
+				$this->aliasField('address'),
+				$this->aliasField('postal_code'),
+				$this->aliasField('address_area_id'),
+				$this->aliasField('birthplace_area_id'),
+				$this->aliasField('gender_id'),
+				$this->aliasField('date_of_birth'),
+				$this->aliasField('date_of_death'),
+				$this->aliasField('nationality_id'),
+				$this->aliasField('identity_type_id'),
+				$this->aliasField('identity_number'),
+				'institution_code' => $Institutions->aliasField('code'),
+				'institution_name' => $Institutions->aliasField('name'),
+				'education_grade' => $EducationGrades->aliasField('name')
+			])
+		    ->innerJoin(['DuplicateStudents' => $duplicateStudentSubquery], [
+		    	'DuplicateStudents.first_name = ' . $this->aliasField('first_name'),
+		    	'DuplicateStudents.last_name = ' . $this->aliasField('last_name'),
+		    	'DuplicateStudents.gender_id = ' . $this->aliasField('gender_id'),
+		    	'DuplicateStudents.date_of_birth = ' . $this->aliasField('date_of_birth')
+		    ])
+		    ->leftJoin([$Students->alias() => $Students->table()], [
+		    	$Students->aliasField('student_id') . ' = ' . $this->aliasField('id'),
+                $Students->aliasField('created') => $latestStudentSubquery
+		    ])
+			->leftJoin([$Institutions->alias() => $Institutions->table()], [
+				$Institutions->aliasField('id') . ' = ' . $Students->aliasField('institution_id')
+			])
+			->leftJoin([$EducationGrades->alias() => $EducationGrades->table()], [
+				$EducationGrades->aliasField('id') . ' = ' . $Students->aliasField('education_grade_id')
+			]);
 	}
+
+    public function onExcelUpdateFields(Event $event, ArrayObject $settings, $fields)
+    {
+        // set formatting to string for certain columns
+        $cloneFields = $fields->getArrayCopy();
+        $stringFormatColumns = ['username', 'openemis_no', 'email', 'postal_code', 'identity_number'];
+        foreach ($cloneFields as $key => $value) {
+            if (in_array($value['field'], $stringFormatColumns)) {
+                $cloneFields[$key]['formatting'] = 'string';
+            }
+        }
+
+        $extraFields = [];
+        $extraFields[] = [
+            'key' => 'Institutions.code',
+            'field' => 'institution_code',
+            'type' => 'string',
+            'label' => __('Institution Code')
+        ];
+
+        $extraFields[] = [
+            'key' => 'Institutions.name',
+            'field' => 'institution_name',
+            'type' => 'string',
+            'label' => __('Institution')
+        ];
+
+        $extraFields[] = [
+            'key' => 'EducationGrades.name',
+            'field' => 'education_grade',
+            'type' => 'string',
+            'label' => __('Education Grade')
+        ];
+
+        $newFields = array_merge($cloneFields, $extraFields);
+        $fields->exchangeArray($newFields);
+    }
 }
