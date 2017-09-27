32de72df2ba08f45c567115c4e04974de1b5faf4
Martinnus Budiarto <mbudiarto@kordit.com>
2017-09-27 11:26:11 +0800

    POCOR-3874 : * display the nationality and identity type on institution > student > add page * fix search by date of birth * fix search by identity number to read identity number from user table * disable search by identity number to user identity table

--- plugins/Institution/src/Model/Table/StudentUserTable.php
+++ plugins/Institution/src/Model/Table/StudentUserTable.php
@@ -610,25 +610,29 @@ class StudentUserTable extends ControllerActionTable
             $conditions['openemis_no LIKE'] = $openemisNo . '%';
         }
         if (!empty($dateOfBirth)) {
-            $conditions['date_of_birth'] = $dateOfBirth;
+            $conditions['date_of_birth'] = date_create($dateOfBirth)->format('Y-m-d');;
         }
-
-        $identityConditions = [];
         if (!empty($identityNumber)) {
-            $identityConditions['Identities.number LIKE'] = $identityNumber . '%';
+            $conditions['identity_number LIKE'] = $identityNumber . '%';
         }
+        
+        //disabled search through entire user identity record.
+        // $identityConditions = [];
+        // if (!empty($identityNumber)) {
+        //     $identityConditions['Identities.number LIKE'] = $identityNumber . '%';
+        // }
 
-        $identityJoinType = (empty($identityNumber))? 'LEFT': 'INNER';
-        $query->join([
-            [
-                'type' => $identityJoinType,
-                'table' => 'user_identities',
-                'alias' => 'Identities',
-                'conditions' => array_merge([
-                        'Identities.security_user_id = ' . $this->aliasField('id')
-                    ], $identityConditions)
-            ]
-        ]);
+        // $identityJoinType = (empty($identityNumber))? 'LEFT': 'INNER';
+        // $query->join([
+        //     [
+        //         'type' => $identityJoinType,
+        //         'table' => 'user_identities',
+        //         'alias' => 'Identities',
+        //         'conditions' => array_merge([
+        //                 'Identities.security_user_id = ' . $this->aliasField('id')
+        //             ], $identityConditions)
+        //     ]
+        // ]);
 
         $query->group([$this->aliasField('id')]);
 
