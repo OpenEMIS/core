19ee1b8e24f36f8339f2f32db3befca21df005d9
Chiang Hao Long <hchiang@kordit.com>
2017-09-27 16:46:50 +0800

    POCOR-4203 - Remove check staff fte validation

--- plugins/Institution/src/Model/Behavior/StaffValidationBehavior.php
+++ plugins/Institution/src/Model/Behavior/StaffValidationBehavior.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 namespace Institution\Model\Behavior;
 
 use Cake\ORM\Behavior;
@@ -6,24 +6,26 @@ use Cake\ORM\Query;
 use Cake\ORM\TableRegistry;
 use Cake\Validation\Validator;
 
-class StaffValidationBehavior extends Behavior {
-	public function buildStaffValidation() {
-		$validator = new Validator();
-		return $validator
-			->allowEmpty('end_date')
-			->add('end_date', 'ruleCompareDateReverse', [
-		        'rule' => ['compareDateReverse', 'start_date', true]
-	    	])
-	    	->add('start_date', 'ruleCheckStaffFTE', [
-				'rule' => ['checkStaffFTE'],
-			])
-			->add('start_date', 'ruleStaffExistWithinPeriod', [
-				'rule' => ['checkStaffExistWithinPeriod'],
-				'on' => 'update'
-			])
-			->add('institution_position_id', 'ruleCheckFTE', [
-				'rule' => ['checkFTE'],
-			])
-		;
-	}
+class StaffValidationBehavior extends Behavior
+{
+    public function buildStaffValidation()
+    {
+        $validator = new Validator();
+        return $validator
+            ->allowEmpty('end_date')
+            ->add('end_date', 'ruleCompareDateReverse', [
+                'rule' => ['compareDateReverse', 'start_date', true]
+            ])
+            // ->add('start_date', 'ruleCheckStaffFTE', [
+            // 	'rule' => ['checkStaffFTE'],
+            // ])
+            ->add('start_date', 'ruleStaffExistWithinPeriod', [
+                'rule' => ['checkStaffExistWithinPeriod'],
+                'on' => 'update'
+            ])
+            ->add('institution_position_id', 'ruleCheckFTE', [
+                'rule' => ['checkFTE'],
+            ])
+        ;
+    }
 }
