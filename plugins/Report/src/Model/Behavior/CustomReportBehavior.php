87d2018fc7538dae43f2bb3a81a854c88a49552b
Livvi Sim <lsim@kordit.com>
2017-09-28 18:13:34 +0800

    POCOR-4198 - Fix select function when using arabic text

--- plugins/Report/src/Model/Behavior/CustomReportBehavior.php
+++ plugins/Report/src/Model/Behavior/CustomReportBehavior.php
@@ -229,7 +229,8 @@ class CustomReportBehavior extends Behavior
             $select = [];
 
             foreach($values as $field => $value) {
-                $select[$field] = $query->newExpr()->add($value);
+                // manually append backticks to column alias to cater for arabic text
+                $select['`'.$field.'`'] = $query->newExpr()->add($value);
             }
 
             $query->select($select);
