a0cfdedfc61eb1befd486a6a30caf6dca5db7a54
Livvi Sim <lsim@kordit.com>
2017-10-04 10:17:36 +0800

    POCOR-3710 - Fix formatting

--- plugins/Report/src/Model/Table/PotentialStudentDuplicatesTable.php
+++ plugins/Report/src/Model/Table/PotentialStudentDuplicatesTable.php
@@ -11,102 +11,102 @@ use Cake\ORM\TableRegistry;
 
 class PotentialStudentDuplicatesTable extends AppTable
 {
-	public function initialize(array $config)
+    public function initialize(array $config)
     {
-		$this->table('security_users');
-		parent::initialize($config);
-		
-		$this->belongsTo('Genders', ['className' => 'User.Genders']);
-		$this->belongsTo('AddressAreas', ['className' => 'Area.AreaAdministratives', 'foreignKey' => 'address_area_id']);
-		$this->belongsTo('BirthplaceAreas', ['className' => 'Area.AreaAdministratives', 'foreignKey' => 'birthplace_area_id']);
-		$this->belongsTo('MainNationalities', ['className' => 'FieldOption.Nationalities', 'foreignKey' => 'nationality_id']);
-		$this->belongsTo('MainIdentityTypes', ['className' => 'FieldOption.IdentityTypes', 'foreignKey' => 'identity_type_id']);
-		
-		$this->addBehavior('Excel', [
-			'excludes' => ['is_student', 'is_staff', 'is_guardian', 'external_reference', 'super_admin', 'status', 'last_login', 'photo_name', 'photo_content', 'preferred_language'],
-			'pages' => false
-		]);
-		$this->addBehavior('Report.ReportList');
-	}
+        $this->table('security_users');
+        parent::initialize($config);
 
-	public function beforeAction(Event $event)
+        $this->belongsTo('Genders', ['className' => 'User.Genders']);
+        $this->belongsTo('AddressAreas', ['className' => 'Area.AreaAdministratives', 'foreignKey' => 'address_area_id']);
+        $this->belongsTo('BirthplaceAreas', ['className' => 'Area.AreaAdministratives', 'foreignKey' => 'birthplace_area_id']);
+        $this->belongsTo('MainNationalities', ['className' => 'FieldOption.Nationalities', 'foreignKey' => 'nationality_id']);
+        $this->belongsTo('MainIdentityTypes', ['className' => 'FieldOption.IdentityTypes', 'foreignKey' => 'identity_type_id']);
+
+        $this->addBehavior('Excel', [
+            'excludes' => ['is_student', 'is_staff', 'is_guardian', 'external_reference', 'super_admin', 'status', 'last_login', 'photo_name', 'photo_content', 'preferred_language'],
+            'pages' => false
+        ]);
+        $this->addBehavior('Report.ReportList');
+    }
+
+    public function beforeAction(Event $event)
     {
-		$this->fields = [];
-		$this->ControllerAction->field('feature');
-		$this->ControllerAction->field('format');
-	}
+        $this->fields = [];
+        $this->ControllerAction->field('feature');
+        $this->ControllerAction->field('format');
+    }
 
-	public function onUpdateFieldFeature(Event $event, array $attr, $action, Request $request)
+    public function onUpdateFieldFeature(Event $event, array $attr, $action, Request $request)
     {
-		$attr['options'] = $this->controller->getFeatureOptions($this->alias());
-		return $attr;
-	}
+        $attr['options'] = $this->controller->getFeatureOptions($this->alias());
+        return $attr;
+    }
 
-	public function onExcelBeforeQuery(Event $event, ArrayObject $settings, Query $query)
-	{
-		$Students = TableRegistry::get('Institution.Students');
-		$Institutions = TableRegistry::get('Institution.Institutions');
-		$EducationGrades = TableRegistry::get('Education.EducationGrades');
+    public function onExcelBeforeQuery(Event $event, ArrayObject $settings, Query $query)
+    {
+        $Students = TableRegistry::get('Institution.Students');
+        $Institutions = TableRegistry::get('Institution.Institutions');
+        $EducationGrades = TableRegistry::get('Education.EducationGrades');
 
-		$duplicateStudentSubquery = $this->find()
-			->select([
-				'first_name' => 'first_name',
-				'last_name' => 'last_name',
-				'gender_id' => 'gender_id',
-				'date_of_birth' => 'date_of_birth'
-			])
-			->where(['is_student' => 1])
-			->group(['first_name', 'last_name', 'gender_id', 'date_of_birth'])
-			->having(['COUNT(*) > ' => 1]);
+        $duplicateStudentSubquery = $this->find()
+            ->select([
+                'first_name' => 'first_name',
+                'last_name' => 'last_name',
+                'gender_id' => 'gender_id',
+                'date_of_birth' => 'date_of_birth'
+            ])
+            ->where(['is_student' => 1])
+            ->group(['first_name', 'last_name', 'gender_id', 'date_of_birth'])
+            ->having(['COUNT(*) > ' => 1]);
 
-		$latestStudentSubquery = $this->find()
-			->select([$this->find()->func()->max('latest_student.created')])
-			->from(['latest_student' => 'institution_students'])
-			->where(['latest_student.student_id = ' . $Students->aliasField('student_id')])
-			->group('latest_student.student_id');
+        $latestStudentSubquery = $this->find()
+            ->select([$this->find()->func()->max('latest_student.created')])
+            ->from(['latest_student' => 'institution_students'])
+            ->where(['latest_student.student_id = ' . $Students->aliasField('student_id')])
+            ->group('latest_student.student_id');
 
-		$query
-			->select([
-				$this->aliasField('username'),
-				$this->aliasField('openemis_no'),
-				$this->aliasField('first_name'),
-				$this->aliasField('middle_name'),
-				$this->aliasField('third_name'),
-				$this->aliasField('last_name'),
-				$this->aliasField('preferred_name'),
-				$this->aliasField('email'),
-				$this->aliasField('address'),
-				$this->aliasField('postal_code'),
-				$this->aliasField('address_area_id'),
-				$this->aliasField('birthplace_area_id'),
-				$this->aliasField('gender_id'),
-				$this->aliasField('date_of_birth'),
-				$this->aliasField('date_of_death'),
-				$this->aliasField('nationality_id'),
-				$this->aliasField('identity_type_id'),
-				$this->aliasField('identity_number'),
-				'institution_code' => $Institutions->aliasField('code'),
-				'institution_name' => $Institutions->aliasField('name'),
-				'education_grade' => $EducationGrades->aliasField('name')
-			])
-		    ->innerJoin(['DuplicateStudents' => $duplicateStudentSubquery], [
-		    	'DuplicateStudents.first_name = ' . $this->aliasField('first_name'),
-		    	'DuplicateStudents.last_name = ' . $this->aliasField('last_name'),
-		    	'DuplicateStudents.gender_id = ' . $this->aliasField('gender_id'),
-		    	'DuplicateStudents.date_of_birth = ' . $this->aliasField('date_of_birth')
-		    ])
-		    ->leftJoin([$Students->alias() => $Students->table()], [
-		    	$Students->aliasField('student_id') . ' = ' . $this->aliasField('id'),
-                $Students->aliasField('created') => $latestStudentSubquery
-		    ])
-			->leftJoin([$Institutions->alias() => $Institutions->table()], [
-				$Institutions->aliasField('id') . ' = ' . $Students->aliasField('institution_id')
-			])
-			->leftJoin([$EducationGrades->alias() => $EducationGrades->table()], [
-				$EducationGrades->aliasField('id') . ' = ' . $Students->aliasField('education_grade_id')
-			])
-            ->group([$this->aliasField('id')]);
-	}
+        $query
+        ->select([
+            $this->aliasField('username'),
+            $this->aliasField('openemis_no'),
+            $this->aliasField('first_name'),
+            $this->aliasField('middle_name'),
+            $this->aliasField('third_name'),
+            $this->aliasField('last_name'),
+            $this->aliasField('preferred_name'),
+            $this->aliasField('email'),
+            $this->aliasField('address'),
+            $this->aliasField('postal_code'),
+            $this->aliasField('address_area_id'),
+            $this->aliasField('birthplace_area_id'),
+            $this->aliasField('gender_id'),
+            $this->aliasField('date_of_birth'),
+            $this->aliasField('date_of_death'),
+            $this->aliasField('nationality_id'),
+            $this->aliasField('identity_type_id'),
+            $this->aliasField('identity_number'),
+            'institution_code' => $Institutions->aliasField('code'),
+            'institution_name' => $Institutions->aliasField('name'),
+            'education_grade' => $EducationGrades->aliasField('name')
+        ])
+        ->innerJoin(['DuplicateStudents' => $duplicateStudentSubquery], [
+            'DuplicateStudents.first_name = ' . $this->aliasField('first_name'),
+            'DuplicateStudents.last_name = ' . $this->aliasField('last_name'),
+            'DuplicateStudents.gender_id = ' . $this->aliasField('gender_id'),
+            'DuplicateStudents.date_of_birth = ' . $this->aliasField('date_of_birth')
+        ])
+        ->leftJoin([$Students->alias() => $Students->table()], [
+            $Students->aliasField('student_id') . ' = ' . $this->aliasField('id'),
+            $Students->aliasField('created') => $latestStudentSubquery
+        ])
+        ->leftJoin([$Institutions->alias() => $Institutions->table()], [
+            $Institutions->aliasField('id') . ' = ' . $Students->aliasField('institution_id')
+        ])
+        ->leftJoin([$EducationGrades->alias() => $EducationGrades->table()], [
+            $EducationGrades->aliasField('id') . ' = ' . $Students->aliasField('education_grade_id')
+        ])
+        ->group([$this->aliasField('id')]);
+    }
 
     public function onExcelUpdateFields(Event $event, ArrayObject $settings, $fields)
     {
