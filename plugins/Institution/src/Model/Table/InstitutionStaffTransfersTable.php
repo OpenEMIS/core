7ed9301ef4c04bced437502e2599fab2a70816fe
Livvi Sim <lsim@kordit.com>
2017-10-05 15:09:49 +0800

    POCOR-3997 - Rename columns in migrations. Clean up InstitutionStaffTransfersTable. Add StaffTransferInTable and StaffTransferOutTable

--- plugins/Institution/src/Model/Table/InstitutionStaffTransfersTable.php
+++ plugins/Institution/src/Model/Table/InstitutionStaffTransfersTable.php
@@ -5,9 +5,6 @@ use ArrayObject;
 use Cake\Event\Event;
 use Cake\ORM\Entity;
 use Cake\ORM\Query;
-use Cake\ORM\TableRegistry;
-use Cake\I18n\Date;
-use Cake\Validation\Validator;
 use App\Model\Table\ControllerActionTable;
 
 class InstitutionStaffTransfersTable extends ControllerActionTable
@@ -17,10 +14,6 @@ class InstitutionStaffTransfersTable extends ControllerActionTable
     const IN_PROGRESS = 2;
     const DONE = 3;
 
-    // Transfer Type
-    const FULL_TRANSFER = 1;
-    const PARTIAL_TRANSFER = 2;
-
     // Initiated By
     const INCOMING = 1;
     const OUTGOING = 2;
@@ -29,16 +22,21 @@ class InstitutionStaffTransfersTable extends ControllerActionTable
     {
         parent::initialize($config);
         $this->belongsTo('Users', ['className' => 'Security.Users', 'foreignKey' => 'staff_id']);
-        $this->belongsTo('OutgoingInstitutions', ['className' => 'Institution.Institutions', 'foreignKey' => 'outgoing_institution_id']);
-        $this->belongsTo('IncomingInstitutions', ['className' => 'Institution.Institutions', 'foreignKey' => 'incoming_institution_id']);
+        $this->belongsTo('Institutions', ['className' => 'Institution.Institutions', 'foreignKey' => 'institution_id']);
+        $this->belongsTo('PreviousInstitutions', ['className' => 'Institution.Institutions', 'foreignKey' => 'previous_institution_id']);
         $this->belongsTo('Statuses', ['className' => 'Workflow.WorkflowSteps', 'foreignKey' => 'status_id']);
         $this->belongsTo('Assignees', ['className' => 'User.Users', 'foreignKey' => 'assignee_id']);
         $this->belongsTo('Staff', ['className' => 'Institution.Staff', 'foreignKey' => 'institution_staff_id']);
         $this->belongsTo('Positions', ['className' => 'Institution.InstitutionPositions', 'foreignKey' => 'institution_position_id']);
         $this->belongsTo('StaffTypes', ['className' => 'Staff.StaffTypes', 'foreignKey' => 'staff_type_id']);
 
-        $this->addBehavior('Workflow.Workflow');
+        $this->addBehavior('Workflow.Workflow', ['model' => 'Institution.InstitutionStaffTransfers']);
         $this->addBehavior('Institution.InstitutionWorkflowAccessControl');
         $this->addBehavior('OpenEmis.Section');
+    }
+
+    public function beforeAction(Event $event, ArrayObject $extra)
+    {
+        $this->field('institution_staff_id', ['type' => 'hidden']);
     }
 }
