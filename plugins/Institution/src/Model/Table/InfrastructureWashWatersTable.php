ea95c38d853c635837d6ab4230a6ba872d5b3d04
Harry Suhendra <hsuhendra@kordit.com>
2017-10-12 09:47:31 +0800

    POCOR-4216 - Adding the association with academic period. - Remove view and delete function in the controller, as not needed, the default function will take care the logic.

--- plugins/Institution/src/Model/Table/InfrastructureWashWatersTable.php
+++ plugins/Institution/src/Model/Table/InfrastructureWashWatersTable.php
@@ -17,6 +17,7 @@ class InfrastructureWashWatersTable extends AppTable
         $this->table('infrastructure_wash_waters');
         parent::initialize($config);
 
+        $this->belongsTo('AcademicPeriods',   ['className' => 'AcademicPeriod.AcademicPeriods', 'foreign_key' => 'academic_period_id']);
         $this->belongsTo('InfrastructureWashWaterTypes',   ['className' => 'Institution.InfrastructureWashWaterTypes', 'foreign_key' => 'infrastructure_wash_water_type_id']);
         $this->belongsTo('InfrastructureWashWaterFunctionalities',   ['className' => 'Institution.InfrastructureWashWaterFunctionalities', 'foreign_key' => 'infrastructure_wash_water_functionality_id']);
         $this->belongsTo('InfrastructureWashWaterProximities',   ['className' => 'Institution.InfrastructureWashWaterProximities', 'foreign_key' => 'infrastructure_wash_water_proximity_id']);
