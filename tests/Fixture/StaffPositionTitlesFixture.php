48c28212da716588f0221797bcba410450b00c5c
Livvi Sim <lsim@kordit.com>
2017-10-23 16:07:54 +0800

    POCOR-3997 - Add fixtures (WIP)

--- tests/Fixture/StaffPositionTitlesFixture.php
+++ tests/Fixture/StaffPositionTitlesFixture.php
@@ -8,6 +8,21 @@ class StaffPositionTitlesFixture extends TestFixture
     public $import = ['table' => 'staff_position_titles'];
     public $records = [
         [
+            'id' => '240',
+            'name' => 'Principal',
+            'type' => '0',
+            'security_role_id' => '4',
+            'order' => '10',
+            'visible' => '1',
+            'editable' => '1',
+            'default' => '0',
+            'international_code' => '01',
+            'national_code' => '01',
+            'modified_user_id' => '1',
+            'modified' => '2015-07-23 03:08:52',
+            'created_user_id' => '1',
+            'created' => '2014-09-29 08:16:10'
+        ], [
             'id' => '242',
             'name' => 'Teacher',
             'type' => '1',
@@ -22,7 +37,22 @@ class StaffPositionTitlesFixture extends TestFixture
             'modified' => '2015-07-10 18:55:50',
             'created_user_id' => '1',
             'created' => '2014-09-29 08:16:36'
-        ]
+        ], [
+            'id' => '465',
+            'name' => 'Assistant Teacher',
+            'type' => '1',
+            'security_role_id' => '6',
+            'order' => '10',
+            'visible' => '1',
+            'editable' => '1',
+            'default' => '0',
+            'international_code' => '09',
+            'national_code' => '09',
+            'modified_user_id' => '2',
+            'modified' => '2015-08-23 17:35:47',
+            'created_user_id' => '2',
+            'created' => '2015-07-23 03:16:22'
+        ],
     ];
 }
 
