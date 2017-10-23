6b4f8ddf42611234e8d191b974fbb01fda63e137
Livvi Sim <lsim@kordit.com>
2017-10-23 16:07:54 +0800

    POCOR-3997 - Add fixtures (WIP)

---
+++ tests/Fixture/WorkflowStepsParamsFixture.php
@@ -0,0 +1,183 @@
+<?php
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+class WorkflowStepsParamsFixture extends TestFixture
+{
+    public $import = ['table' => 'workflow_steps_params'];
+    public $records = [
+        [
+            'id' => '02fc61ec-35e2-4259-95ef-000b332c177e',
+            'workflow_step_id' => '43',
+            'name' => 'institution_visible',
+            'value' => '1'
+        ], [
+            'id' => '089ceb88-f3b6-4d24-b92d-47708af369d1',
+            'workflow_step_id' => '44',
+            'name' => 'institution_visible',
+            'value' => '1'
+        ], [
+            'id' => '08b680a8-a46e-4036-973c-8da490689d6b',
+            'workflow_step_id' => '45',
+            'name' => 'institution_visible',
+            'value' => '1'
+        ], [
+            'id' => '09924c48-4cf0-4c3e-b9eb-6c8f6094ecac',
+            'workflow_step_id' => '45',
+            'name' => 'institution_visible',
+            'value' => '2'
+        ], [
+            'id' => '0dbdab05-271a-444f-a208-4ecc3ae0760d',
+            'workflow_step_id' => '46',
+            'name' => 'institution_visible',
+            'value' => '1'
+        ], [
+            'id' => '16c003bb-3ec4-436f-887c-96d52267bf92',
+            'workflow_step_id' => '46',
+            'name' => 'institution_visible',
+            'value' => '2'
+        ], [
+            'id' => '1a5ec066-9b48-4fa6-81a7-3ea42bee9a34',
+            'workflow_step_id' => '47',
+            'name' => 'institution_visible',
+            'value' => '1'
+        ], [
+            'id' => '24e27677-208f-4864-9c34-b7572bd90290',
+            'workflow_step_id' => '47',
+            'name' => 'institution_visible',
+            'value' => '2'
+        ], [
+            'id' => '345bf0b0-6410-4de7-b2ed-8ae534c9ce4c',
+            'workflow_step_id' => '48',
+            'name' => 'institution_visible',
+            'value' => '1'
+        ], [
+            'id' => '3d0b2dd3-8f8d-46bc-817b-5ef0d584527e',
+            'workflow_step_id' => '48',
+            'name' => 'institution_visible',
+            'value' => '2'
+        ], [
+            'id' => '4a5fe255-19ef-4813-954b-a3f26853368c',
+            'workflow_step_id' => '43',
+            'name' => 'institution_owner',
+            'value' => '1'
+        ], [
+            'id' => '4b7b2657-859f-44cd-9aec-cbab6d07c631',
+            'workflow_step_id' => '44',
+            'name' => 'institution_owner',
+            'value' => '1'
+        ], [
+            'id' => '523cf417-a86a-472c-b6b8-8b124a78cc38',
+            'workflow_step_id' => '45',
+            'name' => 'institution_owner',
+            'value' => '2'
+        ], [
+            'id' => '5927b455-0596-404f-bca6-c3790741202e',
+            'workflow_step_id' => '46',
+            'name' => 'institution_owner',
+            'value' => '1'
+        ], [
+            'id' => '5b15b2d0-cff3-4267-a33b-f2fb8337a425',
+            'workflow_step_id' => '47',
+            'name' => 'institution_owner',
+            'value' => '1'
+        ], [
+            'id' => '65e6f681-54b8-4ef8-a026-1dc751d571b1',
+            'workflow_step_id' => '48',
+            'name' => 'institution_owner',
+            'value' => '1'
+        ], [
+            'id' => '75b568a4-c9a1-49c5-92a8-bd401e283536',
+            'workflow_step_id' => '45',
+            'name' => 'validate_approve',
+            'value' => '1'
+        ], [
+            'id' => '790df69f-e3d3-4284-9ed5-bac2e3e8df54',
+            'workflow_step_id' => '49',
+            'name' => 'institution_owner',
+            'value' => '2'
+        ], [
+            'id' => '891ed121-2630-4c63-9043-5dd266d35fc6',
+            'workflow_step_id' => '50',
+            'name' => 'institution_owner',
+            'value' => '2'
+        ], [
+            'id' => '8a65ee5f-19c2-4386-994e-9b098f59383b',
+            'workflow_step_id' => '51',
+            'name' => 'institution_owner',
+            'value' => '1'
+        ], [
+            'id' => '9c1db1a0-f50d-4d24-9ec2-839487869355',
+            'workflow_step_id' => '52',
+            'name' => 'institution_owner',
+            'value' => '2'
+        ], [
+            'id' => 'baac00eb-549c-4948-8ee4-b2259d230bb4',
+            'workflow_step_id' => '53',
+            'name' => 'institution_owner',
+            'value' => '2'
+        ], [
+            'id' => 'bb561489-568e-4808-b498-d2c1c60fb4f0',
+            'workflow_step_id' => '54',
+            'name' => 'institution_owner',
+            'value' => '2'
+        ], [
+            'id' => 'cbec5a6a-af1a-4537-a705-68a6e390f700',
+            'workflow_step_id' => '49',
+            'name' => 'institution_visible',
+            'value' => '2'
+        ], [
+            'id' => 'd4a207ce-34f5-450b-ab08-93b458024171',
+            'workflow_step_id' => '50',
+            'name' => 'institution_visible',
+            'value' => '2'
+        ], [
+            'id' => 'db1e8a82-6796-4ebe-9ed9-8541c2492611',
+            'workflow_step_id' => '51',
+            'name' => 'institution_visible',
+            'value' => '1'
+        ], [
+            'id' => 'dcfb609a-4133-40b3-8a32-eae8d68c2740',
+            'workflow_step_id' => '51',
+            'name' => 'institution_visible',
+            'value' => '2'
+        ], [
+            'id' => 'dd6bbb4b-864d-4475-a053-d33bcee53ffe',
+            'workflow_step_id' => '52',
+            'name' => 'institution_visible',
+            'value' => '1'
+        ], [
+            'id' => 'ded3fd88-dddb-4bd9-a7e0-0af0683d2d80',
+            'workflow_step_id' => '52',
+            'name' => 'institution_visible',
+            'value' => '2'
+        ], [
+            'id' => 'e627e6aa-8ae3-47a4-a1a4-90cfbd5370d2',
+            'workflow_step_id' => '53',
+            'name' => 'institution_visible',
+            'value' => '1'
+        ], [
+            'id' => 'e700aaea-0dd5-490d-9ad6-c1285a279ac6',
+            'workflow_step_id' => '53',
+            'name' => 'institution_visible',
+            'value' => '2'
+        ], [
+            'id' => 'f3ce245e-012b-4ed1-a694-305fcc9d6637',
+            'workflow_step_id' => '54',
+            'name' => 'institution_visible',
+            'value' => '1'
+        ], [
+            'id' => 'f41f19c3-14f3-4ad4-b803-82fb440f5559',
+            'workflow_step_id' => '54',
+            'name' => 'institution_visible',
+            'value' => '2'
+        ], [
+            'id' => 'fc45b70e-4766-46a0-a074-10ca0428e2f3',
+            'workflow_step_id' => '51',
+            'name' => 'validate_approve',
+            'value' => '1'
+        ]
+    ];
+}
+
