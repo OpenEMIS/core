99bd43c7eb13d20b58c9fb572120d8d6037abe51
Livvi Sim <lsim@kordit.com>
2017-10-27 17:18:14 +0800

    POCOR-4243 - Change limit to 100 instead

--- src/Model/Behavior/ExcelBehavior.php
+++ src/Model/Behavior/ExcelBehavior.php
@@ -32,7 +32,7 @@ class ExcelBehavior extends Behavior
         'folder' => 'export',
         'default_excludes' => ['modified_user_id', 'modified', 'created', 'created_user_id', 'password'],
         'excludes' => [],
-        'limit' => 150,
+        'limit' => 100,
         'pages' => [],
         'autoFields' => true,
         'orientation' => 'landscape', // or portrait
