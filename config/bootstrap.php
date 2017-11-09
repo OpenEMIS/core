ab6deea769f52982c2a4de19a9c5978ab30836d5
Chiang Hao Long <hchiang@kordit.com>
2017-11-09 16:14:59 +0800

    POCOR-4249 - Fixed bootstrap

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -305,18 +305,15 @@ Plugin::load('Examination', ['routes' => true, 'autoload' => true]);
 Plugin::load('Configuration', ['routes' => true, 'autoload' => true]);
 Plugin::load('CustomExcel', ['routes' => true, 'autoload' => true]);
 Plugin::load('Competency', ['routes' => true, 'autoload' => true]);
+Plugin::load('ReportCard', ['routes' => true, 'autoload' => true]);
 Plugin::load('Profile', ['routes' => true, 'autoload' => true]);
 Plugin::load('Transport', ['routes' => true, 'autoload' => true]);
 
-$premiumPlugins = [
-    'ReportCards' => ['name' => 'Report Cards', 'routes' => true, 'autoload' => true]
-];
-
-foreach ($premiumPlugins as $pluginKey => $content) {
-    if (Configure::read('schoolMode') && !array_key_exists($pluginKey, (array)Configure::read('School.excludedPlugins'))) {
-        $contentsToLoad = $content + ['name' => '', 'routes' => false, 'autoload' => false];
-        unset($contentsToLoad['name']);
-        Plugin::load($pluginKey, $contentsToLoad);
+$pluginPath = Configure::read('plugins');
+foreach ($pluginPath as $key => $path) {
+    if (!file_exists($path)) {
+        Plugin::unload($key);
+        Configure::write('School.excludedPlugins.' . $key, Inflector::humanize(Inflector::underscore(Inflector::pluralize($key))));
     }
 }
 
