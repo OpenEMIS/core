027be90ce686fe997957c1a8a8156dac92444d2a
Chiang Hao Long <hchiang@kordit.com>
2017-11-28 17:03:37 +0800

    POCOR-4285 - Shifted counselling and cases to plugin

--- config/Migrations/20171124061855_POCOR4249.php
+++ config/Migrations/20171124061855_POCOR4249.php
@@ -6,12 +6,10 @@ class POCOR4249 extends AbstractMigration
     public function up()
     {
         $this->execute("UPDATE workflow_models SET model = 'Quality.VisitRequests' WHERE model = 'Institution.VisitRequests'");
-        $this->execute("UPDATE workflow_models SET model = 'Cases.InstitutionCases' WHERE model = 'Institution.InstitutionCases'");
     }
 
     public function down()
     {
         $this->execute("UPDATE workflow_models SET model = 'Institution.VisitRequests' WHERE model = 'Quality.VisitRequests'");
-        $this->execute("UPDATE workflow_models SET model = 'Institution.InstitutionCases' WHERE model = 'Cases.InstitutionCases'");
     }
 }
