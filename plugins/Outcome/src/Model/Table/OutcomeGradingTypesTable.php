c49bfd37fb5de20166b2b96527121b18562005b8
Livvi Sim <lsim@kordit.com>
2017-12-07 15:54:33 +0800

    POCOR-4252 - Set up Outcome Grading Types and Options pages

--- plugins/Outcome/src/Model/Table/OutcomeGradingTypesTable.php
+++ plugins/Outcome/src/Model/Table/OutcomeGradingTypesTable.php
@@ -5,8 +5,6 @@ use ArrayObject;
 use Cake\Event\Event;
 use Cake\ORM\Query;
 use Cake\ORM\Entity;
-use Cake\ORM\TableRegistry;
-use Cake\Network\Request;
 use Cake\Validation\Validator;
 
 use App\Model\Table\ControllerActionTable;
@@ -29,10 +27,77 @@ class OutcomeGradingTypesTable extends ControllerActionTable
             'dependent' => true,
             'cascadeCallbacks' => true
         ]);
+
+        $this->setDeleteStrategy('restrict');
+    }
+
+    public function validationDefault(Validator $validator)
+    {
+        $validator = parent::validationDefault($validator);
+        return $validator
+            ->allowEmpty('code')
+            ->add('code', 'ruleUniqueCode', [
+                'rule' => 'validateUnique',
+                'provider' => 'table'
+            ]);
     }
 
     public function beforeAction(Event $event, ArrayObject $extra)
     {
         $this->controller->getOutcomeTabs();
+    }
+
+    public function viewBeforeAction(Event $event, ArrayObject $extra)
+    {
+        $this->setupFields();
+    }
+
+    public function addEditBeforeAction(Event $event, ArrayObject $extra)
+    {
+        $this->setupFields();
+    }
+
+    public function viewEditBeforeQuery(Event $event, Query $query, ArrayObject $extra)
+    {
+        $query->contain(['GradingOptions.InstitutionOutcomeResults']);
+    }
+
+    public function addEditOnAddOption(Event $event, Entity $entity, ArrayObject $data, ArrayObject $options)
+    {
+        $fieldKey = 'grading_options';
+
+        if (empty($data[$this->alias()][$fieldKey])) {
+            $data[$this->alias()][$fieldKey] = [];
+        }
+        if ($data->offsetExists($this->alias())) {
+            $data[$this->alias()][$fieldKey][] = [
+                'code' => '',
+                'name' => '',
+                'description' => '',
+                'outcome_grading_type_id' => '',
+                'id' => ''
+            ];
+        }
+
+        $options['associated'] = [
+            'GradingOptions' => ['validate' => false]
+        ];
+    }
+
+    public function setupFields()
+    {
+        $this->field('grading_options', [
+            'type' => 'element',
+            'element' => 'Outcome.grading_options'
+        ]);
+
+        $this->setFieldOrder(['code', 'name', 'grading_options']);
+    }
+
+    public function deleteOnInitialize(Event $event, Entity $entity, Query $query, ArrayObject $extra)
+    {
+        $extra['excludedModels'] = [
+            $this->GradingOptions->alias()
+        ];
     }
 }
