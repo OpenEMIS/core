3191a44f9f0721c0acc96d2020ce05a4e93d0865
Chiang Hao Long <hchiang@kordit.com>
2018-01-02 10:46:40 +0800

    POCOR-4039 - Remove scheduler controller and add migration to manage event on sql level

--- config/Migrations/20171219083248_POCOR4039.php
+++ config/Migrations/20171219083248_POCOR4039.php
@@ -5,70 +5,23 @@ class POCOR4039 extends AbstractMigration
 {
     public function up()
     {
-        $this->table('schedule_jobs')
-            ->addColumn('job_id', 'integer', [
-                'limit' => 11,
-                'null' => false,
-                'comment' => 'link to jobs.id',
-            ])
-            ->addColumn('pid', 'integer', [
-                'limit' => 11,
-                'null' => true,
-                'default' => null
-            ])
-            ->addColumn('scheduled_time', 'time', [
-                'null' => false
-            ])
-            ->addColumn('interval', 'integer', [
-                'default' => 0,
-                'null' => false
-            ])
-            ->addColumn('last_ran', 'datetime', [
-                'null' => true
-            ])
-            ->addColumn('status', 'integer', [
-                'null' => false,
-                'comment' => '1 => scheduled, 2 => running, 3 => stopped',
-            ])
-            ->insert([
-                [
-                    'id' => '1',
-                    'job_id' => '1',
-                    'pid' => null,
-                    'scheduled_time' => '00:00:00',
-                    'interval' => 86400,
-                    'last_ran' => null,
-                    'status' => 1
-                ]
-            ])
-            ->save();
-
-        $this->table('jobs')
-            ->addColumn('code', 'string', [
-                'limit' => 250,
-                'null' => false,
-            ])
-            ->addColumn('name', 'string', [
-                'limit' => 250,
-                'null' => false,
-            ])
-            ->addColumn('description', 'text', [
-                'null' => true
-            ])
-            ->insert([
-                [
-                    'id' => '1',
-                    'code' => 'institution_status',
-                    'name' => 'Institution Statuses',
-                    'description' => null
-                ]
-            ])
-            ->save();
+        $this->execute('SET GLOBAL event_scheduler = ON');
+        $this->execute('
+            DELIMITER $$
+            DROP EVENT IF EXISTS `update_institution_statuses` $$
+            CREATE EVENT `update_institution_statuses` ON SCHEDULE EVERY 1 DAY STARTS CONCAT(CURRENT_DATE() + INTERVAL '1' DAY, ' 00:00:00')
+            DO BEGIN
+                UPDATE `institutions`
+                SET `institution_status_id` = 2
+                WHERE `date_closed` < CURRENT_DATE();
+            END $$
+            DELIMITER ;'
+        );
     }
 
     public function down()
     {
-        $this->dropTable('schedule_jobs');
-        $this->dropTable('jobs');
+        $this->execute('SET GLOBAL event_scheduler = OFF');
+        $this->execute('DROP EVENT IF EXISTS `update_institution_statuses`');
     }
 }
--- plugins/Schedule/config/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-use Cake\Routing\Router;
-
-Router::scope('/Schedules', ['plugin' => 'Schedule'], function ($routes) {
-    $routes->connect('/', ['controller' => 'Schedules']);
-    $routes->connect('/:action/*', ['controller' => 'Schedules']);
-});
