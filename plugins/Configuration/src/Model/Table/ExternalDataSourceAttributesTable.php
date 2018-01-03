d0b9053e24b0b427a093abcc05654fdcef70ce30
Chiang Hao Long <hchiang@kordit.com>
2018-01-03 14:27:03 +0800

    POCOR-4306 - Fixed remaining translation issue in code level

--- plugins/Configuration/src/Model/Table/ExternalDataSourceAttributesTable.php
+++ plugins/Configuration/src/Model/Table/ExternalDataSourceAttributesTable.php
@@ -10,54 +10,57 @@ use Firebase\JWT\JWT;
 use Cake\Utility\Security;
 use Cake\Core\Configure;
 
-class ExternalDataSourceAttributesTable extends ControllerActionTable {
-	public function initialize(array $config) {
-		parent::initialize($config);
-		$this->addBehavior('Restful.RestfulAccessControl', [
-        	'Students' => ['index'],
+class ExternalDataSourceAttributesTable extends ControllerActionTable
+{
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+        $this->addBehavior('Restful.RestfulAccessControl', [
+            'Students' => ['index'],
             'Staff' => ['index']
         ]);
-	}
+    }
 
-	public function getExternalDataSourceAttributeValues($typeName = null) {
-		$list = $this->find('list', [
+    public function getExternalDataSourceAttributeValues($typeName = null)
+    {
+        $list = $this->find('list', [
                 'groupField' => 'external_data_source_type',
                 'keyField' => 'attribute_field',
                 'valueField' => 'value'
             ])->toArray();
 
-		if (!is_null($typeName)) {
-			if (isset($list[$typeName])) {
-				return $list[$typeName];
-			} else {
-				return [];
-			}
-		} else {
-			return $list;
-		}
-	}
+        if (!is_null($typeName)) {
+            if (isset($list[$typeName])) {
+                return $list[$typeName];
+            } else {
+                return [];
+            }
+        } else {
+            return $list;
+        }
+    }
 
-	public function findAttributes(Query $query, array $options = [])
-	{
-		$ConfigItemTable = TableRegistry::get('ConfigItems');
-		$externalSourceType = $ConfigItemTable
-			->find()
-			->select([$ConfigItemTable->aliasField('value')])
-			->where([$ConfigItemTable->aliasField('code') => 'external_data_source_type'])
-			->first();
+    public function findAttributes(Query $query, array $options = [])
+    {
+        $ConfigItemTable = TableRegistry::get('ConfigItems');
+        $externalSourceType = $ConfigItemTable
+            ->find()
+            ->select([$ConfigItemTable->aliasField('value')])
+            ->where([$ConfigItemTable->aliasField('code') => 'external_data_source_type'])
+            ->first();
 
-		$externalSourceType = $externalSourceType['value'];
+        $externalSourceType = $externalSourceType['value'];
 
-		return $query
-			->find('list', [
-				'keyField' => 'attribute_field',
-				'valueField' => 'value'
-			])
-			->select([$this->aliasField('value'), $this->aliasField('attribute_field')])
-			->where([
-				$this->aliasField('external_data_source_type') => $externalSourceType
-			]);
-	}
+        return $query
+            ->find('list', [
+                'keyField' => 'attribute_field',
+                'valueField' => 'value'
+            ])
+            ->select([$this->aliasField('value'), $this->aliasField('attribute_field')])
+            ->where([
+                $this->aliasField('external_data_source_type') => $externalSourceType
+            ]);
+    }
 
     public function generateServerAuthorisationToken($clientId, $scope, $tokenUri, $encryptedPrivateKey)
     {
@@ -84,23 +87,23 @@ class ExternalDataSourceAttributesTable extends ControllerActionTable {
         return JWT::encode($payload, $privateKey, 'RS256');
     }
 
-	public function findUri(Query $query, array $options = [])
-	{
-		$ConfigItemTable = TableRegistry::get('ConfigItems');
-		$externalSourceType = $ConfigItemTable
-			->find()
-			->select([$ConfigItemTable->aliasField('value')])
-			->where([$ConfigItemTable->aliasField('code') => 'external_data_source_type'])
-			->first();
+    public function findUri(Query $query, array $options = [])
+    {
+        $ConfigItemTable = TableRegistry::get('ConfigItems');
+        $externalSourceType = $ConfigItemTable
+            ->find()
+            ->select([$ConfigItemTable->aliasField('value')])
+            ->where([$ConfigItemTable->aliasField('code') => 'external_data_source_type'])
+            ->first();
 
-		$externalSourceType = $externalSourceType['value'];
-		$attributeField = isset($options['record_type']) ? $options['record_type'] : null;
-		return $query
-			->select([$this->aliasField('value')])
-			->where([
-				$this->aliasField('attribute_field') => $attributeField,
-				$this->aliasField('external_data_source_type') => $externalSourceType
-			])
-			->first();
-	}
+        $externalSourceType = $externalSourceType['value'];
+        $attributeField = isset($options['record_type']) ? $options['record_type'] : null;
+        return $query
+            ->select([$this->aliasField('value')])
+            ->where([
+                $this->aliasField('attribute_field') => $attributeField,
+                $this->aliasField('external_data_source_type') => $externalSourceType
+            ])
+            ->first();
+    }
 }
