9338afeeb8003f93b5d53ed57105fa38fd7230f5
Chiang Hao Long <hchiang@kordit.com>
2018-01-04 11:35:19 +0800

    POCOR-2455 - Rename status column

---
+++ config/Migrations/20180104025254_POCOR2455.php
@@ -0,0 +1,23 @@
+<?php
+use Migrations\AbstractMigration;
+use Cake\ORM\TableRegistry;
+
+class POCOR2455 extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+
+    }
+}
