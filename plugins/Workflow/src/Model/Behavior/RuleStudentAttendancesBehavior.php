16dfb54e9c8def6a743b09b4ad2236c55d87c523
Chiang Hao Long <hchiang@kordit.com>
2018-01-10 17:36:46 +0800

    POCOR-4084 - Added logic to beautify the output. Added logic to remove late entry from the record.

--- plugins/Workflow/src/Model/Behavior/RuleStudentAttendancesBehavior.php
+++ plugins/Workflow/src/Model/Behavior/RuleStudentAttendancesBehavior.php
@@ -5,6 +5,7 @@ use ArrayObject;
 use Cake\ORM\TableRegistry;
 use Cake\ORM\Entity;
 use Cake\Event\Event;
+use Cake\Network\Request;
 use Cake\Utility\Inflector;
 use Cake\Log\Log;
 use Workflow\Model\Behavior\RuleBehavior;
@@ -14,9 +15,17 @@ class RuleStudentAttendancesBehavior extends RuleBehavior
     protected $_defaultConfig = [
         'feature' => 'StudentAttendances',
         'rule' => [
+            'absence_type_id' => [
+                'type' => 'select',
+                'after' => 'workflow_id',
+                'lookupModel' => 'Institution.AbsenceTypes',
+                'attr' => [
+                    'required' => true
+                ]
+            ],
             'days_absent' => [
                 'type' => 'string',
-                'after' => 'workflow_id',
+                'after' => 'absence_type_id',
                 'attr' => [
                     'required' => true
                 ]
@@ -35,12 +44,65 @@ class RuleStudentAttendancesBehavior extends RuleBehavior
         if (isset($data['feature']) && !empty($data['feature']) && $data['feature'] == $this->rule) {
             if (isset($data['submit']) && $data['submit'] == 'save') {
                 $validator = $model->validator();
+                $validator->add('absence_type_id', 'notBlank', ['rule' => 'notBlank']);
+                $validator->requirePresence('absence_type_id');
                 $validator->add('days_absent', 'notBlank', ['rule' => 'notBlank']);
                 $validator->requirePresence('days_absent');
                 $validator->add('days_absent', 'wrongNumberRange', [
                     'rule' => ['comparison', '>', 0],
                     'message' => __('Please enter a valid number more than 0.')
                 ]);
+            }
+        }
+    }
+
+    public function onUpdateFieldAbsenceTypeId(Event $event, array $attr, $action, Request $request)
+    {
+        if ($action == 'add' || $action == 'edit') {
+            $lookupModel = $this->config('rule.absence_type_id.lookupModel');
+            $modelTable = TableRegistry::get($lookupModel);
+            $lateId = $modelTable->findByCode('LATE')->extract('id')->first();
+            unset($attr['options'][$lateId]);
+            return $attr;
+        }
+    }
+
+    public function onGetStudentAttendancesRule(Event $event, Entity $entity)
+    {
+        $model = $this->_table;
+        if ($model->action == 'index' && $entity->has('rule')) {
+            $ruleConfig = $this->config('rule');
+            $ruleArray = json_decode($entity->rule, true);
+
+            $list = [];
+            if (array_key_exists('where', $ruleArray)) {
+                $where = $ruleArray['where'];
+                foreach ($where as $field => $fieldValue) {
+                    $label = Inflector::humanize($field);
+                    if ($model->endsWith($field, '_id') && $model->endsWith($label, ' Id')) {
+                        $label = str_replace(' Id', '', $label);
+                    }
+                    $value = __($label) . ': ';
+
+                    if (isset($ruleConfig[$field]['lookupModel'])) {
+                        $lookupModel = $this->config('rule.'.$field.'.lookupModel');
+                        $modelTable = TableRegistry::get($lookupModel);
+
+                        try {
+                            $fieldRecord = $modelTable->get($fieldValue);
+                            $value .= $fieldRecord->name;
+                        } catch (\Exception $e) {
+                            Log::write('debug', $e->getMessage());
+                        }
+                    } else {
+                        $value .= $fieldValue;
+                    }
+                    $list[] = $value;
+                }
+            }
+
+            if (!empty($list)) {
+                return implode("<br>", $list);
             }
         }
     }
