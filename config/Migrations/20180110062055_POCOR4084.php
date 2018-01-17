32e4573fb24c5805a9e661dc4295313c3bf436e4
Chiang Hao Long <hchiang@kordit.com>
2018-01-17 10:10:00 +0800

    POCOR-4084 - Added new logic to keep track of consecutive days absent

--- config/Migrations/20180110062055_POCOR4084.php
+++ config/Migrations/20180110062055_POCOR4084.php
@@ -5,9 +5,21 @@ class POCOR4084 extends AbstractMigration
 {
     public function up()
     {
+        $this->table('institution_student_absence_days');
+
+
+        $table = $this->table('institution_student_absences');
+        $table->addColumn('institution_student_absence_day_id', 'integer', [
+                'limit' => 11,
+                'null' => true,
+                'default' => null
+            ])
+            ->update();
     }
 
     public function down()
     {
+        $table->removeColumn('institution_student_absence_day_id')
+            ->save();
     }
 }
