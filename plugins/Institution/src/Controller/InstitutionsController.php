f3f2943d2030e0ea44c52e7f6706adf2b8956570
kok keong cheng <kcheng@kordit.com>
2018-01-17 14:59:52 +0800

    POCOR-4353 - Fix map issue on redirecting back to index page

--- plugins/Institution/src/Controller/InstitutionsController.php
+++ plugins/Institution/src/Controller/InstitutionsController.php
@@ -895,6 +895,8 @@ class InstitutionsController extends AppController
             } else {
                 $session->delete('Institution.Institutions.id');
             }
+        } elseif ($action == 'Institutions' && isset($this->request->pass[0]) && $this->request->pass[0] == 'view' && isset($this->request->pass[1])) {
+            $this->request->params['institutionId'] = $this->request->pass[1];
         } elseif ($action == 'StudentUser') {
             $session->write('Student.Students.id', $this->ControllerAction->paramsDecode($this->request->pass[1])['id']);
         } elseif ($action == 'StaffUser') {
@@ -1020,6 +1022,7 @@ class InstitutionsController extends AppController
                     'institutions.comments.ctrl',
                     'institutions.comments.svc'
                 ]);
+                // no break
             case 'Classes':
                 if (isset($this->request->pass[0])) {
                     if ($this->request->param('pass')[0] == 'edit') {
@@ -1137,7 +1140,7 @@ class InstitutionsController extends AppController
                 if ($model->table() == 'security_users' && !$isDownload) {
                     $ids = empty($this->ControllerAction->paramsDecode($params['pass'][1])['id']) ? $session->read('Student.Students.id') : $this->ControllerAction->paramsDecode($params['pass'][1])['id'];
                     $persona = $model->get($ids);
-                } else if ($model->table() == 'staff_appraisals'  && !$isDownload) {
+                } elseif ($model->table() == 'staff_appraisals'  && !$isDownload) {
                     $ids = array_key_exists('user_id', $requestQuery) ? $requestQuery['user_id']: $session->read('Staff.Staff.id');
                     $persona = $model->Users->get($ids);
                 }
@@ -1234,7 +1237,7 @@ class InstitutionsController extends AppController
             if ($model->hasField('institution_id')) {
                 if (!$session->check('Institution.Institutions.id')) {
                     $this->Alert->error('general.notExists');
-                    // should redirect
+                // should redirect
                 } else {
                     if (!in_array($model->alias(), ['Programmes', 'StaffTransferIn', 'StaffTransferOut'])) {
                         $institutionId = $this->request->param('institutionId');
@@ -1313,8 +1316,7 @@ class InstitutionsController extends AppController
                 'conditions' => ['institution_id' => $id, 'staff_status_id' => $assignedStatus]
             ];
             $highChartDatas[] = $InstitutionStaff->getHighChart('number_of_staff_by_year', $params);
-
-        } else if ($classification == $Institutions::NON_ACADEMIC) {
+        } elseif ($classification == $Institutions::NON_ACADEMIC) {
             //Staffs By Position Title for current year, only shows assigned staff
             $params = [
                 'conditions' => ['institution_id' => $id, 'staff_status_id' => $assignedStatus]
@@ -1719,7 +1721,7 @@ class InstitutionsController extends AppController
                     $model->toggle('add', false);
                     $model->toggle('edit', false);
                     $model->toggle('remove', false);
-                } else if ($model instanceof \App\Model\Table\AppTable) {
+                } elseif ($model instanceof \App\Model\Table\AppTable) {
                     // CAv3 hide button and redirect when user change the Url
                     $model->addBehavior('ControllerAction.HideButton');
                 }
