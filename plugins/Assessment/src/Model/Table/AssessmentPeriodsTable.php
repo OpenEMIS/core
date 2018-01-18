fdaae138c785602114067fa59891b887ff4b6859
Nicholas See Jun Jie <nsee@kordit.com>
2018-01-18 15:30:28 +0800

    POCOR4330 - The 'assessment_grading_type_id' field should be validated (not null) before save

--- plugins/Assessment/src/Model/Table/AssessmentPeriodsTable.php
+++ plugins/Assessment/src/Model/Table/AssessmentPeriodsTable.php
@@ -36,13 +36,13 @@ class AssessmentPeriodsTable extends ControllerActionTable
         ]);
 
         $this->belongsToMany('EducationSubjects', [
-        'className' => 'Education.EducationSubjects',
-        'joinTable' => 'assessment_items_grading_types',
-        'foreignKey' => 'assessment_period_id',
-        'targetForeignKey' => 'education_subject_id',
-        'through' => 'Assessment.AssessmentItemsGradingTypes',
-        'dependent' => true,
-        'cascadeCallbacks' => true
+            'className' => 'Education.EducationSubjects',
+            'joinTable' => 'assessment_items_grading_types',
+            'foreignKey' => 'assessment_period_id',
+            'targetForeignKey' => 'education_subject_id',
+            'through' => 'Assessment.AssessmentItemsGradingTypes',
+            'dependent' => true,
+            'cascadeCallbacks' => true
         ]);
 
         $this->addBehavior('Restful.RestfulAccessControl', [
