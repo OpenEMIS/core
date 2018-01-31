022feab9d07fe85bde8ca30fc81f1e0fa010959c
Nicholas See Jun Jie <nsee@kordit.com>
2018-01-31 15:59:54 +0800

    POCOR-4374  1. Dropdownlist will now display all the students that are enrolled to the class.  2. Added display field for status  3. Only students with status='enrolled' are editable

--- plugins/Institution/webroot/js/angular/student_compentencies/institution.student.competencies.svc.js
+++ plugins/Institution/webroot/js/angular/student_compentencies/institution.student.competencies.svc.js
@@ -9,7 +9,6 @@ function InstitutionStudentCompetenciesSvc($http, $q, $filter, KdDataSvc, AlertS
     var service = {
         init: init,
         getClassDetails: getClassDetails,
-        getStudentStatusId: getStudentStatusId,
         getClassStudents: getClassStudents,
         getCompetencyTemplate: getCompetencyTemplate,
         getCompetencyGradingTypes: getCompetencyGradingTypes,
@@ -60,24 +59,14 @@ function InstitutionStudentCompetenciesSvc($http, $q, $filter, KdDataSvc, AlertS
             .ajax({success: success, defer:true});
     }
 
-    function getStudentStatusId(statusCode) {
-        var success = function(response, deferred) {
-            deferred.resolve(response.data.data);
-        };
-        return StudentStatuses
-            .select(['id'])
-            .where({code: statusCode})
-            .ajax({success: success, defer:true});
-    }
-
-    function getClassStudents(classId, enrolledStatusId) {
+    function getClassStudents(classId) {
         var success = function(response, deferred) {
             deferred.resolve(response.data.data);
         };
         return InstitutionClassStudents
             .select()
-            .contain(['Users'])
-            .where({institution_class_id: classId, student_status_id: enrolledStatusId})
+            .contain(['Users','StudentStatuses'])
+            .where({institution_class_id: classId})
             .order(['Users.first_name', 'Users.last_name'])
             .ajax({success: success, defer:true});
     }
@@ -193,6 +182,8 @@ function InstitutionStudentCompetenciesSvc($http, $q, $filter, KdDataSvc, AlertS
             },
             cellRenderer: function(params) {
                 var periodEditable = params.data.period_editable;
+                var studentStatusName = params.data.student_status_name;
+                
                 var gradingOptions = {
                     0 : {
                         id: 0,
@@ -206,7 +197,7 @@ function InstitutionStudentCompetenciesSvc($http, $q, $filter, KdDataSvc, AlertS
                     });
                 }
 
-                if (periodEditable) {
+                if (periodEditable && studentStatusName == "Enrolled") {
                     var oldValue = params.value;
 
                     var eCell = document.createElement('div');
@@ -277,8 +268,9 @@ function InstitutionStudentCompetenciesSvc($http, $q, $filter, KdDataSvc, AlertS
             },
             pinnedRowCellRenderer: function(params) {
                 var periodEditable = params.data.period_editable;
+                var studentStatusName = params.data.student_status_name;
 
-                if (periodEditable) {
+                if (periodEditable && studentStatusName == "Enrolled") {
                     var oldValue = params.value;
 
                     var eCell = document.createElement('div');
