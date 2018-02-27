25e7896e8d5db1ea4a03d6335d4266f77b9334c7
Livvi Sim <lsim@kordit.com>
2018-02-27 11:16:42 +0800

    POCOR-4340 - Convert CAv3 to CAv4

--- plugins/StaffAppraisal/src/Model/Table/AppraisalFormsCriteriasTable.php
+++ plugins/StaffAppraisal/src/Model/Table/AppraisalFormsCriteriasTable.php
@@ -2,12 +2,12 @@
 namespace StaffAppraisal\Model\Table;
 
 use Cake\Validation\Validator;
-use App\Model\Table\AppTable;
 use Cake\Event\Event;
 use ArrayObject;
 use Cake\ORM\Entity;
+use App\Model\Table\ControllerActionTable;
 
-class AppraisalFormsCriteriasTable extends AppTable
+class AppraisalFormsCriteriasTable extends ControllerActionTable
 {
     public function initialize(array $config) : void
     {
