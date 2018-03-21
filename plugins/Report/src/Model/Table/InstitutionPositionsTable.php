cd49d7ae12554fd3ce965b443eed5f0ee51f2bbd
kok keong cheng <kcheng@kordit.com>
2018-03-21 15:00:32 +0800

    POCOR-4461 - Merge institution position and institution position assignments table - Added position filters options to positions features - Remove position assignment options, use position options instead

--- plugins/Report/src/Model/Table/InstitutionPositionsTable.php
+++ plugins/Report/src/Model/Table/InstitutionPositionsTable.php
@@ -5,110 +5,234 @@ use ArrayObject;
 use Cake\ORM\Entity;
 use Cake\ORM\Query;
 use Cake\Event\Event;
-use Cake\Network\Request;
 use App\Model\Table\AppTable;
 use App\Model\Traits\OptionsTrait;
 
-class InstitutionPositionsTable extends AppTable  {
-	use OptionsTrait;
+class InstitutionPositionsTable extends AppTable
+{
+    use OptionsTrait;
 
-	public function initialize(array $config) {
-		$this->table('institution_positions');
-		parent::initialize($config);
+    // position filter
+    const ALL_POSITION = 0;
+    const POSITION_WITH_STAFF = 1;
 
-		$this->belongsTo('Statuses', ['className' => 'Workflow.WorkflowSteps', 'foreignKey' => 'status_id']);
+    public function initialize(array $config)
+    {
+        $this->table('institution_positions');
+        parent::initialize($config);
+        
+        $this->belongsTo('Statuses', ['className' => 'Workflow.WorkflowSteps', 'foreignKey' => 'status_id']);
         $this->belongsTo('StaffPositionTitles', ['className' => 'Institution.StaffPositionTitles']);
         $this->belongsTo('StaffPositionGrades', ['className' => 'Institution.StaffPositionGrades']);
         $this->belongsTo('Institutions', ['className' => 'Institution.Institutions']);
         $this->belongsTo('Assignees', ['className' => 'User.Users']);
-		
-		$this->addBehavior('Excel');
-		$this->addBehavior('Report.ReportList');
-		$this->addBehavior('Report.InstitutionSecurity');
-	}
+        // $this->hasMany('InstitutionStaff', ['className' => 'Institution.Staff', 'foreignKey' => 'institution_staff_id']);
 
-	public function beforeAction(Event $event) {
-		$this->fields = [];
-		$this->ControllerAction->field('feature');
-		$this->ControllerAction->field('format');
-	}
+        $this->addBehavior('Excel', [
+            'autoFields' => false
+        ]);
+        $this->addBehavior('Report.ReportList');
+        $this->addBehavior('Report.InstitutionSecurity');
+    }
 
-	public function onExcelBeforeQuery(Event $event, ArrayObject $settings, Query $query) 
-	{
-		$query
-			->contain(['Institutions.Areas', 'Institutions.AreaAdministratives'])
-			->select(['area_code' => 'Areas.code', 'area_name' => 'Areas.name', 'area_administrative_code' => 'AreaAdministratives.code', 'area_administrative_name' => 'AreaAdministratives.name']);
-	}
+    public function onExcelBeforeQuery(Event $event, ArrayObject $settings, Query $query)
+    {
+        $requestData = json_decode($settings['process']['params']);
+        $positionFilter = $requestData->position_filter;
 
-	public function onUpdateFieldFeature(Event $event, array $attr, $action, Request $request) {
-		$attr['options'] = $this->controller->getFeatureOptions('Institutions');
-		return $attr;
-	}
+        $query
+            ->select([
+                'workflow_steps_name' => 'Statuses.name',
+                'position_no' => $this->aliasField('position_no'),
+                'staff_position_grade_name' => 'StaffPositionGrades.name',
+                'area_code' => 'Areas.code',
+                'area_name' => 'Areas.name',
+                'area_administratives_code' => 'AreaAdministratives.code',
+                'area_administratives_name' => 'AreaAdministratives.name',
+                'assignee_id' => 'Assignees.id',
+                'is_homeroom' => $this->aliasField('is_homeroom')
+            ])
+            ->contain([
+                'Statuses' => [
+                    'fields' => [
+                        'Statuses.name'
+                    ]
+                ],
+                'StaffPositionTitles' => [
+                    'fields' => [
+                        'StaffPositionTitles.id',
+                        'StaffPositionTitles.name',
+                        'StaffPositionTitles.type'
+                    ]
+                ],
+                'StaffPositionGrades' => [
+                    'fields' => [
+                        'StaffPositionGrades.name'
+                    ]
+                ],
+                'Institutions' => [
+                    'fields' => [
+                        'Institutions.id',
+                        'Institutions.name',
+                        'Institutions.code'
+                    ]
+                ],
+                'Institutions.Areas' => [
+                    'fields' => [
+                        'Areas.name',
+                        'Areas.code'
+                    ]
+                ],
+                'Institutions.AreaAdministratives' => [
+                    'fields' => [
+                        'AreaAdministratives.name',
+                        'AreaAdministratives.code'
+                    ]
+                ],
+                'Assignees' => [
+                    'fields' => [
+                        'Assignees.id',
+                        'Assignees.first_name',
+                        'Assignees.middle_name',
+                        'Assignees.last_name',
+                        'Assignees.third_name',
+                        'Assignees.preferred_name'
+                    ]
+                ]
+            ])
+        ;
+    }
 
-	public function onExcelUpdateFields(Event $event, ArrayObject $settings, $fields) 
-	{
-		$cloneFields = $fields->getArrayCopy();
-		$newFields = [];
-		foreach ($cloneFields as $key => $value) {
-			$newFields[] = $value;
-			if ($value['field'] == 'institution_id') {
-				$newFields[] = [
-					'key' => 'Areas.code',
-					'field' => 'area_code',
-					'type' => 'string',
-					'label' => __('Area Code')
-				];
+    public function onExcelUpdateFields(Event $event, ArrayObject $settings, $fields)
+    {
+        $requestData = json_decode($settings['process']['params']);
+        $positionFilter = $requestData->position_filter;
 
-				$newFields[] = [
-					'key' => 'Areas.name',
-					'field' => 'area_name',
-					'type' => 'string',
-					'label' => __('Area')
-				];
+        $newFields = $this->getPositionFields();
 
-				$newFields[] = [
-					'key' => 'AreaAdministratives.code',
-					'field' => 'area_administrative_code',
-					'type' => 'string',
-					'label' => __('Area Administrative Code')
-				];
+        if ($positionFilter == self::POSITION_WITH_STAFF) {
+        }
 
-				$newFields[] = [
-					'key' => 'AreaAdministratives.name',
-					'field' => 'area_administrative_name',
-					'type' => 'string',
-					'label' => __('Area Administrative')
-				];
-			}
-		}
-		$fields->exchangeArray($newFields);
-	}
+        // $newFields[] = [
+        //     'key' => '',
+        //     'field' => '',
+        //     'type' => '',
+        //     'label' => __('')
+        // ];
 
-	public function onExcelGetStatus(Event $event, Entity $entity) {
-		$options = $this->getSelectOptions('general.active');
-		return $options[$entity->status];
-	}
+        $fields->exchangeArray($newFields);
+    }
 
-	public function onExcelGetStaffPositionTitleId(Event $event, Entity $entity) {
-   		$options = $this->getSelectOptions('Staff.position_types');
-		if ($entity->has('staff_position_title')) {
-	        $type = array_key_exists($entity->staff_position_title->type, $options) ? $options[$entity->staff_position_title->type] : '';
-	        if (empty($type)) {
-	    		return $entity->staff_position_title->name;
-   		    } else {
-				return $entity->staff_position_title->name .' - '. $type;
-   			}
-   		} else {
-   			$this->log($entity->name . ' has no staff_position_title...', 'debug');
-   		}
-	}
+    public function onExcelGetStaffPositionId(Event $event, Entity $entity)
+    {
+        $options = $this->getSelectOptions('Staff.position_types');
+        $staffPositionTitleType = '';
 
-	public function onExcelGetInstitutionId(Event $event, Entity $entity) {
-		return $entity->institution->code_name;
-	}
+        if ($entity->has('staff_position_title')) {
+            $staffPositionTitleType = $entity->staff_position_title->name;
+            $staffType = $entity->staff_position_title->type;
+            $type = array_key_exists($staffType, $options) ? $options[$staffType] : '';
 
-	public function onExcelGetIsHomeroom(Event $event, Entity $entity) {
-		$options = $this->getSelectOptions('general.yesno');
-		return $options[$entity->is_homeroom];
-	}
+            if (!empty($type)) {
+                $staffPositionTitleType .= ' - ' . $type;
+            }
+        }
+
+        return $staffPositionTitleType;
+    }
+
+    public function onExcelGetInstitutionId(Event $event, Entity $entity)
+    {
+        return $entity->institution->code_name;
+    }
+
+    public function onExcelGetIsHomeroom(Event $event, Entity $entity)
+    {
+        $options = $this->getSelectOptions('general.yesno');
+        return $options[$entity->is_homeroom];
+    }
+
+    private function getPositionFields()
+    {
+        $newFields = [];
+
+        $newFields[] = [
+            'key' => 'Statuses.name',
+            'field' => 'workflow_steps_name',
+            'type' => 'string',
+            'label' => __('Status')
+        ];
+
+        $newFields[] = [
+            'key' => 'InstitutionPositions.position_no',
+            'field' => 'position_no',
+            'type' => 'string',
+            'label' => __('Number')
+        ];
+
+        $newFields[] = [
+            'key' => 'StaffPositionTitles.id',
+            'field' => 'staff_position_id',
+            'type' => 'string',
+            'label' => __('Title')
+        ];
+
+        $newFields[] = [
+            'key' => 'StaffPositionGrades.name',
+            'field' => 'staff_position_grade_name',
+            'type' => 'string',
+            'label' => __('Grade')
+        ];
+
+        $newFields[] = [
+            'key' => 'Institutions.id',
+            'field' => 'institution_id',
+            'type' => 'string',
+            'label' => __('Institution')
+        ];
+
+        $newFields[] = [
+            'key' => 'Areas.code',
+            'field' => 'area_code',
+            'type' => 'string',
+            'label' => __('Area Code')
+        ];
+
+        $newFields[] = [
+            'key' => 'Areas.name',
+            'field' => 'area_name',
+            'type' => 'string',
+            'label' => __('Area')
+        ];
+
+        $newFields[] = [
+            'key' => 'AreaAdministratives.code',
+            'field' => 'area_administratives_code',
+            'type' => 'string',
+            'label' => __('Area Administrative Code')
+        ];
+
+        $newFields[] = [
+            'key' => 'AreaAdministratives.name',
+            'field' => 'area_administratives_name',
+            'type' => 'string',
+            'label' => __('Area Administrative')
+        ];
+
+        $newFields[] = [
+            'key' => 'Assignees.id',
+            'field' => 'assignee_id',
+            'type' => 'string',
+            'label' => __('Assignee')
+        ];
+
+        $newFields[] = [
+            'key' => 'InstitutionPositions.is_homeroom',
+            'field' => 'is_homeroom',
+            'type' => 'string',
+            'label' => __('Homeroom Teacher')
+        ];
+
+        return $newFields;
+    }
 }
