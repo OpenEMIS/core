26441d4e847f76a1b71089b599664b4b1c48ddff
Livvi Sim <lsim@kordit.com>
2018-04-10 17:25:57 +0800

    POCOR-4483 - Added translations for validation messages

---
+++ config/Migrations/20180410090857_POCOR4483.php
@@ -0,0 +1,42 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class POCOR4483 extends AbstractMigration
+{
+    public function up()
+    {
+        // locale_contents
+        $localeContent = [
+            [
+                'en' => 'File records exceeds maximum size allowed',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],
+            [
+                'en' => 'File format not supported',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],
+            [
+                'en' => 'The file',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],
+            [
+                'en' => 'failed to import.',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ]
+        ];
+        $this->insert('locale_contents', $localeContent);
+    }
+
+    public function down()
+    {
+        $this->execute("DELETE FROM `locale_contents` WHERE `en` = 'File records exceeds maximum size allowed'");
+        $this->execute("DELETE FROM `locale_contents` WHERE `en` = 'File format not supported'");
+        $this->execute("DELETE FROM `locale_contents` WHERE `en` = 'The file'");
+        $this->execute("DELETE FROM `locale_contents` WHERE `en` = 'failed to import.'");
+    }
+}
