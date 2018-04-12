40e32a243c59afd7fdfc2b226ef816cba4fe8bbb
Kok Keong <kcheng@kordit.com>
2018-04-12 10:03:17 +0800

    POCOR-4512 - Update map.ctp to use kdx-map from styleguide - Update map behaviour to have configurations for the new kdx-map element

--- plugins/OpenEmis/src/Model/Behavior/MapBehavior.php
+++ plugins/OpenEmis/src/Model/Behavior/MapBehavior.php
@@ -16,84 +16,115 @@ use Cake\ORM\TableRegistry;
  * Firstly, add this behavior in model's initialize function.
  * 
  * public function initialize(array $config) {
- * 		.............
- * 		
- *   	$this->addBehavior('OpenEmis.Map');
- * 		
- * 		.............
+ *      .............
+ *      
+ *      $this->addBehavior('OpenEmis.Map');
+ *      
+ *      .............
  * }
  *
  * 
  * Secondly, defines the field in model's beforeAction()
  *
- *	public function beforeAction($event) {
- * 		.............
- * 		
- * 		$this->ControllerAction->field('map', ['type' => 'map']);
- * 		
- * 		.............
- * 	}
+ *  public function beforeAction($event) {
+ *      .............
+ *      
+ *      $this->ControllerAction->field('map', ['type' => 'map']);
+ *      
+ *      .............
+ *  }
  *
  * If the $entity does not have 'latitude' and 'longitude' properties,
  * you have to define it as the field $attr before ControllerAction events are called or do it during field declaration.
  * 
- * 		$this->ControllerAction->field('map', ['type' => 'map', 'latitude' => $latitude_val, 'longitude' => $longitude_val]);
+ *      $this->ControllerAction->field('map', ['type' => 'map', 'latitude' => $latitude_val, 'longitude' => $longitude_val]);
  *
  * If the $entity do have the required properties but 'latitude' and 'longitude' parameters are defined in the field declaration, 
  * values in the field declaration will be shown.
  * 
  */
-class MapBehavior extends Behavior {
-	public function implementedEvents() {
-		$events = parent::implementedEvents();
-		$newEvent = [
-			'ControllerAction.Model.view.beforeAction' => 'viewBeforeAction',
-			'ControllerAction.Model.index.beforeAction' => 'indexBeforeAction',
-			'ControllerAction.Model.add.beforeAction' => 'addBeforeAction',
-			'ControllerAction.Model.edit.beforeAction' => 'editBeforeAction'
-		];
-		$events = array_merge($events, $newEvent);
-		return $events;
-	}
+class MapBehavior extends Behavior
+{
+    public function implementedEvents() 
+    {
+        $events = parent::implementedEvents();
+        $newEvent = [
+            'ControllerAction.Model.view.beforeAction' => 'viewBeforeAction',
+            'ControllerAction.Model.index.beforeAction' => 'indexBeforeAction',
+            'ControllerAction.Model.add.beforeAction' => 'addBeforeAction',
+            'ControllerAction.Model.edit.beforeAction' => 'editBeforeAction'
+        ];
+        $events = array_merge($events, $newEvent);
+        return $events;
+    }
 
-	public function indexBeforeAction(Event $event) {
-		$this->_fieldSetup();
-	}
+    public function indexBeforeAction(Event $event)
+    {
+        $this->_fieldSetup();
+    }
 
-	public function viewBeforeAction(Event $event) {
-		$this->_fieldSetup();
-	}
+    public function viewBeforeAction(Event $event)
+    {
+        $this->_fieldSetup();
+    }
 
-	public function addBeforeAction(Event $event) {
-		$this->_fieldSetup();
-	}
+    public function addBeforeAction(Event $event)
+    {
+        $this->_fieldSetup();
+    }
 
-	public function editBeforeAction(Event $event) {
-		$this->_fieldSetup();
-	}
+    public function editBeforeAction(Event $event)
+    {
+        $this->_fieldSetup();
+    }
 
-	private function _fieldSetup() {
-		foreach ($this->_table->fields as $key=>$value) {
-			if ($value['type'] == 'map') {
-				$this->_table->fields[$key]['override'] = true;
-				$this->_table->fields[$key]['label'] = false;
-			}
-		}
-	}
+    private function _fieldSetup()
+    {
+        foreach ($this->_table->fields as $key=>$value) {
+            if ($value['type'] == 'map') {
+                $this->_table->fields[$key]['override'] = true;
+                $this->_table->fields[$key]['label'] = false;
+            }
+        }
+    }
 
-	public function onGetMapElement(Event $event, $action, Entity $entity, $attr, $options) {
+    public function onGetMapElement(Event $event, $action, Entity $entity, $attr, $options) 
+    {
+        $ConfigItems = TableRegistry::get('Configuration.ConfigItems');
+        $mapZoom = $ConfigItems->value('map_zoom');
 
-		if (array_key_exists('latitude', $attr) && array_key_exists('longitude', $attr)) {
-			return $event->subject()->renderElement('OpenEmis.map', ['attr' => $attr]);
-		}
-	
-		if ($entity->latitude!='' && $entity->longitude!='') {
-			$attr['latitude'] = $entity->latitude;
-			$attr['longitude'] = $entity->longitude;
-			return $event->subject()->renderElement('OpenEmis.map', ['attr' => $attr]);
-		}
-	
-		return '<span class="error-message">' . __('Both latitude and longitude value have to be set for map to render') . '<span>';
-	}
+        // map configuration for kdx-map
+        $mapConfig = [
+            'zoom' => [
+                'value' => $mapZoom,
+                'isZoomButton' => true,
+                'isScrollZoom' => true,
+                'isTouchZoom' => true
+            ],
+            'attribution' => 'OpenEMIS',
+            'type' => 'basic'
+        ];
+
+        if (array_key_exists('latitude', $attr) && array_key_exists('longitude', $attr)) {
+            $attr['mapConfig'] = $mapConfig;
+            $attr['mapPosition'] = [
+                'lng' => $attr['longitude'],
+                'lat' => $attr['latitude']
+            ];
+
+            return $event->subject()->renderElement('OpenEmis.map', ['attr' => $attr]);
+        }
+
+        if ($entity->latitude!='' && $entity->longitude!='') {
+            $attr['mapConfig'] = $mapConfig;
+            $attr['mapPosition'] = [
+                'lng' => $entity->longitude,
+                'lat' => $entity->latitude
+            ];
+            return $event->subject()->renderElement('OpenEmis.map', ['attr' => $attr]);
+        }
+
+        return '<span class="error-message">' . __('Both latitude and longitude value have to be set for map to render') . '<span>';
+    }
 
 }
