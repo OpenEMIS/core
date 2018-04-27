c1099880e81f831fffc71958b949b52dd25f97d0
Acacia Wong <awong@kordit.com>
2018-04-27 16:47:39 +0800

    The dob date format passed to the service should be the same date format as the database: yyyy-mm-dd.

--- plugins/Institution/webroot/js/angular/students/institutions.students.svc.js
+++ plugins/Institution/webroot/js/angular/students/institutions.students.svc.js
@@ -644,7 +644,8 @@ function InstitutionsStudentsSvc($http, $q, $filter, KdOrmSvc) {
             var mm = (date.getMonth()+1).toString(); // getMonth() is zero-based
             var dd  = date.getDate().toString();
 
-            return (dd[1]?dd:"0"+dd[0]) + '-' + (mm[1]?mm:"0"+mm[0])  + '-' +   yyyy;
+            //return (dd[1]?dd:"0"+dd[0]) + '-' + (mm[1]?mm:"0"+mm[0])  + '-' +   yyyy;
+            return yyyy+ '-' + (mm[1]?mm:"0"+mm[0]) + '-' + (dd[1]?dd:"0"+dd[0]);
         } else {
             return '';
         }
