ec5891b38a5d77ca3dfb29387c1bb37b81efbf60
Livvi Sim <lsim@kordit.com>
2018-04-27 11:14:03 +0800

    POCOR-4562 - Added const statuses in ReportCardProcessesTable.php - Changed logic for GenerateAllReportCardsShell to use a queue instead of generating all at once - Added logic in ReportCardsTable to delete the report card process once the report card has finished generating

--- plugins/ReportCard/src/Model/Table/ReportCardProcessesTable.php
+++ plugins/ReportCard/src/Model/Table/ReportCardProcessesTable.php
@@ -5,6 +5,11 @@ use App\Model\Table\ControllerActionTable;
 
 class ReportCardProcessesTable extends ControllerActionTable
 {
+    const NEW_PROCESS = 1;
+    const RUNNING = 2;
+    const COMPLETED = 3;
+    const ERROR = -1;
+
     public function initialize(array $config)
     {
         parent::initialize($config);
