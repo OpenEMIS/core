6f8c42a43aa2595c3b15b5c7c34e41834d6573a9
Acacia Wong <awong@kordit.com>
2018-05-03 09:30:43 +0800

    1. Default value for Max Students Per Class and Per Subject in the System Config is set to 100 2. Added field validations to ensure that user input for maximum number of students is not greater than 100 3. Added permission for ClassStudents and SubjectStudents to have access to the ConfigItems webservice 4. Added ConfigItems webservices to get the maximum number of students per class and per subjects 5. Added validation check to ensure that the number of students added into each class/subject is not more than the value in the system config. If found true, error message will display

--- config/Migrations/20180427112300_POCOR4546.php
+++ config/Migrations/20180427112300_POCOR4546.php
@@ -8,11 +8,11 @@ class POCOR4546 extends AbstractMigration
     {
         $this->execute('INSERT INTO `config_items` 
             (`id`, `name`, `code`, `type`, `label`, `value`, `default_value`, `editable`, `visible`, `field_type`, `option_type`, `created_user_id`, `created`) VALUES 
-            (1015, "Max Students Per Class", "max_students_per_class", "Student Settings", "Max Students Per Class", "", "0", 1, 1, "", "", 1, CURRENT_DATE())');
+            (1015, "Max Students Per Class", "max_students_per_class", "Student Settings", "Max Students Per Class", "", "100", 1, 1, "", "", 1, CURRENT_DATE())');
 
         $this->execute('INSERT INTO `config_items`
             (`id`, `name`, `code`, `type`, `label`, `value`, `default_value`, `editable`, `visible`, `field_type`, `option_type`, `created_user_id`, `created`) VALUES
-            (1016, "Max Students Per Subject", "max_students_per_subject", "Student Settings", "Max Students Per Subject", "", "0", 1, 1, "", "", 1, CURRENT_DATE())');
+            (1016, "Max Students Per Subject", "max_students_per_subject", "Student Settings", "Max Students Per Subject", "", "100", 1, 1, "", "", 1, CURRENT_DATE())');
     }
 
     public function down()
