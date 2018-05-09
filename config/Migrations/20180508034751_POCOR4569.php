395e37f19656550e7753797c5995b6ab4e8b6b21
Kok Keong <kcheng@kordit.com>
2018-05-09 16:48:04 +0800

    POCOR-4569 - Updated migration patch with assignee_id and status_id - Dependency to POCOR-4529

--- config/Migrations/20180508034751_POCOR4569.php
+++ config/Migrations/20180508034751_POCOR4569.php
@@ -70,6 +70,17 @@ class POCOR4569 extends AbstractMigration
                 'null' => false,
                 'comment' => 'links to appraisal_forms.id'
             ])
+            ->addColumn('assignee_id', 'integer', [
+                'limit' => 11,
+                'null' => false,
+                'default' => 0,
+                'comment' => 'links to security_users.id'
+            ])
+            ->addColumn('status_id', 'integer', [
+                'limit' => 11,
+                'null' => false,
+                'comment' => 'links to workflow_steps.id'
+            ])
             ->addColumn('modified_user_id', 'integer', [
                 'limit' => 11,
                 'null' => true,
@@ -99,8 +110,8 @@ class POCOR4569 extends AbstractMigration
 
         $this->execute('
             INSERT INTO `institution_staff_appraisals` 
-                (`id`, `title`, `appraisal_period_from`, `appraisal_period_to`, `date_appraised`, `file_name`, `file_content`, `comment`, `institution_id`, `staff_id`, `appraisal_type_id`, `appraisal_period_id`, `appraisal_form_id`, `modified_user_id`, `modified`, `created_user_id`, `created`)
-            SELECT `id`, `title`, `from`, `to`, `created`, `file_name`, `file_content`, `comment`, `institution_id`, `staff_id`, `appraisal_type_id`, `appraisal_period_id`, `appraisal_form_id`, `modified_user_id`, `modified`, `created_user_id`, `created`
+                (`id`, `title`, `appraisal_period_from`, `appraisal_period_to`, `date_appraised`, `file_name`, `file_content`, `comment`, `institution_id`, `staff_id`, `appraisal_type_id`, `appraisal_period_id`, `appraisal_form_id`, `assignee_id`, `status_id`, `modified_user_id`, `modified`, `created_user_id`, `created`)
+            SELECT `id`, `title`, `from`, `to`, `created`, `file_name`, `file_content`, `comment`, `institution_id`, `staff_id`, `appraisal_type_id`, `appraisal_period_id`, `appraisal_form_id`, `assignee_id`, `status_id`, `modified_user_id`, `modified`, `created_user_id`, `created`
             FROM `z_4569_institution_staff_appraisals`
         ');
     }
