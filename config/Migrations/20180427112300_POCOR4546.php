ed6801c7d0da820aa5aade22c41c3e03ef21d085
Acacia Wong <awong@kordit.com>
2018-05-09 17:31:11 +0800

    Change ID for Max Students Per Class to use 1017 and Max Students Per Subject to use 1018

--- config/Migrations/20180427112300_POCOR4546.php
+++ config/Migrations/20180427112300_POCOR4546.php
@@ -8,16 +8,16 @@ class POCOR4546 extends AbstractMigration
     {
         $this->execute('INSERT INTO `config_items` 
             (`id`, `name`, `code`, `type`, `label`, `value`, `default_value`, `editable`, `visible`, `field_type`, `option_type`, `created_user_id`, `created`) VALUES 
-            (1015, "Maximum Students Per Class", "max_students_per_class", "Student Settings", "Maximum Students Per Class", "", "100", 1, 1, "", "", 1, CURRENT_DATE())');
+            (1017, "Maximum Students Per Class", "max_students_per_class", "Student Settings", "Maximum Students Per Class", "", "100", 1, 1, "", "", 1, CURRENT_DATE())');
 
         $this->execute('INSERT INTO `config_items`
             (`id`, `name`, `code`, `type`, `label`, `value`, `default_value`, `editable`, `visible`, `field_type`, `option_type`, `created_user_id`, `created`) VALUES
-            (1016, "Maximum Students Per Subject", "max_students_per_subject", "Student Settings", "Maximum Students Per Subject", "", "100", 1, 1, "", "", 1, CURRENT_DATE())');
+            (1018, "Maximum Students Per Subject", "max_students_per_subject", "Student Settings", "Maximum Students Per Subject", "", "100", 1, 1, "", "", 1, CURRENT_DATE())');
     }
 
     public function down()
     {
-        $this->execute('DELETE FROM `config_items` WHERE `id` = 1015');
-        $this->execute('DELETE FROM `config_items` WHERE `id` = 1016');
+        $this->execute('DELETE FROM `config_items` WHERE `id` = 1017');
+        $this->execute('DELETE FROM `config_items` WHERE `id` = 1018');
     }
 }
