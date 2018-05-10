27d605ff7dbc96e3e1d77495d8d5853b8cab9d8a
Livvi Sim <lsim@kordit.com>
2018-05-10 15:10:47 +0800

    POCOR-2813 - Cleaned up model files

--- plugins/Scholarship/src/Model/Table/FinancialAssistanceTypesTable.php
+++ plugins/Scholarship/src/Model/Table/FinancialAssistanceTypesTable.php
@@ -1,16 +1,15 @@
 <?php
 namespace Scholarship\Model\Table;
 
-use Cake\Event\Event;
-use Cake\Validation\Validator;
 use App\Model\Table\AppTable;
 
 class FinancialAssistanceTypesTable extends AppTable
 {
     public function initialize(array $config)
     {
-    	$this->table('scholarship_financial_assistance_types');
+        $this->table('scholarship_financial_assistance_types');
         parent::initialize($config);
+
         $this->hasMany('Scholarships', ['className' => 'Scholarship.Scholarships', 'dependent' => true, 'cascadeCallbacks' => true]);
     }
 }
