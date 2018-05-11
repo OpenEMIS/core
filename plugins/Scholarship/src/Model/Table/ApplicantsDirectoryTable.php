5d9c537441573ef0959e9fff6b1f17f2f06b00e5
Livvi Sim <lsim@kordit.com>
2018-05-11 18:36:42 +0800

    POCOR-2813 - Renamed ScholarshipApplicationDirectoriesController to ApplicantsDirectoryController - Renamed ApplicationDirectoriesTable to ApplicantsDirectoryTable - Refactored code

--- plugins/Scholarship/src/Model/Table/ApplicantsDirectoryTable.php
+++ plugins/Scholarship/src/Model/Table/ApplicantsDirectoryTable.php
@@ -4,25 +4,25 @@ namespace Scholarship\Model\Table;
 use Cake\ORM\Query;
 use App\Model\Table\AppTable;
 
-class ApplicationDirectoriesTable extends AppTable
+class ApplicantsDirectoryTable extends AppTable
 {
     public function initialize(array $config)
     {
         $this->table('security_users');
         parent::initialize($config);
-        $this->entityClass('User.User');
 
         $this->belongsTo('Genders', ['className' => 'User.Genders']);
         $this->belongsTo('AddressAreas', ['className' => 'Area.AreaAdministratives', 'foreignKey' => 'address_area_id']);
         $this->belongsTo('BirthplaceAreas', ['className' => 'Area.AreaAdministratives', 'foreignKey' => 'birthplace_area_id']);
         $this->belongsTo('MainNationalities', ['className' => 'FieldOption.Nationalities', 'foreignKey' => 'nationality_id']);
         $this->belongsTo('IdentityTypes', ['className' => 'FieldOption.IdentityTypes', 'foreignKey' => 'identity_type_id']);
+
+        $this->entityClass('User.User');
     }
 
     public function findIndex(Query $query, array $options)
     {
         $query->where([$this->aliasField('super_admin') => 0]);
-
         return $query;
     }
 }
