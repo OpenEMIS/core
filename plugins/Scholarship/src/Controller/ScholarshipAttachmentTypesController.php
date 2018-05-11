037fe7e22901289a450b5483670a6d89b44b0a03
Livvi Sim <lsim@kordit.com>
2018-05-11 11:14:30 +0800

    POCOR-2813 - Fixed alignment - Fixed breadcrumbs and header for ScholarshipsTable and ScholarshipAttachmentTypesController - Added validation for AttachmentTypesTable - Fixed rendering of the is_mandatory field during delete for ScholarshipAttachmentTypesController

--- plugins/Scholarship/src/Controller/ScholarshipAttachmentTypesController.php
+++ plugins/Scholarship/src/Controller/ScholarshipAttachmentTypesController.php
@@ -9,16 +9,16 @@ use App\Model\Traits\OptionsTrait;
 
 class ScholarshipAttachmentTypesController extends PageController
 {
-     use OptionsTrait;
+    use OptionsTrait;
 
     private $mandatoryOptions = [];
 
     public function initialize()
     {
         parent::initialize();
-        $this->loadModel('Scholarship.Scholarships'); 
-        $this->loadModel('Scholarship.AttachmentTypes'); 
-        $this->Page->loadElementsFromTable($this->AttachmentTypes); 
+        $this->loadModel('Scholarship.Scholarships');
+        $this->loadModel('Scholarship.AttachmentTypes');
+        $this->Page->loadElementsFromTable($this->AttachmentTypes);
         $this->mandatoryOptions = $this->getSelectOptions('general.yesno');
     }
 
@@ -26,27 +26,25 @@ class ScholarshipAttachmentTypesController extends PageController
     {
         $event = parent::implementedEvents();
         $event['Controller.Page.onRenderIsMandatory'] = 'onRenderIsMandatory';
-
         return $event;
     }
 
     public function beforeFilter(Event $event)
-    {   
+    {
         $page = $this->Page;
-
         parent::beforeFilter($event);
 
-        $queryString = $this->request->query['queryString'];
+        $queryString = $this->request->query('queryString');
         $scholarshipId = $this->paramsDecode($queryString)['id'];
         $scholarshipName = $this->Scholarships->get($scholarshipId)->name;
 
         $page->setQueryString('scholarship_id', $scholarshipId);
 
-        $page->setHeader($scholarshipName . ' - ' . __('Attachments'));     
-        
-        $page->addCrumb('Scholarships', ['plugin' => 'Scholarship', 'controller' => 'Scholarships', 'action' => 'index']);
+        $page->setHeader($scholarshipName . ' - ' . __('Attachments'));
+
+        $page->addCrumb(__('Scholarships'), ['plugin' => 'Scholarship', 'controller' => 'Scholarships', 'action' => 'Scholarships', 'index']);
         $page->addCrumb($scholarshipName);
-        $page->addCrumb('Attachment');
+        $page->addCrumb(__('Attachments'));
 
         $page->get('scholarship_id')
             ->setControlType('hidden')
@@ -60,9 +58,9 @@ class ScholarshipAttachmentTypesController extends PageController
 
         $this->setupTabElements();
     }
-    
+
     public function index()
-    {       
+    {
         parent::index();
         $page = $this->Page;
 
@@ -122,13 +120,10 @@ class ScholarshipAttachmentTypesController extends PageController
     {
         $page = $this->Page;
 
-        if ($page->is(['index', 'view', 'delete'])) {
-            if($entity->is_mandatory == 1) {
-                return "<i class='fa fa-check'></i>";
-            } else {
-                return "<i class='fa fa-close'></i>";
-            }
+        if ($page->is(['index', 'view'])) {
+            return $entity->is_mandatory ? "<i class='fa fa-check'></i>" : "<i class='fa fa-close'></i>";
+        } elseif ($page->is(['delete'])) {
+            return $this->mandatoryOptions[$entity->is_mandatory];
         }
     }
-    
 }
