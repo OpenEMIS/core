19d6be0d53cea5a265db9528698c52da799df8ab
Livvi Sim <lsim@kordit.com>
2018-05-16 14:13:05 +0800

    POCOR-2813 - Moved scholarship tabs into the ScholarshipTabsComponent

--- plugins/Scholarship/src/Controller/Component/ScholarshipTabsComponent.php
+++ plugins/Scholarship/src/Controller/Component/ScholarshipTabsComponent.php
@@ -6,14 +6,66 @@ use Cake\Controller\Component;
 class ScholarshipTabsComponent extends Component
 {
     public $components = ['TabPermission', 'Page.Page'];
+    private $queryString;
 
     public function initialize(array $config)
     {
         $this->controller = $this->_registry->getController();
+        $this->queryString = $this->request->query('queryString');
+    }
+
+    public function getScholarshipTabs($options = [])
+    {
+        $tabElements = [
+            'Scholarships' => [
+                'url' => ['plugin' => 'Scholarship', 'controller' => 'Scholarships', 'action' => 'Scholarships', 'view', $this->queryString, 'queryString' => $this->queryString],
+                'text' => __('Overview')
+            ],
+            'Attachments' => [
+                'url' => ['plugin' => 'Scholarship', 'controller' => 'ScholarshipAttachmentTypes', 'action' => 'index', 'queryString' => $this->queryString],
+                'text' => __('Attachments')
+            ]
+        ];
+        return $this->TabPermission->checkTabPermission($tabElements);
     }
 
     public function getScholarshipApplicationTabs($options = [])
     {
+        $tabElements = [
+            'Applications' => [
+                'url' => ['plugin' => 'Scholarship', 'controller' => 'Scholarships', 'action' => 'Applications', 'view', $this->queryString, 'queryString' => $this->queryString],
+                'text' => __('Overview')
+            ],
+            'Identities' => [
+                'url' => ['plugin' => 'Scholarship', 'controller' => 'Scholarships', 'action' => 'Identities', 'index', 'queryString' => $this->queryString],
+                'text' => __('Identities')
+            ],
+            'UserNationalities' => [
+                'url' => ['plugin' => 'Scholarship', 'controller' => 'Scholarships', 'action' => 'Nationalities', 'index', 'queryString' => $this->queryString],
+                'text' => __('Nationalities')
+            ],
+            'Contacts' => [
+                'url' => ['plugin' => 'Scholarship', 'controller' => 'Scholarships', 'action' => 'Contacts', 'index', 'queryString' => $this->queryString],
+                'text' => __('Contacts')
+            ],
+            'Guardians' => [
+                'url' => ['plugin' => 'Scholarship', 'controller' => 'Scholarships', 'action' => 'Guardians', 'index', 'queryString' => $this->queryString],
+                'text' => __('Guardians')
+            ],
+            'Histories' => [
+                'url' => ['plugin' => 'Scholarship', 'controller' => 'Scholarships', 'action' => 'Histories',  'index', 'queryString' => $this->queryString],
+                'text' => __('Scholarship History')
+            ],
+            'InstitutionChoices' => [
+                'url' => ['plugin' => 'Scholarship', 'controller' => 'ScholarshipApplicationInstitutionChoices', 'action' => 'index', 'queryString' => $this->queryString],
+                'text' => __('Institution Choices')
+            ],
+            'Attachments' => [
+                'url' => ['plugin' => 'Scholarship', 'controller' => 'ScholarshipApplicationAttachments', 'action' => 'index', 'queryString' => $this->queryString],
+                'text' => __('Attachments')
+            ]
+        ];
+        return $this->TabPermission->checkTabPermission($tabElements);
     }
 
     public function getScholarshipRecipientTabs($options = [])
