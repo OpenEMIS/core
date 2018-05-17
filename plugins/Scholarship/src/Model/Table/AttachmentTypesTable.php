86399f782dae5c4176ff02614b04e76ab2e0e369
Thed Kuok Shiun <kthed@kordit.com>
2018-05-17 16:21:37 +0800

    POCOR-2813 - Change association of Scholarship and Attachment Types to become many to many.

--- plugins/Scholarship/src/Model/Table/AttachmentTypesTable.php
+++ plugins/Scholarship/src/Model/Table/AttachmentTypesTable.php
@@ -2,38 +2,62 @@
 namespace Scholarship\Model\Table;
 
 use Cake\ORM\Query;
-use Cake\Validation\Validator;
-use App\Model\Table\AppTable;
+use Cake\ORM\TableRegistry;
+use App\Model\Table\ControllerActionTable;
 
-class AttachmentTypesTable extends AppTable
+class AttachmentTypesTable extends ControllerActionTable
 {
     public function initialize(array $config)
     {
         $this->table('scholarship_attachment_types');
         parent::initialize($config);
 
-        $this->belongsTo('Scholarships', ['className' => 'Scholarship.Scholarships']);
-        $this->hasMany('ApplicationAttachments', ['className' => 'Scholarship.ApplicationAttachments', 'foreignKey' => 'scholarship_attachment_type_id', 'dependent' => true, 'cascadeCallbacks' => true]);
+        $this->belongsToMany('Scholarships', [
+            'className' => 'Scholarship.Scholarships',
+            'joinTable' => 'scholarships_scholarship_attachment_types',
+            'foreignKey' => 'scholarship_attachment_type_id',
+            'targetForeignKey' => 'scholarship_id',
+            'through' => 'Scholarship.AttachmentTypes',
+            'dependent' => true,
+            'cascadeCallbacks' => true
+        ]);
+
+        $this->addBehavior('FieldOption.FieldOption');
+        $this->setDeleteStrategy('restrict');
     }
 
-    public function validationDefault(Validator $validator)
+    public function findAvailableAttachmentTypes(Query $query, array $options)
     {
-        $validator = parent::validationDefault($validator);
-        return $validator
-            ->add('name', [
-                'ruleUniqueCode' => [
-                    'rule' => ['validateUnique', ['scope' => 'scholarship_id']],
-                    'provider' => 'table'
-                ]
+        $applicantId = array_key_exists('applicant_id', $options) ? $options['applicant_id'] : null;
+        $scholarshipId = array_key_exists('scholarship_id', $options) ? $options['scholarship_id'] : null;
+
+        $ApplicationAttachmentsTable = TableRegistry::get('Scholarship.ApplicationAttachments');
+        $existingAttachmentTypeIds = $ApplicationAttachmentsTable
+            ->find('list', [
+                'keyField' => 'scholarship_attachment_type_id',
+                'valueField' => 'scholarship_attachment_type_id'
             ])
-            ->requirePresence(['name', 'is_mandatory']);
-    }
+            ->where([
+                $ApplicationAttachmentsTable->aliasField('applicant_id') => $applicantId,
+                $ApplicationAttachmentsTable->aliasField('scholarship_id') => $scholarshipId
+            ])
+            ->toArray();
 
-    public function findAttachmentTypeOptionList(Query $query, array $options)
-    {
-        $scholarshipId = array_key_exists('scholarship_id', $options) ? $options['scholarship_id'] : 0;
+        $ScholarshipsScholarshipAttachmentTypesTable = TableRegistry::get('Scholarship.ScholarshipsScholarshipAttachmentTypes');
+        $query
+            ->find('visible')
+            ->find('order')
+            ->innerJoin(
+                [$ScholarshipsScholarshipAttachmentTypesTable->alias() => $ScholarshipsScholarshipAttachmentTypesTable->table()],
+                [
+                    $ScholarshipsScholarshipAttachmentTypesTable->aliasField('scholarship_attachment_type_id = ') . $this->aliasField('id'),
+                    $ScholarshipsScholarshipAttachmentTypesTable->aliasField('scholarship_id') => $scholarshipId
+                ]
+            )
+            ->where([
+                $this->aliasField('id NOT IN') => $existingAttachmentTypeIds
+            ]);
 
-        $query->where(['scholarship_id' => $scholarshipId]);
-        return parent::findOptionList($query, $options);
+        return $query;
     }
 }
