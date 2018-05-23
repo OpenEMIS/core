ed35863eda6869d82a398f03dde5988a41e867c4
Acacia Wong <awong@kordit.com>
2018-05-23 12:07:19 +0800

    Remove Title fields from Directory Staff Appraisals.

--- plugins/Institution/src/Model/Table/AppraisalsTable.php
+++ plugins/Institution/src/Model/Table/AppraisalsTable.php
@@ -71,7 +71,7 @@ class AppraisalsTable extends ControllerActionTable
         $this->field('file_content', ['visible' => false]);
         $this->field('comment', ['visible' => false]);
         $this->field('appraisal_period_id', ['visible' => false]);
-        $this->setFieldOrder(['appraisal_type_id', 'appraisal_form_id', 'title', 'appraisal_period_from', 'appraisal_period_to', 'date_appraised']);
+        $this->setFieldOrder(['appraisal_type_id', 'appraisal_form_id', 'appraisal_period_from', 'appraisal_period_to', 'date_appraised']);
         $this->setupTabElements();
     }
 
@@ -96,7 +96,6 @@ class AppraisalsTable extends ControllerActionTable
         );
         // End
         $this->field('staff_id', ['visible' => false]);
-        $this->field('title');
         $this->field('academic_period_id', ['fieldName' => 'appraisal_period.academic_period.name']);
         $this->field('institution_id');
         $this->field('appraisal_period_from');
@@ -108,7 +107,7 @@ class AppraisalsTable extends ControllerActionTable
         $this->field('file_content', ['visible' => false]);
         $this->field('comment');
         $this->printAppraisalCustomField($entity->appraisal_form_id, $entity);
-        $this->setFieldOrder(['academic_period_id', 'appraisal_type_id', 'appraisal_period_id', 'appraisal_form_id', 'title', 'appraisal_period_from', 'appraisal_period_to', 'date_appraised', 'file_content', 'comment']);
+        $this->setFieldOrder(['academic_period_id', 'appraisal_type_id', 'appraisal_period_id', 'appraisal_form_id', 'appraisal_period_from', 'appraisal_period_to', 'date_appraised', 'file_content', 'comment']);
     }
 
     private function printAppraisalCustomField($appraisalFormId, Entity $entity)
@@ -157,7 +156,7 @@ class AppraisalsTable extends ControllerActionTable
                 ]);
                 if ($section != $details['section']) {
                     $section = $details['section'];
-                    $this->field('section' . $sectionCount++, ['type' => 'section', 'title' => $details['section']]);
+                    $this->field('section' . $sectionCount++, ['type' => 'section']);
                 }
                 $this->appraisalCustomFieldExtra($details, $formCritieria, $criteriaCounter, $entity);
             }
