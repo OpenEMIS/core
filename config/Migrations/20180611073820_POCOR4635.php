461f5a6188de98d15af4885137008375a83465bb
Livvi Sim <lsim@kordit.com>
2018-06-11 16:50:29 +0800

    POCOR-4635 - Added migration to setup equipment tables

---
+++ config/Migrations/20180611073820_POCOR4635.php
@@ -0,0 +1,272 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class POCOR4635 extends AbstractMigration
+{
+    public function up()
+    {
+        // equipment_types
+        $table = $this->table('equipment_types', [
+            'collation' => 'utf8mb4_unicode_ci',
+            'comment' => 'This is a field option table containing the list of user-defined equipment types used by institution equipments'
+        ]);
+        $table
+            ->addColumn('name', 'string', [
+                'default' => null,
+                'limit' => 50,
+                'null' => false
+            ])
+            ->addColumn('order', 'integer', [
+                'default' => null,
+                'limit' => 3,
+                'null' => false
+            ])
+            ->addColumn('visible', 'integer', [
+                'default' => 1,
+                'limit' => 1,
+                'null' => false
+            ])
+            ->addColumn('editable', 'integer', [
+                'default' => 1,
+                'limit' => 1,
+                'null' => false
+            ])
+            ->addColumn('default', 'integer', [
+                'default' => 0,
+                'limit' => 1,
+                'null' => false
+            ])
+            ->addColumn('international_code', 'string', [
+                'default' => null,
+                'limit' => 50,
+                'null' => true
+            ])
+            ->addColumn('national_code', 'string', [
+                'default' => null,
+                'limit' => 50,
+                'null' => true
+            ])
+            ->addColumn('modified_user_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => true
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => null,
+                'null' => true
+            ])
+            ->addColumn('created_user_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => false
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'null' => false
+            ])
+            ->addIndex('modified_user_id')
+            ->addIndex('created_user_id')
+            ->save();
+
+        // equipment_purposes
+        $table = $this->table('equipment_purposes', [
+            'collation' => 'utf8mb4_unicode_ci',
+            'comment' => 'This is a field option table containing the list of user-defined equipment purposes used by institution equipments'
+        ]);
+        $table
+            ->addColumn('name', 'string', [
+                'default' => null,
+                'limit' => 50,
+                'null' => false
+            ])
+            ->addColumn('order', 'integer', [
+                'default' => null,
+                'limit' => 3,
+                'null' => false
+            ])
+            ->addColumn('visible', 'integer', [
+                'default' => 1,
+                'limit' => 1,
+                'null' => false
+            ])
+            ->addColumn('editable', 'integer', [
+                'default' => 1,
+                'limit' => 1,
+                'null' => false
+            ])
+            ->addColumn('default', 'integer', [
+                'default' => 0,
+                'limit' => 1,
+                'null' => false
+            ])
+            ->addColumn('international_code', 'string', [
+                'default' => null,
+                'limit' => 50,
+                'null' => true
+            ])
+            ->addColumn('national_code', 'string', [
+                'default' => null,
+                'limit' => 50,
+                'null' => true
+            ])
+            ->addColumn('modified_user_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => true
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => null,
+                'null' => true
+            ])
+            ->addColumn('created_user_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => false
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'null' => false
+            ])
+            ->addIndex('modified_user_id')
+            ->addIndex('created_user_id')
+            ->save();
+
+        // equipment_conditions
+        $table = $this->table('equipment_conditions', [
+            'collation' => 'utf8mb4_unicode_ci',
+            'comment' => 'This is a field option table containing the list of user-defined equipment conditions used by institution equipments'
+        ]);
+        $table
+            ->addColumn('name', 'string', [
+                'default' => null,
+                'limit' => 50,
+                'null' => false
+            ])
+            ->addColumn('order', 'integer', [
+                'default' => null,
+                'limit' => 3,
+                'null' => false
+            ])
+            ->addColumn('visible', 'integer', [
+                'default' => 1,
+                'limit' => 1,
+                'null' => false
+            ])
+            ->addColumn('editable', 'integer', [
+                'default' => 1,
+                'limit' => 1,
+                'null' => false
+            ])
+            ->addColumn('default', 'integer', [
+                'default' => 0,
+                'limit' => 1,
+                'null' => false
+            ])
+            ->addColumn('international_code', 'string', [
+                'default' => null,
+                'limit' => 50,
+                'null' => true
+            ])
+            ->addColumn('national_code', 'string', [
+                'default' => null,
+                'limit' => 50,
+                'null' => true
+            ])
+            ->addColumn('modified_user_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => true
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => null,
+                'null' => true
+            ])
+            ->addColumn('created_user_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => false
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'null' => false
+            ])
+            ->addIndex('modified_user_id')
+            ->addIndex('created_user_id')
+            ->save();
+
+        // institution_equipments
+        $table = $this->table('institution_equipment', [
+            'collation' => 'utf8mb4_unicode_ci',
+            'comment' => 'This table contains all equipment used by institutions'
+        ]);
+        $table
+            ->addColumn('code', 'string', [
+                'null' => true,
+                'limit' => 50
+            ])
+            ->addColumn('name', 'string', [
+                'null' => false,
+                'limit' => 250
+            ])
+            ->addColumn('academic_period_id', 'integer', [
+                'null' => false,
+                'limit' => 11,
+                'comment' => 'links to academic_periods.id'
+            ])
+            ->addColumn('institution_id', 'integer', [
+                'null' => false,
+                'limit' => 11,
+                'comment' => 'links to institutions.id'
+            ])
+            ->addColumn('equipment_type_id', 'integer', [
+                'null' => false,
+                'limit' => 11,
+                'comment' => 'links to equipment_types.id'
+            ])
+            ->addColumn('equipment_purpose_id', 'integer', [
+                'null' => false,
+                'limit' => 11,
+                'comment' => 'links to equipment_purposes.id'
+            ])
+            ->addColumn('equipment_condition_id', 'integer', [
+                'null' => false,
+                'limit' => 11,
+                'comment' => 'links to equipment_conditions.id'
+            ])
+            ->addColumn('modified_user_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => true
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => null,
+                'null' => true
+            ])
+            ->addColumn('created_user_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => false
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'null' => false
+            ])
+            ->addIndex('academic_period_id')
+            ->addIndex('institution_id')
+            ->addIndex('equipment_type_id')
+            ->addIndex('equipment_purpose_id')
+            ->addIndex('equipment_condition_id')
+            ->addIndex('modified_user_id')
+            ->addIndex('created_user_id')
+            ->save();
+    }
+
+    public function down()
+    {
+        $this->dropTable('equipment_types');
+        $this->dropTable('equipment_purposes');
+        $this->dropTable('equipment_conditions');
+        $this->dropTable('institution_equipment');
+    }
+}
