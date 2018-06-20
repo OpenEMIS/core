62318627400df958c359c9fca553685e4618750a
Acacia Wong <awong@kordit.com>
2018-06-20 18:07:38 +0800

    POCOR-4617
    Meeting Date Validation

--- plugins/Institution/src/Model/Table/InstitutionCommitteesTable.php
+++ plugins/Institution/src/Model/Table/InstitutionCommitteesTable.php
@@ -2,9 +2,12 @@
 namespace Institution\Model\Table;
 
 use App\Model\Table\AppTable;
+use Cake\Validation\Validator;
+use App\Model\Traits\MessagesTrait;
 
 class InstitutionCommitteesTable extends AppTable
 {
+    use MessagesTrait;
     public function initialize(array $config)
     {
         parent::initialize($config);
@@ -12,9 +15,19 @@ class InstitutionCommitteesTable extends AppTable
         $this->belongsTo('Institutions', ['className' => 'Institution.Institutions', 'foreignKey' =>'institution_id']);
         $this->belongsTo('InstitutionCommitteeTypes', ['className' => 'Institutions.InstitutionCommitteeTypes']);
         $this->hasMany('InstitutionCommitteeAttachments', [
-            'className' => 'Institutions.InstitutionCommitteeAttachments', 
+            'className' => 'Institutions.InstitutionCommitteeAttachments',
             'dependent' => true,
             'cascadeCallbacks' => true
         ]);
+    }
+
+    public function validationDefault(Validator $validator)
+    {
+        $validator = parent::validationDefault($validator);
+
+        return $validator
+            ->add('meeting_date', 'ruleInAcademicPeriod', [
+                'rule' => ['inAcademicPeriod', 'academic_period_id', []]
+            ]);
     }
 }
