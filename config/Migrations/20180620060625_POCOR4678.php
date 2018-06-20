15085d6bb88bc3acadec7d05a483d60b1f09a671
Livvi Sim <lsim@kordit.com>
2018-06-20 17:33:07 +0800

    POCOR-4678 - Back up institution_assets - Update all purpose in institution_assets to be 1

--- config/Migrations/20180620060625_POCOR4678.php
+++ config/Migrations/20180620060625_POCOR4678.php
@@ -10,6 +10,9 @@ class POCOR4678 extends AbstractMigration
         $this->table('asset_purposes')->rename('z_4678_asset_purposes');
 
         // institution_assets
+        $this->execute('CREATE TABLE `z_4678_institution_assets` LIKE `institution_assets`');
+        $this->execute('INSERT INTO `z_4678_institution_assets` SELECT * FROM `institution_assets`');
+
         $table = $this->table('institution_assets');
         $table
             ->renameColumn('asset_purpose_id', 'purpose')
@@ -21,6 +24,8 @@ class POCOR4678 extends AbstractMigration
             ])
             ->removeIndexByName('asset_purpose_id')
             ->save();
+
+        $this->execute('UPDATE `institution_assets` SET `purpose` = 1');
     }
 
     public function down()
@@ -29,16 +34,7 @@ class POCOR4678 extends AbstractMigration
         $this->table('z_4678_asset_purposes')->rename('asset_purposes');
 
         // institution_assets
-        $table = $this->table('institution_assets');
-        $table
-            ->renameColumn('purpose', 'asset_purpose_id')
-            ->changeColumn('asset_purpose_id', 'integer', [
-                'null' => false,
-                'limit' => 11,
-                'comment' => 'links to asset_purposes.id',
-                'after' => 'asset_type_id'
-            ])
-            ->addIndex('asset_purpose_id')
-            ->save();
+        $this->dropTable('institution_assets');
+        $this->table('z_4678_institution_assets')->rename('institution_assets');
     }
 }
