0b05cba90ee899278d15967ee6ffe866e1fcd6f3
konghongyi <hkong@kordit.com>
2018-06-22 18:09:03 +0800

    POCOR-4634 - Added Guardians Controller in Guardian plugin.

---
+++ plugins/Guardian/src/Controller/GuardianCommentsController.php
@@ -0,0 +1,35 @@
+<?php
+namespace Guardian\Controller;
+
+use Cake\Event\Event;
+use Profile\Controller\CommentsController as BaseController;
+
+class GuardianCommentsController extends BaseController
+{
+    public function beforeFilter(Event $event)
+    {
+        $page = $this->Page;
+        $requestQuery = $this->request->query;
+        $userId = $this->paramsDecode($requestQuery['queryString'])['security_user_id'];
+        $userName = $this->Users->get($userId)->name;
+
+        parent::beforeFilter($event);
+
+        // setup
+        $page->setHeader($userName . ' - ' . __('Comments'));
+        $page->setQueryString('security_user_id', $userId);
+        $this->setBreadCrumb(['userId' => $userId, 'userName' => $userName]);
+        $this->setupTabElements(['userId' => $userId, 'userName' => $userName]);
+    }
+
+    public function add()
+    {
+        $page = $this->Page;
+        $requestQuery = $this->request->query;
+
+        $userId = $this->paramsDecode($requestQuery['queryString'])['security_user_id'];
+        $page->get('security_user_id')->setValue($userId);
+
+        parent::add();
+    }
+}
