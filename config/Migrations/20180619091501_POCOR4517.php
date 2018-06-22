ed9d25b0aa4a515359fe73223be4de106e547590
Acacia Wong <awong@kordit.com>
2018-06-22 11:54:47 +0800

    POCOR-4517
    Migration added for Staff and Student Behaviour Attachments
    Add Staff selected when user in in StaffBehaviourAttachments.add
    Update action button to direct user to Institutions Staff Behaviours instead

--- config/Migrations/20180619091501_POCOR4517.php
+++ config/Migrations/20180619091501_POCOR4517.php
@@ -130,11 +130,141 @@ class POCOR4517 extends AbstractMigration
             ->addIndex('created_user_id')
             ->save();
             // end staff_behaviour_attachments
+
+            //Student Behaviour Attachments
+            //Directory Module
+            $this->execute('UPDATE security_functions SET `order` = `order` + 1 WHERE `order` > 284');
+            $this->insert('security_functions', [
+                'id' => 7059,
+                'name' => 'Student Behaviour Attachments',
+                'controller' => 'Directories',
+                'module' => 'Directory',
+                'category' => 'Students - Academic',
+                'parent_id' => '7000',
+                '_view' => 'StudentBehaviours.index',
+                '_edit' => null,
+                '_add' => null,
+                '_delete' => null,
+                '_execute' => null,
+                'order' => 285,
+                'visible' => 1,
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ]);
+
+            //Institutions Module
+            $this->execute('UPDATE security_functions SET `order` = `order` + 1 WHERE `order` > 54');
+            $this->insert('security_functions', [
+                'id' => 3045,
+                'name' => 'Student Behaviour Attachments',
+                'controller' => 'StudentBehaviourAttachments',
+                'module' => 'Institutions',
+                'category' => 'Students',
+                'parent_id' => 8,
+                '_view' => 'index|view',
+                '_edit' => 'edit',
+                '_add' => 'add',
+                '_delete' => 'remove',
+                'order' => 55,
+                'visible' => 1,
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ]);
+
+            //Institutions Module
+            $this->execute('UPDATE security_functions SET `order` = `order` + 1 WHERE `order` > 122');
+            $this->insert('security_functions', [
+                'id' => 3046,
+                'name' => 'Student Behaviour Attachments',
+                'controller' => 'Students',
+                'module' => 'Institutions',
+                'category' => 'Students - Academic',
+                'parent_id' => '2000',
+                '_view' => 'Behaviours.index|Behaviours.view',
+                '_edit' => null,
+                '_add' => null,
+                '_delete' => null,
+                'order' => 123,
+                'visible' => 1,
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ]);
+
+            //Staff Behaviour Attachments
+            //Directory Module
+            $this->execute('UPDATE security_functions SET `order` = `order` + 1 WHERE `order` > 300');
+            $this->insert('security_functions', [
+                'id' => 7060,
+                'name' => 'Staff Behaviour Attachments',
+                'controller' => 'Directories',
+                'module' => 'Directory',
+                'category' => 'Staff - Career',
+                'parent_id' => '7000',
+                '_view' => 'StaffBehaviours.index',
+                '_edit' => null,
+                '_add' => null,
+                '_delete' => null,
+                'order' => 301,
+                'visible' => '1',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ]);
+            //Institutions Module
+            $this->execute('UPDATE security_functions SET `order` = `order` + 1 WHERE `order` > 61');
+            $this->insert('security_functions', [
+                'id' => 3047,
+                'name' => 'Staff Behaviour Attachments',
+                'controller' => 'StaffBehaviourAttachments',
+                'module' => 'Institutions',
+                'category' => 'Staff',
+                'parent_id' => 8,
+                '_view' => 'index|view',
+                '_edit' => 'edit',
+                '_add' => 'add',
+                '_delete' => 'remove',
+                'order' => 62,
+                'visible' => 1,
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ]);
+
+            //Institutions Module
+            $this->execute('UPDATE security_functions SET `order` = `order` + 1 WHERE `order` > 159');
+            $this->insert('security_functions', [
+                'id' => 3048,
+                'name' => 'Staff Behaviour Attachments',
+                'controller' => 'Staff',
+                'module' => 'Institutions',
+                'category' => 'Staff - Career',
+                'parent_id' => '3000',
+                '_view' => 'Behaviours.index|Behaviours.view',
+                '_edit' => null,
+                '_add' => null,
+                '_delete' => null,
+                'order' => 160,
+                'visible' => 1,
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ]);
     }
 
     public function down()
     {
         $this->dropTable('student_behaviour_attachments');
         $this->dropTable('staff_behaviour_attachments');
+
+        $this->execute('UPDATE security_functions SET `order` = `order` - 1 WHERE `order` > 54');
+        $this->execute('UPDATE security_functions SET `order` = `order` - 1 WHERE `order` > 61');
+        $this->execute('UPDATE security_functions SET `order` = `order` - 1 WHERE `order` > 122');
+        $this->execute('UPDATE security_functions SET `order` = `order` - 1 WHERE `order` > 159');
+        $this->execute('UPDATE security_functions SET `order` = `order` - 1 WHERE `order` > 284');
+        $this->execute('UPDATE security_functions SET `order` = `order` - 1 WHERE `order` > 300');
+
+        $this->execute('DELETE FROM security_functions WHERE id = 3045');
+        $this->execute('DELETE FROM security_functions WHERE id = 3046');
+        $this->execute('DELETE FROM security_functions WHERE id = 3047');
+        $this->execute('DELETE FROM security_functions WHERE id = 3048');
+        $this->execute('DELETE FROM security_functions WHERE id = 7059');
+        $this->execute('DELETE FROM security_functions WHERE id = 7060');
     }
 }
