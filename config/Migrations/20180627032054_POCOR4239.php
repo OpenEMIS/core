c8ed451a724cbf9c1df80536c4b595b1b3e836a9
konghongyi <hkong@kordit.com>
2018-06-28 10:47:24 +0800

    POCOR-4239 - reformat the code such as remove extra space or extra line break.

--- config/Migrations/20180627032054_POCOR4239.php
+++ config/Migrations/20180627032054_POCOR4239.php
@@ -7,7 +7,6 @@ class POCOR4239 extends AbstractMigration
     public function up()
     {
         $this->execute('UPDATE security_functions SET `order` = `order` + 1 WHERE `order` > 145');
-
         $this->insert('security_functions', [
             'id' => 3049,
             'name' => 'Transport',
@@ -26,12 +25,10 @@ class POCOR4239 extends AbstractMigration
         ]);
     }
 
-
     public function down()
     { 
         $this->execute('UPDATE security_functions SET `order` = `order` - 1 WHERE `order` > 145');
         $this->execute('DELETE FROM security_functions WHERE id = 3049');
-
     }
 }
 
