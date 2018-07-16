18455fb2966167e8faafbfc7bf94162c4ca03d2d
konghongyi <hkong@kordit.com>
2018-07-16 14:18:16 +0800

    POCOR-4722 - set longitude and latitude nullable

--- config/Migrations/20180712010553_POCOR4722.php
+++ config/Migrations/20180712010553_POCOR4722.php
@@ -7,8 +7,16 @@ class POCOR4722 extends AbstractMigration
     public function up(){
         $this->table('institutions')->rename('z_4722_institutions');           
         $this->execute('CREATE TABLE `institutions` LIKE `z_4722_institutions`');
-        $this->table('institutions')->changeColumn('longitude', 'string', ['limit' => 25])->save();;  
-        $this->table('institutions')->changeColumn('latitude', 'string', ['limit' => 25])->save();;          
+        $this->table('institutions')->changeColumn('longitude', 'string', [
+                                        'limit' => 25,
+                                        'null' =>true,
+                                    ])
+                                    ->save();
+        $this->table('institutions')->changeColumn('latitude', 'string', [
+                                        'limit' => 25,
+                                        'null' =>true,
+                                    ])
+                                    ->save();       
         $this->execute('INSERT INTO `institutions` SELECT * FROM `z_4722_institutions`'); 
     }
 
@@ -17,7 +25,3 @@ class POCOR4722 extends AbstractMigration
         $this->table('z_4722_institutions')->rename('institutions');
     }
 }
-
-
-
-
