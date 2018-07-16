c7a5088051bfb8014631e0d879c5a4b233d193a4
Thed Kuok Shiun <kthed@kordit.com>
2018-07-16 17:37:24 +0800

    POCOR-4722 - Fix alignment.

--- config/Migrations/20180712010553_POCOR4722.php
+++ config/Migrations/20180712010553_POCOR4722.php
@@ -4,23 +4,25 @@ use Phinx\Migration\AbstractMigration;
 
 class POCOR4722 extends AbstractMigration
 {
-    public function up(){
+    public function up()
+    {
         $this->table('institutions')->rename('z_4722_institutions');           
         $this->execute('CREATE TABLE `institutions` LIKE `z_4722_institutions`');
         $this->table('institutions')->changeColumn('longitude', 'string', [
-                                        'limit' => 25,
-                                        'null' =>true,
-                                    ])
-                                    ->save();
+                'limit' => 25,
+                'null' =>true,
+            ])
+            ->save();
         $this->table('institutions')->changeColumn('latitude', 'string', [
-                                        'limit' => 25,
-                                        'null' =>true,
-                                    ])
-                                    ->save();       
-        $this->execute('INSERT INTO `institutions` SELECT * FROM `z_4722_institutions`'); 
+                'limit' => 25,
+                'null' =>true,
+            ])
+            ->save();
+        $this->execute('INSERT INTO `institutions` SELECT * FROM `z_4722_institutions`');
     }
 
-    public function down(){
+    public function down()
+    {
         $this->dropTable('institutions');
         $this->table('z_4722_institutions')->rename('institutions');
     }
