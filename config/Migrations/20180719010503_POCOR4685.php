36e0d1fae1b5891248ebbbb0a331afac8fd358e3
Thed Kuok Shiun <kthed@kordit.com>
2018-07-19 09:15:44 +0800

    POCOR-4685 - Update migration patch with newer time stamp.

--- config/Migrations/20180719010503_POCOR4685.php
+++ config/Migrations/20180719010503_POCOR4685.php
@@ -6,9 +6,14 @@ class POCOR4685 extends AbstractMigration
 {
     public function up()
     {
-        $this->execute('RENAME TABLE `institution_class_students` TO `z_4685_institution_class_students`');
+        $this->table('institution_class_students')->rename('z_4685_institution_class_students');
         $this->execute('CREATE TABLE `institution_class_students` LIKE `z_4685_institution_class_students`');
-        $this->execute('ALTER TABLE `institution_class_students` ADD `next_institution_class_id` INT AFTER `academic_period_id`');
+        $this->table('institution_class_students')
+            ->addColumn('next_institution_class_id', 'integer', [
+                'after' => 'academic_period_id',
+                'null' => true
+            ])
+            ->save();
         $this->execute('
             INSERT INTO `institution_class_students` (`id`, `student_id`, `institution_class_id`, `education_grade_id`, `academic_period_id`, `next_institution_class_id`, `institution_id`, `student_status_id`, `modified_user_id`, `modified`, `created_user_id`, `created`)
             SELECT `id`, `student_id`, `institution_class_id`, `education_grade_id`, `academic_period_id`, NULL, `institution_id`, `student_status_id`, `modified_user_id`, `modified`, `created_user_id`, `created`
@@ -18,7 +23,7 @@ class POCOR4685 extends AbstractMigration
 
     public function down()
     {
-        $this->execute('DROP TABLE IF EXISTS `institution_class_students`');
-        $this->execute('RENAME TABLE `z_4685_institution_class_students` TO `institution_class_students`');
+        $this->dropTable('institution_class_students');
+        $this->table('z_4685_institution_class_students')->rename('institution_class_students');
     }
 }
