749afbcd2006d92aefbd13a251c32bb0fd5f0fd0
Acacia Wong <awong@kordit.com>
2018-08-03 17:58:50 +0800

    POCOR3906
    Populate the timepicker to time in and time out column.

--- plugins/Staff/src/Model/Table/InstitutionStaffAttendancesTable.php
+++ plugins/Staff/src/Model/Table/InstitutionStaffAttendancesTable.php
@@ -11,5 +11,11 @@ class InstitutionStaffAttendancesTable extends ControllerActionTable {
 	public function initialize(array $config) {
 		$this->table('institution_staff_attendances');
 		parent::initialize($config);
+        $this->belongsTo('Users', ['className' => 'Security.Users', 'foreignKey' => 'staff_id']);
+        $this->belongsTo('Institutions', ['className' => 'Institution.Institutions', 'foreignKey' => 'institution_id']);
+        $this->belongsTo('StaffStatuses', ['className' => 'Staff.StaffStatuses']);
+        $this->addBehavior('Restful.RestfulAccessControl', [
+            'InstitutionStaffAttendances' => ['index', 'view', 'edit']
+        ]);
 	}
 }
