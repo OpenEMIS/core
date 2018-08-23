501948b4e21e7ef85586c9f4a6e936e411233fda
konghongyi <hkong@kordit.com>
2018-08-23 15:32:28 +0800

    POCOR-4763 - Backup existing table

--- config/Migrations/20180820102257_POCOR4763.php
+++ config/Migrations/20180820102257_POCOR4763.php
@@ -6,15 +6,17 @@ class POCOR4763 extends AbstractMigration
 {
     public function up()
     {
+        $this->table('scholarships')->rename('z_4763_scholarships');
+        $this->execute('CREATE TABLE `scholarships` LIKE `z_4763_scholarships`');
+        $this->execute('INSERT INTO `scholarships` SELECT * FROM `z_4763_scholarships`');
         $users = $this->table('scholarships');
-        $users->addColumn('duration', 'integer', ['limit' => 2, 'null' => true, 'default' => null, 'after' => 'total_amount'])
+        $users->addColumn('duration', 'integer', ['limit' => 2, 'null' => false, 'default' => null, 'after' => 'total_amount'])
               ->save();
     }
 
     public function down()
     {
-        $table = $this->table('scholarships');
-        $table->removeColumn('duration')
-              ->save();      
+        $this->dropTable('scholarships');
+        $this->table('z_4763_scholarships')->rename('scholarships');    
     }    
 }
