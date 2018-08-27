53c3bedd9fe187db5f129f5983d086627a05362b
Acacia Wong <awong@kordit.com>
2018-08-27 10:34:14 +0800

    POCOR-3906
    Comment start time and end time if is not full day

--- plugins/Institution/webroot/js/angular/staff/institution.staff.attendances.svc.js
+++ plugins/Institution/webroot/js/angular/staff/institution.staff.attendances.svc.js
@@ -264,9 +264,9 @@ function InstitutionStaffAttendancesSvc($http, $q, $filter, KdDataSvc, AlertSvc)
                 var leaveTypeName = staffLeave._matchingData.StaffLeaveTypes.name;
                 data += '<i class="fa kd-attendance"></i> <font color="#CC5C5C">'+leaveTypeName + '</font><br>';
 
-                if (!full_day){
-                    data += start_time + '<br>' + end_time + '<br>';
-                }
+                // if (!full_day){
+                //     data += start_time + '<br>' + end_time + '<br>';
+                // }
                 // data += 'end of an index <br>';
             });
             data += '<i class="fa fa-file-text" style=" color: #FFFFFF; background-color:  #6699CC; border: 1px solid #6699CC;"></i><a href= "'+leaveIndexURL+ '"target="_blank"> View Details</a>';
