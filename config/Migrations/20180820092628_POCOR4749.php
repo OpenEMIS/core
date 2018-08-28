cbcb347b1e9634be09a655f4ba2918db762c352d
konghongyi <hkong@kordit.com>
2018-08-28 12:09:51 +0800

    POCOR-4749 - set the sequence for Financial Assistance Type. Solve POCOR-4763, move the word 'year' from option to label

--- config/Migrations/20180820092628_POCOR4749.php
+++ config/Migrations/20180820092628_POCOR4749.php
@@ -6,37 +6,47 @@ class POCOR4749 extends AbstractMigration
 {
     public function up()
     {
-
-        $sql = "UPDATE `scholarship_financial_assistance_types` 
-                SET `code` = 'FULLSCHOLARSHIP', 
-                    `name` = 'Full Scholarship' 
-                WHERE `name` = 'Scholarship'";
-
-        $this->execute($sql);
+        $this->table('scholarship_financial_assistance_types')->rename('z_4749_scholarship_financial_assistance_types');
+        $this->execute('CREATE TABLE `scholarship_financial_assistance_types` LIKE `z_4749_scholarship_financial_assistance_types`');
+        $this->table('scholarships')->rename('z_4749_scholarships');
+        $this->execute('CREATE TABLE `scholarships` LIKE `z_4749_scholarships`');
+        $this->execute('INSERT INTO `scholarships` SELECT * FROM `z_4749_scholarships`');
 
         $datas = [
             [
+              'id'  => 1,
+              'code'  => 'FULLSCHOLARSHIP',
+              'name'  => 'Full Scholarship'
+            ],
+            [
+              'id'  => 2,
               'code'  => 'PARTIALSCHOLARSHIP',
               'name'  => 'Partial Scholarship'
             ],
             [
+              'id'  => 3,   
               'code'  => 'GRANT',
               'name'  => 'Grant'
+            ],
+            [
+              'id'  => 4,
+              'code'  => 'LOAN',
+              'name'  => 'Loan'
             ]
         ];
 
-        $this->table('scholarship_financial_assistance_types')->insert($datas)->save();        
+        $this->table('scholarship_financial_assistance_types')->insert($datas)->save(); 
+
+        $sql = "UPDATE `scholarships` SET `scholarship_financial_assistance_type_id`=4 WHERE scholarship_financial_assistance_type_id =2";
+
+        $this->execute($sql);
     }
 
     public function down()
     {
-        $this->execute('DELETE FROM scholarship_financial_assistance_types WHERE name in ("Partial Scholarship","Grant")');
-
-        $sql = "UPDATE `scholarship_financial_assistance_types` 
-                SET `code` = 'SCHOLARSHIP', 
-                    `name` = 'Scholarship' 
-                WHERE `name` = 'Full Scholarship'";
-
-        $this->execute($sql);        
-    }    
+        $this->dropTable('scholarship_financial_assistance_types');
+        $this->table('z_4749_scholarship_financial_assistance_types')->rename('scholarship_financial_assistance_types');
+        $this->dropTable('scholarships');
+        $this->table('z_4749_scholarships')->rename('scholarships');
+    }
 }
