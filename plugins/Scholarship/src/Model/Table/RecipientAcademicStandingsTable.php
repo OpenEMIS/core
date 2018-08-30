75458005b2f5680602e8e5d741fef8c55bad8583
konghongyi <hkong@kordit.com>
2018-08-30 09:39:26 +0800

    POCOR-4787 - rename to ValidGPA

--- plugins/Scholarship/src/Model/Table/RecipientAcademicStandingsTable.php
+++ plugins/Scholarship/src/Model/Table/RecipientAcademicStandingsTable.php
@@ -24,7 +24,7 @@ class RecipientAcademicStandingsTable extends AppTable
 
         return $validator
            ->add('gpa', [
-                'validClassification' => [
+                'validGPA' => [
                     'rule' => ['range', 0, 9],
                     'message' => 'GPA should between 0 to 9'
                 ]
