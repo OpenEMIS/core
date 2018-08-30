d8a5eca117982f48d9414fd34bcf5fc7dcb69abc
Acacia Wong <awong@kordit.com>
2018-08-30 15:48:22 +0800

    POCOR-4764
    Clean up

--- plugins/Scholarship/src/Model/Table/ApplicationInstitutionChoicesTable.php
+++ plugins/Scholarship/src/Model/Table/ApplicationInstitutionChoicesTable.php
@@ -4,7 +4,6 @@ namespace Scholarship\Model\Table;
 use ArrayObject;
 use Cake\Event\Event;
 use Cake\ORM\Entity;
-use Cake\ORM\TableRegistry;
 use Cake\Validation\Validator;
 
 use App\Model\Table\AppTable;
