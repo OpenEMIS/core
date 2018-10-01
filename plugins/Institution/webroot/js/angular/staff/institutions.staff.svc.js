fa7f4148884522bdb08c4dc82d102fe5d2832e39
Kok Keong <kcheng@kordit.com>
2018-10-01 15:42:05 +0800

    POCOR-4829 - Refactor

--- plugins/Institution/webroot/js/angular/staff/institutions.staff.svc.js
+++ plugins/Institution/webroot/js/angular/staff/institutions.staff.svc.js
@@ -785,4 +785,10 @@ function InstitutionsStaffSvc($http, $q, $filter, KdOrmSvc) {
             .contain(['IdentityTypes'])
             .ajax({defer: true});
     }
+
+    function getSpecialNeedTypes() {
+        return SpecialNeedTypes
+            .select()
+            .ajax({defer: true});
+   }
 };
