3714069283df41c216da30d7c98f75a8cef09be0
Kok Keong <kcheng@kordit.com>
2018-10-02 10:09:28 +0800

    POCOR-4831 - Added special needs column in institution classes view and edit action

--- plugins/User/src/Model/Entity/User.php
+++ plugins/User/src/Model/Entity/User.php
@@ -8,16 +8,16 @@ use Cake\ORM\Query;
 use App\Model\Traits\UserTrait;
 
 class User extends Entity {
-	use UserTrait;
+    use UserTrait;
 
-    protected $_virtual = ['name', 'name_with_id', 'default_identity_type'];
+    protected $_virtual = ['name', 'name_with_id', 'default_identity_type', 'has_special_needs'];
 
     protected function _setPassword($password) {
-    	if (empty($password)) {
-    		return null;
-    	} else {
-    		return (new DefaultPasswordHasher)->hash($password);
-    	}
+        if (empty($password)) {
+            return null;
+        } else {
+            return (new DefaultPasswordHasher)->hash($password);
+        }
     }
 
     /**
@@ -57,21 +57,40 @@ class User extends Entity {
         return trim(sprintf('%s - %s', $this->openemis_no, $name));
     }
 
-	protected function _getDefaultIdentityType() {
-		$data = "";
-		$securityUserId = $this->id;
+    protected function _getDefaultIdentityType() {
+        $data = "";
+        $securityUserId = $this->id;
 
         $UserIdentities = TableRegistry::get('User.Identities');
         $IdentityTypes = $UserIdentities->IdentityTypes;
         $default_identity_type = $IdentityTypes->getDefaultValue();
-		$UserIdentity = $UserIdentities
-				->find()
-				->where(['security_user_id' => $this->id, 'identity_type_id' => $default_identity_type])
-				->first();
+        $UserIdentity = $UserIdentities
+                ->find()
+                ->where(['security_user_id' => $this->id, 'identity_type_id' => $default_identity_type])
+                ->first();
 
-		if(!empty($UserIdentity)) {
-			$data = $UserIdentity->number;
-		}
-		return $data;
-	}
+        if(!empty($UserIdentity)) {
+            $data = $UserIdentity->number;
+        }
+        return $data;
+    }
+
+    protected function _getHasSpecialNeeds()
+    {
+        if ($this->has('special_needs') && !empty($this->special_needs)) {
+            return true;
+        }
+
+        $SpecialNeedsAssessments = TableRegistry::get('SpecialNeeds.SpecialNeedsAssessments');
+
+        $userId = $this->id;
+        $specialNeedCount = $SpecialNeedsAssessments
+            ->find()
+            ->where([
+                $SpecialNeedsAssessments->aliasField('security_user_id') => $userId
+            ])
+            ->count();
+
+        return $specialNeedCount > 0;
+    }   
 }
