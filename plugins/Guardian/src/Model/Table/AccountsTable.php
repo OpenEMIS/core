f7dc83870440b36cba996dd5bae27caf464d14f0
konghongyi <hkong@kordit.com>
2018-10-09 13:53:09 +0800

    POCOR-4634 - put comments for getGuardianTabElements

--- plugins/Guardian/src/Model/Table/AccountsTable.php
+++ plugins/Guardian/src/Model/Table/AccountsTable.php
@@ -7,7 +7,6 @@ use App\Model\Table\AppTable;
 
 class AccountsTable extends AppTable {
 	public function initialize(array $config) {
-
 		$this->addBehavior('User.Account', ['permission' => ['Guardian', 'Accounts', 'edit']]);
 		parent::initialize($config);
 	}
