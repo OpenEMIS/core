80eaee4f883c139e853396497f1daa3f05b425b7
Kok Keong <kcheng@kordit.com>
2018-10-12 16:43:31 +0800

    POCOR-4603 - Remove report secondary staff columns as it change from hasOne to hasMany associations for class attendance not marked report and institution classes report - Update education grade column retrieval by adding additional inner join to the new structure for institution staff report - Update institution case on assign secondary staff to select the first secondary staff for the post back event for workflow case creation - Remove column field in competency comments and student outcome table

--- plugins/Institution/src/Model/Table/StudentOutcomesTable.php
+++ plugins/Institution/src/Model/Table/StudentOutcomesTable.php
@@ -30,7 +30,6 @@ class StudentOutcomesTable extends ControllerActionTable
 
         $this->belongsTo('AcademicPeriods', ['className' => 'AcademicPeriod.AcademicPeriods']);
         $this->belongsTo('Staff', ['className' => 'User.Users', 'foreignKey' => 'staff_id']);
-        // $this->belongsTo('SecondaryStaff', ['className' => 'User.Users', 'foreignKey' => 'secondary_staff_id']);
         $this->hasMany('ClassesSecondaryStaff', ['className' => 'Institution.InstitutionClassesSecondaryStaff', 'saveStrategy' => 'replace', 'foreignKey' => 'institution_class_id']);
         $this->belongsTo('InstitutionShifts', ['className' => 'Institution.InstitutionShifts']);
         $this->belongsTo('Institutions', ['className' => 'Institution.Institutions']);
@@ -396,7 +395,6 @@ class StudentOutcomesTable extends ControllerActionTable
     {
         $this->field('class_number', ['type' => 'hidden']);
         $this->field('staff_id', ['type' => 'hidden']);
-        $this->field('secondary_staff_id', ['type' => 'hidden']);
         $this->field('institution_shift_id', ['type' => 'hidden']);
         $this->field('capacity', ['type' => 'hidden']);
         $this->field('modified_user_id', ['type' => 'hidden']);
