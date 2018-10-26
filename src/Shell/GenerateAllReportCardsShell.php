9e5483570caa562874f1797dc0d3c3c7b7e61896
Ervin <ekwan@kordit.com>
2018-10-26 11:27:58 +0800

    recursive calls instead of loop

--- src/Shell/GenerateAllReportCardsShell.php
+++ src/Shell/GenerateAllReportCardsShell.php
@@ -26,68 +26,65 @@ class GenerateAllReportCardsShell extends Shell
             $pid = getmypid();
             $systemProcessId = !empty($this->args[0]) ? $this->args[0] : 0;
             $this->SystemProcesses->updatePid($systemProcessId, $pid);
+            $recordToProcess = $this->ReportCardProcesses->find()
+                ->select([
+                    $this->ReportCardProcesses->aliasField('report_card_id'),
+                    $this->ReportCardProcesses->aliasField('institution_class_id'),
+                    $this->ReportCardProcesses->aliasField('student_id'),
+                    $this->ReportCardProcesses->aliasField('institution_id'),
+                    $this->ReportCardProcesses->aliasField('education_grade_id'),
+                    $this->ReportCardProcesses->aliasField('academic_period_id')
+                ])
+                ->where([
+                    $this->ReportCardProcesses->aliasField('status') => $this->ReportCardProcesses::NEW_PROCESS
+                ])
+                ->order([
+                    $this->ReportCardProcesses->aliasField('created'),
+                    $this->ReportCardProcesses->aliasField('student_id')
+                ])
+                ->hydrate(false)
+                ->first();
 
-            $this->out('Initialize Generate All Report Cards ('.Time::now().')');
+            if (!empty($recordToProcess)) {
+                $this->out('Generating report card for Student '.$recordToProcess['student_id'].' ('. Time::now() .')');
+                $this->out('Total memory used: ' . memory_get_usage());
+                $this->ReportCardProcesses->updateAll(['status' => $this->ReportCardProcesses::RUNNING], [
+                    'report_card_id' => $recordToProcess['report_card_id'],
+                    'institution_class_id' => $recordToProcess['institution_class_id'],
+                    'student_id' => $recordToProcess['student_id']
+                ]);
 
-            $exit = false;
-            while (!$exit) {
-                $recordToProcess = $this->ReportCardProcesses->find()
-                    ->select([
-                        $this->ReportCardProcesses->aliasField('report_card_id'),
-                        $this->ReportCardProcesses->aliasField('institution_class_id'),
-                        $this->ReportCardProcesses->aliasField('student_id'),
-                        $this->ReportCardProcesses->aliasField('institution_id'),
-                        $this->ReportCardProcesses->aliasField('education_grade_id'),
-                        $this->ReportCardProcesses->aliasField('academic_period_id')
-                    ])
-                    ->where([
-                        $this->ReportCardProcesses->aliasField('status') => $this->ReportCardProcesses::NEW_PROCESS
-                    ])
-                    ->order([
-                        $this->ReportCardProcesses->aliasField('created'),
-                        $this->ReportCardProcesses->aliasField('student_id')
-                    ])
-                    ->hydrate(false)
-                    ->first();
+                $excelParams = new ArrayObject([]);
+                $excelParams['className'] = 'CustomExcel.ReportCards';
+                $excelParams['requestQuery'] = $recordToProcess;
 
-                if (!empty($recordToProcess)) {
-                    $this->out('Generating report card for Student '.$recordToProcess['student_id'].' ('. Time::now() .')');
-                    $this->out('Total memory used: ' . memory_get_usage());
-                    $this->ReportCardProcesses->updateAll(['status' => $this->ReportCardProcesses::RUNNING], [
-                        'report_card_id' => $recordToProcess['report_card_id'],
-                        'institution_class_id' => $recordToProcess['institution_class_id'],
-                        'student_id' => $recordToProcess['student_id']
-                    ]);
-
-                    $excelParams = new ArrayObject([]);
-                    $excelParams['className'] = 'CustomExcel.ReportCards';
-                    $excelParams['requestQuery'] = $recordToProcess;
-
-                    try {
-                        $this->ReportCards->renderExcelTemplate($excelParams);
-                    } catch (\Exception $e) {
-                        $this->out('Error generating Report Card for Student ' . $recordToProcess['student_id']);
-                        $this->out($e->getMessage());
-                    }
-
-                    $this->out('End generating report card for Student '.$recordToProcess['student_id'].' ('. Time::now() .')');
-                    $this->out('Total memory used: ' . memory_get_usage());
-                    //To prevent memory leak
-                    $this->ReportCards = null;
-                    $this->ReportCardProcesses = null;
-                    $this->SystemProcesses = null;
-                    $excelParams = null;
-                    TableRegistry::clear();
-                    gc_collect_cycles();
-                    $this->initialize();
-                    $this->out('Total memory used: ' . memory_get_usage() . " (after clearing memory space)");
-                } else {
-                    $exit = true;
-                    $this->SystemProcesses->updateProcess($systemProcessId, Time::now(), $this->SystemProcesses::COMPLETED);
+                try {
+                    $this->ReportCards->renderExcelTemplate($excelParams);
+                } catch (\Exception $e) {
+                    $this->out('Error generating Report Card for Student ' . $recordToProcess['student_id']);
+                    $this->out($e->getMessage());
                 }
-            }
 
-            $this->out('End Generate All Report Cards ('.Time::now().')');
+                $this->out('End generating report card for Student '.$recordToProcess['student_id'].' ('. Time::now() .')');
+                $this->out('Total memory used: ' . memory_get_usage());
+                $this->recursiveCallToMyself($this->args[0]);
+                $this->SystemProcesses->updateProcess($systemProcessId, Time::now(), $this->SystemProcesses::COMPLETED);
+            } else {
+                $this->SystemProcesses->updateProcess($systemProcessId, Time::now(), $this->SystemProcesses::COMPLETED);
+            }
+        }
+    }
+
+    private function recursiveCallToMyself($args)
+    {
+        $cmd = ROOT . DS . 'bin' . DS . 'cake GenerateAllReportCards '.$args;
+        $logs = ROOT . DS . 'logs' . DS . 'GenerateAllReportCards.log & echo $!';
+        $shellCmd = $cmd . ' >> ' . $logs;
+        try {
+            $pid = exec($shellCmd);
+            $this->out('debug: ' . $shellCmd);
+        } catch(\Exception $ex) {
+            $this->out('error : ' . __METHOD__ . ' exception when recursiveCallToMyself : '. $ex);
         }
     }
 }
