1967dafbf5c2f84ed362f4899a9c28b3c838bc1f
Davidson Sim <dsim@kordit.com>
2018-11-08 15:41:01 +0800

    POCOR-4836  -> Missed out the fix in Profile > Scholarship > Add using the latest database scheme.

--- plugins/Profile/src/Model/Table/ScholarshipApplicationsTable.php
+++ plugins/Profile/src/Model/Table/ScholarshipApplicationsTable.php
@@ -9,6 +9,8 @@ use Cake\ORM\TableRegistry;
 use Cake\Validation\Validator;
 use App\Model\Traits\OptionsTrait;
 use App\Model\Table\ControllerActionTable;
+use Cake\Network\Request;
+
 
 class ScholarshipApplicationsTable extends ControllerActionTable
 {
@@ -186,11 +188,15 @@ class ScholarshipApplicationsTable extends ControllerActionTable
             $scholarshipEntity = $this->Scholarships->get($scholarshipId, ['contain' => [
                 'AcademicPeriods',
                 'FinancialAssistanceTypes',
-                'Loans.PaymentFrequencies'
+                'Loans.PaymentFrequencies',
             ]]);
             $entity->scholarship_id = $scholarshipId;
             $entity->scholarship = $scholarshipEntity;
             $entity->applicant_id = $this->Auth->user('id');
+
+            $applicantId = $this->ControllerAction->getQueryString('applicant_id');
+            $applicantEntity = $this->Applicants->get($entity->applicant_id, ['contain' => ['Genders', 'MainIdentityTypes']]);
+            $entity->applicant = $applicantEntity;
         }
 
         $this->setupFields($entity);
@@ -389,6 +395,21 @@ class ScholarshipApplicationsTable extends ControllerActionTable
             $attr['attr']['value'] = $value;
         }
         return $attr;
+    }
+
+    public function onUpdateFieldAssigneeId(Event $event, array $attr, $action, Request $request)
+    {
+        if ($action == 'add') {
+            $entity = $attr['entity'];
+            $displayValue = $entity->applicant->name_with_id;
+            $value = $entity->applicant_id;
+
+            $attr['value'] = $value;
+            $attr['attr']['value'] = $displayValue;
+            $attr['type'] = 'readonly';
+
+            return $attr;
+        }
     }
 
     public function setupFields($entity)
