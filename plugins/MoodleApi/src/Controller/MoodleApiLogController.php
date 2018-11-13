00f3a93a15627fa670484ff71a419d024bf7cea8
Ervin <ekwan@kordit.com>
2018-11-13 16:45:31 +0800

    moodle api logs view

---
+++ plugins/MoodleApi/src/Controller/MoodleApiLogController.php
@@ -0,0 +1,37 @@
+<?php
+namespace MoodleApi\Controller;
+
+use Cake\Event\Event;
+use App\Controller\PageController;
+
+
+class MoodleApiLogController extends PageController
+{
+
+    public function initialize()
+    {
+        parent::initialize();
+        $this->Page->disable(['add', 'edit', 'delete']);
+        $this->Page->loadElementsFromTable($this->MoodleApiLog);
+    }
+
+    public function index()
+    {
+        $page = $this->Page;
+        $page->exclude(['callback', 'callback_param']);
+
+        $statusesOptions = $this->MoodleApiLog->getStatuses();
+        $page->get('status')
+            ->setControlType('select')
+            ->setOptions($statusesOptions);
+
+        parent::index();
+    }
+
+    public function view($id)
+    {
+        $page = $this->Page;
+        parent::view($id);
+    }
+
+}
