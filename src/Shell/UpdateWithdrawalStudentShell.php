384178ae4298b117d538c510eb16eb5d01ce8fd6
Acacia Wong <awong@kordit.com>
2018-11-16 18:05:58 +0800

    POCOR-4367
    Migrations added for new table student_status_updates to keep all the records that needs to be updated when the effective date = today's date.
    StudentWithdrawal modified to add withdrawal records to the student_status_updates and trigger UpdateWithdrawalStudent shell when Workflow.onApproval is triggered.
    UpdateWithdrawalStudentShell will look for all records that needs to be updated from the student_status_updates instead.
    Once the status is updated, the record will be deleted from the student_status_updates

--- src/Shell/UpdateWithdrawalStudentShell.php
+++ src/Shell/UpdateWithdrawalStudentShell.php
@@ -7,135 +7,135 @@ use Cake\Console\Shell;
 use Cake\Event\Event;
 use Cake\ORM\TableRegistry;
 use Cake\Datasource\ConnectionManager;
+use App\Model\Table\SystemProcessesTable as ProcessCode;
 
 class UpdateWithdrawalStudentShell extends Shell {
-	const NEW_PROCESS = 1;
-	const RUNNING = 2;
-	const COMPLETED = 3;
-	const ERROR = -2;
-	const SLEEP_TIME = 86400; //need to change this value to 86400
+    const SLEEP_TIME = 86400;
 
-	public function initialize() {
-		parent::initialize();
-		$this->loadModel('Institution.StudentWithdraw');
+    public function initialize()
+    {
+        parent::initialize();
+        $this->loadModel('Institution.StudentWithdraw');
         $this->loadModel('Institution.Students');
         $this->loadModel('SystemProcesses');
-	}
+    }
 
- 	public function main() {
- 		try {
- 			$today = Time::now();
- 			$model = $this->StudentWithdraw->registryAlias();
- 			$pid = getmypid();
-	 		// Ensure that no process is running currently. Max number of shell: 1
+    public function main()
+    {
+        $this->out('UpdateWithdrawalStudentShell triggered >>>>>>>>>>>> ');
+        try {
+            $model = $this->StudentWithdraw->registryAlias();
+            $pid = getmypid();
+            $systemProcess = $this->SystemProcesses
+                ->find()
+                ->where([$this->SystemProcesses->aliasField('model') => $model])
+                ->first();
 
-	 		$systemProcessId = $this->SystemProcesses
-	 			->find()
-	 			->where([
-	 				$this->SystemProcesses->aliasField('model') => $model
-	 			])
-	 			->extract('id')
-	 			->first();
+            $systemProcessId = $systemProcess->id;
+            $existingPid = $systemProcess->process_id;
 
-	 		if ($systemProcessId) {
-	 			$this->out('Process exists in system_processes >>>>>>>>>>>> ');
-	 			$this->out('Check and purge stuck processes >>>>>>>>>>>> ');
-	 			$this->_totalRunningSystemProcesses($model, $today);
-	 			$this->out('Updating pid >>>>>>>>>>>> ');
+            if ($systemProcess) {
+                $this->out('Kill existing system_processes >>>>>>>>>>>> ' . $existingPid);
+                $this->SystemProcesses->killProcess($existingPid);
+                $this->out('Updating pid >>>>>>>>>>>> ' . $pid);
                 $this->SystemProcesses->updatePid($systemProcessId, $pid);
                 $this->out('Updating process status to NEW_PROCESS >>>>>>>>>>>> ');
-	 			$this->SystemProcesses->updateProcess($systemProcessId, Time::now(), self::NEW_PROCESS);
-	 		} else {
-	 			$this->out('Process does not exists in system_processes. Adding new process >>>>>>>>>>>>');
- 				$systemProcessId = $this->SystemProcesses->addProcess('UpdateWithdrawalStudent', getmypid(), $model);
-        	}
-	 		// die;
- 			$studentWithdrawRecords = $this->getStudentWithdrawRecords($today);
- 			$this->out('Retrieving student withdrawal records >>>>>>>>>>>> ');
- 			if (!empty($studentWithdrawRecords)) {
- 				$this->out('Student withdrawal records found....');
- 				$this->out('Updating process status to RUNNING >>>>>>>>>>>> ');
- 				$this->SystemProcesses->updateProcess($systemProcessId, Time::now(), self::RUNNING);
- 				foreach ($studentWithdrawRecords as $key => $StudentWithdrawRecord) {
- 					$studentWithdrawRecordId = $StudentWithdrawRecord->id;
-					$this->out("Dispatching event to update student status to withdraw for StudentWithdrawTable:" . $studentWithdrawRecordId);
-					$event = $this->StudentWithdraw->dispatchEvent('StudentWithdraw.onApproval', [$studentWithdrawRecordId]);
-					if ($event->isStopped()) {
-						$this->out('stopped');
-					}
-					$this->out("End dispatch event for recordId ".$studentWithdrawRecordId);
- 				}
- 				$this->out('Updating system process to COMPLETED status');
- 				$this->SystemProcesses->updateProcess($systemProcessId, Time::now(), self::COMPLETED);
- 				// $this->SystemProcesses->killProcess($pid);
- 				$this->_recursiveCall();
- 			} else {
- 				// $this->SystemProcesses->killProcess($pid);
- 				$this->_recursiveCall();
- 			}
- 		} catch (\Exception $e) {
-			$this->out('Exception Caught');
-			$this->out($e->getMessage());
-			// should log the message here with the date and $e->getMessage
-			$this->SystemProcesses->updateProcess($systemProcessId, Time::now(), self::ERROR);
- 		}
-	}
+                $this->SystemProcesses->updateProcess($systemProcessId, Time::now(), ProcessCode::NEW_PROCESS);
+            } else {
+                $systemProcessId = $this->SystemProcesses->addProcess('UpdateWithdrawalStudent', getmypid(), $model);
+            }
 
-	private function _recursiveCall()
-	{
+            $this->out('Retrieving student withdrawal records >>>>>>>>>>>> ');
+            $studentWithdrawRecords = $this->getStudentWithdrawRecords();
+
+            if (!empty($studentWithdrawRecords)) {
+                $this->out('Student withdrawal records found....');
+                $this->out('Updating process status to RUNNING >>>>>>>>>>>> ');
+                $this->SystemProcesses->updateProcess($systemProcessId, Time::now(), ProcessCode::RUNNING);
+
+                foreach ($studentWithdrawRecords as $key => $studentWithdrawRecord) {
+                    $this->out("Dispatching event to update student status to withdraw for StudentWithdrawTable: " . $studentWithdrawRecord->id);
+                    $event = $this->StudentWithdraw->dispatchEvent('Shell.StudentWithdraw.updateStudentStatusId', [$studentWithdrawRecord]);
+                    $this->out("End dispatch event for studentWithdrawRecordId " . $studentWithdrawRecord->id);
+                }
+
+                $this->out('Updating system process to COMPLETED status');
+                $this->SystemProcesses->updateProcess($systemProcessId, Time::now(), ProcessCode::COMPLETED);
+            }
+            $this->_recursiveCall();
+        } catch (\Exception $e) {
+            $this->out('Exception Caught: ' . $e->getMessage());
+            $this->SystemProcesses->updateProcess($systemProcessId, Time::now(), ProcessCode::ERROR);
+            $this->_recursiveCall();
+        }
+    }
+
+    private function _recursiveCall()
+    {
+        $this->out('Sleeping.......');
         sleep(self::SLEEP_TIME);
-        $this->out('recursive calling self');
+        $this->out('Recursive calling self');
         $cmd = ROOT . DS . 'bin' . DS . 'cake UpdateWithdrawalStudent';
         $logs = ROOT . DS . 'logs' . DS . 'UpdateWithdrawalStudent.log & echo $!';
         $shellCmd = $cmd . ' >> ' . $logs;
         try {
             $pid = exec($shellCmd);
         } catch(\Exception $e) {
-            $this->out('error : ' . __METHOD__ . ' exception when _recursiveCall : '. $e);
+            $this->out('error : ' . __METHOD__ . ' exception when _recursiveCall : ' . $e->getMessage());
         }
         exit();
     }
 
-    private function getStudentWithdrawRecords($today)
+    private function getStudentWithdrawRecords()
     {
-		$studentWithdrawRecords = $this->StudentWithdraw
-			->find()
-			->innerJoin(
-				[$this->Students->alias() => $this->Students->table()],
-				[$this->Students->aliasField('academic_period_id = ') . $this->StudentWithdraw->aliasField('academic_period_id'),
-                $this->Students->aliasField('education_grade_id = ') . $this->StudentWithdraw->aliasField('education_grade_id'),
-                $this->Students->aliasField('student_id = ') . $this->StudentWithdraw->aliasField('student_id'),
-                $this->Students->aliasField('institution_id = ') . $this->StudentWithdraw->aliasField('institution_id')]
-            )
-			->where([
-				$this->Students->aliasField('student_status_id <> 4'),
-				$this->StudentWithdraw->aliasField('effective_date') => $today,
-				$this->StudentWithdraw->aliasField('status_id') => 76, // hard code to get the Withdrawn workflow status id first
-			])
-			->toArray()
-			;
+        $model = $this->StudentWithdraw->alias();
+        $StudentStatuses = TableRegistry::get('Student.StudentStatuses');
+        $StudentStatusUpdates = TableRegistry::get('StudentStatusUpdates');
+        $today = Time::now();
+        $statuses = $StudentStatuses->findCodeList();
+        // $workflowSteps = $this->getStepsWithOnApprovalEvent();
+
+        $studentWithdrawRecords = $StudentStatusUpdates
+            ->find()
+            ->where([
+                $StudentStatusUpdates->aliasField('status_id') => $statuses['WITHDRAWN'],
+                $StudentStatusUpdates->aliasField('effective_date') => $today,
+                $StudentStatusUpdates->aliasField('model') => $model,
+            ])
+            ->order(['created' => 'asc'])
+            ->toArray();
         return $studentWithdrawRecords;
     }
+    // most probably can remove because of the new table
+    // private function getStepsWithOnApprovalEvent()
+    // {
+    //     $WorkflowActionsTable = TableRegistry::get('Workflow.WorkflowActions');
+    //     $WorkflowStepsTable = TableRegistry::get('Workflow.WorkflowSteps');
+    //     $WorkflowsTable = TableRegistry::get('Workflow.Workflows');
+    //     $WorkflowModelsTable = TableRegistry::get('Workflow.WorkflowModels');
 
-	private function _totalRunningSystemProcesses($model, $today)
-    {
-    	$this->out('Retrieving runningProcess >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ');
-    	// this will always return one record
-        $runningProcess = $this->SystemProcesses->getRunningProcesses($model);
-        if (!empty($runningProcess)) {
-	        foreach ($runningProcess as $key => $processData) {
-	            $systemProcessId = $processData['id'];
-	            $pid = !empty($processData['process_id']) ? $processData['process_id'] : 0;
-	            $createdDate = $processData['created'];
+    //     $stepList = $WorkflowActionsTable
+    //         ->find()
+    //         ->select([
+    //             'workflow_step_id' => $WorkflowStepsTable->aliasField('id')
+    //         ])
+    //         ->where([
+    //             $WorkflowActionsTable->aliasField('event_key LIKE') => '%Workflow.onApproval%',
+    //             $WorkflowModelsTable->aliasField('model') => 'Institution.StudentWithdraw'
+    //         ])
+    //         ->innerJoin([$WorkflowStepsTable->alias() => $WorkflowStepsTable->table()], [
+    //             $WorkflowStepsTable->aliasField('id = ') . $WorkflowActionsTable->aliasField('next_workflow_step_id')
+    //         ])
+    //         ->innerJoin([$WorkflowsTable->alias() => $WorkflowsTable->table()], [
+    //             $WorkflowsTable->aliasField('id = ') . $WorkflowStepsTable->aliasField('workflow_id')
+    //         ])
+    //         ->innerJoin([$WorkflowModelsTable->alias() => $WorkflowModelsTable->table()], [
+    //             $WorkflowModelsTable->aliasField('id = ') . $WorkflowsTable->aliasField('workflow_model_id')
+    //         ])
+    //         ->group(['workflow_step_id'])
+    //         ->extract('workflow_step_id')
+    //         ->toArray();
 
-	            if ($createdDate < $today) {
-	            	$this->out('today is greater than createdDate');
-	            	$this->out('Proceeds to update the process to completed');
-	                $this->SystemProcesses->updateProcess($systemProcessId, Time::now(), self::COMPLETED);
-	                $this->out('Proceeds to kill the process with pId'. $pid);
-	                $this->SystemProcesses->killProcess($pid);
-	            }
-	        }
-        }
-    }
+    //     return $stepList;
+    // }
 }
