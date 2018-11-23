829ddc2b783330f624d184e9b4658584f336071d
Kok Keong <kcheng@kordit.com>
2018-11-23 12:22:59 +0800

    POCOR-4894 - Set directories as the default allow action controller used

--- plugins/Historial/src/Model/Behavior/HistorialBehavior.php
+++ plugins/Historial/src/Model/Behavior/HistorialBehavior.php
@@ -23,7 +23,7 @@ class HistorialBehavior extends Behavior
             'action' => ''
         ],
         'model' => '',
-        'allowedController' => []
+        'allowedController' => ['Directories']
     ];
 
     public function initialize(array $config)
