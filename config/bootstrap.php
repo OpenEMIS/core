ec81bf1caf92a8b6670fa393e722f0e85c268e8b
Kok Keong <kcheng@kordit.com>
2018-12-05 18:11:01 +0800

    POCOR-4356 - Added Schedule plugins - Added to bootstrap.php to autoload - Migration patch added to insert the new tables (without the security_functions table update) - Added Terms to institution plugins - Updated navigation component with terms url - Added error message for terms validations - Added all the new table files for the new tables created

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -324,6 +324,7 @@ Plugin::load('Guardian', ['routes' => true, 'autoload' => true]);
 Plugin::load('Email', ['routes' => true, 'autoload' => true]);
 Plugin::load('SpecialNeeds', ['routes' => true, 'autoload' => true]);
 Plugin::load('Historical', ['autoload' => true]);
+Plugin::load('Schedule', ['autoload' => true]);
 
 $pluginPath = Configure::read('plugins');
 foreach ($pluginPath as $key => $path) {
