74efe469ac78315a0e2251201061c72583972300
Nova <nwongso@kordit.com>
2018-12-20 15:04:37 +0800

    revert fix report filter institution type base on survey custom filter

--- plugins/Report/src/Model/Table/SurveysTable.php
+++ plugins/Report/src/Model/Table/SurveysTable.php
@@ -72,25 +72,16 @@ class SurveysTable extends AppTable
             $academicPeriodName = $this->AcademicPeriods->get($academicPeriodId)->name;
             $userId = $requestData->user_id;
             $superAdmin = $requestData->super_admin;
-
-            $SurveyFormsFilters = TableRegistry::get('Survey.SurveyFormsFilters');
-            $institutionType = $SurveyFormsFilters->find()
-                ->where([
-                    $SurveyFormsFilters->aliasField('survey_form_id').' = '.$surveyFormId,
-                ])
-                ->select([ 'institution_type_id' => $SurveyFormsFilters->aliasField('survey_filter_id') ]);
-
             $InstitutionsTable = $this->Institutions;
+
             $missingRecords = $InstitutionsTable->find()
-                ->where([
-                    'NOT EXISTS ('.
+                ->where(['NOT EXISTS ('.
                     $this->find()->where([
                         $this->aliasField('academic_period_id').' = '.$academicPeriodId,
                         $this->aliasField('survey_form_id').' = '.$surveyFormId,
                         $this->aliasField('institution_id').' = '.$InstitutionsTable->aliasField('id')
                     ])
-                    .')'
-                ])
+                .')'])
                 ->innerJoinWith('Areas')
                 ->leftJoinWith('AreaAdministratives')
                 ->select([
@@ -99,11 +90,6 @@ class SurveysTable extends AppTable
                     'area' => 'Areas.name',
                     'area_administrative' => 'AreaAdministratives.name'
                 ]);
-            if ($institutionType->cleanCopy()->first()->institution_type_id) {
-                $missingRecords->where([
-                    $InstitutionsTable->aliasField('institution_type_id').' IN ('.$institutionType.')'
-                ]);
-            }
 
             if (!$superAdmin) {
                 $missingRecords->find('ByAccess', ['userId' => $userId]);
