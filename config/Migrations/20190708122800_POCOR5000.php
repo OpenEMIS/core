9433f5b65bc7c4d37c6d0980134bf864641d5348
shubhamsharma08 <ssharma@avaloninfosys.com>
2019-07-09 14:07:35 +0530

    POCOR-5000

--- config/Migrations/20190708122800_POCOR5000.php
+++ config/Migrations/20190708122800_POCOR5000.php
@@ -6,9 +6,11 @@ class POCOR5000 extends AbstractMigration
     public function up()
     {
 		$this->execute('ALTER TABLE examination_centres CHANGE COLUMN `fax` `fax` VARCHAR(30) NULL');
+		$this->execute('ALTER TABLE examination_centres CHANGE COLUMN `telephone` `telephone` VARCHAR(30) NOT NULL');
 	}
 	public function down()
     {
 		$this->execute('ALTER TABLE examination_centres CHANGE COLUMN `fax` `fax` VARCHAR(30) NOT NULL');
+		$this->execute('ALTER TABLE examination_centres CHANGE COLUMN `telephone` `telephone` VARCHAR(30) NULL');
 	}
 }
