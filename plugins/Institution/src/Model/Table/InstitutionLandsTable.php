4c345a200ac2639c194c9d2d3ac63c556e5d2abb
asingh1@dataforall.org <asingh1@dataforall.org>
2019-08-20 17:13:01 +0530

    Issue has been fixed, ready for review

--- plugins/Institution/src/Model/Table/InstitutionLandsTable.php
+++ plugins/Institution/src/Model/Table/InstitutionLandsTable.php
@@ -64,6 +64,7 @@ class InstitutionLandsTable extends ControllerActionTable
         $this->levelOptions = $this->Levels->find('list')->toArray();
         $this->accessibilityOptions = $this->getSelectOptions('InstitutionAssets.accessibility');
         $this->accessibilityTooltip = $this->getMessage('InstitutionInfrastructures.accessibilityOption');
+        $this->effectiveDateTooltip = $this->getMessage('InstitutionInfrastructures.effectiveDate');
         $this->setDeleteStrategy('restrict');
     }
 
@@ -831,7 +832,15 @@ class InstitutionLandsTable extends ControllerActionTable
         $this->field('year_acquired');
         $this->field('year_disposed');
         $this->field('land_type_id', ['type' => 'select', 'entity' => $entity]);
-        $this->field('start_date', ['entity' => $entity]);
+        $this->field('start_date', ['entity' => $entity,
+                        'attr' => [
+                        'label' => [
+                            'text' => __('Effective date') . ' <i class="fa fa-info-circle fa-lg fa-right icon-blue" tooltip-placement="bottom" uib-tooltip="' . __($this->effectiveDateTooltip) . '" tooltip-append-to-body="true" tooltip-class="tooltip-blue"></i>',
+                            'escape' => false,
+                            'class' => 'tooltip-desc'
+                        ]
+                    ]                
+                ]);
         $this->field('end_date', ['entity' => $entity]);
         $this->field('infrastructure_ownership_id', ['type' => 'select']);
         $this->field('infrastructure_condition_id', ['type' => 'select']);
