136a185f955091daeb639a55dfa2c64f44b6d93c
Brij Mohan Singh <bmsingh@dataforall.org>
2019-08-22 12:21:15 +0530

    code formate

--- config/Migrations/20190821043500_POCOR4944.php
+++ config/Migrations/20190821043500_POCOR4944.php
@@ -17,60 +17,59 @@ class POCOR4944 extends AbstractMigration
                 'created_user_id' => 1,
                 'created' => date('Y-m-d H:i:s')
             ],
-			[
+            [
                 'en' => 'Security Role',
                 'created_user_id' => 1,
                 'created' => date('Y-m-d H:i:s')
             ],
-			[
+            [
                 'en' => 'Project Director',
                 'created_user_id' => 1,
                 'created' => date('Y-m-d H:i:s')
             ],
-			[
+            [
                 'en' => 'District Education Manager',
                 'created_user_id' => 1,
                 'created' => date('Y-m-d H:i:s')
             ],
-			[
+            [
                 'en' => 'Administrative Officer',
                 'created_user_id' => 1,
                 'created' => date('Y-m-d H:i:s')
             ],
-			[
+            [
                 'en' => 'Assistant Education Officer',
                 'created_user_id' => 1,
                 'created' => date('Y-m-d H:i:s')
             ],
-			[
+            [
                 'en' => 'Director Education Planning Unit*',
                 'created_user_id' => 1,
                 'created' => date('Y-m-d H:i:s')
             ],
-			[
+            [
                 'en' => 'Director Policy, Planning, Research & Eval Unit',
                 'created_user_id' => 1,
                 'created' => date('Y-m-d H:i:s')
             ],
-			[
+            [
                 'en' => 'District Education Officer',
                 'created_user_id' => 1,
                 'created' => date('Y-m-d H:i:s')
             ]
         ];
-		
-		
-		foreach($localeContents as $localeContent){
-			
-			// query()
-			$localeContentData = $this->query('SELECT * FROM `locale_contents` WHERE `en`="'.$localeContent['en'].'"'); // returns PDOStatement
-			$rows = $localeContentData->fetchAll(); // returns the result as an array
-			
-			if(count($rows) <= 0){
-				$this->insert('locale_contents', $localeContent);
-			}
-			
-		}
+
+
+        foreach ($localeContents as $localeContent) {
+
+            // query()
+            $localeContentData = $this->query('SELECT * FROM `locale_contents` WHERE `en`="' . $localeContent['en'] . '"'); // returns PDOStatement
+            $rows = $localeContentData->fetchAll(); // returns the result as an array
+
+            if (count($rows) <= 0) {
+                $this->insert('locale_contents', $localeContent);
+            }
+        }
     }
 
     public function down()
