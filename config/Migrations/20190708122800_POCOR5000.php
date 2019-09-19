0a78dd064012de03039fba7e318de8bb11129a0c
Brij Mohan Singh <bmsingh@dataforall.org>
2019-09-19 18:55:12 +0530

    POCOR-5000#small fix

--- config/Migrations/20190708122800_POCOR5000.php
+++ config/Migrations/20190708122800_POCOR5000.php
@@ -6,13 +6,15 @@ class POCOR5000 extends AbstractMigration
     public function up() 
     {
         $this->execute('ALTER TABLE examination_centres CHANGE COLUMN `fax` `fax` VARCHAR(30) NULL');
-        $this->execute('ALTER TABLE examination_centres CHANGE COLUMN `telephone` `telephone` VARCHAR(30) NOT NULL');
+        $this->execute('ALTER TABLE examination_centres CHANGE COLUMN `telephone` `telephone` VARCHAR(30) NOT NULL DEFAULT 0');
     }
+
+
 
     public function down() 
     {
         $this->execute('ALTER TABLE examination_centres CHANGE COLUMN `fax` `fax` VARCHAR(30) NOT NULL');
-        //$this->execute('ALTER TABLE examination_centres CHANGE COLUMN `telephone` `telephone` VARCHAR(30) NULL');
+        $this->execute('ALTER TABLE examination_centres CHANGE COLUMN `telephone` `telephone` VARCHAR(30) NULL');
     }
 
 }
