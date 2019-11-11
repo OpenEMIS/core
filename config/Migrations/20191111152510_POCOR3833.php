dd78aeb8648f3a5499e07c3114413e47c9f3074b
shubhamsharma08 <ssharma@dataforall.org>
2019-11-11 16:01:49 +0530

    POCOR-3833 #migration file

--- config/Migrations/20191111152510_POCOR3833.php
+++ config/Migrations/20191111152510_POCOR3833.php
@@ -11,15 +11,15 @@ class POCOR3833 extends AbstractMigration
         $this->execute('INSERT INTO `z_3833_config_items` SELECT * FROM `config_items`');
         
         $this->execute('ALTER TABLE `config_items` MODIFY id INT NOT NULL AUTO_INCREMENT');
-		$this->execute("Insert into `config_items` (`name`, `code`, `type`, `label`, `value`, `default_value`, `editable`, `visible`, `field_type`, `option_type`, `modified_user_id`, `modified`, `created_user_id`, `created`) values('Configure Student Name','configure_student_name','Student Settings','Configure Student Name','2','1','1','1','Dropdown','name_type','2','2019-11-11 08:21:48','1','2019-11-08 15:32:13')");
+		$this->execute("INSERT INTO `config_items` (`name`, `code`, `type`, `label`, `value`, `default_value`, `editable`, `visible`, `field_type`, `option_type`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES('Configure Student Name','configure_student_name','Student Settings','Configure Student Name','2','1','1','1','Dropdown','name_type','2','2019-11-11 08:21:48','1','2019-11-08 15:32:13')");
 		
 		//config item options		
 		$this->execute('CREATE TABLE `z_3833_config_item_options` LIKE `config_item_options`');
         $this->execute('INSERT INTO `z_3833_config_item_options` SELECT * FROM `config_item_options`');
 		
 		$this->execute('ALTER TABLE config_item_options MODIFY id INT NOT NULL AUTO_INCREMENT');
-		$this->execute("Insert into `config_item_options` (`id`, `option_type`, `option`, `value`, `order`, `visible`) values('103','name_type','identity - first_name last_name','1','1','1')");
-		$this->execute("Insert into `config_item_options` (`id`, `option_type`, `option`, `value`, `order`, `visible`) values('104','name_type','identity - full_name','2','2','1')");
+		$this->execute("INSERT INTO `config_item_options` (`option_type`, `option`, `value`, `order`, `visible`) VALUES('name_type','identity - first_name last_name','1','1','1')");
+		$this->execute("INSERT INTO `config_item_options` (`option_type`, `option`, `value`, `order`, `visible`) VALUES('name_type','identity - full_name','2','2','1')");
     }
 
      public function down()
@@ -30,6 +30,6 @@ class POCOR3833 extends AbstractMigration
 		
 		// config item options
         $this->execute('DROP TABLE IF EXISTS `config_item_options`');
-        $this->execute('RENAME TABLE `z_3833_config_item_options` TO `config_item_options`');
+        $this->execute('RENAME TABLE `z_3833_config_item_options` TO `config_items`');
 	}
 }
