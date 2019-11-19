d5fe9779ba1d9b1424cc3b3cf3ab9395050baedc
asingh1@dataforall.org <asingh1@dataforall.org>
2019-11-19 14:48:45 +0530

    minor changes

--- config/Migrations/20191119104525_POCOR4356b.php
+++ config/Migrations/20191119104525_POCOR4356b.php
@@ -6,12 +6,12 @@ class POCOR4356b extends AbstractMigration
 {
 	public function up()
     {
-        $this->execute('UPADTE security_functions SET visible=0  WHERE category = "Timetable"');
+        $this->execute('UPDATE security_functions SET visible=0  WHERE category = "Timetable"');
     }
 	
     // rollback
     public function down()
     {
-		$this->execute('UPADTE security_functions SET visible=0  WHERE category = "Timetable"');
+		$this->execute('UPDATE security_functions SET visible=0  WHERE category = "Timetable"');
     }
 }
