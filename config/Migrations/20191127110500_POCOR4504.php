ad4703dafee28e1b0460930cf134ac72d9fa7fa7
shubhamsharma08 <ssharma@dataforall.org>
2019-12-31 10:38:54 +0530

    POCOR-4504 migration code changes

--- config/Migrations/20191127110500_POCOR4504.php
+++ config/Migrations/20191127110500_POCOR4504.php
@@ -6,18 +6,13 @@ class POCOR4504 extends AbstractMigration
 {
     // commit
     public function up()
-    {
-        // backup the table
-        $this->execute('CREATE TABLE `z_4504_textbooks` LIKE `textbooks`');
-        $this->execute('INSERT INTO `z_4504_textbooks` SELECT * FROM `textbooks`');
-		
+    {		
         $this->execute('ALTER TABLE `textbooks` MODIFY author VARCHAR(200)');
     }
 
     // rollback
     public function down()
     {
-        $this->execute('DROP TABLE IF EXISTS `textbooks`');
-        $this->execute('RENAME TABLE `z_4504_textbooks` TO `textbooks`');
+        $this->execute('ALTER TABLE `textbooks` MODIFY author VARCHAR(100)');
     }
 }
