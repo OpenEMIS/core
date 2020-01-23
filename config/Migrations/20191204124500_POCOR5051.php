f38d39fd8258857a55ee93ddc8e2ef69a78c11a9
shubhamsharma08 <ssharma@dataforall.org>
2020-01-23 15:50:32 +0530

    POCOR-5051 #migration code

--- config/Migrations/20191204124500_POCOR5051.php
+++ config/Migrations/20191204124500_POCOR5051.php
@@ -11,14 +11,14 @@ class POCOR5051 extends AbstractMigration
         $this->execute('CREATE TABLE `z_5051_report_cards` LIKE `report_cards`');
         $this->execute('INSERT INTO `z_5051_report_cards` SELECT * FROM `report_cards`');
 
-        $this->execute('ALTER TABLE `report_cards` ADD COLUMN generate_start_date date AFTER end_date');
-         $this->execute('ALTER TABLE `report_cards` ADD COLUMN generate_end_date date AFTER generate_start_date');
+        $this->execute('ALTER TABLE `report_cards` ADD COLUMN generate_start_date date AFTER end_date');   
+        $this->execute('ALTER TABLE `report_cards` ADD COLUMN generate_end_date date AFTER generate_start_date');
     }
 
     // rollback
     public function down()
     {
         $this->execute('DROP TABLE IF EXISTS `report_cards`');
-        $this->execute('RENAME TABLE `z_5051_report_cards` TO `report_cards`');
+        $this->execute('RENAME TABLE `z_5051_report_cards` TO `report_cards`'); 
     }
 }
