3f2cce9096d4e85f8865ae430ac70f4044ad6b5f
asingh1@dataforall.org <asingh1@dataforall.org>
2020-02-03 16:40:29 +0530

    POCOR-5250#has been fixed

--- plugins/Institution/src/Model/Table/StaffLeaveTable.php
+++ plugins/Institution/src/Model/Table/StaffLeaveTable.php
@@ -113,22 +113,22 @@ class StaffLeaveTable extends ControllerActionTable
                         $InstitutionStaff->aliasField('institution_id = ') => $institutionId,
                         $InstitutionStaff->aliasField('staff_id = ') => $staffId
                     ])
-            ->order([$InstitutionStaff->aliasField('created') => 'DESC'])
+            ->order([$InstitutionStaff->aliasField('id') => 'DESC'])
             ->group([
                 $InstitutionStaff->aliasField('staff_id'),
                 $InstitutionStaff->aliasField('id')
             ])
-            ->toArray();
-            
-            $startDate = $staffData[0]['start_date']->format('Y-m-d');
+            ->first();
+        
+        $startDate = $staffData->start_date->format('Y-m-d');
             
         if ($startDate > $dateFrom) {
             $this->Alert->error('AlertRules.StaffLeave.noLeave', ['reset' => true]);
             return false;
         }
         
-        if (!empty($staffData[0]['end_date'])) {
-            $endDate = $staffData[0]['end_date']->format('Y-m-d');
+        if (!empty($staffData->end_date)) {
+            $endDate = $staffData->end_date->format('Y-m-d');
             
             if ($dateFrom > $endDate) {
                 $this->Alert->error('AlertRules.StaffLeave.noLeaveEndDate', ['reset' => true]);
@@ -138,7 +138,7 @@ class StaffLeaveTable extends ControllerActionTable
                 return false;
             }
         }
-
+        
         if (!$entity) {
             // Error message to tell that leave period applied has overlapped exisiting leave records.
             $this->Alert->error('AlertRules.StaffLeave.leavePeriodOverlap', ['reset' => true]);
