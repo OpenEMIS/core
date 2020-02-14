502e79f84099d6a612329d088f2656793306eeb5
asingh1@dataforall.org <asingh1@dataforall.org>
2020-02-14 12:24:16 +0530

    I have add the foreignKey institution_class_id now it is working fine

--- plugins/Institution/src/Model/Table/InstitutionClassesSecondaryStaffTable.php
+++ plugins/Institution/src/Model/Table/InstitutionClassesSecondaryStaffTable.php
@@ -8,7 +8,7 @@ class InstitutionClassesSecondaryStaffTable extends AppTable
     public function initialize(array $config)
     {
         parent::initialize($config);
-        $this->belongsTo('InstitutionClasses', ['className' => 'Institution.InstitutionClasses']);
+        $this->belongsTo('InstitutionClasses', ['className' => 'Institution.InstitutionClasses', 'foreignKey' => 'institution_class_id']);
         $this->belongsTo('SecondaryStaff', ['className' => 'User.Users']);
 
         $this->addBehavior('CompositeKey');
