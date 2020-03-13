41d9a4ed28142ef6c5e6e91fc286a628346a5dc5
asingh1@dataforall.org <asingh1@dataforall.org>
2020-03-13 12:35:07 +0530

    POCOR-4958#Institutions > Classes : Total number of students should not include the count for Repeated students Done

---
+++ config/Migrations/20200312044532_POCOR4958.php
@@ -0,0 +1,34 @@
+<?php
+
+use Cake\Utility\Text;
+use Cake\ORM\TableRegistry;
+use Phinx\Migration\AbstractMigration;
+
+class POCOR4958 extends AbstractMigration
+{
+   
+    public function up()
+    {
+       $this->execute('CREATE TABLE `zz_4958_institution_classes` LIKE `institution_classes`');
+       $this->execute('INSERT INTO `zz_4958_institution_classes` SELECT * FROM `institution_classes`');
+	   
+	   $InstitutionClassesTable = TableRegistry::get('Institution.InstitutionClasses');
+	   $InstitutionClassStudentsTable = TableRegistry::get('Institution.InstitutionClassStudents');
+	   
+	   $InstitutionClasses = $InstitutionClassesTable->find()
+			->hydrate(false)
+            ->toArray();
+		foreach($InstitutionClasses as $InstitutionClass){			
+			$countMale = $InstitutionClassStudentsTable->getMaleCountByClass($InstitutionClass['id']);
+            $countFemale = $InstitutionClassStudentsTable->getFemaleCountByClass($InstitutionClass['id']);	
+			
+			$InstitutionClassesTable->updateAll(['total_male_students' => $countMale, 'total_female_students' => $countFemale], ['id' => $InstitutionClass['id']]);					
+		}		
+    }
+
+    public function down()
+    {
+      $this->execute('DROP TABLE IF EXISTS `institution_classes`');
+      $this->execute('RENAME TABLE `zz_4958_institution_classes` TO `institution_classes`');  
+    }
+}
