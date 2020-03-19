29d5c074094a005078190357d425cd5e0ced0350
shubhamsharma08 <ssharma@dataforall.org>
2020-03-19 18:15:58 +0530

    POCOR-5033 #permission issue

--- config/Migrations/20200318111500_POCOR5033p.php
+++ config/Migrations/20200318111500_POCOR5033p.php
@@ -14,10 +14,18 @@ class POCOR5033p extends AbstractMigration
         $this->execute('CREATE TABLE `z_5033_security_role_functions` LIKE `security_role_functions`');
         $this->execute('INSERT INTO `z_5033_security_role_functions` SELECT * FROM `security_role_functions`');
 
+        $SecurityFunctions = TableRegistry::get('Security.SecurityFunctions');
+        $lastInsertId = $SecurityFunctions
+                          ->find()
+                          ->order($SecurityFunctions->aliasField('id') . ' DESC')
+                          ->first();
+
+        $id = $lastInsertId['id']+1;
+        
         //Insert data into security functions
-        $this->execute("INSERT INTO `security_functions` (`name`, `controller`, `module`, `category`, `parent_id`, `_view`, `_edit`, `_add`, 
+        $this->execute("INSERT INTO `security_functions` (`id`, `name`, `controller`, `module`, `category`, `parent_id`, `_view`, `_edit`, `_add`, 
 		`_delete`, `_execute`, `order`, `visible`, `description`, `modified_user_id`, `modified`, `created_user_id`, `created`) 
-		VALUES('Import Competency Templates','Competencies','Administration','Competencies','5000',NULL,NULL,NULL,NULL,'ImportCompetencyTemplates.add|ImportCompetencyTemplates.template|ImportCompetencyTemplates.results|ImportCompetencyTemplates.downloadFailed|ImportCompetencyTemplates.downloadPassed','9012','1',NULL,'2',NOW(),'1',NOW())");
+		VALUES(".$id.", 'Import Competency Templates','Competencies','Administration','Competencies','5000',NULL,NULL,NULL,NULL,'ImportCompetencyTemplates.add|ImportCompetencyTemplates.template|ImportCompetencyTemplates.results|ImportCompetencyTemplates.downloadFailed|ImportCompetencyTemplates.downloadPassed',".$id.",'1',NULL,'2',NOW(),'1',NOW())");
 
 		$this->execute("INSERT INTO `security_role_functions` (`_view`, `_edit`, `_add`, `_delete`, `_execute`, `security_role_id`, `security_function_id`, `modified_user_id`, `modified`, `created_user_id`, `created`) values('0','0','0','0','0','2',(SELECT id
             FROM security_functions WHERE name = 'Import Competency Templates'),NULL,NULL,'2',NOW())");
