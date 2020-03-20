c35054c40ab4d73a8fb98ca3b870e775fc21036d
shubhamsharma08 <ssharma@dataforall.org>
2020-03-20 18:30:23 +0530

    POCOR-5291 #labels change code fixed

--- config/Migrations/20200320125500_POCOR5291.php
+++ config/Migrations/20200320125500_POCOR5291.php
@@ -11,7 +11,7 @@ class POCOR5291 extends AbstractMigration
         $this->execute('CREATE TABLE `z_5291_labels` LIKE `labels`');
         $this->execute('INSERT INTO `z_5291_labels` SELECT * FROM `labels`');
 
-        $this->execute('UPDATE `labels` SET `module_name` = "Institutions -> Performance -> Statuses" WHERE `module` = "ReportCardStatuses"');
+        $this->execute('UPDATE `labels` SET `module_name` = "Institutions -> Performance -> Report Cards" WHERE `module` = "ReportCardStatuses"');
 
         $table = $this->table('labels');
 
@@ -20,7 +20,7 @@ class POCOR5291 extends AbstractMigration
                 'id' => Text::uuid(),
                 'module' => 'ReportCardStatuses',
                 'field' => 'report_card',
-                'module_name' => 'Institutions -> Performance -> Statuses',
+                'module_name' => 'Institutions -> Performance -> Report Cards',
                 'field_name' => 'Report Card',
                 'code' => NULL,
                 'name' => NULL,
@@ -39,6 +39,5 @@ class POCOR5291 extends AbstractMigration
 		//Restore backups
         $this->execute('DROP TABLE labels');
         $this->table('z_5291_labels')->rename('labels');
-        $this->execute("DELETE FROM labels WHERE id = '1ef9db3e-3f7f-11e7-9c23-525400b263eb'");
     }
 }
