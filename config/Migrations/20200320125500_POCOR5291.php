7d3d137f47faf950a340bdc4c776323a9d4c5500
shubhamsharma08 <ssharma@dataforall.org>
2020-03-20 18:06:11 +0530

    POCOR-5291 #labels change code fixed

--- config/Migrations/20200320125500_POCOR5291.php
+++ config/Migrations/20200320125500_POCOR5291.php
@@ -1,7 +1,7 @@
 <?php
+use Cake\Utility\Text;
 use Cake\ORM\TableRegistry;
 use Phinx\Migration\AbstractMigration;
-use Cake\Utility\Text;
 
 class POCOR5291 extends AbstractMigration
 {
@@ -11,16 +11,16 @@ class POCOR5291 extends AbstractMigration
         $this->execute('CREATE TABLE `z_5291_labels` LIKE `labels`');
         $this->execute('INSERT INTO `z_5291_labels` SELECT * FROM `labels`');
 
-        $this->execute('UPDATE `labels` SET `module_name` = "Institutions -> Performance -> Report Cards" WHERE `module` = "ReportCardStatuses"');
+        $this->execute('UPDATE `labels` SET `module_name` = "Institutions -> Performance -> Statuses" WHERE `module` = "ReportCardStatuses"');
 
         $table = $this->table('labels');
 
         $data = [
             [
-                'id' => '1ef9db3e-3f7f-11e7-9c23-525400b263eb',
+                'id' => Text::uuid(),
                 'module' => 'ReportCardStatuses',
                 'field' => 'report_card',
-                'module_name' => 'Institutions -> Performance -> Report Cards',
+                'module_name' => 'Institutions -> Performance -> Statuses',
                 'field_name' => 'Report Card',
                 'code' => NULL,
                 'name' => NULL,
@@ -39,5 +39,6 @@ class POCOR5291 extends AbstractMigration
 		//Restore backups
         $this->execute('DROP TABLE labels');
         $this->table('z_5291_labels')->rename('labels');
+        $this->execute("DELETE FROM labels WHERE id = '1ef9db3e-3f7f-11e7-9c23-525400b263eb'");
     }
 }
