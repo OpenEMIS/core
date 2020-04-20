e5d2387190260b8b1048cca09fe6ca6e27f0e8bf
nkkumar01 <nkkumar@dataforall.org>
2020-04-20 12:30:15 +0530

    POCOR-5159#Infrastructure Report

---
+++ plugins/Report/src/Model/Table/InstitutionsInfrastructureTable.php
@@ -0,0 +1,405 @@
+<?php
+namespace Report\Model\Table;
+
+use ArrayObject;
+use Cake\ORM\Entity;
+use Cake\ORM\Query;
+use Cake\Event\Event;
+use Cake\Network\Request;
+use App\Model\Table\AppTable;
+use App\Model\Traits\OptionsTrait;
+use Cake\I18n\Time;
+use Cake\ORM\TableRegistry;
+use Cake\ORM\Table;
+use Cake\Utility\Inflector;
+use Cake\Validation\Validator;
+use Institution\Model\Table\InstitutionsTable as Institutions;
+use Report\Model\Table\InstitutionPositionsTable as InstitutionPositions;
+
+class InstitutionsInfrastructureTable extends AppTable
+{
+    use OptionsTrait;
+    private $classificationOptions = [];
+
+    // filter
+    const NO_FILTER = 0;
+    const NO_STUDENT = 1;
+    const NO_STAFF = 2;
+
+    public function initialize(array $config)
+    {
+        
+        $this->table('institutions');
+        parent::initialize($config);
+
+        $this->belongsTo('Localities', ['className' => 'Institution.Localities', 'foreignKey' => 'institution_locality_id']);
+        $this->belongsTo('Types', ['className' => 'Institution.Types', 'foreignKey' => 'institution_type_id']);
+        $this->belongsTo('Ownerships', ['className' => 'Institution.Ownerships', 'foreignKey' => 'institution_ownership_id']);
+        $this->belongsTo('Statuses', ['className' => 'Institution.Statuses', 'foreignKey' => 'institution_status_id']);
+        $this->belongsTo('Sectors', ['className' => 'Institution.Sectors', 'foreignKey' => 'institution_sector_id']);
+        $this->belongsTo('Providers', ['className' => 'Institution.Providers', 'foreignKey' => 'institution_provider_id']);
+        $this->belongsTo('Genders', ['className' => 'Institution.Genders', 'foreignKey' => 'institution_gender_id']);
+        $this->belongsTo('Areas', ['className' => 'Area.Areas']);
+        $this->belongsTo('AreaAdministratives', ['className' => 'Area.AreaAdministratives']);
+        $this->belongsTo('InstitutionLands', ['className' => 'Institution.InstitutionLands']);
+        
+
+        $this->addBehavior('Excel', ['excludes' => ['security_group_id', 'logo_name'], 'pages' => false]);
+        $this->addBehavior('Report.ReportList');
+        $this->addBehavior('Report.CustomFieldList', [
+            'model' => 'Institution.Institutions',
+            'formFilterClass' => ['className' => 'InstitutionCustomField.InstitutionCustomFormsFilters'],
+            'fieldValueClass' => ['className' => 'InstitutionCustomField.InstitutionCustomFieldValues', 'foreignKey' => 'institution_id', 'dependent' => true, 'cascadeCallbacks' => true],
+            'tableCellClass' => ['className' => 'InstitutionCustomField.InstitutionCustomTableCells', 'foreignKey' => 'institution_id', 'dependent' => true, 'cascadeCallbacks' => true, 'saveStrategy' => 'replace']
+        ]);
+        $this->addBehavior('Report.InstitutionSecurity');
+
+        $this->shiftTypes = $this->getSelectOptions('Shifts.types'); //get from options trait
+
+        $this->classificationOptions = [
+            Institutions::ACADEMIC => 'Academic Institution',
+            Institutions::NON_ACADEMIC => 'Non-Academic Institution'
+        ];
+
+    }
+
+    public function validationDefault(Validator $validator)
+    {
+        $validator = parent::validationDefault($validator);
+
+        // validation for attendance marked record feature
+        $validator
+            ->add('report_start_date', [
+                'ruleCompareDate' => [
+                    'rule' => ['compareDate', 'report_end_date', true],
+                    'on' => function ($context) {
+                        $feature = $context['data']['feature'];
+                        return in_array($feature, ['Report.ClassAttendanceNotMarkedRecords', 'Report.InstitutionCases', 'Report.StudentAttendanceSummary']);
+                    }
+                ],
+                'ruleInAcademicPeriod' => [
+                    'rule' => ['inAcademicPeriod', 'academic_period_id', []],
+                    'on' => function ($context) {
+                        $feature = $context['data']['feature'];
+                        return in_array($feature, ['Report.ClassAttendanceNotMarkedRecords', 'Report.InstitutionCases' , 'Report.StudentAttendanceSummary']);
+                    },
+                    'message' => __('Report Start Date should be later than Academic Period Start Date')
+                ],
+            ]);
+
+        $validator
+            ->add('report_end_date', [
+                'ruleInAcademicPeriod' => [
+                    'rule' => ['inAcademicPeriod', 'academic_period_id', []],
+                    'on' => function ($context) {
+                        $feature = $context['data']['feature'];
+                        return in_array($feature, ['Report.ClassAttendanceNotMarkedRecords', 'Report.InstitutionCases', 'Report.StudentAttendanceSummary']);
+                    },
+                    'message' => __('Report End Date should be earlier than Academic Period End Date')
+                ],
+            ]);
+
+        return $validator;
+    }
+
+    public function validationSubjects(Validator $validator)
+    {
+        $validator = $this->validationDefault($validator);
+        $validator = $validator
+            ->notEmpty('institution_type_id')
+            ->notEmpty('institution_id');
+        return $validator;
+    }
+
+    public function validationBodyMasses(Validator $validator)
+    {
+        $validator = $this->validationDefault($validator);
+        $validator = $validator
+            ->notEmpty('institution_type_id')
+            ->notEmpty('institution_id');
+        return $validator;
+    }
+
+    public function validationStudents(Validator $validator)
+    {
+        $validator = $this->validationDefault($validator);
+        $validator = $validator
+            ->notEmpty('education_programme_id')
+            ->notEmpty('status');
+        return $validator;
+    }
+
+    public function validationStaff(Validator $validator)
+    {
+        $validator = $this->validationDefault($validator);
+        $validator = $validator
+            ->notEmpty('type')
+            ->notEmpty('status');
+        return $validator;
+    }
+
+    public function validationStudentAttendanceSummary(Validator $validator)
+    {
+        $validator = $this->validationDefault($validator);
+        $validator = $validator
+            ->notEmpty('institution_type_id')
+            ->notEmpty('institution_id')
+            ->notEmpty('education_grade_id');
+        return $validator;
+    }
+    
+    public function validationStaffAttendances(Validator $validator)
+    {
+        $validator = $this->validationDefault($validator);
+        $validator = $validator
+            ->notEmpty('institution_type_id')
+            ->notEmpty('institution_id');
+        return $validator;
+    }
+
+    public function beforeAction(Event $event)
+    { 
+        $this->fields = [];
+        $this->ControllerAction->field('feature', ['select' => false]);
+        $this->ControllerAction->field('format');
+    }
+
+    public function onGetReportName(Event $event, ArrayObject $data)
+    {
+        return __('Overview');
+    }
+
+    public function addBeforeAction(Event $event)
+    {
+        $this->ControllerAction->field('institution_filter', ['type' => 'hidden']);
+        $this->ControllerAction->field('position_filter', ['type' => 'hidden']);
+        // $this->ControllerAction->field('license', ['type' => 'hidden']);
+        $this->ControllerAction->field('type', ['type' => 'hidden']);
+        $this->ControllerAction->field('status', ['type' => 'hidden']);
+        $this->ControllerAction->field('module', ['type' => 'hidden']);
+        $this->ControllerAction->field('academic_period_id', ['type' => 'hidden']);
+        $this->ControllerAction->field('institution_type_id', ['type' => 'hidden']);
+        $this->ControllerAction->field('institution_id', ['type' => 'hidden']);
+        $this->ControllerAction->field('education_programme_id', ['type' => 'hidden']);
+        $this->ControllerAction->field('education_grade_id', ['type' => 'hidden']);
+        $this->ControllerAction->field('report_start_date', ['type' => 'hidden']);
+        $this->ControllerAction->field('report_end_date', ['type' => 'hidden']);
+    }
+
+    public function addBeforePatch(Event $event, Entity $entity, ArrayObject $data, ArrayObject $options)
+    {
+        if ($data[$this->alias()]['feature'] == 'Report.InstitutionSubjects') {
+            $options['validate'] = 'subjects';
+        } elseif ($data[$this->alias()]['feature'] == 'Report.InstitutionStudents') {
+            $options['validate'] = 'students';
+        } elseif ($data[$this->alias()]['feature'] == 'Report.InstitutionStaff') {
+            $options['validate'] = 'staff';
+        } elseif ($data[$this->alias()]['feature'] == 'Report.StudentAttendanceSummary') {
+            $options['validate'] = 'studentAttendanceSummary';
+        } elseif ($data[$this->alias()]['feature'] == 'Report.StaffAttendances') {
+            $options['validate'] = 'staffAttendances';
+        } elseif ($data[$this->alias()]['feature'] == 'Report.BodyMasses') {
+            $options['validate'] = 'bodyMasses';
+        }
+    }
+
+    public function addAfterAction(Event $event, Entity $entity)
+    {
+        
+        if ($entity->has('feature')) {
+            $feature = $entity->feature;
+           
+            $fieldsOrder = ['feature', 'format'];
+            switch ($feature) {
+                case 'Report.StudentAttendanceSummary':
+                    $fieldsOrder[] = 'institution_type_id';
+                    $fieldsOrder[] = 'institution_id';
+                    $fieldsOrder[] = 'academic_period_id';
+                    $fieldsOrder[] = 'education_grade_id';
+                    break;                
+                
+                default:
+                    break;
+            }
+
+            $this->ControllerAction->setFieldOrder($fieldsOrder);
+        }
+    }
+
+    public function onExcelBeforeStart(Event $event, ArrayObject $settings, ArrayObject $sheets)
+    {
+       
+        $requestData = json_decode($settings['process']['params']);
+        $feature = $requestData->feature;
+        $filter = $requestData->institution_filter;
+        if ($feature == 'Report.InstitutionsInfrastructure' && $filter == self::NO_FILTER) {
+            
+            $sheets[] = [
+                'name' => $this->alias(),
+                'table' => $this,
+                'query' => $this->find(),
+            ];
+          
+            // Stop the customfieldlist behavior onExcelBeforeStart function
+            $event->stopPropagation();
+        }
+    }
+
+    public function onExcelUpdateFields(Event $event, ArrayObject $settings, $fields)
+    {
+        
+        $requestData = json_decode($settings['process']['params']);
+        $feature = $requestData->feature;
+        $filter = $requestData->institution_filter;
+
+        $cloneFields = $fields->getArrayCopy();
+       //print_r( $cloneFields);die;
+        $newFields = [];
+        //foreach ($cloneFields as $key => $value) {
+             //$newFields[] = $value;
+
+         //   if ($value['field'] == 'code') {
+
+            $newFields[] = [
+                'key' => 'InstitutionsInfrastructure.name',
+                'field' => 'name',
+                'type' => 'string',
+                'label' => __('Institution Name')
+            ];
+
+            $newFields[] = [
+                'key' => 'InstitutionsInfrastructure.code',
+                'field' => 'code',
+                'type' => 'string',
+                'alias' => 'institution_code',
+                'label' => __('Institution Code')
+            ];
+
+            $newFields[] = [
+                'key' => 'institutionlands_code',
+                'field' => 'institutionlands_code',
+                'type' => 'string',
+                'label' => __('Infrastructure Code')
+            ];
+        // }
+        //}
+
+        //$fields->exchangeArray($newFields);
+
+        if ($feature == 'Report.InstitutionsInfrastructure' && $filter == self::NO_FILTER) {
+            // Stop the customfieldlist behavior onExcelUpdateFields function
+           
+            $includedFields = ['name', 'code', 'institutionlands_code'];
+            foreach ($newFields as $key => $value) {
+                if (!in_array($value['field'], $includedFields)) {
+                    unset($newFields[$key]);
+                }
+            }
+            $fields->exchangeArray($newFields);
+            $event->stopPropagation();
+        }
+    }
+
+    public function onExcelGetShiftType(Event $event, Entity $entity)
+    {
+        if (isset($this->shiftTypes[$entity->shift_type])) {
+            return __($this->shiftTypes[$entity->shift_type]);
+        } else {
+            return '';
+        }
+    }
+
+    public function onExcelGetClassification(Event $event, Entity $entity)
+    {
+        return __($this->classificationOptions[$entity->classification]);
+    }
+
+    public function onUpdateFieldFeature(Event $event, array $attr, $action, Request $request)
+    {
+        if ($action == 'add') {
+            $attr['options'] = $this->controller->getFeatureOptions($this->alias());
+            $attr['onChangeReload'] = true;
+            if (!(isset($this->request->data[$this->alias()]['feature']))) {
+                $option = $attr['options'];
+                reset($option);
+                $this->request->data[$this->alias()]['feature'] = key($option);
+            }
+            return $attr;
+        }
+    }
+
+    public function onUpdateFieldInstitutionFilter(Event $event, array $attr, $action, Request $request)
+    {
+        if (isset($this->request->data[$this->alias()]['feature'])) {
+            $feature = $this->request->data[$this->alias()]['feature'];
+            if ($feature == 'Report.InstitutionsInfrastructure') {
+                $option[self::NO_FILTER] = __('All Institutions');
+                $option[self::NO_STUDENT] = __('Institutions with No Students');
+                $option[self::NO_STAFF] = __('Institutions with No Staff');
+                $attr['type'] = 'select';
+                $attr['options'] = $option;
+                $attr['onChangeReload'] = true;
+                return $attr;
+            } else {
+                $attr['value'] = self::NO_FILTER;
+            }
+        }
+    }
+
+    public function onUpdateFieldPositionFilter(Event $event, array $attr, $action, Request $request)
+    {
+        if (isset($this->request->data[$this->alias()]['feature'])) {
+            $feature = $this->request->data[$this->alias()]['feature'];
+            if (in_array($feature, ['Report.InstitutionPositions'])) {
+                $options = [
+                    InstitutionPositions::ALL_POSITION => __('All Positions'),
+                    InstitutionPositions::POSITION_WITH_STAFF => __('Positions with Staff')
+                ];
+                $attr['type'] = 'select';
+                $attr['select'] = false;
+                $attr['options'] = $options;
+            } else {
+                $attr['value'] = self::NO_FILTER;
+            }
+            return $attr;
+        }
+    }
+
+ 
+
+    public function onExcelBeforeQuery(Event $event, ArrayObject $settings, Query $query)
+    {
+        $requestData = json_decode($settings['process']['params']);
+        $filter = $requestData->institution_filter;
+        $superAdmin = $requestData->super_admin;
+        $userId = $requestData->user_id;
+        $institutionLands = TableRegistry::get('Institution.InstitutionLands');
+        $institutionBuildings = TableRegistry::get('Institution.InstitutionBuildings');
+        $institutionFloors = TableRegistry::get('Institution.InstitutionFloors');
+        $institutionRooms = TableRegistry::get('Institution.InstitutionRooms');
+        $query
+                    ->select(['institutionlands_code'=>$institutionLands->aliasField('code'),
+                            'institutionlands_code'=>$institutionBuildings->aliasField('code'),
+                            'institutionlands_code'=>$institutionFloors->aliasField('code'),
+                            'institutionlands_code'=>$institutionRooms->aliasField('code')])
+                    ->select(['institutionlands_code'=>$institutionBuildings->aliasField('code')])
+                    ->innerJoin([$institutionLands->alias() => $institutionLands->table()], [
+                       $institutionLands->aliasField('institution_id = ') . $this->aliasField('id'),
+                     ])
+                    ->innerJoin([$institutionBuildings->alias() => $institutionBuildings->table()], [
+                        $institutionBuildings->aliasField('institution_id = ') . $this->aliasField('id'),
+                    ])
+                    ->innerJoin([$institutionFloors->alias() => $institutionFloors->table()], [
+                        $institutionFloors->aliasField('institution_id = ') . $this->aliasField('id'),
+                    ])
+                    ->innerJoin([$institutionRooms->alias() => $institutionRooms->table()], [
+                        $institutionRooms->aliasField('institution_id = ') . $this->aliasField('id'),
+                    ]);
+                    ;
+
+                           
+        if (!$superAdmin) {
+            $query->find('byAccess', ['user_id' => $userId, 'institution_field_alias' => $this->aliasField('id')]);
+        }
+    }
+}
