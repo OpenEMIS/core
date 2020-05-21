3045996e3b2ddbb17e7477f4a93d2d43d19dded5
shubhamsharma08 <ssharma@dataforall.org>
2020-05-21 18:58:03 +0530

    POCOR-5319 #student attendance dynamic periods

--- plugins/Attendance/src/Model/Table/StudentMarkTypesTable.php
+++ plugins/Attendance/src/Model/Table/StudentMarkTypesTable.php
@@ -106,6 +106,39 @@ class StudentMarkTypesTable extends ControllerActionTable
                 $entity = $this->StudentAttendanceMarkTypes->newEntity($studentMarkTypeData);
                 $this->StudentAttendanceMarkTypes->save($entity);
             }
+
+            $StudentAttendancePerDayPeriods = TableRegistry::get('Attendance.StudentAttendancePerDayPeriods');
+            for ($i=1;$i<=$attendancePerDay;$i++) {
+
+               
+                $id = $requestData['p'.$i];
+                
+                $PeriodsData = $StudentAttendancePerDayPeriods
+                ->find('all')
+                ->where([
+                    $StudentAttendancePerDayPeriods->aliasField('id = ') => $id
+                ])
+                ->all()
+                ->toArray();
+                if (!empty($PeriodsData)) {
+
+                    $StudentAttendancePerDayPeriods->updateAll(
+                        ['name' => $requestData['period'][$i]],
+                    [
+                        'id' => $id
+                    ]
+                    );
+                } else {
+                    
+                   $StudentAttendancePerDayPeriodsData = [
+                        'academic_period_id' => $requestData[$this->alias()]['academic_period_id'],
+                        'education_grade_id' => $requestData[$this->alias()]['id'],
+                        'name' => $requestData['period'][$i]
+                    ];
+                    $entity1 = $StudentAttendancePerDayPeriods->newEntity($StudentAttendancePerDayPeriodsData);
+                    $StudentAttendancePerDayPeriods->save($entity1);
+                }
+            }
         }
     }
 
@@ -116,6 +149,25 @@ class StudentMarkTypesTable extends ControllerActionTable
 
     public function viewAfterAction(Event $event, Entity $entity, ArrayObject $extra)
     {
+        $selectedAcademicPeriod = $this->getSelectedAcademicPeriod();
+        $entity->academic_period_id = $selectedAcademicPeriod;
+        $education_grade_id = $entity->id;
+        $StudentAttendancePerDayPeriods = TableRegistry::get('Attendance.StudentAttendancePerDayPeriods');
+        $StudentAttendancePerDayPeriodsData = $StudentAttendancePerDayPeriods
+        ->find('all')
+        ->where([
+            $StudentAttendancePerDayPeriods->aliasField('academic_period_id = ') => $selectedAcademicPeriod,
+            $StudentAttendancePerDayPeriods->aliasField('education_grade_id = ') => $education_grade_id
+        ])
+        ->all()
+        ->toArray();
+        if (!empty($entity->student_attendance_mark_types[0]->attendance_per_day)) {
+            $attendance_per_day = $entity->student_attendance_mark_types[0]->attendance_per_day;
+        } else {
+            $attendance_per_day = 1;
+        }
+        $this->controller->set('StudentAttendancePerDayPeriodsData', $StudentAttendancePerDayPeriodsData);
+        $this->controller->set('attendance_per_day', $attendance_per_day);
         $this->setupField($entity);
     }
 
@@ -123,6 +175,23 @@ class StudentMarkTypesTable extends ControllerActionTable
     {
         $selectedAcademicPeriod = $this->getSelectedAcademicPeriod();
         $entity->academic_period_id = $selectedAcademicPeriod;
+        $education_grade_id = $entity->id;
+        $StudentAttendancePerDayPeriods = TableRegistry::get('Attendance.StudentAttendancePerDayPeriods');
+        $StudentAttendancePerDayPeriodsData = $StudentAttendancePerDayPeriods
+        ->find('all')
+        ->where([
+            $StudentAttendancePerDayPeriods->aliasField('academic_period_id = ') => $selectedAcademicPeriod,
+            $StudentAttendancePerDayPeriods->aliasField('education_grade_id = ') => $education_grade_id
+        ])
+        ->all()
+        ->toArray();
+        if (!empty($entity->student_attendance_mark_types[0]->attendance_per_day)) {
+            $attendance_per_day = $entity->student_attendance_mark_types[0]->attendance_per_day;
+        } else {
+            $attendance_per_day = 1;
+        }
+        $this->controller->set('StudentAttendancePerDayPeriodsData', $StudentAttendancePerDayPeriodsData);
+        $this->controller->set('attendance_per_day', $attendance_per_day);
         $this->setupField($entity);
     }
 
@@ -157,7 +226,7 @@ class StudentMarkTypesTable extends ControllerActionTable
     {
         $selectedAcademicPeriod = $this->getSelectedAcademicPeriod();
 
-        $query
+        $query          
             ->contain([
                 'StudentAttendanceMarkTypes' => function ($q) use ($selectedAcademicPeriod) {
                     return $q->where(['StudentAttendanceMarkTypes.academic_period_id' => $selectedAcademicPeriod]);
@@ -235,9 +304,11 @@ class StudentMarkTypesTable extends ControllerActionTable
             $attr['select'] = false;
             $attr['value'] = $attendancePerDay;
             $attr['attr']['value'] = $attendancePerDay;
+            
 
             return $attr;
         }
+        
     }
 
     public function onGetAcademicPeriodId(Event $event, Entity $entity)
@@ -306,6 +377,14 @@ class StudentMarkTypesTable extends ControllerActionTable
             'index' => false, 'view' => true, 'edit' => true
         ]]);
         
+        
+        if ($this->action == 'edit' || $this->action == 'view') {
+        $this->field('periods', [
+                        'type' => 'element',
+                        'element' => 'Attendance.periods',
+                        
+                    ]);
+        }
         $this->setFieldOrder(['academic_period_id', 'name', 'student_attendance_type_id', 'attendance_per_day']);
     }
 
@@ -321,5 +400,19 @@ class StudentMarkTypesTable extends ControllerActionTable
         }
 
         return $selectedAcademicPeriod;
+    }
+
+    public function addEditOnChangeAcademicPeriodId(Event $event, Entity $entity, ArrayObject $data, ArrayObject $options, ArrayObject $extra)
+    {
+        $request = $this->request;
+        unset($request->query['period']);
+
+        if ($request->is(['post', 'put'])) {
+            if (array_key_exists($this->alias(), $request->data)) {
+                if (array_key_exists('academic_period_id', $request->data[$this->alias()])) {
+                    $request->query['period'] = $request->data[$this->alias()]['academic_period_id'];
+                }
+            }
+        }
     }
 }
---
+++ plugins/Attendance/src/Template/Element/periods.ctp
@@ -0,0 +1,61 @@
+<?php if ($ControllerAction['action'] == 'edit') {?>
+    <div class="toolbar-responsive panel-toolbar">
+        <div class="toolbar-wrapper">
+            <table class="table">
+            <thead>
+                <th><?= __('Period') ?></th>
+                <th><?= __('Name') ?></th>
+                
+            </thead>
+
+            
+                <tbody>
+                    <?php 
+                    $j = 1;
+                    for ($i=0; $i<$attendance_per_day; $i++) {  ?>
+                        <tr>
+                            <td>Period<?= $j;?></td>
+                            <td><?php echo $this->Form->input('period['.$j.']',
+                             [ 
+                                'value' => (!empty($StudentAttendancePerDayPeriodsData[$i]['name'])) ? $StudentAttendancePerDayPeriodsData[$i]['name'] : "Period ".$j
+
+                            ]); 
+                            echo $this->Form->hidden('p'.$j,
+                             [
+                                'value' => (!empty($StudentAttendancePerDayPeriodsData[$i]['id'])) ? $StudentAttendancePerDayPeriodsData[$i]['id'] : ""
+
+                            ]); ?></td>
+                            
+                        </tr>
+                    <?php $j++; } ?>
+                </tbody>
+        </table>
+        </div>
+    </div>
+<?php } else if ($ControllerAction['action'] == 'view') {?>
+    <div class="toolbar-responsive panel-toolbar">
+        <div class="toolbar-wrapper">
+            <table class="table">
+            <thead>
+                <th><?= __('Period') ?></th>
+                <th><?= __('Name') ?></th>
+                
+            </thead>
+
+            
+                <tbody>
+                    <?php 
+                    $j = 1;
+                    for ($i=0; $i<$attendance_per_day; $i++) {  ?>
+                        <tr>
+                            <td>Period<?= $j;?></td>
+                            <td><?php echo (!empty($StudentAttendancePerDayPeriodsData[$i]['name'])) ? $StudentAttendancePerDayPeriodsData[$i]['name'] : "Period ".$j ?></td>
+                            
+                        </tr>
+                    <?php $j++; } ?>
+                </tbody>
+        </table>
+        </div>
+    </div>
+<?php }?>
+
