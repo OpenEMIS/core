6bc58078efe875b64eafa4a420448505d9d30b90
nkkumar01 <nkkumar@dataforall.org>
2020-06-07 12:19:39 +0530

    POCOR-5146#code modify

--- plugins/Report/src/Model/Table/StudentsTable.php
+++ plugins/Report/src/Model/Table/StudentsTable.php
@@ -323,7 +323,7 @@ class StudentsTable extends AppTable
                     $attr['attr']['required'] = true;
                 } else {
 					
-                    if (in_array($feature, ['Report.BodyMasses', 'Report.InstitutionSubjects', 'Report.InstitutionClasses'])) {
+                    if (in_array($feature, ['Report.BodyMasses', 'Report.InstitutionSubjects', 'Report.InstitutionClasses','Report.SubjectsBookLists'])) {
                         $institutionOptions = ['' => '-- ' . __('Select') . ' --', '0' => __('All Institutions')] + $institutionList;
                     } else {
                         $institutionOptions = ['' => '-- ' . __('Select') . ' --'] + $institutionList;
