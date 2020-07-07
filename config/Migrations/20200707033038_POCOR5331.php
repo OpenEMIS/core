d77cde738a2cf2f9433fb62f8143620b030d55a9
asingh1@dataforall.org <asingh1@dataforall.org>
2020-07-07 16:10:48 +0530

    Migration for the security roles order

---
+++ config/Migrations/20200707033038_POCOR5331.php
@@ -0,0 +1,41 @@
+<?php
+use Migrations\AbstractMigration;
+use Cake\ORM\TableRegistry;
+
+class POCOR5331 extends AbstractMigration
+{
+	const FIXED_SYSTEM_GROUP_ID = -1;  // fixed system defined roles
+    const CUSTOM_SYSTEM_GROUP_ID = 0;  // custom system defined roles
+
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function up()
+    {
+        // Backup locale_contents table
+        $this->execute('CREATE TABLE `z_5331_security_roles` LIKE `security_roles`');
+		$this->execute('INSERT INTO `z_5331_security_roles` SELECT * FROM `security_roles`');
+		
+		$table = TableRegistry::get('SecurityRoles');
+		
+		$securityRoles = $table->find()->where(['security_group_id IN ' => [self::FIXED_SYSTEM_GROUP_ID, self::CUSTOM_SYSTEM_GROUP_ID]])->order(['`order` ASC']);
+		$order = 1;
+		
+		foreach($securityRoles as $securityRole){			
+			$this->execute('UPDATE `security_roles` SET `order` = "'.$order.'" WHERE `id`="'.$securityRole->id.'"');
+			$order++;
+		}
+		
+    }
+
+    // rollback
+    public function down()
+    {
+        $this->execute('DROP TABLE IF EXISTS `security_roles`');
+        $this->execute('RENAME TABLE `z_5331_security_roles` TO `security_roles`');
+    }
+}
