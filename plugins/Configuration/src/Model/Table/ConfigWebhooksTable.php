85c2304fd115dc417a677878aa18bd00aea85dfb
Poonam Kharka <poonam.kharka@mail.valuecoders.com>
2020-09-29 14:34:02 +0530

    Subject Update webhook

--- plugins/Configuration/src/Model/Table/ConfigWebhooksTable.php
+++ plugins/Configuration/src/Model/Table/ConfigWebhooksTable.php
@@ -19,7 +19,8 @@ class ConfigWebhooksTable extends ControllerActionTable
         'logout' => 'Logout',
         'institutions_create' => 'Institution Create',
         'class_create'  	  => 'Class Create',
-        'class_update'    	  => 'Class Update'
+        'class_update'    	  => 'Class Update',
+        'subject_update'      => 'Subject Update'
     ];
 
     public function initialize(array $config)
