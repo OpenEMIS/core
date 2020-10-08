7ddaaccacc7040eeeae2d2cdbce016b280d4ed3a
Poonam Kharka <poonam.kharka@mail.valuecoders.com>
2020-10-08 13:20:04 +0530

    staff update webhook

--- plugins/Configuration/src/Model/Table/ConfigWebhooksTable.php
+++ plugins/Configuration/src/Model/Table/ConfigWebhooksTable.php
@@ -24,7 +24,8 @@ class ConfigWebhooksTable extends ControllerActionTable
         'student_create'      => 'Student Create',
         'student_update'      => 'Student Update',
         'subject_update'      => 'Subject Update',
-        'staff_create'    	  => 'Staff Create'
+        'staff_create'    	  => 'Staff Create',
+        'staff_update'        => 'Staff Update'
     ];
 
     public function initialize(array $config)
