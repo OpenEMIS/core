4d8c15214b0d5343569750a9222dc01f45ef3f5d
asingh1@dataforall.org <asingh1@dataforall.org>
2020-10-14 17:03:14 +0530

    POCOR-5363# Modify the migration file and student mark types table file fixed the existing issue

--- config/Migrations/20200810122500_POCOR5363.php
+++ config/Migrations/20200810122500_POCOR5363.php
@@ -7,7 +7,7 @@ class POCOR5363 extends AbstractMigration
     public function up()
     {
         // Create tables
-        $this->execute("CREATE TABLE `student_mark_type_statuses` (
+        $this->execute("CREATE TABLE IF NOT EXISTS `student_mark_type_statuses` (
           `id` int(11) NOT NULL AUTO_INCREMENT,
           `academic_period_id` int(11) DEFAULT NULL,
           `student_attendance_mark_type_id` int(11) DEFAULT NULL,
@@ -15,7 +15,8 @@ class POCOR5363 extends AbstractMigration
           `date_disabled` date DEFAULT NULL,
           PRIMARY KEY (`id`)
         )");
-        $this->execute("CREATE TABLE `student_mark_type_status_grades` (
+        
+        $this->execute("CREATE TABLE IF NOT EXISTS `student_mark_type_status_grades` (
           `id` char(36) NOT NULL,
           `education_grade_id` int(11) NOT NULL,
           `student_mark_type_status_id` int(11) NOT NULL,
@@ -28,22 +29,19 @@ class POCOR5363 extends AbstractMigration
         $this->execute('INSERT INTO `z_5363_student_attendance_mark_types` SELECT * FROM `student_attendance_mark_types`');
         $this->execute('CREATE TABLE `z_5363_student_attendance_per_day_periods` LIKE `student_attendance_per_day_periods`');
         $this->execute('INSERT INTO `z_5363_student_attendance_per_day_periods` SELECT * FROM `student_attendance_per_day_periods`');
-        $this->execute('TRUNCATE `student_attendance_mark_types`');
+        
         $this->execute('ALTER TABLE `student_attendance_mark_types` DROP PRIMARY KEY');
-        $this->execute('ALTER TABLE `student_attendance_mark_types` DROP COLUMN `education_grade_id`');
-        $this->execute('ALTER TABLE `student_attendance_mark_types` DROP COLUMN `academic_period_id`');
-
         $this->execute("ALTER TABLE `student_attendance_mark_types` ADD COLUMN `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT FIRST");
         $this->execute("ALTER TABLE `student_attendance_mark_types` ADD COLUMN `name` varchar(100)  AFTER `id`");
         $this->execute("ALTER TABLE `student_attendance_mark_types` ADD COLUMN `code` varchar(100)  AFTER `name`");
+        
+        $this->execute("ALTER TABLE `student_attendance_mark_types` CHANGE `education_grade_id` `education_grade_id` INT(11) NULL DEFAULT NULL COMMENT 'links to education_grades.id', CHANGE `academic_period_id` `academic_period_id` INT(11) NULL DEFAULT NULL COMMENT 'links to academic_periods.id';");
+        
+        $this->execute("ALTER TABLE `student_attendance_per_day_periods` ADD COLUMN `student_attendance_mark_type_id` int(11) AFTER `name`");
 
-         $this->execute('TRUNCATE `student_attendance_per_day_periods`');
-         $this->execute('ALTER TABLE `student_attendance_per_day_periods` DROP COLUMN `education_grade_id`');
-         $this->execute('ALTER TABLE `student_attendance_per_day_periods` DROP COLUMN `academic_period_id`');
-         $this->execute("ALTER TABLE `student_attendance_per_day_periods` ADD COLUMN `student_attendance_mark_type_id` int(11) AFTER `name`");
-
-         $this->execute('CREATE TABLE `z_5363_locale_contents` LIKE `locale_contents`');
+        $this->execute('CREATE TABLE `z_5363_locale_contents` LIKE `locale_contents`');
         $this->execute('INSERT INTO `z_5363_locale_contents` SELECT * FROM `locale_contents`');
+        
         // locale_contents
         $localeContent = [
             [
@@ -78,9 +76,6 @@ class POCOR5363 extends AbstractMigration
     public function down()
     {
         // For tables
-        $this->execute('DROP TABLE IF EXISTS `student_mark_type_statuses`');
-        $this->execute('DROP TABLE IF EXISTS `student_mark_type_status_grades`');
-        
         $this->execute('DROP TABLE IF EXISTS `student_attendance_mark_types`');
         $this->execute('RENAME TABLE `z_5363_student_attendance_mark_types` TO `student_attendance_mark_types`'); 
         $this->execute('DROP TABLE IF EXISTS `student_attendance_per_day_periods`');
