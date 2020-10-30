57d9adf8dc746c226e6e21abec49caaa15507850
Neelima Sharma <neelimamausmisharma@gmail.com>
2020-10-30 09:57:38 +0530

    fix in migration and log folder

--- config/Migrations/20201029100055_POCOR5674.php
+++ config/Migrations/20201029100055_POCOR5674.php
@@ -14,38 +14,11 @@ class POCOR5674 extends AbstractMigration
     public function up()
     {
         // institution_class_students
-        $this->execute('CREATE TABLE `archive_table_logs` (
-            `id` int(11) NOT NULL,
-            `name` varchar(250) NOT NULL,
-            `path` varchar(255) NOT NULL,
-            `generated_on` datetime NOT NULL,
-            `generated_by` varchar(100) NOT NULL
-          )');
-        $this->execute('CREATE TABLE `deleted_table_logs` (
-            `id` int(11) NOT NULL,
-            `archive_table_logs_id` int(11) NOT NULL,
-            `academic_period_id` int(11) NOT NULL,
-            `deleted_on` datetime NOT NULL,
-            `deleted_by` varchar(100) NOT NULL
-          )');
-        $this->execute('CREATE TABLE `archive_connections` (
-            `id` int(11) NOT NULL,
-            `name` varchar(50) NOT NULL,
-            `db_type_id` int(11) NOT NULL,
-            `host` varchar(100) NOT NULL,
-            `host_port` int(11) NOT NULL,
-            `db_name` varchar(50) NOT NULL,
-            `username` varchar(50) NOT NULL,
-            `password` varchar(255) NOT NULL,
-            `conn_status_id` int(11) NOT NULL DEFAULT 0,
-            `status_checked` datetime DEFAULT NULL,
-            `modified_user_id` int(11) DEFAULT NULL,
-            `modified` datetime DEFAULT NULL,
-            `created_user_id` int(11) NOT NULL,
-            `created` datetime NOT NULL
-          )');
+        $this->execute("CREATE TABLE `archive_table_logs` ( `id` int(11) NOT NULL AUTO_INCREMENT, `name` varchar(250) NOT NULL, `path` varchar(250) NOT NULL,  `generated_on` datetime DEFAULT NULL, `generated_by` int(11) NOT NULL, PRIMARY KEY (`id`) )");
         
+        $this->execute("CREATE TABLE `deleted_logs` ( `id` int(11) NOT NULL AUTO_INCREMENT, `academic_period_id` int(11) NOT NULL, `generated_on` datetime DEFAULT NULL, `generated_by` int(11) NOT NULL, PRIMARY KEY (`id`), KEY `generated_on` (`generated_on`), KEY `generated_by` (`generated_by`) )");
         
+        $this->execute("CREATE TABLE `archive_connections` ( `id` int(11) NOT NULL AUTO_INCREMENT, `name` varchar(50) NOT NULL, `db_type_id` int(3) NOT NULL, `host` varchar(100) NOT NULL, `host_port` int(11) NOT NULL, `db_name` varchar(100) NOT NULL, `username` varchar(50) NOT NULL, `password` varchar(255) NOT NULL, `conn_status_id` int(1) DEFAULT '0', `status_checked` datetime DEFAULT NULL, `modified_user_id` int(11) DEFAULT NULL, `modified` datetime DEFAULT NULL, `created_user_id` int(11) NOT NULL, `created` datetime NOT NULL, PRIMARY KEY (`id`), KEY `modified_user_id` (`modified_user_id`), KEY `created_user_id` (`created_user_id`) )");
     }
 
     // rollback
@@ -53,6 +26,7 @@ class POCOR5674 extends AbstractMigration
     {
         // institution_class_students
         $this->execute('DROP TABLE IF EXISTS `archive_table_logs`');
-        $this->execute('DROP TABLE IF EXISTS `deleted_table_logs`');
+        $this->execute('DROP TABLE IF EXISTS `deleted_logs`');
         $this->execute('DROP TABLE IF EXISTS `archive_connections`');
     }
+}
