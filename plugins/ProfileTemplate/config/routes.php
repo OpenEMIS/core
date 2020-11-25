32db4b00ecd4d6f2b4306fa1c5edcbcdf80560ed
priyanka <priyankaguptaphp@mycontactsca.com>
2020-11-25 13:22:34 +0530

    remove folder

--- plugins/ProfileTemplate/config/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-use Cake\Routing\Router;
-
-Router::scope('/ProfileTemplate', ['plugin' => 'ProfileTemplate'], function ($routes) {
-    Router::connect('/ProfileTemplate', ['plugin' => 'ProfileTemplate', 'controller' => 'ProfileTemplate']);
-    Router::connect('/ProfileTemplate/:action/*', ['plugin' => 'ProfileTemplate', 'controller' => 'ProfileTemplate']);
-});
