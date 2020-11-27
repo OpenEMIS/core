db4a4eba5010bc3f2ae9107dd2f666047493d5fb
asingh1@dataforall.org <asingh1@dataforall.org>
2020-11-27 10:44:12 +0530

    POCOR-5789#i have fixed the migration issue done

--- config/Migrations/20201126044112_POCOR5789.php
+++ config/Migrations/20201126044112_POCOR5789.php
@@ -28,17 +28,21 @@ class POCOR5789 extends AbstractMigration
 		
 		$this->execute('ALTER TABLE `student_attendance_marked_records` ADD `education_grade_id` INT NOT NULL AFTER `institution_class_id`');
 		
+		$this->execute("ALTER TABLE `institution_student_absences` CHANGE `education_grade_id` `education_grade_id` INT(11) NOT NULL DEFAULT '0'");
+		$this->execute("ALTER TABLE `institution_student_absence_details` CHANGE `education_grade_id` `education_grade_id` INT(11) NOT NULL DEFAULT '0'");
+		$this->execute("ALTER TABLE `student_attendance_marked_records` CHANGE `education_grade_id` `education_grade_id` INT(11) NOT NULL DEFAULT '0'");
+		
 		$this->execute('UPDATE `student_attendance_marked_records`
 		LEFT JOIN `institution_class_grades` ON `institution_class_grades`.institution_class_id = `student_attendance_marked_records`.institution_class_id
-		SET student_attendance_marked_records.education_grade_id = institution_class_grades.education_grade_id');
+		SET student_attendance_marked_records.education_grade_id = institution_class_grades.education_grade_id WHERE institution_class_grades.education_grade_id IS NOT NULL');
 		
 		$this->execute('UPDATE `institution_student_absences`
 		LEFT JOIN `institution_class_grades` ON `institution_class_grades`.institution_class_id = `institution_student_absences`.institution_class_id
-		SET institution_student_absences.education_grade_id = institution_class_grades.education_grade_id');
+		SET institution_student_absences.education_grade_id = institution_class_grades.education_grade_id WHERE institution_class_grades.education_grade_id IS NOT NULL');
 		
 		$this->execute('UPDATE `institution_student_absence_details`
 		LEFT JOIN `institution_class_grades` ON `institution_class_grades`.institution_class_id = `institution_student_absence_details`.institution_class_id
-		SET institution_student_absence_details.education_grade_id = institution_class_grades.education_grade_id');
+		SET institution_student_absence_details.education_grade_id = institution_class_grades.education_grade_id WHERE institution_class_grades.education_grade_id IS NOT NULL');
 		
     }
 
