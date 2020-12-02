4223349415cde5e03f4999f617d7a6196ebcea38
Ehteram Ahmad <ehteram.ahmad@mail.valuecoders.com>
2020-12-02 16:52:31 +0530

    Changes for assesment

--- plugins/Institution/src/Model/Table/AssessmentsArchiveTable.php
+++ plugins/Institution/src/Model/Table/AssessmentsArchiveTable.php
@@ -16,6 +16,7 @@ use Cake\Chronos\Date;
 use Cake\Datasource\ResultSetInterface;
 use Cake\Core\Configure;
 use Cake\Datasource\ConnectionManager;
+use Cake\Utility\Security;
 
 use App\Model\Table\ControllerActionTable;
 class AssessmentsArchiveTable extends ControllerActionTable
@@ -27,14 +28,38 @@ class AssessmentsArchiveTable extends ControllerActionTable
     public function initialize(array $config)
     {
         $connectionone = ConnectionManager::get('default');
-        $connectiontwo = ConnectionManager::get('prd_cor_arc');
-        $db1 =  $connectionone->config()['database'];
-
-        $getArchiveConnection = $connectionone->query("SELECT * FROM transfer_connections");
-        $archiveConnection = $getArchiveConnection->fetchAll();
-        foreach($archiveConnection AS $archiveConnectionData){
-            $db2 = $archiveConnectionData[5];
+        $db1 = $connectionone->config()['database'];
+        $transferConnections = TableRegistry::get('TransferConnections.TransferConnections');
+        $transferConnectionsData = $transferConnections->find('all')
+            ->select([
+                'TransferConnections.host','TransferConnections.db_name','TransferConnections.host','TransferConnections.username','TransferConnections.password','TransferConnections.db_name'
+            ])
+            ->first();
+        if ( base64_encode(base64_decode($transferConnectionsData['password'], true)) === $transferConnectionsData['password']){
+        $db_password = $this->decrypt($transferConnectionsData['password'], Security::salt());
         }
+        else {
+        $db_password = $dbConnection['db_password'];
+        }
+        $connectiontwo = ConnectionManager::config($transferConnectionsData['db_name'], [
+            'className' => 'Cake\Database\Connection',
+            'driver' => 'Cake\Database\Driver\Mysql',
+            'persistent' => false,
+            'host' => $transferConnectionsData['host'],
+            'username' => $transferConnectionsData['username'],
+            'password' => $db_password,
+            'database' => $transferConnectionsData['db_name'],
+            'encoding' => 'utf8mb4',
+            'timezone' => 'UTC',
+            'cacheMetadata' => true,
+        ]);
+        $checkconnection = ConnectionManager::get($transferConnectionsData['db_name']);
+        $collection = $checkconnection->schemaCollection();
+        $tableSchema = $collection->listTables();
+        $table1 = $tableSchema[0];
+
+        $stmt1 = $connectionone->prepare("CREATE OR REPLACE VIEW assessment_item_results_archived AS SELECT * FROM $table1");
+        $stmt1->execute();
 
         $getArchiveData = $connectionone->query("SELECT 
         academic_periods.name AS 'Academic Periods',
@@ -45,34 +70,34 @@ class AssessmentsArchiveTable extends ControllerActionTable
         education_subjects.name AS 'Subject',
         security_users.openemis_no AS 'OpenEMIS ID',
         CONCAT(security_users.first_name,' ',security_users.last_name) AS 'Student Name',
-        IF(assessment_item_results.marks IS NULL,'-',assessment_item_results.marks) AS 'Mark',
+        IF(assessment_item_results_archived.marks IS NULL,'-',assessment_item_results_archived.marks) AS 'Mark',
         CONCAT(assessment_grading_options.code,'-',assessment_grading_options.name) AS 'Grade'
-        FROM $db1.assessment_item_results
+        FROM $db1.assessment_item_results_archived
         INNER JOIN assessment_grading_options
-        ON assessment_item_results.assessment_grading_option_id = assessment_grading_options.id
+        ON assessment_item_results_archived.assessment_grading_option_id = assessment_grading_options.id
         INNER JOIN security_users
-        ON assessment_item_results.student_id = security_users.id
+        ON assessment_item_results_archived.student_id = security_users.id
         INNER JOIN academic_periods
-        ON academic_periods.id = assessment_item_results.academic_period_id
+        ON academic_periods.id = assessment_item_results_archived.academic_period_id
         INNER JOIN assessments
-        ON assessments.id = assessment_item_results.assessment_id
+        ON assessments.id = assessment_item_results_archived.assessment_id
         INNER JOIN assessment_periods
         ON assessment_periods.assessment_id = assessments.id
-        AND assessment_periods.id = assessment_item_results.assessment_period_id 
+        AND assessment_periods.id = assessment_item_results_archived.assessment_period_id 
         INNER JOIN assessment_items
         ON assessment_items.assessment_id = assessments.id
         INNER JOIN education_subjects
         ON education_subjects.id = assessment_items.education_subject_id
-        AND education_subjects.id = assessment_item_results.education_subject_id
+        AND education_subjects.id = assessment_item_results_archived.education_subject_id
         INNER JOIN education_grades
         ON education_grades.id = assessments.education_grade_id
-        AND education_grades.id = assessment_item_results.education_grade_id
+        AND education_grades.id = assessment_item_results_archived.education_grade_id
         INNER JOIN institution_class_students
         ON institution_class_students.student_id = security_users.id
-        AND institution_class_students.academic_period_id = assessment_item_results.academic_period_id
+        AND institution_class_students.academic_period_id = assessment_item_results_archived.academic_period_id
         INNER JOIN institution_classes
         ON institution_classes.id = institution_class_students.institution_class_id
-        AND institution_classes.academic_period_id = assessment_item_results.academic_period_id 
+        AND institution_classes.academic_period_id = assessment_item_results_archived.academic_period_id 
         WHERE academic_periods.current = 1;
         ");
         $archiveDataArr = $getArchiveData->fetchAll();
@@ -90,20 +115,16 @@ class AssessmentsArchiveTable extends ControllerActionTable
         // $this->attachAngularModules();
     }
 
-    // private function attachAngularModules()
-    // {
-    //     $action = $this->request->action;
-    //     switch ($action) {
-    //         case 'AssessmentsArchive':
-    //             $this->Angular->addModules([
-    //                 'institution.assessments.archive.ctrl',
-    //                 'institution.assessments.archive.svc'
-    //             ]);
-    //             break;
-    //     }
-    // }
+    public function decrypt($encrypted_string, $secretHash) {
 
-    // To select another one more field from the containable data
+        $iv = substr($secretHash, 0, 16);
+        $data = base64_decode($encrypted_string);
+        $decryptedMessage = openssl_decrypt($data, "AES-256-CBC", $secretHash, $raw_input = false, $iv);
+        $decrypted = rtrim(
+            $decryptedMessage
+        );
+        return $decrypted;
+    }
 
     
 }
