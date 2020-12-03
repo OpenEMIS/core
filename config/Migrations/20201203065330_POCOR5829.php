ae673062fc653b309ab5eaeeafd14ec993894e73
Ehteram Ahmad <ehteram.ahmad@mail.valuecoders.com>
2020-12-03 15:56:11 +0530

    Check

--- config/Migrations/20201203065330_POCOR5829.php
+++ config/Migrations/20201203065330_POCOR5829.php
@@ -12,17 +12,24 @@ class POCOR5829 extends AbstractMigration
      */
     public function up()
     {
+
+        $this->execute("UPDATE `api_securities` SET `execute` = '1' WHERE `name` = 'User Athentication' AND `model` = 'User.Users'");
         // Backup locale_contents table
         $this->execute('CREATE TABLE `zz_5829_api_securities_scopes` LIKE `api_securities_scopes`');
         $this->execute('INSERT INTO `zz_5829_api_securities_scopes` SELECT * FROM `api_securities_scopes`');
         // End
-
-        $this->execute("UPDATE `api_securities` SET `execute` = '1' WHERE `name` = 'User Athentication' AND `model` = 'User.Users'");
         
+        $getApiSecurityId = $this->query("SELECT * FROM api_securities WHERE `name` = 'User Athentication' AND `model` = 'User.Users'");
+        $apiSecurityId = $getApiSecurityId->fetchAll();
+        $apiSecurityKey = $apiSecurityId[0]['id'];
+
+        $getApiScope = $this->query("SELECT * FROM api_scopes WHERE `name` = 'API'");
+        $apiScope = $getApiScope->fetchAll();
+        $apiScopeId = $apiScope[0]['id'];
 		
 		$this->insert('api_securities_scopes', [
-            'api_security_id' => 1031,
-            'api_scope_id' => 1,
+            'api_security_id' => $apiSecurityKey,
+            'api_scope_id' => $apiScopeId,
             'index' => 0,
             'view' => 0,
             'add' => 0,
