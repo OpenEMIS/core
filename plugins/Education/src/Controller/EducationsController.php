d3967c360f9f006e01ab237fcd5e14e25900bb00
Delll <anubhavjain07@outlook.com>
2020-12-17 18:46:29 +0530

    first commit

--- plugins/Education/src/Controller/EducationsController.php
+++ plugins/Education/src/Controller/EducationsController.php
@@ -17,51 +17,58 @@ class EducationsController extends AppController {
     public function beforeFilter(Event $event) {
         parent::beforeFilter($event);
 
-        $selectedAction = $this->request->action;
-        $setupTab = 'Stages';
-        if (in_array($selectedAction, ['Stages', 'Subjects', 'Certifications', 'ProgrammeOrientations', 'FieldOfStudies'])) {
-            $setupTab = $selectedAction;
-        }
+        if($this->request->action != 'CopySystems'){
+            $selectedAction = $this->request->action;
+            $setupTab = 'Stages';
+            if (in_array($selectedAction, ['Stages', 'Subjects', 'Certifications', 'ProgrammeOrientations', 'FieldOfStudies'])) {
+                $setupTab = $selectedAction;
+            }
 
-        $tabElements = [
-            'Systems' => [
-                'url' => ['plugin' => 'Education', 'controller' => 'Educations', 'action' => 'Systems'],
-                'text' => __('Systems')
-            ],
-            'Levels' => [
-                'url' => ['plugin' => 'Education', 'controller' => 'Educations', 'action' => 'Levels'],
-                'text' => __('Levels')
-            ],
-            'Cycles' => [
-                'url' => ['plugin' => 'Education', 'controller' => 'Educations', 'action' => 'Cycles'],
-                'text' => __('Cycles')
-            ],
-            'Programmes' => [
-                'url' => ['plugin' => 'Education', 'controller' => 'Educations', 'action' => 'Programmes'],
-                'text' => __('Programmes')
-            ],
-            'Grades' => [
-                'url' => ['plugin' => 'Education', 'controller' => 'Educations', 'action' => 'Grades'],
-                'text' => __('Grades')
-            ],
-            'GradeSubjects' => [
-                'url' => ['plugin' => 'Education', 'controller' => 'Educations', 'action' => 'GradeSubjects'],
-                'text' => __('Grade Subjects')
-            ],
-            $setupTab => [
-                'url' => ['plugin' => 'Education', 'controller' => 'Educations', 'action' => $setupTab],
-                'text' => __('Setup')
-            ]
-        ];
-        $tabElements = $this->TabPermission->checkTabPermission($tabElements);
-        $this->set('tabElements', $tabElements);
-        $this->set('selectedAction', $selectedAction);
+            $tabElements = [
+                'Systems' => [
+                    'url' => ['plugin' => 'Education', 'controller' => 'Educations', 'action' => 'Systems'],
+                    'text' => __('Systems')
+                ],
+                'Levels' => [
+                    'url' => ['plugin' => 'Education', 'controller' => 'Educations', 'action' => 'Levels'],
+                    'text' => __('Levels')
+                ],
+                'Cycles' => [
+                    'url' => ['plugin' => 'Education', 'controller' => 'Educations', 'action' => 'Cycles'],
+                    'text' => __('Cycles')
+                ],
+                'Programmes' => [
+                    'url' => ['plugin' => 'Education', 'controller' => 'Educations', 'action' => 'Programmes'],
+                    'text' => __('Programmes')
+                ],
+                'Grades' => [
+                    'url' => ['plugin' => 'Education', 'controller' => 'Educations', 'action' => 'Grades'],
+                    'text' => __('Grades')
+                ],
+                'GradeSubjects' => [
+                    'url' => ['plugin' => 'Education', 'controller' => 'Educations', 'action' => 'GradeSubjects'],
+                    'text' => __('Grade Subjects')
+                ],
+                $setupTab => [
+                    'url' => ['plugin' => 'Education', 'controller' => 'Educations', 'action' => $setupTab],
+                    'text' => __('Setup')
+                ]
+            ];
+            $tabElements = $this->TabPermission->checkTabPermission($tabElements);
+            $this->set('tabElements', $tabElements);
+            $this->set('selectedAction', $selectedAction);
+        }
     }
 
     public function onInitialize(Event $event, Table $model, ArrayObject $extra) {
         $header = __('Education');
-
-        $header .= ' - ' . $model->getHeader($model->alias);
+        if($this->request->action == 'CopySystems'){
+            $model->alias = 'Systems';
+            $header .= ' - ' . $model->getHeader('SystemsCopy');
+        }else{
+            $header .= ' - ' . $model->getHeader($model->alias);
+        }
+        
         $this->Navigation->addCrumb('Education Structure', ['plugin' => 'Education', 'controller' => 'Educations', 'action' => $model->alias]);
         $this->Navigation->addCrumb($model->getHeader($model->alias));
 
@@ -85,6 +92,10 @@ class EducationsController extends AppController {
     }
 
     public function Systems() {
+        $this->ControllerAction->process(['alias' => __FUNCTION__, 'className' => 'Education.EducationSystems']);
+    }
+
+    public function CopySystems() {
         $this->ControllerAction->process(['alias' => __FUNCTION__, 'className' => 'Education.EducationSystems']);
     }
 
