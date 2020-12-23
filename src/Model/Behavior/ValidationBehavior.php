7bcc5c99b740da37d432d2d26e474c4d556e4440
Delll <anubhavjain07@outlook.com>
2020-12-23 13:02:39 +0530

    bug solved

--- src/Model/Behavior/ValidationBehavior.php
+++ src/Model/Behavior/ValidationBehavior.php
@@ -559,7 +559,7 @@ class ValidationBehavior extends Behavior
         $ContactOptionsTable = TableRegistry::get('User.ContactOptions');
         $contactOptionOther = $ContactOptionsTable->getIdByCode('OTHER');
 
-    	$flag = false;
+        $flag = false;
         $contactOption = $globalData['data']['contact_option_id'];
         $userId = $globalData['data']['security_user_id'];
         $currentField = $globalData['field'];
@@ -581,7 +581,7 @@ class ValidationBehavior extends Behavior
         if ($currentField == 'preferred') {
             $preferred = $field;
 
-        	if ($preferred == "0" && $contactOption != $contactOptionOther) { //during not preferred set ot contact type is 'others'
+            if ($preferred == "0" && $contactOption != $contactOptionOther) { //during not preferred set ot contact type is 'others'
                 $query->where([$Contacts->aliasField('preferred') => 1]);
                 $count = $query->count();
 
@@ -2947,7 +2947,8 @@ class ValidationBehavior extends Behavior
         //$field is for external variable
         if($field == 1){
             $ConfigItems = TableRegistry::get('Configuration.ConfigItems');
-            $type = $ConfigItems->value('external_data_source_type');
+            $external_data = $ConfigItems->findByCode('external_data_source_type')->first();
+            $type = $external_data->default_value;
             if($type == 'None'){
                 return false;
             }
