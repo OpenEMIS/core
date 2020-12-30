7f2773b5d17f95626a24ff7522da268818c60f2a
Rahul Singh <rahul.singh@mail.valuecoders.com>
2020-12-30 18:17:47 +0530

    benefit feild option add

---
+++ plugins/Meal/src/Model/Table/MealBenefitTable.php
@@ -0,0 +1,14 @@
+<?php
+namespace Meal\Model\Table;
+
+use App\Model\Table\ControllerActionTable;
+
+class MealBenefitTable extends ControllerActionTable
+{
+    public function initialize(array $config)
+    {
+        $this->table('meal_benefits');
+        parent::initialize($config);        
+        $this->addBehavior('FieldOption.FieldOption');
+    }
+}
