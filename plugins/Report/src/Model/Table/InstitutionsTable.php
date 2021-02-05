bba0ab2fc944e1bd5cdfc68243fe3c9184320de8
Delll <anubhavjain07@outlook.com>
2021-02-05 15:43:12 +0530

    add all institution in institution>infrastructure

--- plugins/Report/src/Model/Table/InstitutionsTable.php
+++ plugins/Report/src/Model/Table/InstitutionsTable.php
@@ -881,8 +881,10 @@ class InstitutionsTable extends AppTable
                     $attr['options'] = $institutionOptions;
                     $attr['attr']['required'] = true;
                 } else {
-                    if (in_array($feature, ['Report.BodyMasses', 'Report.InstitutionSubjects', 'Report.InstitutionClasses','Report.StudentAbsences','Report.InstitutionSubjectsClasses', 'Report.StudentAttendanceSummary', 'Report.SpecialNeedsFacilities', 'Report.Income', 'Report.Expenditure', 'Report.WashReports'])) {
+                    if (in_array($feature, ['Report.BodyMasses', 'Report.InstitutionSubjects', 'Report.InstitutionClasses','Report.StudentAbsences','Report.InstitutionSubjectsClasses', 'Report.SpecialNeedsFacilities', 'Report.Income', 'Report.Expenditure', 'Report.WashReports'])) {
                         $institutionOptions = ['' => '-- ' . __('Select') . ' --', '0' => __('All Institutions')] + $institutionList;
+                    }else if (in_array($feature, ['Report.StudentAttendanceSummary'])) {//POCOR-5906 starts
+                            $institutionOptions = ['' => '-- ' . __('Select') . ' --'] + $institutionList;//POCOR-5906 ends
                     } else {
                         //add All Institution task POCOR 5698
                         $institutionOptions = ['' => '-- ' . __('Select') . ' --', '0' => __('All Institutions')] + $institutionList;
