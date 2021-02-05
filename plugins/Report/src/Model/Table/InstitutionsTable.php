9e13168b8409bf1d20270aebad74b8bd242b0d5b
Delll <anubhavjain07@outlook.com>
2021-02-05 17:36:31 +0530

    add space

--- plugins/Report/src/Model/Table/InstitutionsTable.php
+++ plugins/Report/src/Model/Table/InstitutionsTable.php
@@ -883,11 +883,11 @@ class InstitutionsTable extends AppTable
                 } else {
                     if (in_array($feature, ['Report.BodyMasses', 'Report.InstitutionSubjects', 'Report.InstitutionClasses','Report.StudentAbsences','Report.InstitutionSubjectsClasses', 'Report.SpecialNeedsFacilities', 'Report.Income', 'Report.Expenditure', 'Report.WashReports'])) {
                         $institutionOptions = ['' => '-- ' . __('Select') . ' --', '0' => __('All Institutions')] + $institutionList;
-                    }else if (in_array($feature, ['Report.StudentAttendanceSummary'])) {//POCOR-5906 starts
+                    } else if (in_array($feature, ['Report.StudentAttendanceSummary'])) {//POCOR-5906 starts
                             $institutionOptions = ['' => '-- ' . __('Select') . ' --'] + $institutionList;//POCOR-5906 ends
                     } else {
                         //add All Institution task POCOR 5698
-                        $institutionOptions = ['' => '-- ' . __('Select') . ' --', '0' => __('All Institutions')] + $institutionList;
+                                $institutionOptions = ['' => '-- ' . __('Select') . ' --', '0' => __('All Institutions')] + $institutionList;
                     }
                     
                     $attr['type'] = 'chosenSelect';
