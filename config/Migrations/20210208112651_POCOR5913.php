d3d98e91bb35828eb2f4bf97afec7dd556ec355d
Delll <anubhavjain07@outlook.com>
2021-02-08 18:34:38 +0530

    add migration for POCOR-5913

---
+++ config/Migrations/20210208112651_POCOR5913.php
@@ -0,0 +1,29 @@
+<?php
+use Migrations\AbstractMigration;
+
+class POCOR5913 extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function up()
+    {
+        // Backup table
+        $this->execute('CREATE TABLE `zz_5913_import_mapping` LIKE `import_mapping`');
+        $this->execute('INSERT INTO `zz_5913_import_mapping` SELECT * FROM `import_mapping`');
+
+        // End
+        $this->execute("UPDATE `import_mapping` SET `description` = 'Guardian National ID'  WHERE `model` = 'Student.StudentGuardians' AND `column_name` = 'guardian_id'");
+    }
+
+    // rollback
+    public function down()
+    {
+        $this->execute('DROP TABLE IF EXISTS `import_mapping`');
+        $this->execute('RENAME TABLE `zz_5913_import_mapping` TO `import_mapping`');
+    }
+}
