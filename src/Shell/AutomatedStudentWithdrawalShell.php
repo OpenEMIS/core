5a524e2d6d891b517b8d65ddb23894b5af37b24c
Rahul Singh <rahul.singh@mail.valuecoders.com>
2021-02-18 16:15:58 +0530

    withdrawal data

--- src/Shell/AutomatedStudentWithdrawalShell.php
+++ src/Shell/AutomatedStudentWithdrawalShell.php
@@ -8,37 +8,30 @@ use Cake\Datasource\ConnectionManager;
 class AutomatedStudentWithdrawalShell extends Shell {
 	public function initialize() {
 		parent::initialize();
+		$this->loadModel('Configurations.ConfigItems');
+		$this->loadModel('Institution.InstitutionStudentAbsenceDays');
 	}
 
  	public function main() {
- 		die('asdf');
- 		/*$this->out("started");
  		
-		$identityType = $this->args[0];
+		$ConfigItems = TableRegistry::get('Configuration.ConfigItems');
+		$daysAbsent= $ConfigItems->value("automated_student_days_absent");
+		$AcademicPeriod = TableRegistry::get('AcademicPeriod.AcademicPeriods');
 
-		$this->out('default identity type -> ' . $identityType);
+		$currentYearId = $AcademicPeriod->getCurrent();
 
-		$conn = ConnectionManager::get('default');
+		$InstitutionStudentAbsenceDays = TableRegistry::get('Institution.InstitutionStudentAbsenceDays');
+		$data = $InstitutionStudentAbsenceDays
+				->find('all')
+				->where([
+                        $this->InstitutionStudentAbsenceDays->aliasField('absent_days') => $daysAbsent
+                    ])->toArray();
 
-    	$conn->execute("UPDATE `security_users` SET `identity_number` = NULL"); //set all back to NULL
+		print_r($currentYearId);
+ 		die();
 
-    	//update based on the default indentity type and get the latest record to be put on identity_number field.
-    	$query = "UPDATE `security_users` S 
-					INNER JOIN (
-					    SELECT `security_user_id`, `number`
-					    FROM `user_identities` U1
-					    WHERE `created` = (
-			        		SELECT MAX(U2.`created`)
-			        		FROM `user_identities` U2
-			        		WHERE U1.`security_user_id` = U2.`security_user_id`
-			        		AND U2.`identity_type_id` = $identityType
-			        		GROUP BY U2.`security_user_id`)
-						AND `number` <> '') U
-					ON S.`id` = U.`security_user_id`
-					SET S.`identity_number` = U.`number`;";
-		//pr($query);die;
-    	$conn->execute($query);
 
-		$this->out("ended");*/
+
+
 	}
 }
