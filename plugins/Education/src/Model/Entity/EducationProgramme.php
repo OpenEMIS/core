215cf6b2f4e35f533d66dfd7a63e0a4cb05fa0a9
Delll <anubhavjain07@outlook.com>
2021-02-19 16:05:29 +0530

    code enhance

--- plugins/Education/src/Model/Entity/EducationProgramme.php
+++ plugins/Education/src/Model/Entity/EducationProgramme.php
@@ -10,13 +10,26 @@ class EducationProgramme extends Entity
 
     protected function _getCycleProgrammeName() {
     	$name = $this->name;
-    	if ($this->has('education_cycle') && $this->education_cycle->has('name')) {
+        if ($this->has('education_cycle') && $this->education_cycle->has('name')) {
     		$name = __($this->education_cycle->name) . ' - ' . __($name);
     	} else {
     		$table = TableRegistry::get('Education.EducationCycles');
     		$cycleId = $this->education_cycle_id;
-    		$name = __($table->get($cycleId)->name) . ' - ' . __($name);
-    	}
+
+            $checkCycleData =  $table->find()
+                            ->where([$table->aliasField('id') => $cycleId])
+                            ->first();
+
+            if(!empty($checkCycleData)){
+                if(isset($table->get($cycleId)->name) && !empty($table->get($cycleId)->name)){
+                  $name = __($table->get($cycleId)->name) . ' - ' . __($name);
+                }else{
+                  $name = ' - ' . __($name);  
+                }
+            }else{
+                $name = ' - ' . __($name);  
+            }
+        }
 
     	return $name;
 	}
