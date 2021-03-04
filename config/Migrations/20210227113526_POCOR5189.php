3ae5790fafbdf8cb016e7f5784e2ac8d3369d74c
bangullia <bangullia@kordit.com>
2021-03-04 18:25:35 +0800

    Revert "Merge remote-tracking branch 'origin/POCOR-5189' into 3.62.2-dev"
    This reverts commit 6adfa377c03d31509574d39f60bae74c4d49d8d7, reversing
    changes made to b6380b4f5192d5c145114385126d94c9f49995e4.

--- config/Migrations/20210227113526_POCOR5189.php
+++ config/Migrations/20210227113526_POCOR5189.php
@@ -57,7 +57,7 @@ class POCOR5189 extends AbstractMigration
                 'null' => false
             ])
             ->addColumn('created', 'datetime', [
-                'default' => null,
+                'default' => CURRENT_TIMESTAMP,
                 'null' => false
             ])
             ->addIndex('academic_period_id')
@@ -95,7 +95,7 @@ class POCOR5189 extends AbstractMigration
                 'null' => false
             ])
             ->addColumn('created', 'datetime', [
-                'default' => null,
+                'default' => CURRENT_TIMESTAMP,
                 'null' => false
             ])
             ->addIndex('security_user_id')
@@ -142,7 +142,7 @@ class POCOR5189 extends AbstractMigration
                 'null' => false
             ])
             ->addColumn('created', 'datetime', [
-                'default' => null,
+                'default' => CURRENT_TIMESTAMP,
                 'null' => false
             ])
             ->addIndex('security_user_id')
