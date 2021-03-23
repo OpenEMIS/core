205cf679875597f0fbe858a4b3698b7b86c62f3d
Ehteram Ahmad <ehteram333@gmail.com>
2021-03-23 09:37:42 +0530

    POCOR:5699 fixes

--- config/installer_mode_config.php
+++ config/installer_mode_config.php
@@ -1,14 +1,14 @@
 <?php
     
 	$mode = array(
-			'core' => false,
+			'core' => true,
 			'census' => false,
 			'school' => false,
-			'vaccinations' => true
+			'vaccinations' => false
 	);
 
 	$count_mode_value = array_sum($mode);
-	$database_dump_file = 'prd_vac_dmo_2021-03-19';
+	$database_dump_file = 'tst_cor_dmo_2021-02-17';
 	$application_name = $application_colour = $application_login_image = $application_sql_name = $application_db_user_name = $application_mode = $application_theme = $application_favicon = '';
 	
 	if($mode['core'] == true){
