2935286ff0b5e8fb9ee4d473e72b8fa4278a2d22
Rahul Singh <rahul.singh@mail.valuecoders.com>
2021-04-07 11:25:39 +0530

    task done

--- config/Migrations/20210406122718_POCOR5947.php
+++ config/Migrations/20210406122718_POCOR5947.php
@@ -12,6 +12,12 @@ class POCOR5947 extends AbstractMigration
      */
     public function up()
     {
+        $this->execute('CREATE TABLE `z_5947_assessment_item_results` LIKE `assessment_item_results`');
+        $this->execute('INSERT INTO `z_5947_assessment_item_results` SELECT * FROM `assessment_item_results`');
+
+        $this->execute('ALTER TABLE `assessment_item_results` ADD `institution_classes_id` INT(11) NOT NULL AFTER `institution_id`, ADD `student_status_id` INT(11) NULL DEFAULT NULL AFTER `institution_classes_id`');
+
+        $this->execute("ALTER TABLE `assessment_item_results` DROP PRIMARY KEY, ADD primary key (`student_id`,`assessment_id`,`education_subject_id`,`education_grade_id`,`academic_period_id`,`assessment_period_id`,`institution_classes_id`), ADD UNIQUE INDEX (`student_id`,`assessment_id`,`education_subject_id`,`education_grade_id`,`academic_period_id`,`assessment_period_id`,`institution_classes_id`)");
 
         $institutionClassStudents = $this->fetchAll('SELECT * FROM `institution_class_students`');
 
@@ -24,5 +30,12 @@ class POCOR5947 extends AbstractMigration
                 }
         }
 
+    }
+
+     // rollback
+    public function down()
+    {
+        $this->execute('DROP TABLE IF EXISTS `assessment_item_results`');
+        $this->execute('RENAME TABLE `z_5947_assessment_item_results` TO `assessment_item_results`');
     }
 }
