93c686d0e938073f3d2e4ef5169ac5df0c3725bc
Poonam Kharka <poonamkharka78@gmail.com>
2021-04-14 09:02:59 +0530

    resolved merge conflict of file IdentitiesTable.php

--- plugins/User/src/Model/Table/IdentitiesTable.php
+++ plugins/User/src/Model/Table/IdentitiesTable.php
@@ -65,14 +65,24 @@ class IdentitiesTable extends ControllerActionTable
         if (isset($queryString['security_user_id'])) {
             $userId = $queryString['security_user_id'];
         }
-        $NationalityOptions = $UserNationalityTable
+        if ($this->action == 'add') {
+        	$NationalityOptions = $UserNationalityTable
             ->find('list',
                 ['keyField' => '_matchingData.NationalitiesLookUp.id',
                 'valueField' => '_matchingData.NationalitiesLookUp.name'
             ])
             ->matching('NationalitiesLookUp')
-            ->where([$UserNationalityTable->aliasField('security_user_id') => $userId])
             ->toArray();
+        } else {
+        	$NationalityOptions = $this
+            ->find('list',
+                ['keyField' => '_matchingData.Nationalities.id',
+                'valueField' => '_matchingData.Nationalities.name'
+            ])
+            ->matching('Nationalities')
+            ->where([$this->aliasField('security_user_id') => $userId])
+            ->toArray();
+        }
 		$this->fields['identity_type_id']['type'] = 'select';
 		$this->fields['nationality_id']['type'] = 'select';
         $this->fields['nationality_id']['options'] = $NationalityOptions;
@@ -123,7 +133,6 @@ class IdentitiesTable extends ControllerActionTable
 		    //POCOR-5987 starts
 		    ->notEmpty('nationality_id');
 		    //POCOR-5987 ends
-		;
 	}
 
 	public function validationAddByAssociation(Validator $validator)
@@ -139,7 +148,7 @@ class IdentitiesTable extends ControllerActionTable
 	}
 
 	public function afterSave(Event $event, Entity $entity, ArrayObject $extra)
-	{ 
+	{
             if(!empty($entity->nationality_id)){
                 $nationalitiesLookUp = TableRegistry::get('Nationalities')->get($entity->nationality_id);
                 if($nationalitiesLookUp->identity_type_id == $entity->identity_type_id){
