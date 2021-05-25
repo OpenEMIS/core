7c5b4669001259f4d5f39e9f337e8f842e26993a
vishnu sharma <vishnu.sharma@mail.vinove.com>
2021-05-25 11:06:07 +0530

    Make migration for updates fields in english

--- config/Migrations/20210520121056_POCOR6054.php
+++ config/Migrations/20210520121056_POCOR6054.php
@@ -15,13 +15,145 @@ class POCOR6054 extends AbstractMigration
     public function up()
     {
         // Backup locale_contents table
-        $this->execute('CREATE TABLE `z_6056_locale_contents` LIKE `locale_contents`');
-        $this->execute('INSERT INTO `z_6056_locale_contents` SELECT * FROM `locale_contents`');
+        $this->execute('CREATE TABLE `z_6054_locale_contents` LIKE `locale_contents`');
+        $this->execute('INSERT INTO `z_6054_locale_contents` SELECT * FROM `locale_contents`');
 
         // End
 
         $localeContent = [
             [
+                'en' => 'Status Checked',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
+                'en' => 'Database Name',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
+                'en' => 'Host Port',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
+                'en' => 'Host',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
+                'en' => 'Database Type',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
+                'en' => 'Rule Events',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
+                'en' => 'Implementer',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
+                'en' => 'National Content',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
+                'en' => 'Targeting',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ], [
+                'en' => 'Scholarship Name',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
+                'en' => 'Financial Assistance Type',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
+                'en' => 'Recipient',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
+                'en' => 'Bond',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
+                'en' => 'Annual Award Amount',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
+                'en' => 'Application Close Date',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
+                'en' => 'Application Open Date',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
+                'en' => 'Scholarship Name',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
+                'en' => 'Appraisal Types',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ], [
+                'en' => 'Final Score',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
+                'en' => 'Generate End Date',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
+                'en' => 'Generate Start Date',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
+                'en' => 'Translation',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
+                'en' => 'Add Meeting',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
+                'en' => 'Date of Meeting',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
+                'en' => 'Student Meals',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
+                'en' => 'Distribution',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
+                'en' => 'Add Funding Source',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
+                'en' => 'Add Need',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
+                'en' => 'Select Report Class',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
+                'en' => 'No. of Staff on Late',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
+                'en' => 'Duty Types',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
+                'en' => 'Current Class',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
+                'en' => 'Parents and Guardian Informations section',
+                'created_user_id' => 1,
+                'created' => date('Y-m-d H:i:s')
+            ],[
                 'en' => 'Next Institution Class',
                 'created_user_id' => 1,
                 'created' => date('Y-m-d H:i:s')
@@ -126,7 +258,7 @@ class POCOR6054 extends AbstractMigration
     public function down()
     {
         $this->execute('DROP TABLE IF EXISTS `locale_contents`');
-        $this->execute('RENAME TABLE `z_6056_locale_contents` TO `locale_contents`');
+        $this->execute('RENAME TABLE `z_6054_locale_contents` TO `locale_contents`');
 
     }
 }
