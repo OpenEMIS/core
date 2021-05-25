2c8c60ccebcf4d4353f0a53be79ca910d7764061
Rahul Singh <rahul.singh@mail.valuecoders.com>
2021-05-25 08:15:42 +0530

    import student attendance

---
+++ config/Migrations/20210525023714_POCOR5778.php
@@ -0,0 +1,27 @@
+<?php
+use Migrations\AbstractMigration;
+
+class POCOR5778 extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function up()
+    {
+        $this->execute('CREATE TABLE `zz_5778_import_mapping` LIKE `import_mapping`');
+        $this->execute('INSERT INTO `zz_5778_import_mapping` SELECT * FROM `import_mapping`');
+
+          $this->execute("UPDATE `import_mapping` SET `lookup_model` = 'InstitutionSubjects' WHERE `model`='Institution.StudentAbsencesPeriodDetails' AND `column_name` = 'subject_id' ");
+    }
+
+     // rollback
+    public function down()
+    {
+        $this->execute('DROP TABLE IF EXISTS `import_mapping`');
+        $this->execute('RENAME TABLE `zz_5778_import_mapping` TO `import_mapping`');
+    }
+}
