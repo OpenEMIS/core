e94a0f58818f5d18a8629b5122e008bb318ec98f
Asif Kureshi <asif.kureshi@theonetechnologies.co.in>
2021-06-01 18:25:15 +0530

    Generate Webhook url on area education update

--- plugins/Area/src/Model/Table/AreasTable.php
+++ plugins/Area/src/Model/Table/AreasTable.php
@@ -200,6 +200,23 @@ class AreasTable extends ControllerActionTable
         }
         // Webhook Education Area create -- end
 
+        //webhook Education Cycle update -- start
+        if(!$entity->isNew()){
+            $body = array();
+            $body = [
+                'area_id' =>$entity->id,
+                'area_name' =>$entity->name,
+                'area_code' =>$entity->code,
+                'area_parent_id' =>$entity->parent_id,
+                'area_level_id' =>$entity->area_level_id
+            ];
+            $Webhooks = TableRegistry::get('Webhook.Webhooks');
+            if ($this->Auth->user()) {
+                $Webhooks->triggerShell('area_education_update', ['username' => $username], $body);
+            }
+        }
+        //webhook Education Cycle update -- end
+
     }
 
     public function onGetConvertOptions(Event $event, Entity $entity, Query $query)
