ab9d5b5f08b0977a935bd38ea071ae2e301adb3e
Rahul Singh <rahul.singh@mail.valuecoders.com>
2021-06-11 07:31:52 +0530

    rename the migration file

--- config/Migrations/20210611015341_POCOR5778.php
+++ config/Migrations/20210611015341_POCOR5778.php
@@ -1,7 +1,7 @@
 <?php
 use Migrations\AbstractMigration;
 
-class BugPOCOR5778 extends AbstractMigration
+class POCOR5778 extends AbstractMigration
 {
     /**
      * Change Method.
@@ -12,9 +12,13 @@ class BugPOCOR5778 extends AbstractMigration
      */
     public function up()
     {
-          $this->execute("UPDATE `import_mapping` SET `foreign_key` = 2 WHERE `model`='Institution.StudentAbsencesPeriodDetails' AND `column_name` = 'subject_id' ");
-    }
+        $this->execute('DROP TABLE IF EXISTS `zz_5778_import_mapping`');
+        $this->execute('CREATE TABLE `zz_5778_import_mapping` LIKE `import_mapping`');
+        $this->execute('INSERT INTO `zz_5778_import_mapping` SELECT * FROM `import_mapping`');
 
+          $this->execute("UPDATE `import_mapping` SET `foreign_key` = 2 WHERE `model`='Institution.StudentAbsencesPeriodDetails' AND `column_name` = 'subject_id' ");
+          $this->execute("UPDATE `import_mapping` SET `lookup_model` = 'InstitutionSubjects' WHERE `model`='Institution.StudentAbsencesPeriodDetails' AND `column_name` = 'subject_id' ");
+    }
      // rollback
     public function down()
     {
