7b347e38a3772f33af3fef5968ba9b9e94992c05
Poonam Kharka <poonamkharka78@gmail.com>
2021-07-02 11:44:10 +0530

    modified migration script

--- config/Migrations/20210630041719_POCOR6208.php
+++ config/Migrations/20210630041719_POCOR6208.php
@@ -17,8 +17,21 @@ class POCOR6208 extends AbstractMigration
         $this->execute('INSERT INTO `z_6208_security_functions` SELECT * FROM `security_functions`');
 
         //update delete function for all reports module
-        $this->execute("UPDATE security_roles SET code = 'Superrole' WHERE name = 'Superrole'");
-        $this->execute("UPDATE security_roles SET code = 'Guardian' WHERE name = 'Guardian'");
+        $this->execute("UPDATE security_functions SET _delete = 'Institution.delete' WHERE module = 'Reports' AND name = 'Institution'");
+        $this->execute("UPDATE security_functions SET _delete = 'Students.delete' WHERE module = 'Reports' AND name = 'Students'");
+        $this->execute("UPDATE security_functions SET _delete = 'Staff.delete' WHERE module = 'Reports' AND name = 'Staff'");
+        $this->execute("UPDATE security_functions SET _delete = 'Surveys.delete' WHERE module = 'Reports' AND name = 'Surveys'");
+        $this->execute("UPDATE security_functions SET _delete = 'InstitutionRubrics.delete' WHERE module = 'Reports' AND name = 'Rubrics'");
+        $this->execute("UPDATE security_functions SET _delete = 'DataQuality.delete' WHERE module = 'Reports' AND name = 'Data Quality'");
+        $this->execute("UPDATE security_functions SET _delete = 'Audits.delete' WHERE module = 'Reports' AND name = 'Audits'");
+        $this->execute("UPDATE security_functions SET _delete = 'Map.delete' WHERE module = 'Reports' AND name = 'Map'");
+        $this->execute("UPDATE security_functions SET _delete = 'Examinations.delete' WHERE module = 'Reports' AND name = 'Examinations'");
+        $this->execute("UPDATE security_functions SET _delete = 'Textbooks.delete' WHERE module = 'Reports' AND name = 'Textbooks'");
+        $this->execute("UPDATE security_functions SET _delete = 'Trainings.delete' WHERE module = 'Reports' AND name = 'Trainings'");
+        $this->execute("UPDATE security_functions SET _delete = 'CustomReports.delete' WHERE module = 'Reports' AND name = 'Custom'");
+        $this->execute("UPDATE security_functions SET _delete = 'Workflows.delete' WHERE module = 'Reports' AND name = 'Workflows'");
+        $this->execute("UPDATE security_functions SET _delete = 'Scholarships.delete' WHERE module = 'Reports' AND name = 'Scholarships'");
+        $this->execute("UPDATE security_functions SET _delete = 'Directory.delete' WHERE module = 'Reports' AND name = 'Directory'");
     }
 
     //rollback
