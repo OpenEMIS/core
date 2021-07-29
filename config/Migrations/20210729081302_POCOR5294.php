5c42f76b59911bd321d92aab277f1a78291f2906
Poonam Kharka <poonamkharka78@gmail.com>
2021-07-29 14:12:09 +0530

    re-written migartion

--- config/Migrations/20210729081302_POCOR5294.php
+++ config/Migrations/20210729081302_POCOR5294.php
@@ -1,17 +1,16 @@
 <?php
-
-use Phinx\Migration\AbstractMigration;
+use Migrations\AbstractMigration;
 
 class POCOR5294 extends AbstractMigration
 {
-   
     public function up()
     {
       $this->execute('CREATE TABLE `zz_5294_labels` LIKE `labels`');
       $this->execute('INSERT INTO `zz_5294_labels` SELECT * FROM `labels`');
-      $this->execute("INSERT INTO `labels` (`id`, `module`, `field`, `module_name`, `field_name`, `created_user_id`, `created`) VALUES (uuid(), 'Counselling', 'Counselling', 'Institution > Students > Counselling', 'Requester', 1, NOW())");
+      $this->execute("INSERT INTO `labels` (`id`, `module`, `field`, `module_name`, `field_name`, `created_user_id`, `created`) VALUES (uuid(), 'Counselling', 'Counselling', 'Institution-> Students-> Counselling', 'Requester', 1, NOW())");
     }
 
+    //rollback
     public function down()
     {
       $this->execute('DROP TABLE IF EXISTS `labels`');
