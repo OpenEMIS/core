58a5d329a124e2740a05c52bfd2ee82468130a76
Poonam Kharka <poonamkharka78@gmail.com>
2021-08-24 12:37:31 +0530

    new navigation added

--- plugins/Guardian/config/routes.php
+++ plugins/Guardian/config/routes.php
@@ -1,9 +1,11 @@
 <?php
 use Cake\Routing\Router;
 
-Router::scope('/Guardians', ['plugin' => 'Guardian'], function ($routes) {
+Router::scope('/Guardians', ['plugin' => 'Profile'], function ($routes) {
+    // Router::connect('/Profiles', ['plugin' => 'Profile', 'controller' => 'Profiles']);
+    // Router::connect('/Profiles/:action/*', ['plugin' => 'Profile', 'controller' => 'Profiles']);
 
-	$routes->scope('/:controller', [], function ($route) {
+    $routes->scope('/:controller', [], function ($route) {
         $route->connect('/:action',
             [],
             ['action' => '[a-zA-Z]+']
