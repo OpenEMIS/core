5adba402e00013d4e6c8f324c550ef7cc055c5b9
anand malvi <anand.malvi@mail.valuecoders.com>
2021-08-24 12:25:51 +0530

    POCOR-6256: Listing module added | status:WIP

--- plugins/ReportCard/src/Model/Table/ReportCardProcessesTable.php
+++ plugins/ReportCard/src/Model/Table/ReportCardProcessesTable.php
@@ -2,6 +2,10 @@
 namespace ReportCard\Model\Table;
 
 use App\Model\Table\ControllerActionTable;
+use ArrayObject;
+use Cake\Event\Event;
+use Cake\ORM\Entity;
+use Cake\ORM\Query;
 
 class ReportCardProcessesTable extends ControllerActionTable
 {
@@ -12,6 +16,8 @@ class ReportCardProcessesTable extends ControllerActionTable
 
     public function initialize(array $config)
     {
+        $this->table('report_card_processes');
+
         parent::initialize($config);
 
         $this->belongsTo('ReportCards', ['className' => 'ReportCard.ReportCards']);
@@ -20,5 +26,80 @@ class ReportCardProcessesTable extends ControllerActionTable
         $this->belongsTo('Institutions', ['className' => 'Institution.Institutions']);
         $this->belongsTo('EducationGrades', ['className' => 'Education.EducationGrades']);
         $this->belongsTo('AcademicPeriods', ['className' => 'AcademicPeriod.AcademicPeriods']);
+        $this->toggle('add', false);
+        $this->toggle('edit', false);
+    }
+
+    public function indexBeforeQuery(Event $event, Query $query, ArrayObject $extra)
+    {
+        /*
+        $query
+        ->leftJoin(
+            [$this->InstitutionClasses->alias() => $this->InstitutionClasses->table()],
+            [$this->InstitutionClasses->aliasField('id = ') . $this->aliasField('institution_class_id')]
+        )
+        ->leftJoin(
+            [$this->Institutions->alias() => $this->Institutions->table()],
+            [$this->Institutions->aliasField('id = ') . $this->aliasField('institution_id')]
+        )
+        ->leftJoin(
+            [$this->Students->alias() => $this->Students->table()],
+            [$this->Students->aliasField('id = ') . $this->aliasField('student_id')]
+        )
+        ->select([
+            'status' => $this->aliasField('status'),
+            'institution_class_id' => $this->InstitutionClasses->aliasField('name'),
+            'institution_id' => $this->Institutions->aliasField('name'),
+            'student_id' => $this->Students->aliasField('openemis_no'),
+        ]);
+        */
+        $query->order([$this->aliasField('status DESC')]);
+        return $query;
+    }
+
+    public function onGetStatus(Event $event, Entity $entity)
+    {
+        $status = [
+            1  => "New Process",
+            2  => 'Running',
+            3  => 'Completed',
+            -1 => 'Error'
+        ];
+        if (isset($status[$entity->status])) {
+            return $status[$entity->status];
+        }
+        return 'Error';
+    }
+
+    public function indexBeforeAction(Event $event, ArrayObject $extra)
+    {
+        $this->fields['institution_id']['visible']       = true;
+        $this->fields['institution_class_id']['visible'] = true;
+        $this->fields['student_id']['visible']           = true;
+        $this->fields['status']['visible']               = true;
+
+        $this->fields['report_card_id']['visible']       = false;
+        $this->fields['education_grade_id']['visible']   = false;
+        $this->fields['academic_period_id']['visible']   = false;
+        $this->fields['created']['visible']              = false;
+
+        $this->setFieldOrder([
+            'institution_id',
+            'institution_class_id',
+            'student_id',
+            'status'
+        ]);
+    }
+
+    public function beforeAction(Event $event, ArrayObject $extra)
+    {
+        $this->setupNewTabElements();
+    }
+
+    private function setupNewTabElements()
+    {
+        $tabElements = $this->controller->getReportTabElements();
+        $this->controller->set('tabElements', $tabElements);
+        $this->controller->set('selectedAction', 'Process');
     }
 }
