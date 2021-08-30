ef415c8341704fea0644fdd5ef3e301b5ccd36bc
Delll <anubhavjain07@outlook.com>
2021-08-30 16:03:32 +0530

    code enhance for show/hide add button

--- src/Controller/Component/AccessControlComponent.php
+++ src/Controller/Component/AccessControlComponent.php
@@ -277,10 +277,12 @@ class AccessControlComponent extends Component
 
         // exclude profile controllers
         /*commenting Profiles, ProfileInsurances and ProfileBodyMasses as per task POCOR-5312 permission requirement*/
-        //For POCOR-6202 uncomment the profile module  
-        $excludedController = ['ProfileApplicationAttachments', 'ProfileApplicationInstitutionChoices' /*'ProfileBodyMasses'*/, 'ProfileComments', /*'ProfileInsurances', 'Profiles', */ 'Profiles', 'ScholarshipsDirectory'];
-        if (isset($url['controller']) && in_array($url['controller'], $excludedController)) {
-            return true;
+        if($this->request->params['action'] == 'TrainingNeeds'){
+            //For POCOR-6202 uncomment the profile module  
+            $excludedController = ['ProfileApplicationAttachments', 'ProfileApplicationInstitutionChoices' /*'ProfileBodyMasses'*/, 'ProfileComments', /*'ProfileInsurances', 'Profiles', */ 'ScholarshipsDirectory'];
+        }else{
+            //For POCOR-6202 uncomment the profile module  
+            $excludedController = ['ProfileApplicationAttachments', 'ProfileApplicationInstitutionChoices' /*'ProfileBodyMasses'*/, 'ProfileComments', /*'ProfileInsurances', 'Profiles', */ 'Profiles', 'ScholarshipsDirectory'];
         }
 
         if (array_key_exists('controller', $url)) {
