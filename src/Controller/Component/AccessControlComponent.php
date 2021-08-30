06f44e2463b8e54b2c4e4759cb80b15b2184ba92
Delll <anubhavjain07@outlook.com>
2021-08-30 16:05:22 +0530

    remove commented line

--- src/Controller/Component/AccessControlComponent.php
+++ src/Controller/Component/AccessControlComponent.php
@@ -277,8 +277,8 @@ class AccessControlComponent extends Component
 
         // exclude profile controllers
         /*commenting Profiles, ProfileInsurances and ProfileBodyMasses as per task POCOR-5312 permission requirement*/
-        if($this->request->params['action'] == 'TrainingNeeds'){
-           $excludedController = ['ProfileApplicationAttachments', 'ProfileApplicationInstitutionChoices' /*'ProfileBodyMasses'*/, 'ProfileComments', /*'ProfileInsurances', 'Profiles', */ 'ScholarshipsDirectory'];
+        if($this->request->params['action'] == 'TrainingNeeds'){//POCOR-6292 starts
+           $excludedController = ['ProfileApplicationAttachments', 'ProfileApplicationInstitutionChoices' /*'ProfileBodyMasses'*/, 'ProfileComments', /*'ProfileInsurances', 'Profiles', */ 'ScholarshipsDirectory'];//POCOR-6292 ends
         }else{
             //For POCOR-6202 uncomment the profile module  
             $excludedController = ['ProfileApplicationAttachments', 'ProfileApplicationInstitutionChoices' /*'ProfileBodyMasses'*/, 'ProfileComments', /*'ProfileInsurances', 'Profiles', */ 'Profiles', 'ScholarshipsDirectory'];
