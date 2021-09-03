5c9a3e485af0cc8dd05882fdf9ce563c507e5af6
anand malvi <anand.malvi@mail.valuecoders.com>
2021-09-03 14:07:10 +0530

    Resolved the multiple migration file in single ticket

--- config/Migrations/20210824115926_POCOR6256.php
+++ config/Migrations/20210824115926_POCOR6256.php
@@ -28,14 +28,6 @@ class POCOR6256 extends AbstractMigration
             'created' => date('Y-m-d H:i:s')
         ]);
         /** END: security_functions table changes */
-
-
-        /** START: report_card_processes table changes */
-        $this->execute('DROP TABLE IF EXISTS `zz_6256_report_card_processes`');
-        $this->execute('CREATE TABLE `zz_6256_report_card_processes` LIKE `report_card_processes`');
-        $this->execute('INSERT INTO `zz_6256_report_card_processes` SELECT * FROM `report_card_processes`');
-        $this->execute('ALTER TABLE `report_card_processes` DROP PRIMARY KEY, ADD PRIMARY KEY (`report_card_id`) USING BTREE');
-        /** END: report_card_processes table changes */
     }
 
     //rollback
@@ -45,11 +37,5 @@ class POCOR6256 extends AbstractMigration
         $this->execute('DROP TABLE IF EXISTS `security_functions`');
         $this->execute('RENAME TABLE `zz_6256_security_functions` TO `security_functions`');
         /** END: security_functions table changes */
-
-
-        /** START: report_card_processes table changes */
-        $this->execute('DROP TABLE IF EXISTS `report_card_processes`');
-        $this->execute('RENAME TABLE `zz_6256_report_card_processes` TO `report_card_processes`');
-        /** END: report_card_processes table changes */
     }
 }
