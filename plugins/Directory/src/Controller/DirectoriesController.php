e6aa3e39b50cef679307077ac5b5066d53d2c622
Rahul Singh <rahul.singh@mail.valuecoders.com>
2021-09-15 11:07:13 +0530

    absences issue resolved

--- plugins/Directory/src/Controller/DirectoriesController.php
+++ plugins/Directory/src/Controller/DirectoriesController.php
@@ -21,7 +21,8 @@ class DirectoriesController extends AppController
             'Accounts'              => ['className' => 'Directory.Accounts', 'actions' => ['view', 'edit']],
 
             // Student
-            'StudentAbsences'       => ['className' => 'Student.Absences', 'actions' => ['index', 'view']],
+            'StudentAbsences'       => ['className' => 'Student.DirectoryAbsences', 'actions' => ['index', 'view']],
+            // 'StudentAbsences'       => ['className' => 'Student.Absences', 'actions' => ['index', 'view']],
             'StudentBehaviours'     => ['className' => 'Student.StudentBehaviours', 'actions' => ['index', 'view']],
             'StudentExtracurriculars' => ['className' => 'Student.Extracurriculars'],
 
