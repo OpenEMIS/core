52ca58263d1e6393e91006cb6a51bdfcfb3fceed
Rahul Singh <rahul.singh@mail.valuecoders.com>
2021-09-15 16:38:09 +0530

    absence tab issue resolved

--- plugins/Directory/src/Model/Table/AbsencesTable.php
+++ plugins/Directory/src/Model/Table/AbsencesTable.php
@@ -168,9 +168,12 @@ class AbsencesTable extends AppTable
     {
         $userData = $this->Session->read();
 
-        if ($userData['Auth']['User']['is_guardian'] == 1) { 
+       if ($userData['Auth']['User']['is_guardian'] == 1) { 
             $sId = $userData['Student']['ExaminationResults']['student_id']; 
-            $studentId = $this->ControllerAction->paramsDecode($sId)['id'];
+            if ($sId) {
+                $studentId = $this->ControllerAction->paramsDecode($sId)['id'];
+            }
+            $studentId = $userData['Student']['Students']['id'];
         } else {
             $studentId = $userData['Auth']['User']['id'];
         }
