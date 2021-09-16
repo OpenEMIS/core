c8594e4298b41c691df4b982d9abe242013824ab
anand malvi <anand.malvi@mail.valuecoders.com>
2021-09-16 10:33:04 +0530

    Migration changes as suggested by Umairah | status:completed

--- config/Migrations/20210827113459_POCOR5034.php
+++ config/Migrations/20210827113459_POCOR5034.php
@@ -9,14 +9,15 @@ class POCOR5034 extends AbstractMigration
     {
         $this->execute('CREATE TABLE `z_5034_import_mapping` LIKE `import_mapping`');
         $this->execute('INSERT INTO `z_5034_import_mapping` SELECT * FROM `import_mapping`');
+        $this->execute('CREATE TABLE `zz_5034_security_functions` LIKE `security_functions`');
+        $this->execute('INSERT INTO `zz_5034_security_functions` SELECT * FROM `security_functions`');
+
+        ## Update permission
+        $this->execute("UPDATE `security_functions` SET `_execute` = 'ImportOutcomeTemplates.add|ImportOutcomeTemplates.template|ImportOutcomeTemplates.results|ImportOutcomeTemplates.downloadFailed|ImportOutcomeTemplates.downloadPassed' WHERE `name` = 'Outcome Setup' AND `controller` = 'Outcomes' AND `module` = 'Administration' AND `category` = 'Learning Outcomes'");
 
         $this->execute("INSERT into `import_mapping` (`model`, `column_name`, `description`, `order`, `is_optional`, `foreign_key`, `lookup_plugin`, `lookup_model`, `lookup_column`) values('Outcome.OutcomeTemplates','code','','1','0','0',NULL,NULL,NULL)");
 		$this->execute("INSERT into `import_mapping` (`model`, `column_name`, `description`, `order`, `is_optional`, `foreign_key`, `lookup_plugin`, `lookup_model`, `lookup_column`) values('Outcome.OutcomeTemplates','name','','2','0','0',NULL,NULL,NULL)");
 		$this->execute("INSERT into `import_mapping` (`model`, `column_name`, `description`, `order`, `is_optional`, `foreign_key`, `lookup_plugin`, `lookup_model`, `lookup_column`) values('Outcome.OutcomeTemplates','description','','3','0','0',NULL,NULL,NULL)");
-        ## Update permission
-        $this->execute('CREATE TABLE `zz_5034_security_functions` LIKE `security_functions`');
-        $this->execute('INSERT INTO `zz_5034_security_functions` SELECT * FROM `security_functions`');
-        $this->execute("UPDATE `security_functions` SET `_execute` = 'ImportOutcomeTemplates.add|ImportOutcomeTemplates.template|ImportOutcomeTemplates.results|ImportOutcomeTemplates.downloadFailed|ImportOutcomeTemplates.downloadPassed' WHERE `name` = 'Outcome Setup' AND `controller` = 'Outcomes' AND `module` = 'Administration' AND `category` = 'Learning Outcomes'");
     }
 
     // rollback
@@ -25,6 +26,7 @@ class POCOR5034 extends AbstractMigration
         $this->execute('DROP TABLE IF EXISTS `import_mapping`');
         $this->execute('RENAME TABLE `z_5034_import_mapping` TO `import_mapping`');
 
+        $this->execute('DROP TABLE IF EXISTS `security_functions`');
         $this->execute('RENAME TABLE `zz_5034_security_functions` TO `security_functions`');
     }
 }
