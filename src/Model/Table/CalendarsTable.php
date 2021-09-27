e48dc572689ac63f25dbb8068f2470c2235ede70
vikas.rathore@mail.valuecoders.com <vikas.rathore@mail.valuecoders.com>
2021-09-27 15:28:40 +0530

    Add Export button function-calendar

--- src/Model/Table/CalendarsTable.php
+++ src/Model/Table/CalendarsTable.php
@@ -6,6 +6,7 @@ use Cake\I18n\Date;
 use Cake\ORM\Entity;
 use Cake\ORM\Query;
 use Cake\Validation\Validator;
+use Cake\ORM\TableRegistry;
 use Cake\Event\Event;
 use App\Model\Table\ControllerActionTable;
 
@@ -20,9 +21,12 @@ class CalendarsTable extends ControllerActionTable
         $this->belongsTo('Institutions', ['className' => 'Institution.Institutions', 'foreignKey' => 'institution_id']);
         $this->belongsTo('CalendarTypes', ['className' => 'CalendarTypes', 'foreignKey' => 'calendar_type_id']);
 
-        $this->hasMany('CalendarEventDates', ['className' => 'CalendarEventDates', 'dependent' => true, 'cascadeCallbacks' => true]);
+        $d = $this->hasMany('CalendarEventDates', ['className' => 'CalendarEventDates', 'dependent' => true, 'cascadeCallbacks' => true]);
 
-        $this->addBehavior('Excel', ['pages' => ['index']]);
+        $this->addBehavior('Excel', [
+            'excludes' => ['comment', 'security_group_user_id'],
+            'pages' => ['index']
+        ]);
     }
 
     public function validationDefault(Validator $validator)
@@ -121,15 +125,75 @@ class CalendarsTable extends ControllerActionTable
         return $query;
     }
 
+    public function onExcelUpdateFields(Event $event, ArrayObject $settings, ArrayObject $fields)
+    {
+        $extraField[] = [
+            'key'   => 'calendar_type_id',
+            'field' => 'calendar_type_id',
+            'type'  => 'string',
+            'label' => __('Calendar Type')
+        ];
+
+        $extraField[] = [
+            'key'   => 'name',
+            'field' => 'name',
+            'type'  => 'string',
+            'label' => __('Name')
+        ];
+
+        // $extraField[] = [
+        //     'key'   => 'comment',
+        //     'field' => 'comment',
+        //     'type'  => 'string',
+        //     'label' => __('Comment')
+        // ];
+
+        $extraField[] = [
+            'key'   => 'start_date',
+            'field' => 'start_date',
+            'type'  => 'date',
+            'label' => __('Start Date')
+        ];
+
+        $extraField[] = [
+            'key'   => 'end_date',
+            'field' => 'end_date',
+            'type'  => 'date',
+            'label' => __('End Date')
+        ];
+
+        // dump($fields); die;
+
+        $fields->exchangeArray($extraField);
+    }
+
     public function onExcelBeforeQuery(Event $event, ArrayObject $settings, Query $query)
     {
         $session = $this->request->session();
         $institutionId  = $session->read('Institution.Institutions.id');
 
+        // $query = 'SELECT `Calendars`.`id` AS `Calendars__id`, `Calendars`.`name` AS `Calendars__name`, `Calendars`.`comment` AS `Calendars__comment`, `Calendars`.`academic_period_id` AS `Calendars__academic_period_id`, `Calendars`.`institution_id` AS `Calendars__institution_id`, MIN(calendar_event_dates.date) AS `start_date`, MAX(calendar_event_dates.date) AS `end_date`, `Calendars`.`calendar_type_id` AS `Calendars__calendar_type_id`, `Calendars`.`modified_user_id` AS `Calendars__modified_user_id`, `Calendars`.`modified` AS `Calendars__modified`, `Calendars`.`created_user_id` AS `Calendars__created_user_id`, `Calendars`.`created` AS `Calendars__created` FROM `calendar_events` `Calendars` INNER JOIN `calendar_event_dates` ON calendar_event_dates.calendar_event_id = Calendars.id GROUP BY Calendars.id';
+        
         $query
+        ->select([
+            'Calendars.id' , 'Calendars.name', 
+            'Calendars.comment', 'Calendars.academic_period_id', 'Calendars.institution_id',
+            'start_date' => $query->func()->min('calendar_event_dates.date'),
+			'end_date' => $query->func()->max('calendar_event_dates.date'),
+            'Calendars.calendar_type_id', 'Calendars.modified_user_id', 'Calendars.modified', 
+            'Calendars.created_user_id','Calendars.created'
+        ])
+        ->innerJoin(
+            ['calendar_event_dates' => 'calendar_event_dates'],
+            ['calendar_event_dates.calendar_event_id = ' . $this->aliasField('id')]
+        )
+        ->group($this->aliasField('id'))
         ->where([
             'institution_id =' .$institutionId
         ]);
+
+        // dump($query); die;
+        
     }
 
     public function addEditBeforeAction(Event $event, ArrayObject $extra)
