3373171c3b46289a471ae2f8990cdaaad9a86eb1
Poonam Kharka <poonam.kharka@mail.valuecoders.com>
2021-09-29 10:55:46 +0530

    modified query as per task requirement

--- plugins/Directory/src/Model/Table/DirectoriesTable.php
+++ plugins/Directory/src/Model/Table/DirectoriesTable.php
@@ -1185,7 +1185,7 @@ class DirectoriesTable extends ControllerActionTable
         $this->fields['identity_number']['type'] = 'readonly'; //cant edit identity_number field value as its value is auto updated.
     }
 
-    /*POCOR-6332 commented due to this function some error was occuring*/
+
     public function viewAfterAction(Event $event, Entity $entity, ArrayObject $extra)
     {
         $isSet = $this->setSessionAfterAction($event, $entity);
