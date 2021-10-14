3c667b9d3b7c9a8c9baaef24eb6913546a3150aa
anubhav0789 <jainanubhav67@yahoo.in>
2021-10-14 16:27:03 +0530

    code enhance

--- plugins/Institution/src/Model/Table/StudentsTable.php
+++ plugins/Institution/src/Model/Table/StudentsTable.php
@@ -267,6 +267,7 @@ class StudentsTable extends ControllerActionTable
                 'identity_number' => $UserIdentities->aliasField('number'),
                 'contact_type' => $ContactTypes->aliasField('name'),
                 'contact_number' => $UserContact->aliasField('value'),
+                'institution_id' => 'Institutions.id',//POCOR-6338
             ])
             ->leftJoin(
                 [$ClassStudents->alias() => $ClassStudents->table()],
@@ -335,37 +336,46 @@ class StudentsTable extends ControllerActionTable
 
         if ($periodId > 0) {
             $query->where([$this->aliasField('academic_period_id') => $periodId]);
-        }
+        }// POCOR-6338 starts
         $query->formatResults(function (\Cake\Collection\CollectionInterface $results) {
             return $results->map(function ($row) {
 
-                $InstitutionStudents = TableRegistry::get('InstitutionStudents');
-
-                $InstitutionStudentsCurrentData = $InstitutionStudents
-                ->find()
-                ->select([
-                    'InstitutionStudents.id', 'InstitutionStudents.student_status_id', 'InstitutionStudents.previous_institution_student_id'
-                ])
-                ->where([
-                    $InstitutionStudents->aliasField('student_id') => $row->student_id
-                ])
-                ->order([$InstitutionStudents->aliasField('InstitutionStudents.student_status_id') => 'DESC'])
-                ->autoFields(true)
-                ->first();
-                if($row->student_status == "Enrolled"){
-                    if(($InstitutionStudentsCurrentData->student_status_id == 8)){
-                        $student_status = "Enrolled (Repeater)";
-                    }else{
-                        $student_status = $row->student_status;
-                    }
-                }else{
-                        $student_status = $row->student_status;
-                }
-
-                $row['student_status'] = $student_status;
+                $Users = TableRegistry::get('security_users');
+                $institutionStudents = TableRegistry::get('institution_students');
+                $user_data= $Users
+                            ->find()
+                            ->select([
+                                        'student_status'=>'(case 
+                                            when StudentStatuses.name = "Repeated" then "Enrolled (Repeater)"
+                                            when StudentStatuses.name = "Enrolled" then "Enrolled"
+                                            when StudentStatuses.name = "Transferred" then "Transferred"
+                                            when StudentStatuses.name = "Withdrawn" then "Withdrawn"
+                                            when StudentStatuses.name = "Graduated" then "Graduated"
+                                            else "Promoted" end
+                                        )'
+                            ])
+                            ->leftJoin(
+                            ['InstitutionStudents' => 'institution_students'],
+                            [
+                                'InstitutionStudents.student_id = '.$Users->aliasField('id')
+                            ]
+                            ) 
+                            ->leftJoin(
+                                ['StudentStatuses' => 'student_statuses'],
+                                [
+                                    'StudentStatuses.id = InstitutionStudents.student_status_id'
+                                ]
+                            )
+                            ->where(['security_users.openemis_no' => $row->openemis_no,
+                            'InstitutionStudents.institution_id'=> $row->institution_id])
+                            ->order([
+                                'InstitutionStudents.created' => DESC
+                            ])
+                            ->first();
+                $row['student_status'] = $user_data->student_status;
                 return $row;
             });
-        });
+        });// POCOR-6338 ends
     }
 
     public function onExcelUpdateFields(Event $event, ArrayObject $settings, ArrayObject $fields)
