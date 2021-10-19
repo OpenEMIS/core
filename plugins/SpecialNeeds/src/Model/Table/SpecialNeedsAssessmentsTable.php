3e98c48093276cec8d675e8ad653eeae4895127d
vikas.rathore@mail.valuecoders.com <vikas.rathore@mail.valuecoders.com>
2021-10-19 13:07:32 +0530

    POCOR-6139 fields fixed

--- plugins/SpecialNeeds/src/Model/Table/SpecialNeedsAssessmentsTable.php
+++ plugins/SpecialNeeds/src/Model/Table/SpecialNeedsAssessmentsTable.php
@@ -153,14 +153,35 @@ class SpecialNeedsAssessmentsTable extends ControllerActionTable
         $this->setFieldOrder(['date', 'special_need_type_id', 'special_need_difficulty_id', 'file_name', 'file_content', 'comment']);
     }
 
+    // POCOR-6139 start
+    public function onExcelUpdateFields(Event $event, ArrayObject $settings, ArrayObject $fields)
+    {
+        $extraField[] = [
+            'key'   => 'SpecialNeedsAssessments.special_need_type_id',
+            'field' => 'special_need_type_id',
+            'type'  => 'integer',
+            'label' => __('Type')
+        ];
+
+        $extraField[] = [
+            'key'   => 'SpecialNeedsAssessments.special_need_difficulty_id',
+            'field' => 'special_need_difficulty_id',
+            'type'  => 'integer',
+            'label' => __('Difficulty')
+        ];
+
+        $fields->exchangeArray($extraField);
+    }
+    
     public function onExcelBeforeQuery(Event $event, ArrayObject $settings, Query $query){
 
         $session = $this->request->session();
         $staffUserId = $session->read('Institution.StaffUser.primaryKey.id');
-
+        
         $query
         ->where([
             $this->aliasField('security_user_id = ').$staffUserId,
         ]);
     }
+    // POCOR-6139 end
 }
