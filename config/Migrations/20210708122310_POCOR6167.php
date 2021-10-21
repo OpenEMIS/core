49d1216881a88c5e1c4a18ee3175c5a7092b3bd0
vikas.rathore@mail.valuecoders.com <vikas.rathore@mail.valuecoders.com>
2021-10-21 17:17:17 +0530

    POCOR-6167 done

--- config/Migrations/20210708122310_POCOR6167.php
+++ config/Migrations/20210708122310_POCOR6167.php
@@ -12,18 +12,17 @@ class POCOR6167 extends AbstractMigration
      */
     public function change()
     {
-       
+        /** START: security_functions table changes */
+        $this->execute('DROP TABLE IF EXISTS `z_6131_security_functions`');
         $this->execute('CREATE TABLE `z_6167_security_functions` LIKE `security_functions`');
         $this->execute('INSERT INTO `z_6167_security_functions` SELECT * FROM `security_functions`');
 
         //enable execute checkbox in Map permission
-
-        $this->execute("UPDATE security_functions SET _execute = 'excel' WHERE name = 'Providers' AND controller = 'InstitutionTransportProviders' AND module = 'Institutions' AND category = 'Transport'");
+        $this->execute("UPDATE security_functions SET `controller` = 'Institutions', `_view` = 'InstitutionTransportProviders.index|InstitutionTransportProviders.view', `_edit` = 'InstitutionTransportProviders.edit', `_add` = 'InstitutionTransportProviders.add', `_delete` = 'InstitutionTransportProviders.delete', `_execute` = 'InstitutionTransportProviders.excel' WHERE `name` = 'Providers' AND controller = 'InstitutionTransportProviders' AND module = 'Institutions' AND category = 'Transport'");
     }
 
  
     public function down()
-
     {
         $this->execute('DROP TABLE IF EXISTS `security_functions`');
         $this->execute('RENAME TABLE `z_6167_security_functions` TO `security_functions`');
