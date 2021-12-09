d1f80e5c468565e7bac91aa3c3b0fc526511b732
Ehteram Ahmad <ehteram333@gmail.com>
2021-12-09 16:40:51 +0530

    POCOR:6434 Bug fixes done

--- config/Migrations/20211130105436_POCOR6434.php
+++ config/Migrations/20211130105436_POCOR6434.php
@@ -12,6 +12,12 @@ class POCOR6434 extends AbstractMigration
      */
     public function up()
     {
+        //backup
+        $this->execute('CREATE TABLE `z_6434_meal_programmes` LIKE `meal_programmes`');
+        $this->execute('INSERT INTO `z_6434_meal_programmes` SELECT * FROM `meal_programmes`');
+
+        $this->execute('ALTER TABLE `meal_programmes` DROP COLUMN `area_id`');
+        $this->execute('ALTER TABLE `meal_programmes` DROP COLUMN `institution_id`');
         // Create tables
         $this->execute("CREATE TABLE IF NOT EXISTS `meal_institution_programmes` (
             `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -21,5 +27,13 @@ class POCOR6434 extends AbstractMigration
             `created` datetime  DEFAULT CURRENT_TIMESTAMP,
             PRIMARY KEY (`id`)
           )");
+    }
+
+    // rollback
+    public function down()
+    {
+       // meal_programmes
+       $this->execute('DROP TABLE IF EXISTS `meal_programmes`');
+        $this->execute('RENAME TABLE `z_6434_meal_programmes` TO `meal_programmes`');
     }
 }
