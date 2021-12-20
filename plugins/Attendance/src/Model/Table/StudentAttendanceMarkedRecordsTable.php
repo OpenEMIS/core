ae640802bb7ac58580ff92e867f3b3b52175b065
Poonam Kharka <poonam.kharka@mail.valuecoders.com>
2021-12-20 16:39:48 +0530

    modified query and conditions

--- plugins/Attendance/src/Model/Table/StudentAttendanceMarkedRecordsTable.php
+++ plugins/Attendance/src/Model/Table/StudentAttendanceMarkedRecordsTable.php
@@ -58,7 +58,7 @@ class StudentAttendanceMarkedRecordsTable extends AppTable
     /*POCOR-6021 starts*/
     public function findNoScheduledClass(Query $query, array $options) 
     {
-        //echo "<pre>";print_r($query);die();
+        $classStudents = TableRegistry::get('Institution.InstitutionClassStudents');
         $institutionId = $options['institution_id'];
         $academicPeriodId = $options['academic_period_id'];
         $institutionClassId = $options['institution_class_id'];
@@ -66,43 +66,60 @@ class StudentAttendanceMarkedRecordsTable extends AppTable
         $day = $options['day_id'];
         $row = [];
         
-        return $query->find('all')
-                ->where([
-                        $this->aliasField('institution_class_id') => $institutionClassId,
-                        $this->aliasField('education_grade_id') => $educationGradeId,
-                        $this->aliasField('institution_id') => $institutionId,
-                        $this->aliasField('academic_period_id') => $academicPeriodId,
-                        $this->aliasField('date') => $day
+        return $query
+                ->find('all')
+                ->innerJoin([$classStudents->alias() => $classStudents->table()], [
+                        $classStudents->aliasField('institution_id = ') . $this->aliasField('institution_id'),
+                        $classStudents->aliasField('institution_class_id = ') . $this->aliasField('institution_class_id'),
+                        $classStudents->aliasField('education_grade_id = ') . $this->aliasField('education_grade_id'),
+                        $classStudents->aliasField('academic_period_id = ') . $this->aliasField('academic_period_id'),
+                    ]
+                )->where([
+                        $classStudents->aliasField('institution_class_id') => $institutionClassId,
+                        $classStudents->aliasField('education_grade_id') => $educationGradeId,
+                        $classStudents->aliasField('institution_id') => $institutionId,
+                        $classStudents->aliasField('academic_period_id') => $academicPeriodId
                 ])
+                ->group([$classStudents->aliasField('student_id')])
                 ->formatResults(function (ResultSetInterface $results) use ($institutionClassId, $educationGradeId, $institutionId, $academicPeriodId, $day) {
                         return $results->map(function ($row) use ($institutionClassId, $educationGradeId, $institutionId, $academicPeriodId, $day) {
-                                if (!empty($row)) {
-                                    $query = $this->query();
-                                    $query ->update()
-                                            ->set(['period' => 0, 'subject_id' => 0, 'no_scheduled_class' => 1])
+                            
+                            $getRecord = $this->find('all')
                                             ->where([
                                                 $this->aliasField('institution_class_id') => $institutionClassId,
                                                 $this->aliasField('education_grade_id') => $educationGradeId,
                                                 $this->aliasField('institution_id') => $institutionId,
                                                 $this->aliasField('academic_period_id') => $academicPeriodId,
                                                 $this->aliasField('date') => $day
-                                            ])
-                                            ->execute();
-                                } else {
-                                    $query->newEntity([
-                                        'institution_class_id' => $institutionClassId,
-                                        'education_grade_id' => $educationGradeId,
-                                        'institution_id' => $institutionId,
-                                        'academic_period_id' => $academicPeriodId,
-                                        'date' => $day,
-                                        'period' => 0,
-                                        'subject_id' => 0,
-                                        'no_scheduled_class' => 1
-                                    ]);
-                                    $query->save($newEntity);
-                                }
-                                $row->is_Scheduled = 1;
-                                return $row;
+                                        ])->toArray();
+                            if (!empty($getRecord)) {
+                                $query = $this->query();
+                                $query ->update()
+                                        ->set(['period' => 0, 'subject_id' => 0, 'no_scheduled_class' => 1])
+                                        ->where([
+                                            $this->aliasField('institution_class_id') => $institutionClassId,
+                                            $this->aliasField('education_grade_id') => $educationGradeId,
+                                            $this->aliasField('institution_id') => $institutionId,
+                                            $this->aliasField('academic_period_id') => $academicPeriodId,
+                                            $this->aliasField('date') => $day
+                                        ])
+                                        ->execute();
+                            } else {
+                                $query = $this->query();
+                                $query->newEntity([
+                                    'institution_class_id' => $institutionClassId,
+                                    'education_grade_id' => $educationGradeId,
+                                    'institution_id' => $institutionId,
+                                    'academic_period_id' => $academicPeriodId,
+                                    'date' => $day,
+                                    'period' => 0,
+                                    'subject_id' => 0,
+                                    'no_scheduled_class' => 1
+                                ]);
+                                $query->save($newEntity);
+                            }
+                            $row->is_Scheduled = 1;
+                            return $row;
                             });
                         });
     }
