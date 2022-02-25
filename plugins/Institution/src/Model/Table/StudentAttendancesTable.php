b9512c27a957a06bf6a6185595b5ccca6ccd44ad
Poonam Kharka <poonam.kharka@mail.valuecoders.com>
2022-02-25 09:27:01 +0530

    commit local changes only for testing

--- plugins/Institution/src/Model/Table/StudentAttendancesTable.php
+++ plugins/Institution/src/Model/Table/StudentAttendancesTable.php
@@ -15,6 +15,7 @@ use Cake\Validation\Validator;
 use Cake\Chronos\Date;
 use Cake\Datasource\ResultSetInterface;
 use Cake\Core\Configure;
+use Cake\Log\Log;
 
 use App\Model\Table\ControllerActionTable;
 
@@ -850,6 +851,4 @@ class StudentAttendancesTable extends ControllerActionTable
     {
         echo "<pre>";print_r($data);die;
     }
-
-    
 }
