3ce7bb7019bef671e54d2a3d7c5d99e1ece47bc0
anand malvi <anand.malvi@mail.valuecoders.com>
2022-03-09 12:52:43 +0530

    POCOR-6569: Created a new 'Trainers Sessions' Report | status:DONE

--- plugins/Report/src/Controller/ReportsController.php
+++ plugins/Report/src/Controller/ReportsController.php
@@ -154,6 +154,7 @@ class ReportsController extends AppController
                 'Report.TrainingTrainers' => __('Trainers'),
                 'Report.TrainingSessionParticipants' => __('Session Participants'),
                 'Report.ReportTrainingNeedStatistics' => __('Training Needs Statistics'),
+                'Report.TrainersSessions' => __('Trainers Sessions'), // POCOR-6569
             ];
         } elseif ($module == 'Scholarships') {
             $options = [
