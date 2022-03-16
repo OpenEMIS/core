a5da0a6dbfd475e2ecb76dbd126673065d487ad6
Akshay patodi <akshay.patodi@mail.vinove.com>
2022-03-16 10:43:52 +0530

    Issue Resolved

--- plugins/Report/src/Model/Table/TrainingSessionsTable.php
+++ plugins/Report/src/Model/Table/TrainingSessionsTable.php
@@ -16,7 +16,10 @@ class TrainingSessionsTable extends AppTable  {
         $this->belongsTo('Courses', ['className' => 'Training.TrainingCourses', 'foreignKey' => 'training_course_id']);
         $this->belongsTo('TrainingProviders', ['className' => 'Training.TrainingProviders', 'foreignKey' => 'training_provider_id']);
         $this->belongsTo('Assignees', ['className' => 'User.Users']);
-
+         // Starts POCOR-6593
+        $this->belongsTo('TrainingSessionsTrainees', ['className' => 'Training.TrainingSessionsTrainees', 'foreignKey' => 'training_session_id']);
+         $this->belongsTo('Areas', ['className' => 'Area.Areas', 'foreignKey' => 'area_id']);
+          // Ends POCOR-6593
         $this->addBehavior('Excel', [
             'excludes' => ['code', 'name', 'assignee_id', 'status_id', 'training_course_id']
         ]);
@@ -35,24 +38,50 @@ class TrainingSessionsTable extends AppTable  {
 
     public function onExcelBeforeQuery(Event $event, ArrayObject $settings, Query $query)
     {
+         // Starts POCOR-6593
         $requestData = json_decode($settings['process']['params']);
         $selectedStatus = $requestData->status;
+        $areas = TableRegistry::get('areas');
+        $TrainingCourses = TableRegistry::get('training_courses');
+        $startDate=date("Y-m-d", strtotime($requestData->session_start_date));
+        $endDate=date("Y-m-d", strtotime($requestData->session_end_date));
+        $join=[];
+         $join[' '] = [
+            'type' => 'left',
+            'table' => '( SELECT count(*) AS number,training_sessions_trainees.training_session_id FROM training_sessions_trainees GROUP BY training_session_id) AS count_trainees',
+            'conditions' => ['TrainingSessions.id = count_trainees.training_session_id'],
 
-        $query
-            ->select(['course_code' => 'Courses.code'])
+        ];
+
+        $res=$query
+            
+            ->join($join)
+            ->leftJoin([$TrainingCourses->alias() => $TrainingCourses->table()], [
+                $TrainingCourses->aliasField('id = ') . 'TrainingSessions.training_course_id'
+            ])
+            ->select(['course_code' => 'training_courses.code','number' => 'number'])
+            ->where(['start_date >' => $startDate ])
+            ->where(['end_date <' => $endDate ])
             ->order([$this->Courses->aliasField('code'), $this->aliasField('code')]);
-
         if ($selectedStatus != '-1') {
             $query->matching('WorkflowSteps.WorkflowStatuses', function ($q) use ($selectedStatus) {
                 return $q->where(['WorkflowStatuses.id' => $selectedStatus]);
             });
         }
+         // Ends POCOR-6593
     }
 
     public function onExcelUpdateFields(Event $event, ArrayObject $settings, ArrayObject $fields)
     {
         $newFields = [];
-
+        // starts POCOR-6593
+        $newFields[] = [
+            'key' => 'TrainingSessions.created_user_id',
+            'field' => 'created_user_id',
+            'type' => 'integer',
+            'label' => 'Created User',
+        ];
+        
         $newFields[] = [
             'key' => 'TrainingSessions.status_id',
             'field' => 'status_id',
@@ -87,8 +116,14 @@ class TrainingSessionsTable extends AppTable  {
             'type' => 'string',
             'label' => __('Session Name'),
         ];
-
         $newFields = array_merge($newFields, $fields->getArrayCopy());
+        $newFields[] = [
+            'key' => 'training_sessions_trainees.number',
+            'field' => 'number',
+            'type' => 'string',
+            'label' => __('Number of Participant'),
+        ];
         $fields->exchangeArray($newFields);
+         // Ends POCOR-6593
     }
 }
