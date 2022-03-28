c68028b5db79f859c331b290f9295d02a6278832
Ehteram Ahmad <ehteram333@gmail.com>
2022-03-28 09:31:16 +0530

    POCOR-6608: Bug fix in migration STATUS:Done

--- config/Migrations/20220321050151_POCOR6608.php
+++ config/Migrations/20220321050151_POCOR6608.php
@@ -13,19 +13,20 @@ class POCOR6608 extends AbstractMigration
     public function up()
     {
         //backup
-        $this->execute('CREATE TABLE `z_6608_meal_institution_programmes` LIKE `meal_institution_programmes`');
-        $this->execute('INSERT INTO `z_6608_meal_institution_programmes` SELECT * FROM `meal_institution_programmes`');
+        $this->execute('DROP TABLE IF EXISTS `zz_6608_meal_institution_programmes`');
+        $this->execute('CREATE TABLE `zz_6608_meal_institution_programmes` LIKE `meal_institution_programmes`');
+        $this->execute('INSERT INTO `zz_6608_meal_institution_programmes` SELECT * FROM `meal_institution_programmes`');
 
         $this->execute('ALTER TABLE `meal_institution_programmes` ADD COLUMN `area_id` int(11) AFTER `institution_id`');
 
-        $this->execute("ALTER TABLE `meal_institution_programmes` CHANGE `area_id` `area_id` INT(11) NULL DEFAULT NULL COMMENT 'inks to areas.id' ");
+        $this->execute("ALTER TABLE `meal_institution_programmes` CHANGE `area_id` `area_id` INT(11) NULL DEFAULT NULL COMMENT 'Links to areas.id' ");
     }
 
     // rollback
     public function down()
     {
-       // meal_programmes
+       // meal_institution_programmes
        $this->execute('DROP TABLE IF EXISTS `meal_institution_programmes`');
-       $this->execute('RENAME TABLE `z_6608_meal_institution_programmes` TO `meal_institution_programmes`');
+       $this->execute('RENAME TABLE `zz_6608_meal_institution_programmes` TO `meal_institution_programmes`');
     }
 }
