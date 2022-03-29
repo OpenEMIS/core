5d8931fa571867306683cd677c9646cc78461590
divyaa <you@example.com>
2022-03-29 16:09:01 +0530

    get absent days||status:working

--- plugins/Institution/src/Model/Table/InstitutionStandardStudentAbsencesTable.php
+++ plugins/Institution/src/Model/Table/InstitutionStandardStudentAbsencesTable.php
@@ -9,6 +9,7 @@ use Cake\Event\Event;
 use Cake\Network\Request;
 use App\Model\Table\AppTable;
 use Cake\Log\Log;
+use Cake\ORM\Entity;
 
 /**
  * Get the Student Absences details in excel file 
@@ -95,13 +96,32 @@ class InstitutionStandardStudentAbsencesTable extends AppTable
     {
         $requestData = json_decode($settings['process']['params']);
         $academicPeriodId = $requestData->academic_period_id;
+        $academic_period = TableRegistry::get('AcademicPeriod.AcademicPeriods');
+        $getyear = $academic_period->find('all')
+                   ->select(['name'=>$academic_period->aliasField('start_year')])
+                   ->where(['id'=>$academicPeriodId])
+                   ->limit(1);
+        foreach($getyear->toArray() as $val) {
+            $year  = $val['name'];
+        }
         $institutionId = $requestData->institution_id;
         $gradeId = $requestData->education_grade_id;
         $classId = $requestData->institution_class_id;
         $month = $requestData->month;
         $absentDays = TableRegistry::get('Institution. InstitutionStudentAbsenceDays');
+        if ($gradeId != -1) {
+            $query->where([
+                $this->aliasField('education_grade_id') => $gradeId
+            ]);
+        }
+        $date =  '"'.$year.'-'.$month.'%"';
         $query
             ->select([
+                $this->aliasField('student_id'),
+                $this->aliasField('institution_id'),
+                $this->aliasField('education_grade_id'),
+                $this->aliasField('institution_class_id'),
+                $this->aliasField('date'),
                 'openemis_no' => 'Users.openemis_no',
                 'first_name' => 'Users.first_name',
                 'middle_name' => 'Users.middle_name',
@@ -124,55 +144,142 @@ class InstitutionStandardStudentAbsencesTable extends AppTable
             ->leftJoin([$absentDays->alias() => $absentDays->table()],
                 [$absentDays->aliasField('id = ') . $this->aliasField('institution_student_absence_day_id')]
             )
-            ->where([$this->aliasField('academic_period_id') => $academicPeriodId,
+            
+            //->andWhere([$this->aliasField('date LIKE ".$date."')]);
+           ->where([$this->aliasField('date') . ' LIKE ' =>  ".$date."])
+           ->andWhere([$this->aliasField('academic_period_id') => $academicPeriodId,
                     $this->aliasField('institution_id') => $institutionId,
                     $this->aliasField('institution_class_id') => $classId,
                     $this->aliasField('education_grade_id') => $gradeId,
-                    $this->aliasField('date') => $month,
-                ])
-            return $query;
-        
+                ]);
+            //print_r($query->Sql());die;
+            $query->formatResults(function (\Cake\Collection\CollectionInterface $results)
+            {
+            return $results->map(function ($row)
+            {
+                    $row['referrer_full_name'] = $row['first_name'] .' '. $row['last_name'];
+                    return $row;
+            });
+        });
+
     }
 
     /**
-     * Generate the all Header for sheet tab wise
+     * Generate the all Header for sheet
      */
     public function onExcelUpdateFields(Event $event, ArrayObject $settings, ArrayObject $fields)
     {
         $newFields = [];
         $newFields[] = [
-            'key'   => 'positionsNumber',
-            'field' => 'positionsNumber',
-            'type'  => 'string',
-            'label' => __('OpenEMIS ID'),
-        ];
-        $newFields[] = [
-            'key'   => 'referrer_is_home',
-            'field' => 'referrer_is_home',
-            'type'  => 'string',
-            'label' => __('Student full name'),
-        ];
-        $newFields[] = [
             'key'   => 'openemis_no',
             'field' => 'openemis_no',
-            'type'  => 'integer',
-            'label' => __('identity number'),
+            'type'  => 'string',
+            'label' => __('OpenEMIS ID'),
         ];
         $newFields[] = [
             'key'   => 'referrer_full_name',
             'field' => 'referrer_full_name',
             'type'  => 'string',
-            'label' => __('Day'),
+            'label' => __('Student full name'),
         ];
         $newFields[] = [
-            'key'   => 'fte',
-            'field' => 'fte',
+            'key'   => 'identity_number',
+            'field' => 'identity_number',
+            'type'  => 'string',
+            'label' => __('identity number'),
+        ];
+        /*$newFields[] = [
+            'key'   => 'referrer_full_name',
+            'field' => 'referrer_full_name',
+            'type'  => 'string',
+            'label' => __('Day'),
+        ];*/
+        $newFields[] = [
+            'key'   => 'total_absence_day',
+            'field' => 'total_absence_day',
             'type'  => 'integer',
-            'label' => __('Total absence'),
+            'label' => __('Total absences'),
         ];
 
-        $fields->exchangeArray($extraField);
+        $fields->exchangeArray($newFields);
     }
 
+    /**
+    * Get staff absences days
+    */
+    public function onExcelGetTotalAbsenceDay(Event $event, Entity $entity)
+    {
+        $userid =  $entity->student_id;
+        $institutionId =  $entity->institution_id;
+        $Institutionstudent = TableRegistry::get('Institution.InstitutionStudentAbsences');
+        $studentleave = TableRegistry::get('Institution.InstitutionStudentAbsenceDays');
+        $absenceDays = $Institutionstudent->find()
+            ->leftJoin(['InstitutionStudentAbsenceDays' => 'institution_student_absence_days'], ['InstitutionStudentAbsences.student_id = '. $studentleave->aliasField('student_id')])
+            ->select([
+                'days' => "SUM(".$studentleave->aliasField('absent_days').")"
+
+            ])
+           // ->group(['InstitutionStaffLeave.staff_id'])
+            ->where([$studentleave->aliasField('student_id') => $userid,
+                    $studentleave->aliasField('institution_id') => $institutionId
+                ]);
+            if($absenceDays!=null){
+                $data = $absenceDays->toArray();
+                $entity->total_absence_days = '';
+                foreach($data as $key=>$val){
+                    $entity->total_absence_days = $val['days'];
+                }
+                 return $entity->total_absence_days;
+            }
+            return '';
+    }
+    /**
+    * Get staff absences days
+    */
+    public function onExcelGetDays(Event $event, Entity $entity)
+    {
+        $userid =  $entity->student_id;
+        $Institutionstudent = TableRegistry::get('Institution.InstitutionStudentAbsences');
+        $studentleave = TableRegistry::get('Institution.InstitutionStudentAbsenceDays');
+        $absenceDays = $Institutionstudent->find()
+            ->leftJoin(['InstitutionStudentAbsenceDays' => 'institution_student_absence_days'], ['InstitutionStudentAbsences.student_id = '. $studentleave->aliasField('student_id')])
+            ->select([
+                'days' => "SUM(".$studentleave->aliasField('absent_days').")"
+
+            ])
+           // ->group(['InstitutionStaffLeave.staff_id'])
+            ->where([$studentleave->aliasField('student_id') => $userid,
+                    $studentleave->aliasField('institution_id') => $institutionId
+                ]);
+            if($absenceDays!=null){
+                $data = $absenceDays->toArray();
+                $entity->total_absence_days = '';
+                foreach($data as $key=>$val){
+                    $entity->total_absence_days = $val['days'];
+                }
+                 return $entity->total_absence_days;
+            }
+            return '';
+    }
+    public function onExcelGetInstitutionName(Event $event, Entity $entity)
+    {
+
+        return $entity->institution_id;
+    }
+    public function onExcelGetInstitutionCode(Event $event, Entity $entity)
+    {
+
+        return $entity->institution_id;
+    }
+    public function onExcelGetEducationGrade(Event $event, Entity $entity)
+    {
+
+        return $entity->education_grade_id;
+    }
+    public function onExcelGetInstitutionClass(Event $event, Entity $entity)
+    {
+
+        return $entity->institution_class_id;
+    }
    
 }
