076efe31029f12c6359f279a3e650db55b1f5dfb
divyaa <you@example.com>
2022-03-29 16:09:01 +0530

    get absent days||status:working

--- plugins/Institution/src/Model/Table/InstitutionStandardsTable.php
+++ plugins/Institution/src/Model/Table/InstitutionStandardsTable.php
@@ -115,10 +115,11 @@ class InstitutionStandardsTable extends AppTable
     {
         if (($request->data[$this->alias()]['feature'])=='Institution.InstitutionStandardStudentAbsences')
         {
-        
-            $entity = $attr['entity'];
-            $attr['type'] = 'date';
-            // $attr['onChangeReload'] = true;
+            $monthoption = ['01'=>"January",'02'=>"February",'03'=>"March",'04'=>"April",'05'=>"May",'06'=>"June",'07'=>"July",'08'=>"August",'09'=>"September",10=>"October",11=>"November",12=>"December"];
+            $attr['options']        = $monthoption;
+            $attr['type']           = 'select';
+            $attr['select']         = false; 
+            $attr['onChangeReload'] = true;
             return $attr;
         }   
         
