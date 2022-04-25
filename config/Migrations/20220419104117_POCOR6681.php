a6c803fa191dd4e074d2b3b08291eb505ab7df8c
Ehteram Ahmad <ehteram333@gmail.com>
2022-04-25 15:39:48 +0530

    POCOR-6681: Institutions>Meals>Students/Distribution: Changes to Meals STATUS : Done

--- config/Migrations/20220419104117_POCOR6681.php
+++ config/Migrations/20220419104117_POCOR6681.php
@@ -12,9 +12,23 @@ class POCOR6681 extends AbstractMigration
      */
     public function up()
     {
+        //Back up
+        //meal_received
         $this->execute('DROP TABLE IF EXISTS `zz_6681_meal_received`');
         $this->execute('CREATE TABLE `zz_6681_meal_received` LIKE `meal_received`');
         $this->execute('INSERT INTO `zz_6681_meal_received` SELECT * FROM `meal_received`');
+
+        //Back up
+        //import_mapping
+        $this->execute('DROP TABLE IF EXISTS `zz_6681_import_mapping`');
+        $this->execute('CREATE TABLE `zz_6681_import_mapping` LIKE `import_mapping`');
+        $this->execute('INSERT INTO `zz_6681_import_mapping` SELECT * FROM `import_mapping`');
+
+        //Back up
+        //institution_meal_programmesg
+        $this->execute('DROP TABLE IF EXISTS `zz_6681_institution_meal_programmesg`');
+        $this->execute('CREATE TABLE `zz_6681_institution_meal_programmesg` LIKE `institution_meal_programmesg`');
+        $this->execute('INSERT INTO `zz_6681_institution_meal_programmesg` SELECT * FROM `institution_meal_programmesg`');
 
         $checkDataExist = $this->query("SELECT * FROM meal_received WHERE code = 'none'");
 
@@ -39,6 +53,12 @@ class POCOR6681 extends AbstractMigration
 	    // meal_received
         $this->execute('DROP TABLE IF EXISTS `meal_received`');
         $this->execute('RENAME TABLE `zz_6681_meal_received` TO `meal_received`');
+
+        $this->execute('DROP TABLE IF EXISTS `import_mapping`');
+        $this->execute('RENAME TABLE `zz_6681_import_mapping` TO `import_mapping`');
+
+        $this->execute('DROP TABLE IF EXISTS `institution_meal_programmes`');
+        $this->execute('RENAME TABLE `zz_6681_institution_meal_programmes` TO `institution_meal_programmes`');
 		
     }
 }
