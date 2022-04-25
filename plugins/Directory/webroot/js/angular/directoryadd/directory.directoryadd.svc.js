210acdeea98f3d2efedbdfabb7630b471f71724c
arjun.porwal <arjun@metadesignsolutions.co.uk>
2022-04-25 14:26:52 +0530

    Done the staff save api related changes

--- plugins/Directory/webroot/js/angular/directoryadd/directory.directoryadd.svc.js
+++ plugins/Directory/webroot/js/angular/directoryadd/directory.directoryadd.svc.js
@@ -196,7 +196,7 @@ function DirectoryaddSvc($http, $q, $filter, KdOrmSvc, AlertSvc, UtilsSvc, $wind
     function saveDirectoryData (params) {
         var deferred = $q.defer();
         var url = angular.baseUrl + '/Institution/Institutions/saveDirectoryData';
-        $http.post(url, {params: params})
+        $http.post(url, params)
         .then(function(response){
             deferred.resolve(response);
         }, function(error) {
