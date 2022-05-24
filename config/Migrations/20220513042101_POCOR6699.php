520eea83a4942a2ae6da2e3609d4e71ac79086a3
Rishabh Sharma <rishabh.sharma1@mail.valuecoders.com>
2022-05-24 16:26:54 +0530

    POCOR-6699 migration updated Status:done

--- config/Migrations/20220513042101_POCOR6699.php
+++ config/Migrations/20220513042101_POCOR6699.php
@@ -14,7 +14,7 @@ class POCOR6699 extends AbstractMigration
     {
         $this->execute('CREATE TABLE `zz_6699_security_functions` LIKE `security_functions`');
         $this->execute('INSERT INTO `zz_6699_security_functions` SELECT * FROM `security_functions`');
-        $this->execute("INSERT INTO `security_functions` (`id`, `name`, `controller`, `module`, `category`, `parent_id`, `_view`, `_edit`, `_add`, `_delete`, `_execute`, `order`, `visible`, `description`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES (NULL, 'Assessments', 'Profiles', 'Personal', 'Students - Academic', '9030', 'StudentAssessments.index', NULL, NULL, NULL, NULL, '466', '1', NULL, NULL, NULL, '1', NOW())");
+        $this->execute("UPDATE `security_functions` SET `_view` = 'StudentAssessments.index' WHERE  `security_functions`.`category` = 'Students - Academic' AND `security_functions`.`name` = 'Assessments' AND `security_functions`.`controller` = 'Profiles' AND `security_functions`.`module` = 'Personal'  AND `security_functions`.`_view` = 'StudentResults.index.view'");
         //END security_functions..
     }
     
