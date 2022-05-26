d73d6400b310d8a0b29fd8f9609566cfad84fe16
Ehteram Ahmad <ehteram333@gmail.com>
2022-05-26 10:33:34 +0530

    POCOR-6753 Institution>Meals>Students: Current week is not showing properly STATUS: Done

---
+++ config/Migrations/20220526041316_POCOR6753.php
@@ -0,0 +1,34 @@
+<?php
+use Migrations\AbstractMigration;
+
+class POCOR6753 extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function up()
+    {
+        //backup
+        // $this->execute('CREATE TABLE `zz_6753_config_items` LIKE `config_items`');
+        // $this->execute('INSERT INTO `zz_6753_config_items` SELECT * FROM `config_items`');
+
+        //Update security functions for Absence delete permission
+        $this->execute(
+            'UPDATE `config_items` SET `value` = 1 
+            WHERE `name` = "First Day of Week" AND `code` = "first_day_of_week"
+            AND `type` = "Attendance"
+            AND `label` = "First Day of Week"'
+        );
+    }
+
+    public function down()
+    {
+        // rollback
+        $this->execute('DROP TABLE IF EXISTS `config_items`');
+        $this->execute('RENAME TABLE `zz_6753_config_items` TO `config_items`');
+    }
+}
