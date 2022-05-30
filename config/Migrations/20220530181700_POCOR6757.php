a8f762f6dbf85d1ce64f4d9c35adbb29b8bdedd6
Umairah Yusoff <uyusoff@kordit.com>
2022-05-30 18:23:35 +0800

    Added migration script

---
+++ config/Migrations/20220530181700_POCOR6757.php
@@ -0,0 +1,32 @@
+<?php
+use Migrations\AbstractMigration;
+
+class POCOR6757 extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function up()
+    {
+        /** BACKUP OpenEMIS Core report_queries */
+        $this->execute('DROP TABLE IF EXISTS `z_6757_report_queries`');
+        $this->execute('CREATE TABLE `z_6757_report_queries` LIKE `report_queries`');
+
+        /** UPDATE OpenEMIS Core report_queries */
+        $this->execute('UPDATE report_queries SET frequency = `week` WHERE report_queries.name IN (`report_student_assessment_summary_truncate`,`report_student_assessment_summary_insert`)');
+        
+    }
+
+    //rollback
+    public function down()
+    {
+        /** RESTORE OpenEMIS Core report_queries */
+        $this->execute('DROP TABLE IF EXISTS `report_queries`');
+        $this->execute('CREATE TABLE `report_queries` LIKE `z_6757_report_queries`');
+        $this->execute('DROP TABLE IF EXISTS `z_6757_report_queries`');
+    }
+}
