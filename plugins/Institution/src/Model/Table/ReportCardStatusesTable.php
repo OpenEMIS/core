df69f518c34757883b89388925450a2f9c9c3690
vinoverishabh <rishabh.sharma1@mail.valuecoders.com>
2022-06-16 18:19:42 +0530

    POCOR-6785 Administration/Institutions>Performance>Report Card: Update stuck student report card to error status STATUS:DONE

--- plugins/Institution/src/Model/Table/ReportCardStatusesTable.php
+++ plugins/Institution/src/Model/Table/ReportCardStatusesTable.php
@@ -11,6 +11,7 @@ use Cake\ORM\ResultSet;
 use Cake\Event\Event;
 use Cake\I18n\Time;
 use Cake\Log\Log;
+use Cake\Datasource\ConnectionManager; //POCOR-6785
 
 use App\Model\Table\ControllerActionTable;
 
@@ -227,6 +228,14 @@ class ReportCardStatusesTable extends ControllerActionTable
 
     public function indexBeforeAction(Event $event, ArrayObject $extra)
     {
+        //Start:POCOR-6785 need to convert this custom query to cake query
+        $conn = ConnectionManager::get('default');
+        $stmt = $conn->query("UPDATE report_card_processes SET status = CASE WHEN TIMESTAMPDIFF(MINUTE,(report_card_processes.modified), CURRENT_TIMESTAMP) BETWEEN 5 AND 30 THEN 1 WHEN TIMESTAMPDIFF(MINUTE,(report_card_processes.modified), CURRENT_TIMESTAMP) > 30 THEN -1 END WHERE modified IS NOT NULL AND TIMESTAMPDIFF(MINUTE,(report_card_processes.modified), CURRENT_TIMESTAMP) >= 5");
+        $successQ = $stmt->execute();
+
+        $stmtNew = $conn->query("UPDATE institution_students_report_cards INNER JOIN report_card_processes ON institution_students_report_cards.report_card_id = report_card_processes.report_card_id AND institution_students_report_cards.student_id = report_card_processes.student_id AND institution_students_report_cards.institution_id = report_card_processes.institution_id AND institution_students_report_cards.academic_period_id = report_card_processes.academic_period_id AND institution_students_report_cards.education_grade_id = report_card_processes.education_grade_id AND institution_students_report_cards.institution_class_id = report_card_processes.institution_class_id SET institution_students_report_cards.status = report_card_processes.status");
+        $successQQ =$stmtNew->execute();
+        //END:POCOR-6785
         $this->field('report_queue');
         $this->setFieldOrder(['openemis_no', 'student_id', 'report_card', 'status', 'started_on', 'completed_on', 'report_queue', 'email_status']);
 
