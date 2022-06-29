fc3456455da0836b7e8ace2914e0096277d3e646
Rahul Singh <rahul.singh@mail.valuecoders.com>
2022-06-29 13:42:04 +0530

    POCOR-6823:student transfer class issue resolved|Status:Done

--- plugins/Assessment/src/Model/Table/AssessmentItemResultsTable.php
+++ plugins/Assessment/src/Model/Table/AssessmentItemResultsTable.php
@@ -20,7 +20,7 @@ class AssessmentItemResultsTable extends AppTable
         parent::initialize($config);
 
         $this->belongsTo('Assessments', ['className' => 'Assessment.Assessments']);
-        $this->belongsTo('EducationSubjects', ['className' => 'Education.EducationSubjects']);
+         $this->belongsTo('EducationSubjects', ['className' => 'Education.EducationSubjects']);
         $this->belongsTo('EducationGrades', ['className' => 'Education.EducationGrades']);
         $this->belongsTo('AssessmentGradingOptions', ['className' => 'Assessment.AssessmentGradingOptions']);
         $this->belongsTo('Users', ['className' => 'User.Users', 'foreignKey' => 'student_id']);
@@ -106,6 +106,27 @@ class AssessmentItemResultsTable extends AppTable
              $studentId = $options['user']['id'];
        }
 
+       //Start POCOR-6823
+       $InstitutionClassStudents = TableRegistry::get('Institution.InstitutionClassStudents');
+        $conditionsClassStudents = [
+            $InstitutionClassStudents->aliasField('academic_period_id = ') => $academicPeriodId,
+            $InstitutionClassStudents->aliasField('student_id = ') => $studentId,
+            $InstitutionClassStudents->aliasField('institution_id = ') => $institutionId,
+            $InstitutionClassStudents->aliasField('student_status_id = ') => 1,
+        ];
+
+        $ClassStudentsStatusUpdate = $InstitutionClassStudents
+        ->find()
+        ->where($conditionsClassStudents)
+        ->all();
+
+        $className = '';
+        if (!$ClassStudentsStatusUpdate->isEmpty()) {
+            $ClassStudents = $ClassStudentsStatusUpdate->first();
+            $className = $ClassStudents->institution_class_id;
+        }
+        //End POCOR-6823
+       
         return $query
             ->select([
                 $this->aliasField('id'),
@@ -136,6 +157,7 @@ class AssessmentItemResultsTable extends AppTable
             ->where([
                 $this->aliasField('academic_period_id') => $academicPeriodId,
                 $this->aliasField('student_id') => $studentId,
+                $this->aliasField('institution_classes_id ') => $className,  // POCOR-6823
                 $this->aliasField('institution_id') => $institutionId    //POCOR-6823
             ])
             ->order([
