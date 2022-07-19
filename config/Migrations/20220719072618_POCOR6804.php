45aeccb67b1090e7a57ade05ea6052821b1db564
Ehteram Ahmad <ehteram333@gmail.com>
2022-07-19 14:04:17 +0530

    POCOR-6804: Change in migration file default value STATUS: Done

--- config/Migrations/20220719072618_POCOR6804.php
+++ config/Migrations/20220719072618_POCOR6804.php
@@ -13,8 +13,8 @@ class POCOR6804 extends AbstractMigration
     public function up()
     {
         // Backup table
-        $this->execute('CREATE TABLE `zz_6804_config_items` LIKE `config_items`');
-        $this->execute('INSERT INTO `zz_6804_config_items` SELECT * FROM `config_items`');
+        // $this->execute('CREATE TABLE `zz_6804_config_items` LIKE `config_items`');
+        // $this->execute('INSERT INTO `zz_6804_config_items` SELECT * FROM `config_items`');
         // End
 
         $table = $this->table('config_items');
@@ -24,9 +24,9 @@ class POCOR6804 extends AbstractMigration
                 'code' => 'api_settings',
                 'type' => 'API Settings',
                 'label' => 'API Settings',
-                'value' => '',
+                'value' => 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiT3BlbkVNSVMgQ29yZSIsInR5cGUiOiJhcGlfc2V0dGluZ3MifQ.I9rsqLEsSOUH5eYhUhk0BSu-tZ0343NBchTaJRylPmFTma4xfbObNMK_XIA51pFiIX8pVhWnmrVcpnxHYFguOA',
                 'value_selection' => '',
-                'default_value' => '',
+                'default_value' => 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiT3BlbkVNSVMgQ29yZSIsInR5cGUiOiJhcGlfc2V0dGluZ3MifQ.I9rsqLEsSOUH5eYhUhk0BSu-tZ0343NBchTaJRylPmFTma4xfbObNMK_XIA51pFiIX8pVhWnmrVcpnxHYFguOA',
                 'editable' => '1',
                 'visible' => '1',
                 'field_type' => '',
