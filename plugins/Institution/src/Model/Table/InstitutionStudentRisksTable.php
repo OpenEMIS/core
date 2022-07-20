7090b619e16968092091972f5ea9a8bc6190f0f1
divyaa <you@example.com>
2022-07-20 18:23:08 +0530

    event update||status:working

--- plugins/Institution/src/Model/Table/InstitutionStudentRisksTable.php
+++ plugins/Institution/src/Model/Table/InstitutionStudentRisksTable.php
@@ -557,9 +557,9 @@ class InstitutionStudentRisksTable extends ControllerActionTable
 					
 				}
 			}
-			$body = array();
+			$bodys = array();
 				   
-			$body = [   
+			$bodys = [   
 				'security_users_id' => !empty($user_id) ? $user_id : NULL,
 				'security_users_openemis_no' => !empty($openemis_no) ? $openemis_no : NULL,
 				'security_users_first_name' =>	!empty($first_name) ? $first_name : NULL,
@@ -590,7 +590,70 @@ class InstitutionStudentRisksTable extends ControllerActionTable
 				'institution_students_end_date' => !empty($endDate) ? date("d-m-Y", strtotime($endDate)) : NULL,
                 'role_name' => ($role == 1) ? 'student' : NULL	
 			];
-			
+
+            $Guardians = TableRegistry::get('student_custom_field_values');
+            $studentCustomFieldOptions = TableRegistry::get('student_custom_field_options');
+            $studentCustomFields = TableRegistry::get('student_custom_fields');
+
+            $guardianData = $Guardians->find()
+            ->select([
+                'id'                             => $Guardians->aliasField('id'),
+                'custom_id'                      => 'studentCustomField.id',
+                'student_id'                     => $Guardians->aliasField('student_id'),
+                'student_custom_field_id'        => $Guardians->aliasField('student_custom_field_id'),
+                'text_value'                     => $Guardians->aliasField('text_value'),
+                'number_value'                   => $Guardians->aliasField('number_value'),
+                'decimal_value'                  => $Guardians->aliasField('decimal_value'),
+                'textarea_value'                 => $Guardians->aliasField('textarea_value'),
+                'date_value'                     => $Guardians->aliasField('date_value'),
+                'time_value'                     => $Guardians->aliasField('time_value'),
+                'checkbox_value_text'            => $studentCustomFieldOptions->aliasField('name'),
+                'name'                           => 'studentCustomField.name',
+                'field_type'                     => 'studentCustomField.field_type',
+                ])->leftJoin(
+                ['studentCustomField' => 'student_custom_fields'],
+                [
+                    'studentCustomField.id = '.$Guardians->aliasField('student_custom_field_id')
+                ])
+                ->leftJoin(
+                [$studentCustomFieldOptions->alias() => $studentCustomFieldOptions->table()],
+                [
+                    $studentCustomFieldOptions->aliasField('student_custom_field_id') => $Guardians->aliasField('student_custom_field_id')
+                ])
+                ->where([
+                $Guardians->aliasField('student_id') => $user_id,
+                ])->hydrate(false)->toArray();
+                $custom_field = array();
+                $count = 0;
+                if(!empty($guardianData)){
+                    foreach ($guardianData as $val) {
+                        $custom_field['custom_field'][$count]["id"] = (!empty($val['custom_id']) ? $val['custom_id'] : '');
+                        $custom_field['custom_field'][$count]["name"]= (!empty($val['name']) ? $val['name'] : '');
+                        $fieldTypes[$count] = (!empty($val['field_type']) ? $val['field_type'] : '');
+                        $fieldType = $fieldTypes[$count];
+                        if($fieldType == 'TEXT'){
+                            $custom_field['custom_field'][$count]["text_value"] = (!empty($val['text_value']) ? $val['text_value'] : '');
+                        }else if ($fieldType == 'CHECKBOX') {
+                            $custom_field['custom_field'][$count]["checkbox_value"] = (!empty($val['checkbox_value_text']) ? $val['checkbox_value_text'] : '');
+                        }else if ($fieldType == 'NUMBER') {
+                            $custom_field['custom_field'][$count]["number_value"] = (!empty($val['number_value']) ? $val['number_value'] : '');
+                        }else if ($fieldType == 'DECIMAL') {
+                            $custom_field['custom_field'][$count]["decimal_value"] = (!empty($val['decimal_value']) ? $val['decimal_value'] : '');
+                        }else if ($fieldType == 'TEXTAREA') {
+                            $custom_field['custom_field'][$count]["textarea_value"] = (!empty($val['textarea_value']) ? $val['textarea_value'] : '');
+                        }else if ($fieldType == 'DROPDOWN') {
+                            $custom_field['custom_field'][$count]["dropdown_value"] = (!empty($val['checkbox_value_text']) ? $val['checkbox_value_text'] : '');
+                        }else if ($fieldType == 'DATE') {
+                            $custom_field['custom_field'][$count]["date_value"] = date('Y-m-d', strtotime($val->date_value));
+                        }else if ($fieldType == 'TIME') {
+                            $custom_field['custom_field'][$count]["time_value"] = date('h:i A', strtotime($val->time_value));
+                        }else if ($fieldType == 'COORDINATES') {
+                            $custom_field['custom_field'][$count]["cordinate_value"] = (!empty($val['text_value']) ? $val['text_value'] : '');
+                        }
+                        $count++;
+                    }
+                }
+            $body = array_merge($bodys, $custom_field);
 			if (!$afterSaveOrDeleteEntity->isNew()) {
 				$Webhooks = TableRegistry::get('Webhook.Webhooks');
 				if (!empty($afterSaveOrDeleteEntity->modified_user_id)) {
