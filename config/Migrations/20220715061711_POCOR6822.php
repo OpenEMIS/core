2f2ce6571d8bceba83c98fce0c2dc456b7c65709
anubhav0789 <jainanubhav67@yahoo.in>
2022-08-04 11:27:32 +0530

    POCOR-6822| code enhance for migration | Status: Done

--- config/Migrations/20220715061711_POCOR6822.php
+++ config/Migrations/20220715061711_POCOR6822.php
@@ -20,7 +20,22 @@ class POCOR6822 extends AbstractMigration
         $row = $this->fetchRow('SELECT `order` FROM `security_functions` WHERE `name` = "Staff" AND `controller` = "ProfileTemplates" AND `module` = "Administration"');
         $order = $row['order'];
         $this->execute('UPDATE `security_functions` SET `order` = `order` + 1 WHERE `order` >= ' . $order);
-        $this->execute('INSERT INTO `security_functions` (`name`, `controller`, `module`, `category`, `parent_id`, `_view`, `_edit`, `_add`, `_delete`, `_execute`, `order`, `visible`, `created_user_id`, `created` ) VALUES (`Classes`, `ProfileTemplates`, `Administration`, `Profiles`, 5000, `Classes.index|Classes.view|ClassesProfiles.view`, `Classes.edit`, `Classes.add`, `Classes.remove`, `ClassesProfiles.generate|ClassesProfiles.downloadExcel|ClassesProfiles.publish|ClassesProfiles.unpublish|ClassesProfiles.email|ClassesProfiles.downloadAll|ClassesProfiles.generateAll|ClassesProfiles.publishAll|ClassesProfiles.unpublishAll`, ' . $order . ', 1, `1`, date(`Y-m-d H:i:s`))');
+        $this->insert('security_functions', [
+                'name' => 'Classes',
+                'controller' => 'ProfileTemplates',
+                'module' => 'Administration',
+                'category' => 'Profiles',
+                'parent_id' => 5000,
+                '_view' => 'Classes.index|Classes.view|ClassesProfiles.view',
+                '_edit' => 'Classes.edit',
+                '_add' => 'Classes.add',
+                '_delete' => 'Classes.remove',
+                '_execute' => 'ClassesProfiles.generate|ClassesProfiles.downloadExcel|ClassesProfiles.publish|ClassesProfiles.unpublish|ClassesProfiles.email|ClassesProfiles.downloadAll|ClassesProfiles.generateAll|ClassesProfiles.publishAll|ClassesProfiles.unpublishAll',
+                'order' => $order,
+                'visible' => 1,
+                'created_user_id' => '1',
+                'created' => date('Y-m-d H:i:s')
+            ]);
 
         // class_profile_templates
         $this->execute(
@@ -28,12 +43,12 @@ class POCOR6822 extends AbstractMigration
             `id` int(11) NOT NULL AUTO_INCREMENT,
             `code` varchar(50) NOT NULL,
             `name` varchar(150) NOT NULL,
-            `description` text DEFAULT NULL,
-            `generate_start_date` datetime DEFAULT NULL,
-            `generate_end_date` datetime DEFAULT NULL,
-            `excel_template_name` varchar(250) DEFAULT NULL,
-            `excel_template` blob DEFAULT NULL,
-            `academic_period_id` int(11) DEFAULT NULL COMMENT "links to academic_periods.id",
+            `description` text NOT NULL,
+            `generate_start_date` datetime NOT NULL,
+            `generate_end_date` datetime NOT NULL,
+            `excel_template_name` varchar(250) NOT NULL,
+            `excel_template` longblob NOT NULL,
+            `academic_period_id` int(11) NOT NULL COMMENT "links to academic_periods.id",
             `modified_user_id` int(11) DEFAULT NULL,
             `modified` datetime DEFAULT NULL,
             `created_user_id` int(11) NOT NULL,
@@ -47,15 +62,12 @@ class POCOR6822 extends AbstractMigration
         $this->execute(
           'CREATE TABLE IF NOT EXISTS `class_profile_processes` (
             `class_profile_template_id` int(11) NOT NULL COMMENT "links to class_profile_templates.id",
-            `status` int(11) NOT NULL COMMENT "1 => New 2 => Running 3 => Completed -1 => Error",
-            `institution_class_id` int(11) NOT NULL COMMENT "links to institution_classes.id",
+            `status` int(2) NOT NULL COMMENT "1 => New 2 => Running 3 => Completed -1 => Error",
             `institution_id` int(11) NOT NULL COMMENT "links to institutions.id",
             `academic_period_id` int(11) NOT NULL COMMENT "links to academic_periods.id",
-            `modified_user_id` int(11) DEFAULT NULL,
-            `modified` datetime DEFAULT NULL,
-            `created_user_id` int(11) NOT NULL,
+            `institution_class_id` int(11) NOT NULL COMMENT "links to institution_classes.id",
             `created` datetime NOT NULL,
-             PRIMARY KEY (`class_profile_template_id`, `institution_id`),
+             PRIMARY KEY (`class_profile_template_id`, `institution_class_id`),
              FOREIGN KEY (`class_profile_template_id`) REFERENCES `class_profile_templates` (`id`),
              FOREIGN KEY (`institution_class_id`) REFERENCES `institution_classes` (`id`),
              FOREIGN KEY (`institution_id`) REFERENCES `institutions` (`id`),
@@ -66,8 +78,8 @@ class POCOR6822 extends AbstractMigration
         // class_profiles
         $this->execute(
           'CREATE TABLE IF NOT EXISTS `class_profiles` (
-            `id` varchar(50) NOT NULL,
-            `status` int(11) NOT NULL COMMENT "1 => New 2 => Running 3 => Completed -1 => Error",
+            `id` char(64) NOT NULL,
+            `status` int(1) NOT NULL COMMENT "1 => New, 2 => In Progress, 3 => Generated, 4 => Published",
             `file_name` varchar(250) DEFAULT NULL,
             `file_content` longblob DEFAULT NULL,
             `file_content_pdf` longblob DEFAULT NULL,
@@ -89,6 +101,9 @@ class POCOR6822 extends AbstractMigration
              INDEX `id` (`id`)
           )  ENGINE=InnoDB DEFAULT CHARSET=utf8'
      	);
+
+        $this->execute('ALTER TABLE `class_profiles` ADD INDEX(`class_profile_template_id`)');
+        $this->execute('ALTER TABLE `class_profile_processes` ADD INDEX(`class_profile_template_id`)');
     }
      
     // rollback
