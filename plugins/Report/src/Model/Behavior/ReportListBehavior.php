da1a6308b73e08e0d33fa1c52da9cff4764e368f
Rahul Singh <rahul.singh@mail.valuecoders.com>
2022-09-05 06:54:37 +0530

    POCOR-6732:issue resolved|Status:Done

--- plugins/Report/src/Model/Behavior/ReportListBehavior.php
+++ plugins/Report/src/Model/Behavior/ReportListBehavior.php
@@ -242,7 +242,8 @@ class ReportListBehavior extends Behavior {
 
 	public function onExcelGenerateComplete(Event $event, ArrayObject $settings) {
 		$ConfigItems = TableRegistry::get('Configuration.ConfigItems');
-		$timeZone= $ConfigItems->value("time_zone");
+		$setTime= $ConfigItems->value("time_zone");
+		$timeZone= !empty($setTime) ? $setTime : 'UTC', //POCOR-6732
 		date_default_timezone_set($timeZone);
 		$currentTimeZone = date("Y-m-d H:i:s");
 		$process = $settings['process'];
