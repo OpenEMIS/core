218ae901cddaf5ddb689c28f48a0548f90b460ec
vinoverishabh <rishabh.sharma1@mail.valuecoders.com>
2022-09-19 15:10:55 +0530

    POCOR-6975 Institution > Academic > Programmes: Able to delete programmes when classes for the programmes still exist Status:Done

--- plugins/ControllerAction/src/Model/Behavior/RemoveBehavior.php
+++ plugins/ControllerAction/src/Model/Behavior/RemoveBehavior.php
@@ -211,7 +211,7 @@ class RemoveBehavior extends Behavior
                     }
                     $totalCount += $row['count'];
                 }
-                if ($extra['associatedRecordsss'][0]['count'] > 0 && $extra['associatedRecords'][1]['count'] > 0 ) { //POCOR-6964 && POCOR-6975
+                if ($extra['associatedRecordsss'][0]['count'] >= 0 && $extra['associatedRecords'][1]['count'] > 0 ) { //POCOR-6964 && POCOR-6975
                     $model->Alert->error('general.delete.restrictDeleteBecauseAssociation');
                     $this->recordHasAssociatedRecords = true;
                 } else {
