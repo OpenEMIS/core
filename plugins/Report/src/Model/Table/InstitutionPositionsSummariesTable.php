03c5b846838067f81001a9963302f0466ee27147
vinoverishabh <rishabh.sharma1@mail.valuecoders.com>
2022-10-13 17:59:11 +0530

    POCOR-6952 Develop a new Report: Institution Positions Summaries Status:Done

--- plugins/Report/src/Model/Table/InstitutionPositionsSummariesTable.php
+++ plugins/Report/src/Model/Table/InstitutionPositionsSummariesTable.php
@@ -19,9 +19,17 @@ class InstitutionPositionsSummariesTable extends AppTable
 
     public function initialize(array $config)
     {
-        $this->table('institution_positions');
+        $this->table('institution_staff');
         parent::initialize($config);
         
+        $this->belongsTo('InstitutionPositions', ['className' => 'Institution.InstitutionPositions', 'foreignKey' => 'institution_position_id']);
+        $this->belongsTo('StaffPositionTitles', ['className' => 'Institution.StaffPositionTitles']);
+        $this->belongsTo('StaffPositionGrades', ['className' => 'Institution.StaffPositionGrades']);
+        $this->belongsTo('Staffs', ['className' => 'User.Users']);
+        $this->belongsTo('Areas', ['className' => 'Institution.Areas']);
+        $this->belongsTo('Institutions', ['className' => 'Institution.Institutions']);
+
+
         $this->addBehavior('Excel', [
             'autoFields' => false
         ]);
@@ -31,5 +39,138 @@ class InstitutionPositionsSummariesTable extends AppTable
 
 
     }
+
+    public function onExcelBeforeQuery(Event $event, ArrayObject $settings, Query $query)
+    {
+        $requestData = json_decode($settings['process']['params']);
+
+        $academicperiodid = $requestData->academic_period_id;
+        $area_level_id = $requestData->area_level_id;
+       
+        $statusFilter = $requestData->status;  
+
+        $institution_id = $requestData->institution_id;
+        $areaId = $requestData->area_education_id;
+        $where = [];
+        if ($institution_id != 0) {
+            $where[$this->aliasField('institution_id')] = $institution_id;
+        }
+        if ($academicperiodid != -1) {
+            //find academic priod
+            $AcademicPeriodsTable = TableRegistry::get('academic_periods');
+            $AcademicPeriod = $AcademicPeriodsTable->find('all',['conditions'=>['id'=>$academicperiodid]])->first();
+            $where[$this->aliasField('start_year')] = $AcademicPeriod->start_year;
+            //$where[$this->aliasField('end_year')] = $AcademicPeriod->start_year;
+            // if(($this->aliasField('end_year') != null || !empty($this->aliasField('end_year')  ){
+            //     $where[$this->aliasField('start_year')] = $AcademicPeriod->start_year;
+            // }else{
+            //     $where[$this->aliasField('start_year')] = $AcademicPeriod->start_year;
+            //     $where[$this->aliasField('end_year')] = $AcademicPeriod->start_year;
+            // }
+            
+        }
+        if ($statusFilter != 0) {
+            $where[$this->aliasField('InstitutionPositions.status_id')] <= $statusFilter; 
+        }
+        //$where[$this->aliasField('staff_status_id')] = $statusFilter; 
+        if ($areaId != -1) {
+            $where['Institutions.area_id'] = $areaId;
+        }
+
+
+        $query
+        ->SELECT ([
+           'area_code' =>'Areas.code',
+           'area_name' =>'Areas.name',
+           'institutions_code' =>'Institutions.code',
+           'institutions_name' =>'Institutions.name',
+           'institutions_id' =>'Institutions.id',
+           'staff_position_titles' =>'StaffPositionTitles.name',
+           'staff_position_grades' =>'StaffPositionGrades.name',
+           'staff_name' =>'Staffs.first_name',
+           'total_male' => "( SUM(CASE WHEN Staffs.gender_id = 1 THEN 1 ELSE 0 END) )",
+           'total_female' => "( SUM(CASE WHEN Staffs.gender_id = 2 THEN 1 ELSE 0 END) )",
+           'total' => "( SUM(CASE WHEN Staffs.gender_id in (1,2 ) THEN 1 ELSE 0 END) )",
+        ])
+        ->contain(['InstitutionPositions','InstitutionPositions.StaffPositionTitles','InstitutionPositions.StaffPositionGrades','Institutions.Areas','Staffs' ])
+        ->where([$where])
+        ->group(['Institutions.id','StaffPositionTitles.id','StaffPositionGrades.id'])
+        ->order(['Areas.name','Institutions.name','StaffPositionTitles.name','StaffPositionGrades.name']);
+        //echo "<pre>";print_r($query->sql());die;
+    }
+
+    public function onExcelUpdateFields(Event $event, ArrayObject $settings, $fields)
+    {
+
+        $newFields = [];
+
+        $newFields[] = [
+            'key' => 'Areas.code',
+            'field' => 'area_code',
+            'type' => 'string',
+            'label' => __('Area Code')
+        ];
+
+
+        $newFields[] = [
+            'key' => 'Areas.name',
+            'field' => 'area_name',
+            'type' => 'string',
+            'label' => __('Area Name')
+        ];
+
+        $newFields[] = [
+            'key' => 'Institutions.code',
+            'field' => 'institutions_code',
+            'type' => 'string',
+            'label' => __('Institution Code')
+        ];
+
+        $newFields[] = [
+            'key' => 'Institutions.name',
+            'field' => 'institutions_name',
+            'type' => 'string',
+            'label' => __('Institution Name')
+        ];
+
+        $newFields[] = [
+            'key' => 'StaffPositionTitles.name',
+            'field' => 'staff_position_titles',
+            'type' => 'string',
+            'label' => __('Title')
+        ];
+
+        $newFields[] = [
+            'key' => 'StaffPositionGrades.name',
+            'field' => 'staff_position_grades',
+            'type' => 'string',
+            'label' => __('Category')
+        ];
+
+
+        $newFields[] = [
+            'key' => '',
+            'field' => 'total_male',
+            'type' => 'string',
+            'label' => __('Total Male')
+        ];
+
+        $newFields[] = [
+            'key' => '',
+            'field' => 'total_female',
+            'type' => 'string',
+            'label' => __('Total Female')
+        ];
+
+        $newFields[] = [
+            'key' => '',
+            'field' => 'total',
+            'type' => 'string',
+            'label' => __('Total')
+        ];
+
+        $fields->exchangeArray($newFields);
+    }
+
 
 }
