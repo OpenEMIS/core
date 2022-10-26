acb74fd04bd668ba11fd2575d7a71ae77f3c7d6e
Ehteram Ahmad <ehteram333@gmail.com>
2022-10-26 12:14:06 +0530

    POCOR-6799: Migration correction STATUS:Done

--- config/Migrations/20221017090645_POCOR6799.php
+++ config/Migrations/20221017090645_POCOR6799.php
@@ -14,29 +14,6 @@ class POCOR6799 extends AbstractMigration
     public function up()
     {
 
-        // Backup table
-        $this->execute('CREATE TABLE `zz_6799_institution_staff_attendances` LIKE `institution_staff_attendances`');
-        $this->execute('INSERT INTO `zz_6799_institution_staff_attendances` SELECT * FROM `institution_staff_attendances`');
-
-        $this->execute('CREATE TABLE `zz_6799_institution_staff_leave` LIKE `institution_staff_leave`');
-        $this->execute('INSERT INTO `zz_6799_institution_staff_leave` SELECT * FROM `institution_staff_leave`');
-
-        $this->execute('CREATE TABLE `zz_6799_assessment_item_results` LIKE `assessment_item_results`');
-        $this->execute('INSERT INTO `zz_6799_assessment_item_results` SELECT * FROM `assessment_item_results`');
-
-        $this->execute('CREATE TABLE `zz_6799_institution_student_absences` LIKE `institution_student_absences`');
-        $this->execute('INSERT INTO `zz_6799_institution_student_absences` SELECT * FROM `institution_student_absences`');
-
-        $this->execute('CREATE TABLE `zz_6799_student_attendance_marked_records` LIKE `student_attendance_marked_records`');
-        $this->execute('INSERT INTO `zz_6799_student_attendance_marked_records` SELECT * FROM `student_attendance_marked_records`');
-
-        $this->execute('CREATE TABLE `zz_6799_student_attendance_mark_types` LIKE `student_attendance_mark_types`');
-        $this->execute('INSERT INTO `zz_6799_student_attendance_mark_types` SELECT * FROM `student_attendance_mark_types`');
-
-        $this->execute('CREATE TABLE `zz_6799_institution_student_absence_details` LIKE `institution_student_absence_details`');
-        $this->execute('INSERT INTO `zz_6799_institution_student_absence_details` SELECT * FROM `institution_student_absence_details`');
-        // End
-
         // Create Views
         $this->execute("CREATE OR REPLACE VIEW institution_staff_attendances_archive AS SELECT *    
         FROM institution_staff_attendances LIMIT 0");
@@ -63,25 +40,12 @@ class POCOR6799 extends AbstractMigration
     // rollback
     public function down()
     {
-        $this->execute('DROP TABLE IF EXISTS `institution_staff_attendances`');
-        $this->execute('RENAME TABLE `zz_R6799_institution_staff_attendances` TO `institution_staff_attendances`');
-
-        $this->execute('DROP TABLE IF EXISTS `institution_staff_leave`');
-        $this->execute('RENAME TABLE `zz_R6799_institution_staff_leave` TO `institution_staff_leave`');
-
-        $this->execute('DROP TABLE IF EXISTS `assessment_item_results`');
-        $this->execute('RENAME TABLE `zz_R6799_assessment_item_results` TO `assessment_item_results`');
-
-        $this->execute('DROP TABLE IF EXISTS `institution_student_absences`');
-        $this->execute('RENAME TABLE `zz_R6799_institution_student_absences` TO `institution_student_absences`');
-
-        $this->execute('DROP TABLE IF EXISTS `student_attendance_marked_records`');
-        $this->execute('RENAME TABLE `zz_R6799_student_attendance_marked_records` TO `student_attendance_marked_records`');
-
-        $this->execute('DROP TABLE IF EXISTS `student_attendance_mark_types`');
-        $this->execute('RENAME TABLE `zz_R6799_student_attendance_mark_types` TO `student_attendance_mark_types`');
-
-        $this->execute('DROP TABLE IF EXISTS `institution_student_absence_details`');
-        $this->execute('RENAME TABLE `zz_R6799_institution_student_absence_details` TO `institution_student_absence_details`');
+        $this->execute("DROP VIEW IF EXISTS institution_staff_attendances_archive");
+        $this->execute("DROP VIEW IF EXISTS institution_staff_leave_archived");
+        $this->execute("DROP VIEW IF EXISTS assessment_item_results_archived");
+        $this->execute("DROP VIEW IF EXISTS institution_student_absences_archived");
+        $this->execute("DROP VIEW IF EXISTS student_attendance_marked_records_archived");
+        $this->execute("DROP VIEW IF EXISTS student_attendance_mark_types_archived");
+        $this->execute("DROP VIEW IF EXISTS institution_student_absence_details_archived");
     }
 }
