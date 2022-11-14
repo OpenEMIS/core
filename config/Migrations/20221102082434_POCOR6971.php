bb7025abec008889ad0f4d4f125f578c07f9607c
Ehteram Ahmad <ehteram333@gmail.com>
2022-11-14 14:57:44 +0530

    POCOR-6971: Change in mogrations STATUS:Done

--- config/Migrations/20221102082434_POCOR6971.php
+++ config/Migrations/20221102082434_POCOR6971.php
@@ -13,11 +13,15 @@ class POCOR6971 extends AbstractMigration
     public function up()
     {
         // Backup table
-        $this->execute('CREATE TABLE `zz_6971_institution_staff_attendances` LIKE `institution_staff_attendances`');
-        $this->execute('INSERT INTO `zz_6971_institution_staff_attendances` SELECT * FROM `institution_staff_attendances`');
+        // $this->execute('CREATE TABLE `zz_6971_institution_staff_attendances` LIKE `institution_staff_attendances`');
+        // $this->execute('INSERT INTO `zz_6971_institution_staff_attendances` SELECT * FROM `institution_staff_attendances`');
         // End
 
-        $this->execute("ALTER TABLE `institution_staff_attendances` ADD `shift_id` INT NULL DEFAULT NULL AFTER `academic_period_id`");
+        $this->execute("ALTER TABLE `institution_staff_attendances` ADD `shift_id` INT NOT NULL DEFAULT 1 AFTER `academic_period_id`");
+
+        $this->execute("ALTER TABLE institution_staff_attendances DROP PRIMARY KEY");
+
+        $this->execute("ALTER TABLE institution_staff_attendances ADD PRIMARY KEY(staff_id,institution_id,academic_period_id,shift_id,`date`)");
     }
 
     // rollback
