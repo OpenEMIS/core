d091a8cbb900f266d66f671afab1c35c4c83bc91
divyaa <you@example.com>
2022-11-16 17:35:22 +0530

    POCOR-6971||wokring

--- config/Migrations/20221102082434_POCOR6971.php
+++ config/Migrations/20221102082434_POCOR6971.php
@@ -13,21 +13,21 @@ class POCOR6971 extends AbstractMigration
     public function up()
     {
         // Backup table
-        $this->execute('CREATE TABLE `zz_6971_institution_staff_attendances` LIKE `institution_staff_attendances`');
-        $this->execute('INSERT INTO `zz_6971_institution_staff_attendances` SELECT * FROM `institution_staff_attendances`');
+        $this->execute('CREATE TABLE `zz_6971_institution_positions` LIKE `institution_positions`');
+        $this->execute('INSERT INTO `zz_6971_institution_positions` SELECT * FROM `institution_positions`');
         // End
 
-        $this->execute("ALTER TABLE `institution_staff_attendances` ADD `shift_id` INT NOT NULL DEFAULT 1 AFTER `academic_period_id`");
+        $this->execute("ALTER TABLE `institution_positions` ADD `shift_id` INT NOT NULL DEFAULT 1 AFTER `assignee_id`");
 
-        $this->execute("ALTER TABLE institution_staff_attendances DROP PRIMARY KEY");
+        $this->execute("ALTER TABLE institution_positions DROP PRIMARY KEY");
 
-        $this->execute("ALTER TABLE institution_staff_attendances ADD PRIMARY KEY(staff_id,institution_id,academic_period_id,shift_id,`date`)");
+        $this->execute("ALTER TABLE institution_positions ADD PRIMARY KEY(status_id,staff_position_title_id,staff_position_grade_id,institution_id,assignee_id,shift_id,)");
     }
 
     // rollback
     public function down()
     {
-        $this->execute('DROP TABLE IF EXISTS `institution_staff_attendances`');
-        $this->execute('RENAME TABLE `zz_6971_institution_staff_attendances` TO `institution_staff_attendances`');
+        $this->execute('DROP TABLE IF EXISTS `institution_positions`');
+        $this->execute('RENAME TABLE `zz_6971_institution_positions` TO `institution_positions`');
     }
 }
