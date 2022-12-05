ab693601f918fe2b43328ed7d925e1f8673347f0
divyaa <you@example.com>
2022-12-05 22:27:01 +0530

    POCOR-7096

--- plugins/Institution/src/Model/Table/ReportCardStatusesTable.php
+++ plugins/Institution/src/Model/Table/ReportCardStatusesTable.php
@@ -160,7 +160,7 @@ class ReportCardStatusesTable extends ControllerActionTable
                 $SecurityRoleFunctionsTableDownloadExcelData = $SecurityRoleFunctionsTable
                 ->find()
                 ->where([
-                    $SecurityRoleFunctionsTable->aliasField('security_function_id') => $SecurityFunctionsDownloadExcelData->id])->orwhere([$SecurityRoleFunctionsTable->aliasField('security_role_id') => $SecurityGroupUsersData->security_role_id]) //POCOR-7060 //POCOR-7060
+                    $SecurityRoleFunctionsTable->aliasField('security_function_id') => $SecurityFunctionsDownloadExcelData->id])->orWhere([$SecurityRoleFunctionsTable->aliasField('security_role_id') => $SecurityGroupUsersData->security_role_id]) //POCOR-7060 //POCOR-7060
                 ->first();
                 
                 if ($this->AccessControl->isAdmin()) {
@@ -198,7 +198,7 @@ class ReportCardStatusesTable extends ControllerActionTable
                 ->find()
                 ->where([
                     $SecurityRoleFunctionsTable->aliasField('security_function_id') => $SecurityFunctionsDownloadPdfData->id])
-                    ->orwhere([
+                    ->orWhere([
                     $SecurityRoleFunctionsTable->aliasField('security_role_id') => $SecurityGroupUsersData->security_role_id  //POCOR-7060  //POCOR-7060
                 ])
                 ->first();
