0dcabbbe11174253d7a3e50b923e78370f48e54d
Rahul Singh <rahul.singh@valuecoders.com>
2022-12-05 14:25:18 +0530

    POCOR-7089:issue resolved|Status:Done

---
+++ config/Migrations/20221205064509_POCOR7089.php
@@ -0,0 +1,29 @@
+<?php
+use Migrations\AbstractMigration;
+
+class POCOR7089 extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function up()
+    {
+        // Creating backup
+        $this->execute('DROP TABLE IF EXISTS `zz_7089_security_functions`');
+        $this->execute('CREATE TABLE `zz_7089_security_functions` LIKE `security_functions`');
+        $this->execute('INSERT INTO `zz_7089_security_functions` SELECT * FROM `security_functions`');
+
+         $this->execute("UPDATE `security_functions` SET `_execute` = 'StudentReportCard.download' WHERE `name` = 'Report Cards (Excel)' AND `controller` = 'GuardianNavs' AND `module` = 'Guardian' AND `category` = 'Students - Academic'");
+    }
+
+    // Rollback
+    public function down()
+    {
+        $this->execute('DROP TABLE IF EXISTS `security_functions`');
+        $this->execute('RENAME TABLE `zz_7089_security_functions` TO `security_functions`');
+    }
+}
