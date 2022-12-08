7efff789199a66c6944e84c45884bbdc84308485
anubhav0789 <jainanubhav67@yahoo.in>
2022-12-08 14:13:51 +0530

    POCOR-7070: query enhancement in StudentAttendanceMarkedRecordsTable | Status: Done

--- plugins/Attendance/src/Model/Table/StudentAttendanceMarkedRecordsTable.php
+++ plugins/Attendance/src/Model/Table/StudentAttendanceMarkedRecordsTable.php
@@ -8,6 +8,7 @@ use Cake\ORM\Entity;
 use Cake\ORM\Query;
 use Cake\ORM\TableRegistry;
 use Cake\Datasource\ResultSetInterface;
+use Cake\Datasource\ConnectionManager;//POCOR-7023
 
 class StudentAttendanceMarkedRecordsTable extends AppTable
 {
@@ -24,6 +25,160 @@ class StudentAttendanceMarkedRecordsTable extends AppTable
             'StudentAttendances' => ['index', 'add', 'edit']
         ]);
     }
+
+    //POCOR-7023 starts
+    public function beforeSave(Event $event, Entity $entity, ArrayObject $options)
+    { 
+        $path_uri = '/restful/v2/Attendance-StudentAttendanceMarkedRecords.json';
+        if(is_int(strpos($_SERVER['REQUEST_URI'], $path_uri))){
+            $institution_id = $entity['institution_id'];
+            $academic_period_id = $entity['academic_period_id'];
+            $institution_class_id = $entity['institution_class_id'];
+            $education_grade_id = $entity['education_grade_id'];
+            $date = date('Y-m-d', strtotime($entity['date']));
+            $connection = ConnectionManager::get('default');
+            $statement = $connection->prepare("SELECT
+                            education_systems.academic_period_id,
+                            correct_grade.id AS correct_grade_id,
+                            student_attendance_marked_records.*
+                        FROM
+                            `student_attendance_marked_records`
+                        INNER JOIN education_grades wrong_grade ON
+                            wrong_grade.id = student_attendance_marked_records.education_grade_id
+                        INNER JOIN education_grades correct_grade ON
+                            correct_grade.code = wrong_grade.code
+                        INNER JOIN education_programmes ON correct_grade.education_programme_id = education_programmes.id
+                        INNER JOIN education_cycles ON education_programmes.education_cycle_id = education_cycles.id
+                        INNER JOIN education_levels ON education_cycles.education_level_id = education_levels.id
+                        INNER JOIN education_systems ON education_levels.education_system_id = education_systems.id AND education_systems.academic_period_id = student_attendance_marked_records.academic_period_id
+                        WHERE
+                            (correct_grade.id != student_attendance_marked_records.education_grade_id) AND student_attendance_marked_records.academic_period_id = ".$academic_period_id." Group by correct_grade_id LIMIT 1");
+            $statement->execute();
+            $row = $statement->fetchAll(\PDO::FETCH_ASSOC);
+            $StudentAttendanceMarkedRecords = TableRegistry::get('student_attendance_marked_records');
+            $studentMarkTypeStatusGrades = TableRegistry::get('student_mark_type_status_grades');
+            $studentMarkTypeStatuses = TableRegistry::get('student_mark_type_statuses');
+            $studentAttendanceMarkTypes = TableRegistry::get('student_attendance_mark_types');
+            $studentAttendanceTypes = TableRegistry::get('student_attendance_types');
+            if(!empty($row)){
+                $data = $StudentAttendanceMarkedRecords
+                        ->find()
+                        ->select([
+                            $StudentAttendanceMarkedRecords->aliasField('institution_id'),
+                            $StudentAttendanceMarkedRecords->aliasField('academic_period_id'),
+                            $StudentAttendanceMarkedRecords->aliasField('institution_class_id'),
+                            $StudentAttendanceMarkedRecords->aliasField('education_grade_id'),
+                            $StudentAttendanceMarkedRecords->aliasField('date'),
+                            $StudentAttendanceMarkedRecords->aliasField('period'),
+                            $StudentAttendanceMarkedRecords->aliasField('subject_id'),
+                            $studentMarkTypeStatusGrades->aliasField('education_grade_id'),
+                            $studentMarkTypeStatuses->aliasField('student_attendance_mark_type_id'),
+                            $studentAttendanceMarkTypes->aliasField('name'),
+                            $studentAttendanceTypes->aliasField('id'),
+                            $studentAttendanceTypes->aliasField('code'),
+                            $studentAttendanceTypes->aliasField('name')
+                        ])
+                        ->leftJoin([$studentMarkTypeStatusGrades->alias() => $studentMarkTypeStatusGrades->table()], [
+                            $studentMarkTypeStatusGrades->aliasField('education_grade_id = ') . $row[0]['correct_grade_id']
+                        ])
+                        ->leftJoin([$studentMarkTypeStatuses->alias() => $studentMarkTypeStatuses->table()], [
+                            $studentMarkTypeStatuses->aliasField('id = ') . $studentMarkTypeStatusGrades->aliasField('student_mark_type_status_id'),
+                            $studentMarkTypeStatuses->aliasField('academic_period_id = ') . $StudentAttendanceMarkedRecords->aliasField('academic_period_id')
+                        ])
+                        ->leftJoin([$studentAttendanceMarkTypes->alias() => $studentAttendanceMarkTypes->table()], [
+                            $studentAttendanceMarkTypes->aliasField('id = ') . $studentMarkTypeStatuses->aliasField('student_attendance_mark_type_id')
+                        ])
+                        ->leftJoin([$studentAttendanceTypes->alias() => $studentAttendanceTypes->table()], [
+                            $studentAttendanceTypes->aliasField('id = ') . $studentAttendanceMarkTypes->aliasField('student_attendance_type_id')
+                        ])
+                        ->where([
+                            $StudentAttendanceMarkedRecords->aliasField('institution_id') => $entity['institution_id'],
+                            $StudentAttendanceMarkedRecords->aliasField('academic_period_id') => $entity['academic_period_id'],
+                            $StudentAttendanceMarkedRecords->aliasField('institution_class_id') => $entity['institution_class_id'],
+                            $StudentAttendanceMarkedRecords->aliasField('education_grade_id') => $entity['education_grade_id']
+                                ])
+                        ->group([$StudentAttendanceMarkedRecords->aliasField('education_grade_id')])
+                        ->toArray();
+                if(empty($data)){
+                    $response = array('error'=> 'No record found for this request.');
+                    $entity->errors($response);    
+                    return false;
+                }else{
+                    if(!empty($data[0]->student_attendance_types) && $data[0]->student_attendance_types['code'] == 'DAY'){
+                        if(!empty($entity['subject_id'])){
+                            $response = array('error'=> 'The Education Grade for which you are trying to send the API attendance request is configured to mark attendance per Period. Please ensure that the subject_id parameter is equal to 0.');
+                            $entity->errors($response);    
+                            return false;
+                        }
+                    }else if(!empty($data[0]->student_attendance_types) && $data[0]->student_attendance_types['code'] == 'SUBJECT'){
+                        if(!empty($entity['period']) && $entity['period'] > 1){
+                            $response = array('error'=> 'The Education Grade for which you are trying to send the API attendance request is configured to mark attendance per Subject. Please ensure that the period_id parameter is equal to 1.');
+                            $entity->errors($response);    
+                            return false;
+                        }
+                    }
+                }
+            }else{
+                $data = $StudentAttendanceMarkedRecords
+                        ->find()
+                        ->select([
+                            $StudentAttendanceMarkedRecords->aliasField('institution_id'),
+                            $StudentAttendanceMarkedRecords->aliasField('academic_period_id'),
+                            $StudentAttendanceMarkedRecords->aliasField('institution_class_id'),
+                            $StudentAttendanceMarkedRecords->aliasField('education_grade_id'),
+                            $StudentAttendanceMarkedRecords->aliasField('date'),
+                            $StudentAttendanceMarkedRecords->aliasField('period'),
+                            $StudentAttendanceMarkedRecords->aliasField('subject_id'),
+                            $studentMarkTypeStatusGrades->aliasField('education_grade_id'),
+                            $studentMarkTypeStatuses->aliasField('student_attendance_mark_type_id'),
+                            $studentAttendanceMarkTypes->aliasField('name'),
+                            $studentAttendanceTypes->aliasField('id'),
+                            $studentAttendanceTypes->aliasField('code'),
+                            $studentAttendanceTypes->aliasField('name')
+                        ])
+                        ->leftJoin([$studentMarkTypeStatusGrades->alias() => $studentMarkTypeStatusGrades->table()], [
+                            $studentMarkTypeStatusGrades->aliasField('education_grade_id = ') . $education_grade_id
+                        ])
+                        ->leftJoin([$studentMarkTypeStatuses->alias() => $studentMarkTypeStatuses->table()], [
+                            $studentMarkTypeStatuses->aliasField('id = ') . $studentMarkTypeStatusGrades->aliasField('student_mark_type_status_id'),
+                            $studentMarkTypeStatuses->aliasField('academic_period_id = ') . $StudentAttendanceMarkedRecords->aliasField('academic_period_id')
+                        ])
+                        ->leftJoin([$studentAttendanceMarkTypes->alias() => $studentAttendanceMarkTypes->table()], [
+                            $studentAttendanceMarkTypes->aliasField('id = ') . $studentMarkTypeStatuses->aliasField('student_attendance_mark_type_id')
+                        ])
+                        ->leftJoin([$studentAttendanceTypes->alias() => $studentAttendanceTypes->table()], [
+                            $studentAttendanceTypes->aliasField('id = ') . $studentAttendanceMarkTypes->aliasField('student_attendance_type_id')
+                        ])
+                        ->where([
+                            $StudentAttendanceMarkedRecords->aliasField('institution_id') => $entity['institution_id'],
+                            $StudentAttendanceMarkedRecords->aliasField('academic_period_id') => $entity['academic_period_id'],
+                            $StudentAttendanceMarkedRecords->aliasField('institution_class_id') => $entity['institution_class_id'],
+                            $StudentAttendanceMarkedRecords->aliasField('education_grade_id') => $entity['education_grade_id']
+                                ])
+                        ->group([$StudentAttendanceMarkedRecords->aliasField('education_grade_id')])
+                        ->toArray();
+                if(empty($data)){
+                    $response = array('error'=> 'No record found for this request.');
+                    $entity->errors($response);    
+                    return false;
+                }else{
+                    if(!empty($data[0]->student_attendance_types) && $data[0]->student_attendance_types['code'] == 'DAY'){
+                        if(!empty($entity['subject_id'])){
+                            $response = array('error'=> 'The Education Grade for which you are trying to send the API attendance request is configured to mark attendance per Period. Please ensure that the subject_id parameter is equal to 0.');
+                            $entity->errors($response);    
+                            return false;
+                        }
+                    }else if(!empty($data[0]->student_attendance_types) && $data[0]->student_attendance_types['code'] == 'SUBJECT'){
+                        if(!empty($entity['period']) && $entity['period'] > 1){
+                            $response = array('error'=> 'The Education Grade for which you are trying to send the API attendance request is configured to mark attendance per Subject. Please ensure that the period_id parameter is equal to 1.');
+                            $entity->errors($response);    
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+    }//POCOR-7023 ends
 
     public function findPeriodIsMarked(Query $query, array $options)
     {
