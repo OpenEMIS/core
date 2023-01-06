5db5c58096d1874d500e5d817180cad602fde5d8
anubhav0789 <jainanubhav67@yahoo.in>
2023-01-06 09:24:43 +0530

    POCOR-7156: change in migration file | Status: Done

--- config/Migrations/20230103064409_POCOR7156.php
+++ config/Migrations/20230103064409_POCOR7156.php
@@ -14,10 +14,10 @@ class POCOR7156 extends AbstractMigration
     {
         $this->execute('CREATE TABLE `z_7156_config_items` LIKE `config_items`');
         $this->execute('INSERT INTO `z_7156_config_items` SELECT * FROM `config_items`');
-        //Create table `security_user_otps`
-        $table = $this->table('security_user_otps', [
-                'collation' => 'utf8mb4_unicode_ci',
-                'comment' => 'This table will contain the otps of the security users when they do login by two factor authentication'
+        //Create table `security_user_otp`
+        $table = $this->table('security_user_otp', [
+                'collation' => 'utf8_general_ci',
+                'comment' => 'This table will contain the OTP of the security users when they do login by two-factor authentication'
             ]);
         $table->addColumn('security_user_id', 'integer', [
                 'default' => null,
@@ -35,12 +35,12 @@ class POCOR7156 extends AbstractMigration
             ])
             ->save();
 
-        $this->execute("ALTER TABLE `security_user_otps` ADD INDEX(`security_user_id`)");
+        $this->execute("ALTER TABLE `security_user_otp` ADD INDEX(`security_user_id`)");
         
         //INSERT data `Two Factor Authentication` in config_items table
         $this->execute("INSERT INTO `config_items` (`id`, `name`, `code`, `type`, `label`, `value`, `value_selection`, `default_value`, `editable`, `visible`, `field_type`, `option_type`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES (NULL, 'Two Factor Authentication (Email)', 'two_factor_authentication', 'Authentication', 'Two Factor Authentication', '1', '', '1', '1', '1', 'Dropdown', 'completeness', '1', '".date('Y-m-d H:i:s')."', '1', '".date('Y-m-d H:i:s')."')"); 
         //INSERT data in report_queries table for cron (Karl Provided)
-        $this->execute("INSERT INTO `report_queries` (`name`, `query_sql`, `frequency`, `status`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES ('clear_otp', 'DELETE FROM security_user_otps WHERE created < DATE_SUB(NOW(), INTERVAL `1` HOUR);', 'hour', 1, NULL, NULL, 1, '".date('Y-m-d H:i:s')."')");    
+        $this->execute("INSERT INTO `report_queries` (`name`, `query_sql`, `frequency`, `status`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES ('clear_otp', 'DELETE FROM security_user_otp WHERE created < DATE_SUB(NOW(), INTERVAL `1` HOUR);', 'hour', 1, NULL, NULL, 1, '".date('Y-m-d H:i:s')."')");    
     }
 
     // rollback
@@ -48,6 +48,6 @@ class POCOR7156 extends AbstractMigration
     {
         $this->execute('DROP TABLE IF EXISTS `config_items`');
         $this->execute('RENAME TABLE `z_7156_config_items` TO `config_items`');
-        $this->execute('DROP TABLE security_user_otps');
+        $this->execute('DROP TABLE security_user_otp');
     }
 }
