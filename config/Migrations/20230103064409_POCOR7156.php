d19d641c6c3724dea194ecbfe01f01213bb3f5dc
anubhav0789 <jainanubhav67@yahoo.in>
2023-01-06 12:11:27 +0530

    POCOR-7156: changes in migration file and usersController | Status: Done

--- config/Migrations/20230103064409_POCOR7156.php
+++ config/Migrations/20230103064409_POCOR7156.php
@@ -17,7 +17,7 @@ class POCOR7156 extends AbstractMigration
         //Create table `security_user_codes`
         $table = $this->table('security_user_codes', [
                 'collation' => 'utf8_general_ci',
-                'comment' =>  'This table will contain the OTP of the security users when login by two-factor authentication is enabled'
+                'comment' =>  'This table will contain the OTP of the security users when login by two-factor authentication is enabled'•
             ]);
         $table->addColumn('security_user_id', 'integer', [
                 'default' => null,
@@ -38,7 +38,7 @@ class POCOR7156 extends AbstractMigration
         $this->execute("ALTER TABLE `security_user_codes` ADD INDEX(`security_user_id`)");
         
         //INSERT data `Two Factor Authentication` in config_items table
-        $this->execute("INSERT INTO `config_items` (`id`, `name`, `code`, `type`, `label`, `value`, `value_selection`, `default_value`, `editable`, `visible`, `field_type`, `option_type`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES (NULL, 'Two Factor Authentication (Email)', 'two_factor_authentication', 'Authentication', 'Two Factor Authentication', '1', '', '0', '1', '1', 'Dropdown', 'completeness', '1', '".date('Y-m-d H:i:s')."', '1', '".date('Y-m-d H:i:s')."')"); 
+        $this->execute("INSERT INTO `config_items` (`id`, `name`, `code`, `type`, `label`, `value`, `value_selection`, `default_value`, `editable`, `visible`, `field_type`, `option_type`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES (NULL, 'Two Factor Authentication (Email)', 'two_factor_authentication', 'Authentication', 'Two Factor Authentication', '0', '', '0', '1', '1', 'Dropdown', 'completeness', '1', '".date('Y-m-d H:i:s')."', '1', '".date('Y-m-d H:i:s')."')"); 
         //INSERT data in report_queries table for cron (Karl Provided)
         $this->execute("INSERT INTO `report_queries` (`name`, `query_sql`, `frequency`, `status`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES ('clear_otp', 'DELETE FROM security_user_codes WHERE created < DATE_SUB(NOW(), INTERVAL `1` HOUR);', 'hour', 1, NULL, NULL, 1, '".date('Y-m-d H:i:s')."')");    
     }
