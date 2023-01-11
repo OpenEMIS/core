2df262303361bfadd14ac20394db31b96c7ababe
Oleksii Zirka <ozirka@kordit.com>
2023-01-11 02:08:36 +0000

    20230106054622_POCOR7165.php edited online with Bitbucket

--- config/Migrations/20230106054622_POCOR7165.php
+++ config/Migrations/20230106054622_POCOR7165.php
@@ -13,15 +13,17 @@ class POCOR7165 extends AbstractMigration
     public function up()
     {
         // Backup tables
-        // $this->execute('CREATE TABLE `zz_7165_institution_student_absences` LIKE `institution_student_absences`');
-        // $this->execute('INSERT INTO `zz_7165_institution_student_absences` SELECT * FROM `institution_student_absences`');
+        $this->execute('CREATE TABLE `zz_7165_institution_student_absences` LIKE `institution_student_absences`');
+        $this->execute('INSERT INTO `zz_7165_institution_student_absences` SELECT * FROM `institution_student_absences`');
 
-        // $this->execute('CREATE TABLE `zz_7165_institution_student_absence_days` LIKE `institution_student_absence_days`');
-        // $this->execute('INSERT INTO `zz_7165_institution_student_absence_days` SELECT * FROM `institution_student_absence_days`');
+        $this->execute('CREATE TABLE `zz_7165_institution_student_absence_days` LIKE `institution_student_absence_days`');
+        $this->execute('INSERT INTO `zz_7165_institution_student_absence_days` SELECT * FROM `institution_student_absence_days`');
 
-        // $this->execute('CREATE TABLE `zz_7165_institution_student_absence_details` LIKE `institution_student_absence_details`');
-        // $this->execute('INSERT INTO `zz_7165_institution_student_absence_details` SELECT * FROM `institution_student_absence_details`');
+        $this->execute('CREATE TABLE `zz_7165_institution_student_absence_details` LIKE `institution_student_absence_details`');
+        $this->execute('INSERT INTO `zz_7165_institution_student_absence_details` SELECT * FROM `institution_student_absence_details`');
 
+        // Set foreign key checks to disabled
+        $this->execute('SET FOREIGN_KEY_CHECKS=0;');
 
         // Remove records that have absence_type_id = 0
         $this->execute('DELETE FROM institution_student_absences WHERE institution_student_absences.absence_type_id = 0');
@@ -29,7 +31,7 @@ class POCOR7165 extends AbstractMigration
         $this->execute('DELETE FROM institution_student_absence_details WHERE institution_student_absence_details.absence_type_id = 0');
 
 
-        $this->execute('SET FOREIGN_KEY_CHECKS=0;');
+
         $this->execute("ALTER TABLE institution_student_absence_details ADD CONSTRAINT `insti_stude_absen_detai_fk_abs_typ_id` FOREIGN KEY (`absence_type_id`) REFERENCES absence_types(`id`)");
         $this->execute("ALTER TABLE institution_student_absence_details ADD CONSTRAINT `insti_stude_absen_detai_fk_stude_absen_reaso_id` FOREIGN KEY (`student_absence_reason_id`) REFERENCES student_absence_reasons(`id`)");
         $this->execute('SET SESSION FOREIGN_KEY_CHECKS=1;');
