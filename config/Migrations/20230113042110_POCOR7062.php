c281d46add8229fb7098581ec3053291825eb9c7
nuzhat_vinove <nuzhat.khan@mail.vinove.com>
2023-01-16 15:02:44 +0530

    POCOR-7062|migration script|status:readytodeploy

--- config/Migrations/20230113042110_POCOR7062.php
+++ config/Migrations/20230113042110_POCOR7062.php
@@ -20,7 +20,9 @@ class POCOR7062 extends AbstractMigration
         $this->execute('INSERT INTO `zz_7062_user_special_needs_diagnostics` SELECT * FROM `user_special_needs_diagnostics`');
 
         //Drop foreign key
+        $this->execute('SET FOREIGN_KEY_CHECKS=0;');
         $this->execute('ALTER TABLE user_special_needs_diagnostics DROP FOREIGN KEY IF EXISTS special_needs_diagnostics_degree_id;');
+        
 
         //Add foreign key
         $this->execute('ALTER TABLE `user_special_needs_diagnostics` ADD FOREIGN KEY (`special_needs_diagnostics_degree_id`) REFERENCES `special_needs_diagnostics_degree`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT');
@@ -30,14 +32,17 @@ class POCOR7062 extends AbstractMigration
 
         //Add foreign key
         $this->execute('ALTER TABLE `user_special_needs_diagnostics` ADD FOREIGN KEY (`special_needs_diagnostics_type_id`) REFERENCES `special_needs_diagnostics_types`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT');
+        $this->execute('SET SESSION FOREIGN_KEY_CHECKS=1;');
     }
 
     public function down()
     {
+        $this->execute('SET FOREIGN_KEY_CHECKS=0;');
         $this->execute('DROP TABLE IF EXISTS `special_needs_diagnostics_degree`');
         $this->execute('RENAME TABLE `zz_7062_special_needs_diagnostics_degree` TO `special_needs_diagnostics_degree`');
 
         $this->execute('DROP TABLE IF EXISTS `user_special_needs_diagnostics`');
         $this->execute('RENAME TABLE `zz_7062_user_special_needs_diagnostics` TO `user_special_needs_diagnostics`');
+        $this->execute('SET SESSION FOREIGN_KEY_CHECKS=1;');
     }
 }
