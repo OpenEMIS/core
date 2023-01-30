d3abf7e4df32402b738474f09794241b769337b2
vinoverishabh <rishabh.sharma1@mail.valuecoders.com>
2023-01-30 12:21:18 +0530

    POCOR-5070 Migration script modified Status:done

--- config/Migrations/20230116114052_POCOR5070.php
+++ config/Migrations/20230116114052_POCOR5070.php
@@ -1,5 +1,6 @@
 <?php
 use Migrations\AbstractMigration;
+use Cake\ORM\TableRegistry;
 
 class POCOR5070 extends AbstractMigration
 {
@@ -19,8 +20,22 @@ class POCOR5070 extends AbstractMigration
         $this->execute('CREATE TABLE `z_5070_institution_positions` LIKE `institution_positions`');
         $this->execute('INSERT INTO `z_5070_institution_positions` SELECT * FROM `institution_positions`');
         // End
-        $this->execute("ALTER TABLE `institution_positions` DROP `is_homeroom`");
+        
         $this->execute("ALTER TABLE `institution_staff` ADD `is_homeroom` TINYINT(1) NOT NULL DEFAULT '0' AFTER `institution_id`");
+        //insrt data for is_homeroom from institution_positions to institution_staff table
+        $staff = TableRegistry::get('institution_staff');
+        $position = TableRegistry::get('institution_positions');
+        $institutionStaff = $staff->find('all')->toArray();
+        foreach($institutionStaff as $key=> $insStaff){
+            $staffPosition = $position->find('all',['conditions'=>['id'=> $insStaff->institution_position_id]])->first();
+            $staffPositionHomeroom = $staffPosition->is_homeroom;
+            $insStaffId = $insStaff->id;
+            $this->execute("UPDATE `institution_staff` SET `is_homeroom` = $staffPositionHomeroom WHERE `id`= $insStaffId");
+        }
+        //end query
+
+        $this->execute("ALTER TABLE `institution_positions` DROP `is_homeroom`");
+        
     }
 
     public function down()
