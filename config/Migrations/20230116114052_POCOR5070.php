459a8b79f64cb1f5b50044764115a726ae8aae87
vinoverishabh <rishabh.sharma1@mail.valuecoders.com>
2023-01-31 08:05:06 +0530

    POCOR-5070 Migration modified Status:done

--- config/Migrations/20230116114052_POCOR5070.php
+++ config/Migrations/20230116114052_POCOR5070.php
@@ -23,15 +23,7 @@ class POCOR5070 extends AbstractMigration
         
         $this->execute("ALTER TABLE `institution_staff` ADD `is_homeroom` TINYINT(1) NOT NULL DEFAULT '0' AFTER `institution_id`");
         //insrt data for is_homeroom from institution_positions to institution_staff table
-        $staff = TableRegistry::get('institution_staff');
-        $position = TableRegistry::get('institution_positions');
-        $institutionStaff = $staff->find('all')->toArray();
-        foreach($institutionStaff as $key=> $insStaff){
-            $staffPosition = $position->find('all',['conditions'=>['id'=> $insStaff->institution_position_id]])->first();
-            $staffPositionHomeroom = $staffPosition->is_homeroom;
-            $insStaffId = $insStaff->id;
-            $this->execute("UPDATE `institution_staff` SET `is_homeroom` = $staffPositionHomeroom WHERE `id`= $insStaffId");
-        }
+        $this->execute("UPDATE institution_staff INNER JOIN institution_positions ON institution_positions.id = institution_staff.institution_position_id SET institution_staff.is_homeroom = institution_positions.is_homeroom;");
         //end query
 
         $this->execute("ALTER TABLE `institution_positions` DROP `is_homeroom`");
