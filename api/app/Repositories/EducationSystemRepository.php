325cee405a0ebef123af9196f3ec54574814dcbb
Ravi Verma <ravi.verma@mail.valuecoders.com>
2023-02-08 16:52:58 +0530

    Worked on POCOR-7173-BE

--- api/app/Repositories/EducationSystemRepository.php
+++ api/app/Repositories/EducationSystemRepository.php
@@ -138,8 +138,6 @@ class EducationSystemRepository extends Controller
                 $systems = $systems->orderBy($col);
             }
 
-            /*$sql = $systems->toSql();
-            dd($sql);*/
             $list = $systems->paginate($limit)->toArray();
             return $list;
 
@@ -154,5 +152,281 @@ class EducationSystemRepository extends Controller
     }
 
 
+    public function getEducationStructureCycle($systemId, $levelId, $cycleId, $request)
+    {
+        try {
+            $params = $request->all();
+
+            $limit = config('constants.defaultPaginateLimit');
+
+            if(isset($params['limit'])){
+                $limit = $params['limit'];
+            }
+
+            $systems = EducationSystem::with([
+                'levels' => function ($q) use ($levelId, $cycleId) {
+                    $q->where('id', $levelId)
+                        ->with([
+                            'cycles' =>function ($q) use ($cycleId){
+                                $q->where('id', $cycleId)
+                                ->with( 
+                                    'programmes', 
+                                    'programmes.grades', 
+                                    'programmes.grades.subjects'
+                                );
+                            }
+                        ]);
+                        
+                }
+            ])
+            ->where('id', $systemId)
+            ->whereHas('levels', function ($q) use ($levelId) {
+                $q->where('education_levels.id', $levelId);
+            })
+            ->whereHas('levels.cycles', function ($q) use ($cycleId) {
+                $q->where('education_cycles.id', $cycleId);
+            });
+
+            if(isset($params['academic_period_id'])){
+                $systems = $systems->where('academic_period_id', $params['academic_period_id']);
+            }
+
+            if(isset($params['order'])){
+                $col = $params['order'];
+                $systems = $systems->orderBy($col);
+            }
+
+            $list = $systems->paginate($limit)->toArray();
+            return $list;
+
+        } catch (\Exception $e) {
+            Log::error(
+                'Failed to fetch list from DB',
+                ['message'=> $e->getMessage(), 'trace' => $e->getTraceAsString()]
+            );
+
+            return $this->sendErrorResponse('Education System List Not Found');
+        }
+    }
+
+
+    public function getEducationStructureProgramme($systemId, $levelId, $cycleId, $programmeId, $request)
+    {
+        try {
+            $params = $request->all();
+            
+            $limit = config('constants.defaultPaginateLimit');
+
+            if(isset($params['limit'])){
+                $limit = $params['limit'];
+            }
+
+            $systems = EducationSystem::with([
+                'levels' => function ($q) use ($levelId, $cycleId, $programmeId) {
+                    $q->where('id', $levelId)
+                    ->with([
+                        'cycles' =>function ($q) use ($levelId, $cycleId, $programmeId)
+                        {
+                            $q->where('id', $cycleId)
+                            ->with([
+                                'programmes' => function ($q) use ($levelId, $cycleId, $programmeId){
+                                    $q->where('id', $programmeId)
+                                    ->with( 
+                                        'grades', 
+                                        'grades.subjects'
+                                    );
+                                }
+                            ]); 
+                        }
+                    ]);    
+                }
+            ])
+            ->where('id', $systemId)
+            ->whereHas('levels', function ($q) use ($levelId) {
+                $q->where('education_levels.id', $levelId);
+            })
+            ->whereHas('levels.cycles', function ($q) use ($cycleId) {
+                $q->where('education_cycles.id', $cycleId);
+            })
+            ->whereHas('levels.cycles.programmes', function ($q) use ($programmeId) {
+                $q->where('education_programmes.id', $programmeId);
+            });
+
+            if(isset($params['academic_period_id'])){
+                $systems = $systems->where('academic_period_id', $params['academic_period_id']);
+            }
+
+            if(isset($params['order'])){
+                $col = $params['order'];
+                $systems = $systems->orderBy($col);
+            }
+
+            $list = $systems->paginate($limit)->toArray();
+            return $list;
+
+        } catch (\Exception $e) {
+            Log::error(
+                'Failed to fetch list from DB',
+                ['message'=> $e->getMessage(), 'trace' => $e->getTraceAsString()]
+            );
+
+            return $this->sendErrorResponse('Education System List Not Found');
+        }
+    }
+
+
+
+    public function getEducationStructureGrade($systemId, $levelId, $cycleId, $programmeId, $gradeId, $request)
+    {
+        try {
+            $params = $request->all();
+            
+            $limit = config('constants.defaultPaginateLimit');
+
+            if(isset($params['limit'])){
+                $limit = $params['limit'];
+            }
+
+            $systems = EducationSystem::with([
+                'levels' => function ($q) use ($levelId, $cycleId, $programmeId, $gradeId) {
+                    $q->where('id', $levelId)
+                    ->with([
+                        'cycles' =>function ($q) use ($levelId, $cycleId, $programmeId, $gradeId)
+                        {
+                            $q->where('id', $cycleId)
+                            ->with([
+                                'programmes' => function ($q) use ($levelId, $cycleId, $programmeId, $gradeId){
+                                    $q->where('id', $programmeId)
+                                    ->with([
+                                        'grades' => function ($q) use ($levelId, $cycleId, $programmeId, $gradeId){
+                                            $q->where('id', $gradeId)
+                                            ->with( 
+                                                'subjects'
+                                            );
+                                        }
+                                    ]);
+                                }
+                            ]); 
+                        }
+                    ]);    
+                }
+            ])
+            ->where('id', $systemId)
+            ->whereHas('levels', function ($q) use ($levelId) {
+                $q->where('education_levels.id', $levelId);
+            })
+            ->whereHas('levels.cycles', function ($q) use ($cycleId) {
+                $q->where('education_cycles.id', $cycleId);
+            })
+            ->whereHas('levels.cycles.programmes', function ($q) use ($programmeId) {
+                $q->where('education_programmes.id', $programmeId);
+            })
+            ->whereHas('levels.cycles.programmes.grades', function ($q) use ($gradeId) 
+            {
+                $q->where('education_grades.id', $gradeId);
+            });
+
+            if(isset($params['academic_period_id'])){
+                $systems = $systems->where('academic_period_id', $params['academic_period_id']);
+            }
+
+            if(isset($params['order'])){
+                $col = $params['order'];
+                $systems = $systems->orderBy($col);
+            }
+
+            $list = $systems->paginate($limit)->toArray();
+            return $list;
+
+        } catch (\Exception $e) {
+            dd($e);
+            Log::error(
+                'Failed to fetch list from DB',
+                ['message'=> $e->getMessage(), 'trace' => $e->getTraceAsString()]
+            );
+
+            return $this->sendErrorResponse('Education System List Not Found');
+        }
+    }
+
+
+
+    public function getEducationStructureSubject($systemId, $levelId, $cycleId, $programmeId, $gradeId, $subjectId, $request)
+    {
+        try {
+            $params = $request->all();
+            
+            $limit = config('constants.defaultPaginateLimit');
+
+            if(isset($params['limit'])){
+                $limit = $params['limit'];
+            }
+
+            $systems = EducationSystem::with([
+                'levels' => function ($q) use ($levelId, $cycleId, $programmeId, $gradeId, $subjectId) {
+                    $q->where('id', $levelId)
+                    ->with([
+                        'cycles' =>function ($q) use ($levelId, $cycleId, $programmeId, $gradeId, $subjectId)
+                        {
+                            $q->where('id', $cycleId)
+                            ->with([
+                                'programmes' => function ($q) use ($levelId, $cycleId, $programmeId, $gradeId, $subjectId){
+                                    $q->where('id', $programmeId)
+                                    ->with([
+                                        'grades' => function ($q) use ($levelId, $cycleId, $programmeId, $gradeId, $subjectId){
+                                            $q->where('id', $gradeId)
+                                            ->with([ 
+                                                'subjects' => function ($q) use ($levelId, $cycleId, $programmeId, $gradeId, $subjectId){
+                                                    $q->where('education_grades_subjects.education_subject_id', $subjectId);
+                                                }
+                                            ]);
+                                        }
+                                    ]);
+                                }
+                            ]); 
+                        }
+                    ]);    
+                }
+            ])
+            ->where('id', $systemId)
+            ->whereHas('levels', function ($q) use ($levelId) {
+                $q->where('education_levels.id', $levelId);
+            })
+            ->whereHas('levels.cycles', function ($q) use ($cycleId) {
+                $q->where('education_cycles.id', $cycleId);
+            })
+            ->whereHas('levels.cycles.programmes', function ($q) use ($programmeId) {
+                $q->where('education_programmes.id', $programmeId);
+            })
+            ->whereHas('levels.cycles.programmes.grades', function ($q) use ($gradeId) 
+            {
+                $q->where('education_grades.id', $gradeId);
+            })
+            ->whereHas('levels.cycles.programmes.grades.subjects', function ($q) use ($subjectId) 
+            {
+                $q->where('education_subjects.id', $subjectId);
+            });
+
+            if(isset($params['academic_period_id'])){
+                $systems = $systems->where('academic_period_id', $params['academic_period_id']);
+            }
+
+            if(isset($params['order'])){
+                $col = $params['order'];
+                $systems = $systems->orderBy($col);
+            }
+
+            $list = $systems->paginate($limit)->toArray();
+            return $list;
+
+        } catch (\Exception $e) {
+            Log::error(
+                'Failed to fetch list from DB',
+                ['message'=> $e->getMessage(), 'trace' => $e->getTraceAsString()]
+            );
+
+            return $this->sendErrorResponse('Education System List Not Found');
+        }
+    }
 }
 
