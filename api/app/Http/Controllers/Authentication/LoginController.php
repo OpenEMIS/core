733fea7fc2f1df7b6e9644324658d7f12744cd26
Ravi Verma <ravi.verma@mail.valuecoders.com>
2023-02-17 08:31:24 +0530

    Worked on POCOR-7240

--- api/app/Http/Controllers/Authentication/LoginController.php
+++ api/app/Http/Controllers/Authentication/LoginController.php
@@ -8,6 +8,7 @@ use DB;
 use Carbon\Carbon;
 use Hash; 
 use App\Models\SecurityUsers;
+use App\Models\ApiCredentials;
 use JWTAuth;
 use Illuminate\Support\Facades\Log;
 
@@ -22,26 +23,33 @@ class LoginController extends Controller
             if($userCheck->super_admin == config('constants.canLogIn.superAdmin') || $userCheck->is_staff == config('constants.canLogIn.isStaff') || $userCheck->is_student == 1){
                 $input = $request->only('username', 'password');
                 $token = null;
+                
+                $apiCredentials = ApiCredentials::where('public_key', $api_key)->first();
+                if(!$apiCredentials){
+                    return $this->sendErrorResponse("Invalid API key provided.");
+                }
 
-            if (!$token = JWTAuth::attempt($input)) {
-                return $this->sendErrorResponse('Invalid Email or Password');
-            }
-            
-            return $this->sendSuccessResponse('Logged In successfully', ['token' => $token]); 
+
+                if (!$token = JWTAuth::attempt($input)) {
+                    return $this->sendErrorResponse('Invalid Email or Password');
+                }
+                $api_key = $request->api_key??"";
+
+                
+                return $this->sendSuccessResponse('Logged In successfully', ['token' => $token]); 
             } else {
                 return $this->sendErrorResponse("You Are Not Authorized To Access This Page");
             }
                 
             
         } catch (\Exception $e) {
-            dd($e);
-                Log::error(
-                    'Failed to fetch list from DB',
-                    ['message' => $e->getMessage(), 'trace' => $e->getTraceAsString()]
-                );
+            Log::error(
+                'Failed to fetch list from DB',
+                ['message' => $e->getMessage(), 'trace' => $e->getTraceAsString()]
+            );
 
-                return $this->sendErrorResponse("You Are Not Authorized To Access This Page");
-            }
+            return $this->sendErrorResponse("You Are Not Authorized To Access This Page");
+        }
         
     }
 }
