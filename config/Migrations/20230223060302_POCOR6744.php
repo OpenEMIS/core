d003b23148fb969d581264023664424f05041b27
anubhav0789 <jainanubhav67@yahoo.in>
2023-02-23 12:04:31 +0530

    POCOR-6744: add foreign key related checks | Status: Done

--- config/Migrations/20230223060302_POCOR6744.php
+++ config/Migrations/20230223060302_POCOR6744.php
@@ -19,10 +19,16 @@ class POCOR6744 extends AbstractMigration
         $this->execute('CREATE TABLE `z_6744_institution_report_card_processes` LIKE `institution_report_card_processes`');
         $this->execute('INSERT INTO `z_6744_institution_report_card_processes` SELECT * FROM `institution_report_card_processes`'); 
 
+        // Set foreign key checks to disabled
+        $this->execute('SET FOREIGN_KEY_CHECKS=0;');
+
         //alter foreign keys
         $this->execute("ALTER TABLE `institution_report_cards` DROP FOREIGN KEY `insti_repor_cards_fk_repor_card_id`; ALTER TABLE `institution_report_cards` ADD CONSTRAINT `insti_repor_cards_fk_repor_card_id` FOREIGN KEY (`report_card_id`) REFERENCES `openemis_core`.`profile_templates`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT");
 
         $this->execute("ALTER TABLE `institution_report_card_processes` DROP FOREIGN KEY `insti_repor_card_proce_fk_repor_card_id`; ALTER TABLE `institution_report_card_processes` ADD CONSTRAINT `insti_repor_card_proce_fk_repor_card_id` FOREIGN KEY (`report_card_id`) REFERENCES `openemis_core`.`profile_templates`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT");
+        
+        // Set foreign key checks to enabled
+        $this->execute('SET SESSION FOREIGN_KEY_CHECKS=1;');
     }
 
     //rollback
