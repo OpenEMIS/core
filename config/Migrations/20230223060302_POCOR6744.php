c4ef1e061c3ee12e89e9d2b0805308d1083b6245
anubhav0789 <jainanubhav67@yahoo.in>
2023-02-23 11:39:03 +0530

    POCOR-6744: create migration file | Status: Done

---
+++ config/Migrations/20230223060302_POCOR6744.php
@@ -0,0 +1,37 @@
+<?php
+use Migrations\AbstractMigration;
+
+class POCOR6744 extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function up()
+    {
+        //backup
+        $this->execute('CREATE TABLE `z_6744_institution_report_cards` LIKE `institution_report_cards`');
+        $this->execute('INSERT INTO `z_6744_institution_report_cards` SELECT * FROM `institution_report_cards`');
+
+        $this->execute('CREATE TABLE `z_6744_institution_report_card_processes` LIKE `institution_report_card_processes`');
+        $this->execute('INSERT INTO `z_6744_institution_report_card_processes` SELECT * FROM `institution_report_card_processes`'); 
+
+        //alter foreign keys
+        $this->execute("ALTER TABLE `institution_report_cards` DROP FOREIGN KEY `insti_repor_cards_fk_repor_card_id`; ALTER TABLE `institution_report_cards` ADD CONSTRAINT `insti_repor_cards_fk_repor_card_id` FOREIGN KEY (`report_card_id`) REFERENCES `openemis_core`.`profile_templates`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT");
+
+        $this->execute("ALTER TABLE `institution_report_card_processes` DROP FOREIGN KEY `insti_repor_card_proce_fk_repor_card_id`; ALTER TABLE `institution_report_card_processes` ADD CONSTRAINT `insti_repor_card_proce_fk_repor_card_id` FOREIGN KEY (`report_card_id`) REFERENCES `openemis_core`.`profile_templates`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT");
+    }
+
+    //rollback
+    public function down()
+    {
+        $this->execute('DROP TABLE IF EXISTS `institution_report_cards`');
+        $this->execute('RENAME TABLE `z_6744_institution_report_cards` TO `institution_report_cards`');
+
+        $this->execute('DROP TABLE IF EXISTS `institution_report_card_processes`');
+        $this->execute('RENAME TABLE `z_6744_institution_report_card_processes` TO `institution_report_card_processes`');
+    }
+}
