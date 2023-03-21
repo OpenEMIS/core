6d64be319ad42f3763378cdf56d0bea2b70f7e22
divyaa <you@example.com>
2023-03-21 14:37:01 +0530

    POCOR-6673||working

--- plugins/Institution/src/Model/Table/InstitutionCurricularsTable.php
+++ plugins/Institution/src/Model/Table/InstitutionCurricularsTable.php
@@ -33,6 +33,7 @@ class InstitutionCurricularsTable extends ControllerActionTable
         $this->belongsTo('CurricularTypes', ['className' => 'FieldOption.CurricularTypes']);
         
         $this->belongsTo('Institutions', ['className' => 'Institution.Institutions', 'foreignKey' => 'institution_id']);
+        $this->hasMany('InstitutionCurricularStaff', ['className' => 'Institution.InstitutionCurricularStaff', 'dependent' => true, 'cascadeCallbacks' => true]);
         $this->addBehavior('Excel', ['pages' => ['index','view']]);
     }
 
@@ -137,9 +138,68 @@ class InstitutionCurricularsTable extends ControllerActionTable
         $this->field('curricular_type_id', ['type' => 'select']);
         $this->field('category', ['type' => 'select']);
         $this->field('academic_period_id', ['type' => 'select']);
-        //$this->field('staff_id', ['type' => 'select']);
+       // $this->field('staff_id', ['type' => 'select']);
         $this->setFieldOrder([
             'academic_period_id','name','category', 'curricular_type_id']);
+        $entity->institution_curricular_id = $_SESSION['curricularId'];
+        $curricularStaff = TableRegistry::get('institution_curricular_staff');
+        $getStaff = $curricularStaff->find()->select(['staff_id'])
+                    ->where([$curricularStaff->aliasField('institution_curricular_id') => $entity->institution_curricular_id]);
+                    //print_r($getStaff->Sql());die;
+        $staff = [];
+        if(!empty($getStaff)){
+            foreach($getStaff as $value){
+                $staff[] = $value->staff_id; 
+            }
+        }
+
+        $InstitutionStaff = TableRegistry::get('institution_staff');
+        $Institutions = TableRegistry::get('Institution.Institutions');
+        $UserData = TableRegistry::get('User.Users');
+        $session = $this->controller->request->session();
+        $institutionId = $session->read('Institution.Institutions.id');
+        $this->InstitutionCurriculars = TableRegistry::get('institution_curriculars');
+        $AcademicPeriod = TableRegistry::get('AcademicPeriod.AcademicPeriods');
+        $curriculardecode = $entity->institution_curricular_id;
+        $selectedPeriod = $this->InstitutionCurriculars->get($curriculardecode)->academic_period_id;
+        $join = [];
+        $join[''] = [
+        'type' => 'inner',
+        'table' => "(SELECT institution_staff.staff_id user_id
+                        FROM institution_staff
+                        INNER JOIN academic_periods
+                        ON (((institution_staff.end_date IS NOT NULL AND institution_staff.start_date <= academic_periods.start_date AND institution_staff.end_date >= academic_periods.start_date) OR (institution_staff.end_date IS NOT NULL AND institution_staff.start_date <= academic_periods.end_date AND institution_staff.end_date >= academic_periods.end_date) OR (institution_staff.end_date IS NOT NULL AND institution_staff.start_date >= academic_periods.start_date AND institution_staff.end_date <= academic_periods.end_date)) OR (institution_staff.end_date IS NULL AND institution_staff.start_date <= academic_periods.end_date))
+                        WHERE academic_periods.id = $selectedPeriod
+                        AND institution_staff.institution_id = $institutionId
+                        AND institution_staff.staff_status_id = 1
+                        GROUP BY institution_staff.staff_id
+                            ) subq",
+                            'conditions' => ['subq.user_id = Users.id'],
+                ];
+        $requestorOptions = $UserData
+            ->find('list', [
+                'keyField' => 'id',
+                'valueField' => 'name_with_id'
+            ])
+            ->select([
+                    $UserData->aliasField('id'),
+                    $UserData->aliasField('openemis_no'),
+                    $UserData->aliasField('first_name'),
+                    $UserData->aliasField('middle_name'),
+                    $UserData->aliasField('third_name'),
+                    $UserData->aliasField('last_name')
+            ])->order([$UserData->aliasField('first_name'),]);
+
+          $data =   $requestorOptions->join($join)->toArray();
+        
+          $this->field('staff_id', [
+            'type' => 'chosenSelect',
+            'attr' => [
+                'label' => __('Staff')
+            ]
+        ]);
+        $this->fields['staff_id']['options'] = $data;
+        
     }
 
     public function onUpdateFieldCategory(Event $event, array $attr, $action, Request $request)
@@ -278,8 +338,8 @@ class InstitutionCurricularsTable extends ControllerActionTable
             $attr['placeholder'] = __('Select Staff');
         } 
         return $attr;
-    }*/
-
+    }
+*/
     public function editBeforeSave(Event $event, Entity $entity, ArrayObject $requestData, ArrayObject $options)
     {
         $entity->institution_curricular_id = $_SESSION['curricularId'];
@@ -380,7 +440,7 @@ class InstitutionCurricularsTable extends ControllerActionTable
         return $total;
     }
 
-    public function editAfterAction(Event $event, Entity $entity, ArrayObject $extra)
+    /*public function editBeforeAction(Event $event, Entity $entity, ArrayObject $extra)
     {
         $entity->institution_curricular_id = $_SESSION['curricularId'];
         $curricularStaff = TableRegistry::get('institution_curricular_staff');
@@ -436,12 +496,12 @@ class InstitutionCurricularsTable extends ControllerActionTable
           $this->field('staff_id', [
             'type' => 'chosenSelect',
             'attr' => [
-                'label' => __('Period')
+                'label' => __('Staff')
             ]
         ]);
         $this->fields['staff_id']['options'] = $data;
         
-    }
+    }*/
 
     /*public function addEditAfterAction(Event $event, Entity $entity, ArrayObject $extra)
     {
