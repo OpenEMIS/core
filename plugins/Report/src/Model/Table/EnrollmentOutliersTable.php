159485b17ef40461c0f7dcfb1ef577ba1764bf46
divyaa <you@example.com>
2023-03-21 17:24:46 +0530

    POCOR-7211||working

--- plugins/Report/src/Model/Table/EnrollmentOutliersTable.php
+++ plugins/Report/src/Model/Table/EnrollmentOutliersTable.php
@@ -2,18 +2,25 @@
 namespace Report\Model\Table;
 
 use ArrayObject;
+use DateTime;
+use Cake\ORM\Entity;
 use Cake\ORM\Query;
-use Cake\ORM\TableRegistry;
 use Cake\Event\Event;
 use Cake\Network\Request;
 use App\Model\Table\AppTable;
-use Cake\Log\Log;
-use Cake\ORM\Entity;
-use DateTime;
+use Cake\ORM\TableRegistry;
+use Cake\ORM\Table;
+use Cake\Utility\Inflector;
+use Cake\I18n\Time;
+use Cake\Validation\Validator;
+use Cake\Datasource\ConnectionManager;
+use App\Model\Traits\OptionsTrait;
 
 
 //POCOR-7211
 class EnrollmentOutliersTable extends AppTable  {
+	use OptionsTrait;
+
 	public function initialize(array $config) {
 		$this->table('institution_students');
 		parent::initialize($config);
@@ -39,58 +46,88 @@ class EnrollmentOutliersTable extends AppTable  {
 		return $attr;
 	}
 
-	public function onExcelBeforeQuery(Event $event, ArrayObject $settings, Query $query)
+	
+	public function onExcelBeforeQuery(Event $event, ArrayObject $settings, Query $query) 
 	{
-		//$requestData = json_decode($settings['process']['params']);
-        //$academicPeriodId = $requestData->academic_period_id;
+		$requestData = json_decode($settings['process']['params']);
+        $academicPeriodId = $requestData->academic_period_id;
         $academicPeriodId = 32;
-		//$conditions = [];
-		//$conditionQuery = [];
+		$conditions = [];
+		$conditionQuery = [];
 		$join = [];
         $institutionStudents = TableRegistry::get('institution_students');
         $academicPeriod = TableRegistry::get('academic_periods');
         $institutions = TableRegistry::get('institutions');
-       // $conditions[$institutionStudents->aliasField('academic_period_id')] = $academicPeriodId;
-        /*$conditions["IF((CURRENT_DATE >= academic_periods.start_date AND CURRENT_DATE <= academic_periods.end_date), 
-        institution_students.student_status_id = 1, institution_students.student_status_id IN (1, 7, 6, 8))"];*/
+        $conditions[$this->aliasField('academic_period_id')] = $academicPeriodId;
+        $connection = ConnectionManager::get('default');
+        $statement = $connection->prepare("SELECT main_query.academic_period_name AS 'Academic Period'
+    ,main_query.institution_code AS 'Institution Code'
+    ,main_query.institution_name AS 'Institution'
+    ,main_query.count_students AS 'Number of Students'
+						FROM
+						(
+						    SELECT academic_periods.name academic_period_name 	
+						        ,institutions.code institution_code			
+						        ,institutions.name institution_name			
+						        ,COUNT(DISTINCT(institution_students.student_id)) count_students
+						    FROM institution_students
+						    INNER JOIN academic_periods
+						    ON academic_periods.id = institution_students.academic_period_id
+						    INNER JOIN institutions
+						    ON institutions.id = institution_students.institution_id
+						    WHERE academic_periods.id = $academicPeriodId
+						    AND IF((CURRENT_DATE >= academic_periods.start_date AND CURRENT_DATE <= academic_periods.end_date), institution_students.student_status_id = 1, institution_students.student_status_id IN (1, 7, 6, 8))
+						    GROUP BY institutions.id
+						) main_query
+						CROSS JOIN
+						(
+						    SELECT @min_age := MAX(CASE WHEN config_items.code = 'report_outlier_min_age' THEN config_items.value ELSE 0 END) min_age
+						        ,@max_age := MAX(CASE WHEN config_items.code = 'report_outlier_max_age' THEN config_items.value ELSE 0 END) max_age
+						        ,@min_enrolment := MAX(CASE WHEN config_items.code = 'report_outlier_min_student' THEN config_items.value ELSE 0 END) min_enrolment
+						        ,@max_enrolment := MAX(CASE WHEN config_items.code = 'report_outlier_max_student' THEN config_items.value ELSE 0 END) max_enrolment
+						    FROM config_items
+						    WHERE config_items.code IN ('report_outlier_min_age', 'report_outlier_max_age', 
+						        'report_outlier_min_student', 'report_outlier_max_student')
+						) subq
+						WHERE main_query.count_students NOT BETWEEN subq.min_enrolment AND subq.max_enrolment");
+        $statement->execute();
+
+       $enroll = $statement->fetchAll(\PDO::FETCH_ASSOC);
+       /*$row = [];
+       foreach($enroll as $key => $value){
+       	$row['academic_period'] = $value['academic_period_name'];
+       	$row['institution_code'] = $value['institution_code'];
+       	$row['institution_name'] = $value['institution_name'];
+       	$row['total_students'] = $value['count_students'];
+       }*/
+       
         
-        //$conditionQuery['institution_students.count_students NOT BETWEEN subq.min_enrolment AND subq.max_enrolment'];
-        $main_query  = "(SELECT academic_periods.name academic_period_name 	
-				        ,institutions.code institution_code			
-				        ,institutions.name institution_name			
-				        ,COUNT(DISTINCT(institution_students.student_id)) count_students
-				    FROM institution_students
-				    INNER JOIN academic_periods
-				    ON academic_periods.id = institution_students.academic_period_id
-				    INNER JOIN institutions
-				    ON institutions.id = institution_students.institution_id
-				    WHERE academic_periods.id = $academicPeriodId
-				    AND IF((CURRENT_DATE >= academic_periods.start_date AND CURRENT_DATE <= academic_periods.end_date), institution_students.student_status_id = 1, institution_students.student_status_id IN (1, 7, 6, 8))
-				    GROUP BY institutions.id)";
-        $join['subq'] = [
-             'type' => 'cross',
-             'table' => "(SELECT @min_age := MAX(CASE WHEN config_items.code = 'report_outlier_min_age' THEN 					config_items.value ELSE 0 END) min_age
-       						 ,@max_age := MAX(CASE WHEN config_items.code = 'report_outlier_max_age' THEN config_items.value ELSE 0 END) max_age
-        					,@min_enrolment := MAX(CASE WHEN config_items.code = 'report_outlier_min_student' THEN config_items.value ELSE 0 END) min_enrolment
-        					,@max_enrolment := MAX(CASE WHEN config_items.code = 'report_outlier_max_student' THEN config_items.value ELSE 0 END) max_enrolment
-   							 FROM config_items
-   							 WHERE config_items.code IN ('report_outlier_min_age', 'report_outlier_max_age', 'report_outlier_min_student', 'report_outlier_max_student')",
-   						];
-		$query->from(['students_data' => $main_query]);
-                      
-        $query->join($join)->where(['institution_students.count_students NOT BETWEEN subq.min_enrolment AND subq.max_enrolment']);
-        print_r($query->Sql());die('kjkj');
 	}
+
 
 	public function onExcelUpdateFields(Event $event, ArrayObject $settings, $fields)
     {
+
     	$extraFields = [];
         $extraFields[] = [
-            'key' => 'HealthReports.code_name',
-            'field' => 'code_name',
-            'type' => 'string',
-            'label' => __('Code')
+            'key' => 'Academic Period',
+            'field' => 'Academic Period',
+            'type' => 'integer',
+            'label' => __('Academic Period')
         ];
+        /*$extraFields[] = [
+            'key' => '',
+            'field' => 'institution_code',
+            'type' => 'string',
+            'label' => __('Institution code')
+        ];
+        $extraFields[] = [
+            'key' => '',
+            'field' => 'institution_name',
+            'type' => 'string',
+            'label' => __('Institution Name')
+        ];*/
+        
 
         $fields->exchangeArray($extraFields);
     }
