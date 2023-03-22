68de45c7de411091384195942e60894fdd688202
Megha <megha.gupta@mail.valuecoders.com>
2023-03-22 17:13:16 +0530

    POCOR-7289| Handled Homeroom teacher UI| Status:In progress

--- config/Migrations/20231302066713_POCOR7236.php
+++ config/Migrations/20231302066713_POCOR7236.php
@@ -3,7 +3,7 @@
 use Phinx\Migration\AbstractMigration;
 
 /**
- * POCOR-7236 
+ * POCOR-7236
  * unique code validation
 **/
 class POCOR7236 extends AbstractMigration
@@ -12,18 +12,18 @@ class POCOR7236 extends AbstractMigration
     public function up()
     {
         // Backup table
-        $this->execute('CREATE TABLE `zz_7236_security_roles` LIKE `security_roles`');
-        $this->execute('INSERT INTO `zz_7236_security_roles` SELECT * FROM `security_roles`');
+        // $this->execute('CREATE TABLE `zz_7236_security_roles` LIKE `security_roles`');
+        // $this->execute('INSERT INTO `zz_7236_security_roles` SELECT * FROM `security_roles`');
 
-        $this->execute("ALTER TABLE `security_roles` ADD UNIQUE (`code`)");
+        // $this->execute("ALTER TABLE `security_roles` ADD UNIQUE (`code`)");
     }
 
     // rollback
     public function down()
     {
         // Restore table
-        $this->execute('DROP TABLE IF EXISTS `security_roles`');
-        $this->execute('RENAME TABLE `zz_7236_security_roles` TO `security_roles`');
+        // $this->execute('DROP TABLE IF EXISTS `security_roles`');
+        // $this->execute('RENAME TABLE `zz_7236_security_roles` TO `security_roles`');
     }
 }
 
