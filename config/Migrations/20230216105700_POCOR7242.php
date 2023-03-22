35004e414dd656af9f12f95e1ed557b47b551e31
Megha <megha.gupta@mail.valuecoders.com>
2023-03-22 10:45:56 +0530

    POCOR-7289| Changed migration name|Status:Inprogress

--- config/Migrations/20230216105700_POCOR7242.php
+++ config/Migrations/20230216105700_POCOR7242.php
@@ -13,19 +13,19 @@ class POCOR7242 extends AbstractMigration
     public function up()
     {
         // Backup table
-        $this->execute('CREATE TABLE `zz_7242_institution_staff_leave` LIKE `institution_staff_leave`');
-        $this->execute('INSERT INTO `zz_7242_institution_staff_leave` SELECT * FROM `institution_staff_leave`');
+        // $this->execute('CREATE TABLE `zz_7242_institution_staff_leave` LIKE `institution_staff_leave`');
+        // $this->execute('INSERT INTO `zz_7242_institution_staff_leave` SELECT * FROM `institution_staff_leave`');
 
 
-        // DROP foreign key relationship     
-        $this->execute("ALTER TABLE institution_staff_leave DROP FOREIGN KEY `insti_staff_leave_fk_ass_id`;");
-        
+        // DROP foreign key relationship
+        // $this->execute("ALTER TABLE institution_staff_leave DROP FOREIGN KEY `insti_staff_leave_fk_ass_id`;");
+
     }
-         
+
     // rollback
     public function down()
     {
-        
+
         // Restore table
         $this->execute('DROP TABLE IF EXISTS `institution_staff_leave`');
         $this->execute('RENAME TABLE `zz_7242_institution_staff_leave` TO `institution_staff_leave`');
