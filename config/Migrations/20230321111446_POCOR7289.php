1354103cab8dd5baabce08041bd7c4ad1d045b9e
Megha <megha.gupta@mail.valuecoders.com>
2023-03-22 10:45:56 +0530

    POCOR-7289| Changed migration name|Status:Inprogress

---
+++ config/Migrations/20230321111446_POCOR7289.php
@@ -0,0 +1,19 @@
+<?php
+use Phinx\Migration\AbstractMigration;
+  /**
+ * POCOR-7289
+ * adding homeroom teacher
+**/
+class POCOR7289 extends AbstractMigration
+{
+
+    public function up()
+    {
+      $this->execute("INSERT INTO `staff_change_types` (`id`, `code`, `name`) VALUES ( '6','HOMEROOM_TEACHER', 'Homeroom Teacher')");
+    }
+
+    public function down(){
+        $this->execute("DELETE FROM `staff_change_types` WHERE `staff_change_types`.`id` = 6");
+    }
+}
+?>
