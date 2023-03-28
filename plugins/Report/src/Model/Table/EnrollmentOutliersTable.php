60a6c8eb2b3f9e425cd117a4b09f64e1d6594b91
divyaa <you@example.com>
2023-03-28 16:38:48 +0530

    POCOR-7211||working

--- plugins/Report/src/Model/Table/EnrollmentOutliersTable.php
+++ plugins/Report/src/Model/Table/EnrollmentOutliersTable.php
@@ -49,70 +49,64 @@ class EnrollmentOutliersTable extends AppTable  {
 	
 	public function onExcelBeforeQuery(Event $event, ArrayObject $settings, Query $query) 
 	{
-		/*$requestData = json_decode($settings['process']['params']);
-        $academicPeriodId = $requestData->academic_period_id;*/
-        $academicPeriodId = 32;
-		$conditions = [];
-		$conditionQuery = [];
-		$join = [];
-        $institutionStudents = TableRegistry::get('institution_students');
-        $academicPeriod = TableRegistry::get('academic_periods');
-        $institutions = TableRegistry::get('institutions');
-        $conditions[$this->aliasField('academic_period_id')] = $academicPeriodId;
-        $connection = ConnectionManager::get('default');
-        $statement = $connection->prepare("SELECT main_query.academic_period_name 
-    ,main_query.institution_code 
-    ,main_query.institution_name 
-    ,main_query.count_students 
-						FROM
-						(
-						    SELECT academic_periods.name academic_period_name 	
-						        ,institutions.code institution_code			
-						        ,institutions.name institution_name			
-						        ,COUNT(DISTINCT(institution_students.student_id)) count_students
-						    FROM institution_students
-						    INNER JOIN academic_periods
-						    ON academic_periods.id = institution_students.academic_period_id
-						    INNER JOIN institutions
-						    ON institutions.id = institution_students.institution_id
-						    WHERE academic_periods.id = $academicPeriodId
-						    AND IF((CURRENT_DATE >= academic_periods.start_date AND CURRENT_DATE <= academic_periods.end_date), institution_students.student_status_id = 1, institution_students.student_status_id IN (1, 7, 6, 8))
-						    GROUP BY institutions.id
-						) main_query
-						CROSS JOIN
-						(
-						    SELECT @min_age := MAX(CASE WHEN config_items.code = 'report_outlier_min_age' THEN config_items.value ELSE 0 END) min_age
-						        ,@max_age := MAX(CASE WHEN config_items.code = 'report_outlier_max_age' THEN config_items.value ELSE 0 END) max_age
-						        ,@min_enrolment := MAX(CASE WHEN config_items.code = 'report_outlier_min_student' THEN config_items.value ELSE 0 END) min_enrolment
-						        ,@max_enrolment := MAX(CASE WHEN config_items.code = 'report_outlier_max_student' THEN config_items.value ELSE 0 END) max_enrolment
-						    FROM config_items
-						    WHERE config_items.code IN ('report_outlier_min_age', 'report_outlier_max_age', 
-						        'report_outlier_min_student', 'report_outlier_max_student')
-						) subq
-						WHERE main_query.count_students NOT BETWEEN subq.min_enrolment AND subq.max_enrolment");
-        $statement->execute();
+        $query->formatResults(function (\Cake\Collection\CollectionInterface $results) {
+            return $results->map(function ($row) {
+                $academicPeriodId = 32;
+				$conditions = [];
+				$conditionQuery = [];
+				$join = [];
+				$institutionStudents = TableRegistry::get('institution_students');
+				$academicPeriod = TableRegistry::get('academic_periods');
+				$institutions = TableRegistry::get('institutions');
+				$conditions[$this->aliasField('academic_period_id')] = $academicPeriodId;
+				$connection = ConnectionManager::get('default');
+				$statement = $connection->prepare("SELECT main_query.academic_period_name 
+			,main_query.institution_code 
+			,main_query.institution_name 
+			,main_query.count_students 
+								FROM
+								(
+									SELECT academic_periods.name academic_period_name 	
+										,institutions.code institution_code			
+										,institutions.name institution_name			
+										,COUNT(DISTINCT(institution_students.student_id)) count_students
+									FROM institution_students
+									INNER JOIN academic_periods
+									ON academic_periods.id = institution_students.academic_period_id
+									INNER JOIN institutions
+									ON institutions.id = institution_students.institution_id
+									WHERE academic_periods.id = $academicPeriodId
+									AND IF((CURRENT_DATE >= academic_periods.start_date AND CURRENT_DATE <= academic_periods.end_date), institution_students.student_status_id = 1, institution_students.student_status_id IN (1, 7, 6, 8))
+									GROUP BY institutions.id
+								) main_query
+								CROSS JOIN
+								(
+									SELECT @min_age := MAX(CASE WHEN config_items.code = 'report_outlier_min_age' THEN config_items.value ELSE 0 END) min_age
+										,@max_age := MAX(CASE WHEN config_items.code = 'report_outlier_max_age' THEN config_items.value ELSE 0 END) max_age
+										,@min_enrolment := MAX(CASE WHEN config_items.code = 'report_outlier_min_student' THEN config_items.value ELSE 0 END) min_enrolment
+										,@max_enrolment := MAX(CASE WHEN config_items.code = 'report_outlier_max_student' THEN config_items.value ELSE 0 END) max_enrolment
+									FROM config_items
+									WHERE config_items.code IN ('report_outlier_min_age', 'report_outlier_max_age', 
+										'report_outlier_min_student', 'report_outlier_max_student')
+								) subq
+								WHERE main_query.count_students NOT BETWEEN subq.min_enrolment AND subq.max_enrolment");
+				$statement->execute();
 
-       $list = $statement->fetchAll(\PDO::FETCH_ASSOC);
-       /*$academic_period = $list[0]['Academic Period'];
-       $institution_code = $list[0]['Institution Code'];
-       $institution_name = $list[0]['Institution'];
-       $count_students = $list[0]['Number of Students'];*/
-       $row = [];
-       foreach($list as $key => $value){
-       	$row['academic_period'] = $value['academic_period_name'];
-       	$row['institution_code'] = $value['institution_code'];
-       	$row['institution_name'] = $value['institution_name'];
-       	$row['total_students'] = $value['count_students'];
-       }
-       /*$query[] = [
-       		'academic_period' => $academic_period,
-       		'institution_code' => $institution_code,
-       ];*/
-
-      // print_r($row);die('pkk');
-       return $row;
-       
-        
+			$list = $statement->fetchAll(\PDO::FETCH_ASSOC);
+			/*$academic_period = $list[0]['Academic Period'];
+			$institution_code = $list[0]['Institution Code'];
+			$institution_name = $list[0]['Institution'];
+			$count_students = $list[0]['Number of Students'];*/
+			$row = [];
+			foreach($list as $key => $value){
+				$row['academic_period'] = $value['academic_period_name'];
+				$row['institution_code'] = $value['institution_code'];
+				$row['institution_name'] = $value['institution_name'];
+				$row['total_students'] = $value['count_students'];
+			}
+                return $row;
+            });
+        }); 
 	}
 
 	/*public function onExcelGetStatus(Event $event, Entity $entity)
