552e47307a55c9e69d47db77e9ae0be0382bfa1d
vinoverishabh <rishabh.sharma1@mail.valuecoders.com>
2023-03-29 10:22:40 +0530

    POCOR-7323 Add Delete Permission Status:Done

--- config/Migrations/20230327145817_POCOR7323.php
+++ config/Migrations/20230327145817_POCOR7323.php
@@ -17,7 +17,7 @@ class POCOR7323 extends AbstractMigration
         $this->execute('INSERT INTO `zz_7323_security_functions` SELECT * FROM `security_functions`');
         
         //Update security functions for Absence delete permission
-        $this->execute("INSERT INTO `security_functions` (`id`, `name`, `controller`, `module`, `category`, `parent_id`, `_view`, `_edit`, `_add`, `_delete`, `_execute`, `order`, `visible`, `description`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES (NULL, 'User Group List', 'Securities', 'Administration', 'Security', '5000', 'UserGroupsList.index|UserGroupsList.view', 'UserGroupsList.edit', 'UserGroupsList.add', NULL, NULL, '2', '1', NULL, NULL, NULL, '2', '2023-03-27 17:42:05');");      
+        $this->execute("INSERT INTO `security_functions` (`id`, `name`, `controller`, `module`, `category`, `parent_id`, `_view`, `_edit`, `_add`, `_delete`, `_execute`, `order`, `visible`, `description`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES (NULL, 'User Group List', 'Securities', 'Administration', 'Security', '5000', 'UserGroupsList.index|UserGroupsList.view', 'UserGroupsList.edit', 'UserGroupsList.add', 'UserGroupsList.delete', NULL, '2', '1', NULL, NULL, NULL, '2', '2023-03-27 17:42:05');");      
     }
 
     public function down()
