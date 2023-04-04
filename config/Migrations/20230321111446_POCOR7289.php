2de849573277ecb5dfd7c7ee7821579c8af2d8e0
meghagupta979 <megha.gupta@mail.valuecoders.com>
2023-04-04 10:26:30 +0530

    POCOR-7289| created backup table in migration 20230321111446_POCOR7289.php|Status :Complete

--- config/Migrations/20230321111446_POCOR7289.php
+++ config/Migrations/20230321111446_POCOR7289.php
@@ -9,11 +9,20 @@ class POCOR7289 extends AbstractMigration
 
     public function up()
     {
+       // Backup table
+        $this->execute('CREATE TABLE `zz_7289_staff_change_types` LIKE `staff_change_types`');
+        $this->execute('INSERT INTO `zz_7289_staff_change_types` SELECT * FROM `staff_change_types`');
+
+
       $this->execute("INSERT INTO `staff_change_types` (`id`,`code`, `name`) VALUES ( '6','HOMEROOM_TEACHER', 'Homeroom Teacher')");
     }
 
     public function down(){
-        $this->execute("DELETE FROM `staff_change_types` WHERE `staff_change_types`.`id` = 6");
+
+        // Restore table
+        $this->execute('DROP TABLE IF EXISTS `staff_change_types`');
+        $this->execute('RENAME TABLE `zz_7289_staff_change_types` TO `staff_change_types`');
+
     }
 }
 ?>
