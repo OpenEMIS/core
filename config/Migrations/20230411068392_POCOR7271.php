37dceba9485b088dc8f9d15e6b40b1e54c9eb82a
divyaa <you@example.com>
2023-04-20 16:05:14 +0530

    POCOR-7271||working

--- config/Migrations/20230411068392_POCOR7271.php
+++ config/Migrations/20230411068392_POCOR7271.php
@@ -14,36 +14,67 @@ class POCOR7271 extends AbstractMigration
     public function up()
     {
         // Backup tables
-      //  $this->execute('CREATE TABLE `zz_7271_survey_forms_filters` LIKE `survey_forms_filters`');
-        //$this->execute('INSERT INTO `zz_7271_survey_forms_filters` SELECT * FROM `survey_forms_filters`');
+        $this->execute('CREATE TABLE `zz_7271_survey_forms_filters` LIKE `survey_forms_filters`');
+        $this->execute('INSERT INTO `zz_7271_survey_forms_filters` SELECT * FROM `survey_forms_filters`');
 
-        //$this->execute('CREATE TABLE `zz_7271_survey_statuses` LIKE `survey_statuses`');
-        //$this->execute('INSERT INTO `zz_7271_survey_statuses` SELECT * FROM `survey_statuses`');
+        $this->execute('CREATE TABLE `zz_7271_survey_statuses` LIKE `survey_statuses`');
+        $this->execute('INSERT INTO `zz_7271_survey_statuses` SELECT * FROM `survey_statuses`');
 
-       // $this->execute('ALTER TABLE `survey_statuses` ADD `survey_filter_id` INT(11) NOT NULL AFTER `survey_form_id`');
+        $this->execute('ALTER TABLE `survey_statuses` ADD `survey_filter_id` INT(11) NOT NULL AFTER `survey_form_id`');
         $this->execute('ALTER TABLE `survey_forms_filters` ADD `name` varchar(255) NOT NULL AFTER `id`');
         $this->execute('ALTER TABLE `survey_forms_filters` ADD `custom_module_id` INT(11) NOT NULL AFTER `survey_form_id`');
-        $this->execute('ALTER TABLE `survey_forms_filters` ADD `institution_type_id` varchar(255) NOT NULL AFTER `custom_module_id`');
-        $this->execute('ALTER TABLE `survey_forms_filters` ADD `institution_provider_id` varchar(255) NOT NULL AFTER `institution_type_id`');
-        $this->execute('ALTER TABLE `survey_forms_filters` ADD `area_education_id` varchar(255) NOT NULL AFTER `institution_provider_id`');
-
-        $this->execute('ALTER TABLE `survey_forms_filters` ADD `modified_user_id` INT(11) DEFAULT NULL AFTER `institution_provider_id`');
+        $this->execute('ALTER TABLE `survey_forms_filters` ADD `modified_user_id` INT(11) DEFAULT NULL AFTER `custom_module_id`');
         $this->execute('ALTER TABLE `survey_forms_filters` ADD `modified` datetime DEFAULT NULL AFTER `modified_user_id`');
         $this->execute('ALTER TABLE `survey_forms_filters` ADD `created_user_id` INT(11) DEFAULT NULL AFTER `modified`');
         $this->execute('ALTER TABLE `survey_forms_filters` ADD `created` datetime DEFAULT NULL AFTER `created_user_id`');
         // drop survey_filter_id id
         $this->execute('ALTER TABLE `survey_forms_filters` DROP `survey_filter_id`');
+
+        $this->execute('CREATE TABLE `survey_filter_institution_types` (
+                      `id` int(11) NOT NULL,
+                      `survey_filter_id` int(11) NOT NULL,
+                      `institution_type_id` int(11) DEFAULT NULL,
+                      `modified_user_id` int(11) DEFAULT NULL,
+                      `modified` datetime DEFAULT NULL,
+                      `created_user_id` int(11) DEFAULT NULL,
+                      `created` datetime DEFAULT NULL
+                    ) ENGINE=InnoDB DEFAULT CHARSET=latin1');
+
+        $this->execute('INSERT INTO `survey_filter_institution_types` SELECT * FROM `survey_filter_institution_types`');
+
+        $this->execute('CREATE TABLE `survey_filter_institution_providers` (
+                      `id` int(11) NOT NULL,
+                      `survey_filter_id` int(11) NOT NULL,
+                      `institution_provider_id` int(11) DEFAULT NULL,
+                      `modified_user_id` int(11) DEFAULT NULL,
+                      `modified` datetime DEFAULT NULL,
+                      `created_user_id` int(11) DEFAULT NULL,
+                      `created` datetime DEFAULT NULL
+                    ) ENGINE=InnoDB DEFAULT CHARSET=latin1');
+
+        $this->execute('INSERT INTO `survey_filter_institution_providers` SELECT * FROM `survey_filter_institution_providers`');
+
+        $this->execute('CREATE TABLE `survey_filter_areas` (
+                      `id` int(11) NOT NULL,
+                      `survey_filter_id` int(11) NOT NULL,
+                      `area_education_id` int(11) DEFAULT NULL,
+                      `modified_user_id` int(11) DEFAULT NULL,
+                      `modified` datetime DEFAULT NULL,
+                      `created_user_id` int(11) DEFAULT NULL,
+                      `created` datetime DEFAULT NULL
+                    ) ENGINE=InnoDB DEFAULT CHARSET=latin1');
+
+        $this->execute('INSERT INTO `survey_filter_areas` SELECT * FROM `survey_filter_areas`');
         
     }
 
     public function down()
     {
-
-       // $this->execute('DROP TABLE IF EXISTS `survey_forms_filters`');
-        //$this->execute('RENAME TABLE `zz_7271_survey_forms_filters` TO `survey_forms_filters`');
+        $this->execute('DROP TABLE IF EXISTS `survey_forms_filters`');
+        $this->execute('RENAME TABLE `zz_7271_survey_forms_filters` TO `survey_forms_filters`');
 
         $this->execute('DROP TABLE IF EXISTS `survey_statuses`');
-        //$this->execute('RENAME TABLE `zz_7271_survey_statuses` TO `survey_statuses`');
+        $this->execute('RENAME TABLE `zz_7271_survey_statuses` TO `survey_statuses`');
 
     }
 }
