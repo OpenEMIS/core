0e7eda7aa1f3663982a501b42a2f0f2dba041a48
Ehteram Ahmad <ehteram333@gmail.com>
2023-05-17 17:15:53 +0530

    POCOR-7320: Task Done STATUS: Done

--- plugins/Institution/src/Model/Table/ReportCardStatusesTable.php
+++ plugins/Institution/src/Model/Table/ReportCardStatusesTable.php
@@ -1208,7 +1208,7 @@ class ReportCardStatusesTable extends ControllerActionTable
 
     private function mergePDFFiles(Array $filenames, $outFile='', $title = '', $author = '', $subject = '')
     {
-        $mpdf = new \Mpdf\Mpdf(['mode' => 'utf-8', 'format' => [370, 220]]); //POCOR-7090
+        $mpdf = new \Mpdf\Mpdf(['mode' => 'utf-8', 'format' => [220, 390]]);
         $mpdf->SetTitle($title);
         $mpdf->SetAuthor($author);
         $mpdf->SetSubject($subject);
