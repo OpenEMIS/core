e28696e761d737da1753ba63c89b7836c7cca338
meghagupta979 <megha.gupta@mail.valuecoders.com>
2023-05-23 12:13:53 +0530

    POCOR-7402|Fixed item options |Status:Complete

--- config/Migrations/20230511061700_POCOR7402.php
+++ config/Migrations/20230511061700_POCOR7402.php
@@ -10,8 +10,8 @@ class POCOR7402 extends AbstractMigration
     public function up()
     {
        // Backup table
-    //    $this->execute('CREATE TABLE `zz_7402_config_items` LIKE `config_items`');
-    //    $this->execute('INSERT INTO `zz_7402_config_items` SELECT * FROM `config_items`');
+       $this->execute('CREATE TABLE `zz_7402_config_items` LIKE `config_items`');
+       $this->execute('INSERT INTO `zz_7402_config_items` SELECT * FROM `config_items`');
        
         $data=[
             [
@@ -19,7 +19,7 @@ class POCOR7402 extends AbstractMigration
                 'code' => 'address_area_level',
                 'type' => 'Add New Student',
                 'label' => 'Display Address Area Level',
-                'value' => '3',
+                'value' => '1',
                 'value_selection'=>'1',
                 'default_value' => '1',
                 'editable' => '1',
@@ -36,7 +36,7 @@ class POCOR7402 extends AbstractMigration
                 'code' => 'birthplace_area_level',
                 'type' => 'Add New Student',
                 'label' => 'Display Birthplace Area Level',
-                'value' => '3',
+                'value' => '1',
                 'value_selection'=>'1',
                 'default_value' => '1',
                 'editable' => '1',
