86399326f88c208a37e73d40147fe72532091537
vinoverishabh <rishabh.sharma1@mail.valuecoders.com>
2023-05-25 10:34:06 +0530

    POCOR-5191 2.16 Reports > Student Case Report Status:Working

---
+++ config/Migrations/20230524083527_POCOR5191.php
@@ -0,0 +1,28 @@
+<?php
+use Migrations\AbstractMigration;
+
+class POCOR5191 extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function up()
+    {
+        $this->execute("CREATE TABLE `student_profile_security_roles` (
+            `id` int NOT NULL AUTO_INCREMENT,
+            `security_role_id` int NOT NULL,
+            `student_profile_template_id` int NOT NULL,
+            `created` datetime DEFAULT CURRENT_TIMESTAMP,
+            PRIMARY KEY (`id`)
+          ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci");
+    }
+    public function down()
+    {
+        $this->execute('DROP TABLE IF EXISTS `student_profile_security_roles`');
+        $this->execute('RENAME TABLE `zz_5191_student_profile_security_roles` TO `student_profile_security_roles`');
+    }
+}
