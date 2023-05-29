97e7a75d482ef0f2f428c228d91332b8abddb86c
meghagupta979 <megha.gupta@mail.valuecoders.com>
2023-05-29 13:03:11 +0530

    POCOR-7223|changed migration script|Status:complete

--- config/Migrations/20230526045018_POCOR7223.php
+++ config/Migrations/20230526045018_POCOR7223.php
@@ -14,6 +14,9 @@ class POCOR7223 extends AbstractMigration
     {
         $this->execute('CREATE TABLE `zz_7223_student_behaviours` LIKE `student_behaviours`');
         $this->execute('INSERT INTO `zz_7223_student_behaviours` SELECT * FROM `student_behaviours`');
+        $this->execute("UPDATE `student_behaviours`
+         SET `description`= CONCAT(COALESCE(`title`, ''), ' . ',COALESCE(`description`, ''))");
+
         $this->execute('ALTER TABLE `student_behaviours` DROP COLUMN `title`');
 
         $this->execute("CREATE TABLE IF NOT EXISTS `student_behaviour_classifications` (
@@ -27,10 +30,10 @@ class POCOR7223 extends AbstractMigration
 			`national_code` varchar(50) DEFAULT NULL,
             `modified_user_id` int(11) DEFAULT NULL,
             `modified` datetime DEFAULT NULL,
-            `created_user_id` int(11) NOT NULL,
-            `created` datetime NOT NULL,
-             PRIMARY KEY (`id`)
-          )  ENGINE=InnoDB DEFAULT CHARSET=utf8");
+             `created_user_id` int(11) NOT NULL,
+           `created` datetime NOT NULL,
+            PRIMARY KEY (`id`)
+           )  ENGINE=InnoDB DEFAULT CHARSET=utf8");
     
     $this->execute('ALTER TABLE `student_behaviours` ADD COLUMN student_behaviour_classification_id INT(11)');
     $this->execute('SET FOREIGN_KEY_CHECKS=0;');
