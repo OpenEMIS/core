b5a5c85d19d88e3a1854737b124454400b2e0c75
meghagupta979 <megha.gupta@mail.valuecoders.com>
2023-06-01 17:30:30 +0530

    POCOR-7363|Meal rating complete|Status:complete

--- config/Migrations/20230530072123_POCOR7363.php
+++ config/Migrations/20230530072123_POCOR7363.php
@@ -12,153 +12,140 @@ class POCOR7363 extends AbstractMigration
      */
     public function up()
     {
-        // $this->execute('CREATE TABLE `zz_7363_field_options` LIKE `field_options`');
-        // $this->execute('INSERT INTO `zz_7363_field_options` SELECT * FROM `field_options`');
+        //field options
+        $this->execute('CREATE TABLE `zz_7363_field_options` LIKE `field_options`');
+        $this->execute('INSERT INTO `zz_7363_field_options` SELECT * FROM `field_options`');
        
-        // $order = $this->fetchRow("SELECT `order` FROM `field_options` ORDER BY `id` DESC LIMIT 1");
+        $order = $this->fetchRow("SELECT `order` FROM `field_options` ORDER BY `id` DESC LIMIT 1");
      
-        // $data=[
-        //         [
-        //             'name' => 'Food Types',
-        //             'category' => 'Meals',
-        //             'table_name' => 'food_types',
-        //             'order' => $order[0]+1,
-        //             'modified_by' => NULL,
-        //             'modified'=>NULL,
-        //             'created_by' =>'1',
-        //             'created' => date('Y-m-d H:i:s'),
-        //         ],
-        //         [
-        //             'name' => 'Meal Ratings',
-        //             'category' => 'Meals',
-        //             'table_name' => 'meal_ratings',
-        //             'order' => $order[0]+2,
-        //             'modified_by' => NULL,
-        //             'modified'=>NULL,
-        //             'created_by' => '1',
-        //             'created' => date('Y-m-d H:i:s'),
-        //         ],
-        //     ]; 
+        $data=[
+                [
+                    'name' => 'Food Types',
+                    'category' => 'Meals',
+                    'table_name' => 'food_types',
+                    'order' => $order[0]+1,
+                    'modified_by' => NULL,
+                    'modified'=>NULL,
+                    'created_by' =>'1',
+                    'created' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'name' => 'Meal Ratings',
+                    'category' => 'Meals',
+                    'table_name' => 'meal_ratings',
+                    'order' => $order[0]+2,
+                    'modified_by' => NULL,
+                    'modified'=>NULL,
+                    'created_by' => '1',
+                    'created' => date('Y-m-d H:i:s'),
+                ],
+            ]; 
 
-        //     $this->insert('field_options', $data);
+            $this->insert('field_options', $data);
 
-            // $this->execute("CREATE TABLE IF NOT EXISTS `food_types` (
-            //     `id` int(11) NOT NULL AUTO_INCREMENT,
-            //     `name` varchar(50) NOT NULL,
-            //     `order` int(3) DEFAULT NULL,
-            //     `visible` int(1) DEFAULT '1',
-            //     `editable` int(1) DEFAULT '1',
-            //     `default` int(1) DEFAULT '0',
-            //     `international_code` varchar(50) DEFAULT NULL,
-            //     `national_code` varchar(50) DEFAULT NULL,
-            //     `modified_user_id` int(11) DEFAULT NULL,
-            //     `modified` datetime DEFAULT NULL,
-            //     `created_user_id` int(11) NOT NULL,
-            //     `created` datetime NOT NULL,
-            //      PRIMARY KEY (`id`)
-            //   )  ENGINE=InnoDB DEFAULT CHARSET=utf8");
+            //food types
+            $this->execute("CREATE TABLE IF NOT EXISTS `food_types` (
+                `id` int(11) NOT NULL AUTO_INCREMENT,
+                `name` varchar(50) NOT NULL,
+                `order` int(3) DEFAULT NULL,
+                `visible` int(1) DEFAULT '1',
+                `editable` int(1) DEFAULT '1',
+                `default` int(1) DEFAULT '0',
+                `international_code` varchar(50) DEFAULT NULL,
+                `national_code` varchar(50) DEFAULT NULL,
+                `modified_user_id` int(11) DEFAULT NULL,
+                `modified` datetime DEFAULT NULL,
+                `created_user_id` int(11) NOT NULL,
+                `created` datetime NOT NULL,
+                 PRIMARY KEY (`id`)
+              )  ENGINE=InnoDB DEFAULT CHARSET=utf8");
             
-            // $data1=[
-            //          [
-            //             'id'=>1,
-            //             'name' =>'Vegetable',
-            //             'order' =>1,
-            //             'visible' => 1,
-            //             'editable' => 1,
-            //             'default' => 1,
-            //             'international_code' => NULL,
-            //             'national_code'  =>NULL,
-            //             'modified_user_id' => NULL,
-            //             'modified' =>NULL,
-            //             'created_user_id' =>1,
-            //             'created'  =>date('Y-m-d H:i:s')
-            //          ],
-            //          [
-            //             'id'=>2,
-            //             'name' =>'Meat',
-            //             'order' =>2,
-            //             'visible' => 1,
-            //             'editable' => 1,
-            //             'default' => 0,
-            //             'international_code' => NULL,
-            //             'national_code'  =>NULL,
-            //             'modified_user_id' => NULL,
-            //             'modified' =>NULL,
-            //             'created_user_id' =>1,
-            //             'created'  =>date('Y-m-d H:i:s')
-            //          ]
-            //      ];
+            $data1=[
+                     [
+                        'id'=>1,
+                        'name' =>'Vegetable',
+                        'order' =>1,
+                        'visible' => 1,
+                        'editable' => 1,
+                        'default' => 1,
+                        'international_code' => NULL,
+                        'national_code'  =>NULL,
+                        'modified_user_id' => NULL,
+                        'modified' =>NULL,
+                        'created_user_id' =>1,
+                        'created'  =>date('Y-m-d H:i:s')
+                     ],
+                     [
+                        'id'=>2,
+                        'name' =>'Meat',
+                        'order' =>2,
+                        'visible' => 1,
+                        'editable' => 1,
+                        'default' => 0,
+                        'international_code' => NULL,
+                        'national_code'  =>NULL,
+                        'modified_user_id' => NULL,
+                        'modified' =>NULL,
+                        'created_user_id' =>1,
+                        'created'  =>date('Y-m-d H:i:s')
+                     ]
+                 ];
             
-            // $this->insert('food_types',$data1);
+            $this->insert('food_types',$data1);
 
-            // $this->execute("CREATE TABLE IF NOT EXISTS `meal_ratings` (
-            //     `id` int(11) NOT NULL AUTO_INCREMENT,
-            //     `name` varchar(50) NOT NULL,
-            //     `order` int(3) DEFAULT NULL,
-            //     `visible` int(1) DEFAULT '1',
-            //     `editable` int(1) DEFAULT '1',
-            //     `default` int(1) DEFAULT '0',
-            //     `international_code` varchar(50) DEFAULT NULL,
-            //     `national_code` varchar(50) DEFAULT NULL,
-            //     `modified_user_id` int(11) DEFAULT NULL,
-            //     `modified` datetime DEFAULT NULL,
-            //     `created_user_id` int(11) NOT NULL,
-            //     `created` datetime NOT NULL,
-            //      PRIMARY KEY (`id`)
-            //   )  ENGINE=InnoDB DEFAULT CHARSET=utf8");
+            //meal ratings
+            $this->execute("CREATE TABLE IF NOT EXISTS `meal_ratings` (
+                `id` int(11) NOT NULL AUTO_INCREMENT,
+                `name` varchar(50) NOT NULL,
+                `order` int(3) DEFAULT NULL,
+                `visible` int(1) DEFAULT '1',
+                `editable` int(1) DEFAULT '1',
+                `default` int(1) DEFAULT '0',
+                `international_code` varchar(50) DEFAULT NULL,
+                `national_code` varchar(50) DEFAULT NULL,
+                `modified_user_id` int(11) DEFAULT NULL,
+                `modified` datetime DEFAULT NULL,
+                `created_user_id` int(11) NOT NULL,
+                `created` datetime NOT NULL,
+                 PRIMARY KEY (`id`)
+              )  ENGINE=InnoDB DEFAULT CHARSET=utf8");
             
-            // $data2=[
-            //          [
-            //             'id'=>1,
-            //             'name' =>1,
-            //             'order' =>1,
-            //             'visible' => 1,
-            //             'editable' => 1,
-            //             'default' => 1,
-            //             'international_code' => NULL,
-            //             'national_code'  =>NULL,
-            //             'modified_user_id' => NULL,
-            //             'modified' =>NULL,
-            //             'created_user_id' =>1,
-            //             'created'  =>date('Y-m-d H:i:s')
-            //          ],
-            //          [
-            //             'id'=>2,
-            //             'name' =>2,
-            //             'order' =>2,
-            //             'visible' => 1,
-            //             'editable' => 1,
-            //             'default' => 0,
-            //             'international_code' => NULL,
-            //             'national_code'  =>NULL,
-            //             'modified_user_id' => NULL,
-            //             'modified' =>NULL,
-            //             'created_user_id' =>1,
-            //             'created'  =>date('Y-m-d H:i:s')
-            //          ]
-            //      ];
+            $data2=[
+                     [
+                        'id'=>1,
+                        'name' =>1,
+                        'order' =>1,
+                        'visible' => 1,
+                        'editable' => 1,
+                        'default' => 1,
+                        'international_code' => NULL,
+                        'national_code'  =>NULL,
+                        'modified_user_id' => NULL,
+                        'modified' =>NULL,
+                        'created_user_id' =>1,
+                        'created'  =>date('Y-m-d H:i:s')
+                     ],
+                     [
+                        'id'=>2,
+                        'name' =>2,
+                        'order' =>2,
+                        'visible' => 1,
+                        'editable' => 1,
+                        'default' => 0,
+                        'international_code' => NULL,
+                        'national_code'  =>NULL,
+                        'modified_user_id' => NULL,
+                        'modified' =>NULL,
+                        'created_user_id' =>1,
+                        'created'  =>date('Y-m-d H:i:s')
+                     ]
+                 ];
             
-            // $this->insert('meal_ratings',$data2);
+            $this->insert('meal_ratings',$data2);
 
             
 
-            // $table = $this->table('meal_food_records', [
-            //    'collation' => 'utf8mb4_unicode_ci',
-            //    'comment' => 'This field options table contains types of meal food type records'
-            // ]);
-            // $table->addColumn('meal_programmes_id', 'integer', [
-            //    'default' => null,
-            //    'limit' => 11,
-            //    'comment' => 'links to meal_programmes.id',
-            //    'null' => true
-            // ])
-            // ->addColumn('food_type_id', 'integer', [
-            //    'default' => null,
-            //    'limit' => 11,
-            //    'comment' =>'links to food_type.id',
-            //    'null' => true
-            // ])  
-            // ->save(); 
+           //meal food records
             $this->execute('CREATE TABLE IF NOT EXISTS `meal_food_records` (
                `id` int(11) NOT NULL AUTO_INCREMENT,
                `meal_programmes_id` int(11) ,
@@ -168,20 +155,37 @@ class POCOR7363 extends AbstractMigration
                FOREIGN KEY (`food_type_id`) REFERENCES `food_types` (`id`)
              ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
              ');
-           
+
+            //institution meal programmes
+            $this->execute('CREATE TABLE `zz_7363_institution_meal_programmes` LIKE `institution_meal_programmes`');
+            $this->execute('INSERT INTO `zz_7363_institution_meal_programmes` SELECT * FROM `institution_meal_programmes`');
+            $this->execute('ALTER TABLE `institution_meal_programmes` ADD COLUMN meal_rating_id INT(11)');
+            $this->execute('SET FOREIGN_KEY_CHECKS=0;');
+            $this->execute('ALTER TABLE `institution_meal_programmes` ADD FOREIGN KEY (`meal_rating_id`) REFERENCES `meal_ratings` (`id`)');
+            $this->execute('SET FOREIGN_KEY_CHECKS=1;');
 
     }
     
     public function down()
     {
-    // $this->execute('DROP TABLE IF EXISTS `field_options`');
-    // $this->execute('RENAME TABLE `zz_7363_field_options` TO `field_options`');
-
-   //  $this->execute('DROP TABLE IF EXISTS `food_types`');
-   //  $this->execute('DROP TABLE IF EXISTS `meal_ratings`');
-
-    $this->execute('DROP TABLE meal_food_types');
-    }
+            //field options
+            $this->execute('DROP TABLE IF EXISTS `field_options`');
+            $this->execute('RENAME TABLE `zz_7363_field_options` TO `field_options`');
+            //food types
+            $this->execute('DROP TABLE IF EXISTS `food_types`');
+            //meal ratings
+            $this->execute('DROP TABLE IF EXISTS `meal_ratings`');
+            //meal food types
+            $this->execute('DROP TABLE meal_food_types');
+            //institution meal programmes
+            $this->execute('SET FOREIGN_KEY_CHECKS=0;');
+            $this->execute('ALTER TABLE `institution_meal_programmes` DROP FOREIGN KEY `meal_rating_id`');
+            $this->execute('ALTER TABLE `institution_meal_programmes` DROP COLUMN `meal_rating_id`');
+            $this->execute('SET FOREIGN_KEY_CHECKS=1;');
+            $this->execute('DROP TABLE IF EXISTS `institution_meal_programmes`');
+            $this->execute('RENAME TABLE `zz_7363_institution_meal_programmes` TO `institution_meal_programmes`');
+    
+}
 
       
     
