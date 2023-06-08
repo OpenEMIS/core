9a1c57af96d391bf21fe3bcd03faaebe1acd0b51
Sachin Kumar - PHP <sachin.kumar1@mail.valuecoders.com>
2023-06-08 09:37:23 +0530

    POCOR-7362 | enabled migrations | status: in progress

--- config/Migrations/20230523112032_POCOR7362.php
+++ config/Migrations/20230523112032_POCOR7362.php
@@ -13,63 +13,63 @@ class POCOR7362 extends AbstractMigration
     public function up()
     {
         //for textbook dimension
-        // $this->execute("CREATE TABLE IF NOT EXISTS `textbook_dimensions` (
-        //     `id` int(11) NOT NULL AUTO_INCREMENT,
-        //     `name` varchar(50) NOT NULL,
-        //     `order` int(3) NOT NULL,
-        //     `visible` int(1) NOT NULL DEFAULT '1',
-		// 	`editable` int(1) NOT NULL DEFAULT '1',
-		// 	`default` int(1) NOT NULL DEFAULT '0',
-		// 	`international_code` varchar(50) DEFAULT NULL,
-		// 	`national_code` varchar(50) DEFAULT NULL,
-        //     `modified_user_id` int(11) DEFAULT NULL,
-        //     `modified` datetime DEFAULT NULL,
-        //     `created_user_id` int(11) NOT NULL,
-        //     `created` datetime NOT NULL,
-        //      PRIMARY KEY (`id`)
-        //   )  ENGINE=InnoDB DEFAULT CHARSET=utf8");
+        $this->execute("CREATE TABLE IF NOT EXISTS `textbook_dimensions` (
+            `id` int(11) NOT NULL AUTO_INCREMENT,
+            `name` varchar(50) NOT NULL,
+            `order` int(3) NOT NULL,
+            `visible` int(1) NOT NULL DEFAULT '1',
+			`editable` int(1) NOT NULL DEFAULT '1',
+			`default` int(1) NOT NULL DEFAULT '0',
+			`international_code` varchar(50) DEFAULT NULL,
+			`national_code` varchar(50) DEFAULT NULL,
+            `modified_user_id` int(11) DEFAULT NULL,
+            `modified` datetime DEFAULT NULL,
+            `created_user_id` int(11) NOT NULL,
+            `created` datetime NOT NULL,
+             PRIMARY KEY (`id`)
+          )  ENGINE=InnoDB DEFAULT CHARSET=utf8");
 
 
-        // $this->execute("INSERT INTO `textbook_dimensions` (`id`, `name`, `order`, `visible`, `editable`, `default`, `international_code`, `national_code`,`modified_user_id`, `modified`, `created_user_id`, `created`) VALUES (NULL, '50mmx76mm', '1', '1', '1', '0', '', '', NULL, NULL, '1', '2023-05-23 12:00:00')");
+        $this->execute("INSERT INTO `textbook_dimensions` (`id`, `name`, `order`, `visible`, `editable`, `default`, `international_code`, `national_code`,`modified_user_id`, `modified`, `created_user_id`, `created`) VALUES (NULL, '50mmx76mm', '1', '1', '1', '0', '', '', NULL, NULL, '1', '2023-05-23 12:00:00')");
 
-        // $this->execute("INSERT INTO `textbook_dimensions` (`id`, `name`, `order`, `visible`, `editable`, `default`, `international_code`, `national_code`,`modified_user_id`, `modified`, `created_user_id`, `created`) VALUES (NULL, '90mmx140mm', '2', '1', '1', '0', '', '', NULL, NULL, '1', '2023-05-23 12:00:00')");
+        $this->execute("INSERT INTO `textbook_dimensions` (`id`, `name`, `order`, `visible`, `editable`, `default`, `international_code`, `national_code`,`modified_user_id`, `modified`, `created_user_id`, `created`) VALUES (NULL, '90mmx140mm', '2', '1', '1', '0', '', '', NULL, NULL, '1', '2023-05-23 12:00:00')");
 
-        // $this->execute('SET FOREIGN_KEY_CHECKS = 0;');
-        // $this->execute('ALTER TABLE textbooks
-        // ADD COLUMN textbook_dimension_id INT(11) AFTER education_subject_id,
-        // ADD CONSTRAINT fk_textbook_dimensions FOREIGN KEY (textbook_dimension_id) REFERENCES textbook_dimensions (id);');
-        // $this->execute('SET FOREIGN_KEY_CHECKS = 1;');
+        $this->execute('SET FOREIGN_KEY_CHECKS = 0;');
+        $this->execute('ALTER TABLE textbooks
+        ADD COLUMN textbook_dimension_id INT(11) AFTER education_subject_id,
+        ADD CONSTRAINT fk_textbook_dimensions FOREIGN KEY (textbook_dimension_id) REFERENCES textbook_dimensions (id);');
+        $this->execute('SET FOREIGN_KEY_CHECKS = 1;');
 
         //text dimensions ends
 
 
         //for textbook statuses
 
-        // $this->execute('CREATE TABLE `zz_7362_textbook_statuses` LIKE `textbook_statuses`');
-        // $this->execute('INSERT INTO `zz_7362_textbook_statuses` SELECT * FROM `textbook_statuses`');
+        $this->execute('CREATE TABLE `zz_7362_textbook_statuses` LIKE `textbook_statuses`');
+        $this->execute('INSERT INTO `zz_7362_textbook_statuses` SELECT * FROM `textbook_statuses`');
 
-        // $this->execute("ALTER TABLE `textbook_statuses` ADD `order` int(3) NOT NULL");
-        // $this->execute("ALTER TABLE `textbook_statuses` ADD `visible` int(1) NOT NULL DEFAULT '1'");
-        // $this->execute("ALTER TABLE `textbook_statuses` ADD `editable` int(1) NOT NULL DEFAULT '1'");
-        // $this->execute("ALTER TABLE `textbook_statuses` ADD `default` int(1) NOT NULL DEFAULT '0'");
-        // $this->execute("ALTER TABLE `textbook_statuses` ADD `international_code` varchar(50) DEFAULT NULL");
-        // $this->execute("ALTER TABLE `textbook_statuses` ADD `national_code` varchar(50) DEFAULT NULL");
-        // $this->execute("ALTER TABLE `textbook_statuses` ADD `modified_user_id` int(11) DEFAULT NULL");
-        // $this->execute("ALTER TABLE `textbook_statuses` ADD `modified` datetime DEFAULT NULL");
-        // $this->execute("ALTER TABLE `textbook_statuses` ADD `created_user_id` int(11) NOT NULL");
-        // $this->execute("ALTER TABLE `textbook_statuses` ADD `created` datetime");
+        $this->execute("ALTER TABLE `textbook_statuses` ADD `order` int(3) NOT NULL");
+        $this->execute("ALTER TABLE `textbook_statuses` ADD `visible` int(1) NOT NULL DEFAULT '1'");
+        $this->execute("ALTER TABLE `textbook_statuses` ADD `editable` int(1) NOT NULL DEFAULT '1'");
+        $this->execute("ALTER TABLE `textbook_statuses` ADD `default` int(1) NOT NULL DEFAULT '0'");
+        $this->execute("ALTER TABLE `textbook_statuses` ADD `international_code` varchar(50) DEFAULT NULL");
+        $this->execute("ALTER TABLE `textbook_statuses` ADD `national_code` varchar(50) DEFAULT NULL");
+        $this->execute("ALTER TABLE `textbook_statuses` ADD `modified_user_id` int(11) DEFAULT NULL");
+        $this->execute("ALTER TABLE `textbook_statuses` ADD `modified` datetime DEFAULT NULL");
+        $this->execute("ALTER TABLE `textbook_statuses` ADD `created_user_id` int(11) NOT NULL");
+        $this->execute("ALTER TABLE `textbook_statuses` ADD `created` datetime");
 
-        // $this->execute("UPDATE `textbook_statuses` SET `order` = '1' WHERE `id`= 1");
-        // $this->execute("UPDATE `textbook_statuses` SET `international_code` = '' WHERE `id`= 1");
-        // $this->execute("UPDATE `textbook_statuses` SET `national_code` = '' WHERE `id`= 1");
-        // $this->execute("UPDATE `textbook_statuses` SET `created_user_id` = '1' WHERE `id`= 1");
-        // $this->execute("UPDATE `textbook_statuses` SET `created` = '2023-06-07 12:00:00' WHERE `id`= 1");
+        $this->execute("UPDATE `textbook_statuses` SET `order` = '1' WHERE `id`= 1");
+        $this->execute("UPDATE `textbook_statuses` SET `international_code` = '' WHERE `id`= 1");
+        $this->execute("UPDATE `textbook_statuses` SET `national_code` = '' WHERE `id`= 1");
+        $this->execute("UPDATE `textbook_statuses` SET `created_user_id` = '1' WHERE `id`= 1");
+        $this->execute("UPDATE `textbook_statuses` SET `created` = '2023-06-07 12:00:00' WHERE `id`= 1");
 
-        // $this->execute("UPDATE `textbook_statuses` SET `order` = '2' WHERE `id`= 2");
-        // $this->execute("UPDATE `textbook_statuses` SET `international_code` = '' WHERE `id`= 2");
-        // $this->execute("UPDATE `textbook_statuses` SET `national_code` = '' WHERE `id`= 2");
-        // $this->execute("UPDATE `textbook_statuses` SET `created_user_id` = '1' WHERE `id`= 2");
-        // $this->execute("UPDATE `textbook_statuses` SET `created` = '2023-06-07 12:00:00' WHERE `id`= 2");
+        $this->execute("UPDATE `textbook_statuses` SET `order` = '2' WHERE `id`= 2");
+        $this->execute("UPDATE `textbook_statuses` SET `international_code` = '' WHERE `id`= 2");
+        $this->execute("UPDATE `textbook_statuses` SET `national_code` = '' WHERE `id`= 2");
+        $this->execute("UPDATE `textbook_statuses` SET `created_user_id` = '1' WHERE `id`= 2");
+        $this->execute("UPDATE `textbook_statuses` SET `created` = '2023-06-07 12:00:00' WHERE `id`= 2");
 
         // textbook status ends
 
@@ -120,18 +120,18 @@ class POCOR7362 extends AbstractMigration
 
         // textbook status starts
 
-        // $this->execute('DROP TABLE IF EXISTS `textbook_statuses`');
-        // $this->execute('RENAME TABLE `zz_7362_textbook_statuses` TO `textbook_statuses`');
+        $this->execute('DROP TABLE IF EXISTS `textbook_statuses`');
+        $this->execute('RENAME TABLE `zz_7362_textbook_statuses` TO `textbook_statuses`');
 
         // textbook status ends
 
         // textbook dimensions starts
 
-        // $this->execute('SET FOREIGN_KEY_CHECKS = 0;');
-        // $this->execute('ALTER TABLE textbooks DROP FOREIGN KEY fk_textbook_dimensions');
-        // $this->execute('SET FOREIGN_KEY_CHECKS = 1;');
-        // $this->execute('ALTER TABLE textbooks DROP COLUMN textbook_dimension_id');
-        // $this->execute('DROP TABLE IF EXISTS `textbook_dimensions`');
+        $this->execute('SET FOREIGN_KEY_CHECKS = 0;');
+        $this->execute('ALTER TABLE textbooks DROP FOREIGN KEY fk_textbook_dimensions');
+        $this->execute('SET FOREIGN_KEY_CHECKS = 1;');
+        $this->execute('ALTER TABLE textbooks DROP COLUMN textbook_dimension_id');
+        $this->execute('DROP TABLE IF EXISTS `textbook_dimensions`');
 
         // textbook dimensions ends
     }
