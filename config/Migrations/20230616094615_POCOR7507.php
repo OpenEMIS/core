60798a34768d462f2e652445584367a3eb01e829
Nitin Singh Bisht <nitin.singh@mail.valuecoders.com>
2023-06-16 15:29:10 +0530

    POCOR-7507: Migration Script

---
+++ config/Migrations/20230616094615_POCOR7507.php
@@ -0,0 +1,21 @@
+<?php
+use Migrations\AbstractMigration;
+
+class POCOR7507 extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function up()
+    {
+        $this->execute('UPDATE `config_items` SET `type`="External Data Source Identity" WHERE `code`="external_data_source_type" AND `type`="External Data Source"');
+    }
+    public function down()
+    {
+        $this->execute('UPDATE `config_items` SET `type`="External Data Source" WHERE `code`="external_data_source_type" AND `type`="External Data Source Identity"');
+    }
+}
