837e239edf0acc80aa3041e59269aa4c835aa435
tixuz <hindolmirzo@gmail.com>
2023-06-18 21:35:31 +0300

    brushed

--- src/Controller/Component/NavigationComponent.php
+++ src/Controller/Component/NavigationComponent.php
@@ -152,7 +152,7 @@ class NavigationComponent extends Component
                 }
 
                 // $ignoredAction will be excluded from permission checking
-                if (array_key_exists('controller', $url) && !in_array('plugin', $url)) {
+                if (array_key_exists('controller', $url) && !in_array($url['plugin'])) {
                     //   print_r($url);die();
                     if (!$this->AccessControl->check($url, $rolesRestrictedTo)) {
                         unset($navigations[$key]);
