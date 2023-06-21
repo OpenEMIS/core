cea012172d2afcabc9a8b4b8d09b27e14fa3c8e3
meghagupta979 <megha.gupta@mail.valuecoders.com>
2023-06-21 09:58:05 +0530

    POCOR-7530|added condition for error -1|Status:complete

--- src/Shell/UpdateReportCardStatusShell.php
+++ src/Shell/UpdateReportCardStatusShell.php
@@ -23,7 +23,7 @@ class UpdateReportCardStatusShell extends Shell {
                               AND institution_students_report_cards.education_grade_id = report_card_processes.education_grade_id
                               AND institution_students_report_cards.institution_class_id = report_card_processes.institution_class_id 
                               SET institution_students_report_cards.status = report_card_processes.status  
-                              where institution_students_report_cards.status In (1,2,3)");
+                              where institution_students_report_cards.status In (1,2,3,-1)");
         $success=$stmt->execute();
 		$this->out("ended");
 	}
