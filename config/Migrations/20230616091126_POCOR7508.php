0d84a24e59a974e497dfd42d39dec8f49c6fd2d5
Nitin Singh Bisht <nitin.singh@mail.valuecoders.com>
2023-06-21 15:24:02 +0530

    POCOR-7508: Develop External Data Source - Exams page, status:Done

--- config/Migrations/20230616091126_POCOR7508.php
+++ config/Migrations/20230616091126_POCOR7508.php
@@ -14,17 +14,19 @@ class POCOR7508 extends AbstractMigration
     {
         $this->execute('INSERT INTO `config_items` 
             (`id`, `name`, `code`, `type`, `label`, `value`, `value_selection`,`default_value`, `editable`, `visible`, `field_type`, `option_type`, `created_user_id`, `created`) VALUES 
-            (1300, "Type", "external_data_exam_source_type", "External Data Source Exams", "Type", "OpenEMIS Exams", "Test", "None", 1, 1, "Dropdown", "external_data_exam_source_type", 1, CURRENT_DATE())');
+            (1300, "Type", "external_data_exam_source_type", "External Data Source - Exams", "Type", "OpenEMIS Exams", "Test", "None", 1, 1, "Dropdown", "external_data_exam_source_type", 1, CURRENT_DATE())');
 
 
-        $this->execute("INSERT INTO `config_item_options` (`option_type`, `option`, `value`, `order`, `visible`) values('external_data_exam_source_type','None','None','1','1')");
-        $this->execute("INSERT INTO `config_item_options` (`option_type`, `option`, `value`, `order`, `visible`) values('external_data_exam_source_type','OpenEMIS Exams','OpenEMIS Exams','2','1')");
-        $this->execute("INSERT INTO `config_item_options` (`option_type`, `option`, `value`, `order`, `visible`) values('external_data_exam_source_type','Custom','Custom','3','1')");
-        $this->execute("INSERT INTO `config_item_options` (`option_type`, `option`, `value`, `order`, `visible`) values('external_data_exam_source_type','Jordan CSPD','Jordan CSPD','4','1')");
+        $this->execute("INSERT INTO `config_item_options` (`option_type`, `option`, `value`, `order`, `visible`) values('external_data_exam_source_type','OpenEMIS Exams','OpenEMIS Exams','1','1')");
+
+        $this->execute("INSERT INTO `config_item_options` (`option_type`, `option`, `value`, `order`, `visible`) values('external_data_exam_source_type','Caribbean Examinations Council (CXC)','Caribbean Examinations Council (CXC)','2','1')");
+
+        $this->execute("INSERT INTO `config_item_options` (`option_type`, `option`, `value`, `order`, `visible`) values('external_data_exam_source_type','Pacific Schools Information Management System (PacSIMS)','Pacific Schools Information Management System (PacSIMS)','3','1')");
     }
 
     public function down()
     {
+        $this->execute('DELETE FROM `config_items` WHERE `code` = "external_data_exam_source_type"');
         $this->execute('DELETE FROM `config_item_options` WHERE `option_type` = "external_data_exam_source_type"');
     }
 }
