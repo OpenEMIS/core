c2ad32889d681251811c0138b1ffe6635cd6e4a9
Nitin Singh Bisht <nitin.singh@mail.valuecoders.com>
2023-06-26 12:20:18 +0530

    POCOR-7507: Change migrations Script for backup

--- config/Migrations/20230616094615_POCOR7507.php
+++ config/Migrations/20230616094615_POCOR7507.php
@@ -12,10 +12,16 @@ class POCOR7507 extends AbstractMigration
      */
     public function up()
     {
+        $this->execute('CREATE TABLE `zz_7507_config_items` LIKE `config_items`');
+        $this->execute('INSERT INTO `zz_7507_config_items` SELECT * FROM `config_items`');
+
         $this->execute('UPDATE `config_items` SET `type`="External Data Source - Identity" WHERE `code`="external_data_source_type" AND `type`="External Data Source"');
     }
     public function down()
     {
-        $this->execute('UPDATE `config_items` SET `type`="External Data Source - Identity" WHERE `code`="external_data_source_type" AND `type`="External Data Source Identity"');
+        // $this->execute('UPDATE `config_items` SET `type`="External Data Source - Identity" WHERE `code`="external_data_source_type" AND `type`="External Data Source Identity"');
+        $this->execute('DROP TABLE IF EXISTS `config_items`');
+        $this->execute('RENAME TABLE `zz_7507_config_items` TO `config_items`');
+
     }
 }
