657d225f2226054768b5a170620d1b0af8b64a21
anubhav0789 <jainanubhav67@yahoo.in>
2023-06-28 11:10:21 +0530

    POCOR-7534: code enhance for adminsitration module | Status: In review

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -333,18 +333,314 @@ class AppController extends Controller
 
     public function getIdBySecurityFunctionName($actionParam, $controllerParam){
         $name = '';
-        if($actionParam == 'Users' && $controllerParam == 'Securities'){
-            $name = 'Users';
-        }else if(($actionParam == 'UserGroups' || $actionParam == 'SystemGroups') && $controllerParam == 'Securities'){
-            $name = 'Groups';  
-        }else if($actionParam == 'Roles' && $controllerParam == 'Securities'){
-            $name = ($this->request->query['type'] == 'system') ? 'System Roles' : 'User Roles';
-        }else if($actionParam == 'Accounts' && $controllerParam == 'Securities'){
-            $name = 'Accounts';  
-        }else if($actionParam == 'UserGroupsList' && $controllerParam == 'Securities'){
-            $name = 'User Group List';  
+        if($controllerParam == 'Securities'){
+            if($actionParam == 'Users'){
+                $name = 'Users';
+            }else if(($actionParam == 'UserGroups' || $actionParam == 'SystemGroups')){
+                $name = 'Groups';  
+            }else if($actionParam == 'Roles'){
+                $name = ($this->request->query['type'] == 'system') ? 'System Roles' : 'User Roles';
+            }else if($actionParam == 'Accounts'){
+                $name = 'Accounts';  
+            }else if($actionParam == 'UserGroupsList'){
+                $name = 'User Group List';  
+            }
         }else if($controllerParam == 'Credentials'){
-            $name = 'Credentials';  
+            if($actionParam == 'add' || $actionParam == 'index' || $actionParam == 'view' || $actionParam == 'edit' ||  $actionParam == 'delete'){
+                $name = 'Credentials';  
+            }
+        }else if($controllerParam == 'Areas'){
+            if($actionParam == 'Levels' || $actionParam == 'AdministrativeLevels'){
+                $name = 'Area Levels';  
+            }else if($actionParam == 'Areas' || $actionParam == 'Administratives'){
+                $name = 'Areas';  
+            }
+        }else if($controllerParam == 'AcademicPeriods'){
+            if($actionParam == 'Levels'){
+                $name = 'Academic Period Levels';  
+            }else if($actionParam == 'Periods'){
+                $name = 'Academic Periods';  
+            }
+        }else if($controllerParam == 'Educations'){
+            if($actionParam == 'Systems'){
+                $name = 'Education Systems';  
+            }else if($actionParam == 'Levels'){
+                $name = 'Education Levels';  
+            }else if($actionParam == 'Cycles'){
+                $name = 'Education Cycles';  
+            }else if($actionParam == 'Programmes'){
+                $name = 'Education Programmes';  
+            }else if($actionParam == 'Grades'){
+                $name = 'Education Grades';  
+            }else if($actionParam == 'Stages' || $actionParam == 'GradeSubjects'){
+                $name = 'Setup';  
+            }
+        }else if($controllerParam == 'Attendances'){
+            if($actionParam == 'StudentMarkTypes' || $actionParam == 'StudentMarkTypeStatuses'){
+                $name = 'Attendances';  
+            }
+        }else if($controllerParam == 'FieldOptions'){
+            $actionParam = $this->request->params['pass'][0];
+            if(($actionParam == '' || $actionParam == 'index') || $actionParam == 'view' || $actionParam == 'edit' || $actionParam == 'add'  || $actionParam == 'remove' ||  $actionParam == 'transfer'){
+                $name = 'Setup';  
+            }
+        }else if($controllerParam == 'Labels'){
+            if($actionParam == '' || $actionParam == 'index' || $actionParam == 'view' || $actionParam == 'edit'){
+                $name = 'Labels';  
+            }
+        }else if($controllerParam == 'Configurations'){
+            if($actionParam == '' || $actionParam == 'index' || $actionParam == 'view' || $actionParam == 'edit'){
+                $name = 'Configurations';  
+            }else if($actionParam == 'AuthSystemAuthentications'){
+                $name = 'Authentication';  
+            }else if($actionParam == 'ExternalDataSource'){
+                $name = 'External Data Source';  
+            }else if($actionParam == 'ProductLists'){
+                $name = 'Product Lists';  
+            }else if($actionParam == 'Webhooks'){
+                $name = 'Webhooks';  
+            }
+        }else if($controllerParam == 'Themes'){
+            $controllerParam = 'Configurations';
+            if($actionParam == '' || $actionParam == 'index' || $actionParam == 'view' || $actionParam == 'edit'){
+                $name = 'Configurations';  
+            }
+        }else if($controllerParam == 'Notices'){
+            if($actionParam == '' || $actionParam == 'index' || $actionParam == 'view' || $actionParam == 'edit' || $actionParam == 'delete'){
+                $name = 'Notices';  
+            }
+        }else if($controllerParam == 'Risks'){
+            if($actionParam == 'Risks'){
+                $name = 'Risks';  
+            }
+        }else if($controllerParam == 'InstitutionCustomFields'){
+            if($actionParam == 'Fields' || $actionParam == 'Pages'){
+                $name = 'Institution';  
+            }
+        }else if($controllerParam == 'StudentCustomFields'){
+            if($actionParam == 'Fields' || $actionParam == 'Pages'){
+                $name = 'Student';  
+            }
+        }else if($controllerParam == 'StaffCustomFields'){
+            if($actionParam == 'Fields' || $actionParam == 'Pages'){
+                $name = 'Staff';  
+            }
+        }else if($controllerParam == 'Infrastructures'){
+            if($actionParam == 'Fields' || $actionParam == 'Pages' || $actionParam == 'LandPages' || $actionParam == 'LandTypes' || $actionParam == 'BuildingPages' || $actionParam == 'BuildingTypes' || $actionParam == 'FloorPages' || $actionParam == 'FloorTypes' || $actionParam == 'RoomPages' || $actionParam == 'RoomTypes'){
+                $name = 'Infrastructure';  
+            }
+        }else if($controllerParam == 'Locales'){
+            if($actionParam == '' || $actionParam == 'index' || $actionParam == 'view' || $actionParam == 'edit' || $actionParam == 'delete'){
+                $name = 'Languages';  
+            }
+        }else if($controllerParam == 'LocaleContents'){
+            if($actionParam == '' || $actionParam == 'index' || $actionParam == 'view' || $actionParam == 'edit' || $actionParam == 'delete'){
+                $name = 'Translations';  
+            }
+        }else if($controllerParam == 'ProfileTemplates'){
+            if($actionParam == 'Institutions' || $actionParam == 'InstitutionProfiles'){
+                $name = 'Institutions';  
+            }else if($actionParam == 'Staff' || $actionParam == 'StaffProfiles'){
+                $name = 'Staff';  
+            }else if($actionParam == 'Students' || $actionParam == 'StudentProfiles'){
+                $name = 'Students';  
+            }else if($actionParam == 'Classes' || $actionParam == 'ClassesProfiles'){
+                $name = 'Classes';  
+            }
+        }else if($controllerParam == 'Surveys'){
+            if($actionParam == 'Questions'){
+                $name = 'Questions';  
+            }else if($actionParam == 'Forms'){
+                $name = 'Forms';  
+            }else if($actionParam == 'Status'){
+                $name = 'Status';  
+            }else if($actionParam == 'Rules'){
+                $name = 'Rules';  
+            }
+        }else if($controllerParam == 'Rubrics'){
+            if($actionParam == 'Templates' || $actionParam == 'Sections' ||  $actionParam == 'Criterias' || $actionParam == 'Options'){
+                $name = 'Setup';  
+            }else if($actionParam == 'Status'){
+                $name = 'Status';  
+            }
+        }else if($controllerParam == 'Alerts'){
+            if($actionParam == 'Alerts'){
+                $name = 'Alerts';  
+            }else if($actionParam == 'Logs'){
+                $name = 'Logs';  
+            }else if($actionParam == 'AlertRules'){
+                $name = 'AlertRules';  
+            }
+        }else if($controllerParam == 'Trainings'){
+            if($actionParam == 'Courses'){
+                $name = 'Courses';  
+            }else if($actionParam == 'Sessions' || $actionParam == 'ImportTrainees'){
+                $name = 'Sessions';  
+            }else if($actionParam == 'Results' || $actionParam == 'ImportTrainingSessionTraineeResults'){
+                $name = 'Results';  
+            }else if($actionParam == 'Applications'){
+                $name = 'Applications';  
+            }
+        }else if($controllerParam == 'Competencies'){
+            if($actionParam == 'Templates' || $actionParam == 'Items' || $actionParam == 'Criterias'){
+                $name = 'Competency Setup';  
+            }else if($actionParam == 'Periods'){
+                $name = 'Periods';  
+            }else if($actionParam == 'GradingTypes'){
+                $name = 'GradingTypes';  
+            }else if($actionParam == 'ImportCompetencyTemplates'){
+                $name = 'Import Competency Templates';  
+            }
+        }else if($controllerParam == 'Outcomes'){
+            if($actionParam == 'Templates' || $actionParam == 'ImportOutcomeTemplates' || $actionParam == 'Criterias'){
+                $name = 'Outcome Setup';  
+            }else if($actionParam == 'Periods'){
+                $name = 'Periods';  
+            }else if($actionParam == 'GradingTypes'){
+                $name = 'Grading Types';  
+            }
+        }else if($controllerParam == 'Assessments'){
+            if($actionParam == 'Assessments'){
+                $name = 'Assessments';  
+            }else if($actionParam == 'GradingTypes' || $actionParam == 'GradingOptions'){
+                $name = 'Grading Types';  
+            }else if($actionParam == 'Status'){
+                $name = 'Status';  
+            }else if($actionParam == 'AssessmentPeriods'){
+                $name = 'Assessment Periods';  
+            }
+        }else if($controllerParam == 'ReportCards'){
+            if($actionParam == 'Templates'){
+                $name = 'Templates';  
+            }else if($actionParam == 'ReportCardEmail'){
+                $name = 'Email Templates';  
+            }else if($actionParam == 'Processes'){
+                $name = 'Processes';  
+            }
+        }else if($controllerParam == 'Examinations'){
+            if($actionParam == 'Exams'){
+                $name = 'Exams';  
+            }else if($actionParam == 'GradingTypes'){
+                $name = 'Grading Types';  
+            }else if($actionParam == 'ExamCentres' || $actionParam == 'ExamCentreExams'){
+                $name = 'Exam Centres';  
+            }else if($actionParam == 'ImportExaminationCentreRooms'){
+                $name = 'Import Examination Rooms';  
+            }else if($actionParam == 'RegisteredStudents' || $actionParam == 'RegistrationDirectory' || $actionParam == 'BulkStudentRegistration'){
+                $name = 'Registered Students';  
+            }else if($actionParam == 'NotRegisteredStudents' || $actionParam == 'RegistrationDirectory' || $actionParam == 'BulkStudentRegistration'){
+                $name = 'Not Registered Students';  
+            }else if($actionParam == 'ExamResults' || $actionParam == 'Results'){
+                $name = 'Results';  
+            }else if($actionParam == 'ImportResults'){
+                $name = 'Import Results';  
+            }else if($actionParam == 'ExamCentreLinkedInstitutions'){
+                $name = 'Exam Centre Invigilators';  
+            }else if($actionParam == 'ExamCentreInvigilators'){
+                $name = 'Exam Centre Linked Institutions';  
+            }else if($actionParam == 'ExamCentreSubjects'){
+                $name = 'Exam Centre Subjects';  
+            }else if($actionParam == 'ExamCentreRooms'){
+                $name = 'Exam Centre Rooms';  
+            }else if($actionParam == 'ExamCentreStudents'){
+                $name = 'Exam Centre Students';  
+            }
+        }else if($controllerParam == 'Scholarships'){
+            if($actionParam == 'Scholarships'){
+                $name = 'Scholarships';  
+            }else if($actionParam == 'Applications'){
+                $name = 'Applications';  
+            }else if($actionParam == 'Identities'){
+                $name = 'Identities';  
+            }else if($actionParam == 'Nationalities'){
+                $name = 'Nationalities';  
+            }else if($actionParam == 'Contacts'){
+                $name = 'Contacts';  
+            }else if($actionParam == 'Guardians'){
+                $name = 'Guardians';  
+            }else if($actionParam == 'Histories'){
+                $name = 'Histories';  
+            }else if($actionParam == 'RecipientPaymentStructures'){
+                $name = 'Payment Structures';  
+            }else if($actionParam == 'RecipientPayments'){
+                $name = 'Disbursements';  
+            }
+        }else if($controllerParam == 'UsersDirectory'){
+            if($actionParam == '' || $actionParam == 'index' || $actionParam == 'view' || $actionParam == 'edit'){
+                $name = 'Users Directory';
+            }
+        }else if($controllerParam == 'ScholarshipRecipients'){
+            if($actionParam == '' || $actionParam == 'index' || $actionParam == 'view' || $actionParam == 'edit'){
+                $name = 'Recipients';
+            }
+        }else if($controllerParam == 'ScholarshipRecipientInstitutionChoices'){
+            if($actionParam == '' || $actionParam == 'index' || $actionParam == 'view' || $actionParam == 'edit'){
+                $name = 'Institution Choices';
+            }
+        }else if($controllerParam == 'ScholarshipRecipientCollections'){
+            if($actionParam == '' || $actionParam == 'index' || $actionParam == 'view' || $actionParam == 'edit' ||  $actionParam == 'delete'){
+                $name = 'Collections';
+            }
+        }else if($controllerParam == 'ScholarshipRecipientAcademicStandings'){
+            if($actionParam == '' || $actionParam == 'index' || $actionParam == 'view' || $actionParam == 'edit' ||  $actionParam == 'delete'){
+                $name = 'Academic Standings';
+            }
+        }else if($controllerParam == 'ScholarshipApplicationInstitutionChoices'){
+            if($actionParam == '' || $actionParam == 'index' || $actionParam == 'view' || $actionParam == 'edit' ||  $actionParam == 'delete'){
+                $name = 'Institution Choices';
+            }
+        }else if($controllerParam == 'ScholarshipApplicationAttachments'){
+            if($actionParam == '' || $actionParam == 'index' || $actionParam == 'view' || $actionParam == 'edit' ||  $actionParam == 'delete'){
+                $name = 'Application Attachments';
+            }
+        }else if($controllerParam == 'StaffAppraisals'){
+            if($actionParam == 'Criterias'){
+                $name = 'Criterias';
+            }else if($actionParam == 'Forms' || $actionParam == 'Scores'){
+                $name = 'Forms';
+            }else if($actionParam == 'Types'){
+                $name = 'Types';
+            }else if($actionParam == 'Periods'){
+                $name = 'Periods';
+            }
+        }else if($controllerParam == 'Textbooks'){
+            if($actionParam == 'Textbooks'){
+                $name = 'Textbooks';
+            }else if($actionParam == 'ImportTextbooks'){
+                $name = 'Import Textbooks';
+            }
+        }else if($controllerParam == 'Meals'){
+            if($actionParam == 'programme'){
+                $name = 'Meals Programme';
+            }
+        }else if($controllerParam == 'Workflows'){
+            if($actionParam == 'Workflows'){
+                $name = 'Workflows';
+            }else if($actionParam == 'Steps'){
+                $name = 'Steps';
+            }else if($actionParam == 'Statuses'){
+                $name = 'Statuses';
+            }else if($actionParam == 'Actions'){
+                $name = 'Actions';
+            }else if($actionParam == 'Rules'){
+                $name = 'Rules';
+            }
+        }else if($controllerParam == 'Systems'){
+            if($actionParam == 'Updates'){
+                $name = 'Updates';
+            }
+        }else if($controllerParam == 'Calendars'){
+            if($actionParam == 'add' || $actionParam == 'index' || $actionParam == 'view' || $actionParam == 'edit' ||  $actionParam == 'delete'){
+                $name = 'Calendars';
+            }
+        }else if($controllerParam == 'MoodleApiLog'){
+            if($actionParam == 'add' || $actionParam == 'index' || $actionParam == 'view' || $actionParam == 'edit' ||  $actionParam == 'delete'){
+                $name = 'MoodleApi Log';
+            }
+        }else if($controllerParam == 'Archives'){
+            if($actionParam == 'add' || $actionParam == 'index' || $actionParam == 'view' || $actionParam == 'edit' ||  $actionParam == 'delete'){
+                $name = 'Archive';
+            }
         }
         $module = 'Administration';
         $SecurityFunctionsTbl = TableRegistry::get('security_functions');
