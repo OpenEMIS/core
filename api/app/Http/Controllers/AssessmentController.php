855143d1e8e0b8bccd4d6b1547a9da7d2cd184b5
Sachin Kumar - PHP <sachin.kumar1@mail.valuecoders.com>
2023-07-04 13:31:01 +0530

    POCOR-7546 | working on modifying responses of api | status: in progress

--- api/app/Http/Controllers/AssessmentController.php
+++ api/app/Http/Controllers/AssessmentController.php
@@ -15,6 +15,23 @@ class AssessmentController extends Controller
         $this->assessmentService = $assessmentService;
     }
 
+    public function getEducationGradeList(Request $request)
+    {
+        try {
+            
+            $data = $this->assessmentService->getEducationGradeList($request);
+            return $this->sendSuccessResponse("Assessment Education Grade List Found", $data);
+            
+        } catch (\Exception $e) {
+            Log::error(
+                'Failed to fetch Assessment Education Grade List from DB',
+                ['message'=> $e->getMessage(), 'trace' => $e->getTraceAsString()]
+            );
+             dd($e);
+            return $this->sendErrorResponse('Assessment Education Grade List Not Found');
+        }
+    }
+
     public function getAssessmentItemList(Request $request)
     {
         try {
@@ -29,6 +46,23 @@ class AssessmentController extends Controller
             );
              dd($e);
             return $this->sendErrorResponse('Assessment Item List Not Found');
+        }
+    }
+
+    public function getAssessmentPeriodList(Request $request)
+    {
+        try {
+            
+            $data = $this->assessmentService->getAssessmentPeriodList($request);
+            return $this->sendSuccessResponse("Assessment Period List Found", $data);
+            
+        } catch (\Exception $e) {
+            Log::error(
+                'Failed to fetch Assessment Period List from DB',
+                ['message'=> $e->getMessage(), 'trace' => $e->getTraceAsString()]
+            );
+             dd($e);
+            return $this->sendErrorResponse('Assessment Period List Not Found');
         }
     }
 }
