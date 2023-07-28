5591b10436c85c6f24054d6d61486c7547ce1fb4
divyaa <divya.vishwakarma@valuecoders.com>
2023-07-28 13:46:13 +0530

    POCOR-7619

--- plugins/Report/src/Model/Table/WorkflowStudentAdmissionTable.php
+++ plugins/Report/src/Model/Table/WorkflowStudentAdmissionTable.php
@@ -30,4 +30,15 @@ class WorkflowStudentAdmissionTable extends AppTable
             'autoFields' => false
         ]);
     }
+
+    //POCOR-7619
+    public function onExcelGetOpenemisNo(Event $event, Entity $entity)
+    {
+        $openemisNo = '';
+        if(!empty($entity['user'])){
+            $openemisNo = $entity['user']['openemis_no'];
+        }
+        
+        return $openemisNo;
+    }
 }
