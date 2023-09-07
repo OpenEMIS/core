a113c3bf4a0a9751dabb8694579f642fb42076af
Ravi Verma <ravi.verma@mail.valuecoders.com>
2023-09-07 16:27:08 +0530

    Worked on POCOR-7737

--- api/app/Http/Controllers/RegistrationController.php
+++ api/app/Http/Controllers/RegistrationController.php
@@ -219,6 +219,11 @@ class RegistrationController extends Controller
         try {
             $resp = $this->registrationService->institutionStudents($request);
 
+            if(is_array($resp)){
+                return $this->sendErrorResponse("The student should be between ".$resp['loweAgeLimit']." to ".$resp['upperAgeLimit']. " years old.");
+            }
+
+
             if($resp == 1){
                 return $this->sendSuccessResponse("Registration successful. We will contact you shortly.");
             }elseif($resp == 2){
