5ca6ab281af9fdbe977b0917d9b68e24a9574a4c
Ehteram Ahmad <ehteram333@gmail.com>
2023-10-12 13:16:25 +0530

    POCOR:7485 Directory and Institution > Student > Staff

---
+++ plugins/StaffCustomField/src/Model/Table/StaffCustomFormsFiltersTable.php
@@ -0,0 +1,10 @@
+<?php
+namespace StaffCustomField\Model\Table;
+
+use CustomField\Model\Table\CustomFieldOptionsTable;
+
+class StaffCustomFormsFiltersTable extends CustomFieldOptionsTable {
+	public function initialize(array $config): void {
+		parent::initialize($config);
+	}
+}
