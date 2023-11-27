f8954fb1fc9f759bee603a2ee40220345e63c586
Umairah Yusoff <uyusoff@kordit.com>
2023-11-27 09:25:41 +0000

    20231127151500_POCOR7963.php edited to include repeater questions

--- config/Migrations/20231127151500_POCOR7963.php
+++ config/Migrations/20231127151500_POCOR7963.php
@@ -15,10 +15,13 @@ class POCOR7963 extends AbstractMigration
         // Backup table
         $this->execute('CREATE TABLE `zz_7963_institution_survey_answers` LIKE `institution_survey_answers`');
         $this->execute('INSERT INTO `zz_7963_institution_survey_answers` SELECT * FROM `institution_survey_answers`');
+        $this->execute('CREATE TABLE `zz_7963_institution_repeater_survey_answers` LIKE `institution_repeater_survey_answers`');
+        $this->execute('INSERT INTO `zz_7963_institution_repeater_survey_answers` SELECT * FROM `institution_repeater_survey_answers`');
 
 
         // CREATE summary tables and INSERT new rows into report_queries table
         $this->execute('ALTER TABLE `institution_survey_answers` CHANGE `number_value` `number_value` BIGINT NULL DEFAULT NULL');
+        $this->execute('ALTER TABLE `institution_repeater_survey_answers` CHANGE `number_value` `number_value` BIGINT NULL DEFAULT NULL');
     }
          
     // rollback
@@ -28,6 +31,8 @@ class POCOR7963 extends AbstractMigration
         // Restore table
         $this->execute('DROP TABLE IF EXISTS `institution_survey_answers`');
         $this->execute('RENAME TABLE `zz_7963_institution_survey_answers` TO `institution_survey_answers`');
+        $this->execute('DROP TABLE IF EXISTS `institution_repeater_survey_answers`');
+        $this->execute('RENAME TABLE `zz_7963_institution_repeater_survey_answers` TO `institution_repeater_survey_answers`');
 
     }
 }
