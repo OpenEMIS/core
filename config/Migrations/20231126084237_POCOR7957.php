1e9f411c59a2a47f6eb9479557dda3b0414bba80
tixuz <hindolmirzo@gmail.com>
2023-11-30 09:04:46 +0300

    removed try-checks in migration

--- config/Migrations/20231126084237_POCOR7957.php
+++ config/Migrations/20231126084237_POCOR7957.php
@@ -7,36 +7,36 @@ class POCOR7957 extends AbstractMigration
     public function up()
     {
         // Backup table
-        try {
+//        try {
             $this->execute('CREATE TABLE `z_7957 _transfer_logs` LIKE `transfer_logs`');
-        } catch (\Exception $e) {
-
-        }
-        try {
+//        } catch (\Exception $e) {
+//
+//        }
+//        try {
             $this->execute('INSERT INTO `z_7957 _transfer_logs` SELECT * FROM `transfer_logs`');
-        } catch (\Exception $e) {
-
-        }
-        try {
+//        } catch (\Exception $e) {
+//
+//        }
+//        try {
             $this->execute("ALTER TABLE `transfer_logs` ADD `completed_on` datetime DEFAULT NULL after `generated_on`");
-        } catch (\Exception $e) {
-
-        }
+//        } catch (\Exception $e) {
+//
+//        }
     }
 
     // rollback
     public function down()
     {
         // Restore table
-        try {
+//        try {
             $this->execute('DROP TABLE IF EXISTS `transfer_logs`');
-        } catch (\Exception $e) {
-
-        }
-        try {
+//        } catch (\Exception $e) {
+//
+//        }
+//        try {
             $this->execute('RENAME TABLE `z_7957 _transfer_logs` TO `transfer_logs`');
-        } catch (\Exception $e) {
-
-        }
+//        } catch (\Exception $e) {
+//
+//        }
     }
 }
