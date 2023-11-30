e609d4d5a1f706c346ab724d01ac69d045f94b37
tixuz <hindolmirzo@gmail.com>
2023-11-30 09:06:39 +0300

    removed try-checks in migration

--- config/Migrations/20231030131755_POCOR7905.php
+++ config/Migrations/20231030131755_POCOR7905.php
@@ -9,7 +9,7 @@ class POCOR7905 extends AbstractMigration
     public function up()
     {
         // Backup table
-        try {
+//        try {
             $this->execute('CREATE TABLE IF NOT EXISTS `z_7905_security_functions` LIKE `security_functions`');
 
             $this->execute('INSERT IGNORE INTO `z_7905_security_functions` SELECT * FROM `security_functions`');
@@ -20,21 +20,21 @@ class POCOR7905 extends AbstractMigration
                                   AND `module` = 'Personal' 
                                   AND `category` = 'Scholarships' ");
 
-        } catch (\Exception $e) {
-
-        }
+//        } catch (\Exception $e) {
+//
+//        }
     }
 
 // rollback
     public
     function down()
     {
-        try {
+//        try {
         // Restore table
         $this->execute('DROP TABLE IF EXISTS `security_functions`');
         $this->execute('RENAME TABLE `z_7905_security_functions` TO `security_functions`');
-        } catch (\Exception $e) {
-
-        }
+//        } catch (\Exception $e) {
+//
+//        }
     }
 }
