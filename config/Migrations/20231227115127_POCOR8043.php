9784ff124d767cb4612f49ffc7b8a5a01eaf861b
Rishabh <rishabh.sharma1@mail.valuecoders.com>
2023-12-27 19:08:14 +0530

    POCOR-8043 Reports>Survey Reports: Report is showing error Status:Partially done

--- config/Migrations/20231227115127_POCOR8043.php
+++ config/Migrations/20231227115127_POCOR8043.php
@@ -16,7 +16,6 @@ class POCOR8043 extends AbstractMigration
         $this->execute('CREATE TABLE `zz_8043_report_progress` LIKE `report_progress`');
         $this->execute('INSERT INTO `zz_8043_report_progress` SELECT * FROM `report_progress`');
 
-
         // Alter table
         $this->execute("ALTER TABLE `report_progress` CHANGE `name` `name` VARCHAR(500) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL;");
     }
@@ -24,7 +23,6 @@ class POCOR8043 extends AbstractMigration
     // rollback
     public function down()
     {
-        
         // Restore table
         $this->execute('DROP TABLE IF EXISTS `report_progress`');
         $this->execute('RENAME TABLE `zz_8043_report_progress` TO `report_progress`');
