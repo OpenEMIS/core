943dbd82a84492224069e77da17d54f277ffc64d
Ravi Verma <ravi.verma@mail.valuecoders.com>
2024-01-04 16:37:32 +0530

    Worked on POCOR-7996

--- api/app/Http/Controllers/InstitutionController.php
+++ api/app/Http/Controllers/InstitutionController.php
@@ -1609,6 +1609,16 @@ class InstitutionController extends Controller
     public function addInstitutionStaffPayslip(StaffPayslipsRequest $request)
     {
         try {
+
+            //For POCOR-7772 Start
+            $checkPermission = checkPermission(['Profiles', 'StaffPayslips', 'add']);
+            
+            if(!$checkPermission){
+                return $this->sendAuthorizationErrorResponse();
+            }
+            //For POCOR-7772 End
+
+
             $data = $this->institutionService->addInstitutionStaffPayslip($request);
             
             if($data == 1){
@@ -1632,6 +1642,14 @@ class InstitutionController extends Controller
     public function addInstitutionStudentMealBenefits(InstitutionMealStudentsRequest $request)
     {
         try {
+            //For POCOR-7772 Start
+            $checkPermission = checkPermission(['Institutions', 'StudentMeals', 'add'], ['institution_id' => $request['institution_id']??0]);
+            
+            if(!$checkPermission){
+                return $this->sendAuthorizationErrorResponse();
+            }
+            //For POCOR-7772 End
+
             $data = $this->institutionService->addInstitutionStudentMealBenefits($request);
             
             if($data == 1){
@@ -1678,6 +1696,22 @@ class InstitutionController extends Controller
     public function addInstitution(InstitutionsAddRequest $request)
     {
         try {
+
+            //For POCOR-7772 Start
+            
+            $paramArray = [];
+            if(isset($request['id']) && $request['id'] > 0){
+                $paramArray['institution_id'] = $request['id'];  
+            }
+            
+            $checkPermission = checkPermission(['Institutions', 'Institutions', 'edit'], $paramArray);
+            
+            if(!$checkPermission){
+                return $this->sendAuthorizationErrorResponse();
+            }
+            //For POCOR-7772 End
+
+
             $data = $this->institutionService->addInstitution($request);
             
             if($data == 1){
