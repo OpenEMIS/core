9bb6f492edea55adf05fa652c39602faee960560
tixuz <hindolmirzo@gmail.com>
2024-01-16 09:40:21 +0300

    changed to the execute

--- config/Migrations/20240104154848_POCOR8054.php
+++ config/Migrations/20240104154848_POCOR8054.php
@@ -7,8 +7,8 @@ class POCOR8054 extends AbstractMigration
     public function up()
     {
         //backup
-        $this->execute('CREATE TABLE `z_8054_security_functions` LIKE `security_functions`');
-        $this->execute('INSERT INTO `z_8054_security_functions` SELECT * FROM `security_functions`');
+//        $this->execute('CREATE TABLE `z_8054_security_functions` LIKE `security_functions`');
+//        $this->execute('INSERT INTO `z_8054_security_functions` SELECT * FROM `security_functions`');
 
         //change security functions
         $this->execute("UPDATE `security_functions` 
@@ -17,19 +17,22 @@ WHERE `category` = 'Archive' ");
         $this->execute("UPDATE `security_functions` 
 SET `_view` = 'TransferLogs.index|TransferLogs.view|Transfer.view|Transfer.index',
     `_edit` = NULL,
-    `_add` = 'TransferLogs.add|TransferLogs.add|Transfer.add|Transfer.add',
+    `_add` = NULL,
+    `_execute` = 'TransferLogs.add|TransferLogs.add|Transfer.add|Transfer.add',
     `_delete` = NULL
 WHERE `category` = 'Data Management' AND `name` = 'Archive' ");
         $this->execute("UPDATE `security_functions` 
 SET `_view` = 'DataManagementCopy.index|DataManagementCopy.view|DataManagementCopy.index|CopyData.view||CopyData.index',
     `_edit` = NULL,
-    `_add` = 'DataManagementCopy.add|DataManagementCopy.add|DataManagementCopy.add|CopyData.add||CopyData.add',
+    `_add` = NULL,
+    `_execute` = 'DataManagementCopy.add|DataManagementCopy.add|DataManagementCopy.add|CopyData.add||CopyData.add',
     `_delete` = NULL 
 WHERE `category` = 'Data Management' AND `name` = 'Copy' ");
         $this->execute("UPDATE `security_functions` 
 SET `_view` = 'BackupLogs.index|BackupLogs.view|BackupLog.view|BackupLog.index',
     `_edit` = NULL,
-    `_add` = 'BackupLogs.add|BackupLogs.add|BackupLog.add|BackupLog.add',
+    `_add` = NULL,
+    `_execute` = 'BackupLogs.add|BackupLogs.add|BackupLog.add|BackupLog.add',
     `_delete` = NULL 
 WHERE `category` = 'Data Management' AND `name` = 'Backup' ");
     }
