39f46867905f5241495b4bbfb50becdaff30809c
Rishabh <rishabh.sharma1@mail.valuecoders.com>
2024-01-22 16:59:21 +0530

    POCOR-8092 Institutions > Students: The dropdown for Institution Grades is not showing even though there are Institution Programmes Status:Done

--- plugins/Institution/src/Model/Table/InstitutionGradesTable.php
+++ plugins/Institution/src/Model/Table/InstitutionGradesTable.php
@@ -130,13 +130,42 @@ public function indexBeforeQuery(Event $event, Query $query, ArrayObject $extra)
     $academicPeriodOptions = $AcademicPeriod->getYearList(['isEditable' => true]);
     $selectedAcademicPeriod = !is_null($this->request->query('academic_period_id')) ? $this->request->query('academic_period_id') : $AcademicPeriod->getCurrent();
     $this->controller->set(compact('academicPeriodOptions', 'selectedAcademicPeriod'));
+ /********POCOR-8092::Start**************** */
+    // Education Grades
+    $InstitutionEducationGrades = TableRegistry::get('Institution.InstitutionGrades');
+    $session = $this->Session;
+    $institutionId = $session->read('Institution.Institutions.id');
+    $educationGradesOptions = $InstitutionEducationGrades
+        ->find('list', [
+            'keyField' => 'EducationGrades.id',
+            'valueField' => 'EducationGrades.name'
+        ])
+        ->select([
+            'EducationGrades.id', 'EducationGrades.name'
+        ])
+        ->contain(['EducationGrades.EducationProgrammes.EducationCycles.EducationLevels.EducationSystems'])
+        ->where(['institution_id' => $institutionId])
+        ->where(['EducationSystems.academic_period_id' => $selectedAcademicPeriod])
+        ->order(['EducationGrades.name' => 'ASC'])//POCOR-7247
+        ->group('education_grade_id')
+        ->toArray();
+    $educationGradesOptions = ['-1' => __('All Grades')] + $educationGradesOptions;
+    $selectedEducationGrades = $this->queryString('education_grade_id', $educationGradesOptions);
+    $this->controller->set(compact('educationGradesOptions', 'selectedEducationGrades'));
+    $where = [];
+    if ((!empty($selectedEducationGrades)) && ($selectedEducationGrades != -1)) {
+        $where['EducationGrades.id'] = $selectedEducationGrades;
+    }
 
     $query->contain(['EducationGrades.EducationProgrammes.EducationCycles.EducationLevels.EducationSystems'])
     ->where([
-        'EducationSystems.academic_period_id' => $selectedAcademicPeriod
+        'EducationSystems.academic_period_id' => $selectedAcademicPeriod,$where
+        
     ])
     ->order(['EducationLevels.order' =>'ASC','EducationCycles.order'=>'ASC','EducationProgrammes.order' => 'ASC','EducationGrades.order' => 'ASC']); //POCOR-8021 //POCOR:8048:: Modify order
     $sortList = [$this->aliasField('start_date'), $this->aliasField('end_date')];
+    /********POCOR-8092:: end**************** */
+
 
     if (array_key_exists('sortWhitelist', $extra['options'])) {
         $sortList = array_merge($extra['options']['sortWhitelist'], $sortList);
