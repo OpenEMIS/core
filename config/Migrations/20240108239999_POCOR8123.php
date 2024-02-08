a0d26e39312717225014d79a767e0a6ad3e89a83
Umairah Yusoff <uyusoff@kordit.com>
2024-02-08 08:13:15 +0000

    20240108239999_POCOR8123.php edited online with Bitbucket

--- config/Migrations/20240108239999_POCOR8123.php
+++ config/Migrations/20240108239999_POCOR8123.php
@@ -11,7 +11,8 @@ class POCOR8123 extends AbstractMigration
         $this->execute('SET FOREIGN_KEY_CHECKS = 0');
         $this->execute('CREATE TABLE `z_8123_institution_staff_surveys` LIKE `institution_staff_surveys`');
         $this->execute('INSERT INTO `z_8123_institution_staff_surveys` SELECT * FROM `institution_staff_surveys`');
-        $this->execute('ALTER TABLE `institution_staff_surveys` DROP FOREIGN KEY `institution_staff_surveys_ibfk_6`; ALTER TABLE `institution_staff_surveys` ADD CONSTRAINT `institution_staff_surveys_ibfk_6` FOREIGN KEY (`parent_form_id`) REFERENCES `institution_surveys`(`survey_form_id`) ON DELETE RESTRICT ON UPDATE RESTRICT');
+        $this->execute('ALTER TABLE `institution_staff_surveys` DROP FOREIGN KEY `institution_staff_surveys_ibfk_6`'); 
+        $this->execute('ALTER TABLE `institution_staff_surveys` ADD CONSTRAINT `institution_staff_surveys_ibfk_6` FOREIGN KEY (`parent_form_id`) REFERENCES `institution_surveys`(`survey_form_id`) ON DELETE RESTRICT ON UPDATE RESTRICT');
         $this->execute('SET FOREIGN_KEY_CHECKS = 1');
 
     }
