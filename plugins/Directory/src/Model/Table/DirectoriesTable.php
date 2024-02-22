a702b154b2ef93e8de709a8b8f874c27a8f7d0e0
Prajakta K <prajakta.kalghatgi@mail.vinove.com>
2024-02-22 16:40:36 +0530

    POCOR-8133. Binary type fields needed| Status: Done

--- plugins/Directory/src/Model/Table/DirectoriesTable.php
+++ plugins/Directory/src/Model/Table/DirectoriesTable.php
@@ -244,12 +244,6 @@ class DirectoriesTable extends ControllerActionTable
         if ($primary) {
             $schema = $this->schema();
             $fields = $schema->columns();
-            /*POCOR-8133 Photo content field required so commenting
-            foreach ($fields as $key => $field) {
-                if ($schema->column($field)['type'] == 'binary') {
-                    unset($fields[$key]);
-                }
-            }*/
             return $query->select($fields);
         }
     }
