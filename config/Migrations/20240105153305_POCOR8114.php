5fa08d4f09f8bab1047cf1d4a58e533d6aa2a66b
Prajakta K <prajakta.kalghatgi@mail.vinove.com>
2024-02-23 12:25:41 +0530

    POCOR-8114.Migration script updated| Status: Done

--- config/Migrations/20240105153305_POCOR8114.php
+++ config/Migrations/20240105153305_POCOR8114.php
@@ -16,8 +16,17 @@ class POCOR8114 extends AbstractMigration
         $this->execute('CREATE TABLE `z_8114_security_functions` LIKE `security_functions`');
         $this->execute('INSERT INTO `z_8114_security_functions` SELECT * FROM `security_functions`');
 
-        $this->execute("UPDATE `security_functions` SET `_view` = 'Institutions.index|Institutions.view' WHERE `id` = 9178 AND `category`='Manuals' AND `name` = 'Institution'");
-        $this->execute("UPDATE `security_functions` SET `_edit` = 'Reports.edit' WHERE `id` = 9180 And `category`='Manuals' AND `name` = 'Reports'");
+        // Remove Manuals from Institution
+        $this->execute("DELETE FROM security_functions WHERE `controller` LIKE 'Manuals' AND `module` LIKE 'Administration' AND `category` LIKE 'Manuals'");
+        
+        // Insert new Manuals Category to Institution
+        $this->execute("INSERT INTO `security_functions` (`id`, `name`, `controller`, `module`, `category`, `parent_id`, `_view`, `_edit`, `_add`, `_delete`, `_execute`, `order`, `visible`, `description`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES (NULL, 'Institution', 'Manuals', 'Administration', 'Manuals', '5000', 'Institutions.index|Institutions.view', 'Institutions.edit', NULL, NULL, NULL, '284', '1', NULL, NULL, NULL, '2', '2024-01-30 00:01:04');");
+        $this->execute("INSERT INTO `security_functions` (`id`, `name`, `controller`, `module`, `category`, `parent_id`, `_view`, `_edit`, `_add`, `_delete`, `_execute`, `order`, `visible`, `description`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES (NULL, 'Directory', 'Manuals', 'Administration', 'Manuals', '5000', 'Directory.index|Directory.view', 'Directory.edit', NULL, NULL, NULL, '285', '1', NULL, NULL, NULL, '2', '2024-01-30 00:01:04');");
+        $this->execute("INSERT INTO `security_functions` (`id`, `name`, `controller`, `module`, `category`, `parent_id`, `_view`, `_edit`, `_add`, `_delete`, `_execute`, `order`, `visible`, `description`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES (NULL, 'Reports', 'Manuals', 'Administration', 'Manuals', '5000', 'Reports.index|Reports.view', 'Reports.edit', NULL, NULL, NULL, '286', '1', NULL, NULL, NULL, '2', '2024-01-30 00:01:04');");
+        $this->execute("INSERT INTO `security_functions` (`id`, `name`, `controller`, `module`, `category`, `parent_id`, `_view`, `_edit`, `_add`, `_delete`, `_execute`, `order`, `visible`, `description`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES (NULL, 'Administration', 'Manuals', 'Administration', 'Manuals', '5000', 'Administration.index|Administration.view', 'Administration.edit', NULL, NULL, NULL, '287', '1', NULL, NULL, NULL, '2', '2024-01-30 00:01:04');");
+        $this->execute("INSERT INTO `security_functions` (`id`, `name`, `controller`, `module`, `category`, `parent_id`, `_view`, `_edit`, `_add`, `_delete`, `_execute`, `order`, `visible`, `description`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES (NULL, 'Personal', 'Manuals', 'Administration', 'Manuals', '5000', 'Personal.index|Personal.view', 'Personal.edit', NULL, NULL, NULL, '288', '1', NULL, NULL, NULL, '2', '2024-01-30 00:01:04');");
+        $this->execute("INSERT INTO `security_functions` (`id`, `name`, `controller`, `module`, `category`, `parent_id`, `_view`, `_edit`, `_add`, `_delete`, `_execute`, `order`, `visible`, `description`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES (NULL, 'Guardian', 'Manuals', 'Administration', 'Manuals', '5000', 'Guardian.index|Guardian.view', 'Guardian.edit', NULL, NULL, NULL, '289', '1', NULL, NULL, NULL, '2', '2024-01-30 00:01:04');");
+    
     }
 
     public function down()
