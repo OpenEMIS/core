97ec64a547ff38141b8e89e1c658e5e34ce9f70d
anubhav0789 <jainanubhav67@yahoo.in>
2024-02-23 12:36:33 +0530

    POCOR-7485-A20: working on Instituion > Transport > Providers | Status: Done

--- plugins/Institution/src/Controller/InstitutionsController.php
+++ plugins/Institution/src/Controller/InstitutionsController.php
@@ -2480,6 +2480,8 @@ class InstitutionsController extends AppController
                     $params[$model->aliasField('feeder_institution_id IS')] = $institutionID;
                     $exists = $model->exists($params);
                 } else {
+                   /* echo "<pre>"; print_r($params);
+die;*/
                     $checkExists = function ($model, $params) {
                         return $model->exists($params);
                     };
