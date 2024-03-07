f7c749c39a32c2ab0a8ede5f42097b8e9c3d7388
Ravi Verma <ravi.verma@mail.valuecoders.com>
2024-03-07 16:42:24 +0530

    Worked on POCOR-8078

--- api/app/Http/Controllers/MealController.php
+++ api/app/Http/Controllers/MealController.php
@@ -126,5 +126,100 @@ class MealController extends Controller
             return $this->sendErrorResponse('Meal Targets List Not Found');
         }
     }
+
+
+    public function getMealImplementers(Request $request)
+    {
+        try {
+            $options = $request->all();
+            $data = $this->mealService->getMealImplementers($options);
+
+            return $this->sendSuccessResponse("Meal Implementers List Found", $data);
+            
+        } catch (\Exception $e) {
+            Log::error(
+                'Failed to fetch Meal Implementers List from DB',
+                ['message'=> $e->getMessage(), 'trace' => $e->getTraceAsString()]
+            );
+
+            return $this->sendErrorResponse('Meal Implementers List Not Found');
+        }
+    }
+
+
+    public function getMealNutritions(Request $request)
+    {
+        try {
+            $options = $request->all();
+            $data = $this->mealService->getMealNutritions($options);
+
+            return $this->sendSuccessResponse("Meal Nutritions List Found", $data);
+            
+        } catch (\Exception $e) {
+            Log::error(
+                'Failed to fetch Meal Nutritions List from DB',
+                ['message'=> $e->getMessage(), 'trace' => $e->getTraceAsString()]
+            );
+
+            return $this->sendErrorResponse('Meal Nutritions List Not Found');
+        }
+    }
+
+
+    public function getMealRatings(Request $request)
+    {
+        try {
+            $options = $request->all();
+            $data = $this->mealService->getMealRatings($options);
+
+            return $this->sendSuccessResponse("Meal Ratings List Found", $data);
+            
+        } catch (\Exception $e) {
+            Log::error(
+                'Failed to fetch Meal Ratings List from DB',
+                ['message'=> $e->getMessage(), 'trace' => $e->getTraceAsString()]
+            );
+
+            return $this->sendErrorResponse('Meal Ratings List Not Found');
+        }
+    }
+
+
+    public function getMealStatusTypes(Request $request)
+    {
+        try {
+            $options = $request->all();
+            $data = $this->mealService->getMealStatusTypes($options);
+
+            return $this->sendSuccessResponse("Meal Status Types List Found", $data);
+            
+        } catch (\Exception $e) {
+            Log::error(
+                'Failed to fetch Meal Status Types List from DB',
+                ['message'=> $e->getMessage(), 'trace' => $e->getTraceAsString()]
+            );
+
+            return $this->sendErrorResponse('Meal Status Types List Not Found');
+        }
+    }
+
+
+    public function getMealFoodTypes(Request $request)
+    {
+        try {
+            $options = $request->all();
+            $data = $this->mealService->getMealFoodTypes($options);
+
+            return $this->sendSuccessResponse("Meal Food Types List Found", $data);
+            
+        } catch (\Exception $e) {
+            Log::error(
+                'Failed to fetch Meal Food Types List from DB',
+                ['message'=> $e->getMessage(), 'trace' => $e->getTraceAsString()]
+            );
+
+            return $this->sendErrorResponse('Meal Food Types List Not Found');
+        }
+    }
     //For POCOR-8078 End...
 }
--- api/routes/api.php
+++ api/routes/api.php
@@ -419,6 +419,11 @@ Route::group(
         //POCOR-8078 starts
         Route::get('meal-programmes/{mealProgrammeId}', 'MealController@getMealProgrammeData');
         Route::get('meal-targets', 'MealController@getMealTargets');
+        Route::get('meal-implementers', 'MealController@getMealImplementers');
+        Route::get('meal-nutritions', 'MealController@getMealNutritions');
+        Route::get('meal-ratings', 'MealController@getMealRatings');
+        Route::get('meal-status', 'MealController@getMealStatusTypes');
+        Route::get('meal-food-types', 'MealController@getMealFoodTypes');
         //POCOR-8078 ends
     }
 );
