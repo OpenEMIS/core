2cf0c488186d750ad72d02acbdab759817aa8f07
Dr Khindol <khindol.madraimov@gmail.com>
2024-03-18 12:20:00 +0300

    adding textbook-2

--- plugins/Institution/templates/Element/Textbooks/controls.php
+++ plugins/Institution/templates/Element/Textbooks/controls.php
@@ -2,10 +2,13 @@
     <div class="toolbar-responsive panel-toolbar">
         <div class="toolbar-wrapper">
             <?php
+//            die($encodedQueryString);
                 $baseUrl = $this->Url->build([
                     'plugin' => $this->request->getParam('plugin'),
                     'controller' => $this->request->getParam('controller'),
-                    'action' => $this->request->getParam('action')
+                    'action' => $this->request->getParam('action'),
+                    '0' => 'index',
+                    '1' => $encodedQueryString,
                 ]);
                 $template = $this->ControllerAction->getFormTemplate();
                 $this->Form->templates($template);
