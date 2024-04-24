23f6ff28faee486c8af55884dd968b3eee126275
divyaa <divya.vishwakarma@valuecoders.com>
2024-04-24 13:58:33 +0530

    POCOR-8169||chnages done

--- config/Migrations/20240306180000_POCOR8169.php
+++ config/Migrations/20240306180000_POCOR8169.php
@@ -38,7 +38,7 @@ class POCOR8169 extends AbstractMigration
        /* $this->execute("ALTER TABLE `institution_students` DROP PRIMARY KEY, ADD PRIMARY KEY( `student_status_id`, `student_id`, `education_grade_id`, `academic_period_id`, `start_date`, `end_date`, `institution_id` )");*/
 
        $this->execute("ALTER TABLE `institution_students` ADD UNIQUE KEY `unique_institution_students` (`student_status_id`, `student_id`, `education_grade_id`, `academic_period_id`, `start_date`, `end_date`, `institution_id`)");
-       // Drop existing primary key
+       // Drop existing primary key 
         $this->execute("ALTER TABLE `institution_students` DROP PRIMARY KEY");
 
         // Add new primary key with auto-increment column
