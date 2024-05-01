423131249ff0e2bd474a724e9f641480e247d8fb
Prajakta K <prajakta.kalghatgi@mail.vinove.com>
2024-05-01 15:24:08 +0530

    POCOR-8241.Add building issue resolved.|Status: Done

--- plugins/Institution/src/Model/Table/InstitutionBuildingsTable.php
+++ plugins/Institution/src/Model/Table/InstitutionBuildingsTable.php
@@ -88,8 +88,12 @@ class InstitutionBuildingsTable extends ControllerActionTable
                     'rule' => ['inAcademicPeriod', 'academic_period_id', []]
                 ],
                 'ruleCompareDateReverse' => [
-                    'rule' => ['compareDateReverse', 'start_date', true]
+                    'rule' => ['compareDateReverse', 'start_date', true],
+                    'on' => function ($context) { //POCOR-8241 -- Date validation when start_date is not empty
+                        return !empty($context['data']['start_date']);
+                    }
                 ]
+
             ])
             ->add('new_start_date', [
                 'ruleCompareDateReverse' => [
