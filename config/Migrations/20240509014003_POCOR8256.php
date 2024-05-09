80b30549eff76dcbe19025196cd2b993d28fbc64
divyaa <divya.vishwakarma@valuecoders.com>
2024-05-09 17:43:42 +0530

    POCOR-8256||working

---
+++ config/Migrations/20240509014003_POCOR8256.php
@@ -0,0 +1,50 @@
+<?php
+use Migrations\AbstractMigration;
+use Cake\Utility\Text;
+
+class POCOR8256 extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+
+    public function up()
+    {
+        // backup locale_contents table
+        $this->execute('CREATE TABLE `z_8256_locale_contents` LIKE `locale_contents`');
+        $this->execute('INSERT INTO `z_8256_locale_contents` SELECT * FROM `locale_contents`');
+
+        $this->execute('CREATE TABLE `z_8256_training_sessions` LIKE `training_sessions`');
+        $this->execute('INSERT INTO `z_8256_training_sessions` SELECT * FROM `training_sessions`');
+
+        #Insert data into locale_contents table
+        $this->execute('INSERT INTO `locale_contents` (`en`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES ("Training Center",NULL,NULL,"1",NOW())');
+        $this->execute('INSERT INTO `locale_contents` (`en`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES ("Training Evaluators",NULL,NULL,"1",NOW())');
+
+        // Create new column in training_sessions table
+        $this->execute("ALTER TABLE `training_sessions` ADD COLUMN `training_center` varchar(100)  AFTER `area_id`");
+
+        // create training_session_evaluators table
+       $this->execute('CREATE TABLE `training_session_evaluators` (
+                      `id` int(11) NOT NULL AUTO_INCREMENT,
+                      `training_session_id` int(11) NOT NULL,
+                      `evaluator_id` int(11) NOT NULL,
+                      `modified_user_id` int(11) DEFAULT NULL,
+                      `modified` datetime DEFAULT NULL,
+                      `created_user_id` int(11) DEFAULT NULL,
+                      `created` datetime DEFAULT NULL,
+                        PRIMARY KEY (`id`)
+                      )ENGINE=InnoDB DEFAULT CHARSET=utf8');
+    }
+
+    public function down() {
+        $this->execute('DROP TABLE IF EXISTS `locale_contents`');
+        $this->execute('RENAME TABLE `z_8256_locale_contents` TO `locale_contents`');
+        $this->execute('DROP TABLE IF EXISTS `training_sessions`');
+        $this->execute('RENAME TABLE `z_8256_training_sessions` TO `training_sessions`');
+    }
+}
