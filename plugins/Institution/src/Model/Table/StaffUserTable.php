f0ad07c644210e3732a0faf4474ac475f422a158
PrajaktaKV <prajakta.kalghatgi@mail.vinove.com>
2024-06-25 16:32:15 +0530

    POCOR-8364.User can now save staff data.|Status: Done

--- plugins/Institution/src/Model/Table/StaffUserTable.php
+++ plugins/Institution/src/Model/Table/StaffUserTable.php
@@ -566,7 +566,7 @@ class StaffUserTable extends ControllerActionTable
     public function editAfterSave(Event $event, Entity $entity)
     {
         //POCOR-5070:start
-        $staffT = TableRegistry::get('institution_staff');
+        $staffT = TableRegistry::get('Institution.Staff');//POCOR-8364
         $entityData = $staffT->find('all', ['conditions' => ['staff_id' => $entity->id]])->first();
         $entityData->is_homeroom = $entity->is_homeroom;
         $saveData = $staffT->save($entityData);
@@ -650,10 +650,12 @@ class StaffUserTable extends ControllerActionTable
 
                 }
             }
-            $institutionShifts = TableRegistry::get('institution_shifts');
-            $shiftOptions = TableRegistry::get('shift_options');
-            $institutionStaffShifts = TableRegistry::get('institution_staff_shifts');
-            $res = $institutionShifts->find()->select(['name' => 'shift_options.name'])
+            //POCOR-8364
+            $institutionShifts = TableRegistry::get('Institution.InstitutionShifts');
+            $shiftOptions = TableRegistry::get('Institution.ShiftOptions');
+            $institutionStaffShifts = TableRegistry::get('Institution.InstitutionStaffShifts');
+            //POCOR-8364
+            $res = $institutionShifts->find()->select(['name' => 'name'])
                 ->leftJoin(
                     [$shiftOptions->getAlias() => $shiftOptions->getTable()],
                     [
@@ -666,7 +668,7 @@ class StaffUserTable extends ControllerActionTable
                         $institutionStaffShifts->aliasField('shift_id = ') . $institutionShifts->aliasField('id')
                     ]
                 )
-                ->where([$institutionStaffShifts->aliasField('staff_id') => $entity->id])->order($institutionShifts->aliasField('id'))->group('shift_options.name')->order('shift_options.name')->toArray();
+                ->where([$institutionStaffShifts->aliasField('staff_id') => $entity->id])->order($institutionShifts->aliasField('id'))->group('name')->order('name')->toArray();
             $shift = '';
             foreach ($res as $key => $value) {
                 $shift .= $value['name'] . ',';
@@ -713,11 +715,14 @@ class StaffUserTable extends ControllerActionTable
                 'role_name' => ($role == 1) ? 'staff' : NULL
             ];
             //POCOR-6805 start
-            $Guardians = TableRegistry::get('staff_custom_field_values');
-            $staffCustomFieldOptions = TableRegistry::get('staff_custom_field_options');
-            $staffCustomFields = TableRegistry::get('staff_custom_fields');
-            $staffCustomFormsFields = TableRegistry::get('staff_custom_forms_fields');
+            //POCOR-8364
+            $Guardians = TableRegistry::get('StaffCustomField.StaffCustomFieldValues');
+            $staffCustomFieldOptions = TableRegistry::get('StaffCustomField.StaffCustomFieldOptions');
+            $staffCustomFields = TableRegistry::get('StaffCustomField.StaffCustomFields');
+            $staffCustomFormsFields = TableRegistry::get('StaffCustomField.StaffCustomFormsFields');
+            //POCOR-8364
             //POCOR-6805 start
+            
             $guardianData = $Guardians->find()
                 ->select([
                     'id' => $Guardians->aliasField('id'),
@@ -733,12 +738,14 @@ class StaffUserTable extends ControllerActionTable
                     'name' => 'staffCustomField.name',
                     'staff_custom_id' => 'staffCustomField.id',
                     'field_type' => 'staffCustomField.field_type',
-                ])->leftJoin(
+                ])
+                ->leftJoin(
                     ['staffCustomField' => 'staff_custom_fields'],
                     [
                         'staffCustomField.id = ' . $Guardians->aliasField('staff_custom_field_id')
                     ]
-                )->leftJoin(
+                )
+                ->leftJoin(
                     ['staffCustomFieldOptions' => 'staff_custom_field_options'],
                     [
                         'staffCustomFieldOptions.id = ' . $Guardians->aliasField('number_value')
@@ -746,7 +753,9 @@ class StaffUserTable extends ControllerActionTable
                 )
                 ->where([
                     $Guardians->aliasField('staff_id') => $user_id,
-                ])->hydrate(false)->toArray();
+                ])
+                ->toArray();
+
             $custom_field = array();
             $count = 0;
             if (!empty($guardianData)) {
