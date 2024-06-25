c64bd537a939ec19bc24c6643b834d1eeee21842
Dr Khindol <khindol.madraimov@gmail.com>
2024-06-26 01:28:47 +0300

    added bypass for ajax

--- plugins/Student/src/Controller/StudentsController.php
+++ plugins/Student/src/Controller/StudentsController.php
@@ -7,6 +7,7 @@ use ArrayObject;
 use Cake\Event\Event;
 use Cake\ORM\Query;
 use Cake\ORM\Table;
+use Cake\Log\Log;
 use Cake\ORM\TableRegistry;
 use Cake\Routing\Router;
 use Cake\Utility\Inflector;
@@ -293,7 +294,7 @@ class StudentsController extends AppController
     {
         $this->ControllerAction->process(['alias' => __FUNCTION__, 'className' => 'Health.Medications']);
     }
-    
+
 
     public function HealthTests()
     {
@@ -326,7 +327,7 @@ class StudentsController extends AppController
     public function SpecialNeedsPlans()
     {
         $this->ControllerAction->process(['alias' => __FUNCTION__, 'className' => 'SpecialNeeds.SpecialNeedsPlans']);
-    } 
+    }
 
     public function SpecialNeedsDiagnostics()
     {
@@ -545,7 +546,7 @@ class StudentsController extends AppController
         $this->Navigation->addCrumb('Institutions', ['plugin' => 'Institution', 'controller' => 'Institutions', 'action' => 'Institutions', 'index']);
         $action = $this->request->getAttribute('params')['action'];
         $institutionID = $this->getInstitutionID();
-        
+
         $activeInstitution = $this->Institutions->get($institutionID);
         $institutionName = $activeInstitution->name;
 
@@ -611,7 +612,7 @@ class StudentsController extends AppController
         $controller = $request->getParam('controller');
         $plugin = $request->getParam('plugin');
         $furtherAction = $pass[0];
-
+//        Log::debug(print_r([$pass, $action, $controller, $plugin, $furtherAction], true));
         // if (($furtherAction == 'index' || $furtherAction == 'add' || $furtherAction == 'import')
         //     && ($action == 'Students')
         //     && ($plugin == 'Student')
@@ -621,7 +622,7 @@ class StudentsController extends AppController
         if ($pass[0] == 'download' && ($action == 'Qualifications') && ($plugin == 'Student') && ($controller == 'Students')) {
             return true;
         }
-        if ($furtherAction == 'image' || $furtherAction == 'download') {
+        if ($furtherAction == 'image' || $furtherAction == 'download' || $furtherAction == 'ajaxReferrerAutocomplete') {
             return true;
         }
 //        $this->log(print_r($request,true), debug);
@@ -748,7 +749,7 @@ class StudentsController extends AppController
         /*if($studentID == null){
             $studentID =  $this->getUserID();
         }*/
-        
+
         $institutionID = $this->getInstitutionID();
         if ($this->StudentUser->exists([$this->StudentUser->getPrimaryKey() => $studentID])) {
             $entity = $this->StudentUser->get($studentID);
