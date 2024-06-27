3fe9d7698724d3ad8dd3adba931af11796eeb7b4
Ehsan Khan <ehsan@madvit.com>
2024-06-27 08:27:08 +0530

    POCOR-8346: Bug fix

--- plugins/ProfileTemplate/src/Model/Table/StaffProfilesTable.php
+++ plugins/ProfileTemplate/src/Model/Table/StaffProfilesTable.php
@@ -904,7 +904,7 @@ class StaffProfilesTable extends ControllerActionTable
             $inProgress = $StaffReportCardProcesses->find()
                 ->where([
                     $StaffReportCardProcesses->aliasField('staff_profile_template_id') => $params['staff_profile_template_id'],
-                    $StaffReportCardProcesses->aliasField('staff_id') => $params['staff_id'],
+                    // $StaffReportCardProcesses->aliasField('staff_id') => $params['staff_id'],
                     $StaffReportCardProcesses->aliasField('academic_period_id') => $params['academic_period_id'],
                     $StaffReportCardProcesses->aliasField('institution_id') => $params['institution_id'],
                 ])
