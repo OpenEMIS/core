1ee7c45ac2ffdb3a764ca797533da04aad7f9081
Admin <abhi@mail.valuecoders.com>
2024-07-12 11:20:15 +0530

    solved bug of login

--- frontend/src/app/student-meals/student-meals.component.ts
+++ frontend/src/app/student-meals/student-meals.component.ts
@@ -498,7 +498,7 @@ export class StudentMealsComponent extends KdPageBase implements OnInit {
   }
 
   loginData() {
-    this.Rest.setSession();
+    // this.Rest.setSession();
     let token = localStorage.getItem("loginToken");
     if (!token) {
       let userName = sessionStorage.getItem('username');
