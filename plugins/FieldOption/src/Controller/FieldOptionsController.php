4e87ad6cf981e3d1b7ab7aff3284785870a480b7
abhinav0789 <abhinav.kumar@madvit.com>
2024-07-16 12:58:55 +0530

    POCOR-8470: code enahce for Administration > System Setup > FieldOption | Status: Done

--- plugins/FieldOption/src/Controller/FieldOptionsController.php
+++ plugins/FieldOption/src/Controller/FieldOptionsController.php
@@ -8,6 +8,7 @@ use Cake\Event\Event;
 use Cake\ORM\Table;
 use Cake\Utility\Inflector;
 use Cake\ORM\TableRegistry;
+use Cake\Event\EventInterface;//POCOR-8470
 
 class FieldOptionsController extends AppController
 {
@@ -17,7 +18,7 @@ class FieldOptionsController extends AppController
         $this->loadComponent('FieldOption.FieldOption');
     }
 
-    public function beforeFilter(Event $event)
+    public function beforeFilter(EventInterface $event)
     {
         if ($this->getPlugin() == 'FieldOption') {
             $this->Security->setConfig('validatePost', false);
@@ -795,7 +796,7 @@ class FieldOptionsController extends AppController
     }
     //POCOR-7363 end
 
-    public function beforeRender(Event $event)
+    public function beforeRender(EventInterface $event)
     {
         parent::beforeRender($event);
         $this->viewBuilder()->addHelper('ControllerAction.ControllerAction');
