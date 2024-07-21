d973e661f86a69607063f7778d113a788384c452
Dr Khindol <khindol.madraimov@gmail.com>
2024-07-21 11:25:11 +0300

    fixed and simplified some array_key_exist to isset in RecordBehaviour.php

--- plugins/CustomField/src/Model/Behavior/RecordBehavior.php
+++ plugins/CustomField/src/Model/Behavior/RecordBehavior.php
@@ -491,7 +491,8 @@ class RecordBehavior extends Behavior
                                     ])
                                     ->toArray();
                             }
-                            if (!empty($surveyIds) && array_key_exists('repeaterValues', $settings instanceof \ArrayObject ? $settings->getArrayCopy() : $settings)) {
+                            // POCOR-8231 simplified isset
+                            if (!empty($surveyIds) && isset($settings['repeaterValues'])) {
                                 // always deleted all existing answers before re-insert
                                 $RepeaterSurveyAnswers->deleteAll([
                                     $RepeaterSurveyAnswers->aliasField('institution_repeater_survey_id IN ') => $surveyIds
@@ -522,9 +523,15 @@ class RecordBehavior extends Behavior
                                 ]);
                             }
                         }
-
-                        if(array_key_exists('repeaterValues', $settings instanceof \ArrayObject ? $settings->getArrayCopy() : $settings)){
-                            foreach ($settings['repeaterValues'] as $key => $value) {
+                        // POCOR-8436 if settings is an array
+                        // POCOR-8231 simplified and cleancoded
+                        if(is_array($settings)){
+                            $settingsArray = $settings;
+                        }else{
+                            $settingsArray = $settings->getArrayCopy();
+                        }
+                        if(isset($settingsArray['repeaterValues'])){
+                            foreach ($settingsArray['repeaterValues'] as $key => $value) {
                                 $surveyEntity = $RepeaterSurveys->newEntity($value);
                                 $all[] = $surveyEntity;
                                 if ($RepeaterSurveys->save($surveyEntity)) {
@@ -554,7 +561,8 @@ class RecordBehavior extends Behavior
                 } else {
                     $indexedErrors = [];
                     $fields = ['text_value', 'number_value', 'decimal_value', 'textarea_value', 'date_value', 'time_value', 'file'];
-                    if (array_key_exists('custom_field_values', $errors instanceof \ArrayObject ? $errors->getArrayCopy() : $errors)) {
+                    // POCOR-8231 simplified isset
+                    if (isset($errors['custom_field_values'])) {
                         if ($entity->has('custom_field_values')) {
                             foreach ($entity->custom_field_values as $key => $obj) {
                                 $fieldId = $obj->{$this->getConfig('fieldKey')};
@@ -570,11 +578,11 @@ class RecordBehavior extends Behavior
                     }
 
                     $indexedErrors = $indexedErrors + $fileErrors;
-
+                    // POCOR-8231 simplified isset
                     if (!empty($indexedErrors)) {
-                        if (array_key_exists($model->getAlias(), $data instanceof \ArrayObject ? $data->getArrayCopy() : $data)) {
-                            if (array_key_exists('custom_field_values', $data[$model->getAlias()])) {
-                                foreach ($data[$model->getAlias()]['custom_field_values'] as $key => $obj) {
+                        if (isset($data[$alias])) {
+                            if (isset($data[$alias]['custom_field_values'])) {
+                                foreach ($data[$alias]['custom_field_values'] as $key => $obj) {
                                     $fieldId = $obj[$this->getConfig('fieldKey')];
 
                                     if (isset($indexedErrors[$fieldId])) {
@@ -1330,7 +1338,8 @@ class RecordBehavior extends Behavior
             $fieldIds = $model->array_column($customFields, $fieldKey);
 
             $ignoreFields = ['id', 'modified_user_id', 'modified', 'created_user_id', 'created'];
-            if (isset(requestData['custom_field_values'])) {
+            // POCOR-8231 typo fixed
+            if (isset($requestData['custom_field_values'])) {
                 $newRequestData['custom_field_values'] = [];
                 foreach ($requestData['custom_field_values'] as $key => $fieldValue) {
                     if (in_array($fieldValue[$fieldKey], $fieldIds)) {
@@ -1342,8 +1351,8 @@ class RecordBehavior extends Behavior
                     }
                 }
             }
-
-            if (isset(requestData['custom_table_cells'])) {
+            // POCOR-8231 typo fixed
+            if (isset($requestData['custom_table_cells'])) {
                 $newRequestData['custom_table_cells'] = [];
                 foreach ($requestData['custom_table_cells'] as $key => $fieldCell) {
                     if (in_array($fieldCell[$fieldKey], $fieldIds)) {
