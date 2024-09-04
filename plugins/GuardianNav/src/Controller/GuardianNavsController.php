9729c81b0d3ce29c537e8d620c4480de276cf06f
Dr Khindol <khindol.madraimov@gmail.com>
2024-09-04 09:11:59 +0300

    added query String to params

--- plugins/GuardianNav/src/Controller/GuardianNavsController.php
+++ plugins/GuardianNav/src/Controller/GuardianNavsController.php
@@ -260,6 +260,15 @@ class GuardianNavsController extends AppController
 
     public function getAcademicTabElements($options = [])
     {
+        // POCOR-8415:start
+        $queryString = $this->ControllerAction->getQueryString();
+        if (empty($queryString)) {
+            $queryString = $this->getQueryString();
+        }
+        $studentId = $queryString['student_id'] ?? null;
+        $institutionStudentID = $queryString['institution_student_id'] ?? null;
+        $encodedQueryString = !empty($queryString) ? $this->ControllerAction->paramsEncode($queryString) : null;
+        // POCOR-8415:end
         $id = (isset($options['id']))? $options['id']: 0;
         $type = (isset($options['type']))? $options['type']: null;
         $period = (isset($options['academic_period']))? $options['academic_period']: null;
@@ -275,6 +284,9 @@ class GuardianNavsController extends AppController
             if(empty($studentId)){
                 $studentId = $this->request->getQueryParams()['studentId'];
                 $studentId = $this->ControllerAction->paramsDecode($studentId);
+            }//POCOR-8379 ends
+            if(empty($studentId)){
+                $studentId = $queryString['student_id'];
             }//POCOR-8379 ends
             if(!empty($studentId)) {
                 $StudentsTable = TableRegistry::getTableLocator()->get('Institution.Students');
@@ -315,12 +327,23 @@ class GuardianNavsController extends AppController
         ];
 
         $tabElements = array_merge($tabElements, $studentTabElements);
-
+        // POCOR-8415:start
+        if($encodedQueryString){
+            $queryString = $encodedQueryString;
+        }
+        // POCOR-8415:end
         foreach ($studentTabElements as $key => $tab) {
             if(!empty($period) && $key == 'Absences') {
-                $tabElements[$key]['url'] = array_merge($studentUrl, ['action' =>'Student'.$key,  'queryString' => $queryString,'academic_period' => $period]);
+                $tabElements[$key]['url'] = array_merge($studentUrl, [
+                    'action' =>'Student'.$key,
+                    'queryString' => $queryString,
+                    'academic_period' => $period]);
             } else {
-                $tabElements[$key]['url'] = array_merge($studentUrl, ['action' =>'Student'.$key, 'index', 'queryString' => $queryString, 'type' => $type]);
+                $tabElements[$key]['url'] = array_merge($studentUrl, [
+                    'action' =>'Student'.$key,
+                    '0' => 'index',
+                    '1' => $encodedQueryString,
+                'type' => $type]);
             }
         }
 
