7f871c51c5340c1c82834b368b00d60a99aba6f3
Dr Khindol <khindol.madraimov@gmail.com>
2024-10-22 08:40:19 +0300

    added table and angulars

---
+++ plugins/Institution/webroot/js/angular/results/assessment.item.exemptions.ctrl.js
@@ -0,0 +1,377 @@
+// Multi Select v.1.0.0
+agGrid.initialiseAgGridWithAngular1(angular);
+
+angular.module('assessment.item.exemptions.ctrl', [
+    'agGrid',
+    'kd-angular-multi-select',
+    'utils.svc',
+    'alert.svc',
+    'aggrid.locale.svc',
+    'assessment.item.exemptions.svc',
+    'institution.class.students.svc',
+    'angular.chosen'
+])
+    .controller('AssessmentItemExemptionsCtrl', AssessmentItemExemptionsController);
+
+AssessmentItemExemptionsController.$inject = [
+    '$scope',
+    '$q',
+    '$window',
+    '$http',
+    'UtilsSvc',
+    'AlertSvc',
+    'AggridLocaleSvc',
+    'InstitutionClassStudentsSvc',
+    'AssessmentItemExemptionsSvc',
+];
+
+function AssessmentItemExemptionsController(
+    $scope,
+    $q,
+    $window,
+    $http,
+    UtilsSvc,
+    AlertSvc,
+    AggridLocaleSvc,
+    InstitutionClassStudentsSvc,
+    AssessmentItemExemptionsSvc
+) {
+    const ctrl = this;
+
+    // Constants
+    const SUPPRESS_MENU = true;
+    const SUPPRESS_SORTING = true;
+    const DEFAULT_COLUMN_WIDTH = 200;
+    const CHECKBOX_WIDTH = 50;
+    ctrl.dataReady = false;
+    // Initialization
+    ctrl.assessment_period_id = null;
+    ctrl.assessment_item_id = null;
+    ctrl.assessment_item_ids = null;
+    ctrl.institution_class_id = null;
+    ctrl.selected_item_id = null;
+    ctrl.selected_items_ids = [];
+    ctrl.classification = null;
+    ctrl.bodyDir = getComputedStyle(document.body).direction;
+    ctrl.columnTopData = createColumnData();
+    ctrl.columnBottomData = createColumnData();
+    ctrl.rowTopData = [];
+    ctrl.rowBottomData = [];
+    ctrl.gridOptionsTop = createGridOptions();
+    ctrl.gridOptionsBottom = createGridOptions();
+    ctrl.colDef = getColumnDefinitions();
+    ctrl.topKey = 'id';
+    ctrl.bottomKey = 'id';
+    // Event Handlers
+    ctrl.onSubjectChange = onSubjectChange;
+    ctrl.onPeriodChange = onPeriodChange;
+    ctrl.textConfig = {
+        topCheckboxLabel: "Students",
+        topSearchPlaceholder: "Search Students",
+        topToBottomButton: "Exempt",
+        bottomToTopButton: "Unexempt",
+        bottomCheckboxLabel: "Students",
+        bottomSearchPlaceholder: "Search Students"
+    };
+    angular.element(document).ready(initPage);
+
+    function initPage() {
+        InstitutionClassStudentsSvc.init(angular.baseUrl);
+        AssessmentItemExemptionsSvc.init(angular.baseUrl);
+        UtilsSvc.isAppendLoader(true);
+
+        if (ctrl.institution_class_id &&
+            ctrl.assessment_item_id &&
+            ctrl.assessment_period_id) {
+            loadClassDetails();
+            // console.log('init');
+        }
+        UtilsSvc.isAppendLoader(false);
+
+    }
+
+    function getTeacherOptions() {
+        var promise = InstitutionAssociationsSvc.getTeacherOptions(
+            Controller.institutionId,
+            Controller.academicPeriodId
+        );
+        return promise.then(function (result) {
+            return result;
+        });
+    }
+
+    function createColumnData() {
+        return [{
+            headerName: "+",
+            field: "checkbox",
+            checkboxSelection: true,
+            suppressMenu: SUPPRESS_MENU,
+            suppressSorting: SUPPRESS_SORTING,
+            minWidth: CHECKBOX_WIDTH,
+            maxWidth: CHECKBOX_WIDTH,
+            pinned: 'left'
+        }];
+    }
+
+    function createGridOptions() {
+        return {
+            columnDefs: [],
+            rowData: [],
+            primaryKey: []
+        };
+    }
+
+    function getColumnDefinitions() {
+        return [
+            {headerName: 'OpenEMIS ID', field: 'openemis_no'},
+            {headerName: 'Name', field: 'name'},
+            {headerName: 'Gender', field: 'gender_name'},
+            {headerName: 'Student Status', field: 'student_status_name'},
+        ];
+    }
+
+    function onSubjectChange() {
+        UtilsSvc.isAppendLoader(true);
+        updateAssessmentItemIds();
+        // console.log(ctrl);
+        if (ctrl.institution_class_id &&
+            ctrl.assessment_item_id &&
+            ctrl.assessment_period_id) {
+
+            loadClassDetails();
+            // console.log('init');
+        }
+        UtilsSvc.isAppendLoader(false);
+    }
+
+    function onPeriodChange() {
+        UtilsSvc.isAppendLoader(true);
+        // console.log(ctrl);
+        if (ctrl.institution_class_id &&
+            ctrl.assessment_item_id &&
+            ctrl.assessment_period_id) {
+            loadClassDetails();
+            // console.log('init');
+        }
+        UtilsSvc.isAppendLoader(false);
+
+    }
+
+    function loadClassDetails() {
+        // console.log(options);
+        ctrl.message = '';
+        ctrl.messageClass = '';
+        const options = {
+            institution_class_id: ctrl.institution_class_id,
+            assessment_item_ids: ctrl.assessment_item_ids,
+            assessment_item_id: ctrl.assessment_item_id,
+            assessment_period_id: ctrl.assessment_period_id,
+            classification: ctrl.classification
+        }
+        // console.log(options);
+        //     .then(loadAdditionalClassData)
+        //     .then(processAdditionalClassData)
+        if (Array.isArray(options.assessment_item_ids) && options.assessment_item_ids.length > 0) {
+            options.assessment_item_id = options.assessment_item_ids[0];
+            delete options.assessment_item_ids;
+        }
+        ctrl.dataReady = false;
+        // console.log(options);
+        AssessmentItemExemptionsSvc.getExemptStudents(options)
+            .then(setClassDetails)
+            .then(translateColumnHeaders)
+            .then(translatedText => {
+                translatedText.forEach((text, index) => {
+                    ctrl.colDef[index].headerName = text;
+                });
+                ctrl.setTop(ctrl.colDef, ctrl.unexemptStudents);
+                ctrl.setBottom(ctrl.colDef, ctrl.exemptStudents);
+            })
+            .catch(handleError)
+            .finally(() => {
+                // console.log('Finally block executed');
+                ctrl.dataReady = true;
+                UtilsSvc.isAppendLoader(false);
+            });
+    }
+
+
+    function setClassDetails(response) {
+
+        ctrl.exemptStudents = [];
+        ctrl.unexemptStudents = [];
+
+        response.forEach(student => {
+            // console.log(student);
+            const studentData = {
+                openemis_no: student.openemis_no,
+                name: student.name,
+                gender_name: student.gender,
+                student_id: student.student_id,
+                student_status_name: student.student_status_name,
+                encodedVar: {
+                    s_id: student.student_id,
+                    eg_id: student.education_grade_id
+                }
+            };
+
+            // Check if the student is exempt or not
+            if (student.is_exempt) {
+                ctrl.exemptStudents.push(studentData);  // Add to exempt students
+            } else {
+                ctrl.unexemptStudents.push(studentData);  // Add to unexempt students
+            }
+        });
+        // console.log(ctrl.exemptStudents)
+        // console.log(ctrl.unexemptStudents)
+
+    }
+
+    function translateColumnHeaders() {
+        const toTranslate = ctrl.colDef.map(col => col.headerName);
+        return InstitutionClassStudentsSvc.translate(toTranslate);
+    }
+
+    function handleError(error) {
+
+        console.error(error);
+    }
+
+    ctrl.setTop = function (header, content, key = 'name') {
+        for (var i = 0; i < header.length; i++) {
+            header[i].suppressMenu = SUPPRESS_MENU;
+            header[i].filter = 'text';
+            header[i].width = DEFAULT_COLUMN_WIDTH;
+            header[i].minWidth = DEFAULT_COLUMN_WIDTH;
+            ctrl.columnTopData.push(header[i]);
+        }
+        if (ctrl.bodyDir != 'ltr') {
+            ctrl.columnTopData.reverse();
+        }
+        for (var i = 0; i < content.length; i++) {
+            if (content[i].checkbox == undefined) {
+                content[i].checkbox = '';
+            }
+        }
+        ctrl.rowTopData = content;
+        ctrl.topKey = key;
+        ctrl.gridOptionsTop.columnDefs = ctrl.columnTopData;
+        ctrl.gridOptionsTop.rowData = ctrl.rowTopData;
+        ctrl.gridOptionsTop.primaryKey = ctrl.topKey;
+        // console.log(ctrl.gridOptionsTop);
+
+    };
+
+    ctrl.setBottom = function (header, content, key = 'name') {
+        for (var i = 0; i < header.length; i++) {
+            header[i].suppressMenu = SUPPRESS_MENU;
+            header[i].filter = 'text';
+            header[i].width = DEFAULT_COLUMN_WIDTH;
+            header[i].minWidth = DEFAULT_COLUMN_WIDTH;
+            ctrl.columnBottomData.push(header[i]);
+        }
+        if (ctrl.bodyDir != 'ltr') {
+            ctrl.columnBottomData.reverse();
+        }
+
+        for (var i = 0; i < content.length; i++) {
+            if (content[i].checkbox == undefined) {
+                content[i].checkbox = '';
+            }
+        }
+        ctrl.rowBottomData = content;
+        ctrl.bottomKey = key;
+        ctrl.gridOptionsBottom.columnDefs = ctrl.columnBottomData;
+        ctrl.gridOptionsBottom.rowData = ctrl.rowBottomData;
+        ctrl.gridOptionsBottom.primaryKey = ctrl.bottomKey;
+        // console.log(ctrl.gridOptionsBottom);
+
+    };
+
+    function configureGrid(header, content, key, gridOptions, columnData) {
+        header.forEach(col => {
+            col.suppressMenu = SUPPRESS_MENU;
+            col.filter = 'text';
+            col.width = DEFAULT_COLUMN_WIDTH;
+            col.minWidth = DEFAULT_COLUMN_WIDTH;
+            columnData.push(col);
+        });
+
+        if (ctrl.bodyDir !== 'ltr') {
+            columnData.reverse();
+        }
+
+        content.forEach(row => {
+            row.checkbox = row.checkbox || '';
+        });
+
+        gridOptions.columnDefs = columnData;
+        gridOptions.rowData = content;
+        gridOptions.primaryKey = key;
+        gridOptions.rowData;
+    }
+
+    ctrl.postForm = function () {
+        const exemptStudents = ctrl.gridOptionsBottom.rowData.map(row => row.encodedVar);
+        const exempt_students = UtilsSvc.urlsafeBase64Encode(JSON.stringify(exemptStudents));
+        const unexemptStudents = ctrl.gridOptionsTop.rowData.map(row => row.encodedVar);
+        const unexempt_students = UtilsSvc.urlsafeBase64Encode(JSON.stringify(unexemptStudents));
+        const postData = {
+            classification:ctrl.classification,
+            assessment_item_id:ctrl.assessment_item_id,
+            assessment_item_ids:ctrl.assessment_item_ids,
+            assessment_period_id: ctrl.assessment_period_id,
+            institution_class_id: ctrl.institution_class_id,
+            exempt_students: exempt_students,
+            unexempt_students: unexempt_students,
+        };
+        UtilsSvc.isAppendLoader(true);
+            AssessmentItemExemptionsSvc.saveStudents(postData)
+                .then(handlePostSuccess)
+                .catch(handleError)
+                .finally(function(){
+                    UtilsSvc.isAppendLoader(false);
+                });
+
+    };
+
+    function handlePostSuccess(response) {
+
+        if (response.data.status === 'success') {
+            AlertSvc.success(ctrl, 'The exemption list is updated.');
+            ctrl.message = 'The information is updated.';
+            ctrl.messageClass = 'alert-success';
+        } else {
+            AlertSvc.error(ctrl, 'The exemption list is not updated due to errors encountered.');
+            ctrl.message = 'The record is not updated due to errors encountered.';
+            ctrl.messageClass = 'alert-warning';
+            console.error(response);
+        }
+    }
+
+    ctrl.updateQueryStringParameter = function (uri, key, value) {
+        const re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+        const separator = uri.includes('?') ? "&" : "?";
+        return uri.match(re) ? uri.replace(re, '$1' + key + "=" + value + '$2') : uri + separator + key + "=" + value;
+    };
+
+    function updateAssessmentItemIds() {
+        let selectedItem = ctrl.assessment_items[ctrl.assessment_item_id];
+
+        // If the selected assessment_item has 'ids', get the unique IDs
+        if (selectedItem && selectedItem.ids) {
+            ctrl.assessment_item_ids = Array.from(new Set(selectedItem.ids)); // Unique IDs
+            ctrl.classification = selectedItem.classification;
+        } else {
+            // If no 'ids' are present, set it to an empty array
+            ctrl.assessment_item_ids = [];
+            ctrl.classification = null;
+        }
+    }
+
+    $scope.$watch('gridOptionsTop', function(newVal, oldVal) {
+        if (newVal !== oldVal) {
+            // Handle updates to gridOptionsTop
+        }
+    }, true); // The 'true' parameter ensures a deep watch
+}
--- src/Controller/Component/NavigationComponent.php
+++ src/Controller/Component/NavigationComponent.php
@@ -32,10 +32,15 @@ class NavigationComponent extends Component
 
     public function addCrumb($title, $options = [])
     {
+        if (!is_countable($options)) {
+            $size_of_options = 0;
+        } else {
+            $size_of_options = sizeof($options);
+        }
         $item = array(
             'title' => __((string)$title),
             'link' => ['url' => $options],
-            'selected' => sizeof($options) == 0
+            'selected' => $size_of_options == 0
         );
         $this->breadcrumbs[] = $item;
         $this->controller->set('_breadcrumbs', $this->breadcrumbs);
@@ -1324,11 +1329,11 @@ class NavigationComponent extends Component
                     $InstitutionStudentsTable->aliasField('institution_id') => $institutionID,
                 ])
                 ->order([$InstitutionStudentsTable->aliasField('created') => 'DESC']);
-            
+
             $results = $query->all()->extract('id')->toArray();
             $institutionStudentId = !empty($results) ? $results[0] : null;
         }
-        
+
 
         $queryString = $this->controller->paramsEncode([
             'id' => $studentID,
@@ -2410,7 +2415,7 @@ class NavigationComponent extends Component
                     '1' => $queryString], // POCOR-8415
                 'selected' => ['GuardianNavs.StudentUser']
             ],
-            
+
             'GuardianNavs.StudentProgrammes.index' => [
                 'title' => 'Academic',
                 'parent' => 'GuardianNavs.GuardianNavs.index',
