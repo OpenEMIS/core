f33cf23a669bd370aed6f181df298127d996e71f
Shikha Sahu <shikha.sahu@madvit.com>
2024-12-26 18:17:27 +0530

    POCOR-8659: System Setup > System Configuration > Themes: It is not working properly | Status: Done

--- config/Migrations/20241017071806_POCOR8659.php
+++ config/Migrations/20241017071806_POCOR8659.php
@@ -15,16 +15,24 @@ class POCOR8659 extends AbstractMigration
      */
     public function up()
     {
-        // Check if a row with 'colour' value exists in the 'name' column
-        $row = $this->fetchRow("SELECT * FROM themes WHERE name = 'Colour'");
-
-        if (!$row) {
-            //backup
-            $this->execute('CREATE TABLE `z_8659_themes` LIKE `themes`');
-            $this->execute('INSERT INTO `z_8659_themes` SELECT * FROM `themes`');
-
-            $this->execute("INSERT INTO `themes` (`id`, `name`, `value`, `content`, `default_value`, `default_content`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES (NULL, 'Colour', NULL, NULL, '6699CC', NULL, NULL, CURRENT_TIMESTAMP, '1', CURRENT_TIMESTAMP)");
-        }
+        //backup
+        $this->execute('CREATE TABLE `z_8659_themes` LIKE `themes`');
+        $this->execute('INSERT INTO `z_8659_themes` SELECT * FROM `themes`');
+        // Check if a row with 'colour' value not exists in the 'name' column then insert
+        $this->execute("INSERT INTO `themes` (`id`, `name`, `value`, `content`, `default_value`, `default_content`, `modified_user_id`, `modified`, `created_user_id`, `created`)
+            SELECT NULL, 'Colour', NULL, NULL, '6699CC', NULL, NULL, CURRENT_TIMESTAMP, '1', CURRENT_TIMESTAMP
+            WHERE NOT EXISTS (
+                SELECT 1 
+                FROM `themes` 
+                WHERE `name` = 'Colour'
+            )");
+        // Check if a row with 'colour' value exists in the 'name' column but default_value empty then update this
+        $this->execute(
+            "UPDATE `themes`
+            SET `default_value` = '6699CC'
+            WHERE `name` = 'Colour'
+            AND (`default_value` IS NULL OR `default_value` = '')"
+        );
     }
 
     //rollback
