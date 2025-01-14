e5ac69794b9f19af7995c0188d50eb4aa60c3ee5
Shikha Sahu <shikha.sahu@madvit.com>
2025-01-14 09:53:34 +0530

    POCOR-8811: Institutions > Staff > Career > Appraisal: When adding 'Note' type field under staff Appraisal Form, the 'Note' type field is editable upon editing staff Appraisal Form | Status

--- plugins/Institution/src/Model/Behavior/AppraisalBehavior.php
+++ plugins/Institution/src/Model/Behavior/AppraisalBehavior.php
@@ -337,6 +337,11 @@ class AppraisalBehavior extends Behavior
                 $key = 'appraisal_text_answers';
                 $fieldKey = $key.'.'.$criteriaCounter[$fieldTypeCode];
                 $attr['type'] = 'text';
+                $name = $criteria->field_type->name ?? null;
+                $action = $model->action;
+                if($name == 'Note' && $action == 'edit') {
+                    $attr['type'] = 'readonly';
+                }
                 break;
             case 'DROPDOWN':
                 $key = 'appraisal_dropdown_answers';
