8a5c039a3907cc7bdf26481447d835b43f1214ad
abhinav0789 <abhinav.kumar@madvit.com>
2025-01-22 12:10:48 +0530

    POCOR-8434: code enhance for migration | Status: Done

--- config/Migrations/20241023185901_POCOR8434.php
+++ config/Migrations/20241023185901_POCOR8434.php
@@ -130,15 +130,13 @@ class POCOR8434 extends AbstractMigration
         $this->execute("RENAME TABLE `institution_student_admission` TO `institution_student_enrolment`");
         //Drop FOREIGN KEY CONSTRAINT from `institution_student_enrolment` table
         $this->execute('SET FOREIGN_KEY_CHECKS = 0;');
-        $this->execute("ALTER TABLE `institution_student_enrolment`
-                DROP FOREIGN KEY IF EXISTS `insti_stude_admis_fk_aca_per_id`,
-                DROP FOREIGN KEY IF EXISTS `insti_stude_admis_fk_ass_id`,
-                DROP FOREIGN KEY IF EXISTS `insti_stude_admis_fk_edu_gra_id`,
-                DROP FOREIGN KEY IF EXISTS `insti_stude_admis_fk_ins_cla_id`,
-                DROP FOREIGN KEY IF EXISTS `insti_stude_admis_fk_ins_id`,
-                DROP FOREIGN KEY IF EXISTS `insti_stude_admis_fk_statu_id`,
-                DROP FOREIGN KEY IF EXISTS `insti_stude_admis_fk_stude_id`;");
-        $this->execute('SET FOREIGN_KEY_CHECKS = 1;');
+        $this->execute("ALTER TABLE `institution_student_enrolment` DROP FOREIGN KEY `insti_stude_admis_fk_aca_per_id`;");
+        $this->execute("ALTER TABLE `institution_student_enrolment` DROP FOREIGN KEY `insti_stude_admis_fk_ass_id`;");
+        $this->execute("ALTER TABLE `institution_student_enrolment` DROP FOREIGN KEY `insti_stude_admis_fk_edu_gra_id`;");
+        $this->execute("ALTER TABLE `institution_student_enrolment` DROP FOREIGN KEY `insti_stude_admis_fk_ins_cla_id`;");
+        $this->execute("ALTER TABLE `institution_student_enrolment` DROP FOREIGN KEY `insti_stude_admis_fk_ins_id`;");
+        $this->execute("ALTER TABLE `institution_student_enrolment` DROP FOREIGN KEY `insti_stude_admis_fk_statu_id`;");
+        $this->execute("ALTER TABLE `institution_student_enrolment` DROP FOREIGN KEY `insti_stude_admis_fk_stude_id`;");
         //Not able to add FOREIGN KEY CONSTRAINT in `institution_student_enrolment` table because in this table in assignee_id column have 0 and -1 value 
         //ADD CONSTRAINT insti_stude_enrol_fk_ass_id FOREIGN KEY (assignee_id) REFERENCES security_users(id),
         //ADD CONSTRAINT insti_stude_enrol_fk_ins_cla_id FOREIGN KEY (institution_class_id) REFERENCES institution_classes(id) in `institution_student_admission` table some records have null values in institution_class_id column
@@ -273,13 +271,12 @@ class POCOR8434 extends AbstractMigration
         //DROP institution_student_admission table 
         $this->execute('DROP TABLE IF EXISTS `institution_student_admission`');
         //Drop FOREIGN KEY CONSTRAINT from `z_8434_institution_student_admission` table
-        $this->execute("ALTER TABLE `z_8434_institution_student_admission`
-            DROP FOREIGN KEY z_insti_stude_admis_fk_aca_per_id,
-            DROP FOREIGN KEY z_insti_stude_admis_fk_edu_gra_id,
-            DROP FOREIGN KEY z_insti_stude_admis_fk_ins_cla_id,
-            DROP FOREIGN KEY z_insti_stude_admis_fk_ins_id,
-            DROP FOREIGN KEY z_insti_stude_admis_fk_status_id,
-            DROP FOREIGN KEY z_insti_stude_admis_fk_stude_id;");
+        $this->execute("ALTER TABLE `z_8434_institution_student_admission` DROP FOREIGN KEY `z_insti_stude_admis_fk_aca_per_id`;");
+        $this->execute("ALTER TABLE `z_8434_institution_student_admission` DROP FOREIGN KEY `z_insti_stude_admis_fk_edu_gra_id`;");
+        $this->execute("ALTER TABLE `z_8434_institution_student_admission` DROP FOREIGN KEY `z_insti_stude_admis_fk_ins_cla_id`;");
+        $this->execute("ALTER TABLE `z_8434_institution_student_admission` DROP FOREIGN KEY `z_insti_stude_admis_fk_ins_id`;");
+        $this->execute("ALTER TABLE `z_8434_institution_student_admission` DROP FOREIGN KEY `z_insti_stude_admis_fk_status_id`;");
+        $this->execute("ALTER TABLE `z_8434_institution_student_admission` DROP FOREIGN KEY `z_insti_stude_admis_fk_stude_id`;");
         //Restore `institution_student_admission` table 
         $this->execute('RENAME TABLE `z_8434_institution_student_admission` TO `institution_student_admission`');
         //Add FOREIGN KEY CONSTRAINT in `institution_student_admission`
