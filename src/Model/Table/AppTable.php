f799092e91e0aabd7e4873abf81688700243a899
Dr Khindol <khindol.madraimov@gmail.com>
2025-04-02 02:07:14 +0300

    finished-1

--- src/Model/Table/AppTable.php
+++ src/Model/Table/AppTable.php
@@ -281,8 +281,10 @@ class AppTable extends Table
                     ->first();
             if ($field == 'openemis_no' && !empty($fieldLabel['name'])) {
                  return $fieldLabel['name'];
+
             } else if ($field == 'openemis_no') {
                 return self::OpenEMIS;
+
     		} else if ($field == 'fax' && !empty($fieldLabel['name'])) {
     		    return $fieldLabel['name'];
             }
