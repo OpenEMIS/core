23f19bd1af7c9cde705065815d44a1d958d8b3cf
Dr Khindol <khindol.madraimov@gmail.com>
2025-04-13 23:51:18 -0400

    fixed labels as well

--- plugins/Institution/templates/Element/Classes/students.php
+++ plugins/Institution/templates/Element/Classes/students.php
@@ -196,13 +196,14 @@
 									'controller' => 'Institutions',
 									'action' => 'StudentUser',
 									'view',
+                                    // POCOR-9046 start
 									$this->ControllerAction->paramsEncode([
                                         'student_id' => $obj->student_user_id,
                                         'id' => $obj->student_user_id,
                                         'institution_id' => $obj->institution_id,])
 								];
     							?>
-							<?= $this->html->link($obj->student_openemis_no, $url) ?>
+							<?= $this->html->link($obj->student_openemis_no, $url) // POCOR-9046 end ?>
 						</td>
 						<td><?php
 							//START: POCOR-6623
