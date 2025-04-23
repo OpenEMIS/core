302c85fb0d63bd77734870e16889182aec732036
Dr Khindol <khindol.madraimov@gmail.com>
2025-04-22 20:27:56 -0400

    made changes-1

--- plugins/Institution/src/Model/Table/InstitutionBuildingsTable.php
+++ plugins/Institution/src/Model/Table/InstitutionBuildingsTable.php
@@ -114,7 +114,7 @@ class InstitutionBuildingsTable extends ControllerActionTable
                         // Skip validation when datatype is 'copy'
                         return true;
                     }
-            
+
                     // Proceed with validation when datatype is not 'copy'
                     return $this->validateCustomLandSize($value, 'Maximum_institution_infrastructure_building_size', $context);
                 },
@@ -333,7 +333,7 @@ class InstitutionBuildingsTable extends ControllerActionTable
 
         // Building Types
         list($typeOptions, $selectedType) = array_values($this->getTypeOptions(['withAll' => true]));
-        if ($selectedType != '-1') {
+        if ($selectedType && $selectedType != '-1') { // POCOR-9074
             $query->where([$this->aliasField('building_type_id') => $selectedType]);
         }
         $this->controller->set(compact('typeOptions', 'selectedType'));
