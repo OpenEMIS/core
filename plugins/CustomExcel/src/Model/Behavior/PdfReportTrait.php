5ad09446a236146e13f545361d1dc87d8e0021cf
Dr Khindol <khindol.madraimov@gmail.com>
2025-05-15 12:57:48 +0300

    normalized breaks-5

--- plugins/CustomExcel/src/Model/Behavior/PdfReportTrait.php
+++ plugins/CustomExcel/src/Model/Behavior/PdfReportTrait.php
@@ -326,12 +326,14 @@ trait PdfReportTrait
                     $finalStyle .= "$key: $value; ";
                 }
                 $normalized = $this->normalizeBorderStylesOnly($finalStyle);
-                $normalized = $this->normalizeTextWrappingStyles($normalized);
+//                $normalized = $this->normalizeTextWrappingStyles($normalized);
                 $cell->setAttribute('style', trim($normalized));
-                // Replace &nbsp; and non-breaking spaces in cell text with regular space
-//                $cell->nodeValue = str_replace("\u{00A0}", ' ', $cell->nodeValue); // PHP 7+ Unicode
-//                $cell->nodeValue = str_replace('&nbsp;', ' ', $cell->nodeValue);   // literal HTML entity
-//                $cell->nodeValue = preg_replace('/\s+/u', ' ', $cell->nodeValue);
+                $cell->nodeValue = str_replace("\u{00A0}", ' ', $cell->nodeValue);
+                $cell->nodeValue = str_replace('&nbsp;', ' ', $cell->nodeValue);
+// Optional: normalize multiple spaces
+                $cell->nodeValue = preg_replace('/\s+/u', ' ', $cell->nodeValue);
+
+
             }
 
             // Remove class
