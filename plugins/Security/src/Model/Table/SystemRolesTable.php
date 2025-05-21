4776c44a750c88192c2be49bccab183b43e6dc95
Dr Khindol <khindol.madraimov@gmail.com>
2025-05-21 03:26:07 +0300

    fixed

--- plugins/Security/src/Model/Table/SystemRolesTable.php
+++ plugins/Security/src/Model/Table/SystemRolesTable.php
@@ -64,7 +64,7 @@ class SystemRolesTable extends AppTable
 
     public function indexBeforeAction(Event $event)
     {
-        $this->ControllerAction->setFieldOrder(['order', // POCOR
+        $this->ControllerAction->setFieldOrder(['order', // POCOR-9140
             'visible',
             'name',
             'permissions']);
