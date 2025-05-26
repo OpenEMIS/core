89b5e842899ae7420bbe66f04292a03c5d59835c
Dr Khindol <khindol.madraimov@gmail.com>
2025-05-26 10:23:22 +0300

    added small try-catch-6

--- plugins/Institution/src/Model/Table/InstitutionSurveysTable.php
+++ plugins/Institution/src/Model/Table/InstitutionSurveysTable.php
@@ -210,18 +210,24 @@ class InstitutionSurveysTable extends ControllerActionTable
             foreach ($rules as $key => $rule) {
                 foreach ($rule as $supportFieldKey => $options) {
                     $supportQuestionOptions = json_decode($options);
-                    if (isset($newData[$supportFieldKey])) {
-                        $userSelectedOption = $newData[$supportFieldKey]['number_value'];
-                        if (is_array($supportQuestionOptions)) {
-                            if (!(in_array($userSelectedOption, $supportQuestionOptions)) && $newData[$key]['mandatory'] == 1) {
+                    if (!empty($supportQuestionOptions)) {
+                        if (isset($newData[$supportFieldKey])) {
+                            $userSelectedOption = $newData[$supportFieldKey]['number_value'];
+
+                            // Replacing in_array() with isset() to check keys instead
+                            if (!isset($supportQuestionOptions[$userSelectedOption]) && $newData[$key]['mandatory'] == 1) {
                                 $dataAliasKey = $newData[$key]['dataKey'];
                                 $data[$this->getAlias()]['custom_field_values'][$dataAliasKey]['mandatory'] = 0;
                             }
-                        } else {
-                            $this->Alert->error(
-                                "There is archive process currently running. Please try again once the process has ended",
-                                ['type' => 'string', 'reset' => true]
-                            );
+                        }
+                    } else {
+                        // supportQuestionOptions is empty or missing
+                        $this->Alert->error(
+                            __('There is a missing question option configuration. Please contact the administrator.'),
+                            ['type' => 'string', 'reset' => true]
+                        );
+
+                        if (isset($event)) {
                             $event->stopPropagation();
                         }
                     }
