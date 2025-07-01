31f3ed21b11269634777823babc9ce435fa08c7e
Dr Khindol <khindol.madraimov@gmail.com>
2025-07-01 22:03:28 +0300

    POCOR-9243: start checking-4

--- plugins/Institution/src/Model/Table/InstitutionSubjectsTable.php
+++ plugins/Institution/src/Model/Table/InstitutionSubjectsTable.php
@@ -402,7 +402,7 @@ class InstitutionSubjectsTable extends ControllerActionTable
                         foreach ($value['subject_students'] as $student) {
                             //POCOR-6463- in edit page we'll only display those students who are "Enrolled"
                             if ($student->student_status_id != 1) {
-                                unset($arrResults[0]['subject_students'][$i]);
+//                                unset($arrResults[0]['subject_students'][$i]);
                             }
                             $i++;
                         }
