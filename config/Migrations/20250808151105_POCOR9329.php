24e1bb3b45ab5b60d2d6dde7ee4ca5d836689814
unknown <divya.vishwakarma@metadesignsolutions.com>
2025-08-12 11:35:35 +0530

    POCOR-9329||System is still showing fax column despite having the fax removed from the institutions table

--- config/Migrations/20250808151105_POCOR9329.php
+++ config/Migrations/20250808151105_POCOR9329.php
@@ -11,18 +11,40 @@ class POCOR9329 extends AbstractMigration
         $this->execute('CREATE TABLE `zz_9329_import_mapping` LIKE `import_mapping`');
         $this->execute('INSERT INTO `zz_9329_import_mapping` SELECT * FROM `import_mapping`');
 
-        // Delete only the target row
-        $this->execute("
-            DELETE FROM `import_mapping`
+        // Get fax order dynamically 
+        $faxOrder = $this->fetchRow("
+            SELECT `order` FROM `import_mapping`
             WHERE `model` = 'Institution.Institutions'
               AND `column_name` = 'fax'
         ");
+
+        if ($faxOrder) {
+            $orderNum = (int) $faxOrder['order'];
+
+            // Delete fax row
+            $this->execute("
+                DELETE FROM `import_mapping`
+                WHERE `model` = 'Institution.Institutions'
+                  AND `column_name` = 'fax'
+            ");
+
+            // Shift all rows after fax order by -1
+            $this->execute("
+                UPDATE `import_mapping`
+                SET `order` = `order` - 1
+                WHERE `model` = 'Institution.Institutions'
+                  AND `order` > {$orderNum}
+            ");
+        }
     }
 
     public function down(): void
     {
-        // Restore table from backup
+        // Restore from backup
         $this->execute('DROP TABLE IF EXISTS `import_mapping`');
         $this->execute('RENAME TABLE `zz_9329_import_mapping` TO `import_mapping`');
+
+        // Drop backup table if it somehow still exists (cleanup)
+        $this->execute('DROP TABLE IF EXISTS `zz_9329_import_mapping`');
     }
 }
