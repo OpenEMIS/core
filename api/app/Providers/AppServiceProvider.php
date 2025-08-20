e7c36d2fe3f41e2329f1602647f3d0605a8d163d
Dr Khindol <khindol.madraimov@gmail.com>
2025-08-20 09:12:54 +0500

    POCOR-9352: started checking-5

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -33,8 +33,19 @@ class AppServiceProvider extends ServiceProvider
         //For POCOR-8215 end...
         // AppServiceProvider::boot()
         //For POCOR-9352 start..
+        DB::statement('SET SESSION max_execution_time=60000'); // 60s
+
         DB::listen(function ($query) {
             if ($query->time > 30000) { // миллисекунды
+                Log::warning('Slow query detected', [
+                    'sql' => $query->sql,
+                    'bindings' => $query->bindings,
+                    'time_ms' => $query->time,
+                ]);
+            }
+        });
+        DB::listen(function ($query) {
+            if ($query->time > 300) { // миллисекунды
                 Log::warning('Slow query detected', [
                     'sql' => $query->sql,
                     'bindings' => $query->bindings,
