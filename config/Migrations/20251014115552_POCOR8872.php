d25369d5ad7ce0500dfd63d2ad1c5712f1c5105a
Dr Khindol <khindol.madraimov@gmail.com>
2025-10-14 11:58:34 +0300

    POCOR-9403: started migration

---
+++ config/Migrations/20251014115552_POCOR8872.php
@@ -0,0 +1,135 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+use Cake\Auth\DefaultPasswordHasher;
+use Cake\Utility\Text;
+
+class POCOR4403 extends AbstractMigration
+{
+    public function up()
+    {
+
+
+        $this->backupTables();
+//
+        $this->insertNewExternalDataSourceAttributes();
+//
+        $this->insertNewConfigItems();
+
+    }
+
+    // rollback
+
+    /**
+     * @return void
+     */
+    public function backupTables()
+    {
+        if(!$this->hasTable('z_4403_config_items')){
+            $this->execute('SET FOREIGN_KEY_CHECKS=0;');
+            $this->execute('CREATE TABLE `z_4403_config_items` LIKE `config_items`');
+            $this->execute('INSERT INTO `z_4403_config_items` SELECT * FROM `config_items`');
+            $this->execute('SET FOREIGN_KEY_CHECKS=1;');
+        }
+        if(!$this->hasTable('z_4403_external_data_source_attributes')){
+            $this->execute('SET FOREIGN_KEY_CHECKS=0;');
+            $this->execute('CREATE TABLE `z_4403_external_data_source_attributes` LIKE `external_data_source_attributes`');
+            $this->execute('INSERT INTO `z_4403_external_data_source_attributes` SELECT * FROM `external_data_source_attributes`');
+            $this->execute('SET FOREIGN_KEY_CHECKS=1;');
+        }
+    }
+
+    public function down()
+    {
+        $this->restoreTable();
+    }
+
+    /**
+     * @return void
+     */
+    public function restoreTable()
+    {
+        if ($this->hasTable('z_4403_config_items')) {
+            $this->execute('SET FOREIGN_KEY_CHECKS=0;');
+            $this->execute('DROP TABLE IF EXISTS `config_items`');
+            $this->execute('RENAME TABLE `z_4403_config_items` TO `config_items`');
+            $this->execute('SET FOREIGN_KEY_CHECKS=1;');
+        }
+        if ($this->hasTable('z_4403_external_data_source_attributes')) {
+            $this->execute('SET FOREIGN_KEY_CHECKS=0;');
+            $this->execute('DROP TABLE IF EXISTS `external_data_source_attributes`');
+            $this->execute('RENAME TABLE `z_4403_external_data_source_attributes` TO `external_data_source_attributes`');
+            $this->execute('SET FOREIGN_KEY_CHECKS=1;');
+        }
+    }
+
+    function generateConfigData($name, $code, $type, $label) {
+        return [
+            'id' => NULL,
+            'name' => $name,
+            'code' => $code,
+            'type' => $type,
+            'label' => $label,
+            'value' => '1',
+            'value_selection' => '0',
+            'default_value' => '0',
+            'editable' => '1',
+            'visible' => '1',
+            'field_type' => 'Dropdown',
+            'option_type' => 'completeness',
+            'created_user_id' => '1',
+            'created' => date('Y-m-d H:i:s')
+        ];
+    }
+
+    /**
+     * @return void
+     */
+    public function insertNewConfigItems()
+    {
+        $table = $this->table('config_items');
+        $data = [
+            $this->generateConfigData(
+                'External Data Source - Webhook',
+                'external_data_source_webhook',
+                'External Data Source - Webhook',
+                'External Data Source - Webhook'),
+        ];
+        $table->insert($data)->save();
+    }
+
+    public function insertNewExternalDataSourceAttributes()
+    {
+        $table = $this->table('external_data_source_attributes');
+        $password = 'demo';
+//        $password = (new DefaultPasswordHasher)->hash($password);
+
+        $attributes = [
+            // API Credentials
+            ['api_url', 'api_url', 'https://demo.openemis.org/exams/api/v5'],
+            ['username', 'username', 'admin'],
+            ['password', 'password', $password],
+            ['api_key', 'api_key', 'apikeytest'],
+
+        ];
+
+        $data = array_map(fn($attr) => $this->generateExternalDataSourceAttribute('External Data Source - Webhook', ...$attr), $attributes);
+
+        $table->insert($data)->save();
+    }
+
+    private function generateExternalDataSourceAttribute($type, $field, $name, $value)
+    {
+        return [
+            'id' => Text::uuid(),
+            'external_data_source_type' => $type,
+            'attribute_field' => $field,
+            'attribute_name' => $name,
+            'value' => $value,
+            'created' => date('Y-m-d H:i:s'),
+            'created_user_id' => 1,
+        ];
+    }
+
+}
