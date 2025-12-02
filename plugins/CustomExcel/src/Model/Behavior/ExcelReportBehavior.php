7b0a6818527ea0ed58de0bc62f694217075de15a
Dr Khindol <khindol.madraimov@gmail.com>
2025-12-02 14:09:44 +0300

    POCOR-9479: started-15

--- plugins/CustomExcel/src/Model/Behavior/ExcelReportBehavior.php
+++ plugins/CustomExcel/src/Model/Behavior/ExcelReportBehavior.php
@@ -1516,7 +1516,7 @@ class ExcelReportBehavior extends Behavior
         Log::warning($errorMessage);
 
         // Return placeholder-style array for consistency with normal extract
-        return [ [ '__error' => $errorMessage ] ];
+        return [ '__missing__' => $errorMessage ];
     }
 
     private function combineVarSafe(array $vars, string $keyPath, string $valuePath): array
@@ -1532,6 +1532,7 @@ class ExcelReportBehavior extends Behavior
         Log::warning($errorMessage);
 
         // Return a placeholder array so repeatRows() still works
-        return [ '__missing__' => $errorMessage ];
+
+        return [ [ '__error' => $errorMessage ] ];
     }
 }
