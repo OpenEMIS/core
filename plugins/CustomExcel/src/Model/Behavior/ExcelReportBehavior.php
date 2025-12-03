2ebedaba2fc62d984a6b20e2089fef948edf2de3
Dr Khindol <khindol.madraimov@gmail.com>
2025-12-03 15:48:22 +0300

    POCOR-9479: checking outputs for libreoffice-12

--- plugins/CustomExcel/src/Model/Behavior/ExcelReportBehavior.php
+++ plugins/CustomExcel/src/Model/Behavior/ExcelReportBehavior.php
@@ -202,7 +202,6 @@ class ExcelReportBehavior extends Behavior
         }
 
         if ($this->getConfig('download')) {
-            Log::debug('Im here');
             $tempfile = new File($temppath);
             $tempinfo = $tempfile->info();
             $tempcontent = $tempfile->read();
