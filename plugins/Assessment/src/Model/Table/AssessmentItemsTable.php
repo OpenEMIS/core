1dd30b8ec2f26fdbb160a34d2100081b0ce6d9e7
Ehteram Ahmad <ehteram.ahmad@dataforall.org>
2025-12-04 15:14:15 +0530

    POCOR-9487: Institutions>Performance>Assessments: When user is not a homeroom/secondary/subject teacher, edit assessment button should be hidden STATUS: Done

--- plugins/Assessment/src/Model/Table/AssessmentItemsTable.php
+++ plugins/Assessment/src/Model/Table/AssessmentItemsTable.php
@@ -237,6 +237,28 @@ class AssessmentItemsTable extends AppTable
         $EducationSubject = TableRegistry::get('Education.EducationSubjects');
         $Assessments = TableRegistry::get('Assessment.Assessments');
         $InstitutionSubjectStaff = TableRegistry::get('Institution.InstitutionSubjectStaff');
+        $isHomeRoomTeacherOrSecondaryTeacher = 0;
+
+        //POCOR-9487[START]
+        //Check for Homeroom/Secondary teacher
+        $InstitutionClasses = TableRegistry::get('Institution.InstitutionClasses');
+        $InstitutionClassesSecondaryStaff = TableRegistry::get('Institution.InstitutionClassesSecondaryStaff');
+        $InstitutionClassesData = $InstitutionClasses->find()
+                                ->where([$InstitutionClasses->aliasField('id') => $class_id,
+                                    $InstitutionClasses->aliasField('staff_id') => $logged_in_user_id
+                                    ])
+                                ->toArray();
+
+        $InstitutionClassesSecondaryStaffData = $InstitutionClassesSecondaryStaff->find()
+                                ->where([$InstitutionClassesSecondaryStaff->aliasField('institution_class_id') => $class_id,
+                                    $InstitutionClassesSecondaryStaff->aliasField('secondary_staff_id') => $logged_in_user_id
+                                    ])
+                                ->toArray();
+        if(!empty($InstitutionClassesData) || !empty($InstitutionClassesSecondaryStaffData)){
+            $isHomeRoomTeacherOrSecondaryTeacher = 1;
+        }
+        //POCOR-9487[END]
+
         $query
             ->select([
                 $this->aliasField('education_subject_id'),
@@ -288,13 +310,15 @@ class AssessmentItemsTable extends AppTable
                 $InstitutionSubjectStaff,
                 $logged_in_user_id,
                 $super_admin,
-                $institution_id
+                $institution_id,
+                $isHomeRoomTeacherOrSecondaryTeacher
             ) {
                 return $results->map(function ($row) use (
                     $InstitutionSubjectStaff,
                     $logged_in_user_id,
                     $super_admin,
-                    $institution_id
+                    $institution_id,
+                    $isHomeRoomTeacherOrSecondaryTeacher
                 ) {
                     $row['education_subject_id'] = $row->education_subject_id;
                     $row['id'] = $row->id;
@@ -332,6 +356,26 @@ class AssessmentItemsTable extends AppTable
                     $SecurityRoleFunTable = TableRegistry::get('Security.SecurityRoleFunctions');
                     $SecurityRoleTable = TableRegistry::get('Security.SecurityRoles');
                     $SecurityGroupTable=TableRegistry::get('Security.UserGroups');
+
+                    //POCOR-9487[START]
+                    //Get the dynamic value of security function
+                    $permissionModule = ['Assessments'];
+                    $categories = ['Students'];
+                    $SecurityFunctionsTbl = TableRegistry::get('Security.SecurityFunctions');
+                    $SecurityFunctions = $SecurityFunctionsTbl->find()
+                        ->select([$SecurityFunctionsTbl->aliasField('id')])
+                        ->where([
+                            $SecurityFunctionsTbl->aliasField('name IN') => $permissionModule,
+                            $SecurityFunctionsTbl->aliasField('category IN') => $categories,
+                        ])->enableHydration(false)->toArray();
+                    $funArr = [];
+                    if (!empty($SecurityFunctions)) {
+                        foreach ($SecurityFunctions as $funkey => $funval) {
+                            $funArr[$funkey] = $funval['id'];
+                        }
+                    }
+                    //POCOR-9487[END]
+
                     $securityGroupUserData = $SecurityGroupUsersTable->find('all')
                             ->select([$SecurityGroupUsersTable->aliasField('security_role_id'),
                                     'edit' => $SecurityRoleFunTable->aliasField('_edit'),
@@ -350,7 +394,7 @@ class AssessmentItemsTable extends AppTable
                                 [
                                     $SecurityRoleFunTable->aliasField('security_role_id = ') .
                                     $SecurityGroupUsersTable->aliasField('security_role_id'),
-                                    $SecurityRoleFunTable->aliasField('security_function_id') => 1015,
+                                    $SecurityRoleFunTable->aliasField('security_function_id IN') => $funArr,
                                     // $SecurityRoleFunTable->aliasField('_edit') => '1'
                                 ]
                             )
@@ -375,11 +419,18 @@ class AssessmentItemsTable extends AppTable
                     //POCOR-7551 end
 //                    $this->log($securityGroupUserEditAccessCount, 'debug');
                     if ($securityGroupUserEditAccessCount > 0) {
-                                $row['is_editable'] = 1;
-                                return (array) $row;
-                            } else {
-                                $row['is_editable'] = 0;
-                            }
+                        //POCOR-9487[START]
+                         if($isHomeRoomTeacherOrSecondaryTeacher == 1){
+                            $row['is_editable'] = 1;
+                            return (array) $row;
+                        }else{
+                            $row['is_editable'] = '';
+                            return (array) $row;
+                        }
+                        //POCOR-9487[END]
+                    } else {
+                        $row['is_editable'] = 0;
+                    }
                     //POCOR-7541 end
                     return (array) $row;
                 });
