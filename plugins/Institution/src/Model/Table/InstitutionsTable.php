ead47174684fbcecd54634fb68fbf736e63d0fbf
Dr Khindol <khindol.madraimov@gmail.com>
2025-12-22 20:02:45 +0300

    POCOR-9519: Institution Overview now gives link to a manual

--- plugins/Institution/src/Model/Table/InstitutionsTable.php
+++ plugins/Institution/src/Model/Table/InstitutionsTable.php
@@ -1816,6 +1816,25 @@ class InstitutionsTable extends ControllerActionTable
                 $extra['toolbarButtons'][$key] = $button;
             }
         }
+        // POCOR-9519 start
+        $is_manual_exist = $this->getManualUrl('Institutions', 'Institution', 'General');
+        if (!empty($is_manual_exist)) {
+            $btnAttr = [
+                'class' => 'btn btn-xs btn-default icon-big',
+                'data-toggle' => 'tooltip',
+                'data-placement' => 'bottom',
+                'escape' => false,
+                'target' => '_blank'
+            ];
+
+            $helpBtn['url'] = $is_manual_exist['url'];
+            $helpBtn['type'] = 'button';
+            $helpBtn['label'] = '<i class="fa fa-question-circle"></i>';
+            $helpBtn['attr'] = $btnAttr;
+            $helpBtn['attr']['title'] = __('Help');
+            $extra['toolbarButtons']['help'] = $helpBtn;
+        }
+    // POCOR-9519 end
     }
 
     public function viewAfterAction(Event $event, Entity $entity, ArrayObject $extra)
