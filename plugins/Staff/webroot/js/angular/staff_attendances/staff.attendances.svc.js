ea1255a969553b9bee05c97d1ac8a86bbc002f8f
tixuz <hindolmirzo@gmail.com>
2026-02-09 07:15:49 +0300

    POCOR-9564: Made a Fix for Saving Bahamas in Tongas-1

--- plugins/Staff/webroot/js/angular/staff_attendances/staff.attendances.svc.js
+++ plugins/Staff/webroot/js/angular/staff_attendances/staff.attendances.svc.js
@@ -29,7 +29,7 @@ function StaffAttendancesSvc($http, $q, $filter, KdDataSvc, AlertSvc, UtilsSvc) 
         getAcademicPeriodOptions: getAcademicPeriodOptions,
         getWeekListOptions: getWeekListOptions,
         getStaffAttendances: getStaffAttendances,
-        getColumnDefs: getColumnDefs
+        getColumnDefs: getColumnDefs,
     };
     return service;
 
@@ -454,6 +454,4 @@ function StaffAttendancesSvc($http, $q, $filter, KdDataSvc, AlertSvc, UtilsSvc) 
             console.log(e.message);
         }
     }
-
-
 };
