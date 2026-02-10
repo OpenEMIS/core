0785d923c3ff90d927032ec05da68f1026a1acf6
tixuz <hindolmirzo@gmail.com>
2026-02-10 07:10:42 +0300

    POCOR-9569: students with status 'TRANSFERRED','WITHDRAWN' are removed from listing-1

--- plugins/Institution/src/Model/Table/ReportCardStatusesTable.php
+++ plugins/Institution/src/Model/Table/ReportCardStatusesTable.php
@@ -1849,10 +1849,16 @@ class ReportCardStatusesTable extends ControllerActionTable
         $classStudentsTable = TableRegistry::getTableLocator()->get('Institution.InstitutionClassStudents');
         $where = [];
         $where[$classStudentsTable->aliasField('institution_class_id')] = $institutionClassId;
+
         if (!is_null($studentId)) {
             $where[$classStudentsTable->aliasField('student_id')] = $studentId;
         }
+        // POCOR-9569: filter out students with withdrawn or transferred status
+        $StudentStatuses = TableRegistry::getTableLocator()->get('Student.StudentStatuses');
+        $where[$StudentStatuses->aliasField('code NOT IN ')] = ['TRANSFERRED','WITHDRAWN'];
+        // POCOR-9569 end
         $classStudents = $classStudentsTable->find()
+            ->contain('StudentStatuses')
             ->select([
                 $classStudentsTable->aliasField('student_id'),
                 $classStudentsTable->aliasField('institution_id'),
