f23cb790ebcb59aaab7db0760d69222ec5d32ea3
Prajakta K <prajakta.kalghatgi@dataforall.org>
2026-03-12 16:19:21 +0530

    POCOR-9599.The system enters exact period weight.| Status: Done

--- config/Migrations/20260312062543_POCOR9599.php
+++ config/Migrations/20260312062543_POCOR9599.php
@@ -11,18 +11,15 @@ class POCOR9599 extends AbstractMigration
         $this->execute('CREATE TABLE `zz_9599_assessment_periods` LIKE `assessment_periods`');
         $this->execute('INSERT INTO `zz_9599_assessment_periods` SELECT * FROM `assessment_periods`');
 
-        // Update column precision to prevent rounding of weight values
-        $this->execute('ALTER TABLE `assessment_periods` 
+        // Modify column precision to prevent rounding
+        $this->execute('ALTER TABLE `assessment_periods`
                         MODIFY `weight` DECIMAL(6,5) NULL DEFAULT 0.00000');
     }
 
     public function down(): void
     {
-        // Revert column precision to original definition
-        $this->execute('ALTER TABLE `assessment_periods` 
-                        MODIFY `weight` DECIMAL(6,2) NULL DEFAULT 0.00');
-
-        // Drop backup table
-        $this->execute('DROP TABLE IF EXISTS `zz_9599_assessment_periods`');
+        // Restore from backup
+        $this->execute('DROP TABLE IF EXISTS `assessment_periods`');
+        $this->execute('RENAME TABLE `zz_9599_assessment_periods` TO `assessment_periods`');
     }
 }
