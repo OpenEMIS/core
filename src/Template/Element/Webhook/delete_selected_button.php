26b66ace136ff439f8c74457614dc829f811f5f7
Dr Khindol Madraimov <khindol.madraimov@dataforall.org>
2026-03-15 06:05:48 +0300

    POCOR-9257: Add bulk select/delete template elements for webhook queue and logs

---
+++ src/Template/Element/Webhook/delete_selected_button.php
@@ -0,0 +1,11 @@
+<?php
+//POCOR-9257: Delete selected button for webhook bulk actions
+?>
+<button id="webhook-delete-selected-btn"
+        class="btn btn-danger btn-xs"
+        disabled="disabled"
+        title="<?= __('Delete Selected') ?>"
+        data-toggle="tooltip"
+        data-placement="bottom">
+    <i class="fa fa-trash"></i> <?= __('Delete Selected') ?>
+</button>
