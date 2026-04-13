cbab736d0d1986564e77b59e778e3e89ef982fb6
unknown <divya.vishwakarma@dataforall.org>
2026-04-13 15:05:42 +0530

    POCOR-9634||Administrations > Surveys : Filters and Recipients tab are not showing even though permission is given

---
+++ config/Migrations/20260413021413_POCOR9634.php
@@ -0,0 +1,34 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class POCOR9634 extends AbstractMigration
+{
+
+    /**
+     * Change Method.
+     * @author Poonam Kharka <poonam.kharka@mail.valuecoders.com>
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function up()
+    {
+       //backup of security_functions table
+        $this->execute('CREATE TABLE `zz_9634_security_functions` LIKE `security_functions`');
+        $this->execute('INSERT INTO `zz_9634_security_functions` SELECT * FROM `security_functions`');
+
+        // update for permission 
+        $this->execute("UPDATE security_functions SET _view = 'Filters.index|Filters.view', _edit = 'Filters.edit', _add = 'Filters.add', _delete = 'Filters.remove' WHERE name = 'Filters' AND controller = 'Surveys' AND module = 'Administration' AND category = 'Survey'");
+
+        $this->execute("UPDATE security_functions SET _view = 'Recipients.index|Recipients.view', _edit = NULL, _add = NULL, _delete = NULL WHERE name = 'Recipients' AND controller = 'Surveys' AND module = 'Administration' AND category = 'Survey'");
+    }
+
+
+    //rollback migration script
+    public function down()
+    {
+        $this->execute('DROP TABLE IF EXISTS `security_functions`');
+        $this->execute('RENAME TABLE `zz_9634_security_functions` TO `security_functions`');
+    }
+}
