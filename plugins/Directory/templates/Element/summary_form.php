7fc1ab155c6ac0f02e6031583444ad64804565fb
Dr Khindol Madraimov <khindol.madraimov@dataforall.org>
2026-05-22 07:11:34 +0300

    POCOR-9590: drop inline comment from Preferred cell — keep template tight

--- plugins/Directory/templates/Element/summary_form.php
+++ plugins/Directory/templates/Element/summary_form.php
@@ -79,7 +79,7 @@
                                             <td class="vertical-align-top">
                                                 {{selectedUserData.nationality_name}}
                                             </td>
-                                            <td class="vertical-align-top"><?= __('Yes') //POCOR-9590: the only identity on a new person is preferred=1 (DB default) — display matched ?></td>
+                                            <td class="vertical-align-top"><?= __('Yes') ?></td>
                                         </tr>
                                         </tbody>
                                     </table>
