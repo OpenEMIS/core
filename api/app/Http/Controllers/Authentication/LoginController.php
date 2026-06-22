297e5ca58d72586991052583ff9bf15fc6eac269
unknown <divya.vishwakarma@dataforall.org>
2026-06-22 11:15:18 +0530

    POCOR-9745

--- api/app/Http/Controllers/Authentication/LoginController.php
+++ api/app/Http/Controllers/Authentication/LoginController.php
@@ -78,7 +78,7 @@ class LoginController extends Controller
             if ($request->has('enc') && !empty($request->enc)) {
                 switch ($request->enc) {
                     case 'RSA-OAEP-256':
-                        $privateKey = file_get_contents(storage_path('private.pem'));
+                        $privateKey = file_get_contents(public_path('private.pem'));
                         openssl_private_decrypt(
                             base64_decode($password),
                             $decryptedPassword,
