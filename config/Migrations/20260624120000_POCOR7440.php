5e9ee54b2fdc76d4fe8f36dcff7e45e1d3e4a20f
Ehteram Ahmad <ehteram.ahmad@dataforall.org>
2026-06-30 14:34:42 +0530

    POCOR-7440: Migration script

--- config/Migrations/20260624120000_POCOR7440.php
+++ config/Migrations/20260624120000_POCOR7440.php
@@ -7,15 +7,15 @@ class POCOR7440 extends AbstractMigration
 {
     public function up(): void
     {
-        // $this->execute('CREATE TABLE IF NOT EXISTS `zz_9738_config_items` LIKE `config_items`');
-        // $this->execute('INSERT INTO `zz_9738_config_items` SELECT * FROM `config_items`');
+        $this->execute('CREATE TABLE IF NOT EXISTS `zz_9738_config_items` LIKE `config_items`');
+        $this->execute('INSERT INTO `zz_9738_config_items` SELECT * FROM `config_items`');
         $this->execute('DROP TABLE IF EXISTS `security_user_passwords`');
 
         $exists = $this->fetchRow("SELECT `id` FROM `config_items` WHERE `code` = 'password_rotation'");
-        // if (empty($exists)) {
-        //     $this->execute("INSERT INTO `config_items` (`name`, `code`, `type`, `label`, `value`, `value_selection`, `default_value`, `editable`, `visible`, `field_type`, `option_type`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES
-        //         ('Password Rotation', 'password_rotation', 'Password', 'Password Rotation', '0', '', '0', '0', '1', 'Dropdown', 'completeness', NULL, NULL, '1', CURRENT_TIMESTAMP)");
-        // }
+        if (empty($exists)) {
+            $this->execute("INSERT INTO `config_items` (`name`, `code`, `type`, `label`, `value`, `value_selection`, `default_value`, `editable`, `visible`, `field_type`, `option_type`, `modified_user_id`, `modified`, `created_user_id`, `created`) VALUES
+                ('Password Rotation', 'password_rotation', 'Password', 'Password Rotation', '0', '', '0', '0', '1', 'Dropdown', 'completeness', NULL, NULL, '1', CURRENT_TIMESTAMP)");
+        }
 
 
         $table = $this->table('security_user_passwords', [
